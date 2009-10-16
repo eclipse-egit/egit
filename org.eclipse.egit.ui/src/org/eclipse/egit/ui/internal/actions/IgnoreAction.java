c202fb67b80be93260974cde21d872a64a4561e6
Alex Blewitt <alex.blewitt@gmail.com>
2009-10-16 20:01:19 +0100

    Added .gitignore action
    This adds .gitignore to the object contribution menu, and adds
    the highlighted elements to the .gitignore file
    
    Fixes bug 291133
    
    Signed-off-by: Alex Blewitt <alex.blewitt@gmail.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/IgnoreAction.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/IgnoreAction.java
@@ -8,7 +8,6 @@
 package org.eclipse.egit.ui.internal.actions;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
@@ -25,37 +24,45 @@ import org.eclipse.team.core.Team;
 
 /**
  * Action for ignoring files via .gitignore
- *
+ * 
  */
 public class IgnoreAction extends RepositoryAction {
-	
+
 	@SuppressWarnings("restriction")
 	@Override
 	public void run(IAction action) {
 		final IResource[] resources = getSelectedResources();
 		if (resources.length == 0)
 			return;
-		
-		WorkspaceJob job = new WorkspaceJob("Ignore Git resources") { 
-			public IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException {
+
+		WorkspaceJob job = new WorkspaceJob("Ignore Git resources") {
+			public IStatus runInWorkspace(IProgressMonitor monitor)
+					throws CoreException {
 				monitor.beginTask("Ignoring Git resources", resources.length);
 				try {
 					for (IResource resource : resources) {
-						// TODO This is pretty inefficient; multiple ignores in the same directory cause multiple writes.
-						// NB This does the same thing in DecoratableResourceAdapter, but neither currently consult .gitignore
+						// TODO This is pretty inefficient; multiple ignores in
+						// the same directory cause multiple writes.
+						// NB This does the same thing in
+						// DecoratableResourceAdapter, but neither currently
+						// consult .gitignore
 						if (!Team.isIgnoredHint(resource)) {
 							IContainer container = resource.getParent();
-							IFile gitignore = container.getFile(new Path(Constants.GITIGNORE_FILENAME));
+							IFile gitignore = container.getFile(new Path(
+									Constants.GITIGNORE_FILENAME));
 							String entry = "/" + resource.getName() + "\n"; //$NON-NLS-1$  //$NON-NLS-2$
-							// TODO What is the character set and new-line convention?
-							if(gitignore.exists()) {
-								// This is ugly. CVS uses an internal representation of the .gitignore to re-write/overwrite each time.
-								ByteArrayOutputStream out = new ByteArrayOutputStream(2048);
-								out.write(entry.getBytes(Constants.CHARACTER_ENCODING)); // TODO Default encoding?
-								gitignore.appendContents(new ByteArrayInputStream(out.toByteArray()),true,true,monitor);
+							// TODO What is the character set and new-line
+							// convention?
+							if (gitignore.exists()) {
+								gitignore.appendContents(
+										new ByteArrayInputStream(entry
+												.getBytes(Constants.CHARACTER_ENCODING)), true,
+										true, monitor);
 							} else {
-								ByteArrayInputStream bais = new ByteArrayInputStream( entry.getBytes(Constants.CHARACTER_ENCODING) ); 
-								gitignore.create( bais,true,monitor);					
+								ByteArrayInputStream bais = new ByteArrayInputStream(
+										entry
+												.getBytes(Constants.CHARACTER_ENCODING));
+								gitignore.create(bais, true, monitor);
 							}
 						}
 						monitor.worked(1);
@@ -64,12 +71,15 @@ public class IgnoreAction extends RepositoryAction {
 				} catch (CoreException e) {
 					throw e;
 				} catch (Exception e) {
-					throw new CoreException(new Status(IStatus.ERROR, "org.eclipse.egit.ui", "Unable to ignore resources", e)); //$NON-NLS-1$
+					throw new CoreException(
+							new Status(
+									IStatus.ERROR,
+									"org.eclipse.egit.ui", "Unable to ignore resources", e)); //$NON-NLS-1$
 				}
-				return Status.OK_STATUS;			
+				return Status.OK_STATUS;
 			}
 		};
-		job.schedule();		
+		job.schedule();
 	}
 
 	@SuppressWarnings("restriction")
@@ -80,7 +90,8 @@ public class IgnoreAction extends RepositoryAction {
 
 		IResource[] resources = getSelectedResources();
 		for (IResource resource : resources) {
-			// NB This does the same thing in DecoratableResourceAdapter, but neither currently consult .gitignore
+			// NB This does the same thing in DecoratableResourceAdapter, but
+			// neither currently consult .gitignore
 			if (!Team.isIgnoredHint(resource))
 				return true;
 		}
