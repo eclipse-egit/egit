331aeda0c3b8d340e5efbe21c5fb29e91c5f6596
Mykola Nikishov <mn@mn.com.ua>
2009-10-23 13:58:38 +0300

    Remove a test project from workspace before creation
    Creating a test project when it exists throws:
    
        org.eclipse.core.internal.resources.ResourceException: Resource '/Project-1' already exists.
    
    Change-Id: I9d8d548fa8ebc697467f33a387ad2ae8ba78f022
    Signed-off-by: Mykola Nikishov <mn@mn.com.ua>

--- org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/GitTestCase.java
+++ org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/GitTestCase.java
@@ -26,7 +26,7 @@ public abstract class GitTestCase extends TestCase {
 
 	protected void setUp() throws Exception {
 		super.setUp();
-		project = new TestProject();
+		project = new TestProject(true);
 		checkNotNested();
 		gitDir = new File(project.getProject().getWorkspace().getRoot()
 				.getRawLocation().toFile(), ".git");
