4f6371397f0880f6ce4c011eb1c8461fe44f9c53
Robin Rosenberg <robin.rosenberg@dewire.com>
2010-01-26 07:34:12 +0100

    Create an automatic UI test for the Git Import Wizard
    Things like dialogs and such require a different approach. The
    strategy is to use SWTBot for runnings UI-oriented tests.
    
    Cleanup and restructuring by Ketan Padegaonkar using page
    objects * (http://code.google.com/p/webdriver/wiki/PageObjects)
    
    CQ: 3738
    Bug: 300768
    Change-Id: Ibc4c7a5ad6d9747fceb516c02d96dfc2b09d3b5a
    Signed-off-by: Robin Rosenberg <robin.rosenberg@dewire.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

---
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/AllTests.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (C) 2010, Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.test;
+
+import org.eclipse.egit.ui.wizards.clone.GitCloneWizardTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ GitCloneWizardTest.class })
+public class AllTests {
+	// empty class, don't need anything here
+}
---
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/wizards/clone/GitImportRepoWizard.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (C) 2009, Robin Rosenberg <robin.rosenberg@dewire.com>
+ * Copyright (C) 2010, Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.wizards.clone;
+
+import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+
+public class GitImportRepoWizard {
+
+	private static final SWTWorkbenchBot bot = new SWTWorkbenchBot();
+
+	// TODO: speed it up by calling the wizard using direct eclipse API.
+	public RepoPropertiesPage openWizard() {
+		bot.menu("File").menu("Import...").click();
+		bot.shell("Import").activate();
+
+		bot.tree().expandNode("Git").select("Git Repository");
+
+		bot.button("Next >").click();
+
+		bot.shell("Import Git Repository").activate();
+
+		return new RepoPropertiesPage();
+	}
+
+}
