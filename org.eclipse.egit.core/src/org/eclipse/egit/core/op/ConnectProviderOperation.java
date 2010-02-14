eclipse-egit/egit

commit 667f571b266f7c0dab952733c22bb5f029754f44

Fix enabling team operations on project import
The old code only worked if .git directory was one level above
the Eclipse projects being imported.

Bug: 298027
Change-Id: I5eb0a867b2d6fa8e1f63abfa2291950507fdf3f3
Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

org.eclipse.egit.core/src/org/eclipse/egit/core/op/ConnectProviderOperation.java
@@ -127,16 +127,16 @@ public class ConnectProviderOperation implements IWorkspaceRunnable {
 
 	/**
 	 * @param repos
-	 *            available repositories
+	 *         available repositories
 	 * @param suggestedRepo
-	 *            relative path to git repository
+	 *         relative path to git repository
 	 * @return a repository mapping which corresponds to a suggested repository
 	 *         location, <code>null</code> otherwise
 	 */
 	private RepositoryMapping findActualRepository(
 			Collection<RepositoryMapping> repos, File suggestedRepo) {
 		for (RepositoryMapping rm : repos) {
-			if (Path.fromOSString(rm.getGitDir()).equals(Path.fromOSString(suggestedRepo.getPath())))
+			if (rm.getGitDirAbsolutePath().equals(Path.fromOSString(suggestedRepo.getPath())))
 				return rm;
 		}
 		return null;

org.eclipse.egit.core/src/org/eclipse/egit/core/op/CloneOperation.java
@@ -54,6 +54,8 @@ public class CloneOperation implements IRunnableWithProgress {
 
 	private final File workdir;
 
+	private final File gitdir;
+
 	private final String branch;
 
 	private final String remoteName;
@@ -91,6 +93,7 @@ public class CloneOperation implements IRunnableWithProgress {
 		this.allSelected = allSelected;
 		this.selectedBranches = selectedBranches;
 		this.workdir = workdir;
+		this.gitdir = new File(workdir, Constants.DOT_GIT);
 		this.branch = branch;
 		this.remoteName = remoteName;
 	}
@@ -124,6 +127,14 @@ public class CloneOperation implements IRunnableWithProgress {
 		}
 	}
 
+
+	/**
+	 * @return The git directory which will contain the repository
+	 */
+	public File getGitDir() {
+		return gitdir;
+	}
+
 	private void closeLocal() {
 		if (local != null) {
 			local.close();
@@ -135,7 +146,6 @@ public class CloneOperation implements IRunnableWithProgress {
 			throws URISyntaxException, IOException {
 		monitor.setTaskName("Initializing local repository");
 
-		final File gitdir = new File(workdir, Constants.DOT_GIT);
 		local = new Repository(gitdir);
 		local.create();
 

org.eclipse.egit.core/src/org/eclipse/egit/core/project/RepositoryMapping.java
@@ -253,4 +253,11 @@ public class RepositoryMapping {
 	public String getGitDir() {
 		return gitdirPath;
 	}
+
+	/**
+	 * @return The GIT DIR absolute path
+	 */
+	public IPath getGitDirAbsolutePath() {
+		return container.getLocation().append(getGitDirPath());
+	}
 }

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/GitCloneWizard.java
@@ -140,6 +140,7 @@ public class GitCloneWizard extends Wizard implements IImportWizard {
 
 		final CloneOperation op = new CloneOperation(uri, allSelected,
 				selectedBranches, workdir, branch, remoteName);
+		importProject.setGitDir(op.getGitDir());
 		if (background) {
 			final Job job = new Job(NLS.bind(UIText.GitCloneWizard_jobName, uri
 					.toString())) {

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/GitProjectsImportPage.java
@@ -83,6 +83,11 @@ public class GitProjectsImportPage extends WizardPage {
 	 */
 	private IImportStructureProvider structureProvider;
 
+	/**
+	 * The git directory which will contain the repository
+	 */
+	private File gitDir;
+
 	class ProjectRecord {
 		File projectSystemFile;
 
@@ -95,12 +100,6 @@ public class GitProjectsImportPage extends WizardPage {
 		IProjectDescription description;
 
 		/**
-		 * Relative path to repository, '../.git' is by default. If you'll set
-		 * it null you're on your own, no checks for null performed.
-		 */
-		File repository = new File("../.git"); //$NON-NLS-1$
-
-		/**
 		 * Create a record for a project based on the info in the file.
 		 *
 		 * @param file
@@ -108,19 +107,6 @@ public class GitProjectsImportPage extends WizardPage {
 		ProjectRecord(File file) {
 			projectSystemFile = file;
 			setProjectName();
-		}
-
-		/**
-		 * Create a record for a project based on the info in the file.
-		 *
-		 * @param file
-		 * @param aRepository
-		 *            relative path to repository
-		 */
-		ProjectRecord(File file, File aRepository) {
-			projectSystemFile = file;
-			setProjectName();
-			repository = aRepository;
 		}
 
 		/**
@@ -216,7 +202,7 @@ public class GitProjectsImportPage extends WizardPage {
 	private String lastPath;
 
 	// The last time that the file or folder at the selected path was modified
-	// to mimize searches
+	// to minimize searches
 	private long lastModified;
 
 	private Button shareCheckBox;
@@ -486,6 +472,15 @@ public class GitProjectsImportPage extends WizardPage {
 	}
 
 	/**
+	 * Set the git directory which will contain the repository
+	 *
+	 * @param gitDir
+	 */
+	public void setGitDir(File gitDir) {
+		this.gitDir = gitDir;
+	}
+
+	/**
 	 * Update the list of projects based on path.
 	 *
 	 * @param path
@@ -732,7 +727,7 @@ public class GitProjectsImportPage extends WizardPage {
 					monitor, openTicks));
 			if (share) {
 				ConnectProviderOperation connectProviderOperation = new ConnectProviderOperation(
-						project, record.repository);
+						project, gitDir);
 				connectProviderOperation
 						.run(new SubProgressMonitor(monitor, 20));
 			}

commit 21ed5f8661b9c196d61add77507d44937f1ad1ad

Fix build and test failures
- Correcting versions.

[ms:
- Correcting wrong version for org.eclipse.core.filesystem to [1.2,2.0)
- "Require-Bundle:" needs to use "bundle-version:" to specify version.
- Manifest refered to JUnit 4 but test execution specified JUnit 3.
- Fix tests which have been broken by the change
  667f571b "Fix enabling team operations on project import."
- Split packages between org.eclipse.egit.core and org.eclipse.egit.core.test
  caused ClassNotFoundException in test execution hence moving test packages.
]

Change-Id: Ic76335ecb34daa76431b3d7daae566d97a7413f0
Signed-off-by: Shawn O. Pearce <spearce@spearce.org>
Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

org.eclipse.egit-feature/feature.xml
@@ -26,14 +26,14 @@
    <requires>
       <import plugin="org.eclipse.core.runtime" version="3.4" match="compatible"/>
       <import plugin="org.eclipse.core.resources" version="3.4.0" match="compatible"/>
-      <import plugin="org.eclipse.core.filesystem" version="3.4.0" match="compatible"/>
+      <import plugin="org.eclipse.core.filesystem" version="1.1.0" match="compatible"/>
       <import plugin="org.eclipse.ui" version="3.4.0" match="compatible"/>
       <import plugin="org.eclipse.team.ui" version="3.4.0" match="compatible"/>
       <import plugin="org.eclipse.jface.text" version="3.4.0" match="compatible"/>
       <import plugin="org.eclipse.ui.ide" version="3.4.0" match="compatible"/>
       <import plugin="org.eclipse.ui.workbench.texteditor" version="3.4.0" match="compatible"/>
       <import plugin="org.eclipse.compare" version="3.4.0" match="compatible"/>
-      <import plugin="org.eclipse.core.net" version="3.4.0" match="compatible"/>
+      <import plugin="org.eclipse.core.net" version="1.1.0" match="compatible"/>
       <import plugin="org.eclipse.team.core" version="3.4.0" match="compatible"/>
       <import plugin="org.eclipse.jsch.ui" version="1.1.100" match="compatible"/>
       <import plugin="org.eclipse.jsch.core" version="1.1.100" match="compatible"/>

org.eclipse.egit.core.test/META-INF/MANIFEST.MF
@@ -5,12 +5,13 @@ Bundle-Vendor: Eclipse.org
 Bundle-SymbolicName: org.eclipse.egit.core.test
 Bundle-Version: 0.6.0.qualifier
 Bundle-Activator: org.eclipse.egit.core.test.Activator
-Require-Bundle: org.eclipse.core.runtime;version="[3.4.0,4.0.0)",
- org.eclipse.core.filesystem;version="[3.4.0,4.0.0)",
- org.eclipse.core.resources;version="[3.4.0,4.0.0)",
- org.eclipse.team.core;version="[3.4.0,4.0.0)",
- org.eclipse.jdt.core;version="[3.4.0,4.0.0)",
- org.eclipse.jdt.launching;version="[3.4.0,4.0.0)"
+Require-Bundle: org.eclipse.core.runtime;bundle-version="[3.4.0,4.0.0)",
+ org.eclipse.core.filesystem;bundle-version="[1.2.0,2.0.0)",
+ org.eclipse.core.resources;bundle-version="[3.4.0,4.0.0)",
+ org.eclipse.team.core;bundle-version="[3.4.0,4.0.0)",
+ org.eclipse.jdt.core;bundle-version="[3.4.0,4.0.0)",
+ org.eclipse.jdt.launching;bundle-version="[3.4.0,4.0.0)",
+ org.eclipse.egit.ui;bundle-version="[0.6.0,0.7.0)"
 Bundle-ActivationPolicy: lazy
 Import-Package: junit.framework;version="[4.3.1,5.0.0)",
  org.eclipse.egit.core;version="[0.6.0,0.7.0)",

org.eclipse.egit.core/META-INF/MANIFEST.MF
@@ -6,11 +6,11 @@ Bundle-Version: 0.6.0.qualifier
 Bundle-Activator: org.eclipse.egit.core.Activator
 Bundle-Vendor: %provider_name
 Bundle-Localization: plugin
-Require-Bundle: org.eclipse.core.runtime;version="[3.4.0,4.0.0)",
- org.eclipse.team.core;version="[3.4.0,4.0.0)",
- org.eclipse.core.resources;version="[3.4.0,4.0.0)",
- org.eclipse.core.filesystem;version="[3.4.0,4.0.0)",
- org.eclipse.ui;version="[3.4.0,4.0.0)"
+Require-Bundle: org.eclipse.core.runtime;bundle-version="[3.4.0,4.0.0)",
+ org.eclipse.team.core;bundle-version="[3.4.0,4.0.0)",
+ org.eclipse.core.resources;bundle-version="[3.4.0,4.0.0)",
+ org.eclipse.core.filesystem;bundle-version="[1.2.0,2.0.0)",
+ org.eclipse.ui;bundle-version="[3.4.0,4.0.0)"
 Export-Package: org.eclipse.egit.core;version="0.6.0",
  org.eclipse.egit.core.internal.storage;version="0.6.0";x-friends:="org.eclipse.egit.ui",
  org.eclipse.egit.core.internal.util;version="0.6.0";x-friends:="org.eclipse.egit.ui",

org.eclipse.egit.core/src/org/eclipse/egit/core/op/ConnectProviderOperation.java
@@ -51,7 +51,7 @@ public class ConnectProviderOperation implements IWorkspaceRunnable {
 	 *            the project to connect to the Git team provider.
 	 */
 	public ConnectProviderOperation(final IProject proj) {
-		this(proj, new File(Constants.DOT_GIT));
+		this(proj, proj.getLocation().append("/").append(Constants.DOT_GIT).toFile());
 	}
 
 	/**
@@ -60,7 +60,7 @@ public class ConnectProviderOperation implements IWorkspaceRunnable {
 	 * @param proj
 	 *            the project to connect to the Git team provider.
 	 * @param pathToRepo
-	 *            relative path to the repository
+	 *            absolute path to the repository
 	 */
 	public ConnectProviderOperation(final IProject proj, File pathToRepo) {
 		this.projects.put(proj, pathToRepo);

org.eclipse.egit.ui/META-INF/MANIFEST.MF
@@ -7,23 +7,25 @@ Bundle-Activator: org.eclipse.egit.ui.Activator
 Bundle-Vendor: %provider_name
 Bundle-Localization: plugin
 Require-Bundle: org.eclipse.core.runtime;version="[3.4.0,4.0.0)",
- org.eclipse.core.filesystem;version="[3.4.0,4.0.0)",
+ org.eclipse.core.filesystem;version="[1.1.0,2.0.0)",
  org.eclipse.core.resources;version="[3.4.0,4.0.0)",
- org.eclipse.core.net;version="[3.4.0,4.0.0)",
+ org.eclipse.core.net;version="[1.1.0,2.0.0)",
  org.eclipse.ui;version="[3.4.0,4.0.0)",
- org.eclipse.team.core;bundle-version="3.4.0",
+ org.eclipse.team.core;version="[3.4.0,4.0.0)",
  org.eclipse.team.ui;version="[3.4.0,4.0.0)",
  org.eclipse.jface.text;version="[3.4.0,4.0.0)",
  org.eclipse.ui.ide;version="[3.4.0,4.0.0)",
  org.eclipse.ui.workbench.texteditor;version="[3.4.0,4.0.0)",
  org.eclipse.compare;version="[3.4.0,4.0.0)",
  org.eclipse.ui.ide;version="[3.4.0,4.0.0)",
- org.eclipse.jsch.ui;bundle-version="1.1.100",
- org.eclipse.jsch.core;bundle-version="[1.1.100,2.0.0)"
+ org.eclipse.jsch.ui;version="[1.1.100,2.0.0)",
+ org.eclipse.jsch.core;version="[1.1.100,2.0.0)"
 Bundle-ActivationPolicy: lazy
 Bundle-RequiredExecutionEnvironment: J2SE-1.5
 Import-Package: com.jcraft.jsch;version="[0.1.41,0.2.0)",
  org.eclipse.egit.core;version="[0.6.0,0.7.0)",
+ org.eclipse.egit.core.internal.storage;version="[0.6.0,0.7.0)",
+ org.eclipse.egit.core.internal.util;version="[0.6.0,0.7.0)",
  org.eclipse.egit.core.op;version="[0.6.0,0.7.0)",
  org.eclipse.egit.core.project;version="[0.6.0,0.7.0)",
  org.eclipse.jgit.diff;version="0.6.0",

org.eclipse.egit.core.test/org.eclipse.egit.core--All-Tests.launch
@@ -26,7 +26,7 @@
 <stringAttribute key="org.eclipse.jdt.junit.CONTAINER" value="=org.eclipse.egit.core.test"/>
 <booleanAttribute key="org.eclipse.jdt.junit.KEEPRUNNING_ATTR" value="false"/>
 <stringAttribute key="org.eclipse.jdt.junit.TESTNAME" value=""/>
-<stringAttribute key="org.eclipse.jdt.junit.TEST_KIND" value="org.eclipse.jdt.junit.loader.junit3"/>
+<stringAttribute key="org.eclipse.jdt.junit.TEST_KIND" value="org.eclipse.jdt.junit.loader.junit4"/>
 <stringAttribute key="org.eclipse.jdt.launching.MAIN_TYPE" value=""/>
 <stringAttribute key="org.eclipse.jdt.launching.PROJECT_ATTR" value="org.eclipse.egit.core.test"/>
 <stringAttribute key="org.eclipse.jdt.launching.SOURCE_PATH_PROVIDER" value="org.eclipse.pde.ui.workbenchClasspathProvider"/>

org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/T0003_AdaptableFileTreeIteratorTest.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-package org.eclipse.egit.core;
+package org.eclipse.egit.core.test;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -16,10 +16,10 @@ import java.util.Set;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.egit.core.AdaptableFileTreeIterator;
+import org.eclipse.egit.core.ContainerTreeIterator;
 import org.eclipse.egit.core.op.ConnectProviderOperation;
 import org.eclipse.egit.core.project.RepositoryMapping;
-import org.eclipse.egit.core.test.GitTestCase;
-import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.treewalk.TreeWalk;
 import org.eclipse.jgit.treewalk.WorkingTreeIterator;
@@ -46,7 +46,7 @@ public class T0003_AdaptableFileTreeIteratorTest extends GitTestCase {
 		fileWriter.close();
 
 		final ConnectProviderOperation operation = new ConnectProviderOperation(
-				project.getProject(), new File("..", Constants.DOT_GIT));
+				project.getProject(), gitDir);
 		operation.run(null);
 	}
 

org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/internal/mapping/T0002_history.java
@@ -7,7 +7,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-package org.eclipse.egit.core.internal.mapping;
+package org.eclipse.egit.core.test.internal.mapping;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -102,7 +102,7 @@ public class T0002_history extends GitTestCase {
 		assertEquals(RefUpdate.Result.NEW, lck.forceUpdate());
 
 		ConnectProviderOperation operation = new ConnectProviderOperation(
-				project.getProject(), new File("..", Constants.DOT_GIT));
+				project.getProject(), gitDir);
 		operation.run(null);
 	}
 

org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/op/T0001_ConnectProviderOperationTest.java
@@ -7,7 +7,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-package org.eclipse.egit.core.op;
+package org.eclipse.egit.core.test.op;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -53,7 +53,7 @@ public class T0001_ConnectProviderOperationTest extends GitTestCase {
 		repository.create();
 		repository.close();
 		ConnectProviderOperation operation = new ConnectProviderOperation(
-				project.getProject(), new File("..", Constants.DOT_GIT));
+				project.getProject(), gitDir);
 		operation.run(null);
 
 		assertTrue(RepositoryProvider.isShared(project.getProject()));
@@ -95,7 +95,7 @@ public class T0001_ConnectProviderOperationTest extends GitTestCase {
 		assertEquals(RefUpdate.Result.NEW, lck.forceUpdate());
 
 		ConnectProviderOperation operation = new ConnectProviderOperation(
-				project.getProject(), new File("..", Constants.DOT_GIT));
+				project.getProject(), gitDir);
 		operation.run(null);
 
 		final boolean f[] = new boolean[1];