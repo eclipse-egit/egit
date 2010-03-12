cd2daa6d4302258adb1fca98053fac3316dcd3f5
Roland Grunberg <rgrunber@redhat.com>
2010-03-12 15:40:04 -0500

    Add option to replace selected files with version in the git index.
    This is the equivalent of 'git checkout' on a list of files from the
    command-line.
    
    Change-Id: I8958ddcfc1d2a09f52eff7534db6942195eaf5ef

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -1128,6 +1128,27 @@ public class UIText extends NLS {
 	/** */
 	public static String Track_see_log;
 
+	/** */
+	public static String DiscardChangesAction_confirmActionTitle;
+
+	/** */
+	public static String DiscardChangesAction_confirmActionMessage;
+
+	/** */
+	public static String DiscardChangesAction_unexpectedErrorTitle;
+
+	/** */
+	public static String DiscardChangesAction_unexpectedErrorMessage;
+
+	/** */
+	public static String DiscardChangesAction_unexpectedIndexErrorMessage;
+
+	/** */
+	public static String DiscardChangesAction_refreshErrorTitle;
+
+	/** */
+	public static String DiscardChangesAction_refreshErrorMessage;
+
 	static {
 		initializeMessages("org.eclipse.egit.ui.uitext", UIText.class); //$NON-NLS-1$
 	}
