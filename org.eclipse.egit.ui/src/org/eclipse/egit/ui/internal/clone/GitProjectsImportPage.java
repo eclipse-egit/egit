5f0deb8d06452030ff738e5bf9d6afda5a98a3ba
Robin Rosenberg <robin.rosenberg@dewire.com>
2010-04-04 15:39:40 +0200

    EGit plugin not compatible with Eclipse 3.4, use of unavailable FilteredTree constructor
    Bug: 308033
    
    Change-Id: Idd785391e2833482e2bf58029c9b0d165d23a5e3
    Signed-off-by: Robin Rosenberg <robin.rosenberg@dewire.com>
    Signed-off-by: Mykola Nikishov <mn@mn.com.ua>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/GitProjectsImportPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/GitProjectsImportPage.java
@@ -319,7 +319,7 @@ public class GitProjectsImportPage extends WizardPage {
 		};
 
 		FilteredTree filteredTree = new FilteredTree(listComposite, SWT.CHECK
-				| SWT.BORDER, filter, true);
+				| SWT.BORDER, filter);
 		filteredTree.setInitialText(UIText.WizardProjectsImportPage_filterText);
 		projectsList = filteredTree.getViewer();
 		GridData listData = new GridData(GridData.GRAB_HORIZONTAL
