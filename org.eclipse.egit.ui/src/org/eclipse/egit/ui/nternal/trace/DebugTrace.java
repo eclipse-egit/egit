b30cf0280eb80fb582c06ee600ff851390f5bd45
Mathias Kinzler <mathias.kinzler@sap.com>
2010-04-06 16:20:37 +0200

    Make tracing plug-in local
    Sharing trace classes between plug-ins
    is bad, as the OSGI tracing infrastructure
    will write the ID of plug-in that exposes
    the trace class into the trace file.
    Instead the ID of the plug-in that is
    writing the trace should be written.
    This is achieved by obtaining a separate
    Location for each plug-in.
    
    Bug: 306793
    
    Change-Id: 	I2765961f9c0b886995e74b03404e1f20aadf7b28
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/nternal/trace/DebugTrace.java
@@ -0,0 +1,34 @@
+package org.eclipse.egit.ui.nternal.trace;
+
+/**
+ * Just a place holder for OSGI Debug Trace support until
+ * we drop 3.4 support in EGit/JGit;
+ *
+ * TODO replace this by OSGI's DebugTrace once we drop 3.4 support
+ */
+public interface DebugTrace {
+
+	/**
+	 * @param location
+	 * @param message
+	 */
+	public void trace(String location, String message);
+	/**
+	 * @param location
+	 * @param message
+	 * @param error
+	 */
+	public void trace(String location, String message, Throwable error);
+
+	/**
+	 * @param location
+	 */
+	public void traceEntry(String location);
+
+	/**
+	 * @param location
+	 * @param message
+	 */
+	public void traceEntry(String location, String message);
+
+}
