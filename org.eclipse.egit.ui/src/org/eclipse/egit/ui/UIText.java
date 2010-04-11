34b1b01c18980e8e0156f94ef326499fe664fd91
Dariusz Luksza <dariusz@luksza.org>
2010-04-11 22:25:44 +0200

    Add tagging UI support
    Implements UI for unsigned and annotated tags. This implementation
    allows user to select with commit object should be associated with
    created tag. List of all commits is available in 'Advanced' section of
    dialog. There is a special combo widget that can suggest commits based
    on typed in commit SHA-1 or part from first line of commit message. User
    can also edit/overwrite message or associated commit of existing tag by
    selecting it from existing tag list (this list can be found in 'Details'
    section) and marking 'Overwrite existing tag' check box.
    
    By default tag is created for current HEAD, this can be changed in
    'Advanced' section.
    
    Tag dialog is available in project's context menu ('Team->Tag...').
    
    CQ: 4073
    Bug: 311262
    
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>
    Change-Id: Icb30655845d1e1198e59992a10148421ab9f9bd5

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -1875,6 +1875,72 @@ public class UIText extends NLS {
 	/** */
 	public static String UIIcons_errorLoadingPluginImage;
 
+	/** */
+	public static String TagAction_cannotCheckout;
+
+	/** */
+	public static String TagAction_cannotGetBranchName;
+
+	/** */
+	public static String TagAction_repositoryState;
+
+	/** */
+	public static String TagAction_errorCreatingTag;
+
+	/** */
+	public static String TagAction_unableToCreateTag;
+
+	/** */
+	public static String TagAction_errorDuringTagging;
+
+	/** */
+	public static String TagAction_errorWhileGettingRevCommits;
+
+	/** */
+	public static String TagAction_unableToResolveHeadObjectId;
+
+	/** */
+	public static String TagAction_errorWhileMappingRevTag;
+
+	/** */
+	public static String TagAction_creating;
+
+	/** */
+	public static String TagAction_taggingFailed;
+
+	/** */
+	public static String CreateTagDialog_tagName;
+
+	/** */
+	public static String CreateTagDialog_tagMessage;
+
+	/** */
+	public static String CreateTagDialog_questionNewTagTitle;
+
+	/** */
+	public static String CreateTagDialog_overwriteTag;
+
+	/** */
+	public static String CreateTagDialog_overwriteTagToolTip;
+
+	/** */
+	public static String CreateTagDialog_existingTags;
+
+	/** */
+	public static String CreateTagDialog_advanced;
+
+	/** */
+	public static String CreateTagDialog_advancedToolTip;
+
+	/** */
+	public static String CreateTagDialog_advancedMessage;
+
+	/** */
+	public static String CreateTagDialog_tagNameToolTip;
+
+	/** */
+	public static String CommitCombo_showSuggestedCommits;
+
 	static {
 		initializeMessages("org.eclipse.egit.ui.uitext", UIText.class); //$NON-NLS-1$
 	}
