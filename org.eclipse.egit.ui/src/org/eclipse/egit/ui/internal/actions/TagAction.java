c4d3fc6b1508ef101f6f4c8a3e29a2c073ae6e4e
Dariusz Luksza <dariusz@luksza.org>
2010-04-11 22:25:44 +0200

    Add tagging UI support
    Implements UI for unsigned and annotated tags. This implementation
    allows user to select with commit object should be associated with
    created tag. List of all commits is available in 'Advanced' section of
    dialog. There is a special combo widget that can suggest commits based
    on typed in commit SHA-1 or part from first line of commit message. User
    can also edit/overwrite message or associated commit of existing tag by
    selecting it from existing tag list and marking 'Force replace existing
    tag' check box.
    
    By default tag is created for current HEAD, this can be changed in
    'Advanced' section.
    
    Tag dialog is available in project's context menu ('Team->Tag...').
    
    CQ: 4073
    Bug: 311262
    
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>
    Change-Id: Icb30655845d1e1198e59992a10148421ab9f9bd5

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/TagAction.java
@@ -0,0 +1,183 @@
+/*******************************************************************************
+ * Copyright (C) 2010, Darusz Luksza <dariusz@luksza.org>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.actions;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.egit.core.op.TagOperation;
+import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.UIText;
+import org.eclipse.egit.ui.internal.ValidationUtils;
+import org.eclipse.egit.ui.internal.decorators.GitLightweightDecorator;
+import org.eclipse.egit.ui.internal.dialogs.CreateTagDialog;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jgit.lib.AnyObjectId;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.Tag;
+import org.eclipse.jgit.revwalk.RevSort;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * An action for creating tag.
+ *
+ * @see TagOperation
+ */
+public class TagAction extends RepositoryAction {
+
+	private Repository repo;
+
+	@Override
+	public boolean isEnabled() {
+		return getRepository(false) != null;
+	}
+
+	@Override
+	protected void execute(IAction action) throws InvocationTargetException,
+			InterruptedException {
+		repo = getRepository(true);
+		if (repo == null)
+			return;
+
+		if (!repo.getRepositoryState().canCheckout()) {
+			MessageDialog.openError(getShell(),
+					UIText.TagAction_cannotCheckout, NLS.bind(
+							UIText.TagAction_repositoryState, repo
+									.getRepositoryState().getDescription()));
+			return;
+		}
+
+		String currentBranchName;
+		try {
+			currentBranchName = repo.getBranch();
+		} catch (IOException e) {
+			throw new InvocationTargetException(e,
+					UIText.TagAction_cannotGetBranchName);
+		}
+
+		CreateTagDialog dialog = new CreateTagDialog(getShell(),
+				ValidationUtils
+						.getRefNameInputValidator(repo, Constants.R_TAGS),
+				currentBranchName);
+
+		// get and set commits
+		RevWalk revCommits = getRevCommits();
+		dialog.setRevCommitList(revCommits);
+
+		// get and set existing tags
+		List<Tag> tags = getRevTags();
+		dialog.setExistingTags(tags);
+
+		if (dialog.open() != IDialogConstants.OK_ID)
+			return;
+
+		final Tag tag = new Tag(repo);
+		PersonIdent personIdent = new PersonIdent(repo);
+		String tagName = dialog.getTagName();
+
+		tag.setTag(tagName);
+		tag.setTagger(personIdent);
+		tag.setMessage(dialog.getTagMessage());
+
+		ObjectId tagCommit = getTagCommit(dialog.getTagCommit());
+		tag.setObjId(tagCommit);
+
+		String tagJobName = NLS.bind(UIText.TagAction_creating, tagName);
+		final boolean shouldMoveTag = dialog.shouldOverWriteTag();
+
+		Job tagJob = new Job(tagJobName) {
+			protected IStatus run(IProgressMonitor monitor) {
+				try {
+					new TagOperation(repo, tag, shouldMoveTag).execute(monitor);
+				} catch (CoreException e) {
+					return Activator.createErrorStatus(
+							UIText.TagAction_taggingFailed, e);
+				} finally {
+					GitLightweightDecorator.refresh();
+				}
+
+				return Status.OK_STATUS;
+			}
+
+		};
+
+		tagJob.setUser(true);
+		tagJob.schedule();
+	}
+
+	private List<Tag> getRevTags() {
+		Collection<Ref> revTags = repo.getTags().values();
+		List<Tag> tags = new ArrayList<Tag>();
+		for (Ref ref : revTags) {
+			try {
+				Tag tag = repo.mapTag(ref.getName());
+				tags.add(tag);
+			} catch (IOException e) {
+				ErrorDialog.openError(getShell(),
+						UIText.TagAction_errorDuringTagging, NLS.bind(
+								UIText.TagAction_errorWhileMappingRevTag, ref
+										.getName()), new Status(IStatus.ERROR,
+								Activator.getPluginId(), e.getMessage(), e));
+			}
+		}
+		return tags;
+	}
+
+	private RevWalk getRevCommits() {
+		RevWalk revWalk = new RevWalk(repo);
+		revWalk.sort(RevSort.COMMIT_TIME_DESC, true);
+		revWalk.sort(RevSort.BOUNDARY, true);
+
+		try {
+			AnyObjectId headId = repo.resolve(Constants.HEAD);
+			if (headId != null)
+				revWalk.markStart(revWalk.parseCommit(headId));
+		} catch (IOException e) {
+			ErrorDialog.openError(getShell(),
+					UIText.TagAction_errorDuringTagging,
+					UIText.TagAction_errorWhileGettingRevCommits, new Status(
+							IStatus.ERROR, Activator.getPluginId(), e
+									.getMessage(), e));
+		}
+
+		return revWalk;
+	}
+
+	private ObjectId getTagCommit(ObjectId objectId)
+			throws InvocationTargetException {
+		ObjectId result = null;
+		if (objectId == null) {
+			try {
+				result = repo.resolve(Constants.HEAD);
+			} catch (IOException e) {
+				throw new InvocationTargetException(e,
+						UIText.TagAction_unableToResolveHeadObjectId);
+			}
+		} else {
+			result = objectId;
+		}
+		return result;
+	}
+}
