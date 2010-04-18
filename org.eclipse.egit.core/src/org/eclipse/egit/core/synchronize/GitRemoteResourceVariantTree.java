11b7edf24d4c33c9fb03668f793b622501b887e8
Dariusz Luksza <dariusz@luksza.org>
2010-04-17 22:35:08 -0400

    Add preliminary synchronization support within Eclipse for branches.
    Implements synchronization participants and resource variants for
    allowing clients to synchronize Eclipse resources against the Git
    index and a given branch.
    
    Synchronization can be launched for project's context menu by 'Team ->
    Synchronize...' and from 'Repositories View' from branch context menu.
    
    In 'Synchronize repository' dialog user can choose source and
    destination branch (or tag). He can also define does local uncommited
    changes should be included in 'Synchronize view'.
    
    CQ: 4122
    Bug: 309582
    Also-by: Remy Suen <remysuen@ca.ibm.com>
    Change-Id: Ib838a6c88eccbef8c7ffb6aa1c602bdc8778dbc6
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

---
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/GitRemoteResourceVariantTree.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Dariusz Luksza <dariusz@luksza.org>
+ *******************************************************************************/
+package org.eclipse.egit.core.synchronize;
+
+import java.io.IOException;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.egit.core.synchronize.dto.GitSynchronizeDataSet;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Tree;
+import org.eclipse.team.core.variants.ResourceVariantByteStore;
+
+class GitRemoteResourceVariantTree extends GitResourceVariantTree {
+
+	GitRemoteResourceVariantTree(GitSynchronizeDataSet data, ResourceVariantByteStore store) {
+		super(data, store);
+	}
+
+	@Override
+	Tree getRevTree(IResource resource) throws IOException {
+		return getSyncData().getData(resource.getProject()).mapDstTree();
+	}
+
+	@Override
+	ObjectId getRevObjId(IResource resource) throws IOException {
+		return getSyncData().getData(resource.getProject()).getDstObjectId();
+	}
+
+}
