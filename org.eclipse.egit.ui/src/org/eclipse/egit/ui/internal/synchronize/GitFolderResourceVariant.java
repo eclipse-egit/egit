c211fd1291fd776c3138e6c29d5350a7654d53c5
Dariusz Luksza <dariusz@luksza.org>
2010-04-17 22:35:08 -0400

    Add preliminary synchronization support within Eclipse for branches.
    Implements synchronization participants and resource variants for
    allowing clients to synchronize Eclipse resources against the Git
    index and a given branch.
    
    Bug: 309582
    Also-by: Remy Suen <remysuen@ca.ibm.com>
    Change-Id: Ib838a6c88eccbef8c7ffb6aa1c602bdc8778dbc6
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/GitFolderResourceVariant.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.synchronize;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IStorage;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.team.core.TeamException;
+
+class GitFolderResourceVariant extends GitResourceVariant {
+
+	GitFolderResourceVariant(IResource resource) {
+		super(resource);
+	}
+
+	IContainer getContainer() {
+		return (IContainer) getResource();
+	}
+
+	public boolean isContainer() {
+		return true;
+	}
+
+	public IStorage getStorage(IProgressMonitor monitor) throws TeamException {
+		return null;
+	}
+
+	public String getContentIdentifier() {
+		return getName();
+	}
+
+	@Override
+	public int hashCode() {
+		return 31 * getResource().hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		} else if (obj == null) {
+			return false;
+		} else if (getClass() != obj.getClass()) {
+			return false;
+		}
+		GitFolderResourceVariant other = (GitFolderResourceVariant) obj;
+		return getResource().equals(other.getResource());
+	}
+
+}
---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/GitResourceVariant.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.synchronize;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.team.core.variants.IResourceVariant;
+
+abstract class GitResourceVariant implements IResourceVariant {
+
+	private IResource resource;
+
+	GitResourceVariant(IResource resource) {
+		this.resource = resource;
+	}
+
+	public IResource getResource() {
+		return resource;
+	}
+
+	public String getName() {
+		return resource.getName();
+	}
+
+	public byte[] asBytes() {
+		return getContentIdentifier().getBytes();
+	}
+
+}
