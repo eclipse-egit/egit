ca322b2c74c5950b46b2cb4775a9a785b5fbb815
Remy Suen <remysuen@ca.ibm.com>
2010-04-22 20:40:00 -0400

    Implement equals(Object) and hashCode() for CommitFileRevision so editors can be reused
    When editors are asked to be opened in the Eclipse workbench, the
    default behaviour is to simply activate an existing editor if an
    existing editor shares the input with the input that is to be
    opened. The contract for comparing editor inputs is via the
    equals(Object) method. For a FileRevisionEditorInput, this
    is implemented by checking the equality between the two inputs'
    underlying file revisions. As such, CommitFileRevision should 
    implement equals(Object) (and hashCode() for consistency) so that
    the same editor can be reused instead of having multiple editors
    open on a given revision.
    
    Change-Id: I2abeef934b21986979f84f9836e0bf2b44eff41f

--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/storage/CommitFileRevision.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/storage/CommitFileRevision.java
@@ -132,4 +132,53 @@ class CommitFileRevision extends GitFileRevision {
 							.getId()), e);
 		}
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + ((blobId == null) ? 0 : blobId.hashCode());
+		result = prime * result + ((commit == null) ? 0 : commit.hashCode());
+		result = prime * result + ((db == null) ? 0 : db.hashCode());
+		result = prime * result + ((path == null) ? 0 : path.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CommitFileRevision other = (CommitFileRevision) obj;
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
+			return false;
+		if (blobId == null) {
+			if (other.blobId != null)
+				return false;
+		} else if (!blobId.equals(other.blobId))
+			return false;
+		if (commit == null) {
+			if (other.commit != null)
+				return false;
+		} else if (!commit.equals(other.commit))
+			return false;
+		if (db == null) {
+			if (other.db != null)
+				return false;
+		} else if (!db.equals(other.db))
+			return false;
+		if (path == null) {
+			if (other.path != null)
+				return false;
+		} else if (!path.equals(other.path))
+			return false;
+		return true;
+	}
 }
