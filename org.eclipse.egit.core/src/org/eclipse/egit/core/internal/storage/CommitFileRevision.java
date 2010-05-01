74f22c9090c0d14395284061a9217d2770348146
Robin Rosenberg <robin.rosenberg@dewire.com>
2010-05-01 19:53:36 +0200

    Remove usage of unpublished API's in org.eclipse.egit.core
    This also flags further such use in this bundle as errors and
    upgrades other compiler settings to Eclipse 3.5.2 defaults.
    
    A testcase is introduced to cover error handling pattern, but
    it is incomplete and only implemented for BlobStorage.
    
    Bug: 310933 (partial fix)
    
    Change-Id: I40f6b9d457010a0e1a68d1d33edb11f4da7a1321
    Signed-off-by: Robin Rosenberg <robin.rosenberg@dewire.com>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/storage/CommitFileRevision.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/storage/CommitFileRevision.java
@@ -14,17 +14,12 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 
-import org.eclipse.core.internal.resources.ResourceException;
-import org.eclipse.core.resources.IResourceStatus;
 import org.eclipse.core.resources.IStorage;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.Path;
+import org.eclipse.egit.core.Activator;
 import org.eclipse.egit.core.CoreText;
 import org.eclipse.egit.core.GitTag;
-import org.eclipse.osgi.util.NLS;
-import org.eclipse.team.core.history.IFileRevision;
-import org.eclipse.team.core.history.ITag;
 import org.eclipse.jgit.lib.AnyObjectId;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.PersonIdent;
@@ -32,6 +27,9 @@ import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.treewalk.TreeWalk;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.team.core.history.IFileRevision;
+import org.eclipse.team.core.history.ITag;
 
 /**
  * An {@link IFileRevision} for a version of a specified resource in the
@@ -120,16 +118,14 @@ class CommitFileRevision extends GitFileRevision {
 		try {
 			final TreeWalk w = TreeWalk.forPath(db, path, commit.getTree());
 			if (w == null)
-				throw new ResourceException(IResourceStatus.FAILED_READ_LOCAL,
-						Path.fromPortableString(path), NLS.bind(
-								CoreText.CommitFileRevision_pathNotIn, commit
-										.getId()), null);
+				throw new CoreException(Activator.error(NLS.bind(
+						CoreText.CommitFileRevision_pathNotIn, commit.getId().name(),
+						path), null));
 			return w.getObjectId(0);
 		} catch (IOException e) {
-			throw new ResourceException(IResourceStatus.FAILED_READ_LOCAL, Path
-					.fromPortableString(path), NLS.bind(
+			throw new CoreException(Activator.error(NLS.bind(
 					CoreText.CommitFileRevision_errorLookingUpPath, commit
-							.getId()), e);
+							.getId().name(), path), e));
 		}
 	}
 }
