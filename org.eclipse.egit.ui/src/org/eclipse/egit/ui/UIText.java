3643937dec912c8da636e659c75b587b2bcd3d19
Jens Baumgart <jens.baumgart@sap.com>
2010-05-06 17:28:23 +0200

    Improve DiscardChangesAction
    Currently DiscardChangesAction (Trigger: Replace With->File in Git
    Index) runs in the UI thread. This might block the UI. Furthermore
    the discard operation is located in the action.
    Now DiscardChangesAction uses a Job to execute. The discard operation
    was moved in a new class DiscardChangesOperation.
    
    Change-Id: I06fe4efa096ae4ca6b1110c2f7259243e1d2c99d
    Signed-off-by: Jens Baumgart <jens.baumgart@sap.com>
    Signed-off-by: Chris Aniszczyk <caniszczyk@gmail.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -1723,6 +1723,9 @@ public class UIText extends NLS {
 	public static String DiscardChangesAction_confirmActionMessage;
 
 	/** */
+	public static String DiscardChangesAction_discardChanges;
+
+	/** */
 	public static String DiscardChangesAction_unexpectedErrorTitle;
 
 	/** */
