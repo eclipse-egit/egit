ac2c4901009e5ed98e47854569f98a1f28016bc7
Dariusz Luksza <dariusz@luksza.org>
2010-06-15 19:30:54 +0200

    Refactored UI tests
    All common classes moved into org.eclipse.egit.ui.common package.
    Created EGitTestCase class, a base class for EGit SWTBoot tests.
    
    Bug: 316954
    Change-Id: I4575638c11456d6621b6f9831fab60fd2efa9f71
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/common/GitImportRepoWizard.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/common/GitImportRepoWizard.java
@@ -1,13 +1,14 @@
 /*******************************************************************************
  * Copyright (C) 2009, Robin Rosenberg <robin.rosenberg@dewire.com>
  * Copyright (C) 2010, Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
+ * Copyright (C) 2010, Dariusz Luksza <dariusz@luksza.org>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-package org.eclipse.egit.ui.wizards.clone;
+package org.eclipse.egit.ui.common;
 
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 
@@ -16,14 +17,16 @@ public class GitImportRepoWizard {
 	private static final SWTWorkbenchBot bot = new SWTWorkbenchBot();
 
 	// TODO: speed it up by calling the wizard using direct eclipse API.
-	public RepoPropertiesPage openWizard() {
+	public void openWizard() {
 		bot.menu("File").menu("Import...").click();
 		bot.shell("Import").activate();
 
 		bot.tree().expandNode("Git").select("Projects from Git");
 
 		bot.button("Next >").click();
+	}
 
+	public RepoPropertiesPage openCloneWizard() {
 		bot.shell("Import Projects from Git").activate();
 
 		bot.button("Clone...").click();
