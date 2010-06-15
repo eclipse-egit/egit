050a6379d934bfd82926b8a67c6c922ff95ac721
Dariusz Luksza <dariusz@luksza.org>
2010-06-15 19:30:54 +0200

    Refactored UI tests
    All common classes moved into org.eclipse.egit.ui.common package.
    Created EGitTestCase class, a base class for EGit SWTBoot tests.
    
    Bug: 316954
    Change-Id: I4575638c11456d6621b6f9831fab60fd2efa9f71
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/common/ExistingOrNewPage.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/common/ExistingOrNewPage.java
@@ -9,7 +9,7 @@
  * Contributors:
  *    Stefan Lay (SAP AG) - initial implementation
  *******************************************************************************/
-package org.eclipse.egit.ui.wizards.share;
+package org.eclipse.egit.ui.common;
 
 import static org.junit.Assert.assertEquals;
 
