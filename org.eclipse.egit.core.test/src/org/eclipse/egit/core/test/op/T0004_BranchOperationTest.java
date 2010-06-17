5f6852cb8d85b434720d052e05b7c8f8157f88a6
Jens Baumgart <jens.baumgart@sap.com>
2010-06-17 16:22:42 +0200

    Add Test for ResetOperation
    A test for ResetOperation was added.
    Common test functionality for creating a test repository
    was moved to a new class TestRepository.
    
    Signed-off-by: Jens Baumgart <jens.baumgart@sap.com>

--- org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/op/T0004_BranchOperationTest.java
+++ org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/op/T0004_BranchOperationTest.java
@@ -4,25 +4,12 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.io.IOException;
 
 import org.eclipse.egit.core.op.BranchOperation;
 import org.eclipse.egit.core.test.GitTestCase;
-import org.eclipse.jgit.api.CommitCommand;
-import org.eclipse.jgit.api.ConcurrentRefUpdateException;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.JGitInternalException;
-import org.eclipse.jgit.api.NoHeadException;
-import org.eclipse.jgit.api.NoMessageException;
-import org.eclipse.jgit.api.WrongRepositoryStateException;
-import org.eclipse.jgit.errors.UnmergedPathException;
+import org.eclipse.egit.core.test.TestRepository;
 import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.GitIndex;
-import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.Ref;
-import org.eclipse.jgit.lib.RefUpdate;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.lib.GitIndex.Entry;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -31,36 +18,36 @@ public class T0004_BranchOperationTest extends GitTestCase{
 
 	private static final String TEST = Constants.R_HEADS + "test";
 	private static final String MASTER = Constants.R_HEADS + "master";
+	TestRepository testRepository;
 	Repository repository;
 
 	@Before
 	public void setUp() throws Exception {
 		super.setUp();
-		repository = new Repository(gitDir);
-		repository.create();
+		testRepository = new TestRepository(gitDir);
+		repository = testRepository.getRepository();
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		repository.close();
+		testRepository.dispose();
 		repository = null;
 		super.tearDown();
 	}
 
-
 	@Test
 	public void testBranchOperation() throws Exception {
 		// create first commit containing a dummy file
-		createInitialCommit();
+		testRepository.createInitialCommit("testBranchOperation\n\nfirst commit\n");
 		// create branch test and switch to branch test
-		createBranch(MASTER, TEST);
+		testRepository.createBranch(MASTER, TEST);
 		new BranchOperation(repository, TEST).execute(null);
 		assertTrue(repository.getFullBranch().equals(TEST));
 		// add .project to version control and commit
 		String path = project.getProject().getLocation().append(".project").toOSString();
 		File file = new File(path);
-		track(file);
-		commit("Add .project file");
+		testRepository.track(file);
+		testRepository.commit("Add .project file");
 		// switch back to master branch
 		// .project must disappear, related Eclipse project must be deleted
 		new BranchOperation(repository, MASTER).execute(null);
@@ -71,45 +58,4 @@ public class T0004_BranchOperationTest extends GitTestCase{
 		new BranchOperation(repository, TEST).execute(null);
 		assertTrue(file.exists());
 	}
-
-	private void createInitialCommit() throws IOException, NoHeadException, NoMessageException, ConcurrentRefUpdateException, JGitInternalException, WrongRepositoryStateException {
-		String repoPath = project.getProject().getWorkspace().getRoot().getLocation().toOSString();
-		File file = new File(repoPath, "dummy");
-		file.createNewFile();
-		track(file);
-		commit("testBranchOperation\n\nfirst commit\n");
-	}
-
-	private void createBranch(String refName, String newRefName) throws IOException {
-		RefUpdate updateRef;
-		updateRef = repository.updateRef(newRefName);
-		Ref startRef = repository.getRef(refName);
-		ObjectId startAt = repository.resolve(refName);
-		String startBranch;
-		if (startRef != null)
-			startBranch = refName;
-		else
-			startBranch = startAt.name();
-		startBranch = repository.shortenRefName(startBranch);
-		updateRef.setNewObjectId(startAt);
-		updateRef.setRefLogMessage("branch: Created from " + startBranch, false); //$NON-NLS-1$
-		updateRef.update();
-	}
-
-	private void track(File file) throws IOException {
-		GitIndex index = repository.getIndex();
-		Entry entry = index.add(repository.getWorkDir(), file);
-		entry.setAssumeValid(false);
-		index.write();
-	}
-
-	private void commit(String message) throws NoHeadException, NoMessageException, UnmergedPathException, ConcurrentRefUpdateException, JGitInternalException, WrongRepositoryStateException {
-		Git git = new Git(repository);
-		CommitCommand commitCommand = git.commit();
-		commitCommand.setAuthor("J. Git", "j.git@egit.org");
-		commitCommand.setCommitter(commitCommand.getAuthor());
-		commitCommand.setMessage(message);
-		commitCommand.call();
-	}
-
 }
