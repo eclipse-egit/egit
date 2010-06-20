f44ab93169b7245bfeff3f89b061419c987d7562
Alex Blewitt <alex.blewitt@gmail.com>
2010-06-20 02:09:17 +0100

    Enable HTTP clipboard contents only if .git is present
    When selecting a repository from the clipboard, don't be over-eager
    with the HTTP or HTTPS URLs, and only pre-fill those with .git 
    in their name.
    
    If the URL has a space, trim at that, rather than everything.
    
    Bug: 315589
    Signed-off-by: Alex Blewitt <alex.blewitt@gmail.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RepositorySelectionPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RepositorySelectionPage.java
@@ -171,8 +171,13 @@ public class RepositorySelectionPage extends BaseWizardPage {
 			try {
 				if(text != null) {
 					text = text.trim();
-					if(Transport.canHandleProtocol(new URIish(text), FS.DETECTED))
-						preset = text;
+					int index = text.indexOf(' ');
+					if(index > 0)
+						text = text.substring(0,index);
+					if(Transport.canHandleProtocol(new URIish(text), FS.DETECTED)) {
+						if(!text.startsWith("http") || text.contains(".git")) //$NON-NLS-1$ //$NON-NLS-2$
+							preset = text;
+					}
 				}
 			} catch (URISyntaxException e) {
 				preset = null;
