9718f053df30bd6ba3887677da44248f3652621a
Mathias Kinzler <mathias.kinzler@sap.com>
2010-06-21 23:37:29 +0200

    Make Global properties available as Preference page
    This adds a preference page enabling display/simple editing
    of the global properties.
    
    Bug: 315563
    Change-Id: Ib101d4d28a778d83f389b0880af2465bbc9dd582
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/AllTests.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/AllTests.java
@@ -8,6 +8,7 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.test;
 
+import org.eclipse.egit.ui.prefpages.configuration.GlobalConfigurationPageTest;
 import org.eclipse.egit.ui.view.repositories.GitRepositoriesViewTest;
 import org.eclipse.egit.ui.wizards.clone.GitCloneWizardTest;
 import org.eclipse.egit.ui.wizards.share.SharingWizardTest;
@@ -16,8 +17,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses( { GitCloneWizardTest.class, SharingWizardTest.class,
-		GitRepositoriesViewTest.class })
+@SuiteClasses( { GitCloneWizardTest.class, GlobalConfigurationPageTest.class,
+		SharingWizardTest.class, GitRepositoriesViewTest.class })
 public class AllTests {
 	// empty class, don't need anything here
 }
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -40,6 +40,33 @@ public class UIText extends NLS {
 	public static String Activator_scanningRepositories;
 
 	/** */
+	public static String AddConfigEntryDialog_AddConfigTitle;
+
+	/** */
+	public static String AddConfigEntryDialog_ConfigKeyTooltip;
+
+	/** */
+	public static String AddConfigEntryDialog_DialogMessage;
+
+	/** */
+	public static String AddConfigEntryDialog_EnterValueMessage;
+
+	/** */
+	public static String AddConfigEntryDialog_EntryExistsMessage;
+
+	/** */
+	public static String AddConfigEntryDialog_KeyComponentsMessage;
+
+	/** */
+	public static String AddConfigEntryDialog_KeyLabel;
+
+	/** */
+	public static String AddConfigEntryDialog_MustEnterKeyMessage;
+
+	/** */
+	public static String AddConfigEntryDialog_ValueLabel;
+
+	/** */
 	public static String AddToIndexAction_addingFiles;
 
 	/** */
@@ -1889,6 +1916,66 @@ public class UIText extends NLS {
 
 	/** */
 	public static String GitShareProjectsPage_RepositoryLabel;
+
+	/** */
+	public static String GlobalConfigurationPreferencePage_AddButton;
+
+	/** */
+	public static String GlobalConfigurationPreferencePage_ChangeButton;
+
+	/** */
+	public static String GlobalConfigurationPreferencePage_ConfigLocationLabel;
+
+	/** */
+	public static String GlobalConfigurationPreferencePage_EmptyStringNotAllowed;
+
+	/** */
+	public static String GlobalConfigurationPreferencePage_KeyColumnHeader;
+
+	/** */
+	public static String GlobalConfigurationPreferencePage_NewValueButton;
+
+	/** */
+	public static String GlobalConfigurationPreferencePage_NoEntrySelectedMessage;
+
+	/** */
+	public static String GlobalConfigurationPreferencePage_NoSectionSubsectionMessage;
+
+	/** */
+	public static String GlobalConfigurationPreferencePage_OpenEditorButton;
+
+	/** */
+	public static String GlobalConfigurationPreferencePage_OpenEditorTooltip;
+
+	/** */
+	public static String GlobalConfigurationPreferencePage_RemoveAllButton;
+
+	/** */
+	public static String GlobalConfigurationPreferencePage_RemoveAllTooltip;
+
+	/** */
+	public static String GlobalConfigurationPreferencePage_RemoveButton;
+
+	/** */
+	public static String GlobalConfigurationPreferencePage_RemoveSectionMessage;
+
+	/** */
+	public static String GlobalConfigurationPreferencePage_RemoveSectionTitle;
+
+	/** */
+	public static String GlobalConfigurationPreferencePage_RemoveSubsectionMessage;
+
+	/** */
+	public static String GlobalConfigurationPreferencePage_RemoveSubsectionTitle;
+
+	/** */
+	public static String GlobalConfigurationPreferencePage_ValueColumnHeader;
+
+	/** */
+	public static String GlobalConfigurationPreferencePage_ValueLabel;
+
+	/** */
+	public static String GlobalConfigurationPreferencePage_WrongNumberOfTokensMessage;
 
  	/** */
 	public static String MergeAction_CannotMerge;
