79d6bfe61473e3ddcbc9a96045c9e1d5c2603bf8
Matthias Sohn <matthias.sohn@sap.com>
2010-06-25 02:41:45 +0200

    Use "ssh" instead of "git+ssh" as default scheme for ssh protocol
    This is the canonical URI form used by most other git tools. Add tooltips
    to reduce the confusion for users used to the old syntax.
    
    Change-Id: Id5e888d8ea5b0a09d435416d0212cee78f8b512b
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -472,6 +472,27 @@ public class UIText extends NLS {
 	public static String RepositorySelectionPage_ShowPreviousURIs_HoverText;
 
 	/** */
+	public static String RepositorySelectionPage_tip_file;
+
+	/** */
+	public static String RepositorySelectionPage_tip_ftp;
+
+	/** */
+	public static String RepositorySelectionPage_tip_git;
+
+	/** */
+	public static String RepositorySelectionPage_tip_http;
+
+	/** */
+	public static String RepositorySelectionPage_tip_https;
+
+	/** */
+	public static String RepositorySelectionPage_tip_sftp;
+
+	/** */
+	public static String RepositorySelectionPage_tip_ssh;
+
+	/** */
 	public static String RepositorySelectionPage_uriChoice;
 
 	/** */
