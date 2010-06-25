46527c91247601604d215eb1aaddf29c71493eff
Mathias Kinzler <mathias.kinzler@sap.com>
2010-06-25 12:39:01 +0200

    Fix RepositorySelectionPage
    which runs into a NPE if an existing local directory
    is in the clipboard.
    
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RepositorySelectionPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RepositorySelectionPage.java
@@ -178,7 +178,8 @@ public class RepositorySelectionPage extends BaseWizardPage {
 					URIish u = new URIish(text);
 					if (Transport.canHandleProtocol(u, FS.DETECTED)) {
 						String s = u.getScheme();
-						if (s.equals(DEFAULT_SCHEMES[S_GIT])
+						// s may be null if an existing local directory was in text
+						if (s != null && s.equals(DEFAULT_SCHEMES[S_GIT])
 								|| s.equals(DEFAULT_SCHEMES[S_SSH])
 								|| text.endsWith(Constants.DOT_GIT))
 							preset = text;
