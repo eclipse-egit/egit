a1e44002c2949a243326798c182a26c4caad56d2
Mathias Kinzler <mathias.kinzler@sap.com>
2010-06-30 10:19:17 +0200

    Git Repositories View UI Tests III: remote configuration handling
    Remote configuration handling in the Git Repositories view must
    behave like the standard: it is possible to push to a remote repository
    if only a "url" is present in the config, but no "pushurl". If both
    url and pushurl exist, they are all used.
    Accordingly, the "configure push" UI must show the (fetch-) URL
    if it is present and make addition of "pushurl"s optional in this case.
    Also the Repo View content provider had to be adjusted in order to
    show the "push" node if only a "url" and a "push" but no "pushurl"
    is defined.
    
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/PropertyTester.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/PropertyTester.java
@@ -15,7 +15,6 @@ import java.net.URISyntaxException;
 
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Ref;
-import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.transport.RemoteConfig;
 
 /**
@@ -23,9 +22,7 @@ import org.eclipse.jgit.transport.RemoteConfig;
  * Repositories View.
  */
 public class PropertyTester extends org.eclipse.core.expressions.PropertyTester {
-	/**
-	 * TODO javadoc missing
-	 */
+
 	public boolean test(Object receiver, String property, Object[] args,
 			Object expectedValue) {
 
@@ -33,10 +30,9 @@ public class PropertyTester extends org.eclipse.core.expressions.PropertyTester 
 			return false;
 		RepositoryTreeNode node = (RepositoryTreeNode) receiver;
 
-		if (property.equals("isBare")) { //$NON-NLS-1$
-			Repository rep = node.getRepository();
-			return rep.getConfig().getBoolean("core", "bare", false); //$NON-NLS-1$//$NON-NLS-2$
-		}
+		if (property.equals("isBare")) //$NON-NLS-1$
+			return node.getRepository().isBare();
+
 		if (property.equals("isRefCheckedOut")) { //$NON-NLS-1$
 			if (!(node.getObject() instanceof Ref))
 				return false;
@@ -63,13 +59,10 @@ public class PropertyTester extends org.eclipse.core.expressions.PropertyTester 
 					rconfig = new RemoteConfig(
 							node.getRepository().getConfig(), configName);
 				} catch (URISyntaxException e2) {
-					// TODO Exception handling
-					rconfig = null;
+					return false;
 				}
 
-				boolean fetchExists = rconfig != null
-						&& !rconfig.getURIs().isEmpty();
-				return fetchExists;
+				return !rconfig.getURIs().isEmpty();
 			}
 		}
 		if (property.equals("pushExists")) { //$NON-NLS-1$
@@ -81,12 +74,10 @@ public class PropertyTester extends org.eclipse.core.expressions.PropertyTester 
 					rconfig = new RemoteConfig(
 							node.getRepository().getConfig(), configName);
 				} catch (URISyntaxException e2) {
-					// TODO Exception handling
-					rconfig = null;
+					return false;
 				}
-				boolean pushExists = rconfig != null
-						&& !rconfig.getPushURIs().isEmpty();
-				return pushExists;
+				// TODO || !rconfig.getUris().isEmpty()?
+				return !rconfig.getPushURIs().isEmpty();
 			}
 		}
 		return false;
