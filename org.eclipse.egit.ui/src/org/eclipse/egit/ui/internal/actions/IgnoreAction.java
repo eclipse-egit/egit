36f44da3e1a764b5b6d672e3d0ecf014a0378ec7
Mathias Kinzler <mathias.kinzler@sap.com>
2010-07-01 10:51:21 +0200

    Exploit command framework in EGit
    Bug: 313589
    Bug: 306459
    Bug: 314096
    
    We should not use the (internal anyway) class TeamAction in order to
    implement our actions. We still have to contribute Actions as
    Object contributions in the action framework so that they
    appear in the Team context menu, but we should
    put the implementing code into command handlers
    which can then be re-used, for example between the team context
    menu actions and the command contributions used in the 
    Git Repository view. The actions are just lightweight objects that
    delegate their run() method to the command.
    
    In addition, this is a prerequisite for adding key bindings (it
    is possible to add a command id to the contributed actions using the 
    "definition id" feature of the corresponding extension point; the
    key binding (if found) defined for that command will then automaticall
    be displayed in the context menu).
    
    This only the first step which splits the current actions contributed
    to the team menu into a lightweight action part and a command handler part
    which is doing the actual work. The next step would be to weed out
    the double implementations in the Git Repositories view and either use
    the handler defined here or use a second set of handlers and make use
    of the "enabledWhen" feature of the "handler" extension point to enable
    the correct handler depending on the workbench selection.
    
    In a future step, key bindings could be added to the commands.
    
    Change-Id: I910f234e8880bc2e2fbfbd495ac97b63967a38b9
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/IgnoreAction.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/IgnoreAction.java
@@ -7,98 +7,12 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.actions;
 
-import java.io.ByteArrayInputStream;
-import java.io.UnsupportedEncodingException;
-
-import org.eclipse.core.resources.IContainer;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.WorkspaceJob;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.egit.ui.UIText;
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jgit.lib.Constants;
-import org.eclipse.team.core.Team;
-
 /** Action for ignoring files via .gitignore. */
 public class IgnoreAction extends RepositoryAction {
-	@SuppressWarnings("restriction")
-	@Override
-	public void execute(IAction action) {
-		final IResource[] resources = getSelectedResources();
-		if (resources.length == 0)
-			return;
-
-		WorkspaceJob job = new WorkspaceJob(UIText.IgnoreAction_jobName) {
-			public IStatus runInWorkspace(IProgressMonitor monitor)
-					throws CoreException {
-				monitor.beginTask(UIText.IgnoreAction_taskName, resources.length);
-				try {
-					for (IResource resource : resources) {
-						// TODO This is pretty inefficient; multiple ignores in
-						// the same directory cause multiple writes.
-
-						// NB This does the same thing in
-						// DecoratableResourceAdapter, but neither currently
-						// consult .gitignore
-
-						if (!Team.isIgnoredHint(resource)) {
-							addIgnore(monitor, resource);
-						}
-						monitor.worked(1);
-					}
-					monitor.done();
-				} catch (CoreException e) {
-					throw e;
-				} catch (Exception e) {
-					throw new CoreException(
-							new Status(
-									IStatus.ERROR,
-									"org.eclipse.egit.ui", UIText.IgnoreAction_error, e)); //$NON-NLS-1$
-				}
-				return Status.OK_STATUS;
-			}
-
-			private void addIgnore(IProgressMonitor monitor, IResource resource)
-					throws UnsupportedEncodingException, CoreException {
-				IContainer container = resource.getParent();
-				IFile gitignore = container.getFile(new Path(
-						Constants.GITIGNORE_FILENAME));
-				String entry = "/" + resource.getName() + "\n"; //$NON-NLS-1$  //$NON-NLS-2$
-				ByteArrayInputStream entryBytes = asStream(entry);
-
-				if (gitignore.exists())
-					gitignore.appendContents(entryBytes, true, true, monitor);
-				else
-					gitignore.create(entryBytes, true, monitor);
-			}
-
-			private ByteArrayInputStream asStream(String entry)
-					throws UnsupportedEncodingException {
-				return new ByteArrayInputStream(entry
-						.getBytes(Constants.CHARACTER_ENCODING));
-			}
-		};
-		job.schedule();
-	}
-
-	@SuppressWarnings("restriction")
-	@Override
-	public boolean isEnabled() {
-		if (getProjectsInRepositoryOfSelectedResources().length == 0)
-			return false;
-
-		IResource[] resources = getSelectedResources();
-		for (IResource resource : resources) {
-			// NB This does the same thing in DecoratableResourceAdapter, but
-			// neither currently consult .gitignore
-			if (!Team.isIgnoredHint(resource))
-				return true;
-		}
-		return false;
+	/**
+	 *
+	 */
+	public IgnoreAction() {
+		super(ActionCommands.IGNORE_ACTION);
 	}
 }
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/MergeAction.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/MergeAction.java
@@ -11,127 +11,15 @@
 
 package org.eclipse.egit.ui.internal.actions;
 
-import java.io.IOException;
-
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
-import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.jobs.JobChangeAdapter;
-import org.eclipse.egit.core.op.MergeOperation;
-import org.eclipse.egit.ui.Activator;
-import org.eclipse.egit.ui.UIText;
-import org.eclipse.egit.ui.internal.dialogs.MergeTargetSelectionDialog;
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.Ref;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.lib.RepositoryState;
-import org.eclipse.osgi.util.NLS;
-import org.eclipse.swt.widgets.Display;
-
 /**
  * Action for selecting a commit and merging it with the current branch.
  */
 public class MergeAction extends RepositoryAction {
 
-	@Override
-	public void execute(IAction action) {
-		final Repository repository = getRepository(true);
-		if (repository == null)
-			return;
-
-		if (!canMerge(repository))
-			return;
-
-		MergeTargetSelectionDialog mergeTargetSelectionDialog = new MergeTargetSelectionDialog(
-				getShell(), repository);
-		if (mergeTargetSelectionDialog.open() == IDialogConstants.OK_ID) {
-
-			final String refName = mergeTargetSelectionDialog.getRefName();
-
-			String jobname = NLS.bind(UIText.MergeAction_JobNameMerge, refName);
-			final MergeOperation op = new MergeOperation(repository, refName);
-			Job job = new Job(jobname) {
-				@Override
-				protected IStatus run(IProgressMonitor monitor) {
-					try {
-						op.execute(monitor);
-					} catch (final CoreException e) {
-						return e.getStatus();
-					}
-					return Status.OK_STATUS;
-				}
-			};
-			job.setUser(true);
-			job.addJobChangeListener(new JobChangeAdapter() {
-				@Override
-				public void done(IJobChangeEvent event) {
-					IStatus result = event.getJob().getResult();
-					if (result.getSeverity() == IStatus.CANCEL) {
-						Display.getDefault().asyncExec(new Runnable() {
-							public void run() {
-								MessageDialog
-										.openInformation(
-												getShell(),
-												UIText.MergeAction_MergeCanceledTitle,
-												UIText.MergeAction_MergeCanceledMessage);
-							}
-						});
-					} else if (!result.isOK()) {
-						Activator.handleError(result.getMessage(), result
-								.getException(), true);
-					} else {
-						Display.getDefault().asyncExec(new Runnable() {
-							public void run() {
-								MessageDialog.openInformation(getShell(),
-										UIText.MergeAction_MergeResultTitle, op
-												.getResult().toString());
-							}
-						});
-					}
-				}
-			});
-			job.schedule();
-
-		}
-
+	/**
+	 *
+	 */
+	public MergeAction() {
+		super(ActionCommands.MERGE_ACTION);
 	}
-
-	private boolean canMerge(final Repository repository) {
-		String message = null;
-		try {
-			Ref head = repository.getRef(Constants.HEAD);
-			if (head == null || !head.isSymbolic())
-				message = UIText.MergeAction_HeadIsNoBranch;
-			else if (!repository.getRepositoryState().equals(
-					RepositoryState.SAFE))
-				message = NLS.bind(
-						UIText.MergeAction_WrongRepositoryState,
-						repository.getRepositoryState());
-		} catch (IOException e) {
-			Activator.logError(e.getMessage(), e);
-			message = e.getMessage();
-		}
-
-		if (message != null) {
-			MessageDialog.openError(getShell(),
-					UIText.MergeAction_CannotMerge, message);
-		}
-		return (message == null);
-	}
-
-	@Override
-	public boolean isEnabled() {
-		boolean enabled = true;
-		Repository repository = getRepository(false);
-		if (repository == null)
-			enabled = false;
-		return enabled;
-	}
-
 }
