b04945225a94911589c7a56b50ae67420ebf6340
Mathias Kinzler <mathias.kinzler@sap.com>
2010-07-01 10:51:21 +0200

    Exploit command framework in EGit
    Bug: 313589
    Bug: 306459
    Bug: 314096
    
    We should not use the (internal anyway) class TeamAction in order to
    implement our actions. We still have to contribute Actions as
    Object contributions in the action framework so that they
    appear in the Team context menu, but we should
    put the implementing code into command handlers
    which can then be re-used, for example between the team context
    menu actions and the command contributions used in the 
    Git Repository view. The actions are just lightweight objects that
    delegate their run() method to the command.
    
    In addition, this is a prerequisite for adding key bindings (it
    is possible to add a command id to the contributed actions using the 
    "definition id" feature of the corresponding extension point; the
    key binding (if found) defined for that command will then automaticall
    be displayed in the context menu).
    
    This only the first step which splits the current actions contributed
    to the team menu into a lightweight action part and a command handler part
    which is doing the actual work. The next step would be to weed out
    the double implementations in the Git Repositories view and either use
    the handler defined here or use a second set of handlers and make use
    of the "enabledWhen" feature of the "handler" extension point to enable
    the correct handler depending on the workbench selection.
    
    In a future step, key bindings could be added to the commands.
    
    Change-Id: I910f234e8880bc2e2fbfbd495ac97b63967a38b9
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/MergeAction.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/MergeAction.java
@@ -11,99 +11,14 @@
 
 package org.eclipse.egit.ui.internal.actions;
 
-import java.io.IOException;
-
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.egit.core.op.MergeOperation;
-import org.eclipse.egit.ui.Activator;
-import org.eclipse.egit.ui.UIText;
-import org.eclipse.egit.ui.internal.dialogs.MergeTargetSelectionDialog;
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.Ref;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.lib.RepositoryState;
-import org.eclipse.osgi.util.NLS;
-import org.eclipse.team.internal.ui.Utils;
-
 /**
  * Action for selecting a commit and merging it with the current branch.
  */
 public class MergeAction extends RepositoryAction {
-
-	@Override
-	public void execute(IAction action) {
-		final Repository repository = getRepository(true);
-		if (repository == null)
-			return;
-
-		if (!canMerge(repository))
-			return;
-
-		MergeTargetSelectionDialog mergeTargetSelectionDialog = new MergeTargetSelectionDialog(
-				getShell(), repository);
-		if (mergeTargetSelectionDialog.open() == IDialogConstants.OK_ID) {
-
-			final String refName = mergeTargetSelectionDialog.getRefName();
-
-			String jobname = NLS.bind(UIText.MergeAction_JobNameMerge, refName);
-			Job job = new Job(jobname) {
-				@Override
-				protected IStatus run(IProgressMonitor monitor) {
-					try {
-						new MergeOperation(repository, refName).execute(monitor);
-					} catch (final CoreException e) {
-						getShell().getDisplay().asyncExec(new Runnable(){
-							public void run() {
-								Utils.handleError(getShell(), e, "Merge impossible", "Unsupported Operation"); //$NON-NLS-1$ //$NON-NLS-2$
-							}});
-					}
-					return Status.OK_STATUS;
-				}
-			};
-			job.setUser(true);
-			job.schedule();
-
-		}
-
+	/**
+	 *
+	 */
+	public MergeAction() {
+		super(ActionCommands.MERGE_ACTION);
 	}
-
-	private boolean canMerge(final Repository repository) {
-		String message = null;
-		try {
-			Ref head = repository.getRef(Constants.HEAD);
-			if (head == null || !head.isSymbolic())
-				message = UIText.MergeAction_HeadIsNoBranch;
-			else if (!repository.getRepositoryState().equals(
-					RepositoryState.SAFE))
-				message = NLS.bind(
-						UIText.MergeAction_WrongRepositoryState,
-						repository.getRepositoryState());
-		} catch (IOException e) {
-			Activator.logError(e.getMessage(), e);
-			message = e.getMessage();
-		}
-
-		if (message != null) {
-			MessageDialog.openError(getShell(),
-					UIText.MergeAction_CannotMerge, message);
-		}
-		return (message == null);
-	}
-
-	@Override
-	public boolean isEnabled() {
-		boolean enabled = true;
-		Repository repository = getRepository(false);
-		if (repository == null)
-			enabled = false;
-		return enabled;
-	}
-
 }
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/ResetAction.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/ResetAction.java
@@ -10,22 +10,7 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.actions;
 
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.egit.core.op.ResetOperation;
-import org.eclipse.egit.core.op.ResetOperation.ResetType;
-import org.eclipse.egit.ui.Activator;
-import org.eclipse.egit.ui.UIText;
-import org.eclipse.egit.ui.internal.decorators.GitLightweightDecorator;
-import org.eclipse.egit.ui.internal.dialogs.ResetTargetSelectionDialog;
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.osgi.util.NLS;
 
 /**
  * An action to reset the current branch to a specific revision.
@@ -33,45 +18,10 @@ import org.eclipse.osgi.util.NLS;
  * @see ResetOperation
  */
 public class ResetAction extends RepositoryAction {
-
-	@Override
-	public void execute(IAction action) {
-		final Repository repository = getRepository(true);
-		if (repository == null)
-			return;
-		if (!repository.getRepositoryState().canResetHead()) {
-			MessageDialog.openError(getShell(), UIText.ResetAction_errorResettingHead,
-					NLS.bind(UIText.ResetAction_repositoryState, repository.getRepositoryState().getDescription()));
-			return;
-		}
-		ResetTargetSelectionDialog branchSelectionDialog = new ResetTargetSelectionDialog(getShell(), repository);
-		if (branchSelectionDialog.open() == IDialogConstants.OK_ID) {
-			final String refName = branchSelectionDialog.getRefName();
-			final ResetType type = branchSelectionDialog.getResetType();
-			String jobname = NLS.bind(UIText.ResetAction_reset, refName);
-			final ResetOperation operation = new ResetOperation(repository,
-					refName, type);
-			Job job = new Job(jobname) {
-				@Override
-				protected IStatus run(IProgressMonitor monitor) {
-					try {
-						operation.execute(monitor);
-						GitLightweightDecorator.refresh();
-					} catch (CoreException e) {
-						return Activator.createErrorStatus(e.getStatus()
-								.getMessage(), e);
-					}
-					return Status.OK_STATUS;
-				}
-			};
-			job.setRule(operation.getSchedulingRule());
-			job.setUser(true);
-			job.schedule();
-		}
-	}
-
-	@Override
-	public boolean isEnabled() {
-		return getRepository(false) != null;
+	/**
+	 *
+	 */
+	public ResetAction() {
+		super(ActionCommands.RESET_ACTION);
 	}
 }
