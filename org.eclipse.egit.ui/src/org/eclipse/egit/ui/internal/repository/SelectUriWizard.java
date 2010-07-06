7ce6eb2387082263d74504b52f8b9ffe14f86c4b
Matthias Sohn <matthias.sohn@sap.com>
2010-07-06 16:10:01 +0200

    Fix typo in class name and add missing license header
    Also fix javadoc for class SelectUriWizard.
    
    Change-Id: I0fe457d0c22c39acace72fd5b0a0e9781ca637a0
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/SelectUriWizard.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2010 SAP AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Mathias Kinzler (SAP AG) - initial implementation
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.repository;
+
+import org.eclipse.egit.ui.internal.components.RepositorySelectionPage;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.jgit.transport.URIish;
+
+/**
+ * Wizard to select Uri
+ */
+public class SelectUriWizard extends Wizard {
+
+	private URIish uri;
+
+	/**
+	 * @param sourceSelection
+	 */
+	public SelectUriWizard(boolean sourceSelection) {
+		addPage(new RepositorySelectionPage(sourceSelection, null));
+	}
+
+	/**
+	 * @param sourceSelection
+	 * @param presetUri
+	 */
+	public SelectUriWizard(boolean sourceSelection, String presetUri) {
+		addPage(new RepositorySelectionPage(sourceSelection, presetUri));
+	}
+
+	/**
+	 * @return the URI
+	 */
+	public URIish getUri() {
+		return uri;
+	}
+
+	@Override
+	public boolean performFinish() {
+		uri = ((RepositorySelectionPage) getPages()[0]).getSelection().getURI();
+		return uri != null;
+	}
+
+}
