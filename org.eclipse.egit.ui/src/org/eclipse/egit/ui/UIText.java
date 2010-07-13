9ce2a79b2230d2b5c1dbf78ce282798032667480
Mathias Kinzler <mathias.kinzler@sap.com>
2010-07-13 11:44:07 +0200

    Enable Reset To Tag
    This enables "reset" to tag on the Team->Reset menu as well as in the
    Git Repositories View.
    
    Bug: 317350
    Change-Id: Iea59392ae9fd8834e4270908d795b48ecf4662b1
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -142,6 +142,12 @@ public class UIText extends NLS {
 	public static String SelectRemoteNamePage_SelectRemoteNameMessage;
 
 	/** */
+	public static String SelectResetTypePage_PageMessage;
+
+	/** */
+	public static String SelectResetTypePage_PageTitle;
+
+	/** */
 	public static String SetQuickdiffBaselineAction_setQuickdiffBaseline;
 
 	/** */
@@ -826,6 +832,12 @@ public class UIText extends NLS {
 	public static String ResetAction_reset;
 
 	/** */
+	public static String ResetCommand_ResetFailureMessage;
+
+	/** */
+	public static String ResetCommand_WizardTitle;
+
+	/** */
 	public static String ResetQuickdiffBaselineAction_resetQuickdiffBaseline;
 
 	/** */
@@ -857,6 +869,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String ResetTargetSelectionDialog_SelectBranchForResetMessage;
+
+	/** */
+	public static String ResetTargetSelectionDialog_WindowTitle;
 
 	/** */
 	public static String ResourceHistory_toggleCommentWrap;
