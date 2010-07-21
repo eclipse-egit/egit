dc41892521b926cdf99f2e5f2592565b08dc89b8
Mathias Kinzler <mathias.kinzler@sap.com>
2010-07-21 11:35:02 +0200

    EGit UI Tests: fix timing problems
    With the advent of http://egit.eclipse.org/r/#change,735,
    one of the GitCloneWizard tests fails if a "replay delay" is used,
    since the "fix" branch of the test repository alone is cloning too fast for the
    SWT Bot to capture the window that is displayed during clone.
    On the conrary, without a delay, one of the Git Repositories View
    tests fails when checking for the label decorations after check-out.
    
    With these fixes, both the delayed and the undelayed launch
    configurations should work for running AllTests (the memory
    settings for the launch configurations were also adjusted in order
    to accommodate the Synchronize tests).
    
    Change-Id: Id9d29a625b6163dab3bd1a660f811f8610346773
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/repositories/GitRepositoriesViewTagHandlingTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/repositories/GitRepositoriesViewTagHandlingTest.java
@@ -114,6 +114,7 @@ public class GitRepositoriesViewTagHandlingTest extends
 		SWTBotShell resetDialog = bot.shell(UIText.ResetCommand_WizardTitle);
 		pressAltAndChar(resetDialog, 'H');
 		resetDialog.bot().button(IDialogConstants.FINISH_LABEL).click();
+		waitInUI();
 
 		bot.shell(UIText.ResetTargetSelectionDialog_ResetQuestion).bot()
 				.button(IDialogConstants.YES_LABEL).click();
