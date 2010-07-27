d7018d9afd50db3acd2368b1ea75534d7b7c826e
Shawn O. Pearce <spearce@spearce.org>
2010-07-27 10:10:24 -0700

    Fix RepositoryPropertySource's use of FileBasedConfig
    Instead try to use the StoredConfig on the Repository, but use
    special case for the common FileBasedConfig case to ensure it
    works like the user would expect for file based repositories.
    
    Change-Id: I8b553e71509917c9b655efc0082112786d4f8c14
    Signed-off-by: Shawn O. Pearce <spearce@spearce.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoryPropertySource.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoryPropertySource.java
@@ -65,7 +65,7 @@ public class RepositoryPropertySource implements IPropertySource,
 
 	private final FileBasedConfig userHomeConfig;
 
-	private final FileBasedConfig repositoryConfig;
+	private final StoredConfig repositoryConfig;
 
 	private final StoredConfig effectiveConfig;
 
@@ -83,9 +83,13 @@ public class RepositoryPropertySource implements IPropertySource,
 
 		effectiveConfig = rep.getConfig();
 		userHomeConfig = SystemReader.getInstance().openUserConfig(FS.DETECTED);
-		// TODO constant?
-		File configFile = new File(rep.getDirectory(), "config"); //$NON-NLS-1$
-		repositoryConfig = new FileBasedConfig(configFile);
+
+		if (effectiveConfig instanceof FileBasedConfig) {
+			File configFile = ((FileBasedConfig) effectiveConfig).getFile();
+			repositoryConfig = new FileBasedConfig(configFile, rep.getFS());
+		} else {
+			repositoryConfig = effectiveConfig;
+		}
 
 		try {
 			effectiveConfig.load();
@@ -319,8 +323,12 @@ public class RepositoryPropertySource implements IPropertySource,
 					resultList.add(desc);
 				}
 			}
-			categoryString = UIText.RepositoryPropertySource_RepositoryConfigurationCategory
-					+ repositoryConfig.getFile().getAbsolutePath();
+
+			categoryString = UIText.RepositoryPropertySource_RepositoryConfigurationCategory;
+			if (repositoryConfig instanceof FileBasedConfig) {
+				categoryString += ((FileBasedConfig) repositoryConfig)
+						.getFile().getAbsolutePath();
+			}
 
 			boolean editable = true;
 
@@ -427,7 +435,7 @@ public class RepositoryPropertySource implements IPropertySource,
 		return isPropertySet(id);
 	}
 
-	private void setConfigValue(FileBasedConfig configuration, String key,
+	private void setConfigValue(StoredConfig configuration, String key,
 			String value) throws IOException {
 		// we un-set empty strings, as the config API does not allow to
 		// distinguish this case
