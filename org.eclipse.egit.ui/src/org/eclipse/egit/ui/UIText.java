36ba0919fe12b576a0c661a9282181d4d007b19b
Mathias Kinzler <mathias.kinzler@sap.com>
2010-08-06 13:33:14 +0200

    Git Configuration UI
    The "Properties" view shows the configuration of the repository
    currently selected in the repositories view. The toolbar was cleaned up
    and simplified and the "direct editing" available in the properties view
    was removed. Instead, when hitting a new "Edit" button in the toolbar, a 
    dialog appears presenting the same UI that is used in the "Global Git Configuration"
    preference page which can then be used to edit the configuration in a comfortable
    way.
    
    Change-Id: I9c15f03379683fc61c61fcf53cc2668883f12bc3
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -310,6 +310,12 @@ public class UIText extends NLS {
 	public static String RepositoryPropertySource_ConfigureKeysAction;
 
 	/** */
+	public static String RepositoryPropertySource_EditConfigButton;
+
+	/** */
+	public static String RepositoryPropertySource_EditConfigurationTitle;
+
+	/** */
 	public static String RepositoryPropertySource_EffectiveConfigurationAction;
 
 	/** */
@@ -322,10 +328,28 @@ public class UIText extends NLS {
 	public static String RepositoryPropertySource_GlobalConfigurationCategory;
 
 	/** */
+	public static String RepositoryPropertySource_GlobalConfigurationMenu;
+
+	/** */
+	public static String RepositoryPropertySource_RepositoryConfigurationButton;
+
+	/** */
 	public static String RepositoryPropertySource_RepositoryConfigurationCategory;
 
 	/** */
 	public static String RepositoryPropertySource_RestoreStandardAction;
+
+	/** */
+	public static String RepositoryPropertySource_SelectModeTooltip;
+
+	/** */
+	public static String RepositoryPropertySource_SingleValueButton;
+
+	/** */
+	public static String RepositoryPropertySource_SuppressMultipleValueTooltip;
+
+	/** */
+	public static String RepositoryPropertySource_WrongFormatMessage;
 
 	/** */
 	public static String RepositoryRemotePropertySource_ErrorHeader;
@@ -1148,6 +1172,66 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CommitDialog_AddChangeIdTooltip;
+
+	/** */
+	public static String ConfigurationEditorComponent_AddButton;
+
+	/** */
+	public static String ConfigurationEditorComponent_ChangeButton;
+
+	/** */
+	public static String ConfigurationEditorComponent_ConfigLocationLabel;
+
+	/** */
+	public static String ConfigurationEditorComponent_EmptyStringNotAllowed;
+
+	/** */
+	public static String ConfigurationEditorComponent_KeyColumnHeader;
+
+	/** */
+	public static String ConfigurationEditorComponent_NewValueButton;
+
+	/** */
+	public static String ConfigurationEditorComponent_NoEntrySelectedMessage;
+
+	/** */
+	public static String ConfigurationEditorComponent_NoSectionSubsectionMessage;
+
+	/** */
+	public static String ConfigurationEditorComponent_OpenEditorButton;
+
+	/** */
+	public static String ConfigurationEditorComponent_OpenEditorTooltip;
+
+	/** */
+	public static String ConfigurationEditorComponent_RemoveAllButton;
+
+	/** */
+	public static String ConfigurationEditorComponent_RemoveAllTooltip;
+
+	/** */
+	public static String ConfigurationEditorComponent_RemoveButton;
+
+	/** */
+	public static String ConfigurationEditorComponent_RemoveSectionMessage;
+
+	/** */
+	public static String ConfigurationEditorComponent_RemoveSectionTitle;
+
+	/** */
+	public static String ConfigurationEditorComponent_RemoveSubsectionMessage;
+
+	/** */
+	public static String ConfigurationEditorComponent_RemoveSubsectionTitle;
+
+	/** */
+	public static String ConfigurationEditorComponent_ValueColumnHeader;
+
+	/** */
+	public static String ConfigurationEditorComponent_ValueLabel;
+
+	/** */
+	public static String ConfigurationEditorComponent_WrongNumberOfTokensMessage;
 
 	/** */
 	public static String ConfigureKeysDialog_AddStandardButton;
@@ -2014,66 +2098,6 @@ public class UIText extends NLS {
 	public static String GitShareProjectsPage_RepositoryLabel;
 
 	/** */
-	public static String GlobalConfigurationPreferencePage_AddButton;
-
-	/** */
-	public static String GlobalConfigurationPreferencePage_ChangeButton;
-
-	/** */
-	public static String GlobalConfigurationPreferencePage_ConfigLocationLabel;
-
-	/** */
-	public static String GlobalConfigurationPreferencePage_EmptyStringNotAllowed;
-
-	/** */
-	public static String GlobalConfigurationPreferencePage_KeyColumnHeader;
-
-	/** */
-	public static String GlobalConfigurationPreferencePage_NewValueButton;
-
-	/** */
-	public static String GlobalConfigurationPreferencePage_NoEntrySelectedMessage;
-
-	/** */
-	public static String GlobalConfigurationPreferencePage_NoSectionSubsectionMessage;
-
-	/** */
-	public static String GlobalConfigurationPreferencePage_OpenEditorButton;
-
-	/** */
-	public static String GlobalConfigurationPreferencePage_OpenEditorTooltip;
-
-	/** */
-	public static String GlobalConfigurationPreferencePage_RemoveAllButton;
-
-	/** */
-	public static String GlobalConfigurationPreferencePage_RemoveAllTooltip;
-
-	/** */
-	public static String GlobalConfigurationPreferencePage_RemoveButton;
-
-	/** */
-	public static String GlobalConfigurationPreferencePage_RemoveSectionMessage;
-
-	/** */
-	public static String GlobalConfigurationPreferencePage_RemoveSectionTitle;
-
-	/** */
-	public static String GlobalConfigurationPreferencePage_RemoveSubsectionMessage;
-
-	/** */
-	public static String GlobalConfigurationPreferencePage_RemoveSubsectionTitle;
-
-	/** */
-	public static String GlobalConfigurationPreferencePage_ValueColumnHeader;
-
-	/** */
-	public static String GlobalConfigurationPreferencePage_ValueLabel;
-
-	/** */
-	public static String GlobalConfigurationPreferencePage_WrongNumberOfTokensMessage;
-
- 	/** */
 	public static String MergeAction_CannotMerge;
 
 	/** */
