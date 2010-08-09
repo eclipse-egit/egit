90c15d22db42e66383608be9ec7ea20d7b40f805
Dariusz Luksza <dariusz@luksza.org>
2010-08-09 17:46:18 +0200

    Update of GitSynchoronizeData API
    In Git synchronization we always operate on commit level changes
    therefore source and destination points should be always RevCommit
    objects. In case of synchronizing tags we should map given tag with
    proper RevCommit object. Instead of doing this multiple times on
    different levels I've add this resolution to GitSynchronizeData
    constructor.
    
    Change-Id: I6476b4940783d4b719d08d9c0c5bb3060b1ee412
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/GitRemoteResourceVariantTree.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/GitRemoteResourceVariantTree.java
@@ -11,15 +11,9 @@
  *******************************************************************************/
 package org.eclipse.egit.core.synchronize;
 
-import java.io.IOException;
-
 import org.eclipse.egit.core.synchronize.dto.GitSynchronizeData;
 import org.eclipse.egit.core.synchronize.dto.GitSynchronizeDataSet;
-import org.eclipse.jgit.lib.Ref;
-import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.revwalk.RevWalk;
-import org.eclipse.jgit.revwalk.filter.RevFilter;
 import org.eclipse.team.core.TeamException;
 import org.eclipse.team.core.variants.SessionResourceVariantByteStore;
 
@@ -31,22 +25,7 @@ class GitRemoteResourceVariantTree extends GitResourceVariantTree {
 
 	@Override
 	protected RevCommit getRevCommit(GitSynchronizeData gsd) throws TeamException {
-		RevCommit result;
-		Repository repo = gsd.getRepository();
-		RevWalk rw = new RevWalk(repo);
-		rw.setRevFilter(RevFilter.MERGE_BASE);
-
-		try {
-			Ref dstRef = gsd.getDstRev();
-			if (dstRef == null)
-				result = null;
-			else
-				result = rw.parseCommit(dstRef.getObjectId());
-		} catch (IOException e) {
-			throw new TeamException("", e); //$NON-NLS-1$
-		}
-
-		return result != null ? result : null;
+		return gsd.getDstRevCommit();
 	}
 
 }
