4810f05435adfb3874ddfede308321120c93ced8
Stefan Lay <stefan.lay@sap.com>
2010-08-10 14:32:04 +0200

    Set labels in the compare Editor used for contents of change sets
    Show the file name and the abbreviated commit id for the two files
    which are compared when double clicking on a member of a 
    change set in the synchronize view.
    
    Change-Id: Ie5e00c57a5c050293fd427ba6a272a24688f58ac
    Signed-off-by: Stefan Lay <stefan.lay@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/model/GitModelBlob.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/model/GitModelBlob.java
@@ -10,22 +10,29 @@ package org.eclipse.egit.ui.internal.synchronize.model;
 
 import java.io.IOException;
 
+import org.eclipse.compare.CompareConfiguration;
 import org.eclipse.compare.ITypedElement;
 import org.eclipse.compare.structuremergeviewer.Differencer;
-import org.eclipse.compare.structuremergeviewer.ICompareInput;
 import org.eclipse.compare.structuremergeviewer.ICompareInputChangeListener;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.internal.CompareUtils;
+import org.eclipse.egit.ui.internal.FileRevisionTypedElement;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.team.ui.mapping.ISynchronizationCompareInput;
+import org.eclipse.team.ui.mapping.SaveableComparison;
 
 /**
  * Git blob object representation in Git ChangeSet
  */
-public class GitModelBlob extends GitModelCommit implements ICompareInput {
+public class GitModelBlob extends GitModelCommit implements ISynchronizationCompareInput {
 
 	private final String name;
 
@@ -131,6 +138,34 @@ public class GitModelBlob extends GitModelCommit implements ICompareInput {
 
 	public void copy(boolean leftToRight) {
 		// do nothing, we should disallow coping content between commits
+	}
+
+	public SaveableComparison getSaveable() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void prepareInput(CompareConfiguration configuration,
+			IProgressMonitor monitor) throws CoreException {
+		configuration.setLeftLabel(getFileRevisionLabel((FileRevisionTypedElement)getLeft()));
+		configuration.setRightLabel(getFileRevisionLabel((FileRevisionTypedElement)getRight()));
+
+	}
+
+	private String getFileRevisionLabel(FileRevisionTypedElement element) {
+		return NLS.bind(UIText.GitCompareFileRevisionEditorInput_RevisionLabel,
+				new Object[]{element.getName(),
+				CompareUtils.truncatedRevision(element.getContentIdentifier()), element.getAuthor()});
+	}
+
+	public String getFullPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isCompareInputFor(Object object) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
