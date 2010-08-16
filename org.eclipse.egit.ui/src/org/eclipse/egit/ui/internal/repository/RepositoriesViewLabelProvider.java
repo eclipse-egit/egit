b1b9889bc8b677e79428beef7c968880fa7f8db7
Chris Aniszczyk <caniszczyk@gmail.com>
2010-08-16 15:17:06 -0500

    Support styled label provider in Repositories view
    The RepositoriesViewLabelProvider was enhanced to implement
    IStyledLabelProvider. I also added the current branch as
    a styled text decoration to repositories.
    
    Change-Id: I0726e9251b7c0db9dd1db5cbceaa5421f8bb7b0d
    Signed-off-by: Chris Aniszczyk <caniszczyk@gmail.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoriesViewLabelProvider.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoriesViewLabelProvider.java
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *    Mathias Kinzler (SAP AG) - initial implementation
+ *    Chris Aniszczyk <caniszczyk@gmail.com> - added styled label support
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.repository;
 
@@ -18,8 +19,11 @@ import java.util.Map;
 import org.eclipse.egit.ui.UIIcons;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
+import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNodeType;
 import org.eclipse.jface.resource.CompositeImageDescriptor;
+import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider.IStyledLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.StyledString;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
@@ -33,7 +37,7 @@ import org.eclipse.swt.graphics.Rectangle;
 /**
  * Label Provider for the Git Repositories View
  */
-public class RepositoriesViewLabelProvider extends LabelProvider {
+public class RepositoriesViewLabelProvider extends LabelProvider implements IStyledLabelProvider {
 
 	/**
 	 * A map of regular images to their decorated counterpart.
@@ -52,64 +56,8 @@ public class RepositoriesViewLabelProvider extends LabelProvider {
 			return null;
 
 		RepositoryTreeNode node = (RepositoryTreeNode) element;
-		switch (node.getType()) {
-		case REPO:
-			File directory = ((Repository) node.getObject()).getDirectory();
-			StringBuilder sb = new StringBuilder();
-			sb.append(directory.getParentFile().getName());
-			sb.append(" - "); //$NON-NLS-1$
-			sb.append(directory.getAbsolutePath());
-			return sb.toString();
-		case FILE:
-			// fall through
-		case FOLDER:
-			return ((File) node.getObject()).getName();
-		case BRANCHES:
-			return UIText.RepositoriesView_Branches_Nodetext;
-		case LOCALBRANCHES:
-			return UIText.RepositoriesViewLabelProvider_LocalBranchesNodetext;
-		case REMOTEBRANCHES:
-			return UIText.RepositoriesViewLabelProvider_RemoteBrancheNodetext;
-		case TAGS:
-			return UIText.RepositoriesViewLabelProvider_TagsNodeText;
-		case SYMBOLICREFS:
-			return UIText.RepositoriesViewLabelProvider_SymbolicRefNodeText;
-		case REMOTES:
-			return UIText.RepositoriesView_RemotesNodeText;
-		case REMOTE:
-			// fall through
-		case ERROR:
-			return (String) node.getObject();
-		case REF:
-			// fall through
-		case TAG:
-			// fall through
-		case SYMBOLICREF:
-			Ref ref = (Ref) node.getObject();
-			// shorten the name
-			String refName = node.getRepository().shortenRefName(ref.getName());
-			if (ref.isSymbolic()) {
-				refName = refName + " - " //$NON-NLS-1$
-						+ ref.getLeaf().getName();
-			}
-			return refName;
-		case WORKINGDIR:
-			if (node.getRepository().getConfig().getBoolean(
-					"core", "bare", false)) //$NON-NLS-1$ //$NON-NLS-2$
-				return UIText.RepositoriesView_WorkingDir_treenode
-						+ " - " //$NON-NLS-1$
-						+ UIText.RepositoriesViewLabelProvider_BareRepositoryMessage;
-			else
-				return UIText.RepositoriesView_WorkingDir_treenode + " - " //$NON-NLS-1$
-						+ node.getRepository().getWorkTree().getAbsolutePath();
-		case PUSH:
-			// fall through
-		case FETCH:
-			return (String) node.getObject();
 
-		}
-
-		return null;
+		return getSimpleText(node);
 	}
 
 	@Override
@@ -204,6 +152,86 @@ public class RepositoriesViewLabelProvider extends LabelProvider {
 			decoratedImages.put(image, decoratedImage);
 		}
 		return decoratedImage;
+	}
+
+	public StyledString getStyledText(Object element) {
+		if (!(element instanceof RepositoryTreeNode))
+			return null;
+
+		RepositoryTreeNode node = (RepositoryTreeNode) element;
+
+		String label = getSimpleText(node);
+		if (label == null) {
+			return new StyledString(element.toString());
+		}
+		StyledString text = new StyledString(label);
+
+		if (node.getType().equals(RepositoryTreeNodeType.REPO)) {
+			// do this
+		}
+
+		return text;
+
+	}
+
+	private String getSimpleText(RepositoryTreeNode node) {
+		switch (node.getType()) {
+		case REPO:
+			File directory = ((Repository) node.getObject()).getDirectory();
+			StringBuilder sb = new StringBuilder();
+			sb.append(directory.getParentFile().getName());
+			sb.append(" - "); //$NON-NLS-1$
+			sb.append(directory.getAbsolutePath());
+			return sb.toString();
+		case FILE:
+			// fall through
+		case FOLDER:
+			return ((File) node.getObject()).getName();
+		case BRANCHES:
+			return UIText.RepositoriesView_Branches_Nodetext;
+		case LOCALBRANCHES:
+			return UIText.RepositoriesViewLabelProvider_LocalBranchesNodetext;
+		case REMOTEBRANCHES:
+			return UIText.RepositoriesViewLabelProvider_RemoteBrancheNodetext;
+		case TAGS:
+			return UIText.RepositoriesViewLabelProvider_TagsNodeText;
+		case SYMBOLICREFS:
+			return UIText.RepositoriesViewLabelProvider_SymbolicRefNodeText;
+		case REMOTES:
+			return UIText.RepositoriesView_RemotesNodeText;
+		case REMOTE:
+			// fall through
+		case ERROR:
+			return (String) node.getObject();
+		case REF:
+			// fall through
+		case TAG:
+			// fall through
+		case SYMBOLICREF:
+			Ref ref = (Ref) node.getObject();
+			// shorten the name
+			String refName = node.getRepository().shortenRefName(ref.getName());
+			if (ref.isSymbolic()) {
+				refName = refName + " - " //$NON-NLS-1$
+				+ ref.getLeaf().getName();
+			}
+			return refName;
+		case WORKINGDIR:
+			if (node.getRepository().getConfig().getBoolean(
+					"core", "bare", false)) //$NON-NLS-1$ //$NON-NLS-2$
+				return UIText.RepositoriesView_WorkingDir_treenode
+				+ " - " //$NON-NLS-1$
+				+ UIText.RepositoriesViewLabelProvider_BareRepositoryMessage;
+			else
+				return UIText.RepositoriesView_WorkingDir_treenode + " - " //$NON-NLS-1$
+				+ node.getRepository().getWorkTree().getAbsolutePath();
+		case PUSH:
+			// fall through
+		case FETCH:
+			return (String) node.getObject();
+
+		}
+		return null;
 	}
 
 }
