868c34aecce725b6bcbe1bf3678053bb075968f5
Robin Rosenberg <robin.rosenberg@dewire.com>
2010-08-19 07:13:52 +0200

    Close the input stream after reading the merge message
    Change-Id: I5da1b698b9583dcf2d3a8b23c4f016793064b152
    Signed-off-by: Robin Rosenberg <robin.rosenberg@dewire.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CommitActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CommitActionHandler.java
@@ -346,27 +346,33 @@ public class CommitActionHandler extends RepositoryActionHandler {
 		FileReader reader;
 		try {
 			reader = new FileReader(mergeMsg);
+			BufferedReader br = new BufferedReader(reader);
+			try {
+				StringBuffer message = new StringBuffer();
+				String s;
+				String newLine = newLine();
+				while ((s = br.readLine()) != null) {
+					message.append(s).append(newLine);
+				}
+				return message.toString();
+			} catch (IOException e) {
+				MessageDialog.openError(getShell(event),
+						UIText.CommitAction_MergeHeadErrorTitle,
+						UIText.CommitAction_ErrorReadingMergeMsg);
+				throw new IllegalStateException(e);
+			} finally {
+				try {
+					br.close();
+				} catch (IOException e) {
+					// Empty
+				}
+			}
 		} catch (FileNotFoundException e) {
 			MessageDialog.openError(getShell(event),
 					UIText.CommitAction_MergeHeadErrorTitle,
 					UIText.CommitAction_MergeHeadErrorMessage);
 			throw new IllegalStateException(e);
 		}
-		BufferedReader br = new BufferedReader(reader);
-		StringBuffer message = new StringBuffer();
-		String s;
-		String newLine = newLine();
-		try {
-			while ((s = br.readLine()) != null) {
-				message.append(s).append(newLine);
-			}
-		} catch (IOException e) {
-			MessageDialog.openError(getShell(event),
-					UIText.CommitAction_MergeHeadErrorTitle,
-					UIText.CommitAction_ErrorReadingMergeMsg);
-			throw new IllegalStateException(e);
-		}
-		return message.toString();
 	}
 
 	private String newLine() {
