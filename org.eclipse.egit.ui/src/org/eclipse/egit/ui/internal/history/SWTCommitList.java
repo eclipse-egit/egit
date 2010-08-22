bc901dd584454cf8b3f55013f68b466f02fe9b99
Robin Rosenberg <robin.rosenberg@dewire.com>
2010-08-22 21:23:35 +0200

    Ovveride hashCode in SWTCommitList since superclass defines it
    Change-Id: Ic6474e85e8b8a09653e77a14351988b756b1be2a
    Signed-off-by: Robin Rosenberg <robin.rosenberg@dewire.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/SWTCommitList.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/SWTCommitList.java
@@ -59,5 +59,10 @@ class SWTCommitList extends PlotCommitList<SWTCommitList.SWTLane> {
 		public boolean equals(Object o) {
 			return super.equals(o) && color.equals(((SWTLane)o).color);
 		}
+
+		@Override
+		public int hashCode() {
+			return super.hashCode() ^ color.hashCode();
+		}
 	}
 }
