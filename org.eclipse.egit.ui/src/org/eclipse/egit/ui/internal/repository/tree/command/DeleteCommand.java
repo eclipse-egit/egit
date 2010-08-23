b19756872050dc22cd883b37daa2bb4493820a80
Mathias Kinzler <mathias.kinzler@sap.com>
2010-08-23 17:40:33 +0200

    Support Repository Deletion
    Bug: 297668
    Change-Id: Ice562d007d71dd1f3493f3ae748ec5ba21e6330a
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>
    Signed-off-by: Chris Aniszczyk <caniszczyk@gmail.com>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/DeleteCommand.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2010 SAP AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Mathias Kinzler (SAP AG) - initial implementation
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.repository.tree.command;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+
+/**
+ * Deletes a Repository
+ */
+public class DeleteCommand extends RemoveCommand implements IHandler {
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		super.removeRepository(event, true);
+		return null;
+	}
+}
