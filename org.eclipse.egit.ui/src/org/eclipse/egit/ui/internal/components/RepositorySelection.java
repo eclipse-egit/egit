2d5e652d728a23356bc009e5df94d11824702044
Mathias Kinzler <mathias.kinzler@sap.com>
2010-08-25 14:08:44 +0200

    Fix problems when pushing to a remote that has only a Push URL
    Push dosen't work if the remote to push to has only Push URLs, but no
    URL.
    
    Bug: 317389
    Bug: 321527
    Change-Id: If681dcdd8dc38bcc1282d65e7361fa610d67620c
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RepositorySelection.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RepositorySelection.java
@@ -56,7 +56,12 @@ public class RepositorySelection {
 	 */
 	public URIish getURI() {
 		if (isConfigSelected())
-			return config.getURIs().get(0);
+			if (config.getURIs().size() > 0)
+				return config.getURIs().get(0);
+			else if (config.getPushURIs().size() > 0)
+				return config.getPushURIs().get(0);
+			else
+				return null;
 		return uri;
 	}
 
