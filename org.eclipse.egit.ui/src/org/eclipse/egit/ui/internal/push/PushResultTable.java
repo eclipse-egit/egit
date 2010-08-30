648f5b8dcbbf892a555a68668cb0469cd8d3da92
Jens Baumgart <jens.baumgart@sap.com>
2010-08-30 13:45:18 +0200

    Display details of push result
    PushResultTable was enhanced to show the details of a push result.
    Error messages and the information returned by the push operation
    (e.g. the gerrit review URL) are shown in a separate text control.
    The colors of the result status (green, red) were replaced by an icon.
    
    Bug: 319974
    Change-Id: I4000c80b8a3365dea3b4b6977f3e03271e634154
    Signed-off-by: Jens Baumgart <jens.baumgart@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/push/PushResultTable.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/push/PushResultTable.java
@@ -11,6 +11,7 @@ package org.eclipse.egit.ui.internal.push;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.egit.core.op.PushOperationResult;
 import org.eclipse.egit.ui.UIIcons;
@@ -22,6 +23,11 @@ import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.jface.viewers.ColumnViewerToolTipSupport;
 import org.eclipse.jface.viewers.ColumnWeightData;
 import org.eclipse.jface.viewers.IElementComparer;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.jgit.lib.Constants;
@@ -30,8 +36,8 @@ import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.transport.RemoteRefUpdate;
-import org.eclipse.jgit.transport.URIish;
 import org.eclipse.jgit.transport.RemoteRefUpdate.Status;
+import org.eclipse.jgit.transport.URIish;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.DisposeEvent;
@@ -44,6 +50,7 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.Text;
 
 /**
  * Table displaying push operation results.
@@ -52,6 +59,8 @@ class PushResultTable {
 	private static final int TABLE_PREFERRED_WIDTH = 650;
 
 	private static final int TABLE_PREFERRED_HEIGHT = 300;
+
+	private static final int TEXT_PREFERRED_HEIGHT = 100;
 
 	private static final int COLUMN_STATUS_WEIGHT = 40;
 
@@ -64,6 +73,12 @@ class PushResultTable {
 	private static final String IMAGE_DELETE = "MODE_DELETE"; //$NON-NLS-1$
 
 	private static final String IMAGE_ADD = "MODE_ADD"; //$NON-NLS-1$
+
+	private static final String EMPTY_STRING = ""; //$NON-NLS-1$
+
+	private static final String SPACE = " "; //$NON-NLS-1$
+
+	private static final String NEWLINE = "\n"; //$NON-NLS-1$
 
 	private final TableViewer tableViewer;
 
@@ -127,6 +142,63 @@ class PushResultTable {
 		});
 		tableViewer.setContentProvider(new RefUpdateContentProvider());
 		tableViewer.setInput(null);
+		// detail message
+		final Text text = new Text(parent, SWT.MULTI | SWT.READ_ONLY
+				| SWT.BORDER);
+		GridData textLayoutData = new GridData(SWT.FILL, SWT.FILL, true, true);
+		textLayoutData.heightHint = TEXT_PREFERRED_HEIGHT;
+		text.setLayoutData(textLayoutData);
+		tableViewer
+				.addSelectionChangedListener(new ISelectionChangedListener() {
+					public void selectionChanged(SelectionChangedEvent event) {
+						ISelection selection = event.getSelection();
+						if (!(selection instanceof IStructuredSelection)) {
+							text.setText(EMPTY_STRING);
+							return;
+						}
+						IStructuredSelection structuredSelection = (IStructuredSelection) selection;
+						if (structuredSelection.size() != 1) {
+							text.setText(EMPTY_STRING);
+							return;
+						}
+						RefUpdateElement element = (RefUpdateElement) structuredSelection
+								.getFirstElement();
+						text.setText(getResult(element));
+					}
+				});
+	}
+
+	private String getResult(RefUpdateElement element) {
+		StringBuffer result = new StringBuffer(EMPTY_STRING);
+		PushOperationResult pushOperationResult = element
+				.getPushOperationResult();
+		Set<URIish> urIs = pushOperationResult.getURIs();
+		for (URIish uri : urIs) {
+			result.append(UIText.PushResultTable_repository);
+			result.append(SPACE);
+			result.append(uri.toString());
+			result.append(NEWLINE);
+			result.append(NEWLINE);
+			String message = element.getRemoteRefUpdate(uri).getMessage();
+			if (message != null) {
+				result.append(message);
+				result.append(NEWLINE);
+			}
+			StringBuffer messagesBuffer = new StringBuffer(pushOperationResult
+					.getPushResult(uri).getMessages());
+			trim(messagesBuffer);
+			result.append(messagesBuffer);
+		}
+		return result.toString();
+	}
+
+	private static void trim(StringBuffer s) {
+		// remove leading new lines
+		while (s.length()>0 && s.charAt(0)=='\n')
+			s.deleteCharAt(0);
+		// remove trailing new lines
+		while (s.length()>0 && s.charAt(s.length()-1)=='\n')
+			s.deleteCharAt(s.length()-1);
 	}
 
 	void setData(final Repository localDb, final PushOperationResult result) {
@@ -189,11 +261,17 @@ class PushResultTable {
 		for (final URIish uri : result.getURIs()) {
 			final TableViewerColumn statusViewer = createColumn(layout, NLS
 					.bind(UIText.PushResultTable_columnStatusRepo, Integer
-							.toString(++i)), COLUMN_STATUS_WEIGHT, SWT.CENTER);
+							.toString(++i)), COLUMN_STATUS_WEIGHT, SWT.LEFT);
 			statusViewer.getColumn().setToolTipText(uri.toString());
 			statusViewer.setLabelProvider(new UpdateStatusLabelProvider(uri));
 		}
 		tableViewer.setInput(result);
+		// select the first row of table to get the details of the first
+		// push result shown in the Text control
+		Table table = tableViewer.getTable();
+		if (table.getItemCount()>0) {
+			tableViewer.setSelection(new StructuredSelection(table.getItem(0).getData()));
+		}
 		tablePanel.layout();
 	}
 
@@ -272,23 +350,22 @@ class PushResultTable {
 		}
 
 		@Override
-		public Color getBackground(Object element) {
+		public Image getImage(Object element) {
 			final RefUpdateElement rue = (RefUpdateElement) element;
 			if (!rue.isSuccessfulConnection(uri))
-				return rejectedColor;
-
+				return imageRegistry.get(IMAGE_DELETE);
 			final Status status = rue.getRemoteRefUpdate(uri).getStatus();
 			switch (status) {
 			case OK:
-				return updatedColor;
+				return null; // no icon for ok
 			case UP_TO_DATE:
 			case NON_EXISTING:
-				return upToDateColor;
+				return null; // no icon for up to date
 			case REJECTED_NODELETE:
 			case REJECTED_NONFASTFORWARD:
 			case REJECTED_REMOTE_CHANGED:
 			case REJECTED_OTHER_REASON:
-				return rejectedColor;
+				return imageRegistry.get(IMAGE_DELETE);
 			default:
 				throw new IllegalArgumentException(NLS.bind(
 						UIText.PushResultTable_statusUnexpected, status));
