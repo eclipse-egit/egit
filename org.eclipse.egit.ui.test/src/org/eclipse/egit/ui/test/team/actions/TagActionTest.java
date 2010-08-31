b3aff99e5f92452b7569f13163e1d793ff020d60
Matthias Sohn <matthias.sohn@sap.com>
2010-08-31 21:54:47 +0200

    Replace removed PersonIndent constructor by RawParseUtils
    JGit change http://egit.eclipse.org/r/#change,1472 removed
    the deprecated constructor PersonIndent(String).
    
    Change-Id: If28171482d937a49f94c8e85351dd16daacf6a4b
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/team/actions/TagActionTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/team/actions/TagActionTest.java
@@ -25,9 +25,9 @@ import org.eclipse.egit.ui.test.ContextMenuHelper;
 import org.eclipse.egit.ui.test.TestUtil;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.PersonIdent;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.TagBuilder;
+import org.eclipse.jgit.util.RawParseUtils;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotPerspective;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
@@ -57,7 +57,7 @@ public class TagActionTest extends LocalRepositoryTestCase {
 
 		TagBuilder tag = new TagBuilder();
 		tag.setTag("SomeTag");
-		tag.setTagger(new PersonIdent(TestUtil.TESTAUTHOR));
+		tag.setTagger(RawParseUtils.parsePersonIdent(TestUtil.TESTAUTHOR));
 		tag.setMessage("I'm just a little tag");
 		tag.setObjectId(repo.resolve(repo.getFullBranch()), Constants.OBJ_COMMIT);
 		TagOperation top = new TagOperation(repo, tag, false);
