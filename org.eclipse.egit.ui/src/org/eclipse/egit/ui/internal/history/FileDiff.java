f097acc40d92a5ba7a24ced49ba9da6b2d7092bc
Matthias Sohn <matthias.sohn@sap.com>
2010-09-03 01:31:30 +0200

    Adapt to incompatible change in DiffFormater
    JGit commit ec2fdbf2ba changed 
    	public void formatEdits(final RawText a, final RawText b, 
    		final EditList edits)
    incompatibly to
    	public void format(final EditList edits, final RawText a,
    		final RawText b)
    
    Change-Id: I634a9154d3fc6f3c8f1500b34f71879ccf888070
    Signed-off-by: Jens Baumgart <jens.baumgart@sap.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/FileDiff.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/FileDiff.java
@@ -153,7 +153,7 @@ class FileDiff {
 		final RawText a = getRawText(id1, reader);
 		final RawText b = getRawText(id2, reader);
 		final MyersDiff diff = new MyersDiff(a, b);
-		diffFmt.formatEdits(a, b, diff.getEdits());
+		diffFmt.format(diff.getEdits(), a, b);
 	}
 
 	private String getProjectRelativePath(Repository db, String repoPath) {
