6fc349ef976a63a8a3db6878b0d529d64d9c2b5a
Mathias Kinzler <mathias.kinzler@sap.com>
2010-09-07 14:51:55 +0200

    Supportability: Provide a Trace Configuration Preference Page
    Since we have the infrastructure in place, we can now switch debug
    tracing on and off in a running IDE.
    This can be very helpful in situations where we can't debug.
    Unfortunately, there is no generic UI to do this,
    so this adds a dialog to show the existing trace locations for
    the EGit UI and EGit Core plug-ins and allows to activate them.
    The dialog can not be started from a "normal" UI control, but only
    using CTRL-3 and triggering a dedicated command in order to avoid
    polllution of the Preference UI.
    
    Change-Id: I4681acf85de8d05be6290d88acde5d99453ce89d
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commands/ConfigureDebugTraceCommand.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (C) 2010, Mathias Kinzler <mathias.kinzler@sap.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.commands;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.egit.ui.internal.dialogs.GitTraceConfigurationDialog;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+/**
+ * Opens the "Configure Debug Trace" page
+ */
+public class ConfigureDebugTraceCommand extends AbstractHandler {
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		new GitTraceConfigurationDialog(HandlerUtil
+				.getActiveShellChecked(event)).open();
+		return null;
+	}
+}
