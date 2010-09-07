b8bd9d58dff3329843ac7b452408e2a63ac41740
Mathias Kinzler <mathias.kinzler@sap.com>
2010-09-07 15:11:11 +0200

    Supportability: Provide a Trace Configuration Preference Page
    Since we have the infrastructure in place, we can now switch debug
    tracing on and off in a running IDE. This can be very helpful in
    situations where we can't debug.
    
    Unfortunately, there is no generic UI to do this, so this adds a
    dialog to show the existing trace locations for the EGit UI and
    EGit Core plug-ins and allows to activate them.
    
    The dialog cannot be started from a "normal" UI control, but only
    using CTRL-3 and triggering a dedicated command "Configure Git
    Debug Trace" in order to avoid pollution of the Preference UI.
    
    Change-Id: I4681acf85de8d05be6290d88acde5d99453ce89d
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

---
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/trace/TraceConfigurationDialogTest.java
@@ -0,0 +1,170 @@
+/*******************************************************************************
+ * Copyright (c) 2010, SAP AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Mathias Kinzler (SAP AG) - initial implementation
+ *******************************************************************************/
+package org.eclipse.egit.ui.test.trace;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.UIText;
+import org.eclipse.egit.ui.common.EGitTestCase;
+import org.eclipse.egit.ui.internal.dialogs.GitTraceConfigurationDialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotCheckBox;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SWTBotJunit4ClassRunner.class)
+public class TraceConfigurationDialogTest {
+
+	private static final SWTWorkbenchBot bot = new SWTWorkbenchBot();
+
+	private SWTBotShell configurationDialog;
+
+	@BeforeClass
+	public static void beforeClass() throws Exception {
+		EGitTestCase.closeWelcomePage();
+		// make sure tracing is off globally
+		Activator.getDefault().getDebugOptions().setDebugEnabled(false);
+	}
+
+	@AfterClass
+	public static void afterClass() throws Exception {
+		// make sure tracing is off globally
+		Activator.getDefault().getDebugOptions().setDebugEnabled(false);
+	}
+
+	@Before
+	public void before() throws Exception {
+		getDialog();
+	}
+
+	@Test
+	public void testMainSwitch() throws Exception {
+		SWTBotCheckBox box = configurationDialog.bot().checkBox(
+				UIText.GitTraceConfigurationDialog_PlatformSwitchCheckbox);
+		SWTBotTree tree = findTree();
+		assertFalse(box.isChecked());
+		assertFalse(tree.isEnabled());
+		box.click();
+		assertTrue(box.isChecked());
+		assertTrue(tree.isEnabled());
+
+		configurationDialog.close();
+		getDialog();
+		box = configurationDialog.bot().checkBox(
+				UIText.GitTraceConfigurationDialog_PlatformSwitchCheckbox);
+
+		tree = findTree();
+		assertFalse(box.isChecked());
+		assertFalse(tree.isEnabled());
+		box.click();
+		assertTrue(box.isChecked());
+		assertTrue(tree.isEnabled());
+
+		configurationDialog.bot().button(IDialogConstants.OK_LABEL).click();
+		getDialog();
+		box = configurationDialog.bot().checkBox(
+				UIText.GitTraceConfigurationDialog_PlatformSwitchCheckbox);
+
+		tree = findTree();
+		assertTrue(box.isChecked());
+		assertTrue(tree.isEnabled());
+		box.click();
+		assertFalse(box.isChecked());
+		assertFalse(tree.isEnabled());
+		configurationDialog.bot().button(IDialogConstants.OK_LABEL).click();
+	}
+
+	@Test
+	public void testTreeNode() throws Exception {
+		SWTBotCheckBox box = configurationDialog.bot().checkBox(
+				UIText.GitTraceConfigurationDialog_PlatformSwitchCheckbox);
+		SWTBotTree tree = findTree();
+		assertFalse(box.isChecked());
+		assertFalse(tree.isEnabled());
+		box.click();
+		assertTrue(box.isChecked());
+		assertTrue(tree.isEnabled());
+
+		SWTBotTreeItem item = tree.getAllItems()[0];
+		assertFalse(item.isChecked());
+		item.check();
+		assertTrue(item.isChecked());
+
+		configurationDialog.close();
+		getDialog();
+		box = configurationDialog.bot().checkBox(
+				UIText.GitTraceConfigurationDialog_PlatformSwitchCheckbox);
+
+		tree = findTree();
+		assertFalse(box.isChecked());
+		assertFalse(tree.isEnabled());
+		box.click();
+		assertTrue(box.isChecked());
+		assertTrue(tree.isEnabled());
+
+		item = tree.getAllItems()[0];
+		assertFalse(item.isChecked());
+		item.check();
+		assertTrue(item.isChecked());
+
+		configurationDialog.bot().button(IDialogConstants.OK_LABEL).click();
+		getDialog();
+		box = configurationDialog.bot().checkBox(
+				UIText.GitTraceConfigurationDialog_PlatformSwitchCheckbox);
+
+		tree = findTree();
+		assertTrue(box.isChecked());
+		assertTrue(tree.isEnabled());
+
+		item = tree.getAllItems()[0];
+		assertTrue(item.isChecked());
+
+		configurationDialog.bot().button(
+				UIText.GitTraceConfigurationDialog_DefaultButton).click();
+
+		assertFalse(item.isChecked());
+		box.click();
+		configurationDialog.bot().button(IDialogConstants.OK_LABEL).click();
+	}
+
+	private SWTBotTree findTree() {
+		return configurationDialog.bot().treeWithId("LocationTree");
+	}
+
+	private void getDialog() {
+		Display.getDefault().syncExec(new Runnable() {
+			public void run() {
+				if (configurationDialog != null)
+					configurationDialog.close();
+
+				GitTraceConfigurationDialog dlg = new GitTraceConfigurationDialog(
+						new Shell(Display.getDefault()));
+				dlg.setBlockOnOpen(false);
+				dlg.open();
+				configurationDialog = bot.shell(
+						UIText.GitTraceConfigurationDialog_ShellTitle)
+						.activate();
+			}
+		});
+	}
+}
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -2447,6 +2447,36 @@ public class UIText extends NLS {
 	public static String GitBranchSynchronizeWizardPage_deselectAll;
 
 	/** */
+	public static String GitTraceConfigurationDialog_ApplyButton;
+
+	/** */
+	public static String GitTraceConfigurationDialog_DefaultButton;
+
+	/** */
+	public static String GitTraceConfigurationDialog_DialogTitle;
+
+	/** */
+	public static String GitTraceConfigurationDialog_LocationHeader;
+
+	/** */
+	public static String GitTraceConfigurationDialog_MainSwitchNodeText;
+
+	/** */
+	public static String GitTraceConfigurationDialog_OpenInEditorButton;
+
+	/** */
+	public static String GitTraceConfigurationDialog_PlatformSwitchCheckbox;
+
+	/** */
+	public static String GitTraceConfigurationDialog_PlatformTraceDisabledMessage;
+
+	/** */
+	public static String GitTraceConfigurationDialog_ShellTitle;
+
+	/** */
+	public static String GitTraceConfigurationDialog_TraceFileLocationLabel;
+
+	/** */
 	public static String LocalFileRevision_CurrentVersion;
 
 	/** */
