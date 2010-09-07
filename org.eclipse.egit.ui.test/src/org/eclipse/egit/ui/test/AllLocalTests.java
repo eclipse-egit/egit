23135546765ae1049050d73d2203743f6c4c0879
Mathias Kinzler <mathias.kinzler@sap.com>
2010-09-07 13:54:14 +0200

    Supportability: Provide a Trace Configuration Preference Page
    Since we have the infrastructure in place, we can now switch debug
    tracing on and off in a running IDE.
    This can be very helpful in situations where we can't debug.
    Unfortunately, there is no generic UI to do this,
    so this adds a dialog to show the existing trace locations for
    the EGit UI and EGit Core plug-ins and allows to activate them.
    The dialog can not be started from a "normal" UI control, but only
    using CTRL-3 and triggering a dedicated command in order to avoid
    polllution of the Preference UI.
    
    Change-Id: I4681acf85de8d05be6290d88acde5d99453ce89d
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/AllLocalTests.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/AllLocalTests.java
@@ -11,6 +11,7 @@ package org.eclipse.egit.ui.test;
 import org.eclipse.egit.ui.prefpages.configuration.GlobalConfigurationPageTest;
 import org.eclipse.egit.ui.test.history.HistoryViewTest;
 import org.eclipse.egit.ui.test.team.actions.AllTeamActionTests;
+import org.eclipse.egit.ui.test.trace.TraceConfigurationDialogTest;
 import org.eclipse.egit.ui.view.repositories.AllRepositoriesViewTests;
 import org.eclipse.egit.ui.wizards.clone.GitCloneWizardTest;
 import org.eclipse.egit.ui.wizards.share.SharingWizardTest;
@@ -24,7 +25,8 @@ import org.junit.runners.Suite.SuiteClasses;
 		GitCloneWizardTest.class,                   //
 		SharingWizardTest.class,					//
 		AllTeamActionTests.class,                   //
-		HistoryViewTest.class })
+		HistoryViewTest.class,                      //
+		TraceConfigurationDialogTest.class})
 public class AllLocalTests {
 	// empty class, don't need anything here
 }
