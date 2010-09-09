5f626d360f3381d95a3719ffd301308670ecf943
Mathias Kinzler <mathias.kinzler@sap.com>
2010-09-09 15:56:58 +0200

    [repoView] Add a couple UI test for Repository Creation and Properties view
    Change-Id: I17f9720667556e30599412296354567440ef9b64
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/repositories/GitRepositoriesViewRemoteHandlingTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/repositories/GitRepositoriesViewRemoteHandlingTest.java
@@ -106,6 +106,14 @@ public class GitRepositoriesViewRemoteHandlingTest extends
 				assertTrue(data instanceof ErrorNode);
 			}
 		});
+		
+		// test the properties view on remote
+		remotesItem.getNode("test").select();
+		ContextMenuHelper.clickContextMenu(tree, myUtil
+				.getPluginLocalizedValue("OpenPropertiesCommand"));
+		waitInUI();
+		assertEquals("org.eclipse.ui.views.PropertySheet", bot.activeView()
+				.getReference().getId());
 
 		removeRemotesConfig(repositoryFile);
 		refreshAndWait();
