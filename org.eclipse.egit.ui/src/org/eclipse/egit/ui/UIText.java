97137adea124b8b5223053f58b29a24b198cf3ab
Robin Rosenberg <robin.rosenberg@dewire.com>
2010-09-14 00:51:53 +0200

    Trigger repository refresh independently of workspace refresh setting
    Initially we always refreshed when the repository changed, but users
    were not happy, so we tied it to the workspace refresh option. This ment
    Eclipse performed unnecessary work by scanning resources twice. Both
    an infrequent periodical scan and a triggered scan.
    
    Now we only scan for index changes since a ref change by itself does
    not hint that a resource has changed. In addition the default is to
    only scan for repository changes and refresh resources when Eclipse
    has an active workbench window, which means that you can perform Git
    operations outside of Eclipse with no (or at least less) interaction
    between Eclipse and C Git. This used to be a big problem for Windows
    users performing rebase operations, because Eclipse locked files for
    brief periods preventing C Git from updating them.
    
    Change-Id: Id3fef6b884509e1a1a0bbf62fd1ae40ee0f1211e
    Signed-off-by: Robin Rosenberg <robin.rosenberg@dewire.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -2294,6 +2294,15 @@ public class UIText extends NLS {
 	public static String RemoteSelectionCombo_sourceRef;
 
 	/** */
+	public static String RefreshPreferencePage_title;
+
+	/** */
+	public static String RefreshPreferencesPage_RefreshOnlyWhenActive;
+
+	/** */
+	public static String RefreshPreferencesPage_RefreshWhenIndexChange;
+
+	/** */
 	public static String RemoteSelectionCombo_destinationName;
 
 	/** */
