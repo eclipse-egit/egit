16422b5d13ef7e5f378bef4d838b4e15741111bc
Stefan Lay <stefan.lay@sap.com>
2010-09-30 14:28:29 +0200

    Re-add mail header and commit message to git patch
    The mail header and the commit message have not been written to
    the git patch file since commit 7445a9123e5. This is corrected
    with this commit.
    
    Bug: 324362
    Change-Id: I7792b86a4333c4efa812031e9c6c2988a84493c3
    Signed-off-by: Stefan Lay <stefan.lay@sap.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitCreatePatchWizard.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitCreatePatchWizard.java
@@ -21,6 +21,8 @@ import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
@@ -155,6 +157,9 @@ public class GitCreatePatchWizard extends Wizard {
 						}
 
 					}));
+
+					if (isGit)
+						writeGitPatchHeader(sb);
 					try {
 						FileDiff[] diffs = FileDiff.compute(walker, commit);
 						for (FileDiff diff : diffs) {
@@ -194,6 +199,31 @@ public class GitCreatePatchWizard extends Wizard {
 			return false;
 		}
 		return true;
+	}
+
+	private void writeGitPatchHeader(StringBuilder sb) {
+
+		final SimpleDateFormat dtfmt;
+		dtfmt = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z", Locale.US); //$NON-NLS-1$
+		dtfmt.setTimeZone(commit.getAuthorIdent().getTimeZone());
+		sb.append("From").append(" ") //$NON-NLS-1$ //$NON-NLS-2$
+				.append(commit.getId().getName()).append(" ") //$NON-NLS-1$
+				.append(dtfmt.format(Long.valueOf(System.currentTimeMillis())))
+				.append("\n"); //$NON-NLS-1$
+		sb.append("From") //$NON-NLS-1$
+				.append(": ") //$NON-NLS-1$
+				.append(commit.getAuthorIdent().getName())
+				.append(" <").append(commit.getAuthorIdent().getEmailAddress()) //$NON-NLS-1$
+				.append(">\n"); //$NON-NLS-1$
+		sb.append("Date").append(": ") //$NON-NLS-1$ //$NON-NLS-2$
+				.append(dtfmt.format(commit.getAuthorIdent().getWhen()))
+				.append("\n"); //$NON-NLS-1$
+		sb.append("Subject").append(": [PATCH] ") //$NON-NLS-1$ //$NON-NLS-2$
+				.append(commit.getShortMessage());
+
+		String message = commit.getFullMessage().substring(
+				commit.getShortMessage().length());
+		sb.append(message).append("\n\n"); //$NON-NLS-1$
 	}
 
 	private void copyToClipboard(final String content) {
