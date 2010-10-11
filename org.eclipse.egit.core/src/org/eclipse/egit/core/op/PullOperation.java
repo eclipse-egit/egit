d79a76644f09ade3d22e452c7a05768ad7313a9a
Mathias Kinzler <mathias.kinzler@sap.com>
2010-10-11 13:27:49 +0200

    Add "Pull into current branch" action to Team menu
    This uses the new JGit Pull Command API which doesn't offer any
    parameters (yet) and assumes that the upstream configuration for the
    currently checked out branch specifies the correct origin and remote
    branch for merging.
    
    Bug: 303404
    
    Change-Id: I5ad04c7e46e754ae46c4e56400fae185b57aa2d6
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

---
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/op/PullOperation.java
@@ -0,0 +1,90 @@
+/*******************************************************************************
+ * Copyright (c) 2010 SAP AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Mathias Kinzler <mathias.kinzler@sap.com> - initial implementation
+ *******************************************************************************/
+package org.eclipse.egit.core.op;
+
+import org.eclipse.core.resources.IWorkspaceRunnable;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.ISchedulingRule;
+import org.eclipse.egit.core.Activator;
+import org.eclipse.egit.core.CoreText;
+import org.eclipse.egit.core.EclipseGitProgressTransformer;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PullCommand;
+import org.eclipse.jgit.api.PullResult;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Repository;
+
+/**
+ * Wraps the JGit API {@link PullCommand} into an operation
+ */
+public class PullOperation implements IEGitOperation {
+	private final Repository repository;
+
+	private PullResult pullResult;
+
+	private final int timeout;
+
+	/**
+	 * @param repository
+	 *            the repository
+	 * @param timeout
+	 *            in seconds
+	 */
+	public PullOperation(Repository repository, int timeout) {
+		this.timeout = timeout;
+		this.repository = repository;
+	}
+
+	public void execute(IProgressMonitor m) throws CoreException {
+		if (pullResult != null)
+			throw new CoreException(new Status(IStatus.ERROR, Activator
+					.getPluginId(), CoreText.OperationAlreadyExecuted));
+		IProgressMonitor monitor;
+		if (m == null)
+			monitor = new NullProgressMonitor();
+		else
+			monitor = m;
+		IWorkspaceRunnable action = new IWorkspaceRunnable() {
+			public void run(IProgressMonitor mymonitor) throws CoreException {
+				PullCommand pull = new Git(repository).pull();
+				try {
+					pull.setProgressMonitor(new EclipseGitProgressTransformer(
+							mymonitor));
+					pull.setTimeout(timeout);
+					pullResult = pull.call();
+				} catch (GitAPIException e) {
+					IStatus error = new Status(IStatus.ERROR, Activator
+							.getPluginId(), e.getMessage(), e);
+					throw new CoreException(error);
+				}
+			}
+		};
+		// lock workspace to protect working tree changes
+		ResourcesPlugin.getWorkspace().run(action, monitor);
+	}
+
+	/**
+	 * @return the merge result, or <code>null</code> if this has not been
+	 *         executed or if an exception occurred
+	 */
+	public PullResult getResult() {
+		return this.pullResult;
+	}
+
+	public ISchedulingRule getSchedulingRule() {
+		return ResourcesPlugin.getWorkspace().getRoot();
+	}
+}
