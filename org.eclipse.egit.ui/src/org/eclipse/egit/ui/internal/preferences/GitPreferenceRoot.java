54474c72fae864f00206704bcc4c0dc70baf337f
Mathias Kinzler <mathias.kinzler@sap.com>
2010-10-15 00:18:54 +0200

    Consolidate preference pages
    Remove History, Remote Connections, and Repository Change Scanner
    preference pages and move their fields into respective Groups on
    the Git Root Preference page.
    
    Bug: 326800
    Change-Id: I12d7b59829fdc80d576052f51c08e01cbfc1c6be
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/preferences/GitPreferenceRoot.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/preferences/GitPreferenceRoot.java
@@ -12,17 +12,25 @@ package org.eclipse.egit.ui.internal.preferences;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.UIPreferences;
 import org.eclipse.egit.ui.UIText;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.DirectoryFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.IntegerFieldEditor;
 import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
 /** Root preference page for the all of our workspace preferences. */
 public class GitPreferenceRoot extends FieldEditorPreferencePage implements
 		IWorkbenchPreferencePage {
+	private final static int GROUP_SPAN = 3;
+
 	/**
 	 * The default constructor
 	 */
@@ -40,10 +48,14 @@ public class GitPreferenceRoot extends FieldEditorPreferencePage implements
 
 	@Override
 	protected void createFieldEditors() {
+		Composite main = getFieldEditorParent();
+		Group cloningGroup = new Group(main, SWT.SHADOW_ETCHED_IN);
+		cloningGroup.setText(UIText.GitPreferenceRoot_CloningRepoGroupHeader);
+		GridDataFactory.fillDefaults().grab(true, false).span(GROUP_SPAN, 1)
+				.applyTo(cloningGroup);
 		DirectoryFieldEditor editor = new DirectoryFieldEditor(
 				UIPreferences.DEFAULT_REPO_DIR,
-				UIText.GitPreferenceRoot_DefaultRepoFolderLabel,
-				getFieldEditorParent()) {
+				UIText.GitPreferenceRoot_DefaultRepoFolderLabel, cloningGroup) {
 
 			@Override
 			protected void createControl(Composite parent) {
@@ -54,9 +66,63 @@ public class GitPreferenceRoot extends FieldEditorPreferencePage implements
 			}
 
 		};
+		updateMargins(cloningGroup);
 		editor.setEmptyStringAllowed(false);
-		addField(editor);
-		editor.getLabelControl(getFieldEditorParent()).setToolTipText(
+		editor.getLabelControl(cloningGroup).setToolTipText(
 				UIText.GitPreferenceRoot_DefaultRepoFolderTooltip);
+		addField(editor);
+
+		Group historyGroup = new Group(main, SWT.SHADOW_ETCHED_IN);
+		historyGroup.setText(UIText.GitPreferenceRoot_HistoryGroupHeader);
+		GridDataFactory.fillDefaults().grab(true, false).span(GROUP_SPAN, 1)
+				.applyTo(historyGroup);
+
+		addField(new BooleanFieldEditor(
+				UIPreferences.RESOURCEHISTORY_SHOW_COMMENT_WRAP,
+				UIText.ResourceHistory_toggleCommentWrap, historyGroup));
+
+		addField(new BooleanFieldEditor(
+				UIPreferences.RESOURCEHISTORY_SHOW_REV_COMMENT,
+				UIText.ResourceHistory_toggleRevComment, historyGroup));
+		addField(new BooleanFieldEditor(
+				UIPreferences.RESOURCEHISTORY_SHOW_REV_DETAIL,
+				UIText.ResourceHistory_toggleRevDetail, historyGroup));
+		updateMargins(historyGroup);
+
+		Group remoteConnectionsGroup = new Group(main, SWT.SHADOW_ETCHED_IN);
+		GridDataFactory.fillDefaults().grab(true, false).span(GROUP_SPAN, 1)
+				.applyTo(remoteConnectionsGroup);
+		remoteConnectionsGroup
+				.setText(UIText.GitPreferenceRoot_RemoteConnectionsGroupHeader);
+
+		IntegerFieldEditor timeoutEditor = new IntegerFieldEditor(
+				UIPreferences.REMOTE_CONNECTION_TIMEOUT,
+				UIText.RemoteConnectionPreferencePage_TimeoutLabel,
+				remoteConnectionsGroup);
+		timeoutEditor.getLabelControl(remoteConnectionsGroup).setToolTipText(
+				UIText.RemoteConnectionPreferencePage_ZeroValueTooltip);
+		addField(timeoutEditor);
+		updateMargins(remoteConnectionsGroup);
+
+		Group repoChangeScannerGroup = new Group(main, SWT.SHADOW_ETCHED_IN);
+		GridDataFactory.fillDefaults().grab(true, false).span(GROUP_SPAN, 1)
+				.applyTo(repoChangeScannerGroup);
+		repoChangeScannerGroup
+				.setText(UIText.GitPreferenceRoot_RepoChangeScannerGroupHeader);
+		addField(new BooleanFieldEditor(UIPreferences.REFESH_ON_INDEX_CHANGE,
+				UIText.RefreshPreferencesPage_RefreshWhenIndexChange,
+				repoChangeScannerGroup));
+		addField(new BooleanFieldEditor(UIPreferences.REFESH_ONLY_WHEN_ACTIVE,
+				UIText.RefreshPreferencesPage_RefreshOnlyWhenActive,
+				repoChangeScannerGroup));
+		updateMargins(repoChangeScannerGroup);
+	}
+
+	private void updateMargins(Group group) {
+		// make sure there is some room between the group border
+		// and the controls in the group
+		GridLayout layout = (GridLayout) group.getLayout();
+		layout.marginWidth = 5;
+		layout.marginHeight = 5;
 	}
 }
