d20568217f0043678eb0fd2926bcef36666cad3b
Jens Baumgart <jens.baumgart@sap.com>
2010-10-14 17:15:28 +0200

    Remove usages of waitInUI from LocalRepositoryTestCase
    Usages of method waitInUI were removed from test base class
    LocalRepositoryTestCase.
    
    Change-Id: I65487aac91caaa91b60ea17eda665f3343323d41
    Signed-off-by: Jens Baumgart <jens.baumgart@sap.com>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/common/LocalRepositoryTestCase.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/common/LocalRepositoryTestCase.java
@@ -351,16 +351,20 @@ public abstract class LocalRepositoryTestCase extends EGitTestCase {
 		updateRef.update();
 	}
 
-	protected void assertClickOpens(SWTBotTree tree, String menu, String window)
-			throws InterruptedException {
+	protected void assertClickOpens(SWTBotTree tree, String menu, String window) {
 		ContextMenuHelper.clickContextMenu(tree, menu);
 		SWTBotShell shell = bot.shell(window);
 		shell.activate();
-		waitInUI();
 		shell.bot().button(IDialogConstants.CANCEL_LABEL).click();
 		shell.close();
 	}
 
+	/**
+	 *  This method should only be used in exceptional cases.
+	 *  Try to avoid using it e.g. by joining execution jobs
+	 *  instead of waiting a given amount of time {@link TestUtil#joinJobs(Object)}
+	 * @throws InterruptedException
+	 */
 	protected static void waitInUI() throws InterruptedException {
 		Thread.sleep(1000);
 	}
@@ -425,7 +429,6 @@ public abstract class LocalRepositoryTestCase extends EGitTestCase {
 		String message = commitMessage;
 		if (message == null)
 			message = newContent;
-		waitInUI();
 		CommitOperation op = new CommitOperation(commitables,
 				new ArrayList<IFile>(), untracked, TestUtil.TESTAUTHOR,
 				TestUtil.TESTCOMMITTER, message);
