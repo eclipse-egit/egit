2f30b51d4c665a60481566cfa67e216e9920dd6e
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2010-10-14 12:06:13 +0200

    Initial commit of a GIT fetch factory
    PDE Build uses fetch factories for fetching code from SCM to local discs 
    for building. This implements a fetch factory which is able to clone Git
    repos into a local cache location and checkout projects from there.
    
    Bug: 289838
    Change-Id: I76a03f273155e0ad517489f6daa6fe393dcfa865
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>

---
+++ org.eclipse.egit.fetchfactory/src/org/eclipse/egit/fetchfactory/internal/Messages.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2010 AGETO Service GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Gunnar Wagenknecht - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.egit.fetchfactory.internal;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.eclipse.releng.git.fetch.internal.messages";//$NON-NLS-1$
+
+	public static String error_incorrectDirectoryEntry;
+
+	public static String error_incorrectDirectoryEntryKeyValue;
+
+	public static String error_directoryEntryRequiresRepo;
+
+	static {
+		// load message values from bundle file
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+}
