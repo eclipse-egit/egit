c102a2bc1ea35c694bac1998be664f50cb1f39d2
Mathias Kinzler <mathias.kinzler@sap.com>
2010-10-20 02:08:31 +0200

    [historyView] Refactoring fourth part
    Replace "Show all Branches" and "Compare mode" actions with
    BooleanPrefActions and add the corresponding preferences so that
    the toggle state of these buttons survives a restart.
    Decouple the different sub-components (file list, diff viewer)
    of the history page by using the Preferences directly instead 
    of setting the preference values in the components programmatically.
    
    Change-Id: I220bf134494463f9269d69d22c310d5e5a0a1251
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/PluginPreferenceInitializer.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/PluginPreferenceInitializer.java
@@ -38,6 +38,8 @@ public class PluginPreferenceInitializer extends AbstractPreferenceInitializer {
 		store.setDefault(UIPreferences.RESOURCEHISTORY_SHOW_REV_DETAIL, true);
 		store.setDefault(UIPreferences.RESOURCEHISTORY_SHOW_REV_COMMENT, true);
 		store.setDefault(UIPreferences.RESOURCEHISTORY_SHOW_TOOLTIPS, false);
+		store.setDefault(UIPreferences.RESOURCEHISTORY_SHOW_ALL_BRANCHES, false);
+		store.setDefault(UIPreferences.RESOURCEHISTORY_COMPARE_MODE, false);
 
 		store.setDefault(UIPreferences.DECORATOR_RECOMPUTE_ANCESTORS, true);
 		store.setDefault(UIPreferences.DECORATOR_RECURSIVE_LIMIT,
