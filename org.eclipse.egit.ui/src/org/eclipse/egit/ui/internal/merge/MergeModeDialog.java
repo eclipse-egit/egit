67076ebd51c47e98d9121d896253410be002c472
Mathias Kinzler <mathias.kinzler@sap.com>
2010-10-27 13:45:55 +0200

    RFC: Start integration of a merge tool
    At the moment, this is intended as Request for Feedback: this adds a
    "Merge Tool" action to the team menu which is only active if the
    Repository of the selected resources is in state "Merging". When
    selected, a dialog will appear to ask the user whether they want to
    use the workspace (pre-merged) or HEAD (un-merged) version of
    the conflicting files as input for the merge editor.
    The selected files will be shown in the upper part of the editor (only
    such files who have conflicts or whose content differs between the
    workspace and HEAD version). On double-click, the actual merge editor
    will be shown. The user can now edit the left part and save the changed
    content to the workspace. With next/previous they can navigate to the
    next/previous conflict/change (even across file boundaries).
    
    Change-Id: I6d931c24a7122466b566fa61104ea2dbdf72356e
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/merge/MergeModeDialog.java
@@ -0,0 +1,93 @@
+/*******************************************************************************
+ * Copyright (C) 2010, Mathias Kinzler <mathias.kinzler@sap.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.merge;
+
+import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.UIPreferences;
+import org.eclipse.egit.ui.UIText;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * Asks the user whether to use the workspace or HEAD
+ */
+public class MergeModeDialog extends Dialog {
+	private boolean useWs = true;
+
+	Button dontAskAgain;
+
+	/**
+	 * @param parentShell
+	 */
+	public MergeModeDialog(Shell parentShell) {
+		super(parentShell);
+	}
+
+	/**
+	 * @return whether the workspace should be used
+	 */
+	public boolean useWorkspace() {
+		return useWs;
+	}
+
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		Composite main = new Composite(parent, SWT.NONE);
+		main.setLayout(new GridLayout(1, false));
+		final Button useWorkspace = new Button(main, SWT.RADIO);
+		useWorkspace.setText(UIText.MergeModeDialog_MergeMode_1_Label);
+		useWorkspace.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				useWs = useWorkspace.getSelection();
+			}
+		});
+		final Button useHead = new Button(main, SWT.RADIO);
+		useHead.setText(UIText.MergeModeDialog_MergeMode_2_Label);
+		useHead.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				useWs = !useHead.getSelection();
+			}
+		});
+
+		dontAskAgain = new Button(main, SWT.CHECK);
+		dontAskAgain.setText(UIText.MergeModeDialog_DontAskAgainLabel);
+		useWorkspace.setSelection(useWs);
+		return main;
+	}
+
+	@Override
+	protected void okPressed() {
+		boolean save = dontAskAgain.getSelection();
+		super.okPressed();
+		if (save) {
+			int value = 1;
+			if (!useWs)
+				value = 2;
+			IPreferenceStore store = Activator.getDefault()
+					.getPreferenceStore();
+			store.setValue(UIPreferences.MERGE_MODE, value);
+		}
+	}
+
+	@Override
+	protected void configureShell(Shell newShell) {
+		super.configureShell(newShell);
+		newShell.setText(UIText.MergeModeDialog_DialogTitle);
+	}
+}
