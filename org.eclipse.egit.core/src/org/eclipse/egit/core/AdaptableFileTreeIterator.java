bb04be1b0661e2e8528401215984db00b29560a4
Jens Baumgart <jens.baumgart@sap.com>
2010-10-27 14:13:55 +0200

    AdaptableFileTreeIterator: handling of nested / closed projects
    In case of nested projects AdaptableFileTreeIterator finds
    more than one container for a given file. The old implementation just
    took the first found container. This could cause problems if the
    container is not accessible (project closed). The new implementation
    does the following:
    1. Non accessible containers are excluded
    2. The remaining containers checked for the containing projects:
    The container contained in the innermost  project is chosen.
    
    Bug: 328489
    Change-Id: Iac58c50914e9b9fcafba9f0e0fc4d08a33455ccd
    Signed-off-by: Jens Baumgart <jens.baumgart@sap.com>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/AdaptableFileTreeIterator.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/AdaptableFileTreeIterator.java
@@ -11,9 +11,14 @@ package org.eclipse.egit.core;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
 import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.treewalk.AbstractTreeIterator;
@@ -81,9 +86,44 @@ public class AdaptableFileTreeIterator extends FileTreeIterator {
 		final File currentFile = ((FileEntry) current()).getFile();
 		final IContainer[] containers = root
 				.findContainersForLocationURI(currentFile.toURI());
-		if (containers.length > 0)
-			return new ContainerTreeIterator(this, containers[0]);
-		else
+		if (containers.length == 0)
 			return new AdaptableFileTreeIterator(this, currentFile, root);
+		if (containers.length == 1) {
+			if (containers[0].isAccessible())
+				return new ContainerTreeIterator(this, containers[0]);
+		} else {
+			// there is more than one container
+			IContainer container = findContainer(containers);
+			if (container != null)
+				return new ContainerTreeIterator(this, container);
+		}
+		return new AdaptableFileTreeIterator(this, currentFile, root);
+	}
+
+	private IContainer findContainer(IContainer[] containers) {
+		// filter out non accessible containers
+		List<IContainer> validContainers = new ArrayList<IContainer>(containers.length);
+		for (IContainer container : containers) {
+			if (container.isAccessible())
+				validContainers.add(container);
+		}
+		if (validContainers.size()==0)
+			return null;
+		if (validContainers.size()==1)
+			return validContainers.get(0);
+		// there is more than one accessible container
+		// sort containers according to project path containment
+		Collections.sort(validContainers, new Comparator<IContainer>(){
+			public int compare(IContainer o1, IContainer o2) {
+				IPath location1 = o1.getProject().getLocation();
+				IPath location2 = o2.getProject().getLocation();
+				if(location1.equals(location2))
+					return 0;
+				if(location1.isPrefixOf(location2))
+					return -1;
+				return 1;
+			}});
+		// take the container of the innermost project
+		return validContainers.get(validContainers.size()-1);
 	}
 }
