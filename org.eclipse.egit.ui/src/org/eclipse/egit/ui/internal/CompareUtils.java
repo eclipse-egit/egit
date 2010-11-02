b22f7c75199e1dde22a5539fdccb22967fb6fb91
Mathias Kinzler <mathias.kinzler@sap.com>
2010-11-02 11:01:31 +0100

    [historyView] Refactor menu contributions third part
    Adds new actions for "Open in Editor" and "Compare with Ancestor"
    to the context menu of the CommitFileViewer and improves the
    Label provider to show icons instead of text for the file status.
    
    Bug: 323072
    Change-Id: I6b4579a687894c0ad43d68f4fa4f5c6e3222501f
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/CompareUtils.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/CompareUtils.java
@@ -162,8 +162,13 @@ public class CompareUtils {
 	}
 
 	/**
+<<<<<<< HEAD
 	 * Determine the encoding used by Eclipse for the resource which belongs to
 	 * repoPath in the eclipse workspace or null if no resource is found
+=======
+	 * Determine the encoding used by eclipse for the resource which belongs to
+	 * repoPath to in the eclipse workspace or null if no resource is found
+>>>>>>> [historyView] Refactor menu contributions third part
 	 *
 	 * @param db
 	 *            the repository
@@ -191,7 +196,7 @@ public class CompareUtils {
 	 *
 	 * @param resource
 	 *            must be an instance of IEncodedStorage
-	 * @return the encoding used in Eclipse for the resource if found or null
+	 * @return the encoding used in eclipse for the resource if found or null
 	 */
 	public static String getResourceEncoding(IResource resource) {
 		// Get the encoding for the current version. As a matter of
