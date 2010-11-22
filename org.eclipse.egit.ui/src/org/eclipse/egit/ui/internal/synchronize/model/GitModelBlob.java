f30dfffb24be5e3123c9eb03a25c8f552d989a3c
Dariusz Luksza <dariusz@luksza.org>
2010-11-23 00:39:38 +0100

    Fixes compare input for local changes nodes.
    GitModelBlob class was using compare() and hashCode() methods
    implementation from GitModelCommit, therefore in some cases Compare View
    was showing wrong files in comparison.
    
    Bug: 326674
    Change-Id: If42607deca500f3439c512382af0b13b01b90b3d
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/model/GitModelBlob.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/model/GitModelBlob.java
@@ -120,6 +120,30 @@ public class GitModelBlob extends GitModelCommit {
 		compareInput.prepareInput(configuration, monitor);
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this)
+			return true;
+
+		if (obj instanceof GitModelBlob) {
+			GitModelBlob objCommit = (GitModelBlob) obj;
+
+			return objCommit.baseId.equals(baseId)
+					&& objCommit.remoteId.equals(remoteId);
+		}
+
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = baseId.hashCode();
+		if (remoteId != null)
+			result ^= remoteId.hashCode();
+
+		return result;
+	}
+
 	private void createCompareInput() {
 		if (compareInput == null) {
 			ComparisonDataSource baseData = new ComparisonDataSource(
