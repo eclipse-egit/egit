83266003e9c40bb5a88aed3e1a6fcc94ecfc5436
Dariusz Luksza <dariusz@luksza.org>
2010-11-29 22:51:23 +0100

    Fix compare input for local changes nodes.
    GitModelBlob class was using compare() and hashCode() methods
    implementation from GitModelCommit, therefore in some cases Compare View
    was showing wrong files in comparison. It also fixes wrong headers in the
    compare editor.
    
    Bug: 326674
    Change-Id: If42607deca500f3439c512382af0b13b01b90b3d
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/compare/GitCacheCompareInput.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (C) 2010, Dariusz Luksza <dariusz@luksza.org>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.synchronize.compare;
+
+import static org.eclipse.egit.ui.internal.CompareUtils.getFileCachedRevisionTypedElement;
+import static org.eclipse.egit.ui.internal.CompareUtils.getFileRevisionTypedElement;
+
+import org.eclipse.compare.ITypedElement;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.team.ui.mapping.ISynchronizationCompareInput;
+
+/**
+ * Specific implementation of {@link ISynchronizationCompareInput} for showing
+ * cached files
+ */
+public class GitCacheCompareInput extends GitCompareInput {
+
+	/**
+	 * Creates {@link GitCacheCompareInput}
+	 *
+	 * @param repo
+	 *            repository that is connected with this object
+	 * @param ancestorDataSource
+	 *            data that should be use to obtain common ancestor object data
+	 * @param baseDataSource
+	 *            data that should be use to obtain base object data
+	 * @param remoteDataSource
+	 *            data that should be used to obtain remote object data
+	 * @param gitPath
+	 *            repository relative path of object
+	 */
+	public GitCacheCompareInput(Repository repo,
+			ComparisonDataSource ancestorDataSource,
+			ComparisonDataSource baseDataSource,
+			ComparisonDataSource remoteDataSource, String gitPath) {
+		super(repo, ancestorDataSource, baseDataSource, remoteDataSource,
+				gitPath);
+	}
+
+	public ITypedElement getLeft() {
+		return getFileCachedRevisionTypedElement(gitPath, repo);
+	}
+
+	public ITypedElement getRight() {
+		return getFileRevisionTypedElement(gitPath, remoteCommit, repo);
+	}
+
+}
