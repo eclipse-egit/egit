914f93bbe29737f9299c85b85dd7caa1878ffcbe
Mathias Kinzler <mathias.kinzler@sap.com>
2010-12-02 10:59:39 +0100

    Initialize test classes properly
    GitCloneWizardTests fail when running isolated because the clone
    tries to put the repository into <user_home>/<repo_name>; <user_home>
    is the "default-default" for the repository location Preference. The
    fix initializes that preference to point to the test directory created
    during test setup. This initialization already existed in some test
    classes, but was pushed down to LocalRepositoryTestCase; the
    GitCloneWizard tests were made to inherit from that class to take
    advantage of the initialization.
    
    Change-Id: I742a93c67f4f0f56f7eb328bb8712a50d75b3b1e
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/wizards/clone/GitCloneWizardTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/wizards/clone/GitCloneWizardTest.java
@@ -20,8 +20,8 @@ import java.io.IOException;
 
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.egit.ui.common.EGitTestCase;
 import org.eclipse.egit.ui.common.GitImportRepoWizard;
+import org.eclipse.egit.ui.common.LocalRepositoryTestCase;
 import org.eclipse.egit.ui.common.RepoPropertiesPage;
 import org.eclipse.egit.ui.common.RepoRemoteBranchesPage;
 import org.eclipse.egit.ui.common.WorkingCopyPage;
@@ -36,7 +36,7 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
-public class GitCloneWizardTest extends EGitTestCase {
+public class GitCloneWizardTest extends LocalRepositoryTestCase {
 
 	private static final int NUMBER_RANDOM_COMMITS = 100;
 
