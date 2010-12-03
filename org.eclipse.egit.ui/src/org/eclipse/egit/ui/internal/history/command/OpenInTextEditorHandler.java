379c2c6110031ffc6874eff596a0fd0e8204ebd7
Matthias Sohn <matthias.sohn@sap.com>
2010-12-03 16:09:02 +0100

    [findbugs] Use StringBuilder to concatenate Strings
    Change-Id: I9e55038a4388cae4c147681bfa6a8b3d2fffe7ea
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/OpenInTextEditorHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/OpenInTextEditorHandler.java
@@ -110,13 +110,13 @@ public class OpenInTextEditorHandler extends AbstractHistoryCommanndHandler {
 		if (errorOccured)
 			Activator.showError(UIText.GitHistoryPage_openFailed, null);
 		if (ids.size() > 0) {
-			String idList = ""; //$NON-NLS-1$
+			StringBuilder idList = new StringBuilder(""); //$NON-NLS-1$
 			for (ObjectId objectId : ids)
-				idList += objectId.getName() + " "; //$NON-NLS-1$
+				idList.append(objectId.getName()).append(' ');
 			MessageDialog.openError(getPart(event).getSite().getShell(),
 					UIText.GitHistoryPage_fileNotFound, NLS.bind(
 							UIText.GitHistoryPage_notContainedInCommits,
-							gitPath, idList));
+							gitPath, idList.toString()));
 		}
 		return null;
 	}
