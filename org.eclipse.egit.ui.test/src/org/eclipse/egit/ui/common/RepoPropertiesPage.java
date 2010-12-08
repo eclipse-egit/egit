e3b524a1fb4ba945794507f565db3cad2773400b
Jens Baumgart <jens.baumgart@sap.com>
2010-12-08 17:13:48 +0100

    Implement UI Tests for http authentication
    Implements a test for the clone wizard using http authentication.
    Depends on JGit Change I13bfc4c6c47e27d8f97d3e9752347d6d23e553d4
    
    Change-Id: Ibe56d8e7352a121431e02ebeceab158267945843
    Signed-off-by: Jens Baumgart <jens.baumgart@sap.com>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/common/RepoPropertiesPage.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/common/RepoPropertiesPage.java
@@ -15,6 +15,7 @@ import static org.eclipse.swtbot.swt.finder.SWTBotAssert.assertText;
 import static org.junit.Assert.assertEquals;
 
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotCheckBox;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotText;
 
 public class RepoPropertiesPage {
@@ -23,6 +24,21 @@ public class RepoPropertiesPage {
 
 	public void setURI(String uri) {
 		uri().setText(uri);
+	}
+
+	public void setUser(String user) {
+		user().setText(user);
+	}
+
+	public void setPassword(String password) {
+		password().setText(password);
+	}
+
+	public void setStoreInSecureStore(boolean store) {
+		if (store)
+			storeCheckBox().select();
+		else
+			storeCheckBox().deselect();
 	}
 
 	public void appendToURI(String toAppend) {
@@ -65,9 +81,26 @@ public class RepoPropertiesPage {
 		return bot.textWithLabel("URI:");
 	}
 
+	private SWTBotText user() {
+		return bot.textWithLabel("User:");
+	}
+
+	private SWTBotText password() {
+		return bot.textWithLabel("Password:");
+	}
+
+	private SWTBotCheckBox storeCheckBox() {
+		return bot.checkBoxWithLabel("Store in Secure Store");
+	}
+
 	public RepoRemoteBranchesPage nextToRemoteBranches(String string) {
 		setURI(string);
+		return nextToRemoteBranches();
+	}
+
+	public RepoRemoteBranchesPage nextToRemoteBranches() {
 		bot.button("Next >").click();
 		return new RepoRemoteBranchesPage();
 	}
+
 }
