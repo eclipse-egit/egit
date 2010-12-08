526569fa0dad9f43cc17e33eaee3c58ab1917648
Matthias Sohn <matthias.sohn@sap.com>
2010-12-08 22:59:18 +0100

    Adapt egit to jgit change Idb176736
    JGit change Idb176736fa0dc97af372f1d652a94ecc72fb457c
    changed handling of WorkingTreeOptions and filemode
    breaking the egit build.
    
    Change-Id: Iee407b7bfb3c65df5c3562b3bdabb5637f04b481
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>
    Signed-off-by: Chris Aniszczyk <caniszczyk@gmail.com>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/op/AddToIndexOperation.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/op/AddToIndexOperation.java
@@ -102,9 +102,8 @@ public class AddToIndexOperation implements IEGitOperation {
 		if (command == null) {
 			Repository repo = map.getRepository();
 			Git git = new Git(repo);
-			AdaptableFileTreeIterator it =
-				new AdaptableFileTreeIterator(repo.getWorkTree(),
-						resource.getWorkspace().getRoot());
+			AdaptableFileTreeIterator it = new AdaptableFileTreeIterator(repo,
+					resource.getWorkspace().getRoot());
 			command = git.add().setWorkingTreeIterator(it);
 			addCommands.put(map, command);
 		}
