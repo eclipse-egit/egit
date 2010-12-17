3626d29e1af33a8b4e1f0652e4b5d249573e6c4d
Mathias Kinzler <mathias.kinzler@sap.com>
2010-12-17 11:18:29 +0100

    Checkout: display checkout conflicts in a user-friendly way
    Also, if some files could not be deleted during checkout, these files
    are displayed to the user so that they have a chance to retry the
    deletion.
    
    Depends on JGit change
    
    http://egit.eclipse.org/r/#change,2139
    
    which exposes the list of conflicts and fixes some issues when
    checking out a commit or remote tracking branch.
    
    Change-Id: Ic8b491b268dffdc3928a4d8175ed2be0de3fafc6
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/branch/BranchResultDialog.java
@@ -0,0 +1,117 @@
+/*******************************************************************************
+ * Copyright (c) 2010 SAP AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Mathias Kinzler (SAP AG) - initial implementation
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.branch;
+
+import java.io.File;
+import java.util.List;
+
+import org.eclipse.egit.ui.UIText;
+import org.eclipse.egit.ui.internal.dialogs.NonDeletedFilesDialog;
+import org.eclipse.egit.ui.internal.dialogs.NonDeletedFilesTree;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jgit.api.CheckoutResult;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.PlatformUI;
+
+/**
+ * Display the result of a Branch operation.
+ */
+public class BranchResultDialog extends MessageDialog {
+	private static final Image INFO = PlatformUI.getWorkbench()
+			.getSharedImages().getImage(ISharedImages.IMG_OBJS_INFO_TSK);
+
+	private final CheckoutResult result;
+
+	private final Repository repository;
+
+	TreeViewer tv;
+
+	/**
+	 * @param result
+	 *            the result to show
+	 * @param repository
+	 * @param target
+	 *            the target (branch name or commit id)
+	 */
+	public static void show(final CheckoutResult result,
+			final Repository repository, final String target) {
+		if (result.getStatus() == CheckoutResult.Status.CONFLICTS)
+			PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+				public void run() {
+					Shell shell = PlatformUI.getWorkbench()
+							.getActiveWorkbenchWindow().getShell();
+					new BranchResultDialog(shell, repository, result, target)
+							.open();
+				}
+			});
+		else if (result.getStatus() == CheckoutResult.Status.NONDELETED) {
+			// double-check if the files are still there
+			boolean show = false;
+			List<String> pathList = result.getUndeletedList();
+			for (String path : pathList) {
+				if (new File(repository.getWorkTree(), path).exists()) {
+					show = true;
+					break;
+				}
+			}
+			if (!show)
+				return;
+			PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+				public void run() {
+					Shell shell = PlatformUI.getWorkbench()
+							.getActiveWorkbenchWindow().getShell();
+					new NonDeletedFilesDialog(shell, repository, result
+							.getUndeletedList()).open();
+				}
+			});
+		}
+	}
+
+	/**
+	 * @param shell
+	 * @param repository
+	 * @param result
+	 * @param target
+	 */
+	private BranchResultDialog(Shell shell, Repository repository,
+			CheckoutResult result, String target) {
+		super(shell, UIText.BranchResultDialog_CheckoutConflictsTitle, INFO,
+				NLS.bind(UIText.BranchResultDialog_CheckoutConflictsMessage,
+						target), MessageDialog.INFORMATION,
+				new String[] { IDialogConstants.OK_LABEL }, 0);
+		setShellStyle(getShellStyle() | SWT.SHELL_TRIM);
+		this.repository = repository;
+		this.result = result;
+	}
+
+	@Override
+	protected Control createCustomArea(Composite parent) {
+		Composite main = new Composite(parent, SWT.NONE);
+		main.setLayout(new GridLayout(1, false));
+		GridDataFactory.fillDefaults().indent(0, 0).grab(true, true).applyTo(
+				main);
+		new NonDeletedFilesTree(main, repository, this.result.getConflictList());
+		applyDialogFont(main);
+
+		return main;
+	}
+}
---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/FileTreeContentProvider.java
@@ -0,0 +1,317 @@
+/*******************************************************************************
+ * Copyright (C) 2010, Dariusz Luksza <dariusz@luksza.org>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Mathias Kinzler (SAP AG) - improve UI responsiveness
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.dialogs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.egit.core.project.RepositoryMapping;
+import org.eclipse.egit.ui.UIText;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE.SharedImages;
+
+/**
+ * Builds the nodes for a file tree
+ *
+ */
+public class FileTreeContentProvider implements ITreeContentProvider {
+	private final Repository repository;
+
+	private Node[] rootNodes;
+
+	private List<String> input;
+
+	private Mode mode = Mode.REPO_RELATIVE_PATHS;
+
+	private List<IPath> projectPaths;
+
+	private Image folderImage = PlatformUI.getWorkbench().getSharedImages()
+			.getImage(ISharedImages.IMG_OBJ_FOLDER);
+
+	private Image fileImage = PlatformUI.getWorkbench().getSharedImages()
+			.getImage(ISharedImages.IMG_OBJ_FILE);
+
+	private Image projectImage = PlatformUI.getWorkbench().getSharedImages()
+			.getImage(SharedImages.IMG_OBJ_PROJECT);
+
+	/**
+	 * Describe how to show the paths
+	 *
+	 */
+	public enum Mode {
+		/**
+		 * The paths are shown relative to the repository (default)
+		 */
+		REPO_RELATIVE_PATHS,
+		/**
+		 * The paths are shown as full file system paths
+		 */
+		FULL_PATHS,
+		/**
+		 * The paths are shown as resource paths (relative to the workspace)
+		 */
+		RESOURCE_PATHS;
+	}
+
+	/**
+	 * The node
+	 */
+	public abstract class Node {
+		private final String myName;
+
+		private final Node myParent;
+
+		/**
+		 * Child list
+		 */
+		protected final List<Node> myChildren = new ArrayList<Node>();
+
+		Node(Node parent, String name) {
+			myParent = parent;
+			myName = name;
+			if (parent != null)
+				parent.addChild(this);
+		}
+
+		/**
+		 * @return a boolean
+		 */
+		public abstract boolean hasChildren();
+
+		/**
+		 * @return an image
+		 */
+		public abstract Image getImage();
+
+		/**
+		 * @return the name
+		 */
+		public String getName() {
+			return myName;
+		}
+
+		/**
+		 * @return the parent
+		 */
+		public Node getParent() {
+			return myParent;
+		}
+
+		private void addChild(Node child) {
+			myChildren.add(child);
+		}
+
+		/**
+		 * @return the children or null
+		 */
+		public List<Node> getChildren() {
+			if (myChildren != null)
+				return myChildren;
+			return null;
+		}
+
+		@Override
+		public String toString() {
+			return myName;
+		}
+	}
+
+	private class FolderNode extends Node {
+		FolderNode(Node parent, String name) {
+			super(parent, name);
+		}
+
+		@Override
+		public boolean hasChildren() {
+			return true;
+		}
+
+		@Override
+		public Image getImage() {
+			if (mode == Mode.RESOURCE_PATHS
+					&& getParent() instanceof VirtualNode)
+				return projectImage;
+			return folderImage;
+		}
+
+	}
+
+	private class NoResourceNode extends Node {
+		NoResourceNode(Node parent, String name) {
+			super(parent, name);
+		}
+
+		@Override
+		public boolean hasChildren() {
+			return true;
+		}
+
+		@Override
+		public Image getImage() {
+			return folderImage;
+		}
+
+	}
+
+	class FileNode extends Node {
+		FileNode(Node parent, String name) {
+			super(parent, name);
+		}
+
+		@Override
+		public boolean hasChildren() {
+			return false;
+		}
+
+		@Override
+		public Image getImage() {
+			return fileImage;
+		}
+	}
+
+	private class VirtualNode extends FolderNode {
+		VirtualNode() {
+			super(null, ""); //$NON-NLS-1$
+		}
+
+		@Override
+		public boolean hasChildren() {
+			return !myChildren.isEmpty();
+		}
+	}
+
+	/**
+	 * @param repository
+	 */
+	public FileTreeContentProvider(Repository repository) {
+		this.repository = repository;
+	}
+
+	/**
+	 * @param mode
+	 */
+	public void setMode(Mode mode) {
+		this.mode = mode;
+	}
+
+	public Object[] getChildren(Object parent) {
+		return ((Node) parent).getChildren().toArray();
+	}
+
+	public Object getParent(Object child) {
+		return ((Node) child).getParent();
+	}
+
+	public boolean hasChildren(Object parent) {
+		return ((Node) parent).hasChildren();
+	}
+
+	public Object[] getElements(Object arg0) {
+		return rootNodes;
+	}
+
+	public void dispose() {
+		// nothing to dispose
+	}
+
+	@SuppressWarnings("unchecked")
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		rootNodes = new Node[0];
+
+		input = (List<String>) newInput;
+		if (input == null)
+			return;
+
+		VirtualNode virtualNode = new VirtualNode();
+		Node parentNode = virtualNode;
+		NoResourceNode noResourceNode = null;
+
+		IPath repositoryRoot = new Path(repository.getWorkTree()
+				.getAbsolutePath());
+
+		for (String pathString : input) {
+			IPath path;
+			if (mode == Mode.FULL_PATHS)
+				path = repositoryRoot.append(pathString);
+			else if (mode == Mode.RESOURCE_PATHS)
+				path = resolveResourcePath(repositoryRoot.append(pathString));
+			else
+				path = new Path(pathString);
+			if (path == null) {
+				path = repositoryRoot.append(pathString);
+				if (noResourceNode == null) {
+					noResourceNode = new NoResourceNode(
+							virtualNode,
+							UIText.FileTreeContentProvider_NonWorkspaceResourcesNode);
+				}
+				parentNode = noResourceNode;
+			} else
+				parentNode = virtualNode;
+			for (int i = 0; i < path.segmentCount(); i++) {
+				String segment = path.segment(i);
+				Node foundNode = null;
+				for (Node node : parentNode.getChildren()) {
+					if (node.getName().equals(segment)) {
+						foundNode = node;
+						break;
+					}
+				}
+				if (foundNode == null) {
+					if (i < path.segmentCount() - 1)
+						parentNode = new FolderNode(parentNode, segment);
+					else
+						new FileNode(parentNode, segment);
+				} else {
+					parentNode = foundNode;
+				}
+			}
+		}
+
+		rootNodes = virtualNode.getChildren().toArray(
+				new Node[virtualNode.getChildren().size()]);
+	}
+
+	private IPath resolveResourcePath(IPath fullPath) {
+		for (IPath projectPath : getProjectPaths()) {
+			if (projectPath.isPrefixOf(fullPath)) {
+				return fullPath
+						.removeFirstSegments(projectPath.segmentCount() - 1);
+			}
+		}
+		return null;
+	}
+
+	private List<IPath> getProjectPaths() {
+		if (projectPaths == null) {
+			projectPaths = new ArrayList<IPath>();
+			for (IProject project : ResourcesPlugin.getWorkspace().getRoot()
+					.getProjects()) {
+				RepositoryMapping mapping = RepositoryMapping
+						.getMapping(project);
+				if (mapping != null
+						&& mapping.getRepository().equals(repository)) {
+					projectPaths.add(project.getLocation());
+				}
+			}
+		}
+		return projectPaths;
+	}
+}
