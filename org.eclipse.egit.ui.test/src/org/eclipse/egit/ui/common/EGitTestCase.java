6ce2a41a35ea0502709f39fdfc9fd7dc86c369e4
Jens Baumgart <jens.baumgart@sap.com>
2010-12-17 17:45:03 +0100

    Speed up UI tests: do not wait for non existing welcome page
    The welcome page only needs to be closed once.
    Currently each test class waits for time out of finding the welcome
    page.
    
    Change-Id: Ieb7f46d4dcece1abe2aa4e12986264bbf808bd7d
    Signed-off-by: Jens Baumgart <jens.baumgart@sap.com>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/common/EGitTestCase.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/common/EGitTestCase.java
@@ -28,11 +28,15 @@ public abstract class EGitTestCase {
 
 	protected static final SWTWorkbenchBot bot = new SWTWorkbenchBot();
 	protected static final TestUtil util = new TestUtil();
+	private static volatile boolean welcomePageClosed = false;
 
 	@BeforeClass
 	public static void closeWelcomePage() {
+		if (welcomePageClosed)
+			return;
 		try {
 			bot.viewByTitle("Welcome").close();
+			welcomePageClosed = true;
 		} catch (WidgetNotFoundException e) {
 			// somebody else probably closed it, lets not feel bad about it.
 		}
