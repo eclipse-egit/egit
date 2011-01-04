f805036e34b013625beab95f360ba1799eb34e03
Robin Rosenberg <robin.rosenberg@dewire.com>
2011-01-04 20:56:41 +0100

    Implement a merge comand that works from the History page
    Any commit can be merged, but tHere is room for improvement
    as it does not attempt to figure out whether a commit can be
    identified via a ref.
    
    Change-Id: I2fa2971e25b04595cb37e23d72c17ae7b124e09a
    Signed-off-by: Robin Rosenberg <robin.rosenberg@dewire.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -404,6 +404,9 @@ public class UIText extends NLS {
 	public static String GitHistoryPage_revertMenuItem;
 
 	/** */
+	public static String GitHistoryPage_mergeMenuItem;
+
+	/** */
 	public static String GitHistoryPage_SetAsBaselineMenuLabel;
 
 	/** */
