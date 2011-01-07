6173ab62de3ca60bde60cc1e0dad4797db98a887
Thorsten Kamann <thorsten@kamann.info>
2011-01-07 09:23:11 +0100

    Created an extension point to extend the commit message.
    To integrate Mylyn's commit templates an extension point
    is needed. This extension point provides an interface you
    must implement. During the preparation of the commit
    dialogs the extensions using this extension points will be
    requested for a message to add to the dialog.
    Here is a sample configuration:
    <extension
        point="org.eclipse.egit.ui.commitMessageProvider">
        <commitMessageProvider
            class="org.eclipse.egit.mylyn.CommitMessageProvider">
        </commitMessageProvider>
    </extension>
    The mylyn integration is done. You will find them in the
    org.eclipse.egit.mylyn project. The implementation of the
    ICommitMessagProvider has the ability to check if there are a
    project-specific configuration for mylyn's commit message templates.
    Change-Id: I3e0b8cd23be957c8c6a8199ce6818282fb24dc68

---
+++ org.eclipse.egit.mylyn/src/org/eclipse/egilt/mylyn/EgitMylynPlugin.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (C) 2010, Thorsten Kamann <thorsten@kamann.info>
+ *
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egilt.mylyn;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Plugin;
+import org.eclipse.core.runtime.Status;
+import org.osgi.framework.BundleContext;
+
+/**
+ * Activator class for the <code>org.eclipse.egit.mylyn</code>-PlugIn.
+ * Here you can find common methods you can use in all classes of this plugin.
+ */
+public class EgitMylynPlugin extends Plugin{
+	private static EgitMylynPlugin plugin;
+
+	/**
+	 * @return the singleton {@link EgitMylynPlugin}
+	 */
+	public static EgitMylynPlugin getDefault() {
+		if (plugin == null){
+			plugin = new EgitMylynPlugin();
+		}
+		return plugin;
+	}
+	
+	/**
+	 * @return the name of this plugin
+	 */
+	public static String getPluginId() {
+		return getDefault().getBundle().getSymbolicName();
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		super.start(bundleContext);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		super.stop(bundleContext);
+	}
+
+	/**
+	 * Utility method to log errors in the Egit plugin.
+	 * @param message User comprehensible message
+	 * @param thr The exception through which we noticed the error
+	 */
+	public static void logError(final String message, final Throwable thr) {
+		getDefault().getLog().log(
+				new Status(IStatus.ERROR, "egit.mylyn", 0, message, thr)); //$NON-NLS-1$
+	}
+}
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitDialog.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitDialog.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -30,13 +31,16 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.egit.core.Activator;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.core.AdaptableFileTreeIterator;
 import org.eclipse.egit.core.GitProvider;
 import org.eclipse.egit.core.internal.storage.GitFileHistoryProvider;
 import org.eclipse.egit.core.op.AddToIndexOperation;
 import org.eclipse.egit.core.project.RepositoryMapping;
+import org.eclipse.egit.ui.ICommitMessageProvider;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.UIUtils;
 import org.eclipse.egit.ui.UIUtils.IPreviousValueProposalHandler;
@@ -100,6 +104,11 @@ import org.eclipse.ui.model.WorkbenchLabelProvider;
  * selected portion of the tree are shown.
  */
 public class CommitDialog extends Dialog {
+
+	/**
+	 * Constant for the extension point for the commit message provider
+	 */
+	private static final String COMMIT_MESSAGE_PROVIDER_ID = "org.eclipse.egit.ui.commitMessageProvider"; //$NON-NLS-1$
 
 	static class CommitLabelProvider extends WorkbenchLabelProvider implements
 			ITableLabelProvider {
@@ -202,7 +211,7 @@ public class CommitDialog extends Dialog {
 		int minHeight = commitText.getTextWidget().getLineHeight() * 3;
 		commitText.setLayoutData(GridDataFactory.fillDefaults().span(2, 1).grab(true, true)
 				.hint(size).minSize(size.x, minHeight).align(SWT.FILL, SWT.FILL).create());
-		commitText.setText(commitMessage);
+		commitText.setText(calculateCommitMessage());
 
 		// allow to commit with ctrl-enter
 		commitText.getTextWidget().addKeyListener(new KeyAdapter() {
@@ -432,6 +441,39 @@ public class CommitDialog extends Dialog {
 		applyDialogFont(container);
 		container.pack();
 		return container;
+	}
+
+	/**
+	 * @return the calculated commit message
+	 */
+	protected String calculateCommitMessage() {
+		String calculatedCommitMessage = commitMessage;
+
+		Set<IResource> resources = new HashSet<IResource>();
+		for (CommitItem item : items) {
+			IResource resource = item.file.getProject();
+			resources.add(resource);
+		}
+
+		IConfigurationElement[] config = Platform.getExtensionRegistry()
+				.getConfigurationElementsFor(COMMIT_MESSAGE_PROVIDER_ID);
+		for (IConfigurationElement e : config) {
+			Object provider;
+			try {
+				provider = e.createExecutableExtension("class");//$NON-NLS-1$
+				if (provider instanceof ICommitMessageProvider) {
+					if (!"".equals(calculatedCommitMessage)){ //$NON-NLS-1$
+						calculatedCommitMessage += Text.DELIMITER;
+					}
+					calculatedCommitMessage += ((ICommitMessageProvider) provider).getMessage(resources.toArray(new IResource[0])) ;
+				}else{
+					Activator.logWarning(UIText.CommitDialog_WrongTypeOfCommitMessageProvider, null);
+				}
+			} catch (CoreException coreException) {
+				Activator.error(coreException.getLocalizedMessage(), coreException);
+			}
+		}
+		return calculatedCommitMessage;
 	}
 
 	private void saveOriginalChangeId() {
