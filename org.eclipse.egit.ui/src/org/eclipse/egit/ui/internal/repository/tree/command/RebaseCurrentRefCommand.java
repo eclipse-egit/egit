cd2d40671b8b56b2c0236264a307b88ed13e74a1
Robin Rosenberg <robin.rosenberg@dewire.com>
2011-01-07 23:36:54 +0100

    Refactor much of RebaseCommand implementation into a utility
    Change-Id: Iba6db867d39a863eea2a2e09bf7f8c1a1c129c41
    Signed-off-by: Robin Rosenberg <robin.rosenberg@dewire.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/RebaseCurrentRefCommand.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/RebaseCurrentRefCommand.java
@@ -15,31 +15,18 @@ import java.io.IOException;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.expressions.IEvaluationContext;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
-import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.jobs.JobChangeAdapter;
-import org.eclipse.egit.core.op.RebaseOperation;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.internal.dialogs.RebaseTargetSelectionDialog;
-import org.eclipse.egit.ui.internal.rebase.RebaseResultDialog;
+import org.eclipse.egit.ui.internal.rebase.RebaseHelper;
 import org.eclipse.egit.ui.internal.repository.tree.RefNode;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
 import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jgit.api.RebaseCommand.Operation;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.RepositoryState;
 import org.eclipse.osgi.util.NLS;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.ISources;
-import org.eclipse.ui.PlatformUI;
 
 /**
  * Implements "Rebase" to the currently checked out {@link Ref}
@@ -72,51 +59,7 @@ public class RebaseCurrentRefCommand extends
 		String jobname = NLS.bind(
 				UIText.RebaseCurrentRefCommand_RebasingCurrentJobName, ref
 						.getName());
-		final RebaseOperation rebase = new RebaseOperation(repository, ref);
-		Job job = new Job(jobname) {
-			@Override
-			protected IStatus run(IProgressMonitor monitor) {
-				try {
-					rebase.execute(monitor);
-				} catch (final CoreException e) {
-					try {
-						new RebaseOperation(repository, Operation.ABORT)
-								.execute(monitor);
-					} catch (CoreException e1) {
-						return e1.getStatus();
-					}
-					return e.getStatus();
-				}
-				return Status.OK_STATUS;
-			}
-		};
-		job.setUser(true);
-		job.setRule(rebase.getSchedulingRule());
-		job.addJobChangeListener(new JobChangeAdapter() {
-			@Override
-			public void done(IJobChangeEvent cevent) {
-				IStatus result = cevent.getJob().getResult();
-				if (result.getSeverity() == IStatus.CANCEL) {
-					Display.getDefault().asyncExec(new Runnable() {
-						public void run() {
-							// don't use getShell(event) here since
-							// the active shell has changed since the
-							// execution has been triggered.
-							Shell shell = PlatformUI.getWorkbench()
-									.getActiveWorkbenchWindow().getShell();
-							MessageDialog
-									.openInformation(
-											shell,
-											UIText.RebaseCurrentRefCommand_RebaseCanceledTitle,
-											UIText.RebaseCurrentRefCommand_RebaseCanceledMessage);
-						}
-					});
-				} else if (result.isOK()) {
-					RebaseResultDialog.show(rebase.getResult(), repository);
-				}
-			}
-		});
-		job.schedule();
+		RebaseHelper.runRebaseJob(repository, jobname, ref);
 		return null;
 	}
 
