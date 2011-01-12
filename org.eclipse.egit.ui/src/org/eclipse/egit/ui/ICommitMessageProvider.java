71f2aa68135a2a67a043a23fc640161164d6b4a8
Thorsten Kamann <thorsten@kamann.info>
2011-01-12 21:03:57 +0100

    Created an extension point to extend the commit message.
    To integrate Mylyn's commit templates an extension point
    is needed. This extension point provides an interface you
    must implement. During the preparation of the commit
    dialogs the extensions using this extension points will be
    requested for a message to add to the dialog.
    
    Here is a sample configuration:
    <extension
        point="org.eclipse.egit.ui.commitMessageProvider">
        <commitMessageProvider
            class="org.eclipse.egit.mylyn.CommitMessageProvider">
        </commitMessageProvider>
    </extension>
    
    The mylyn integration is done. You will find them in the
    org.eclipse.egit.mylyn project. The implementation of the
    ICommitMessagProvider has the ability to check if there are a
    project-specific configuration for mylyn's commit message templates.
    
    Change-Id: I3e0b8cd23be957c8c6a8199ce6818282fb24dc68

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/ICommitMessageProvider.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (C) 2010, Thorsten Kamann <thorsten@kamann.info>
+ *
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.egit.ui.internal.dialogs.CommitDialog;
+
+/**
+ * This interface must be implemented to be a commit message provider. A commit
+ * message provider provides the complete or a fragment of a commit message.
+ * This message will be added to the text field in the {@link CommitDialog}. <br/>
+ * Primary use is the integration of mylyns commit templates.
+ *
+ * @see CommitDialog
+ * @since 0.10
+ */
+public interface ICommitMessageProvider {
+
+	/**
+	 * @param resources
+	 * @return the message the CommitDialogs should use as default message
+	 */
+	public String getMessage(IResource[] resources);
+}
