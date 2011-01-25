5dbe5b18eaa16b5d6289d2cb73e4592b59d0abce
Mathias Kinzler <mathias.kinzler@sap.com>
2011-01-25 14:47:08 +0100

    Tree compare: make left hand side editable if it is in workspace
    If the left-hand side of the compare editor opened for a file in the
    tree compare view refers to the workspace version of a file, it should
    be editable.
    Bug: 334998
    Change-Id: Idc7369d9fb0a943acd1fbe7006141f0be2da1662
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CompareTreeView.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CompareTreeView.java
@@ -11,6 +11,7 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.dialogs;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
@@ -26,6 +27,7 @@ import org.eclipse.compare.ITypedElement;
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
@@ -36,6 +38,7 @@ import org.eclipse.egit.ui.UIIcons;
 import org.eclipse.egit.ui.UIPreferences;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.internal.CompareUtils;
+import org.eclipse.egit.ui.internal.EditableRevision;
 import org.eclipse.egit.ui.internal.FileRevisionTypedElement;
 import org.eclipse.egit.ui.internal.GitCompareFileRevisionEditorInput;
 import org.eclipse.egit.ui.internal.LocalFileRevision;
@@ -290,8 +293,35 @@ public class CompareTreeView extends ViewPart {
 			tv.setExpandedState(selected, !tv.getExpandedState(selected));
 			return;
 		} else if (selected instanceof IFile) {
-			IFile res = (IFile) selected;
-			left = new FileRevisionTypedElement(new LocalFileRevision(res));
+			final IFile res = (IFile) selected;
+			left = new EditableRevision(new LocalFileRevision(res)) {
+				@Override
+				public void setContent(final byte[] newContent) {
+					try {
+						PlatformUI.getWorkbench().getProgressService().run(
+								false, false, new IRunnableWithProgress() {
+									public void run(IProgressMonitor myMonitor)
+											throws InvocationTargetException,
+											InterruptedException {
+										try {
+											res.setContents(
+													new ByteArrayInputStream(
+															newContent), false,
+													true, myMonitor);
+										} catch (CoreException e) {
+											throw new InvocationTargetException(
+													e);
+										}
+									}
+								});
+					} catch (InvocationTargetException e) {
+						Activator.handleError(e.getTargetException()
+								.getMessage(), e.getTargetException(), true);
+					} catch (InterruptedException e) {
+						// ignore here
+					}
+				}
+			};
 			GitFileRevision rightRevision = rightVersionMap.get(new Path(
 					repositoryMapping.getRepoRelativePath(res)));
 			if (rightRevision == null)
