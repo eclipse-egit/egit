36561c1b31d7a0649108515ac359618a9c6fea4f
Mathias Kinzler <mathias.kinzler@sap.com>
2011-01-25 17:06:16 +0100

    Add a "Compare with -> Commit..." action
    When the user selects this, a dialog is shown to select a commit to
    compare the resource with. Depending on whether the resource is a file
    or a container, either the Git Tree Compare View or a Compare Editor
    is opened for the selected commit.
    
    
    Change-Id: I45bea9ea8e700543ac55643f28d13f4d0404afa2
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitSelectionDialog.java
@@ -0,0 +1,209 @@
+/*******************************************************************************
+ * Copyright (C) 2011, Mathias Kinzler <mathias.kinzler@sap.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.history;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map.Entry;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.UIPreferences;
+import org.eclipse.jface.dialogs.IMessageProvider;
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.IOpenListener;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.OpenEvent;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevFlag;
+import org.eclipse.jgit.revwalk.RevSort;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+
+/**
+ * Allows to select a single commit TODO localize
+ */
+public class CommitSelectionDialog extends TitleAreaDialog {
+	private static final int BATCH_SIZE = 256;
+
+	private final Repository repository;
+
+	private CommitGraphTable table;
+
+	private SWTCommitList allCommits;
+
+	private RevFlag highlightFlag;
+
+	private ObjectId commitId;
+
+	/**
+	 * @param parentShell
+	 * @param repository
+	 */
+	public CommitSelectionDialog(Shell parentShell, Repository repository) {
+		super(parentShell);
+		setShellStyle(getShellStyle() | SWT.SHELL_TRIM);
+		this.repository = repository;
+	}
+
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		Composite main = new Composite(parent, SWT.NONE);
+		main.setLayout(new GridLayout(1, false));
+		GridDataFactory.fillDefaults().grab(true, true).applyTo(main);
+		table = new CommitGraphTable(main);
+		table.getTableView().addSelectionChangedListener(
+				new ISelectionChangedListener() {
+					public void selectionChanged(SelectionChangedEvent event) {
+						commitId = null;
+						IStructuredSelection sel = (IStructuredSelection) event
+								.getSelection();
+						if (sel.size() == 1)
+							commitId = ((SWTCommit) sel.getFirstElement())
+									.getId();
+						getButton(OK).setEnabled(commitId != null);
+					}
+				});
+		table.getTableView().addOpenListener(new IOpenListener() {
+			public void open(OpenEvent event) {
+				buttonPressed(OK);
+			}
+		});
+		// allow for some room here
+		GridDataFactory.fillDefaults().grab(true, true).minSize(SWT.DEFAULT,
+				400).applyTo(table.getControl());
+		allCommits = new SWTCommitList(getShell().getDisplay());
+		return main;
+	}
+
+	@Override
+	protected void configureShell(Shell newShell) {
+		super.configureShell(newShell);
+		newShell.setText("Select a commit"); //$NON-NLS-1$
+	}
+
+	@Override
+	public void create() {
+		super.create();
+		getButton(OK).setEnabled(false);
+		try {
+			PlatformUI.getWorkbench().getProgressService().run(true, true,
+					new IRunnableWithProgress() {
+						public void run(IProgressMonitor monitor)
+								throws InvocationTargetException,
+								InterruptedException {
+							try {
+								monitor.beginTask("Building commit list", //$NON-NLS-1$
+										IProgressMonitor.UNKNOWN);
+								SWTWalk currentWalk = new SWTWalk(repository);
+								currentWalk
+										.sort(RevSort.COMMIT_TIME_DESC, true);
+								currentWalk.sort(RevSort.BOUNDARY, true);
+								highlightFlag = currentWalk
+										.newFlag("highlight"); //$NON-NLS-1$
+								allCommits.source(currentWalk);
+
+								try {
+
+									if (Activator
+											.getDefault()
+											.getPreferenceStore()
+											.getBoolean(
+													UIPreferences.RESOURCEHISTORY_SHOW_ALL_BRANCHES)) {
+										markStartAllRefs(currentWalk,
+												Constants.R_HEADS);
+										markStartAllRefs(currentWalk,
+												Constants.R_REMOTES);
+									} else
+										currentWalk
+												.markStart(currentWalk
+														.parseCommit(repository
+																.resolve(Constants.HEAD)));
+									for (;;) {
+										final int oldsz = allCommits.size();
+										allCommits.fillTo(oldsz + BATCH_SIZE
+												- 1);
+
+										if (monitor.isCanceled()
+												|| oldsz == allCommits.size())
+											break;
+										String taskName = NLS
+												.bind(
+														"Found {0} commits", Integer.valueOf(allCommits.size())); //$NON-NLS-1$
+										monitor.setTaskName(taskName);
+
+									}
+								} catch (IOException e) {
+									throw new InvocationTargetException(e);
+								}
+								getShell().getDisplay().asyncExec(
+										new Runnable() {
+											public void run() {
+												updateUi();
+											}
+										});
+								if (monitor.isCanceled())
+									throw new InterruptedException();
+							} finally {
+								monitor.done();
+							}
+						}
+					});
+		} catch (InvocationTargetException e) {
+			setErrorMessage(e.getCause().getMessage());
+		} catch (InterruptedException e) {
+			setMessage(
+					"The commit list may be incomplete", IMessageProvider.WARNING); //$NON-NLS-1$
+		}
+	}
+
+	/**
+	 * @return the commit id
+	 */
+	public ObjectId getCommitId() {
+		return commitId;
+	}
+
+	private void updateUi() {
+		setTitle(NLS.bind("{0} commits in Repository {1}", Integer //$NON-NLS-1$
+				.valueOf(allCommits.size()), repository.getDirectory()
+				.toString()));
+		setMessage("Please select a commit from the list"); //$NON-NLS-1$
+		table.setInput(highlightFlag, allCommits, allCommits
+				.toArray(new SWTCommit[allCommits.size()]), null);
+	}
+
+	private void markStartAllRefs(RevWalk currentWalk, String prefix)
+			throws IOException, MissingObjectException,
+			IncorrectObjectTypeException {
+		for (Entry<String, Ref> refEntry : repository.getRefDatabase().getRefs(
+				prefix).entrySet()) {
+			Ref ref = refEntry.getValue();
+			if (ref.isSymbolic())
+				continue;
+			currentWalk.markStart(currentWalk.parseCommit(ref.getObjectId()));
+		}
+	}
+
+}
