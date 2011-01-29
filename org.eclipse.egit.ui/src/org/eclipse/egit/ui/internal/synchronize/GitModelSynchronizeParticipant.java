a8a72aca070c42146c61f66efea6bbecddbd9c7b
Dariusz Luksza <dariusz@luksza.org>
2011-01-29 13:39:26 +0100

    [sync] Show compare editor for non-workspace files
    Bug: 335560
    Change-Id: Id9a61b875d5302ed93587b4865f2d5dd78959181
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/GitModelSynchronizeParticipant.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/GitModelSynchronizeParticipant.java
@@ -24,6 +24,7 @@ import org.eclipse.egit.core.synchronize.dto.GitSynchronizeDataSet;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.internal.synchronize.compare.ComparisonDataSource;
 import org.eclipse.egit.ui.internal.synchronize.compare.GitCompareInput;
+import org.eclipse.egit.ui.internal.synchronize.model.GitModelBlob;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.treewalk.TreeWalk;
@@ -109,6 +110,11 @@ public class GitModelSynchronizeParticipant extends ModelSynchronizeParticipant 
 		}
 
 		return providers.toArray(new ModelProvider[providers.size()]);
+	}
+
+	@Override
+	public boolean hasCompareInputFor(Object object) {
+		return object instanceof GitModelBlob;
 	}
 
 	@Override
