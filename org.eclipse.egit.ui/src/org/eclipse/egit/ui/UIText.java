8201f78bed1882428b3f08b24790cb9b8776a4b0
Mathias Kinzler <mathias.kinzler@sap.com>
2011-02-02 14:46:35 +0100

    Simplify Fetch and Push: configure "Pull" when creating branch
    This allows the user to configure "Pull" during branch creation. The
    default configuration as in autosetupmerge and autosetuprebase is
    taken into account and the user can override this.
    
    The Branch Creation wizard page has become a bit more complex due to
    this, but the user should be able to work just fine with the defaults.
    
    There is now an information message telling the user that they
    normally want to base their branches on remote tracking branches.
    
    The remote tracking branches now appear first in the list of available
    branches to also support users in this.
    
    Note that the underlying JGit API operation does not yet support the
    autosetup* flags yet.
    
    The PullResultDialog now is able to show a Rebase result, too.
    
    Change-Id: If3cb758b8c04039219cbb090c2385ca5315ad2df
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>
    Signed-off-by: Chris Aniszczyk <caniszczyk@gmail.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -1313,6 +1313,12 @@ public class UIText extends NLS {
 	public static String PullResultDialog_NothingToFetchFromLocal;
 
 	/** */
+	public static String PullResultDialog_RebaseStatusLabel;
+
+	/** */
+	public static String PullResultDialog_RebaseStoppedMessage;
+
+	/** */
 	public static String PushAction_wrongURIDescription;
 
 	/** */
@@ -1724,7 +1730,37 @@ public class UIText extends NLS {
 	public static String CreateBranchPage_CreatingBranchMessage;
 
 	/** */
+	public static String CreateBranchPage_LocalBranchWarningText;
+
+	/** */
+	public static String CreateBranchPage_LocalBranchWarningTooltip;
+
+	/** */
+	public static String CreateBranchPage_MergeRadioButton;
+
+	/** */
 	public static String CreateBranchPage_MissingSourceMessage;
+
+	/** */
+	public static String CreateBranchPage_NoneRadioButton;
+
+	/** */
+	public static String CreateBranchPage_PullMergeTooltip;
+
+	/** */
+	public static String CreateBranchPage_PullNoneTooltip;
+
+	/** */
+	public static String CreateBranchPage_PullRebaseTooltip;
+
+	/** */
+	public static String CreateBranchPage_PullStrategyGroupHeader;
+
+	/** */
+	public static String CreateBranchPage_PullStrategyTooltip;
+
+	/** */
+	public static String CreateBranchPage_RebaseRadioButton;
 
 	/** */
 	public static String CreateBranchPage_SourceBranchLabel;
