a5275c8fc6de6e65f7c13e92ceeb955d0e7ce715
Matthias Sohn <matthias.sohn@sap.com>
2011-02-06 00:00:08 +0100

    Fix simple fetch and push on repository node
    Simple fetch and push failed with a ClassCastException if called from
    repository node in the Repository View. Also show message dialog if no
    configuration could be found.
    
    Change-Id: Ia16b077d629fdc3f44c41d693327a357be0d28f1
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/PushConfiguredRemoteCommand.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/PushConfiguredRemoteCommand.java
@@ -1,5 +1,6 @@
 /*******************************************************************************
  * Copyright (c) 2010 SAP AG.
+ * Copyright (c) 2011, Matthias Sohn <matthias.sohn@sap.com>
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -16,9 +17,14 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.UIPreferences;
+import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.internal.push.PushOperationUI;
+import org.eclipse.egit.ui.internal.push.SimpleConfigurePushDialog;
 import org.eclipse.egit.ui.internal.repository.tree.PushNode;
 import org.eclipse.egit.ui.internal.repository.tree.RemoteNode;
+import org.eclipse.egit.ui.internal.repository.tree.RepositoryNode;
+import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jgit.transport.RemoteConfig;
 
 /**
@@ -27,17 +33,28 @@ import org.eclipse.jgit.transport.RemoteConfig;
 public class PushConfiguredRemoteCommand extends
 		RepositoriesViewCommandHandler<PushNode> {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		PushNode node = getSelectedNodes(event).get(0);
-		RemoteNode remote = (RemoteNode) node.getParent();
-		RemoteConfig config;
-		try {
-			config = new RemoteConfig(node.getRepository().getConfig(), remote
-					.getObject());
-		} catch (URISyntaxException e) {
-			throw new ExecutionException(e.getMessage());
+		RepositoryTreeNode node = getSelectedNodes(event).get(0);
+		RemoteConfig config = null;
+		if (node instanceof PushNode) {
+			try {
+				RemoteNode remote = (RemoteNode) node.getParent();
+				config = new RemoteConfig(node.getRepository().getConfig(),
+						remote.getObject());
+			} catch (URISyntaxException e) {
+				throw new ExecutionException(e.getMessage());
+			}
+		} else if (node instanceof RepositoryNode) {
+			config = SimpleConfigurePushDialog.getConfiguredRemote(node
+					.getRepository());
 		}
-		int timeout = Activator.getDefault().getPreferenceStore().getInt(
-				UIPreferences.REMOTE_CONNECTION_TIMEOUT);
+		if (config == null) {
+			MessageDialog.openInformation(getShell(event),
+					UIText.SimplePushActionHandler_NothingToPushDialogTitle,
+					UIText.SimplePushActionHandler_NothingToPushDialogMessage);
+			return null;
+		}
+		int timeout = Activator.getDefault().getPreferenceStore()
+				.getInt(UIPreferences.REMOTE_CONNECTION_TIMEOUT);
 		new PushOperationUI(node.getRepository(), config, timeout, false)
 				.start();
 		return null;
