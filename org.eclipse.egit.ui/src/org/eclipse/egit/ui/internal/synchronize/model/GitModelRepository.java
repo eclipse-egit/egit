0c87ab90936718b73b0d0bb0ed1bea1f0ca1c3bc
Dariusz Luksza <dariusz@luksza.org>
2011-02-08 22:28:54 +0100

    [sync] Don't show empty staged and working tree nodes
    Staged and working tree nodes were show even when there were no changes
    inside. With this change such situation shouldn't occur any more.
    
    Change-Id: I26010a1df1abbcb051d2916761aac53718d12624
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/model/GitModelRepository.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/model/GitModelRepository.java
@@ -150,8 +150,14 @@ public class GitModelRepository extends GitModelObject {
 			}
 
 			if (includeLocal) {
-				result.add(new GitModelCache(this, srcCommit));
-				result.add(new GitModelWorkingTree(this, srcCommit));
+				GitModelCache gitModelCache = new GitModelCache(this, srcCommit);
+				if (gitModelCache.getChildren().length > 0)
+					result.add(gitModelCache);
+
+				GitModelWorkingTree gitModelWorkingTree = new GitModelWorkingTree(
+						this, srcCommit);
+				if (gitModelWorkingTree.getChildren().length > 0)
+					result.add(gitModelWorkingTree);
 			}
 		} catch (IOException e) {
 			Activator.logError(e.getMessage(), e);
