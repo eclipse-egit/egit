f99341427455023650e4875ea772fee400fc5e20
Ilya Ivanov <ilya.ivanov@intland.com>
2011-02-12 21:26:56 +0300

    File revision actions
    Added object contribution actions those can be used by third party
    plugins. Actions list:
    Compare With HEAD
    Compare With Parent
    Compare With Workspace
    Open HEAD Version
    Open Selected Version
    Open Workspace Version
    
    Bug: 336949
    Change-Id: I6b056293f7be1183a17078f96bfc1510d60cd452
    Signed-off-by: Ilya Ivanov <ilya.ivanov@intland.com>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CompareSelectedWithParentAction.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Ilya Ivanov (Intland) - implementation
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.actions;
+
+
+import java.io.IOException;
+
+import org.eclipse.compare.CompareUI;
+import org.eclipse.compare.ITypedElement;
+import org.eclipse.egit.core.GitFileRevisionReference;
+import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.internal.CompareUtils;
+import org.eclipse.egit.ui.internal.GitCompareFileRevisionEditorInput;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+/**
+ * Compares selected file revision with it's parent revision.
+ */
+public class CompareSelectedWithParentAction implements IObjectActionDelegate {
+
+	private GitFileRevisionReference fileRevision;
+
+	public void run(IAction action) {
+		if (fileRevision == null)
+			return;
+
+		Repository repository = fileRevision.getRepository();
+
+		ITypedElement selectedFileRevision = CompareUtils.getFileRevisionTypedElement(
+				fileRevision.getPath(), fileRevision.getRevCommit(), repository);
+
+		try {
+			RevCommit commit = new RevWalk(repository).parseCommit(fileRevision.getRevCommit().getParent(0));
+
+			ITypedElement parentFileRevision = CompareUtils.getFileRevisionTypedElement(
+					fileRevision.getPath(), commit, repository);
+
+			GitCompareFileRevisionEditorInput in = new GitCompareFileRevisionEditorInput(
+					selectedFileRevision, parentFileRevision, null);
+
+			CompareUI.openCompareEditor(in);
+		} catch (IOException e) {
+			Activator.handleError(e.getMessage(), e, true);
+			return;
+		}
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+		if (selection instanceof IStructuredSelection) {
+			IStructuredSelection ss = (IStructuredSelection) selection;
+			Object element = ss.getFirstElement();
+			if (element instanceof GitFileRevisionReference) {
+				this.fileRevision = (GitFileRevisionReference) element;
+				action.setEnabled(true);
+				return;
+			}
+		}
+		fileRevision = null;
+		action.setEnabled(false);
+	}
+
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		//
+	}
+}
---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/OpenFileAtHeadRevisionAction.java
@@ -0,0 +1,106 @@
+/*******************************************************************************
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Ilya Ivanov (Intland) - implementation
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.actions;
+
+
+import java.io.IOException;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.egit.core.GitFileRevisionReference;
+import org.eclipse.egit.core.internal.storage.CommitFileRevision;
+import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.internal.EgitUiEditorUtils;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+
+/**
+ * Compares selected file revision with it's parent revision.
+ */
+public class OpenFileAtHeadRevisionAction implements IObjectActionDelegate {
+
+	private CommitFileRevision commitFileRevision = null;
+
+	public void run(IAction action) {
+		if (commitFileRevision == null)
+			return;
+
+		IWorkbenchWindow window = getWorkbenchWindow();
+		if (window == null)
+			return;
+
+		IWorkbenchPage page = window.getActivePage();
+		if (page == null)
+			return;
+
+		try {
+			EgitUiEditorUtils.openEditor(page, commitFileRevision, new NullProgressMonitor());
+		} catch (Exception e) {
+			Activator.handleError(e.getMessage(), e, true);
+		}
+	}
+
+	private IWorkbenchWindow getWorkbenchWindow() {
+		final IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		return window;
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+		if (selection instanceof IStructuredSelection) {
+			IStructuredSelection ss = (IStructuredSelection) selection;
+			Object element = ss.getFirstElement();
+			if (element instanceof GitFileRevisionReference) {
+				prepareCommitFileRevision((GitFileRevisionReference) element);
+
+				if (commitFileRevision != null) {
+					action.setEnabled(true);
+					return;
+				}
+			}
+		}
+		commitFileRevision = null;
+		action.setEnabled(false);
+	}
+
+	private void prepareCommitFileRevision(GitFileRevisionReference selection) {
+		Repository repository = selection.getRepository();
+
+		try {
+			Ref head = repository.getRef(Constants.HEAD);
+			RevWalk rw = new RevWalk(repository);
+			RevCommit commit = rw.parseCommit(head.getObjectId());
+
+			this.commitFileRevision = new CommitFileRevision(repository, commit, selection.getPath());
+
+			// check if file exists in this change set
+			// if no - will throw exception
+			commitFileRevision.getStorage(new NullProgressMonitor());
+		} catch (CoreException e) {
+			this.commitFileRevision = null;
+		} catch (IOException e) {
+			Activator.handleError(e.getMessage(), e, true);
+		}
+	}
+
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		//
+	}
+}
