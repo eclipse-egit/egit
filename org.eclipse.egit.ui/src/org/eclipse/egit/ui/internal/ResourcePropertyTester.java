c3c787a67d2392c6221ee7adf4980a7a5f5f199d
Mathias Kinzler <mathias.kinzler@sap.com>
2011-02-14 14:14:07 +0100

    Hide "Team > Remote" sub-menu if project is not shared with Git
    Bug: 336716
    Change-Id: I6d4b1b6bca125629c189870e42a6a3e522e50d48
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/ResourcePropertyTester.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2011 SAP AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Mathias Kinzler (SAP AG) - initial implementation
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.egit.core.project.RepositoryMapping;
+
+/**
+ * Resource-based property tester
+ */
+public class ResourcePropertyTester extends PropertyTester {
+
+	public boolean test(Object receiver, String property, Object[] args,
+			Object expectedValue) {
+		if (!(receiver instanceof IResource))
+			return false;
+		IResource res = (IResource) receiver;
+		if ("isShared".equals(property)) { //$NON-NLS-1$
+			RepositoryMapping mapping = RepositoryMapping.getMapping(res
+					.getProject());
+			return mapping != null && mapping.getRepository() != null;
+		}
+		return false;
+	}
+
+}
