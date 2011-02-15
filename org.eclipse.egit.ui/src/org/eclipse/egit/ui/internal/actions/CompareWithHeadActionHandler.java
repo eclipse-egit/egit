4914052baadc2b615fa048cb678e939a7bef8958
Mathias Kinzler <mathias.kinzler@sap.com>
2011-02-15 09:37:03 +0100

    Enable "Compare With > HEAD Revision" for folders and projects
    This actions should be available for folders and projects just like
    the other "Compare With" actions.
    
    Change-Id: Icf03a9464d58897958a4e8ec6ffa8f964361611b
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CompareWithHeadActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CompareWithHeadActionHandler.java
@@ -20,62 +20,76 @@ import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.internal.CompareUtils;
 import org.eclipse.egit.ui.internal.GitCompareFileRevisionEditorInput;
+import org.eclipse.egit.ui.internal.dialogs.CompareTreeView;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.team.ui.synchronize.SaveableCompareEditorInput;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 
 /**
  * Compares the working tree content of a file with the version of the file in
  * the HEAD commit.
  */
 public class CompareWithHeadActionHandler extends RepositoryActionHandler {
-
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		final IResource resource = getSelectedResources(event)[0];
-		final RepositoryMapping mapping = RepositoryMapping.getMapping(resource
-				.getProject());
-		final Repository repository = mapping.getRepository();
-		final String gitPath = mapping.getRepoRelativePath(resource);
+		final Repository repository = getRepository(true, event);
+		// assert all resources map to the same repository
+		if (repository == null)
+			return null;
+		final IResource[] resources = getSelectedResources(event);
 
-		final IFile baseFile = (IFile) resource;
-		final ITypedElement base = SaveableCompareEditorInput
-				.createFileElement(baseFile);
+		if (resources.length == 1 && resources[0] instanceof IFile) {
+			final IFile baseFile = (IFile) resources[0];
+			final String gitPath = RepositoryMapping.getMapping(
+					baseFile.getProject()).getRepoRelativePath(baseFile);
+			final ITypedElement base = SaveableCompareEditorInput
+					.createFileElement(baseFile);
 
-		ITypedElement next;
-		try {
-			Ref head = repository.getRef(Constants.HEAD);
-			RevWalk rw = new RevWalk(repository);
-			RevCommit commit = rw.parseCommit(head.getObjectId());
+			ITypedElement next;
+			try {
+				Ref head = repository.getRef(Constants.HEAD);
+				RevWalk rw = new RevWalk(repository);
+				RevCommit commit = rw.parseCommit(head.getObjectId());
 
-			next = CompareUtils.getFileRevisionTypedElement(gitPath, commit,
-					repository);
-		} catch (IOException e) {
-			Activator.handleError(e.getMessage(), e, true);
+				next = CompareUtils.getFileRevisionTypedElement(gitPath,
+						commit, repository);
+			} catch (IOException e) {
+				Activator.handleError(e.getMessage(), e, true);
+				return null;
+			}
+
+			final GitCompareFileRevisionEditorInput in = new GitCompareFileRevisionEditorInput(
+					base, next, null);
+			CompareUI.openCompareEditor(in);
+			return null;
+
+		} else {
+			CompareTreeView view;
+			try {
+				view = (CompareTreeView) PlatformUI.getWorkbench()
+						.getActiveWorkbenchWindow().getActivePage().showView(
+								CompareTreeView.ID);
+				try {
+					view.setInput(resources, repository.resolve(Constants.HEAD)
+							.name());
+				} catch (IOException e) {
+					Activator.handleError(e.getMessage(), e, true);
+					return null;
+				}
+			} catch (PartInitException e) {
+				Activator.handleError(e.getMessage(), e, true);
+				return null;
+			}
 			return null;
 		}
-
-		final GitCompareFileRevisionEditorInput in = new GitCompareFileRevisionEditorInput(
-				base, next, null);
-		CompareUI.openCompareEditor(in);
-		return null;
 	}
 
 	@Override
 	public boolean isEnabled() {
-		final IResource[] selectedResources = getSelectedResources();
-		if (selectedResources.length != 1)
-			return false;
-
-		final IResource resource = selectedResources[0];
-		if (!(resource instanceof IFile)) {
-			return false;
-		}
-		final RepositoryMapping mapping = RepositoryMapping.getMapping(resource
-				.getProject());
-		return mapping != null;
+		return getRepository() != null;
 	}
-
 }
