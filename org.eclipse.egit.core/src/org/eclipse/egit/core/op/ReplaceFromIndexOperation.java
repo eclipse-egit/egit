b06f73c0b6af036479ba084d77bf4c4203860e7f
Ilya Ivanov <ilya.ivanov@intland.com>
2011-02-16 16:48:39 +0300

    Added 'Replace With HEAD' menu option
    Bug: 326638
    Change-Id: I859d5c0ff40aadacd017c39fcc03437571f00216
    Signed-off-by: Ilya Ivanov <ilya.ivanov@intland.com>

---
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/op/ReplaceFromIndexOperation.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (C) 2010, Jens Baumgart <jens.baumgart@sap.com>
+ * Copyright (C) 2010, Roland Grunberg <rgrunber@redhat.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Code extracted from org.eclipse.egit.ui.internal.actions.DiscardChangesAction
+ * and reworked.
+ *******************************************************************************/
+package org.eclipse.egit.core.op;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.egit.core.project.RepositoryMapping;
+import org.eclipse.jgit.dircache.DirCache;
+import org.eclipse.jgit.dircache.DirCacheCheckout;
+import org.eclipse.jgit.dircache.DirCacheEntry;
+import org.eclipse.jgit.lib.Repository;
+
+/**
+ * The operation discards changes on a set of resources. In case of a folder
+ * resource all file resources in the sub tree are processed.
+ */
+public class ReplaceFromIndexOperation extends DiscardChangesOperation {
+
+	/**
+	 * @param files
+	 */
+	public ReplaceFromIndexOperation(IResource[] files) {
+		super(files);
+	}
+
+	protected void discardChange(IResource res, Repository repository)
+			throws IOException {
+		String resRelPath = RepositoryMapping.getMapping(res)
+				.getRepoRelativePath(res);
+		DirCache dc = repository.lockDirCache();
+		try {
+			DirCacheEntry entry = dc.getEntry(resRelPath);
+			if (entry != null) {
+				File file = new File(res.getLocationURI());
+				DirCacheCheckout.checkoutEntry(repository, file, entry);
+			}
+		} finally {
+			dc.unlock();
+		}
+	}
+
+}
