7e03d77197f650e33e6b4b079a8acc188ba884fb
Dariusz Luksza <dariusz@luksza.org>
2011-02-25 17:59:47 +0100

    [sync] Show branches with '/' in sync-dialog
    Bug: 327600
    Change-Id: I7f19947fb2545a32526e2d92dc3a8b6273a6a0e5
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/SynchronizeWithActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/SynchronizeWithActionHandler.java
@@ -8,12 +8,18 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.actions;
 
+import static org.eclipse.jgit.lib.Constants.R_HEADS;
+import static org.eclipse.jgit.lib.Constants.R_REFS;
+import static org.eclipse.jgit.lib.Constants.R_REMOTES;
+import static org.eclipse.jgit.lib.Constants.R_TAGS;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -36,6 +42,13 @@ import org.eclipse.jgit.transport.RemoteConfig;
  * An action that launch synchronization with selected repository
  */
 public class SynchronizeWithActionHandler extends RepositoryActionHandler {
+
+	private static final Pattern PATTERN = Pattern
+			.compile("^("  //$NON-NLS-1$
+					+ R_HEADS + ")|("  //$NON-NLS-1$
+					+ R_REMOTES + ")|("  //$NON-NLS-1$
+					+ R_TAGS + ")|("  //$NON-NLS-1$
+					+ R_REFS + ")"); //$NON-NLS-1$
 
 	@Override
 	public boolean isEnabled() {
@@ -106,11 +119,12 @@ public class SynchronizeWithActionHandler extends RepositoryActionHandler {
 
 	private SyncRepoEntity getRemoteSyncRepo(RefDatabase refDatabase,
 			RemoteConfig rc) throws IOException {
-		SyncRepoEntity syncRepoEnt = new SyncRepoEntity(rc.getName());
-		Collection<Ref> remoteRefs = getRemoteRef(refDatabase, rc.getName());
+		String name = rc.getName();
+		SyncRepoEntity syncRepoEnt = new SyncRepoEntity(name);
+		Collection<Ref> remoteRefs = getRemoteRef(refDatabase, name);
 
 		for (Ref ref : remoteRefs)
-			syncRepoEnt.addRef(createSyncRepoEntity(ref.getName()));
+			syncRepoEnt.addRef(createSyncRepoEntity(name, ref.getName()));
 
 		return syncRepoEnt;
 	}
@@ -127,7 +141,15 @@ public class SynchronizeWithActionHandler extends RepositoryActionHandler {
 	}
 
 	private SyncRefEntity createSyncRepoEntity(String ref) {
-		String name = ref.substring(ref.lastIndexOf('/') + 1);
+		return createSyncRepoEntity("", ref); //$NON-NLS-1$
+	}
+
+	private SyncRefEntity createSyncRepoEntity(String repoName, String ref) {
+		String name = PATTERN.matcher(ref).replaceFirst(""); //$NON-NLS-1$
+
+		if (name.startsWith(repoName + "/")) //$NON-NLS-1$
+			name = name.substring(repoName.length() + 1);
+
 		return new SyncRefEntity(name, ref);
 	}
 
