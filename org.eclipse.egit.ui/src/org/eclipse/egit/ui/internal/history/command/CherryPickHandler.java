1c7fbe0c06ca2eb20b36978876c80a835717b1ac
Philipp Thun <philipp.thun@sap.com>
2011-03-16 15:28:02 +0100

    Use CherryPickResult in CherryPickHandler
    Change "Introduce CherryPickResult" (http://egit.eclipse.org/r/2646)
    breaks the current CherryPickCommand API. This change adapts to the
    new API (i.e. CherryPickResult instead of RevCommit returned by
    CherryPickCommand.call()).
    
    Change-Id: I119f3ed16d6e7aa85f2f1d610640844cef14cea1
    Signed-off-by: Philipp Thun <philipp.thun@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/CherryPickHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/CherryPickHandler.java
@@ -16,7 +16,7 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jgit.api.CherryPickCommand;
+import org.eclipse.jgit.api.CherryPickResult;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
@@ -30,12 +30,12 @@ public class CherryPickHandler extends AbstractHistoryCommandHandler {
 		RevCommit newHead;
 		Repository repo = getRepository(event);
 
-		CherryPickCommand cherryPick;
+		CherryPickResult cherryPickResult;
 		Git git = new Git(repo);
 		try {
-			cherryPick = git.cherryPick().include(commit.getId());
-			newHead = cherryPick.call();
-			if (newHead != null && cherryPick.getCherryPickedRefs().isEmpty())
+			cherryPickResult = git.cherryPick().include(commit.getId()).call();
+			newHead = cherryPickResult.getNewHead();
+			if (newHead != null && cherryPickResult.getCherryPickedRefs().isEmpty())
 				MessageDialog.openWarning(getPart(event).getSite().getShell(),
 						UIText.CherryPickHandler_NoCherryPickPerformedTitle,
 						UIText.CherryPickHandler_NoCherryPickPerformedMessage);
