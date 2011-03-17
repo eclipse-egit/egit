4bd059aaf1c6461adb2f836080026ec5200e5961
Mathias Kinzler <mathias.kinzler@sap.com>
2011-03-17 14:44:46 +0100

    Remove a "restricted access" warning
    Change-Id: Id33da0c823352f35a4da43fcc2497ca6f86698c2
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/synchronize/SynchronizeViewTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/synchronize/SynchronizeViewTest.java
@@ -61,7 +61,7 @@ import org.eclipse.swtbot.swt.finder.widgets.SWTBotRadio;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotToolbarDropDownButton;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
-import org.eclipse.team.internal.ui.synchronize.RefreshParticipantJob;
+import org.eclipse.team.ui.synchronize.ISynchronizeManager;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -534,8 +534,9 @@ public class SynchronizeViewTest extends LocalRepositoryTestCase {
 			implements ICondition {
 		private boolean state = false;
 
-		@SuppressWarnings("restriction") public void done(IJobChangeEvent event) {
-			if (event.getJob() instanceof RefreshParticipantJob)
+		public void done(IJobChangeEvent event) {
+			if (event.getJob().belongsTo(
+					ISynchronizeManager.FAMILY_SYNCHRONIZE_OPERATION))
 				state = true;
 		}
 
