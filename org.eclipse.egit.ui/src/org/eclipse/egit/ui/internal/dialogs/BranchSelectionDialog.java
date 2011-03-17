bcce975d5e5ebc71015bb78507d4bdcbcdebe750
Dariusz Luksza <dariusz@luksza.org>
2011-03-17 15:02:51 +0100

    Close branch selection dialog when new branch was checkouted
    Branch selection dialog should be automatically closed when user
    selects 'checkout' check box in 'Create New Branch' wizard.
    
    Bug: 338660
    Change-Id: I65d456a0dd9a915819c35c12925d3fdd9a8aba43
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/BranchSelectionDialog.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/BranchSelectionDialog.java
@@ -148,6 +148,10 @@ public class BranchSelectionDialog extends AbstractBranchSelectionDialog {
 					try {
 						branchTree.refresh();
 						markRef(Constants.R_HEADS + newRefName);
+						if (repo.getBranch().equals(newRefName))
+							// close branch selection dialog when new branch was
+							// already checkouted from new branch wizard
+							BranchSelectionDialog.this.okPressed();
 					} catch (Throwable e1) {
 						reportError(
 								e1,
