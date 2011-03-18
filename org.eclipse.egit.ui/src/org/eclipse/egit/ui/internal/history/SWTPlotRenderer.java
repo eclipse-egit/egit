e289f5f52507c1f7c359639cef0efd067a309b4c
Mathias Kinzler <mathias.kinzler@sap.com>
2011-03-18 10:35:28 +0100

    Remove UTF-8 character from code and silence compiler warnings
    Change-Id: I79e8ede24a250010edc5d84d0fd301db995a2532
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/SWTPlotRenderer.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/SWTPlotRenderer.java
@@ -70,9 +70,9 @@ class SWTPlotRenderer extends AbstractPlotRenderer<SWTLane, Color> {
 	}
 
 	@SuppressWarnings("unchecked")
-	void paint(final Event event, Ref headRef) {
+	void paint(final Event event, Ref actHeadRef) {
 		g = event.gc;
-		this.headRef = headRef;
+		this.headRef = actHeadRef;
 		cellX = event.x;
 		cellY = event.y;
 		cellFG = g.getForeground();
@@ -150,7 +150,7 @@ class SWTPlotRenderer extends AbstractPlotRenderer<SWTLane, Color> {
 		}
 
 		if (txt.length() > 12)
-			txt = txt.substring(0,11) + "\u2026"; // ellipsis "…" (in UTF-8) //$NON-NLS-1$
+			txt = txt.substring(0,11) + "\u2026"; // ellipsis "..." (in UTF-8) //$NON-NLS-1$
 
 		Point textsz = g.stringExtent(txt);
 		int arc = textsz.y/2;
