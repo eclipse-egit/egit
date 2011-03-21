f3805267401ac2ab3a308e52f4bd5e2cb73dabec
Dariusz Luksza <dariusz@luksza.org>
2011-03-21 18:37:59 +0100

    [sync] Fix copy-paste issue in GitChangeSetSorter
    Change-Id: Ic44ba7b14925cfb7f0ad73017551db38591e4b87
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/mapping/GitChangeSetSorter.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/mapping/GitChangeSetSorter.java
@@ -51,8 +51,8 @@ public class GitChangeSetSorter extends ViewerSorter {
 		if (e2 instanceof GitModelCache)
 			return 2;
 
-		if ((e1 instanceof GitModelTree && e1 instanceof GitModelTree) ||
-				(e1 instanceof GitModelBlob && e1 instanceof GitModelBlob))
+		if ((e1 instanceof GitModelTree && e2 instanceof GitModelTree) ||
+				(e1 instanceof GitModelBlob && e2 instanceof GitModelBlob))
 			return super.compare(viewer, e1, e2);
 
 		if (e1 instanceof GitModelTree && e2 instanceof GitModelCommit)
