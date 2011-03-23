d1077cb65755a7acd0c4a1946042faf6739694c9
Mathias Kinzler <mathias.kinzler@sap.com>
2011-03-23 19:02:49 +0100

    [repoView] "SimpleConfigure" dialogs in RepositoriesView second part
    The "SimpleConfigure" dialogs should also be used when creating a new
    Remote in the Git Repositories View.
    
    This is a follow-up on bug 338384, but not directly mentioned there.
    
    Change-Id: I8d1934ee33da6122759f221e248ff5c0931592ab
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/ConfigureRemoteCommand.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/ConfigureRemoteCommand.java
@@ -10,23 +10,14 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.repository.tree.command;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.Set;
-
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.egit.ui.Activator;
-import org.eclipse.egit.ui.UIText;
+import org.eclipse.egit.ui.internal.dialogs.NewRemoteDialog;
+import org.eclipse.egit.ui.internal.fetch.SimpleConfigureFetchDialog;
+import org.eclipse.egit.ui.internal.push.SimpleConfigurePushDialog;
 import org.eclipse.egit.ui.internal.repository.tree.RemotesNode;
-import org.eclipse.jface.dialogs.IInputValidator;
-import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.window.Window;
-import org.eclipse.jgit.lib.ConfigConstants;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.lib.StoredConfig;
-import org.eclipse.jgit.transport.RemoteConfig;
-import org.eclipse.osgi.util.NLS;
 
 /**
  * Configures the Remote
@@ -36,37 +27,17 @@ public class ConfigureRemoteCommand extends
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		RemotesNode node = getSelectedNodes(event).get(0);
 		Repository repository = node.getRepository();
-		StoredConfig config = repository.getConfig();
-		final Set<String> remoteNames = config
-				.getSubsections(ConfigConstants.CONFIG_REMOTE_SECTION);
 
-		IInputValidator validator = new IInputValidator() {
-			public String isValid(String newText) {
-				if (remoteNames.contains(newText))
-					return NLS
-							.bind(
-									UIText.ConfigureRemoteCommand_RemoteAlreadyExistsMessage,
-									newText);
-				return null;
-			}
-		};
-		InputDialog dlg = new InputDialog(getShell(event),
-				UIText.ConfigureRemoteCommand_RemoteNameDialogTitle,
-				UIText.ConfigureRemoteCommand_RemoteNameDialogMessage, null,
-				validator);
-		if (dlg.open() == Window.OK) {
-			try {
-				RemoteConfig rc = new RemoteConfig(config, dlg.getValue());
-				rc.update(config);
-				config.save();
-			} catch (URISyntaxException e) {
-				Activator.handleError(e.getMessage(), e, true);
-				return null;
-			} catch (IOException e) {
-				Activator.handleError(e.getMessage(), e, true);
-				return null;
-			}
-		}
+		NewRemoteDialog nrd = new NewRemoteDialog(getShell(event), repository);
+		if (nrd.open() != Window.OK)
+			return null;
+
+		if (nrd.getPushMode())
+			SimpleConfigurePushDialog.getDialog(getShell(event), repository,
+					nrd.getName()).open();
+		else
+			SimpleConfigureFetchDialog.getDialog(getShell(event), repository,
+					nrd.getName()).open();
 		return null;
 	}
 }
