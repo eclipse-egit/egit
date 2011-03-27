0ed0f7bed77c7c6516890ed55332e30976f7d955
Robin Rosenberg <robin.rosenberg@dewire.com>
2011-03-27 22:46:50 +0200

    Do not normalize URIishes
    We used to normalize URI's since it seems simple. This however causes
    inconsistencies to the user and to out tests. Just pass backslashes
    through and make sure our parser can handle them. 
    
    Bug: 341062
    Depends-on: JGit I2c8e917a086faabcd8749160c2acc9dd05a42838
    Signed-off-by: Robin Rosenberg <robin.rosenberg@dewire.com>
    Change-Id: Idf27a985d8870c6d6c8dddaeda60b5fe62b1f75f

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/wizards/clone/GitCloneWizardTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/wizards/clone/GitCloneWizardTest.java
@@ -149,22 +149,21 @@ public class GitCloneWizardTest extends GitCloneWizardTestBase {
 		else
 			propertiesPage.setURI("file://" + System.getProperty("user.home"));
 		propertiesPage.assertSourceParams(null, "", System.getProperty(
-				"user.home").replace('\\', '/'), "file", "", false, "", "",
+				"user.home"), "file", "", false, "", "",
 				false, false);
 
 		// Local protocol without file: prefix
 		propertiesPage.setURI(System.getProperty("user.home"));
 		propertiesPage.assertSourceParams(null, "", System.getProperty(
-				"user.home").replace('\\', '/'), "file", "", false, "", "",
+				"user.home"), "file", "", false, "", "",
 				false, false);
 
 		// On windows the use can choose forward or backward slashes, so add
 		// a case for forward slashes using the non prefixed local protocol.
 		if (Platform.getOS().equals(Platform.OS_WIN32)) {
-			propertiesPage.setURI(System.getProperty("user.home").replace('\\',
-					'/'));
+			propertiesPage.setURI(System.getProperty("user.home"));
 			propertiesPage.assertSourceParams(null, "", System.getProperty(
-					"user.home").replace('\\', '/'), "file", "", false, "", "",
+					"user.home"), "file", "", false, "", "",
 					false, false);
 		}
 		bot.button("Cancel").click();
