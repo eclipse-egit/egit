43c107796530513c8f61855a4dba0da8fa292e33
Mathias Kinzler <mathias.kinzler@sap.com>
2011-03-31 20:18:53 +0200

    [RFC] Refactoring Branch operations fourth part: more cleanup
    Move the "Assume Unchanged and "No Assume Unchanged" actions into the
    new "Advanced" sub-menu and do some further cleanup of the remaining
    subclasses of AbstractResourceOperation to get rid of this class.
    
    Change-Id: I667b35548177c4d1f7684c5bc8d85dfdcf70bd87
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/NoAssumeUnchangedAction.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/NoAssumeUnchangedAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2010, Matthias Sohn <matthias.sohn@sap.com>
+ * Copyright (C) 2011, Mathias Kinzler <mathias.kinzler@sap.com >
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,26 +8,15 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.actions;
 
-import java.util.List;
-
-import org.eclipse.core.resources.IResource;
-import org.eclipse.egit.core.op.AssumeUnchangedOperation;
-import org.eclipse.egit.core.op.IEGitOperation;
-import org.eclipse.egit.ui.UIText;
-
 /**
- * This operation unsets the assume-valid bit in the index for the
- * selected resources.
- *
- * @see AssumeUnchangedOperation
+ * Action for displaying fetch wizard - allowing selection of specifications for
+ * fetch, and fetching objects/refs from another repository.
  */
-public class NoAssumeUnchangedAction extends AbstractResourceOperationAction {
-	protected IEGitOperation createOperation(final List<IResource> sel) {
-		return sel.isEmpty() ? null : new AssumeUnchangedOperation(sel, false);
-	}
-
-	@Override
-	protected String getJobName() {
-		return UIText.AssumeUnchanged_noAssumeUnchanged;
+public class NoAssumeUnchangedAction extends RepositoryAction {
+	/**
+	 *
+	 */
+	public NoAssumeUnchangedAction() {
+		super(ActionCommands.NO_ASSUME_UNCHANGED, new NoAssumeUnchangedActionHandler());
 	}
 }
