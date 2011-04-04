6ff4bbc215628a64994431756c15c11e5eabdf9d
Philipp Thun <philipp.thun@sap.com>
2011-04-04 10:57:40 +0200

    Never ignore project nodes
    With this change project nodes are no longer ignored
    (Team.isIgnoredHint). This affects the decoration of resources as well
    as entries in a ContainerTreeIterator.
    
    Bug: 336744
    Change-Id: I2e559b1c59a268956a92808b2cb68d9d90547eb0
    Signed-off-by: Philipp Thun <philipp.thun@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/decorators/GitLightweightDecorator.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/decorators/GitLightweightDecorator.java
@@ -273,7 +273,8 @@ public class GitLightweightDecorator extends LabelProvider implements
 		// Step 3: Perform more expensive tests
 
 		// Don't decorate ignored resources (e.g. bin folder content)
-		if (Team.isIgnoredHint(resource))
+		if (resource.getType() != IResource.PROJECT
+				&& Team.isIgnoredHint(resource))
 			return;
 
 		// Cannot decorate linked resources
