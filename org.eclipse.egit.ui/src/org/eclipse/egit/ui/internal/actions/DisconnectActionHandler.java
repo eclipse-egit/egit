7e3638c59b3c7c1c4bdee0be216fa6f9f4e116dc
Mathias Kinzler <mathias.kinzler@sap.com>
2011-04-19 17:47:00 +0200

    Refactoring Branch operations (4): more cleanup
    Move the "Assume Unchanged and "No Assume Unchanged" actions into the
    new "Advanced" sub-menu and do some further cleanup of the remaining
    subclasses of AbstractResourceOperation to get rid of this class.
    
    Change-Id: I8a62bcee301caf07f3104b4b54495ce78442c9ff
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>
    Signed-off-by: Stefan Lay <stefan.lay@sap.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/DisconnectActionHandler.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (C) 2011, Mathias Kinzler <mathias.kinzler@sap.com>
+ * Copyright (C) 2007, Robin Rosenberg <robin.rosenberg@dewire.com>
+ * Copyright (C) 2007, Shawn O. Pearce <spearce@spearce.org>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.actions;
+
+import java.util.Arrays;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.jobs.IJobChangeEvent;
+import org.eclipse.core.runtime.jobs.JobChangeAdapter;
+import org.eclipse.egit.core.op.DisconnectProviderOperation;
+import org.eclipse.egit.ui.JobFamilies;
+import org.eclipse.egit.ui.UIText;
+import org.eclipse.egit.ui.internal.decorators.GitLightweightDecorator;
+import org.eclipse.egit.ui.internal.job.JobUtil;
+
+/**
+ * Action to disassociate a project from its Git repository.
+ *
+ * @see DisconnectProviderOperation
+ */
+public class DisconnectActionHandler extends RepositoryActionHandler {
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IProject[] projects = getProjectsForSelectedResources();
+		if (projects.length == 0)
+			return null;
+		JobUtil.scheduleUserJob(new DisconnectProviderOperation(Arrays
+				.asList(projects)), UIText.Disconnect_disconnect,
+				JobFamilies.DISCONNECT, new JobChangeAdapter() {
+					@Override
+					public void done(IJobChangeEvent actEvent) {
+						GitLightweightDecorator.refresh();
+					}
+				});
+		return null;
+	}
+}
