c461d189ab32495d9fb3869b24d29141d5a8c10b
Matthias Sohn <matthias.sohn@sap.com>
2011-04-21 10:14:56 +0200

    Refactoring Branch operations (5): improve icons and menu labels
    Change-Id: Iea05ca20b982286b3c500cfc900d1812fe118fd2
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>
    Signed-off-by: Stefan Lay <stefan.lay@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIIcons.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIIcons.java
@@ -143,6 +143,9 @@ public class UIIcons {
 	/** Branch icon */
 	public final static ImageDescriptor BRANCH;
 
+	/** Create Branch icon */
+	public final static ImageDescriptor CREATE_BRANCH;
+
 	/** Clone Icon */
 	public final static ImageDescriptor CLONEGIT;
 
@@ -205,6 +208,7 @@ public class UIIcons {
 		TAGS = map("obj16/tags.gif"); //$NON-NLS-1$
 		TAG = map("obj16/version_rep.gif"); //$NON-NLS-1$
 		BRANCH = map("obj16/branch_obj.gif"); //$NON-NLS-1$
+		CREATE_BRANCH = map("obj16/new_branch_obj.gif"); //$NON-NLS-1$
 		COLLAPSEALL = map("elcl16/collapseall.gif"); //$NON-NLS-1$
 		CLONEGIT = map("obj16/cloneGit.gif"); //$NON-NLS-1$
 		RESET = map("obj16/reset.gif"); //$NON-NLS-1$
