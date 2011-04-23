bfc867ee6ee0ae1c9b9c6fa24db09eec141919eb
Kevin Sawicki <kevin@github.com>
2011-04-22 17:29:48 -0700

    Add persistence support for commit editor input
    Bug: 343000
    Change-Id: I32ee9168d500edfd7c038e954f1c4c9c49804617
    Signed-off-by: Kevin Sawicki <kevin@github.com>
    Signed-off-by: Chris Aniszczyk <caniszczyk@gmail.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commit/CommitEditorInput.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commit/CommitEditorInput.java
@@ -20,13 +20,15 @@ import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IMemento;
 import org.eclipse.ui.IPersistableElement;
 
 /**
  * Commit editor input class. This class wraps a {@link RepositoryCommit} to be
  * viewed in an editor.
  */
-public class CommitEditorInput extends PlatformObject implements IEditorInput {
+public class CommitEditorInput extends PlatformObject implements IEditorInput,
+		IPersistableElement {
 
 	private RepositoryCommit commit;
 
@@ -89,7 +91,7 @@ public class CommitEditorInput extends PlatformObject implements IEditorInput {
 	 * @see org.eclipse.ui.IEditorInput#exists()
 	 */
 	public boolean exists() {
-		return false;
+		return true;
 	}
 
 	/**
@@ -111,7 +113,7 @@ public class CommitEditorInput extends PlatformObject implements IEditorInput {
 	 * @see org.eclipse.ui.IEditorInput#getPersistable()
 	 */
 	public IPersistableElement getPersistable() {
-		return null;
+		return this;
 	}
 
 	/**
@@ -120,6 +122,29 @@ public class CommitEditorInput extends PlatformObject implements IEditorInput {
 	public String getToolTipText() {
 		return MessageFormat.format(UIText.CommitEditorInput_ToolTip, commit
 				.getRevCommit().name(), commit.getRepositoryName());
+	}
+
+	/**
+	 * Get repository commit
+	 *
+	 * @return commit
+	 */
+	public RepositoryCommit getCommit() {
+		return this.commit;
+	}
+
+	/**
+	 * @see org.eclipse.ui.IPersistable#saveState(org.eclipse.ui.IMemento)
+	 */
+	public void saveState(IMemento memento) {
+		CommitEditorInputFactory.saveState(memento, this);
+	}
+
+	/**
+	 * @see org.eclipse.ui.IPersistableElement#getFactoryId()
+	 */
+	public String getFactoryId() {
+		return CommitEditorInputFactory.ID;
 	}
 
 }
