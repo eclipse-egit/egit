4a28945a38994e519a6c5eb9b8dd3c1a82487dda
Kevin Sawicki <kevin@github.com>
2011-04-27 12:12:55 -0700

    Add compare with previous revision menu.
    This opens the compare editor for single file
    selections and the Git Tree compare view for
    non-file selections.
    
    Bug: 344026
    Change-Id: Ibb1cadfdc56376a5cf0110b8cc0105d8067cd20d
    Signed-off-by: Kevin Sawicki <kevin@github.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -1741,6 +1741,15 @@ public class UIText extends NLS {
 	public static String CompareWithIndexAction_errorOnAddToIndex;
 
 	/** */
+	public static String CompareWithPreviousActionHandler_MessageRevisionNotFound;
+
+	/** */
+	public static String CompareWithPreviousActionHandler_TaskGeneratingInput;
+
+	/** */
+	public static String CompareWithPreviousActionHandler_TitleRevisionNotFound;
+
+	/** */
 	public static String ConfirmationPage_cantConnectToAnyTitle;
 
 	/** */
