e81f1400306b90977d8a5920e7d685292cd0f76a
Kevin Sawicki <kevin@github.com>
2011-04-28 14:56:48 -0700

    Add form editor with commit and diff pages.
    Bug: 343000
    Change-Id: I81f1d0e814e7ad7998a268ea9fbe0140a737c573
    Signed-off-by: Kevin Sawicki <kevin@github.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -2915,6 +2915,9 @@ public class UIText extends NLS {
 	public static String CommitActionHandler_repository;
 
 	/** */
+	public static String CommitEditor_TitleHeader;
+
+	/** */
 	public static String CommitEditorInput_Name;
 
 	/** */
