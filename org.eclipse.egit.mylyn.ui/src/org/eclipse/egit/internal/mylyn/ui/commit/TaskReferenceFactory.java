63aa1303c6b9b66da534bb45b33c20f963ce7e84
Benjamin Muskalla <benjamin.muskalla@tasktop.com>
2011-04-28 11:11:57 +0200

    UI blocked while opening menu
    We should not check for host resolution while opening the menu.
    
    Bug: 340412
    Change-Id: I7282cc9648d95354c6b2bc52ae74b8aac51038ab
    Signed-off-by: Benjamin Muskalla <benjamin.muskalla@tasktop.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.mylyn.ui/src/org/eclipse/egit/internal/mylyn/ui/commit/TaskReferenceFactory.java
+++ org.eclipse.egit.mylyn.ui/src/org/eclipse/egit/internal/mylyn/ui/commit/TaskReferenceFactory.java
@@ -192,15 +192,6 @@ public class TaskReferenceFactory implements IAdapterFactory {
 				|| (hostname1.equals(resolvedHostName) && hostname2.equals(localHost)))
 			return true;
 
-		try {
-			String ipAddress1 = InetAddress.getByName(hostname1).getHostAddress();
-			String ipAddress2 = InetAddress.getByName(hostname2).getHostAddress();
-
-			return ipAddress1.equals(ipAddress2);
-		} catch (UnknownHostException ex) {
-			// Ignore it.
-		}
-
 		return false;
 	}
 
