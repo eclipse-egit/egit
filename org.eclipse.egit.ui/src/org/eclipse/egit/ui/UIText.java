c7d57998c000c6ed3e28cc00679f7a51cb38450e
Bernard Leach <leachbj@bouncycastle.org>
2011-04-30 20:40:10 +1000

    [RFC] Add a 'Staging' view
    Git's staging area is one of its most powerful features. This view
    provides users with the ability to drag-and-drop files between
    nodes to place modified or untracked files into the staging area.
    Such an operation would be synonymous to adding the modified files
    to the index and tracking the untracked files. Other operations
    like committing is also possible from the view's context menu.
    
    Change-Id: I7d78aff4db32d864ac0410a178bad149c45b6440
    CQ: 5115
    Bug: 313263
    Also-by: Remy Suen <remysuen@ca.ibm.com>
    Also-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -3512,6 +3512,35 @@ public class UIText extends NLS {
 	public static String DecoratableResourceHelper_noHead;
 
 	/** */
+	public static String ModifiedNode_Label;
+
+	/** */
+	public static String StagedNode_Label;
+
+	/** */
+	public static String UntrackedNode_Label;
+
+	/** */
+	public static String ConflictingNode_Label;
+
+	/** */
+	public static String StagingView_StagingJobLabel;
+
+	/** */
+	public static String StagingView_StagingJobTaskName;
+
+	/** */
+	public static String StagingView_AnalyzeJobLabel;
+
+	/** */
+	public static String StagingView_AnalyzeJobTaskName;
+
+	/** */
+	public static String StagingView_AnalyzeJobSubTaskName;
+
+	/** */
+	public static String StagingView_FailedToStageFiles;
+
 	static {
 		initializeMessages(BUNDLE_NAME, UIText.class);
 	}
