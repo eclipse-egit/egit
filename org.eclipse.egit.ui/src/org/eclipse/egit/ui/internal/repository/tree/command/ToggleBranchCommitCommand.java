87d8ba9282f06dd88daa4ad3dabbc42ecf193fd8
Kevin Sawicki <kevin@github.com>
2011-05-03 13:21:51 -0700

    Add commit message decorations to ref nodes.
    Bug: 344639
    Change-Id: I70a5c1afb2d65581e90642f726625ad0b670cceb
    Signed-off-by: Kevin Sawicki <kevin@github.com>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/ToggleBranchCommitCommand.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ *  Copyright (c) 2011 GitHub Inc.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *    Kevin Sawicki (GitHub Inc.) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.repository.tree.command;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.egit.ui.internal.repository.RepositoriesView;
+import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+/**
+ * Toggles the "Display Latest Branch Commit" preference
+ */
+public class ToggleBranchCommitCommand extends
+		RepositoriesViewCommandHandler<RepositoryTreeNode> {
+
+	/**
+	 * The toggle branch latest commit command id
+	 */
+	public static final String ID = "org.eclipse.egit.ui.RepositoriesToggleBranchCommit"; //$NON-NLS-1$
+
+	/**
+	 * The toggle state of this command
+	 */
+	public static final String TOGGLE_STATE = "org.eclipse.ui.commands.toggleState"; //$NON-NLS-1$
+
+	/**
+	 * @see org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.ExecutionEvent)
+	 */
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		HandlerUtil.toggleCommandState(event.getCommand());
+		IWorkbenchPart part = HandlerUtil.getActivePartChecked(event);
+		if (part instanceof RepositoriesView)
+			(((RepositoriesView) part).getCommonViewer()).refresh();
+		return null;
+	}
+
+}
