6a577c370b0881ebda0146ff07003b99bc4636ec
Kevin Sawicki <kevin@github.com>
2011-05-03 19:12:31 -0700

    Add create tag icon.
    This icon is similar to the create branch
    icon with a '+' in the upper right corner.
    
    Change-Id: I84e72df3234c3d17dc4e908ff5afa2be6ae46859
    Signed-off-by: Kevin Sawicki <kevin@github.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIIcons.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIIcons.java
@@ -140,6 +140,9 @@ public class UIIcons {
 	/** Tag icon */
 	public final static ImageDescriptor TAG;
 
+	/** Create Tag icon */
+	public final static ImageDescriptor CREATE_TAG;
+
 	/** Branch icon */
 	public final static ImageDescriptor BRANCH;
 
@@ -207,6 +210,7 @@ public class UIIcons {
 		OVR_CHECKEDOUT = map("ovr/checkedout_ov.gif"); //$NON-NLS-1$
 		TAGS = map("obj16/tags.gif"); //$NON-NLS-1$
 		TAG = map("obj16/version_rep.gif"); //$NON-NLS-1$
+		CREATE_TAG = map("obj16/new_tag_obj.gif"); //$NON-NLS-1$
 		BRANCH = map("obj16/branch_obj.gif"); //$NON-NLS-1$
 		CREATE_BRANCH = map("obj16/new_branch_obj.gif"); //$NON-NLS-1$
 		COLLAPSEALL = map("elcl16/collapseall.gif"); //$NON-NLS-1$
