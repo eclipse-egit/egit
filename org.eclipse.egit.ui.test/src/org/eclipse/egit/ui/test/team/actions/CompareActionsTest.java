6928f4dc0392113c90d0ca2446d016a75e512ded
Matthias Sohn <matthias.sohn@sap.com>
2011-05-04 16:18:39 +0200

    Fix UI test CompareActionsTest.testCompareWithRef()
    Change-Id: If2756a910bd34384395e246d015221fb161d40af
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/team/actions/CompareActionsTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/team/actions/CompareActionsTest.java
@@ -12,14 +12,15 @@
 package org.eclipse.egit.ui.test.team.actions;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import java.io.File;
 
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.egit.core.op.BranchOperation;
 import org.eclipse.egit.core.op.ResetOperation;
-import org.eclipse.egit.core.op.TagOperation;
 import org.eclipse.egit.core.op.ResetOperation.ResetType;
+import org.eclipse.egit.core.op.TagOperation;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.common.LocalRepositoryTestCase;
 import org.eclipse.egit.ui.internal.dialogs.CompareTreeView;
@@ -123,12 +124,12 @@ public class CompareActionsTest extends LocalRepositoryTestCase {
 		String dialogTitle = UIText.CompareTargetSelectionDialog_WindowTitle;
 		SWTBotShell dialog = openCompareWithDialog(compareWithRefActionLabel,
 				dialogTitle);
-		// use the default (the last commit) -> no changes
-		dialog.bot().button(UIText.CompareTargetSelectionDialog_CompareButton)
-				.click();
-		waitUntilCompareTreeViewTreeHasNodeCount(0);
+		// use the default (the last commit) -> no changes -> compare button
+		// disabled
+		assertFalse(dialog.bot()
+				.button(UIText.CompareTargetSelectionDialog_CompareButton)
+				.isEnabled());
 		// use the tag -> should have a change
-		dialog = openCompareWithDialog(compareWithRefActionLabel, dialogTitle);
 		dialog.bot().tree().getTreeItem(TAGS).expand().getNode("SomeTag")
 				.select();
 		dialog.bot().button(UIText.CompareTargetSelectionDialog_CompareButton)
