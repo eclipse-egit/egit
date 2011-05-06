0ae2b083a2e446f902356e554566ea87e00c3a7c
Christian Georgi <christian.georgi@sap.com>
2011-05-06 14:41:09 +0200

    Persist commit dialog's bounds and sash weights
    Change-Id: I9e08f0be6ab2fe08bb34dc064d8a3c87d78d694c

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitDialog.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitDialog.java
@@ -262,6 +262,8 @@ public class CommitDialog extends Dialog {
 
 	private static final String SHOW_UNTRACKED_PREF = "CommitDialog.showUntracked"; //$NON-NLS-1$
 
+	private static final String SASH_WEIGHTS_PREF_PREFIX = "CommitDialog.sashWeights_"; //$NON-NLS-1$
+
 	SashForm sashForm;
 
 	SpellcheckableMessageArea commitText;
@@ -779,7 +781,7 @@ public class CommitDialog extends Dialog {
 			}
 		}
 
-		sashForm.setWeights(new int[] {1, 3});
+		restoreSashWeights(sashForm, settings, new int[] {1, 3});
 
 		applyDialogFont(sashForm);
 		resourceCol.pack();
@@ -1132,6 +1134,55 @@ public class CommitDialog extends Dialog {
 	@Override
 	protected int getShellStyle() {
 		return super.getShellStyle() | SWT.RESIZE;
+	}
+
+	@Override
+	protected IDialogSettings getDialogBoundsSettings() {
+		String sectionName = getClass().getSimpleName() + "_dialogBounds"; //$NON-NLS-1$
+		IDialogSettings settings = org.eclipse.egit.ui.Activator.getDefault().getDialogSettings();
+		IDialogSettings section = settings.getSection(sectionName);
+		if (section == null) {
+			section = settings.addNewSection(sectionName);
+		}
+		return section;
+	}
+
+	@Override
+	public boolean close() {
+		// store sash weights
+		IDialogSettings settings = org.eclipse.egit.ui.Activator.getDefault().getDialogSettings();
+		saveSashWeights(sashForm, settings);
+
+		return super.close();
+	}
+
+	/*
+	 * Restores the given sash form's weights with saved values
+	 * or falls back to the given defaults
+	 */
+	private void restoreSashWeights(SashForm form, IDialogSettings settings, int[] defaults) {
+		Control[] children = form.getChildren();
+		if (children.length > 0) {
+			int[] weights = new int[children.length];
+			boolean storedWeightsOk = true;
+			for (int i = 0; i < children.length; i++) {
+				try {
+					weights[i] = settings.getInt(SASH_WEIGHTS_PREF_PREFIX + i);
+				} catch (NumberFormatException e) {
+					// no or invalid value
+					storedWeightsOk = false;
+					break;
+				}
+			}
+			form.setWeights(storedWeightsOk ? weights : defaults);
+		}
+	}
+
+	private void saveSashWeights(SashForm form, IDialogSettings settings) {
+		int[] weights = form.getWeights();
+		for (int i = 0; i < weights.length; i++) {
+			settings.put(SASH_WEIGHTS_PREF_PREFIX + i, weights[i]);
+		}
 	}
 
 }
