32e3f574864e453492f21a377c0c08ab07827434
Dariusz Luksza <dariusz@luksza.org>
2011-05-07 16:05:20 +0200

    Simplify 'create new branch' work-flow
    Immediately show 'create new branch' wizard in with one can select
    base branch instead of showing separate dialog for selecting base
    branch
    
    Change-Id: I1e7bd6226a4de4c31aa3e66f7b03188cb471d600
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/CreateBranchPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/CreateBranchPage.java
@@ -149,6 +149,7 @@ class CreateBranchPage extends WizardPage {
 					.setToolTipText(UIText.CreateBranchPage_SourceBranchTooltip);
 		}
 		this.branchCombo = new Combo(main, SWT.READ_ONLY | SWT.DROP_DOWN);
+		branchCombo.setData("org.eclipse.swtbot.widget.key", "BaseBranch"); //$NON-NLS-1$ //$NON-NLS-2$
 
 		GridDataFactory.fillDefaults().span(2, 1).grab(true, false).applyTo(
 				this.branchCombo);
