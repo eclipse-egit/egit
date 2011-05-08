d402c8eb29cdc48838feb7c1e16cd4b7f90b4ec8
Manuel Doninger <manuel.doninger@googlemail.com>
2011-05-08 10:14:42 +0200

    [DRAFT] task activation integration support
    This change enables the automatic creation or checkout of branches
    according to the activated mylyn task. If there's no context in the
    task, a dialog appears, where the repositories can be selected. If
    there's a context, the resources of that context are browsed and the
    contained Git repositories are selected for branch creation/checkout.
    
    Bug: 334597
    Change-Id: I03b927ab8701caa13ec62e536ebdb351d11439e6
    Signed-off-by: Manuel Doninger <manuel.doninger@googlemail.com>

---
+++ org.eclipse.egit.mylyn.ui/src/org/eclipse/egit/internal/mylyn/ui/AutoStart.java
@@ -0,0 +1,17 @@
+package org.eclipse.egit.internal.mylyn.ui;
+
+import org.eclipse.ui.IStartup;
+
+/**
+ * Hack for autostart of the EGit-Mylyn-Plugin
+ *
+ * @author Manuel Doninger
+ *
+ */
+public class AutoStart implements IStartup {
+
+	public void earlyStartup() {
+	//doesn't do anything
+	}
+
+}
