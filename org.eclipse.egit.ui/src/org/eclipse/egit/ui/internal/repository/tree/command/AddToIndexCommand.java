0aba7e24d3997db3d81a0c0159584d9da340e368
Jens Baumgart <jens.baumgart@sap.com>
2011-05-16 17:37:13 +0200

    Repositories View: implement an add action
    An add action is implemented for the Repositories View. This allows
    adding files outside the workspace to the Git index.
    
    Change-Id: I4f52967a72ccd2f8316d4a82308e9b20f3a79547
    Signed-off-by: Jens Baumgart <jens.baumgart@sap.com>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/AddToIndexCommand.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * Copyright (C) 2011, Jens Baumgart <jens.baumgart@sap.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.repository.tree.command;
+
+import java.util.List;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.UIText;
+import org.eclipse.egit.ui.internal.repository.tree.FileNode;
+import org.eclipse.egit.ui.internal.repository.tree.FolderNode;
+import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
+import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNodeType;
+import org.eclipse.jgit.api.AddCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.NoFilepatternException;
+import org.eclipse.jgit.lib.Repository;
+
+/**
+ * Implements an action to add a file to the Git index
+ *
+ */
+public class AddToIndexCommand extends RepositoriesViewCommandHandler<FileNode> {
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		AddCommand addCommand = null;
+		List selectedNodes = getSelectedNodes(event);
+		for (Object selectedNode : selectedNodes) {
+			RepositoryTreeNode node = (RepositoryTreeNode) selectedNode;
+			if (addCommand == null)
+				addCommand = new Git(node.getRepository()).add();
+			Repository repository = node.getRepository();
+			String path;
+			if (node.getType().equals(RepositoryTreeNodeType.FOLDER))
+				path = ((FolderNode) node).getObject().getAbsolutePath();
+			else if (node.getType().equals(RepositoryTreeNodeType.FILE))
+				path = ((FileNode) node).getObject().getAbsolutePath();
+			else
+				path = repository.getWorkTree().getAbsolutePath();
+			String repoRelativepath;
+			String workTreePath = repository.getWorkTree().getAbsolutePath();
+			if (path.equals(workTreePath))
+				repoRelativepath = "."; //$NON-NLS-1$
+			else
+				repoRelativepath = path.substring(repository.getWorkTree()
+						.getPath().length() + 1);
+			addCommand.addFilepattern(repoRelativepath);
+		}
+		if (addCommand != null)
+			try {
+				addCommand.call();
+			} catch (NoFilepatternException e) {
+				Activator.logError(UIText.AddToIndexCommand_addingFilesFailed,
+						e);
+			}
+		return null;
+	}
+
+	@Override
+	public void setEnabled(Object evaluationContext) {
+		enableWorkingDirCommand(evaluationContext);
+	}
+
+}
