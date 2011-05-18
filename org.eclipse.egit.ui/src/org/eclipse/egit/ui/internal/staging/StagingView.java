9d5e1b382d198c74caac72431b5699b32ea8d95d
Jens Baumgart <jens.baumgart@sap.com>
2011-05-18 15:46:45 +0200

    StagingView: exclude ignored files
    Ignored files (e.g. derived files in the bin folder)
    are excluded by using ContainerTreeIterator /
    AdaptableFileTreeIterator.
    
    Change-Id: I74c8956c07bff2d0bda354d1f43d670407606b9d
    Signed-off-by: Jens Baumgart <jens.baumgart@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingView.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingView.java
@@ -26,6 +26,7 @@ import org.eclipse.core.expressions.EvaluationContext;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.egit.core.IteratorService;
 import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.UIText;
@@ -54,7 +55,7 @@ import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.RepositoryState;
 import org.eclipse.jgit.lib.UserConfig;
 import org.eclipse.jgit.revwalk.RevWalk;
-import org.eclipse.jgit.treewalk.FileTreeIterator;
+import org.eclipse.jgit.treewalk.WorkingTreeIterator;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.dnd.DND;
@@ -394,10 +395,8 @@ public class StagingView extends ViewPart {
 
 		final IndexDiff indexDiff;
 		try {
-			// TODO IteratorService.createInitialIterator(repository)?
-			FileTreeIterator fileTreeIterator = new FileTreeIterator(repository);
-			indexDiff = new IndexDiff(repository, Constants.HEAD,
-					fileTreeIterator);
+			WorkingTreeIterator iterator = IteratorService.createInitialIterator(repository);
+			indexDiff = new IndexDiff(repository, Constants.HEAD, iterator);
 			indexDiff.diff();
 		} catch (IOException e) {
 			throw new RuntimeException(e);
