2cb3fecefec914f0621c143469a47597e4fcee4f
Kevin Sawicki <kevin@github.com>
2011-05-18 12:11:20 -0700

    Add Synchronize view shortcut to Git perspective.
    Change-Id: I71de73a74c785b1f772a6201c350964d11a1f34b
    Signed-off-by: Kevin Sawicki <kevin@github.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/GitRepositoriesPerspectiveFactory.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/GitRepositoriesPerspectiveFactory.java
@@ -12,6 +12,7 @@ package org.eclipse.egit.ui;
 
 import org.eclipse.egit.ui.internal.repository.RepositoriesView;
 import org.eclipse.team.ui.history.IHistoryView;
+import org.eclipse.team.ui.synchronize.ISynchronizeView;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
@@ -36,7 +37,8 @@ public class GitRepositoriesPerspectiveFactory implements IPerspectiveFactory {
 		layout.addShowViewShortcut("org.eclipse.jdt.ui.PackageExplorer"); //$NON-NLS-1$
 		// shortcut to History view
 		layout.addShowViewShortcut(IHistoryView.VIEW_ID);
-
+		// shortcut to Synchronize view
+		layout.addShowViewShortcut(ISynchronizeView.VIEW_ID);
 
 
 	}
