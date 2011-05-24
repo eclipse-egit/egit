13c72210538bca900490a1fca481999133bde17f
Mathias Kinzler <mathias.kinzler@sap.com>
2011-05-24 23:00:53 +0200

    Use <user_home>/git as default-default repository path
    This is better than the currently used <user_home>, as this is very
    likely to result in Repositories being created directly under
    <user_home>. We should instead support creation of the Repositories
    under their own folder.
    
    The folder is created immediately during startup, otherwise the tools
    that will use this folder won't be able to use it, e.g. point a
    DirectoryDialog to this folder when the user hits "Browse..." etc. If
    the folder can not be created during startup, we just log an error.
    
    Change-Id: I2aee1b053cf991db064ece28231218bb4db399bb
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/PluginPreferenceInitializer.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/PluginPreferenceInitializer.java
@@ -10,6 +10,8 @@
  *******************************************************************************/
 package org.eclipse.egit.ui;
 
+import java.io.File;
+
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.egit.ui.internal.decorators.GitLightweightDecorator;
 import org.eclipse.egit.ui.internal.synchronize.mapping.GitChangeSetLabelProvider;
@@ -71,7 +73,7 @@ public class PluginPreferenceInitializer extends AbstractPreferenceInitializer {
 
 		store.setDefault(UIPreferences.REFESH_ON_INDEX_CHANGE, true);
 		store.setDefault(UIPreferences.REFESH_ONLY_WHEN_ACTIVE, true);
-		store.setDefault(UIPreferences.DEFAULT_REPO_DIR, FS.DETECTED.userHome().getPath());
+		store.setDefault(UIPreferences.DEFAULT_REPO_DIR, new File(FS.DETECTED.userHome(), "git").getPath()); //$NON-NLS-1$
 		store.setDefault(UIPreferences.SHOW_REBASE_CONFIRM, true);
 		store.setDefault(UIPreferences.SHOW_INITIAL_CONFIG_DIALOG, true);
 		store.setDefault(UIPreferences.SHOW_HOME_DIR_WARNING, true);
