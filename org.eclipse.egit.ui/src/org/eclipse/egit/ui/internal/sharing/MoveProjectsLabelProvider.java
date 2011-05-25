662b55dd4356be2b360b86ec2219ceb3b9011f2a
Mathias Kinzler <mathias.kinzler@sap.com>
2011-05-25 08:37:43 +0200

    Teach the Sharing wizard to share with an existing Repository
    This uses the Refactoring capabilities available in Eclipse to allow
    sharing an Eclipse project with a Repository that is not located in a
    parent folder of the project.
    
    By default, the Sharing Wizard now allows selection of an existing
    Repository (from the RepoView) and optionally a relative path
    within this repository into which the project is to be shared (always
    using the project name as terminating folder).
    
    Upon finish, the project location is then moved to the new target folder
    (a child folder of the working directory of the selected
    Repository) before the ConnectProviderOperation is executed.
    
    Users can click a checkbox to enable the previous behavior where the
    Repository can only be created in a parent folder of the selected
    project or projects.
    
    Change-Id: I5e0f34e331b5427801f06ad8026ff1a78b488333
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/sharing/MoveProjectsLabelProvider.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (C) 2011, Mathias Kinzler <mathias.kinzler@sap.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.sharing;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.viewers.BaseLabelProvider;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.swt.graphics.Image;
+
+/**
+ * A label provide for showing projects to be moved during sharing
+ */
+public class MoveProjectsLabelProvider extends BaseLabelProvider implements
+		ITableLabelProvider {
+	IPath targetFolder;
+
+	public Image getColumnImage(Object element, int columnIndex) {
+		return null;
+	}
+
+	public String getColumnText(Object element, int columnIndex) {
+		IProject prj = (IProject) element;
+		switch (columnIndex) {
+		case 0:
+			return prj.getName();
+		case 1:
+			return prj.getLocation().toString();
+		case 2:
+			if (targetFolder != null)
+				return targetFolder.append(prj.getName()).toString();
+			return null;
+		default:
+			return null;
+		}
+	}
+}
