3cb78c974d1148c55eb5d3d6988e167b6812e198
Robin Stocker <robin@nibor.org>
2011-05-25 22:50:03 +0200

    Fix CommonUtils.STRING_ASCENDING_COMPARATOR issues
    The comparator provides the means to sort a list of strings using a
    natural sort order (i.e. "2" before "10"). It had two problems.
    
    The first one was that it could not handle strings that contained many
    numbers (when all the numbers concatenated exceeded Integer.MAX_VALUE).
    This lead to a NumberFormatException. Because the comparator is used in
    CreateBranchPage, it was impossible to create a branch in a repository
    that had a ref with such a name (e.g. stable-1-0-0_2011-05-25).
    
    The second one was that it resulted in the wrong order for some inputs.
    For example, the following inputs would not be sorted as listed here:
    
      project-1-0-0-final
      project-1-0-1-beta
    
    With the implementation of this change, it behaves correctly for these
    two problems.
    
    Change-Id: I599b2783b6ac23c429c08144f41aac9916e06356
    Signed-off-by: Robin Stocker <robin@nibor.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/CommonUtils.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/CommonUtils.java
@@ -1,5 +1,6 @@
 /*******************************************************************************
  * Copyright (C) 2011, Dariusz Luksza <dariusz@luksza.org>
+ * Copyright (C) 2011, Robin Stocker <robin@nibor.org>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -22,38 +23,55 @@ public class CommonUtils {
 		// non-instantiable utility class
 	}
 
-	private static final Pattern NUMBERS = Pattern.compile("[\\d]*"); //$NON-NLS-1$
+	private static final Pattern BETWEEN_PARTS =
+			Pattern.compile("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)"); //$NON-NLS-1$
 
-	private static final Pattern CHARS = Pattern.compile("[^0-9]*"); //$NON-NLS-1$
+	private static final Pattern LEADING_ZEROS = Pattern.compile("^0*"); //$NON-NLS-1$
 
 	/**
 	 * Instance of comparator that sorts strings in ascending alphabetical and
-	 * numerous order.
+	 * numerous order (also known as natural order).
 	 */
 	public static final Comparator<String> STRING_ASCENDING_COMPARATOR = new Comparator<String>() {
 		public int compare(String o1, String o2) {
-			String o1Chars = NUMBERS.matcher(o1).replaceAll(""); //$NON-NLS-1$
-			String o2Chars = NUMBERS.matcher(o2).replaceAll(""); //$NON-NLS-1$
-			int charCompare = o1Chars.compareTo(o2Chars);
+			if (o1.length() == 0)
+				return -1;
+			if (o2.length() == 0)
+				return 1;
 
-			if (charCompare == 0) {
-				String o1Numbers = CHARS.matcher(o1).replaceAll(""); //$NON-NLS-1$
-				String o2Numbers = CHARS.matcher(o2).replaceAll(""); //$NON-NLS-1$
-				if (o1Numbers.length() == 0)
-					o1Numbers = "0"; //$NON-NLS-1$
-				if (o2Numbers.length() == 0)
-					o2Numbers = "0"; //$NON-NLS-1$
+			String[] o1Parts = BETWEEN_PARTS.split(o1);
+			String[] o2Parts = BETWEEN_PARTS.split(o2);
 
-				return Integer.parseInt(o2Numbers)
-						- Integer.parseInt(o1Numbers);
+			for (int i = 0; i < o1Parts.length; i++) {
+				if (i >= o2Parts.length)
+					return 1;
+
+				String o1Part = o1Parts[i];
+				String o2Part = o2Parts[i];
+
+				int result;
+
+				if (Character.isDigit(o1Part.charAt(0)) && Character.isDigit(o2Part.charAt(0))) {
+					o1Part = LEADING_ZEROS.matcher(o1Part).replaceFirst(""); //$NON-NLS-1$
+					o2Part = LEADING_ZEROS.matcher(o2Part).replaceFirst(""); //$NON-NLS-1$
+					result = o1Part.length() - o2Part.length();
+					if (result == 0)
+						result = o1Part.compareTo(o2Part);
+				} else {
+					result = o1Part.compareTo(o2Part);
+				}
+
+				if (result != 0)
+					return result;
 			}
 
-			return charCompare;
+			return -1;
 		}
 	};
 
 	/**
-	 *
+	 * Instance of comparator which sorts {@link Ref} names using
+	 * {@link CommonUtils#STRING_ASCENDING_COMPARATOR}.
 	 */
 	public static final Comparator<Ref> REF_ASCENDING_COMPARATOR = new Comparator<Ref>() {
 		public int compare(Ref o1, Ref o2) {
