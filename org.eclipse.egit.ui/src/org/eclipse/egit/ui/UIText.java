8c80edfbb068df4f2378aa161744b5101c1928ba
Mathias Kinzler <mathias.kinzler@sap.com>
2011-05-25 15:47:40 +0200

    Teach the Sharing wizard to share with an existing Repository
    This uses the Refactoring capabilities available in Eclipse to allow
    sharing an Eclipse project with a Repository that is not located in a
    parent folder of the project.
    
    By default, the Sharing Wizard now allows selection of an existing
    Repository (from the RepoView) and optionally a relative path
    within this repository into which the project is to be shared (always
    using the project name as terminating folder).
    
    Upon finish, the project location is then moved to the new target folder
    (a child folder of the working directory of the selected
    Repository) before the ConnectProviderOperation is executed.
    
    Users can click a checkbox to enable the previous behavior where the
    Repository can only be created in a parent folder of the selected
    project or projects.
    
    Change-Id: I5e0f34e331b5427801f06ad8026ff1a78b488333
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -148,6 +148,9 @@ public class UIText extends NLS {
 	public static String SharingWizard_failed;
 
 	/** */
+	public static String SharingWizard_MoveProjectActionLabel;
+
+	/** */
 	public static String GenerateHistoryJob_BuildingListMessage;
 
 	/** */
@@ -187,7 +190,19 @@ public class UIText extends NLS {
 	public static String EgitUiUtils_CouldNotOpenEditorMessage;
 
 	/** */
+	public static String ExistingOrNewPage_BrowseRepositoryButton;
+
+	/** */
 	public static String ExistingOrNewPage_CreateButton;
+
+	/** */
+	public static String ExistingOrNewPage_CreateRepositoryButton;
+
+	/** */
+	public static String ExistingOrNewPage_CreationInWorkspaceWarningTooltip;
+
+	/** */
+	public static String ExistingOrNewPage_CurrentLocationColumnHeader;
 
 	/** */
 	public static String ExistingOrNewPage_title;
@@ -196,10 +211,25 @@ public class UIText extends NLS {
 	public static String ExistingOrNewPage_description;
 
 	/** */
+	public static String ExistingOrNewPage_DescriptionExternalMode;
+
+	/** */
 	public static String ExistingOrNewPage_ErrorFailedToCreateRepository;
 
 	/** */
 	public static String ExistingOrNewPage_ErrorFailedToRefreshRepository;
+
+	/** */
+	public static String ExistingOrNewPage_ExistingRepositoryLabel;
+
+	/** */
+	public static String ExistingOrNewPage_ExistingTargetErrorMessage;
+
+	/** */
+	public static String ExistingOrNewPage_FailedToDetectRepositoryMessage;
+
+	/** */
+	public static String ExistingOrNewPage_FolderWillBeCreatedMessage;
 
 	/** */
 	public static String ExistingOrNewPage_HeaderPath;
@@ -211,7 +241,37 @@ public class UIText extends NLS {
 	public static String ExistingOrNewPage_HeaderRepository;
 
 	/** */
+	public static String ExistingOrNewPage_InternalModeCheckbox;
+
+	/** */
+	public static String ExistingOrNewPage_NestedProjectErrorMessage;
+
+	/** */
+	public static String ExistingOrNewPage_NewLocationTargetHeader;
+
+	/** */
+	public static String ExistingOrNewPage_NoRepositorySelectedMessage;
+
+	/** */
+	public static String ExistingOrNewPage_ProjectNameColumnHeader;
+
+	/** */
+	public static String ExistingOrNewPage_RelativePathLabel;
+
+	/** */
+	public static String ExistingOrNewPage_RepoCreationInWorkspaceCreationWarning;
+
+	/** */
 	public static String ExistingOrNewPage_SymbolicValueEmptyMapping;
+
+	/** */
+	public static String ExistingOrNewPage_WorkingDirectoryLabel;
+
+	/** */
+	public static String ExistingOrNewPage_WrongPathErrorDialogMessage;
+
+	/** */
+	public static String ExistingOrNewPage_WrongPathErrorDialogTitle;
 
 	/** */
 	public static String GitCloneWizard_abortingCloneMsg;
