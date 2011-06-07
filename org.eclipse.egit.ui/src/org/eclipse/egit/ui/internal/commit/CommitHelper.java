73564cf2f7cdb9e4870473e468d6e8672d131b5d
Jens Baumgart <jens.baumgart@sap.com>
2011-06-07 16:52:20 +0200

    Implement Tests for the Commit Dialog
    Additional tests were implemented to verify the behavior of the toggles
    in the commit dialog (amend, signed-off, change-id).
    
    Change-Id: Ieb9107ed1ea9336f1320323eacc261542506c9c7
    Signed-off-by: Jens Baumgart <jens.baumgart@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commit/CommitHelper.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commit/CommitHelper.java
@@ -229,7 +229,10 @@ public class CommitHelper {
 		PersonIdent authorIdent = headCommit.getAuthorIdent();
 		String author = authorIdent.getName()
 				+ " <" + authorIdent.getEmailAddress() + ">"; //$NON-NLS-1$ //$NON-NLS-2$
-		return new CommitInfo(headCommit, author, commitMessage);
+		PersonIdent committerIdent = headCommit.getCommitterIdent();
+		String committer = committerIdent.getName()
+				+ " <" + committerIdent.getEmailAddress() + ">"; //$NON-NLS-1$ //$NON-NLS-2$
+		return new CommitInfo(headCommit, author, committer, commitMessage);
 	}
 
 	/**
@@ -239,17 +242,20 @@ public class CommitHelper {
 	public static class CommitInfo {
 		private RevCommit commit;
 		private String author;
+		private String committer;
 		private String commitMessage;
 
 		/**
 		 * @param commit
 		 * @param author
+		 * @param committer
 		 * @param commitMessage
 		 */
-		public CommitInfo(RevCommit commit, String author, String commitMessage) {
+		public CommitInfo(RevCommit commit, String author, String committer, String commitMessage) {
 			super();
 			this.commit = commit;
 			this.author = author;
+			this.committer = committer;
 			this.commitMessage = commitMessage;
 		}
 
@@ -265,6 +271,13 @@ public class CommitHelper {
 		 */
 		public String getAuthor() {
 			return author;
+		}
+
+		/**
+		 * @return committer
+		 */
+		public String getCommitter() {
+			return committer;
 		}
 
 		/**
