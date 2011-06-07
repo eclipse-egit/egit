eclipse-egit/egit

commit 655055eb5706e269825124484ef8318e98f2ae15

Fix cleanup of tests in SharingWizardTest
We should delete the projects on disk between every
test. Furthermore we should delete the Git repos that
we create, even if they are located outside the git
repos.

Change-Id: I8782579993074349f94a7da49e7b1198d26ad52a
Signed-off-by: Robin Rosenberg <robin.rosenberg@dewire.com>

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/wizards/share/SharingWizardTest.java
@@ -16,14 +16,19 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.QualifiedName;
+import org.eclipse.egit.core.op.DisconnectProviderOperation;
 import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.UIPreferences;
@@ -45,6 +50,7 @@ import org.eclipse.jgit.junit.MockSystemReader;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepository;
+import org.eclipse.jgit.util.FileUtils;
 import org.eclipse.jgit.util.SystemReader;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.waits.Conditions;
@@ -54,6 +60,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 
 @RunWith(SWTBotJunit4ClassRunner.class)
@@ -99,21 +106,37 @@ public class SharingWizardTest extends LocalRepositoryTestCase {
 
 	@After
 	public void after() throws Exception {
-		erase(projectName0);
-		erase(projectName1);
-		erase(projectName2);
-		erase(projectName3);
+		Set<File> d = new TreeSet<File>();
+		erase(projectName0, d);
+		erase(projectName1, d);
+		erase(projectName2, d);
+		erase(projectName3, d);
+		for (File f : d)
+			if (f.exists())
+				FileUtils.delete(f, FileUtils.RECURSIVE);
 		ResourcesPlugin.getWorkspace().getRoot().refreshLocal(
 				IResource.DEPTH_INFINITE, null);
 		new Eclipse().reset();
 	}
 
-	private void erase(String projectName) throws CoreException {
+	private void erase(String projectName, Set<File> dirs) throws CoreException, IOException {
 		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(
 				projectName);
 		if (project.exists()) {
+			RepositoryMapping repo = RepositoryMapping.getMapping(project);
+			if (repo != null) {
+				IPath gitDirAbsolutePath = repo.getGitDirAbsolutePath();
+				File canonicalFile = gitDirAbsolutePath.toFile().getCanonicalFile();
+				dirs.add(canonicalFile);
+				File workspacePath = ResourcesPlugin.getWorkspace().getRoot().getLocation().toFile().getCanonicalFile();
+				File gitDirParent = canonicalFile.getParentFile();
+				if (!(gitDirParent.toString()+"/").startsWith(workspacePath.toString()+"/"))
+					if (!(gitDirParent.toString()+"/").startsWith(getTestDirectory().getCanonicalPath().toString()+"/"))
+						fail("Attempting cleanup of directory neither in workspace nor test directory" + canonicalFile);
+				new DisconnectProviderOperation(Collections.singleton(project)).execute(null);
+			}
 			project.close(null);
-			project.delete(false, null);
+			project.delete(true, true, null);
 		}
 	}
 

commit f3b51cce3a4eb94ccf9210f1be5a265d8eb7c640

Fix broken SharingWizardTest on Windows
Commit 655055eb5706e269825124484ef8318e98f2ae15 broke
SharingWizardTest on Windows.

Change-Id: Ifb8ab28e81564f46289d2ce5a6418b845471ecf6
Signed-off-by: Jens Baumgart <jens.baumgart@sap.com>

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/wizards/share/SharingWizardTest.java
@@ -130,10 +130,15 @@ public class SharingWizardTest extends LocalRepositoryTestCase {
 				dirs.add(canonicalFile);
 				File workspacePath = ResourcesPlugin.getWorkspace().getRoot().getLocation().toFile().getCanonicalFile();
 				File gitDirParent = canonicalFile.getParentFile();
-				if (!(gitDirParent.toString()+"/").startsWith(workspacePath.toString()+"/"))
-					if (!(gitDirParent.toString()+"/").startsWith(getTestDirectory().getCanonicalPath().toString()+"/"))
-						fail("Attempting cleanup of directory neither in workspace nor test directory" + canonicalFile);
-				new DisconnectProviderOperation(Collections.singleton(project)).execute(null);
+				if (!(gitDirParent.toString() + File.separator)
+						.startsWith(workspacePath.toString() + File.separator))
+					if (!(gitDirParent.toString() + File.separator)
+							.startsWith(getTestDirectory().getCanonicalPath()
+									.toString() + File.separator))
+						fail("Attempting cleanup of directory neither in workspace nor test directory"
+								+ canonicalFile);
+				new DisconnectProviderOperation(Collections.singleton(project))
+						.execute(null);
 			}
 			project.close(null);
 			project.delete(true, true, null);