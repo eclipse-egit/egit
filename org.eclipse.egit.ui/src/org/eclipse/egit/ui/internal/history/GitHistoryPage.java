9e2a579bc5736fb58dba65ee5dc477febfc16845
Matthias Sohn <matthias.sohn@sap.com>
2011-06-16 15:29:27 +0200

    [historyView] Display HEAD and other additional refs
    When "Show all branches" is selected also always show HEAD and
    additional refs. This helps e.g. in case of a detached
    HEAD.
    
    Change-Id: Ia23915c927aab406348ebb6ca040a5f6e6f7387f
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitHistoryPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitHistoryPage.java
@@ -1517,6 +1517,7 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener {
 				markStartAllRefs(Constants.R_HEADS);
 				markStartAllRefs(Constants.R_REMOTES);
 				markStartAllRefs(Constants.R_TAGS);
+				markStartAdditionalRefs();
 			} else
 				currentWalk.markStart(currentWalk.parseCommit(headId));
 		} catch (IOException e) {
@@ -1623,10 +1624,30 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener {
 			Ref ref = refEntry.getValue();
 			if (ref.isSymbolic())
 				continue;
-			Object refTarget = currentWalk.parseAny(ref.getObjectId());
-			if (refTarget instanceof RevCommit)
-				currentWalk.markStart((RevCommit) refTarget);
+			markStartRef(ref);
 		}
+	}
+
+	private void markStartAdditionalRefs() throws IOException {
+		AnyObjectId head = resolveHead(input.getRepository(), true);
+		if (head != null)
+			markStartObj(head);
+		List<Ref> additionalRefs = input.getRepository().getRefDatabase()
+				.getAdditionalRefs();
+		for (Ref ref : additionalRefs) {
+			markStartRef(ref);
+		}
+	}
+
+	private void markStartRef(Ref ref) throws MissingObjectException,
+			IOException, IncorrectObjectTypeException {
+		markStartObj(ref.getLeaf().getObjectId());
+	}
+
+	private void markStartObj(AnyObjectId id) throws MissingObjectException, IOException {
+		Object refTarget = currentWalk.parseAny(id);
+		if (refTarget instanceof RevCommit)
+			currentWalk.markStart((RevCommit) refTarget);
 	}
 
 	private void cancelRefreshJob() {
