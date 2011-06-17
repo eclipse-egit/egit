9ddb5f1e04cb23ab18e52db08c015d9926df3ca1
Robin Rosenberg <robin.rosenberg@dewire.com>
2011-06-17 08:32:09 +0200

    Add support for git variables in launchers
    This adds git_dir, git_work_tree, git_repo_relative_path and
    git_branch. They all work on the selected or named resource, i.e.
    ${git_branch} would return the current branch in the (first)
    selected project. ${git_branch:org.eclipse.jgit/.project} would
    return the current branch of the main JGit repository in the
    workspace.
    
    The code to pick the selected resource is stolen from Chris
    Aniszczyk's commit on a similar topic.
    
    Change-Id: Idad98f57440eb3b083ffd1be0f382bdac33963e3
    Signed-off-by: Robin Rosenberg <robin.rosenberg@dewire.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -3740,6 +3740,15 @@ public class UIText extends NLS {
 	public static String GitActionContributor_Pull;
 
 	/** */
+	public static String GitVariableResolver_InternalError;
+
+	/** */
+	public static String GitVariableResolver_NoSelectedResource;
+
+	/** */
+	public static String GitVariableResolver_VariableReferencesNonExistentResource;
+
+	/** */
 	public static String DecoratableResourceHelper_noHead;
 
 	/** */
