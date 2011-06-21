fb988f82cecce6616c4901604c5af154e6a5b6d1
Benjamin Muskalla <benjamin.muskalla@tasktop.com>
2011-06-21 07:56:03 +0200

    Consider logical model when replacing files
    When replacing files from the repository, we should ask all model
    providers if this set of resources is consistent in itself or if we
    need to replace more resources to have a consitent model.
    
    Bug: 348060
    Signed-off-by: Benjamin Muskalla <benjamin.muskalla@tasktop.com>
    Change-Id: I95ed9d53dbf8509fdd89f3c47b0280b6e40e946b

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/DiscardChangesActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/DiscardChangesActionHandler.java
@@ -5,6 +5,9 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Benjamin Muskalla (Tasktop Technologies Inc.) - support for model scoping
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.actions;
 
@@ -15,14 +18,17 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.egit.core.op.DiscardChangesOperation;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.UIText;
+import org.eclipse.egit.ui.internal.operations.GitScopeUtil;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.RepositoryState;
+import org.eclipse.ui.IWorkbenchPart;
 
 /**
  * Checkout all selected dirty files.
@@ -31,12 +37,13 @@ public class DiscardChangesActionHandler extends RepositoryActionHandler {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 
+		IWorkbenchPart part = getPart(event);
 		boolean performAction = MessageDialog.openConfirm(getShell(event),
 				UIText.DiscardChangesAction_confirmActionTitle,
 				UIText.DiscardChangesAction_confirmActionMessage);
 		if (!performAction)
 			return null;
-		final DiscardChangesOperation operation = createOperation(event);
+		final DiscardChangesOperation operation = createOperation(part, event);
 		if (operation == null)
 			return null;
 		String jobname = UIText.DiscardChangesAction_discardChanges;
@@ -73,18 +80,46 @@ public class DiscardChangesActionHandler extends RepositoryActionHandler {
 		return true;
 	}
 
-	/**
-	 * Create discard operation to execute. Returning a null operation aborts
-	 * this execution of this handler.
-	 *
-	 * @param event
-	 * @return revision
-	 * @throws ExecutionException
-	 */
-	protected DiscardChangesOperation createOperation(ExecutionEvent event)
+
+	private DiscardChangesOperation createOperation(IWorkbenchPart part, ExecutionEvent event)
 			throws ExecutionException {
-		return new DiscardChangesOperation(getSelectedResources(event), null);
+
+		IResource[] selectedResources = gatherResourceToOperateOn(event);
+		String revision;
+		try {
+			revision = gatherRevision(event);
+		} catch (OperationCanceledException e) {
+			return null;
+		}
+
+		IResource[] resourcesInScope;
+		try {
+			resourcesInScope = GitScopeUtil.getRelatedChanges(part, selectedResources);
+		} catch (InterruptedException e) {
+			// ignore, we will not discard the files in case the user
+			// cancels the scope operation
+			return null;
+		}
+
+		return new DiscardChangesOperation(resourcesInScope, revision);
 
 	}
 
+	/**
+	 * @param event
+	 * @return set of resources to operate on
+	 * @throws ExecutionException
+	 */
+	protected IResource[] gatherResourceToOperateOn(ExecutionEvent event) throws ExecutionException {
+		return getSelectedResources(event);
+	}
+
+	/**
+	 * @param event
+	 * @return the revision to use
+	 * @throws ExecutionException
+	 */
+	protected String gatherRevision(ExecutionEvent event) throws ExecutionException {
+		return null;
+	}
 }
