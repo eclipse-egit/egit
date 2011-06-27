f46ac0f901b8c19f2b3e4386b20caf09f3e82a1d
Henning Heitkoetter <hheitkoetter@googlemail.com>
2011-06-27 09:42:16 -0700

    [blame] Add a preference to ignore whitespace changes
    This adds a git preference that controls if whitespace changes are to
    be ignored when calculating blame annotations from the context menu.
    
    If checked, use the WS_IGNORE_ALL text comparator instead of the default
    one when constructing the BlameCommand.
    
    Change-Id: Id22c3d5d0ca7f6603b47f1c71fdc182ea75a8527
    Signed-off-by: Henning Heitkoetter <hheitkoetter@googlemail.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/preferences/GitPreferenceRoot.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/preferences/GitPreferenceRoot.java
@@ -175,6 +175,14 @@ public class GitPreferenceRoot extends FieldEditorPreferencePage implements
 				UIText.GitPreferenceRoot_automaticallyEnableChangesetModel,
 				synchronizeGroup));
 		updateMargins(synchronizeGroup);
+
+		Group blameGroup = new Group(main, SWT.SHADOW_ETCHED_IN);
+		GridDataFactory.fillDefaults().grab(true, false).span(GROUP_SPAN, 1)
+				.applyTo(blameGroup);
+		blameGroup.setText(UIText.GitPreferenceRoot_BlameGroupHeader);
+		addField(new BooleanFieldEditor(UIPreferences.BLAME_IGNORE_WHITESPACE,
+				UIText.GitPreferenceRoot_BlameIgnoreWhitespaceLabel, blameGroup));
+		updateMargins(blameGroup);
 	}
 
 	private void updateMargins(Group group) {
