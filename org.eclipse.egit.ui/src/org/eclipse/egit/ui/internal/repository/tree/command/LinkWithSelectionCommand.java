b78860ab75920b5c1d7c9a1c400d155e93f8a64e
Matthias Sohn <matthias.sohn@sap.com>
2011-06-29 11:51:43 +0200

    Fix ClassCastException in LinkWithSelectionCommand
    Bug: 348780
    Change-Id: Ie0a6848c52c27dd843d388ad0fe3ba1ab191978c
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/LinkWithSelectionCommand.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/LinkWithSelectionCommand.java
@@ -14,7 +14,9 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
 import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.ToolItem;
+import org.eclipse.swt.widgets.Widget;
 
 /**
  * Implements "Link With Selection" toggle
@@ -24,8 +26,17 @@ public class LinkWithSelectionCommand extends
 	public Object execute(final ExecutionEvent event) throws ExecutionException {
 		// is there a better way?
 		Event evt = (Event) event.getTrigger();
-		ToolItem item = (ToolItem) evt.widget;
-		boolean selected = item.getSelection();
+		Widget w = evt.widget;
+		final boolean selected;
+		if (w instanceof ToolItem) {
+			ToolItem item = (ToolItem) w;
+			selected = item.getSelection();
+		} else if (w instanceof MenuItem) {
+			MenuItem item = (MenuItem) w;
+			selected = item.getSelection();
+		} else
+			throw new IllegalStateException(
+					"unexpected type: " + w.getClass().toString()); //$NON-NLS-1$
 		getView(event).setReactOnSelection(selected);
 		return null;
 	}
