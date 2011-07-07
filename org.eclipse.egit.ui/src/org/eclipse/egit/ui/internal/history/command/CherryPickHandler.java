10e51a448e6f4e2be035f4e10366e8d13ddf54f4
Jens Baumgart <jens.baumgart@sap.com>
2011-07-07 15:01:32 +0200

    Fix cherry pick error handling
    The cherry pick action did not show information to the user in case
    of failure. Now the user is shown an error dialog.
    
    Bug: 332660
    Change-Id: I161fb65e333df4af23a9d4316d531406c3f4e1ba
    Signed-off-by: Jens Baumgart <jens.baumgart@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/CherryPickHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/CherryPickHandler.java
@@ -12,14 +12,20 @@
 
 package org.eclipse.egit.ui.internal.history.command;
 
+import java.util.Map;
+
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.MultiStatus;
+import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jgit.api.CherryPickResult;
-import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.CherryPickResult.CherryPickStatus;
+import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.merge.ResolveMerger.MergeFailureReason;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.swt.widgets.Shell;
 
@@ -27,6 +33,8 @@ import org.eclipse.swt.widgets.Shell;
  * Executes the CherryPick
  */
 public class CherryPickHandler extends AbstractHistoryCommandHandler {
+	private static final String SPACE = " ";  //$NON-NLS-1$
+
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		RevCommit commit = (RevCommit) getSelection(getPage()).getFirstElement();
 		RevCommit newHead;
@@ -45,14 +53,47 @@ public class CherryPickHandler extends AbstractHistoryCommandHandler {
 		} catch (Exception e) {
 			throw new ExecutionException(UIText.CherryPickOperation_InternalError, e);
 		}
-		if (newHead == null)
-			if (cherryPickResult.getStatus() == CherryPickStatus.CONFLICTING)
+		if (newHead == null) {
+			CherryPickStatus status = cherryPickResult.getStatus();
+			switch (status) {
+			case CONFLICTING:
 				MessageDialog.openWarning(parent,
 						UIText.CherryPickHandler_CherryPickConflictsTitle,
 						UIText.CherryPickHandler_CherryPickConflictsMessage);
-			else
-				throw new ExecutionException(UIText.CherryPickOperation_Failed);
-
+				break;
+			case FAILED:
+				IStatus details = getErrorList(cherryPickResult.getFailingPaths());
+				Activator.showErrorStatus(
+						UIText.CherryPickHandler_CherryPickFailedMessage, details);
+				break;
+			case OK:
+				break;
+			}
+		}
 		return null;
+	}
+
+	private IStatus getErrorList(Map<String, MergeFailureReason> failingPaths) {
+		MultiStatus result = new MultiStatus(Activator.getPluginId(),
+				IStatus.ERROR,
+				UIText.CherryPickHandler_CherryPickFailedMessage, null);
+		for (String path : failingPaths.keySet()) {
+			String reason = getReason(failingPaths.get(path));
+			String errorMessage = path + SPACE + reason;
+			result.add(Activator.createErrorStatus(errorMessage));
+		}
+		return result;
+	}
+
+	private String getReason(MergeFailureReason mergeFailureReason) {
+		switch (mergeFailureReason) {
+		case COULD_NOT_DELETE:
+			return UIText.CherryPickHandler_CouldNotDeleteFile;
+		case DIRTY_INDEX:
+			return UIText.CherryPickHandler_IndexDirty;
+		case DIRTY_WORKTREE:
+			return UIText.CherryPickHandler_WorktreeDirty;
+		}
+		return "unknown"; //$NON-NLS-1$
 	}
 }
