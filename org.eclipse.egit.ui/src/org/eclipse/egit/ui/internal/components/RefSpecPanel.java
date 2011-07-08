cde04742024918735b53869746bf6589dc64561d
Stefan Lay <stefan.lay@sap.com>
2011-07-08 10:50:42 +0200

    Fix wrong destination ref proposal in PushWizard
    When the user entered "tags/myTag" into the source field the PushWizard
    proposed "tags/myTag" in the destination field. This is then expanded to
    "refs/tags/tags/myTag" by JGit. The analogous behavior was there if the
    user entered heads/myBranch.
    
    This commit fixes the bug; now only the name after "tags/" resp.
    "heads/" is proposed in the destination field.
    
    
    Bug: 341069
    Change-Id: I108ff09ef39c3aefa5676c7724af2e42e3600172
    Signed-off-by: Stefan Lay <stefan.lay@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RefSpecPanel.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RefSpecPanel.java
@@ -1358,8 +1358,13 @@ public class RefSpecPanel {
 
 		// dst is empty, src is ref or wildcard, so we can rewrite it as user
 		// would perhaps
-		if (pushSpecs)
-			creationDstCombo.setText(src);
+		if (pushSpecs) {
+			String newDst = src;
+			newDst = deletePrefixes(src,
+					Constants.R_TAGS.substring(Constants.R_REFS.length()),
+					Constants.R_HEADS.substring(Constants.R_REFS.length()));
+			creationDstCombo.setText(newDst);
+		}
 		else {
 			for (final RefSpec spec : predefinedConfigured) {
 				if (spec.matchSource(src)) {
@@ -1375,6 +1380,17 @@ public class RefSpecPanel {
 				creationDstCombo.setText(newDst);
 			}
 		}
+	}
+
+	private String deletePrefixes(String ref, String... prefixes) {
+		String result = ref;
+		for (String prefix : prefixes) {
+			if (result.startsWith(prefix)) {
+				result = result.substring(prefix.length());
+				return result;
+			}
+		}
+		return result;
 	}
 
 	private void tryAutoCompleteDstToSrc() {
