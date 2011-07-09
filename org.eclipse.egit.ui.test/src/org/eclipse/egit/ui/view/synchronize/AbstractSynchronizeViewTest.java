76c81cc200ac01032972cc5db7cf1b111269e01b
Dariusz Luksza <dariusz@luksza.org>
2011-07-10 01:48:58 +0200

    [sync] Fixes for GitModelSynchronize
    Fixes NPE in GitModelSYnchronize when active workbench window cannot be
    determined. Also fixes launching synchronization action when prefetching changes is disabled.
    
    Additionally disables prefetching changes in SWTBot test cases.
    
    Change-Id: Ic5cbabd0ac4e3445ba12b3452cfebe3d2cfda5a5
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/synchronize/AbstractSynchronizeViewTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/synchronize/AbstractSynchronizeViewTest.java
@@ -39,6 +39,7 @@ import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.egit.core.synchronize.dto.GitSynchronizeData;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.JobFamilies;
+import org.eclipse.egit.ui.UIPreferences;
 import org.eclipse.egit.ui.common.LocalRepositoryTestCase;
 import org.eclipse.egit.ui.internal.synchronize.GitModelSynchronize;
 import org.eclipse.egit.ui.test.Eclipse;
@@ -79,6 +80,8 @@ public abstract class AbstractSynchronizeViewTest extends
 	@Before public void setupViews() {
 		bot.perspectiveById("org.eclipse.jdt.ui.JavaPerspective").activate();
 		bot.viewByTitle("Package Explorer").show();
+		Activator.getDefault().getPreferenceStore()
+				.setValue(UIPreferences.SYNC_VIEW_FETCH_BEFORE_LAUNCH, false);
 	}
 
 	@BeforeClass public static void setupEnvironment() throws Exception {
