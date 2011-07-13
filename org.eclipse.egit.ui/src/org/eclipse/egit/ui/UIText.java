3275d485dd534f2150c0b24093322e04bf2512ff
Kevin Sawicki <kevin@github.com>
2011-07-13 13:41:24 -0700

    Add externalized string for history page preferences link.
    The issue was that the CommitDialog now displays a toolbar
    drop-down menu and the GitHistoryPage displays a hyperlink
    and they can't both use the same string since the history
    page requires a wrapping '<a>' tag.
    
    Change-Id: I2494ce39df8dae7fa64b564f29bd2d9ed882c2a8
    Signed-off-by: Kevin Sawicki <kevin@github.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -445,6 +445,9 @@ public class UIText extends NLS {
 	public static String GitHistoryPage_OpenMenuLabel;
 
 	/** */
+	public static String GitHistoryPage_PreferencesLink;
+
+	/** */
 	public static String GitHistoryPage_ProjectType;
 
 	/** */
