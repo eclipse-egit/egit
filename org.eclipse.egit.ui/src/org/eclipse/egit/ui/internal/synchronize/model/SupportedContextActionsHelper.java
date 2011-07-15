fbc1a75f682154c7e875d61b831d3fc29f49e3f1
Benjamin Muskalla <benjamin.muskalla@tasktop.com>
2011-07-15 18:41:58 +0200

    Integrate merge tool into synchronize view
    Bug: 343595
    
    Change-Id: I970e47e4063e780274e073a38574ff99bf1a37a6
    Signed-off-by: Benjamin Muskalla <benjamin.muskalla@tasktop.com>
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/model/SupportedContextActionsHelper.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/model/SupportedContextActionsHelper.java
@@ -36,6 +36,16 @@ public final class SupportedContextActionsHelper {
 
 	/**
 	 * @param object
+	 * @return {@code true} if the merge tool can be used on
+	 *         {@code object}
+	 */
+	public static boolean canUseMergeTool(GitModelObject object) {
+		return object instanceof GitModelWorkingFile
+				|| object instanceof GitModelCacheFile;
+	}
+
+	/**
+	 * @param object
 	 * @return {@code true} if stage/add operation can be performed on
 	 *         {@code object}
 	 */
