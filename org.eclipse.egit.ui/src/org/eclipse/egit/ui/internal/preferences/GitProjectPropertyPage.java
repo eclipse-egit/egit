4fa30d75a0d4127c7b2ff7106caa087934651fa4
Benjamin Muskalla <benjamin.muskalla@tasktop.com>
2011-07-16 02:14:57 +0200

    Link commit in Properties page
    Adds a link to the properties page for the HEAD reference and opens the
    Commit editor upon click.
    
    Change-Id: I3fb6c492ebf10b74cd04d80322e09c9ab7201fab
    Signed-off-by: Benjamin Muskalla <benjamin.muskalla@tasktop.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/preferences/GitProjectPropertyPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/preferences/GitProjectPropertyPage.java
@@ -5,6 +5,9 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *    Benjamin Muskalla (Tasktop Technologies Inc) - Hyperlinking of HEAD
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.preferences;
 
@@ -14,7 +17,16 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.egit.ui.UIText;
+import org.eclipse.egit.ui.internal.commit.CommitEditor;
+import org.eclipse.egit.ui.internal.commit.RepositoryCommit;
 import org.eclipse.egit.ui.internal.trace.GitTraceLocation;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.resource.JFaceColors;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -24,8 +36,9 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.dialogs.PropertyPage;
-import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.Repository;
+import org.eclipse.ui.forms.events.HyperlinkAdapter;
+import org.eclipse.ui.forms.events.HyperlinkEvent;
+import org.eclipse.ui.forms.widgets.Hyperlink;
 
 /**
  * Property page to be shown in project properties, if project is shared using
@@ -37,7 +50,7 @@ public class GitProjectPropertyPage extends PropertyPage {
 
 	private Text branch;
 
-	private Text id;
+	private Hyperlink headLink;
 
 	private Text state;
 
@@ -63,7 +76,7 @@ public class GitProjectPropertyPage extends PropertyPage {
 		gitDir = createLabeledReadOnlyText(composite, UIText.GitProjectPropertyPage_LabelGitDir);
 		workDir = createLabeledReadOnlyText(composite, UIText.GitProjectPropertyPage_LabelWorkdir);
 		branch = createLabeledReadOnlyText(composite, UIText.GitProjectPropertyPage_LabelBranch);
-		id = createLabeledReadOnlyText(composite, UIText.GitProjectPropertyPage_LabelId);
+		headLink = createHeadHyperLink(composite, UIText.GitProjectPropertyPage_LabelId);
 		state = createLabeledReadOnlyText(composite, UIText.GitProjectPropertyPage_LabelState);
 
 		// Get the project that is the source of this property page
@@ -93,7 +106,7 @@ public class GitProjectPropertyPage extends PropertyPage {
 		return composite;
 	}
 
-	private void fillValues(Repository repository) throws IOException {
+	private void fillValues(final Repository repository) throws IOException {
 		gitDir.setText(repository.getDirectory().getAbsolutePath());
 		branch.setText(repository.getBranch());
 		workDir.setText(repository.getWorkTree().getAbsolutePath());
@@ -104,13 +117,37 @@ public class GitProjectPropertyPage extends PropertyPage {
 				.resolve(repository.getFullBranch());
 		if (objectId == null) {
 			if (repository.getAllRefs().size() == 0)
-				id.setText(UIText.GitProjectPropertyPage_ValueEmptyRepository);
+				headLink.setText(UIText.GitProjectPropertyPage_ValueEmptyRepository);
 			else
-				id.setText(UIText.GitProjectPropertyPage_ValueUnbornBranch);
-		} else
-			id.setText(objectId.name());
+				headLink.setText(UIText.GitProjectPropertyPage_ValueUnbornBranch);
+		} else {
+			headLink.setText(objectId.name());
+			headLink.setUnderlined(true);
+			headLink.setFont(JFaceResources.getDialogFont());
+			headLink.setForeground(JFaceColors.getHyperlinkText(headLink
+					.getDisplay()));
+			headLink.addHyperlinkListener(new HyperlinkAdapter() {
+				@Override
+				public void linkActivated(HyperlinkEvent e) {
+					CommitEditor.openQuiet(getCommit(repository, objectId));
+				}
+			});
+		}
 	}
 
+	private RepositoryCommit getCommit(Repository repository, ObjectId objectId) {
+		RevWalk walk = new RevWalk(repository);
+		try {
+			RevCommit commit = walk.parseCommit(objectId);
+			for (RevCommit parent : commit.getParents())
+				walk.parseBody(parent);
+			return new RepositoryCommit(repository, commit);
+		} catch (IOException e) {
+			return null;
+		} finally {
+			walk.release();
+		}
+	}
 	/**
 	 * Create a read only text field with a label
 	 *
@@ -120,23 +157,41 @@ public class GitProjectPropertyPage extends PropertyPage {
 	 *            text for label
 	 * @return the new read only text field
 	 */
-	protected Text createLabeledReadOnlyText(Composite parent,
+	private Text createLabeledReadOnlyText(Composite parent,
 			final String labelText) {
+		createLabel(parent, labelText);
+
+		Text text = createText(parent);
+		return text;
+	}
+
+	private Hyperlink createHeadHyperLink(Composite composite,
+			String labelText) {
+		createLabel(composite, labelText);
+
+		Hyperlink hyperlink = new Hyperlink(composite, SWT.NONE);
+		hyperlink.setLayoutData(GridDataFactory.fillDefaults().create());
+		return hyperlink;
+	}
+
+	private Text createText(Composite parent) {
+		GridData data = new GridData();
+		Text text = new Text(parent, SWT.LEFT | SWT.READ_ONLY);
+		text.setBackground(Display.getDefault().getSystemColor(
+				SWT.COLOR_WIDGET_BACKGROUND));
+		data.horizontalSpan = 1;
+		data.horizontalAlignment = GridData.FILL;
+		text.setLayoutData(data);
+		return text;
+	}
+
+	private void createLabel(Composite parent, final String labelText) {
 		Label label = new Label(parent, SWT.LEFT);
 		label.setText(labelText);
 		GridData data = new GridData();
 		data.horizontalSpan = 1;
 		data.horizontalAlignment = GridData.FILL;
 		label.setLayoutData(data);
-
-		Text text = new Text(parent, SWT.LEFT | SWT.READ_ONLY);
-		text.setBackground(Display.getDefault().getSystemColor(
-				SWT.COLOR_WIDGET_BACKGROUND));
-		data = new GridData();
-		data.horizontalSpan = 1;
-		data.horizontalAlignment = GridData.FILL;
-		text.setLayoutData(data);
-		return text;
 	}
 
 }
