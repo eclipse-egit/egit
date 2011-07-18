86f6e33d63c668a0f8a6c3938b14884daf4fc2c5
Remy Suen <remysuen@ca.ibm.com>
2011-07-18 15:55:35 -0400

    Do not downcast provided evaluation context as an EvaluationContext
    The context that's passed into handlers are not guaranteed to be an
    EvaluationContext so performing an instanceof check for that type
    may potentially go nowhere. The fix is to use HandlerUtil's
    getVariable(Object, String) method for retrieving variables from the
    context. This lets the framework handle the processing of the
    context instead of having EGit make arbitrary assumptions about the
    type of the provided context.
    
    Bug: 352382
    Change-Id: I835f5629ba6e3b7853b2233e1ddb95453e01332f
    Signed-off-by: Remy Suen <remysuen@ca.ibm.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/RepositoriesViewCommandHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/RepositoriesViewCommandHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 SAP AG.
+ * Copyright (c) 2010, 2011 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -62,25 +62,23 @@ abstract class RepositoriesViewCommandHandler<T> extends AbstractHandler {
 	}
 
 	protected void enableWhenRepositoryHaveHead(Object evaluationContext) {
-		if (evaluationContext instanceof EvaluationContext) {
-			EvaluationContext context = (EvaluationContext) evaluationContext;
-			Object selection = context.getVariable(ISources.ACTIVE_CURRENT_SELECTION_NAME);
-			if (selection instanceof TreeSelection) {
-				TreeSelection treeSelection = (TreeSelection) selection;
-				Object firstElement = treeSelection.getFirstElement();
-				if (firstElement instanceof RepositoryTreeNode) {
-					RepositoryTreeNode<?> treeNode = (RepositoryTreeNode<?>) firstElement;
-					Repository repo = treeNode.getRepository();
-					boolean enabled = false;
-					try {
-						Ref ref = repo.getRef(Constants.HEAD);
-						enabled = ref != null && ref.getObjectId() != null;
-					} catch (IOException e) {
-						enabled = false;
-					}
-					setBaseEnabled(enabled);
-					return;
+		Object selection = HandlerUtil.getVariable(evaluationContext,
+				ISources.ACTIVE_CURRENT_SELECTION_NAME);
+		if (selection instanceof TreeSelection) {
+			TreeSelection treeSelection = (TreeSelection) selection;
+			Object firstElement = treeSelection.getFirstElement();
+			if (firstElement instanceof RepositoryTreeNode) {
+				RepositoryTreeNode<?> treeNode = (RepositoryTreeNode<?>) firstElement;
+				Repository repo = treeNode.getRepository();
+				boolean enabled = false;
+				try {
+					Ref ref = repo.getRef(Constants.HEAD);
+					enabled = ref != null && ref.getObjectId() != null;
+				} catch (IOException e) {
+					enabled = false;
 				}
+				setBaseEnabled(enabled);
+				return;
 			}
 		}
 
