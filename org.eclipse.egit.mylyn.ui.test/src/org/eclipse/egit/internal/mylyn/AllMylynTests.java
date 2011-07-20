32a4f1f1e551897453137a7cf301ce546dde7c64
Benjamin Muskalla <benjamin.muskalla@tasktop.com>
2011-07-20 12:58:08 +0200

    Support hyperlink detector for Commit ids
    In order to have better navigation between code and tasks, this
    hyperlink detector recognizes commit ids inside the task descriptions
    and let's the user open the corresponding commit in the Commit editor.
    
    Change-Id: Ic60d11a45b8cb8cc9383e4120a84eae2f1859e32
    Signed-off-by: Benjamin Muskalla <benjamin.muskalla@tasktop.com>

---
+++ org.eclipse.egit.mylyn.ui.test/src/org/eclipse/egit/internal/mylyn/AllMylynTests.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Benjamin Muskalla and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Benjamin Muskalla <benjamin.muskalla@tasktop.com> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.egit.internal.mylyn;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CommitHyperlinkDetectorTest.class })
+public class AllMylynTests {
+	// empty class, don't need anything here
+}
