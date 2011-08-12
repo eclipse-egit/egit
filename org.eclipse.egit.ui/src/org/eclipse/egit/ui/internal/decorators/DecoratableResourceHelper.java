0962bc7587ae5984176b1f41570eea33140ddfcb
Robin Stocker <robin@nibor.org>
2011-08-12 21:14:17 +0200

    [RFC] Show detailed branch status in label decoration of project
    The branch status is shown after the branch name in the decoration if
    the current branch is different from the remote tracking branch. This is
    useful as a visual indication that the branch needs to be pushed (or
    merged/rebased).
    
    If there are commits on the branch that are not on the tracking branch,
    an upwards arrow (↑) followed by the number of commits is shown. This
    can be read as "the branch is N commits ahead" or "N commits need to be
    pushed".
    
    If there are commits on the tracking branch that are not on the local
    branch, a downwards arrow (↓) is shown, followed by the number of
    commits. This can be read as "the branch is N commits behind" or "N
    commits need to be merged".
    
    For example, if there are 2 unpushed commits on master, the project is
    now shown like this:
    
      project [repo master ↑2]
    
    If there are both unpushed and unmerged commits (the branches have
    diverged), it's shown like this:
    
      project [repo master ↑2 ↓1]
    
    In C Git, information about the tracking branch is displayed in "git
    status" output. EGit currently doesn't have any indication that a branch
    needs to be pushed, apart from the labels in History View. When there
    are multiple repositories involved, it can easily happen that a push is
    forgotten. This change helps prevent that.
    
    Change-Id: I1e1caca561d1b0a0c194bfc42e64b698f42c6e6a
    Signed-off-by: Robin Stocker <robin@nibor.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/decorators/DecoratableResourceHelper.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/decorators/DecoratableResourceHelper.java
@@ -25,11 +25,16 @@ import org.eclipse.egit.ui.internal.decorators.IDecoratableResource.Staged;
 import org.eclipse.jgit.dircache.DirCache;
 import org.eclipse.jgit.dircache.DirCacheEntry;
 import org.eclipse.jgit.dircache.DirCacheIterator;
+import org.eclipse.jgit.lib.ConfigConstants;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.FileMode;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.RepositoryState;
+import org.eclipse.jgit.lib.StoredConfig;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.revwalk.filter.RevFilter;
 import org.eclipse.jgit.treewalk.TreeWalk;
 import org.eclipse.jgit.treewalk.WorkingTreeIterator;
 
@@ -215,5 +220,83 @@ public class DecoratableResourceHelper {
 			return UIText.DecoratableResourceHelper_noHead;
 
 		return repository.getBranch();
+	}
+
+	static String getBranchStatus(Repository repo) throws IOException {
+		String localName = repo.getBranch();
+		if (localName == null)
+			return null;
+
+		String trackingName = getTrackingBranch(repo, localName);
+		if (trackingName == null)
+			return null;
+
+		Ref tracking = repo.getRef(trackingName);
+		if (tracking == null)
+			return null;
+
+		Ref local = repo.getRef(localName);
+		if (local == null)
+			return null;
+
+		RevWalk walk = new RevWalk(repo.newObjectReader());
+
+		RevCommit localCommit = walk.parseCommit(local.getObjectId());
+		RevCommit trackingCommit = walk.parseCommit(tracking.getObjectId());
+
+		walk.setRevFilter(RevFilter.MERGE_BASE);
+		walk.markStart(localCommit);
+		walk.markStart(trackingCommit);
+		RevCommit mergeBase = walk.next();
+		if (mergeBase == null)
+			return null;
+
+		int localCount = count(walk, localCommit, mergeBase);
+		int trackingCount = count(walk, trackingCommit, mergeBase);
+
+		if (localCount == 0 && trackingCount == 0)
+			return null;
+
+		StringBuilder sb = new StringBuilder();
+		if (localCount != 0) {
+			sb.append(" "); //$NON-NLS-1$
+			sb.append("↑"); //$NON-NLS-1$
+			sb.append(localCount);
+		}
+		if (trackingCount != 0) {
+			sb.append(" "); //$NON-NLS-1$
+			sb.append("↓"); //$NON-NLS-1$
+			sb.append(trackingCount);
+		}
+		return sb.toString();
+	}
+
+	// TODO: This should probably be in the Repository class
+	private static String getTrackingBranch(Repository repo, String branchName) {
+		StoredConfig config = repo.getConfig();
+		String remoteName = config.getString(ConfigConstants.CONFIG_BRANCH_SECTION,
+				branchName, ConfigConstants.CONFIG_KEY_REMOTE);
+		String mergeRef = config.getString(ConfigConstants.CONFIG_BRANCH_SECTION,
+				branchName, ConfigConstants.CONFIG_KEY_MERGE);
+		if (remoteName == null || mergeRef == null)
+			return null;
+
+		String remoteBranchName = Repository.shortenRefName(mergeRef);
+		String trackingRefName = Constants.R_REMOTES +
+				remoteName + "/" + remoteBranchName;  //$NON-NLS-1$
+		return trackingRefName;
+	}
+
+	// TODO: Move somewhere appropriate
+	private static int count(RevWalk walk, RevCommit start, RevCommit base) throws IOException {
+		walk.reset();
+		walk.setRevFilter(RevFilter.ALL);
+		walk.markStart(start);
+		walk.markUninteresting(base);
+
+		int count = 0;
+		for (RevCommit c = walk.next(); c != null; c = walk.next())
+			count++;
+		return count;
 	}
 }
