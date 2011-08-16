fb845e848a69ae36c6b4d3f283e08daf189e60e7
Dariusz Luksza <dariusz@luksza.org>
2011-08-16 09:14:40 +0200

    [sync] Improve performance
    Adds thin cache that contains meta data (like changed object id's and
    change type) from git. This cache is filled in background process just
    before synchronization is launched and then it is used for quick
    obtaining list of members for particular folder.
    
    During my tests this implementation reduce time needed to compare linux
    kernel source (version 2.6.38-rc2 against 2.6.36) in less then 1min
    (usually 55s), when implementation from 1.0 needs 1m 40s.
    
    Additionally some tests were updated.
    
    This change requires also jgit change
    I5a680a73e1cffa18ade3402cc86008f46c1da1f1
    
    Change-Id: I7ddb21e7ff54333dd6d7ace3209bbcf83da2b219
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/JobFamilies.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/JobFamilies.java
@@ -103,4 +103,7 @@ public class JobFamilies {
 	 */
 	public static final Object REMOVE_FROM_INDEX = new Object();
 
+	/** Fetch data from git job */
+	public static final Object SYNCHRONIZE_READ_DATA = new Object();
+
 }
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -3940,6 +3940,9 @@ public class UIText extends NLS {
 	/** */
 	public static String CustomPromptDialog_information_about;
 
+	/** */
+	public static String GitModelSynchonize_fetchGitDataJobName;
+
 	static {
 		initializeMessages(BUNDLE_NAME, UIText.class);
 	}
