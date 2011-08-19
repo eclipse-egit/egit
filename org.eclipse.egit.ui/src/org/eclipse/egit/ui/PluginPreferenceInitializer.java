58382400982dcc9858be3fae88077e5b456630af
Kevin Sawicki <kevin@github.com>
2011-08-19 15:40:22 -0700

    [stagingView] Enable file name mode by default.
    This mode handles long paths better and can still be disabled
    in the view drop-down menu if desired.
    
    Change-Id: Ia39443df6557e8d3fe2beec761ac0f0fdb3753fd
    Signed-off-by: Kevin Sawicki <kevin@github.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/PluginPreferenceInitializer.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/PluginPreferenceInitializer.java
@@ -91,6 +91,7 @@ public class PluginPreferenceInitializer extends AbstractPreferenceInitializer {
 		store.setDefault(UIPreferences.HISTORY_SHOW_TAG_SEQUENCE, false);
 		store.setDefault(UIPreferences.BLAME_IGNORE_WHITESPACE, false);
 		store.setDefault(UIPreferences.REMOTE_CONNECTION_TIMEOUT, 30 /* seconds */);
+		store.setDefault(UIPreferences.STAGING_VIEW_FILENAME_MODE, true);
 	}
 
 }
