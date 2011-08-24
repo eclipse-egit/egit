be138865dc994d70f90c7b198d1c5b711ad685b5
Benjamin Muskalla <benjamin.muskalla@tasktop.com>
2011-08-24 19:03:57 +0200

    Properly handle TextSelections in Compare Editors
    Currently most menu items in the Team Menu have no effect inside Compare
    editors as we explicitly need to ask for IFile as their inputs don't
    adapt to IResource.
    
    Change-Id: I0ac6085b5d0d843bc179cddd912676fd07bed498
    Signed-off-by: Benjamin Muskalla <benjamin.muskalla@tasktop.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoriesView.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoriesView.java
@@ -570,6 +570,10 @@ public class RepositoriesView extends CommonNavigator {
 				}
 			}
 		}
+		if(context.getInput() instanceof IFileEditorInput) {
+			IFileEditorInput input = (IFileEditorInput) context.getInput();
+			showResource(input.getFile());
+		}
 		return false;
 	}
 
