dc77aadb1db4e7ba1e1c9cda65c030d1135bc288
Benjamin Muskalla <benjamin.muskalla@tasktop.com>
2011-08-24 16:29:56 +0200

    Fix CCE comparing files
    Happens when you open a compare on a file in the Git Changeset mode and
    try to open the popup menu inside the compare editor.
    
    Bug: 354074
    Change-Id: Iaa2e9a4feb89063be510cdb5f5f77a9abc2f8ca0
    Signed-off-by: Benjamin Muskalla <benjamin.muskalla@tasktop.com>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/internal/synchronize/model/GitModelBlobTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/internal/synchronize/model/GitModelBlobTest.java
@@ -12,12 +12,16 @@ import static org.eclipse.compare.structuremergeviewer.Differencer.LEFT;
 import static org.eclipse.jgit.lib.Constants.HEAD;
 import static org.eclipse.jgit.lib.ObjectId.fromString;
 import static org.eclipse.jgit.lib.ObjectId.zeroId;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.jgit.lib.ObjectId;
 import org.junit.BeforeClass;
@@ -219,6 +223,18 @@ public class GitModelBlobTest extends GitModelTestCase {
 
 		// then
 		assertFalse(actual);
+	}
+
+	@Test public void shouldActAsResourceProvider()
+			throws Exception {
+		// given
+		GitModelBlob left = createGitModelBlob();
+
+		// then
+		IFile file = ResourcesPlugin.getWorkspace().getRoot().getProject(PROJ1)
+				.getFile(new Path("folder/test.txt"));
+		IPath leftLocation = left.getResource().getLocation();
+		assertEquals(file.getLocation(), leftLocation);
 	}
 
 	@BeforeClass public static void setupEnvironment() throws Exception {
