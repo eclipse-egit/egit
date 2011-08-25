eclipse-egit/egit

commit 521e41c93c6b601e8d1d465b9699986bc8608628

[sync] Simplify implementation of git remote objects
Simplify implementation of git remote objects by using
CachedResourceVariant from team framework.

Also do some cleanup in core tests.

Change-Id: I32bb63c96676f8213b800558e7d52eb67ae30b2a
Signed-off-by: Dariusz Luksza <dariusz@luksza.org>
Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/GitRemoteFile.java
@@ -1,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (C) 2011, Dariusz Luksza <dariusz@luksza.org>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.core.synchronize;
+
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+
+class GitRemoteFile extends GitRemoteResource {
+
+	GitRemoteFile(Repository repo, RevCommit revCommit,
+			ObjectId objectId, String path) {
+		super(repo, revCommit, objectId, path);
+	}
+
+	public boolean isContainer() {
+		return false;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this)
+			return true;
+
+		if (obj instanceof GitRemoteFile) {
+			GitRemoteFile that = (GitRemoteFile) obj;
+
+			return getPath().equals(that.getPath())
+					&& getObjectId().equals(that.getObjectId());
+		}
+
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return getObjectId().hashCode() ^ getPath().hashCode();
+	}
+
+}

org.eclipse.egit.core.test/src/org/eclipse/egit/core/synchronize/GitResourceVariantComparatorTest.java
@@ -34,7 +34,6 @@ import org.eclipse.egit.core.test.GitTestCase;
 import org.eclipse.egit.core.test.TestRepository;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.treewalk.TreeWalk;
@@ -98,7 +97,7 @@ public class GitResourceVariantComparatorTest extends GitTestCase {
 
 	@Test
 	@SuppressWarnings("boxing")
-	public void shouldReturnFalseWhenRemoteDoesNotExist2() throws Exception{
+	public void shouldReturnFalseWhenRemoteDoesNotExist2() throws Exception {
 		// when
 		GitResourceVariantComparator grvc = new GitResourceVariantComparator(
 				null);
@@ -106,8 +105,7 @@ public class GitResourceVariantComparatorTest extends GitTestCase {
 		// given
 		IResource local = mock(IResource.class);
 		when(local.exists()).thenReturn(false);
-		IResourceVariant remote = new GitFolderResourceVariant(repo, null,
-				ObjectId.zeroId(), "./");
+		IResourceVariant remote = new GitRemoteFolder(repo, null, null, "./");
 
 		// then
 		assertFalse(grvc.compare(local, remote));
@@ -161,7 +159,7 @@ public class GitResourceVariantComparatorTest extends GitTestCase {
 				"initial commit");
 		String path = Repository.stripWorkDir(repo.getWorkTree(), file);
 
-		GitFolderResourceVariant remote = new GitFolderResourceVariant(repo, null,
+		GitRemoteFolder remote = new GitRemoteFolder(repo, null,
 				commit.getTree(), path);
 
 		// then
@@ -194,7 +192,7 @@ public class GitResourceVariantComparatorTest extends GitTestCase {
 		when(local.exists()).thenReturn(true);
 		when(local.getLocation()).thenReturn(iPath);
 
-		GitFolderResourceVariant remote = new GitFolderResourceVariant(repo, null,
+		GitRemoteFolder remote = new GitRemoteFolder(repo, null,
 				commit.getTree(), path);
 
 		// then
@@ -463,9 +461,9 @@ public class GitResourceVariantComparatorTest extends GitTestCase {
 				"second commit");
 		String path = Repository.stripWorkDir(repo.getWorkTree(), file);
 
-		GitBlobResourceVariant base = new GitBlobResourceVariant(repo, null,
+		GitRemoteFile base = new GitRemoteFile(repo, null,
 				baseCommit.getTree(), path);
-		GitBlobResourceVariant remote = new GitBlobResourceVariant(repo, null,
+		GitRemoteFile remote = new GitRemoteFile(repo, null,
 				remoteCommit.getTree(), path);
 
 		// then
@@ -494,9 +492,9 @@ public class GitResourceVariantComparatorTest extends GitTestCase {
 				"second commit");
 		String path = Repository.stripWorkDir(repo.getWorkTree(), file);
 
-		GitBlobResourceVariant base = new GitBlobResourceVariant(repo, null,
+		GitRemoteFile base = new GitRemoteFile(repo, null,
 				baseCommit.getTree(), path);
-		GitBlobResourceVariant remote = new GitBlobResourceVariant(repo, null,
+		GitRemoteFile remote = new GitRemoteFile(repo, null,
 				remoteCommit.getTree(), path);
 
 		// then
@@ -524,9 +522,9 @@ public class GitResourceVariantComparatorTest extends GitTestCase {
 		String filePath = Repository.stripWorkDir(repo.getWorkTree(), file);
 		String folderPath = Repository.stripWorkDir(repo.getWorkTree(),
 				new File(file.getParent()));
-		GitBlobResourceVariant base = new GitBlobResourceVariant(repo, null,
+		GitRemoteFile base = new GitRemoteFile(repo, null,
 				commit.getTree(), filePath);
-		GitFolderResourceVariant remote = new GitFolderResourceVariant(repo, null,
+		GitRemoteFolder remote = new GitRemoteFolder(repo, null,
 				commit.getTree(), folderPath);
 
 		// then
@@ -555,9 +553,9 @@ public class GitResourceVariantComparatorTest extends GitTestCase {
 		String folderPath = Repository.stripWorkDir(repo.getWorkTree(),
 				new File(file.getParent()));
 
-		GitFolderResourceVariant base = new GitFolderResourceVariant(repo, null,
+		GitRemoteFolder base = new GitRemoteFolder(repo, null,
 				commit.getTree(), folderPath);
-		GitBlobResourceVariant remote = new GitBlobResourceVariant(repo, null,
+		GitRemoteFile remote = new GitRemoteFile(repo, null,
 				commit.getTree(), filePath);
 
 		// then
@@ -594,11 +592,11 @@ public class GitResourceVariantComparatorTest extends GitTestCase {
 		tw.next();
 		tw.enterSubtree(); // enter project node
 		tw.next();
-		GitFolderResourceVariant base = new GitFolderResourceVariant(repo, null,
+		GitRemoteFolder base = new GitRemoteFolder(repo, null,
 				tw.getObjectId(nth), tw.getNameString());
 
 		tw.next();
-		GitFolderResourceVariant remote = new GitFolderResourceVariant(repo, null,
+		GitRemoteFolder remote = new GitRemoteFolder(repo, null,
 				tw.getObjectId(nth), tw.getNameString());
 
 		// then
@@ -627,9 +625,9 @@ public class GitResourceVariantComparatorTest extends GitTestCase {
 		String path1 = Repository.stripWorkDir(repo.getWorkTree(), new File(
 				file1.getParent()));
 
-		GitFolderResourceVariant base = new GitFolderResourceVariant(repo, null,
+		GitRemoteFolder base = new GitRemoteFolder(repo, null,
 				commit.getTree(), path1);
-		GitFolderResourceVariant remote = new GitFolderResourceVariant(repo, null,
+		GitRemoteFolder remote = new GitRemoteFolder(repo, null,
 				commit.getTree(), path1);
 
 		// then
@@ -656,10 +654,10 @@ public class GitResourceVariantComparatorTest extends GitTestCase {
 				file, "bc", "second commit");
 
 		String path = Repository.stripWorkDir(repo.getWorkTree(), file);
-		GitBlobResourceVariant base = new GitBlobResourceVariant(repo, null,
+		GitRemoteFile base = new GitRemoteFile(repo, null,
 				baseCommit.getTree(), path);
 
-		GitBlobResourceVariant remote = new GitBlobResourceVariant(repo, null,
+		GitRemoteFile remote = new GitRemoteFile(repo, null,
 				remoteCommit.getTree(), path);
 
 		// then
@@ -669,6 +667,7 @@ public class GitResourceVariantComparatorTest extends GitTestCase {
 	/**
 	 * Comparing two remote files that have the same git ObjectId should return
 	 * true.
+	 *
 	 * @throws Exception
 	 */
 	@Test
@@ -683,11 +682,10 @@ public class GitResourceVariantComparatorTest extends GitTestCase {
 				"a", "initial commit");
 
 		String path = Repository.stripWorkDir(repo.getWorkTree(), file);
-		GitBlobResourceVariant base = new GitBlobResourceVariant(repo, null,
-				commit.getTree(), path);
+		GitRemoteFile base = new GitRemoteFile(repo, null, commit.getTree(),
+				path);
 
-		GitBlobResourceVariant remote = new GitBlobResourceVariant(repo, null,
-				commit.getTree(), path);
+		GitRemoteFile remote = new GitRemoteFile(repo, null, commit.getTree(), path);
 
 		// then
 		assertTrue(grvc.compare(base, remote));

org.eclipse.egit.core.test/src/org/eclipse/egit/core/synchronize/GitResourceVariantTreeSubscriberTest.java
@@ -173,7 +173,7 @@ public class GitResourceVariantTreeSubscriberTest extends GitTestCase {
 		IResourceVariant resourceVariant = remoteTree
 				.getResourceVariant(mainJava);
 		assertNotNull(resourceVariant);
-		assertTrue(resourceVariant instanceof GitResourceVariant);
+		assertTrue(resourceVariant instanceof GitRemoteResource);
 		assertEquals(fileId.getName(), resourceVariant.getContentIdentifier());
 	}
 
@@ -207,7 +207,7 @@ public class GitResourceVariantTreeSubscriberTest extends GitTestCase {
 		IResourceVariant resourceVariant = baseTree
 				.getResourceVariant(resource);
 		assertNotNull(resourceVariant);
-		assertTrue(resourceVariant instanceof GitResourceVariant);
+		assertTrue(resourceVariant instanceof GitRemoteResource);
 		return resourceVariant;
 	}
 

org.eclipse.egit.core.test/src/org/eclipse/egit/core/synchronize/GitResourceVariantTreeSubscriberTest1.java
@@ -1,113 +1,0 @@
-/*******************************************************************************
- * Copyright (C) 2010, Dariusz Luksza <dariusz@luksza.org>
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *******************************************************************************/
-package org.eclipse.egit.core.synchronize;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-import java.io.File;
-import java.io.IOException;
-
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.egit.core.project.RepositoryMapping;
-import org.eclipse.egit.core.synchronize.dto.GitSynchronizeData;
-import org.eclipse.egit.core.synchronize.dto.GitSynchronizeDataSet;
-import org.eclipse.egit.core.test.GitTestCase;
-import org.eclipse.egit.core.test.TestRepository;
-import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.team.core.TeamException;
-import org.eclipse.team.core.variants.IResourceVariant;
-import org.eclipse.team.core.variants.IResourceVariantTree;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-public class GitResourceVariantTreeSubscriberTest1 extends GitTestCase {
-
-	private Repository repo;
-
-	private IProject iProject;
-
-	private TestRepository testRepo;
-
-	@Before
-	public void setUp() throws Exception {
-		super.setUp();
-		iProject = project.getProject();
-		testRepo = new TestRepository(gitDir);
-		testRepo.connect(iProject);
-		repo = RepositoryMapping.getMapping(iProject).getRepository();
-	}
-
-	@After
-	public void clearGitResources() throws Exception {
-		testRepo.disconnect(iProject);
-		testRepo.dispose();
-		repo = null;
-		super.tearDown();
-	}
-
-	/**
-	 * This test simulates that user work and made some changes on branch 'test'
-	 * and then try to synchronize "test" and 'master' branch.
-	 *
-	 * @throws Exception
-	 */
-	@Test
-	public void shouldReturnSrcBranchAsBase() throws Exception {
-		// when
-		String fileName = "Main.java";
-		File file = testRepo.createFile(iProject, fileName);
-		RevCommit commit = testRepo.appendContentAndCommit(iProject, file,
-				"class Main {}", "initial commit");
-		IFile mainJava = testRepo.getIFile(iProject, file);
-		testRepo.createAndCheckoutBranch(Constants.HEAD, Constants.R_HEADS
-				+ "test");
-		testRepo.appendContentAndCommit(iProject, file, "// test1",
-				"secont commit");
-
-		// given
-		GitResourceVariantTreeSubscriber grvts = createGitResourceVariantTreeSubscriber(
-				Constants.HEAD, Constants.R_HEADS + Constants.MASTER);
-		grvts.getBaseTree();
-		IResourceVariantTree baseTree = grvts.getBaseTree();
-
-		// then
-		IResourceVariant actual = commonAssertionsForBaseTree(baseTree,
-				mainJava);
-		assertEquals(commit.abbreviate(7).name() + "...",
-				actual.getContentIdentifier());
-	}
-
-	private GitResourceVariantTreeSubscriber createGitResourceVariantTreeSubscriber(
-			String src, String dst) throws IOException {
-		GitSynchronizeData gsd = new GitSynchronizeData(repo, src, dst, false);
-		GitSynchronizeDataSet gsds = new GitSynchronizeDataSet(gsd);
-		new GitResourceVariantTreeSubscriber(gsds);
-		return new GitResourceVariantTreeSubscriber(gsds);
-	}
-
-	private IResourceVariant commonAssertionsForBaseTree(
-			IResourceVariantTree baseTree, IResource resource)
-			throws TeamException {
-		assertNotNull(baseTree);
-		assertTrue(baseTree instanceof GitBaseResourceVariantTree);
-		IResourceVariant resourceVariant = baseTree
-				.getResourceVariant(resource);
-		assertNotNull(resourceVariant);
-		assertTrue(resourceVariant instanceof GitResourceVariant);
-		return resourceVariant;
-	}
-
-}

org.eclipse.egit.core.test/src/org/eclipse/egit/core/synchronize/GitResourceVariantTreeSubscriberTest2.java
@@ -1,124 +1,0 @@
-/*******************************************************************************
- * Copyright (C) 2010, Dariusz Luksza <dariusz@luksza.org>
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *******************************************************************************/
-package org.eclipse.egit.core.synchronize;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-import java.io.File;
-import java.io.IOException;
-
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.egit.core.project.RepositoryMapping;
-import org.eclipse.egit.core.synchronize.dto.GitSynchronizeData;
-import org.eclipse.egit.core.synchronize.dto.GitSynchronizeDataSet;
-import org.eclipse.egit.core.test.GitTestCase;
-import org.eclipse.egit.core.test.TestRepository;
-import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.team.core.TeamException;
-import org.eclipse.team.core.variants.IResourceVariant;
-import org.eclipse.team.core.variants.IResourceVariantTree;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-public class GitResourceVariantTreeSubscriberTest2 extends GitTestCase {
-
-	private Repository repo;
-
-	private IProject iProject;
-
-	private TestRepository testRepo;
-
-	@Before
-	public void setUp() throws Exception {
-		super.setUp();
-		iProject = project.getProject();
-		testRepo = new TestRepository(gitDir);
-		testRepo.connect(iProject);
-		repo = RepositoryMapping.getMapping(iProject).getRepository();
-	}
-
-	@After
-	public void clearGitResources() throws Exception {
-		testRepo.disconnect(iProject);
-		testRepo.dispose();
-		repo = null;
-		super.tearDown();
-	}
-
-	/**
-	 * Both source and destination branches has some different commits but they
-	 * has also common ancestor. This situation is described more detailed in
-	 * bug #317934
-	 *
-	 * This test passes when it is run as a stand alone test case, but it fails
-	 * when it is run as part of test suite. It throws NPE when it try's to
-	 * checkout master branch.
-	 *
-	 * @throws Exception
-	 */
-	@Test
-	public void shouldReturnCommonAncestorAsBase() throws Exception {
-		// when
-		String fileName = "Main.java";
-		File file = testRepo.createFile(iProject, fileName);
-		RevCommit commit = testRepo.appendContentAndCommit(iProject, file,
-				"class Main {}", "initial commit");
-		IFile mainJava = testRepo.getIFile(iProject, file);
-		// this should be our common ancestor
-
-		testRepo.createAndCheckoutBranch(Constants.HEAD, Constants.R_HEADS
-				+ "test");
-		testRepo.appendContentAndCommit(iProject, file, "// test 1",
-				"second commit");
-
-		testRepo.checkoutBranch(Constants.R_HEADS + Constants.MASTER);
-		testRepo.appendContentAndCommit(iProject, file, "// test 2",
-				"third commit");
-
-		// given
-		GitResourceVariantTreeSubscriber grvts = createGitResourceVariantTreeSubscriber(
-				Constants.HEAD, Constants.R_HEADS + "test");
-		grvts.getBaseTree();
-		IResourceVariantTree baseTree = grvts.getBaseTree();
-
-		// then
-		IResourceVariant actual = commonAssertionsForBaseTree(baseTree,
-				mainJava);
-		assertEquals(commit.abbreviate(7).name() + "...",
-				actual.getContentIdentifier());
-	}
-
-	private GitResourceVariantTreeSubscriber createGitResourceVariantTreeSubscriber(
-			String src, String dst) throws IOException {
-		GitSynchronizeData gsd = new GitSynchronizeData(repo, src, dst, false);
-		GitSynchronizeDataSet gsds = new GitSynchronizeDataSet(gsd);
-		new GitResourceVariantTreeSubscriber(gsds);
-		return new GitResourceVariantTreeSubscriber(gsds);
-	}
-
-	private IResourceVariant commonAssertionsForBaseTree(
-			IResourceVariantTree baseTree, IResource resource)
-			throws TeamException {
-		assertNotNull(baseTree);
-		assertTrue(baseTree instanceof GitBaseResourceVariantTree);
-		IResourceVariant resourceVariant = baseTree
-				.getResourceVariant(resource);
-		assertNotNull(resourceVariant);
-		assertTrue(resourceVariant instanceof GitResourceVariant);
-		return resourceVariant;
-	}
-
-}

org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/GitBlobResourceVariant.java
@@ -1,43 +1,0 @@
-/*******************************************************************************
- * Copyright (c) 2010 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *     Dariusz Luksza <dariusz@luksza.org>
- *******************************************************************************/
-package org.eclipse.egit.core.synchronize;
-
-import java.io.IOException;
-
-import org.eclipse.core.resources.IStorage;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.egit.core.internal.storage.CommitBlobStorage;
-import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.team.core.TeamException;
-
-/**
- * This is a representation of a file's blob in some branch.
- */
-public class GitBlobResourceVariant extends GitResourceVariant {
-
-	GitBlobResourceVariant(Repository repo, RevCommit revCommit,
-			ObjectId objectId, String path) throws IOException {
-		super(repo, revCommit, objectId, path);
-	}
-
-	public boolean isContainer() {
-		return false;
-	}
-
-	public IStorage getStorage(IProgressMonitor monitor) throws TeamException {
-		return new CommitBlobStorage(getRepository(), getPath(),
-					getObjectId(), getRevCommit());
-	}
-
-}

org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/GitFolderResourceVariant.java
@@ -1,112 +1,0 @@
-/*******************************************************************************
- * Copyright (c) 2010 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *     Dariusz Luksza <dariusz@luksza.org>
- *******************************************************************************/
-package org.eclipse.egit.core.synchronize;
-
-import static org.eclipse.jgit.lib.ObjectId.zeroId;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.eclipse.core.resources.IStorage;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.OperationCanceledException;
-import org.eclipse.core.runtime.SubMonitor;
-import org.eclipse.egit.core.CoreText;
-import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.treewalk.FileTreeIterator;
-import org.eclipse.jgit.treewalk.TreeWalk;
-import org.eclipse.jgit.treewalk.filter.NotIgnoredFilter;
-import org.eclipse.osgi.util.NLS;
-import org.eclipse.team.core.TeamException;
-import org.eclipse.team.core.variants.IResourceVariant;
-
-/**
- *
- */
-public class GitFolderResourceVariant extends GitResourceVariant {
-
-	private IResourceVariant members[];
-
-	GitFolderResourceVariant(Repository repo, RevCommit revCommit,
-			ObjectId objectId, String path) throws IOException {
-		super(repo, revCommit, objectId, path);
-	}
-
-	public boolean isContainer() {
-		return true;
-	}
-
-	public IStorage getStorage(IProgressMonitor monitor) throws TeamException {
-		return null;
-	}
-
-	/**
-	 * @param progress
-	 * @return members
-	 * @throws IOException
-	 */
-	public IResourceVariant[] getMembers(IProgressMonitor progress)
-			throws IOException {
-		if (members != null)
-			try {
-				return members;
-			} finally {
-				progress.done();
-			}
-
-		Repository repo = getRepository();
-		TreeWalk tw = new TreeWalk(repo);
-		tw.reset();
-
-		int nth = tw.addTree(getObjectId());
-		int iteratorNth = tw.addTree(new FileTreeIterator(repo));
-
-		tw.setFilter(new NotIgnoredFilter(iteratorNth));
-
-		IProgressMonitor monitor = SubMonitor.convert(progress);
-		monitor.beginTask(
-				NLS.bind(CoreText.GitFolderResourceVariant_fetchingMembers, this),
-				tw.getTreeCount());
-
-		int i = 0;
-		List<IResourceVariant> result = new ArrayList<IResourceVariant>();
-		try {
-			while (tw.next()) {
-				if (monitor.isCanceled())
-					throw new OperationCanceledException();
-
-				ObjectId newObjectId = tw.getObjectId(nth);
-				String path = getPath() + "/" + new String(tw.getRawPath()); //$NON-NLS-1$
-				if (!newObjectId.equals(zeroId()))
-					if (tw.isSubtree())
-						result.add(new GitFolderResourceVariant(repo,
-								getRevCommit(), newObjectId, path));
-					else
-						result.add(new GitBlobResourceVariant(repo,
-								getRevCommit(), newObjectId, path));
-				if (i % 10 == 0)
-					monitor.worked(10);
-
-				i++;
-			}
-
-			members = result.toArray(new IResourceVariant[result.size()]);
-			return members;
-		} finally {
-			monitor.done();
-		}
-	}
-
-}

org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/GitRemoteFolder.java
@@ -1,0 +1,105 @@
+/*******************************************************************************
+ * Copyright (C) 2011, Dariusz Luksza <dariusz@luksza.org>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.core.synchronize;
+
+import static org.eclipse.jgit.lib.ObjectId.zeroId;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.core.runtime.SubMonitor;
+import org.eclipse.egit.core.CoreText;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.team.core.variants.IResourceVariant;
+
+class GitRemoteFolder extends GitRemoteResource {
+
+	/**
+	 *
+	 * @param repo
+	 * @param revCommit
+	 * @param objectId
+	 *            given object sha-1 id or {@code null} if this object doesn't
+	 *            exist in remote
+	 * @param path
+	 */
+	GitRemoteFolder(Repository repo, RevCommit revCommit, ObjectId objectId,
+			String path) {
+		super(repo, revCommit, objectId, path);
+	}
+
+	public boolean isContainer() {
+		return true;
+	}
+
+	public boolean equals(Object object) {
+		if (object == this)
+			return true;
+
+		if (object instanceof GitRemoteFolder) {
+			GitRemoteFolder that = (GitRemoteFolder) object;
+
+			return getPath().equals(that.getPath())
+					&& getObjectId().equals(that.getObjectId());
+		}
+
+		return false;
+	}
+
+	public int hashCode() {
+		return getObjectId().hashCode() ^ getPath().hashCode();
+	}
+
+	GitRemoteResource[] members(IProgressMonitor monitor) throws IOException {
+		Repository repo = getRepo();
+		TreeWalk tw = new TreeWalk(repo);
+		int nth = tw.addTree(getObjectId());
+
+		IProgressMonitor m = SubMonitor.convert(monitor);
+		m.beginTask(NLS.bind(CoreText.GitFolderResourceVariant_fetchingMembers,
+				this), tw.getTreeCount());
+
+		int i = 0;
+		List<IResourceVariant> result = new ArrayList<IResourceVariant>();
+		try {
+			while (tw.next()) {
+				if (monitor.isCanceled())
+					throw new OperationCanceledException();
+
+				ObjectId newObjectId = tw.getObjectId(nth);
+				String path = getPath() + "/" + tw.getPathString(); //$NON-NLS-1$
+
+				if (!zeroId().equals(newObjectId))
+					if (tw.isSubtree())
+						result.add(new GitRemoteFolder(repo, getCommit(),
+								newObjectId, path));
+					else
+						result.add(new GitRemoteFile(repo, getCommit(),
+								newObjectId, path));
+
+				if (i % 10 == 0)
+					m.worked(10);
+
+				i++;
+			}
+		} finally {
+			m.done();
+		}
+
+		return result.toArray(new GitRemoteResource[result.size()]);
+	}
+
+}

commit f2e09ac1a59f37d15c5ebcf087e7d1d9d298df4a

[sync] Fixes refreshing Workspace model after repository change
Refreshing Workspace model was broken after commit 521e41c because
GitRemoteFile wasn't obtaining proper content of remote file.

Bug: 355691
Change-Id: I47ca5ca7417cff7a86dae048733d1c90453d821a
Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/GitRemoteFile.java
@@ -8,9 +8,13 @@
  *******************************************************************************/
 package org.eclipse.egit.core.synchronize;
 
+import org.eclipse.core.resources.IStorage;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.egit.core.internal.storage.CommitBlobStorage;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.team.core.TeamException;
 
 class GitRemoteFile extends GitRemoteResource {
 
@@ -36,6 +40,12 @@ class GitRemoteFile extends GitRemoteResource {
 		}
 
 		return false;
+	}
+
+	@Override
+	public IStorage getStorage(IProgressMonitor monitor) throws TeamException {
+		return new CommitBlobStorage(getRepo(), getCachePath(), getObjectId(),
+				getCommit());
 	}
 
 	@Override