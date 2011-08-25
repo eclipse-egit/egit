98daa3e804772aaa454bdcda011b7bbf079a7e3a
Benjamin Muskalla <benjamin.muskalla@tasktop.com>
2011-08-25 23:40:29 +0200

    Consider logical model when replacing files
    When replacing files from the repository, we should ask all model
    providers if this set of resources is consistent in itself or if we
    need to replace more resources to have a consitent model.
    
    Bug: 348060
    Change-Id: I95ed9d53dbf8509fdd89f3c47b0280b6e40e946b
    Signed-off-by: Benjamin Muskalla <benjamin.muskalla@tasktop.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/ReplaceWithCommitActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/ReplaceWithCommitActionHandler.java
@@ -7,12 +7,13 @@
  *
  *  Contributors:
  *    Kevin Sawicki (GitHub Inc.) - initial API and implementation
+ *    Benjamin Muskalla (Tasktop Technologies Inc.) - support for model scoping
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.actions;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.egit.core.op.DiscardChangesOperation;
+import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.egit.ui.internal.history.CommitSelectionDialog;
 import org.eclipse.jface.window.Window;
 
@@ -22,12 +23,12 @@ import org.eclipse.jface.window.Window;
 public class ReplaceWithCommitActionHandler extends DiscardChangesActionHandler {
 
 	@Override
-	protected DiscardChangesOperation createOperation(ExecutionEvent event)
-			throws ExecutionException {
+	protected String gatherRevision(ExecutionEvent event) throws ExecutionException {
 		CommitSelectionDialog dlg = new CommitSelectionDialog(getShell(event),
 				getRepository(true, event));
-		return dlg.open() == Window.OK ? new DiscardChangesOperation(
-				getSelectedResources(event), dlg.getCommitId().name()) : null;
+		if (dlg.open() == Window.OK)
+			return dlg.getCommitId().name();
+		else
+			throw new OperationCanceledException();
 	}
-
 }
