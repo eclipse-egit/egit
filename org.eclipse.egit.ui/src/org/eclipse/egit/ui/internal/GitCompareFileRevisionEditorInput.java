588c6f6db7bf7a5453e3dff289b57ce2d9555e7f
Remy Suen <remysuen@ca.ibm.com>
2011-08-26 17:49:36 -0400

    Diff node should notify listeners when the input changes
    The text merge viewers need to be notified when changes to a
    compare input are saved. The text viewer can be notified of
    changes from the diff node. Hence, when the compare input itself
    has been notified that its been changed, this change event must
    be forwarded to the diff node so that the text viewer can be
    updated and reflect the differences of the newly changed compare
    input. 
    
    Bug: 352576
    Change-Id: I2f83544f271fea4a4e21f346218a75ef67bae623

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/GitCompareFileRevisionEditorInput.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/GitCompareFileRevisionEditorInput.java
@@ -332,7 +332,8 @@ public class GitCompareFileRevisionEditorInput extends SaveableCompareEditorInpu
 
 	@Override
 	protected void fireInputChange() {
-		// nothing
+		// have the diff node notify its listeners of a change
+		((NotifiableDiffNode) getCompareResult()).fireChange();
 	}
 
 	private LocalResourceTypedElement getLocalElement() {
@@ -354,7 +355,8 @@ public class GitCompareFileRevisionEditorInput extends SaveableCompareEditorInpu
 
 		// The compare editor (Structure Compare) will show the diff filenames
 		// with their project relative path. So, no need to also show directory entries.
-		DiffNode flatDiffNode = new DiffNode(null,Differencer.CHANGE,null,left,right);
+		DiffNode flatDiffNode = new NotifiableDiffNode(null,
+				Differencer.CHANGE, null, left, right);
 		flatDiffView(flatDiffNode, (DiffNode) input);
 
 		return flatDiffNode;
