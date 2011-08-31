7aad71169277621e93ab34172f86b1f0a32413b9
Kevin Sawicki <kevin@github.com>
2011-08-31 12:28:06 -0700

    Show messages in CommitDialog for errors that prevent committing.
    Previously validation was done when the Commit button was selected
    and dialogs were opened explaining why committing was not possible.
    
    Now these messages are displayed in the title area of the commit
    dialog and the Commit button is disabled when the commit cannot
    be completed.
    
    Bug: 355957
    Change-Id: Iffb7f0b5dfdc70c465b4ffee0c3e27886cccf296
    Signed-off-by: Kevin Sawicki <kevin@github.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitDialog.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitDialog.java
@@ -79,11 +79,14 @@ import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.events.KeyAdapter;
 import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Event;
@@ -297,6 +300,8 @@ public class CommitDialog extends TitleAreaDialog {
 
 	Section filesSection;
 
+	Button commitButton;
+
 	ArrayList<CommitItem> items = new ArrayList<CommitItem>();
 
 	private String commitMessage = null;
@@ -331,7 +336,6 @@ public class CommitDialog extends TitleAreaDialog {
 	 */
 	public CommitDialog(Shell parentShell) {
 		super(parentShell);
-		setTitleImage(UIIcons.WIZBAN_CONNECT_REPO.createImage());
 	}
 
 	/**
@@ -481,9 +485,11 @@ public class CommitDialog extends TitleAreaDialog {
 	@Override
 	protected void createButtonsForButtonBar(Composite parent) {
 		toolkit.adapt(parent, false, false);
-		createButton(parent, IDialogConstants.OK_ID, UIText.CommitDialog_Commit, true);
+		commitButton = createButton(parent, IDialogConstants.OK_ID,
+				UIText.CommitDialog_Commit, true);
 		createButton(parent, IDialogConstants.CANCEL_ID,
 				IDialogConstants.CANCEL_LABEL, false);
+		updateMessage();
 	}
 
 	@Override
@@ -803,6 +809,7 @@ public class CommitDialog extends TitleAreaDialog {
 				showUntracked = showUntrackedItem.getSelection();
 				filesViewer.refresh(true);
 				updateFileSectionText();
+				updateMessage();
 			}
 
 		});
@@ -817,6 +824,7 @@ public class CommitDialog extends TitleAreaDialog {
 			public void widgetSelected(SelectionEvent e) {
 				filesViewer.setAllChecked(true);
 				updateFileSectionText();
+				updateMessage();
 			}
 
 		});
@@ -831,6 +839,7 @@ public class CommitDialog extends TitleAreaDialog {
 			public void widgetSelected(SelectionEvent e) {
 				filesViewer.setAllChecked(false);
 				updateFileSectionText();
+				updateMessage();
 			}
 
 		});
@@ -867,10 +876,39 @@ public class CommitDialog extends TitleAreaDialog {
 		resourceCol.pack();
 		container.pack();
 		commitText.setFocus();
+		Image titleImage = UIIcons.WIZBAN_CONNECT_REPO.createImage();
+		UIUtils.hookDisposal(parent, titleImage);
+		setTitleImage(titleImage);
 		setTitle(UIText.CommitDialog_Title);
 		setMessage(UIText.CommitDialog_Message, IMessageProvider.INFORMATION);
+
+		ModifyListener validator = new ModifyListener() {
+
+			public void modifyText(ModifyEvent e) {
+				updateMessage();
+			}
+		};
+		commitText.getTextWidget().addModifyListener(validator);
+		authorText.addModifyListener(validator);
+		committerText.addModifyListener(validator);
+		filesViewer.addCheckStateListener(new ICheckStateListener() {
+
+			public void checkStateChanged(CheckStateChangedEvent event) {
+				updateMessage();
+			}
+		});
+
 		updateFileSectionText();
 		return container;
+	}
+
+	private void updateMessage() {
+		String message = commitMessageComponent.getMessage();
+		if (message == null && filesViewer.getCheckedElements().length == 0
+				&& !amendingItem.getSelection())
+			message = UIText.CommitDialog_MessageNoFilesSelected;
+		setMessage(message, IMessageProvider.INFORMATION);
+		commitButton.setEnabled(message == null);
 	}
 
 	private Collection<String> getFileList() {
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitMessageComponent.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitMessageComponent.java
@@ -325,6 +325,30 @@ public class CommitMessageComponent {
 	}
 
 	/**
+	 * Get an informational message about the state of the commit message
+	 * component input. This method checks the current state of the widgets and
+	 * must always be called from the UI-thread.
+	 *
+	 * @return information message or null if none
+	 */
+	public String getMessage() {
+		if (commitText.getText().trim().length() == 0)
+			return UIText.CommitDialog_Message;
+
+		String authorValue = authorText.getText();
+		if (authorValue.length() == 0
+				|| RawParseUtils.parsePersonIdent(authorValue) == null)
+			return UIText.CommitMessageComponent_MessageInvalidAuthor;
+
+		String committerValue = committerText.getText();
+		if (committerValue.length() == 0
+				|| RawParseUtils.parsePersonIdent(committerValue) == null)
+			return UIText.CommitMessageComponent_MessageInvalidCommitter;
+
+		return null;
+	}
+
+	/**
 	 * @return true if commit info is ok
 	 */
 	public boolean checkCommitInfo() {
