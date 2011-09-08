7f019b48c6df099722b697aa3a546761d8b7402b
Dariusz Luksza <dariusz@luksza.org>
2011-09-08 11:06:53 +0200

    [sync] Allow to move changes in compare editor for working tree file
    Now in 'Git Commits' model you can move particular change in files from
    '<working tree>' node between working copy and staged version. All
    changes are persisted to file system/git index after saving compare
    results.
    
    Change-Id: Ib38ee2d05934aa0f5cb9ddec9b5be91cbbe38759
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CompareIndexWithHeadActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CompareIndexWithHeadActionHandler.java
@@ -49,10 +49,11 @@ public class CompareIndexWithHeadActionHandler extends RepositoryActionHandler {
 		final IFile baseFile = (IFile) resources[0];
 		final String gitPath = RepositoryMapping.getMapping(
 				baseFile.getProject()).getRepoRelativePath(baseFile);
-		final ITypedElement base = CompareUtils.getFileCachedRevisionTypedElement(gitPath, repository);
+		ITypedElement base;
 
 		ITypedElement next;
 		try {
+			base = CompareUtils.getHeadTypedElement(baseFile);
 			Ref head = repository.getRef(Constants.HEAD);
 			RevWalk rw = new RevWalk(repository);
 			RevCommit commit = rw.parseCommit(head.getObjectId());
