cc9f290599e1f31201ff5ffec7862f5bdeedc366
Dariusz Luksza <dariusz@luksza.org>
2011-09-08 11:12:30 +0200

    [sync] Allow to move changes in compare editor for working tree file
    Now in 'Git Commits' model you can move particular change in files from
    '<working tree>' node between working copy and staged version. All
    changes are persisted to file system/git index after saving compare
    results.
    
    To add this functionality we need to wrap team's specific OpenInCompre
    action into ours own (GitOpenInCompare) action, because team framework
    forces right hand side to be always read only. Such behavior ins't true
    for us when we want to move changes from working copy to git index.
    
    
    Change-Id: Ib38ee2d05934aa0f5cb9ddec9b5be91cbbe38759
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CompareWithIndexActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CompareWithIndexActionHandler.java
@@ -9,35 +9,18 @@
 
 package org.eclipse.egit.ui.internal.actions;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
-import org.eclipse.compare.IContentChangeListener;
-import org.eclipse.compare.IContentChangeNotifier;
 import org.eclipse.compare.ITypedElement;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.egit.core.internal.CompareCoreUtils;
-import org.eclipse.egit.core.internal.storage.GitFileRevision;
-import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.internal.CompareUtils;
-import org.eclipse.egit.ui.internal.EditableRevision;
 import org.eclipse.egit.ui.internal.GitCompareFileRevisionEditorInput;
 import org.eclipse.egit.ui.internal.dialogs.CompareTreeView;
-import org.eclipse.jgit.dircache.DirCache;
-import org.eclipse.jgit.dircache.DirCacheEditor;
-import org.eclipse.jgit.dircache.DirCacheEditor.PathEdit;
-import org.eclipse.jgit.dircache.DirCacheEntry;
-import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.ObjectInserter;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.osgi.util.NLS;
-import org.eclipse.team.core.history.IFileRevision;
 import org.eclipse.team.ui.synchronize.SaveableCompareEditorInput;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
@@ -64,7 +47,7 @@ public class CompareWithIndexActionHandler extends RepositoryActionHandler {
 
 			final ITypedElement next;
 			try {
-				next = getHeadTypedElement(baseFile);
+				next = CompareUtils.getHeadTypedElement(baseFile);
 			} catch (IOException e) {
 				Activator.handleError(
 						UIText.CompareWithIndexAction_errorOnAddToIndex, e,
@@ -88,86 +71,6 @@ public class CompareWithIndexActionHandler extends RepositoryActionHandler {
 			}
 		}
 		return null;
-	}
-
-	private ITypedElement getHeadTypedElement(final IFile baseFile)
-			throws IOException {
-		final RepositoryMapping mapping = RepositoryMapping.getMapping(baseFile
-				.getProject());
-		final Repository repository = mapping.getRepository();
-		final String gitPath = mapping.getRepoRelativePath(baseFile);
-
-		DirCache dc = repository.lockDirCache();
-		final DirCacheEntry entry = dc.getEntry(gitPath);
-		dc.unlock();
-		if (entry == null) {
-			// the file cannot be found in the index
-			return new GitCompareFileRevisionEditorInput.EmptyTypedElement(NLS
-					.bind(UIText.CompareWithIndexAction_FileNotInIndex,
-							baseFile.getName()));
-		}
-
-		IFileRevision nextFile = GitFileRevision.inIndex(repository, gitPath);
-		String encoding = CompareCoreUtils.getResourceEncoding(baseFile);
-		final EditableRevision next = new EditableRevision(nextFile, encoding);
-
-		IContentChangeListener listener = new IContentChangeListener() {
-			public void contentChanged(IContentChangeNotifier source) {
-				final byte[] newContent = next.getModifiedContent();
-				DirCache cache = null;
-				try {
-					cache = repository.lockDirCache();
-					DirCacheEditor editor = cache.editor();
-					editor.add(new PathEdit(gitPath) {
-						@Override
-						public void apply(DirCacheEntry ent) {
-							ent.copyMetaData(entry);
-
-							ObjectInserter inserter = repository
-									.newObjectInserter();
-							ent.copyMetaData(entry);
-							ent.setLength(newContent.length);
-							ent.setLastModified(System.currentTimeMillis());
-							InputStream in = new ByteArrayInputStream(
-									newContent);
-							try {
-								ent.setObjectId(inserter.insert(
-										Constants.OBJ_BLOB, newContent.length,
-										in));
-								inserter.flush();
-							} catch (IOException ex) {
-								throw new RuntimeException(ex);
-							} finally {
-								try {
-									in.close();
-								} catch (IOException e) {
-									// ignore here
-								}
-							}
-						}
-					});
-					try {
-						editor.commit();
-					} catch (RuntimeException e) {
-						if (e.getCause() instanceof IOException)
-							throw (IOException) e.getCause();
-						else
-							throw e;
-					}
-
-				} catch (IOException e) {
-					Activator.handleError(
-							UIText.CompareWithIndexAction_errorOnAddToIndex, e,
-							true);
-				} finally {
-					if (cache != null)
-						cache.unlock();
-				}
-			}
-		};
-
-		next.addContentChangeListener(listener);
-		return next;
 	}
 
 	@Override
