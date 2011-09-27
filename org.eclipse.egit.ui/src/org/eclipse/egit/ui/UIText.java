7b537d3be063188bd7fa3fd5baabe7c9dbbb5d53
Matthias Sohn <matthias.sohn@sap.com>
2011-09-28 01:17:51 +0200

    [reflogView] Show who moved ref when
    Change-Id: I909a8032b46657ea34d446f11365ee492a0cb457
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -1156,6 +1156,12 @@ public class UIText extends NLS {
 	public static String RefContentProposal_unknownObject;
 
 	/** */
+	public static String ReflogView_CommitterColumnHeader;
+
+	/** */
+	public static String ReflogView_DateColumnHeader;
+
+	/** */
 	public static String ReflogView_ErrorOnOpenCommit;
 
 	/** */
