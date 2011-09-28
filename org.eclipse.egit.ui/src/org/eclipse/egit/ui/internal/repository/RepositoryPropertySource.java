d3551b0b31035704b1e45d1245684cc11b5787d7
Kevin Sawicki <kevin@github.com>
2011-09-27 18:34:40 -0700

    Update property source on edit action.
    The edit action is only created once for all RepositoryPropertySource
    instances and so the source it uses to open the configuration
    dialog needs to access the latest source.
    
    Bug: 358908
    Change-Id: Ia60e736f89aa83f313a484a8fea9b6743d9889c9
    Signed-off-by: Kevin Sawicki <kevin@github.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoryPropertySource.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoryPropertySource.java
@@ -28,6 +28,7 @@ import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jgit.errors.ConfigInvalidException;
 import org.eclipse.jgit.lib.Config;
 import org.eclipse.jgit.lib.Repository;
@@ -70,6 +71,59 @@ public class RepositoryPropertySource implements IPropertySource {
 	private static final String REPO_ID_PREFIX = "repo"; //$NON-NLS-1$
 
 	private static final String EFFECTIVE_ID_PREFIX = "effe"; //$NON-NLS-1$
+
+	private static class EditAction extends Action {
+
+		private RepositoryPropertySource source;
+
+		public EditAction(String text, ImageDescriptor image,
+				RepositoryPropertySource source) {
+			super(text, image);
+			this.source = source;
+		}
+
+		public EditAction setSource(RepositoryPropertySource source) {
+			this.source = source;
+			return this;
+		}
+
+		@Override
+		public String getId() {
+			return EDITACTIONID;
+		}
+
+		@Override
+		public void run() {
+			final StoredConfig config;
+
+			DisplayMode mode = source.getCurrentMode();
+			switch (mode) {
+			case EFFECTIVE:
+				return;
+			case SYSTEM:
+				config = source.systemConfig;
+				break;
+			case USER:
+				config = source.userHomeConfig;
+				break;
+			case REPO:
+				config = source.repositoryConfig;
+				break;
+			default:
+				return;
+			}
+
+			new EditDialog(source.myPage.getSite().getShell(),
+					(FileBasedConfig) config, mode.getText()).open();
+			source.myPage.refresh();
+		}
+
+		@Override
+		public int getStyle() {
+			return IAction.AS_PUSH_BUTTON;
+		}
+
+	}
 
 	private final PropertySheetPage myPage;
 
@@ -117,6 +171,11 @@ public class RepositoryPropertySource implements IPropertySource {
 					.getToolBarManager().find(CHANGEMODEACTIONID);
 			singleValueToggleAction = (ActionContributionItem) bars
 					.getToolBarManager().find(SINGLEVALUEACTIONID);
+
+			editAction = ((ActionContributionItem) bars.getToolBarManager()
+					.find(EDITACTIONID));
+			if (editAction != null)
+				((EditAction) editAction.getAction()).setSource(this);
 
 			if (changeModeAction != null) {
 				return;
@@ -179,46 +238,9 @@ public class RepositoryPropertySource implements IPropertySource {
 
 			});
 
-			editAction = new ActionContributionItem(new Action(
+			editAction = new ActionContributionItem(new EditAction(
 					UIText.RepositoryPropertySource_EditConfigButton,
-					UIIcons.EDITCONFIG) {
-				@Override
-				public String getId() {
-					return EDITACTIONID;
-				}
-
-				@Override
-				public void run() {
-
-					final StoredConfig config;
-
-					switch (getCurrentMode()) {
-					case EFFECTIVE:
-						return;
-					case SYSTEM:
-						config = systemConfig;
-						break;
-					case USER:
-						config = userHomeConfig;
-						break;
-					case REPO:
-						config = repositoryConfig;
-						break;
-					default:
-						return;
-					}
-
-					new EditDialog(myPage.getSite().getShell(),
-							(FileBasedConfig) config, getCurrentMode()
-									.getText()).open();
-					myPage.refresh();
-				}
-
-				@Override
-				public int getStyle() {
-					return IAction.AS_PUSH_BUTTON;
-				}
-			});
+					UIIcons.EDITCONFIG, this));
 
 			singleValueToggleAction = new ActionContributionItem(new Action(
 					UIText.RepositoryPropertySource_SingleValueButton) {
