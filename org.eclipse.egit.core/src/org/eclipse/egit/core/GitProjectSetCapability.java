19827c2c02d97df940ae7dbe383ada66b9121fd2
Renato Bertacco <renato.bertacco@finantix.com>
2011-09-29 17:40:41 +0200

    Proposed cumulative fix for defects: 358530, 358534
    Change-Id: Ic69e9cf01bf20dc567590370b5cbfc1ee3030631

--- org.eclipse.egit.core/src/org/eclipse/egit/core/GitProjectSetCapability.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/GitProjectSetCapability.java
@@ -29,6 +29,7 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.IWorkspaceRunnable;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
@@ -54,16 +55,13 @@ public final class GitProjectSetCapability extends ProjectSetCapability {
 	private static final String SEPARATOR = ","; //$NON-NLS-1$
 	private static final String VERSION = "1.0"; //$NON-NLS-1$
 
-	private static final class ProjectReferenceComparator implements
-			Comparator<ProjectReference>, Serializable {
+	private static final class ProjectReferenceComparator implements Comparator<ProjectReference>, Serializable {
 		private static final long serialVersionUID = 1L;
 
 		public int compare(ProjectReference o1, ProjectReference o2) {
 			final boolean reposEqual = o1.repository.equals(o2.repository);
-			final boolean branchesEqual = o1.branch
-					.equals(o2.branch);
-			final boolean projectDirsEqual = o1.projectDir
-					.equals(o2.projectDir);
+			final boolean branchesEqual = o1.branch.equals(o2.branch);
+			final boolean projectDirsEqual = o1.projectDir.equals(o2.projectDir);
 			return reposEqual && branchesEqual && projectDirsEqual ? 0 : 1;
 		}
 	}
@@ -92,9 +90,7 @@ public final class GitProjectSetCapability extends ProjectSetCapability {
 		ProjectReference(final String reference) throws URISyntaxException, IllegalArgumentException {
 			final String[] tokens = reference.split(Pattern.quote(SEPARATOR));
 			if (tokens.length != 4)
-				throw new IllegalArgumentException(NLS.bind(
-						CoreText.GitProjectSetCapability_InvalidTokensCount, new Object[] {
-								4, tokens.length, tokens }));
+				throw new IllegalArgumentException(NLS.bind(CoreText.GitProjectSetCapability_InvalidTokensCount, new Object[] { 4, tokens.length, tokens }));
 
 			this.repository = new URIish(tokens[1]);
 			if (!"".equals(tokens[2])) //$NON-NLS-1$
@@ -104,9 +100,7 @@ public final class GitProjectSetCapability extends ProjectSetCapability {
 	}
 
 	@Override
-	public String[] asReference(IProject[] projects,
-			ProjectSetSerializationContext context, IProgressMonitor monitor)
-			throws TeamException {
+	public String[] asReference(IProject[] projects, ProjectSetSerializationContext context, IProgressMonitor monitor) throws TeamException {
 		String[] references = new String[projects.length];
 		for (int i = 0; i < projects.length; i++)
 			references[i] = asReference(projects[i]);
@@ -119,19 +113,13 @@ public final class GitProjectSetCapability extends ProjectSetCapability {
 		try {
 			branch = mapping.getRepository().getBranch();
 		} catch (IOException e) {
-			throw new TeamException(NLS.bind(
-					CoreText.GitProjectSetCapability_ExportCouldNotGetBranch,
-					project.getName()));
+			throw new TeamException(NLS.bind(CoreText.GitProjectSetCapability_ExportCouldNotGetBranch, project.getName()));
 		}
 		StoredConfig config = mapping.getRepository().getConfig();
-		String remote = config.getString(ConfigConstants.CONFIG_BRANCH_SECTION,
-				branch, ConfigConstants.CONFIG_KEY_REMOTE);
-		String url = config.getString(ConfigConstants.CONFIG_REMOTE_SECTION,
-				remote, ConfigConstants.CONFIG_KEY_URL);
+		String remote = config.getString(ConfigConstants.CONFIG_BRANCH_SECTION, branch, ConfigConstants.CONFIG_KEY_REMOTE);
+		String url = config.getString(ConfigConstants.CONFIG_REMOTE_SECTION, remote, ConfigConstants.CONFIG_KEY_URL);
 		if (url == null)
-			throw new TeamException(NLS.bind(
-					CoreText.GitProjectSetCapability_ExportNoRemote,
-					project.getName()));
+			throw new TeamException(NLS.bind(CoreText.GitProjectSetCapability_ExportNoRemote, project.getName()));
 
 		String projectPath = mapping.getRepoRelativePath(project);
 		if (projectPath.equals("")) //$NON-NLS-1$
@@ -151,110 +139,116 @@ public final class GitProjectSetCapability extends ProjectSetCapability {
 	}
 
 	@Override
-	public IProject[] addToWorkspace(final String[] referenceStrings,
-			final ProjectSetSerializationContext context,
-			final IProgressMonitor monitor) throws TeamException {
-		final Map<URIish, Map<String, Set<ProjectReference>>> repositories =
-				new LinkedHashMap<URIish, Map<String, Set<ProjectReference>>>();
-		for (final String reference : referenceStrings) {
-			try {
-				final ProjectReference projectReference = new ProjectReference(
-						reference);
-				Map<String, Set<ProjectReference>> repositoryBranches = repositories
-						.get(projectReference.repository);
-				if (repositoryBranches == null) {
-					repositoryBranches = new HashMap<String, Set<ProjectReference>>();
-					repositories.put(projectReference.repository,
-							repositoryBranches);
-				}
-				Set<ProjectReference> projectReferences = repositoryBranches.get(projectReference.branch);
-				if (projectReferences == null) {
-					projectReferences = new TreeSet<ProjectReference>(new ProjectReferenceComparator());
-					repositoryBranches.put(projectReference.branch, projectReferences);
-				}
-
-				projectReferences.add(projectReference);
-			} catch (final IllegalArgumentException e) {
-				throw new TeamException(reference, e);
-			} catch (final URISyntaxException e) {
-				throw new TeamException(reference, e);
-			}
-		}
+	public IProject[] addToWorkspace(final String[] referenceStrings, final ProjectSetSerializationContext context, final IProgressMonitor monitor) throws TeamException {
 		final ArrayList<IProject> importedProjects = new ArrayList<IProject>();
-		for (final Map.Entry<URIish, Map<String, Set<ProjectReference>>> entry : repositories.entrySet()) {
-			final URIish gitUrl = entry.getKey();
-			final Map<String, Set<ProjectReference>> branches = entry.getValue();
 
-			for (final Map.Entry<String, Set<ProjectReference>> branchEntry : branches.entrySet()) {
-				final String branch = branchEntry.getKey();
-				final Set<ProjectReference> projects = branchEntry.getValue();
+		try {
+			// FIX/358530
+			ResourcesPlugin.getWorkspace().run(new IWorkspaceRunnable() {
 
-				try {
-					final IPath workDir = getWorkingDir(gitUrl, branch,
-							branches.keySet());
-					if (workDir.toFile().exists()) {
-						final Collection<String> projectNames = new LinkedList<String>();
-						for (final ProjectReference projectReference : projects)
-							projectNames.add(projectReference.projectDir);
-						throw new TeamException(NLS.bind(
-								CoreText.GitProjectSetCapability_CloneToExistingDirectory,
-								new Object[] { workDir, projectNames, gitUrl }));
+				public void run(IProgressMonitor monitor) throws CoreException {
+					final Map<URIish, Map<String, Set<ProjectReference>>> repositories = new LinkedHashMap<URIish, Map<String, Set<ProjectReference>>>();
+					for (final String reference : referenceStrings) {
+						try {
+							final ProjectReference projectReference = new ProjectReference(reference);
+							Map<String, Set<ProjectReference>> repositoryBranches = repositories.get(projectReference.repository);
+							if (repositoryBranches == null) {
+								repositoryBranches = new HashMap<String, Set<ProjectReference>>();
+								repositories.put(projectReference.repository, repositoryBranches);
+							}
+							Set<ProjectReference> projectReferences = repositoryBranches.get(projectReference.branch);
+							if (projectReferences == null) {
+								projectReferences = new TreeSet<ProjectReference>(new ProjectReferenceComparator());
+								repositoryBranches.put(projectReference.branch, projectReferences);
+							}
+
+							projectReferences.add(projectReference);
+						} catch (final IllegalArgumentException e) {
+							throw new TeamException(reference, e);
+						} catch (final URISyntaxException e) {
+							throw new TeamException(reference, e);
+						}
 					}
 
-					int timeout = 60;
-					String refName = Constants.R_HEADS + branch;
-					final CloneOperation cloneOperation = new CloneOperation(
-							gitUrl, true, null, workDir.toFile(), refName,
-							Constants.DEFAULT_REMOTE_NAME, timeout);
-					cloneOperation.run(monitor);
+					for (final Map.Entry<URIish, Map<String, Set<ProjectReference>>> entry : repositories.entrySet()) {
+						final URIish gitUrl = entry.getKey();
+						final Map<String, Set<ProjectReference>> branches = entry.getValue();
 
-					final File repositoryPath = workDir.append(Constants.DOT_GIT_EXT).toFile();
+						for (final Map.Entry<String, Set<ProjectReference>> branchEntry : branches.entrySet()) {
+							final String branch = branchEntry.getKey();
+							final Set<ProjectReference> projects = branchEntry.getValue();
 
-					Activator.getDefault().getRepositoryUtil().addConfiguredRepository(repositoryPath);
+							try {
+								final IPath workDir = getWorkingDir(gitUrl, branch, branches.keySet());
+								if (workDir.toFile().exists()) {
+									final Collection<String> projectNames = new LinkedList<String>();
+									for (final ProjectReference projectReference : projects)
+										projectNames.add(projectReference.projectDir);
+									throw new TeamException(NLS.bind(CoreText.GitProjectSetCapability_CloneToExistingDirectory, new Object[] { workDir, projectNames, gitUrl }));
+								}
 
-					// import projects from the current repository to workspace
-					final IWorkspace workspace = ResourcesPlugin.getWorkspace();
-					final IWorkspaceRoot root = workspace.getRoot();
-					for (final ProjectReference projectToImport : projects) {
-						final IPath projectDir = workDir
-								.append(projectToImport.projectDir);
-						final IProjectDescription projectDescription = workspace
-								.loadProjectDescription(projectDir
-										.append(IProjectDescription.DESCRIPTION_FILE_NAME));
-						final IProject project = root
-								.getProject(projectDescription.getName());
-						project.create(projectDescription, monitor);
-						importedProjects.add(project);
+								int timeout = 60;
+								String refName = Constants.R_HEADS + branch;
+								final CloneOperation cloneOperation = new CloneOperation(gitUrl, true, null, workDir.toFile(), refName, Constants.DEFAULT_REMOTE_NAME, timeout);
+								cloneOperation.run(monitor);
 
-						project.open(monitor);
-						final ConnectProviderOperation connectProviderOperation = new ConnectProviderOperation(
-								project, repositoryPath);
-						connectProviderOperation.execute(monitor);
+								final File repositoryPath = workDir.append(Constants.DOT_GIT_EXT).toFile();
+
+								Activator.getDefault().getRepositoryUtil().addConfiguredRepository(repositoryPath);
+
+								// import projects from the current repository to workspace
+								final IWorkspace workspace = ResourcesPlugin.getWorkspace();
+								final IWorkspaceRoot root = workspace.getRoot();
+								for (final ProjectReference projectToImport : projects) {
+									final IPath projectDir = workDir.append(projectToImport.projectDir);
+									final IProjectDescription projectDescription = workspace.loadProjectDescription(projectDir.append(IProjectDescription.DESCRIPTION_FILE_NAME));
+									final IProject project = root.getProject(projectDescription.getName());
+									project.create(projectDescription, monitor);
+									importedProjects.add(project);
+
+									project.open(monitor);
+									final ConnectProviderOperation connectProviderOperation = new ConnectProviderOperation(project, repositoryPath);
+									connectProviderOperation.execute(monitor);
+								}
+							} catch (final InvocationTargetException e) {
+								// FIX/358534
+								throwTeamException(e);
+								// throw TeamException.asTeamException(e);
+							} catch (final CoreException e) {
+								throw TeamException.asTeamException(e);
+							} catch (final InterruptedException e) {
+								// was canceled by user
+								//return new IProject[0];
+							}
+						}
 					}
-				} catch (final InvocationTargetException e) {
-					throw TeamException.asTeamException(e);
-				} catch (final CoreException e) {
-					throw TeamException.asTeamException(e);
-				} catch (final InterruptedException e) {
-					// was canceled by user
-					return new IProject[0];
 				}
-			}
+			}, ResourcesPlugin.getWorkspace().getRoot(), IWorkspace.AVOID_UPDATE, monitor);
+		} catch (CoreException e) {
+			throwTeamException(e);
 		}
-		final IProject[] result = importedProjects
-				.toArray(new IProject[importedProjects.size()]);
+		final IProject[] result = importedProjects.toArray(new IProject[importedProjects.size()]);
 		return result;
+	}
+
+	private TeamException throwTeamException(Throwable th) throws TeamException{
+		Throwable current = th;
+		while(current.getCause()!=null){
+			current = current.getCause();
+		}
+		throw new TeamException(current.getMessage(), current);
 	}
 
 	/**
 	 * @param gitUrl
-	 * @param branch the branch to check out
-	 * @param allBranches all branches which should be checked out for this gitUrl
+	 * @param branch
+	 *            the branch to check out
+	 * @param allBranches
+	 *            all branches which should be checked out for this gitUrl
 	 * @return the directory where the project should be checked out
 	 */
 	private static IPath getWorkingDir(URIish gitUrl, String branch, Set<String> allBranches) {
-		final IPath workspaceLocation = ResourcesPlugin.getWorkspace()
-				.getRoot().getRawLocation();
+		final IPath workspaceLocation = ResourcesPlugin.getWorkspace().getRoot().getRawLocation();
 		final String humanishName = gitUrl.getHumanishName();
 		String extendedName;
 		if (allBranches.size() == 1 || branch.equals(Constants.MASTER))
