eea6f8ebfeb17bfb6ba2479946b599332ba8f1c8
Matthias Sohn <matthias.sohn@sap.com>
2011-09-29 23:03:53 +0200

    Auto share projects located in a git working tree
    Automatically share projects on import or creation if they are not
    shared with another team provider already and if they are contained in
    the working tree of an existing Git repository. Auto-sharing can be
    configured with a new preference.
    
    Bug: 356953
    Change-Id: I638fc7afd155f45e392d2a74bf8c36ef2d7ad063
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/JobFamilies.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/JobFamilies.java
@@ -24,4 +24,8 @@ public class JobFamilies {
 	 */
 	public static final Object INDEX_DIFF_CACHE_UPDATE = new Object();
 
+	/**
+	 * Job family for auto share job
+	 */
+	public static final Object AUTO_SHARE = new Object();
 }
