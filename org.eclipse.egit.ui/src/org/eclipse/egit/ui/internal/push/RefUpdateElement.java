dbe36117e89d70830b53a7784418f17bbd30b8f1
Remy Suen <remysuen@ca.ibm.com>
2011-10-23 00:55:05 +0200

    Ignore the name of the source ref if it is a deletion request
    For ref deletion updates, the source ref is 'null'. As such, the
    label provider should ignore the source ref and only make use of
    the destination ref for figuring out how the label should be
    rendered.
    
    Bug: 358889
    Change-Id: I937541192d55481b22a0478c9622fc6eccb1fbb9
    Signed-off-by: Remy Suen <remysuen@ca.ibm.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/push/RefUpdateElement.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/push/RefUpdateElement.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2008, Marek Zawirski <marek.zawirski@gmail.com>
+ * Copyright (C) 2008, 2011 Marek Zawirski <marek.zawirski@gmail.com> and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -223,18 +223,19 @@ class RefUpdateElement extends WorkbenchAdapter {
 
 		styled.append(shortenRef(remote));
 
-		if (!tag) {
+		if (!tag && local != null) {
 			styled.append(": ", StyledString.QUALIFIER_STYLER); //$NON-NLS-1$
 			styled.append(shortenRef(local), StyledString.QUALIFIER_STYLER);
 		}
 		styled.append(' ');
 		// Include uri if more than one
-		if (result.getURIs().size() > 1)
+		if (result.getURIs().size() > 1) {
 			styled.append(MessageFormat.format(
 					UIText.RefUpdateElement_UrisDecoration, uri.toString()),
 					StyledString.QUALIFIER_STYLER);
+			styled.append(' ');
+		}
 
-		styled.append(' ');
 		switch (getStatus()) {
 		case OK:
 			if (update.isDelete())
