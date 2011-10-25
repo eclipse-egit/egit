6f76b96d20d443176c521f985e06acb1702023b5
Jens Baumgart <jens.baumgart@sap.com>
2011-10-25 16:14:01 +0200

    Fix NPE in CommitMessageViewer
    An NPE occurred in CommitMessageViewer during execution of the UI tests.
    
    Change-Id: I63c727c9f484717b3dcc1592c6a228c0adde5fbf
    Signed-off-by: Jens Baumgart <jens.baumgart@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitMessageViewer.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitMessageViewer.java
@@ -242,6 +242,8 @@ class CommitMessageViewer extends TextViewer implements
 					final FormatJob job = (FormatJob) event.getJob();
 					getTextWidget().getDisplay().asyncExec(new Runnable() {
 						public void run() {
+							if (getTextWidget().isDisposed())
+								return;
 							setDocument(new Document(job.getFormatResult().getCommitInfo()));
 							getTextWidget().setStyleRanges(job.getFormatResult().getStyleRange());
 						}
