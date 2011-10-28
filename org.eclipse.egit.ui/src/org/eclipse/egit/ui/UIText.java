5b507dfc8df3c112e625e0b225c0972a6a7423f0
Stefan Lay <stefan.lay@sap.com>
2011-10-28 13:04:18 +0200

    Fix exception when no review notes are available
    When there has not yet been done a code review on the remote Gerrit
    fetch throws an exception when there is the refspec
    refs/notes/review:refs/notes/review. Since it is standard git behavior
    to expect a ref on the remote if there is no wildcard in the refspec we
    do not add this refspec in this case. 
    
    Bug: 357781
    Change-Id: I14a65273a43ae53640d822b26f44e8afc558a4bd
    Signed-off-by: Stefan Lay <stefan.lay@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -313,6 +313,12 @@ public class UIText extends NLS {
 	public static String GitCloneWizard_errorCannotCreate;
 
 	/** */
+	public static String GitCloneWizard_MissingNotesMessage;
+
+	/** */
+	public static String GitCloneWizard_MissingNotesTitle;
+
+	/** */
 	public static String GitDecoratorPreferencePage_bindingRepositoryNameFlag;
 
 	/** */
