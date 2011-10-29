ee879e4522b317d0a31351b58079a8fe41f9a716
Robin Rosenberg <robin.rosenberg@dewire.com>
2011-10-29 09:42:07 +0200

    Use GitDateFormatter from JGit for formatting
    This also changes the default long format to be locale dependent
    
    Bug: 359961
    Change-Id: Ifb31dc38ea673b096869437b03e3c738fa89a07c

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitHistoryPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitHistoryPage.java
@@ -595,12 +595,12 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener {
 
 	// react on changes to the relative date preference
 	private final IPropertyChangeListener listener = new IPropertyChangeListener() {
+		@SuppressWarnings("boxing")
 		public void propertyChange(PropertyChangeEvent event) {
 			if (UIPreferences.RESOURCEHISTORY_SHOW_RELATIVE_DATE.equals(event
 					.getProperty()))
-				if (graph.setRelativeDate(((Boolean) event.getNewValue())
-						.booleanValue()))
-					graph.getTableView().refresh();
+				graph.setRelativeDate(((Boolean) event.getNewValue()));
+				graph.getTableView().refresh();
 			}
 	};
 
