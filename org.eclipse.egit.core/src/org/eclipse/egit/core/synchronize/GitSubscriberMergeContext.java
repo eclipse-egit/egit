175a68c847bbcc61dcee5f5c8d91e7a3fd2c3c3d
Dariusz Luksza <dariusz@luksza.org>
2011-10-30 22:33:09 +0100

    [sync] Simplify operations on change actions
    Simplify operations when repository changed occurs. Also use
    IndexDiffChangedListener for retrieving information about changed
    resource (in this case break point add/remove events are not
    propagated).
    
    Bug: 358898
    Change-Id: I6ae88da36dffaa913dc37525aec5ad8dca240fe0
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/GitSubscriberMergeContext.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/GitSubscriberMergeContext.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2010, Dariusz Luksza <dariusz@luksza.org>
+ * Copyright (C) 2010,2011 Dariusz Luksza <dariusz@luksza.org>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -11,22 +11,21 @@ package org.eclipse.egit.core.synchronize;
 import java.io.IOException;
 
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IResourceChangeEvent;
-import org.eclipse.core.resources.IResourceChangeListener;
-import org.eclipse.core.resources.IResourceDelta;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.resources.mapping.ResourceTraversal;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.egit.core.Activator;
 import org.eclipse.egit.core.CoreText;
+import org.eclipse.egit.core.internal.indexdiff.IndexDiffChangedListener;
+import org.eclipse.egit.core.internal.indexdiff.IndexDiffData;
 import org.eclipse.egit.core.op.AddToIndexOperation;
 import org.eclipse.egit.core.project.GitProjectData;
 import org.eclipse.egit.core.project.RepositoryChangeListener;
 import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.egit.core.synchronize.dto.GitSynchronizeData;
 import org.eclipse.egit.core.synchronize.dto.GitSynchronizeDataSet;
+import org.eclipse.jgit.lib.Repository;
 import org.eclipse.team.core.diff.IDiff;
 import org.eclipse.team.core.mapping.ISynchronizationScopeManager;
 import org.eclipse.team.core.subscribers.SubscriberMergeContext;
@@ -40,7 +39,7 @@ public class GitSubscriberMergeContext extends SubscriberMergeContext {
 
 	private final RepositoryChangeListener repoChangeListener;
 
-	private final IResourceChangeListener resourceChangeListener;
+	private final IndexDiffChangedListener indexDiffChangeListener;
 
 	/**
 	 * @param subscriber
@@ -55,24 +54,18 @@ public class GitSubscriberMergeContext extends SubscriberMergeContext {
 
 		repoChangeListener = new RepositoryChangeListener() {
 			public void repositoryChanged(RepositoryMapping which) {
-				update(subscriber, which);
+				handleRepositoryChange(subscriber, which);
 			}
 		};
-		resourceChangeListener = new IResourceChangeListener() {
+		indexDiffChangeListener = new IndexDiffChangedListener() {
 
-			public void resourceChanged(IResourceChangeEvent event) {
-				if (event.getDelta() == null)
-					return;
-
-				for (IResourceDelta delta : event.getDelta().getAffectedChildren()) {
-					RepositoryMapping repo = RepositoryMapping.getMapping(delta.getResource());
-					if (repo != null)
-						update(subscriber, repo);
-				}
+			public void indexDiffChanged(Repository repository,
+					IndexDiffData indexDiffData) {
+				handleResourceChange(subscriber, repository);
 			}
 		};
 		GitProjectData.addRepositoryChangeListener(repoChangeListener);
-		ResourcesPlugin.getWorkspace().addResourceChangeListener(resourceChangeListener);
+		Activator.getDefault().getIndexDiffCache().addIndexDiffChangedListener(indexDiffChangeListener);
 
 		initialize();
 	}
@@ -108,27 +101,33 @@ public class GitSubscriberMergeContext extends SubscriberMergeContext {
 	@Override
 	public void dispose() {
 		GitProjectData.removeRepositoryChangeListener(repoChangeListener);
-		ResourcesPlugin.getWorkspace().removeResourceChangeListener(resourceChangeListener);
+		Activator.getDefault().getIndexDiffCache().removeIndexDiffChangedListener(indexDiffChangeListener);
 		super.dispose();
 	}
 
-
-	private void update(GitResourceVariantTreeSubscriber subscriber,
-			RepositoryMapping which) {
+	private void handleRepositoryChange(
+			GitResourceVariantTreeSubscriber subscriber, RepositoryMapping which) {
 		for (GitSynchronizeData gsd : gsds) {
 			if (which.getRepository().equals(gsd.getRepository())) {
 				try {
 					gsd.updateRevs();
 				} catch (IOException e) {
-					Activator.error(
-							CoreText.GitSubscriberMergeContext_FailedUpdateRevs,
-							e);
+					Activator
+							.error(CoreText.GitSubscriberMergeContext_FailedUpdateRevs,
+									e);
 
 					return;
 				}
 
 				subscriber.reset(this.gsds);
+			}
+		}
+	}
 
+	private void handleResourceChange(GitResourceVariantTreeSubscriber subscriber,
+			Repository which) {
+		for (GitSynchronizeData gsd : gsds) {
+			if (which.equals(gsd.getRepository())) {
 				ResourceTraversal[] traversals = getScopeManager().getScope()
 						.getTraversals();
 				try {
