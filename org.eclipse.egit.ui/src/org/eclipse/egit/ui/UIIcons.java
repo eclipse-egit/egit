068b3e8b1d3ca474adb0fb11124cacb9c9aa2798
Kevin Sawicki <kevin@github.com>
2011-11-03 14:22:45 +0100

    Add action links when Git Repositories view is empty.
    Display links to clone, add existing repository, and
    create new repository to the main view area when no
    repositories are present.
    
    Change-Id: I468d9ca08a375f072fb60cbcc10b2fcee7aa3e81
    Signed-off-by: Kevin Sawicki <kevin@github.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIIcons.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIIcons.java
@@ -122,6 +122,9 @@ public class UIIcons {
 	/** New Repository button */
 	public final static ImageDescriptor NEW_REPOSITORY;
 
+	/** Create Repository button */
+	public final static ImageDescriptor CREATE_REPOSITORY;
+
 	/** Remote Repository tree node */
 	public final static ImageDescriptor REMOTE_REPOSITORY;
 
@@ -271,6 +274,7 @@ public class UIIcons {
 		OVR_ERROR = map("ovr/error.png"); //$NON-NLS-1$
 		MERGE = map("obj16/merge.gif"); //$NON-NLS-1$
 		TAG_ANNOTATED = map("obj16/annotated-tag.gif"); //$NON-NLS-1$
+		CREATE_REPOSITORY = map("etool16/createRepository.gif"); //$NON-NLS-1$
 	}
 
 	private static ImageDescriptor map(final String icon) {
