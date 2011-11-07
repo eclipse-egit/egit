cfb2a498d0b8f83df79b95e08b76189d608e2cfe
Kevin Sawicki <kevin@github.com>
2011-11-07 22:22:01 +0100

    Add all Git related views to Git perspective.
    Add history, staging, synchronize and reflog views to the Git
    perspective. Added views are placed in the bottom area behind the
    Properties view.
    
    Change-Id: Ic43ddb0b652bf1eee259775700a20674253d6ee1
    Signed-off-by: Kevin Sawicki <kevin@github.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/GitRepositoriesPerspectiveFactory.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/GitRepositoriesPerspectiveFactory.java
@@ -16,11 +16,12 @@ import org.eclipse.egit.ui.internal.repository.RepositoriesView;
 import org.eclipse.egit.ui.internal.staging.StagingView;
 import org.eclipse.team.ui.history.IHistoryView;
 import org.eclipse.team.ui.synchronize.ISynchronizeView;
+import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
 /**
- *
+ * Git Repository Exploring perspective factory
  */
 public class GitRepositoriesPerspectiveFactory implements IPerspectiveFactory {
 
@@ -29,9 +30,16 @@ public class GitRepositoriesPerspectiveFactory implements IPerspectiveFactory {
 		// repositories on the left hand
 		layout.addView(RepositoriesView.VIEW_ID, IPageLayout.LEFT, (float) 0.5, layout.getEditorArea());
 
-		// Properties under editor area
-		layout.addView(IPageLayout.ID_PROP_SHEET, IPageLayout.BOTTOM, (float) 0.5, layout
-				.getEditorArea());
+		IFolderLayout bottom = layout.createFolder(
+				"bottom", IPageLayout.BOTTOM, (float) 0.5, //$NON-NLS-1$
+				layout.getEditorArea());
+
+		// Views under editor area
+		bottom.addView(IPageLayout.ID_PROP_SHEET);
+		bottom.addView(IHistoryView.VIEW_ID);
+		bottom.addView(ISynchronizeView.VIEW_ID);
+		bottom.addView(StagingView.VIEW_ID);
+		bottom.addView(ReflogView.VIEW_ID);
 
 		// place holder for Package Explorer under repositories
 		layout.addPlaceholder("org.eclipse.jdt.ui.PackageExplorer", IPageLayout.BOTTOM, (float) 0.7, RepositoriesView.VIEW_ID); //$NON-NLS-1$
