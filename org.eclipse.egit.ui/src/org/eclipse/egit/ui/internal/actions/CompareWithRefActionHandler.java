c8f751eeddc592e77075a9424b4ba559003adcf5
Kevin Sawicki <kevin@github.com>
2011-11-08 17:27:03 -0800

    Use saveable editor input in Compare With actions
    Compare with commit and compare with ref actions
    now support editing and saving the local file input
    
    Bug: 345409
    Change-Id: I4dba2f836271f916569a44a9ad22795d13049f8b
    Signed-off-by: Kevin Sawicki <kevin@github.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CompareWithRefActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CompareWithRefActionHandler.java
@@ -21,9 +21,7 @@ import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.internal.CompareUtils;
-import org.eclipse.egit.ui.internal.FileRevisionTypedElement;
 import org.eclipse.egit.ui.internal.GitCompareFileRevisionEditorInput;
-import org.eclipse.egit.ui.internal.LocalFileRevision;
 import org.eclipse.egit.ui.internal.dialogs.CompareTargetSelectionDialog;
 import org.eclipse.egit.ui.internal.dialogs.CompareTreeView;
 import org.eclipse.jface.window.Window;
@@ -31,6 +29,7 @@ import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.team.ui.synchronize.SaveableCompareEditorInput;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 
@@ -54,8 +53,8 @@ public class CompareWithRefActionHandler extends RepositoryActionHandler {
 			if (resources.length == 1 && resources[0] instanceof IFile) {
 				final IFile baseFile = (IFile) resources[0];
 
-				final ITypedElement base = new FileRevisionTypedElement(
-						new LocalFileRevision(baseFile));
+				final ITypedElement base = SaveableCompareEditorInput
+						.createFileElement(baseFile);
 
 				final ITypedElement next;
 				try {
