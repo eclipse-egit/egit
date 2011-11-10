0120b8e741216ebac54ed9d7ca6a59055c1e0ba2
Dariusz Luksza <dariusz@luksza.org>
2011-11-10 22:50:04 +0100

    [findBugs] Remove unused field and parameter in CommitOperation
    Change-Id: I374ed66c8ad0472b48a56fa8c7a8c43b7aeae6f6
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/common/LocalRepositoryTestCase.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/common/LocalRepositoryTestCase.java
@@ -256,8 +256,8 @@ public abstract class LocalRepositoryTestCase extends EGitTestCase {
 		untracked.addAll(Arrays.asList(commitables));
 		// commit to stable
 		CommitOperation op = new CommitOperation(commitables,
-				new ArrayList<IFile>(), untracked, TestUtil.TESTAUTHOR,
-				TestUtil.TESTCOMMITTER, "Initial commit");
+				untracked, TestUtil.TESTAUTHOR, TestUtil.TESTCOMMITTER,
+				"Initial commit");
 		op.execute(null);
 
 		// now create a stable branch (from master)
@@ -436,8 +436,8 @@ public abstract class LocalRepositoryTestCase extends EGitTestCase {
 		// TODO: remove after replacing GitIndex in CommitOperation
 		waitInUI();
 		CommitOperation op = new CommitOperation(commitables,
-				new ArrayList<IFile>(), untracked, TestUtil.TESTAUTHOR,
-				TestUtil.TESTCOMMITTER, message);
+				untracked, TestUtil.TESTAUTHOR, TestUtil.TESTCOMMITTER,
+				message);
 		op.execute(null);
 	}
 
@@ -450,8 +450,8 @@ public abstract class LocalRepositoryTestCase extends EGitTestCase {
 		ArrayList<IFile> untracked = new ArrayList<IFile>();
 		untracked.addAll(Arrays.asList(commitables));
 		CommitOperation op = new CommitOperation(commitables,
-				new ArrayList<IFile>(), untracked, TestUtil.TESTAUTHOR,
-				TestUtil.TESTCOMMITTER, commitMessage);
+				untracked, TestUtil.TESTAUTHOR, TestUtil.TESTCOMMITTER,
+				commitMessage);
 		op.execute(null);
 	}
 
