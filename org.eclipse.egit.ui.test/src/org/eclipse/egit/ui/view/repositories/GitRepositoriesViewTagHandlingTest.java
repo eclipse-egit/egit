6d185698b464267c5f5d511a2b5cd156082e3196
Kevin Sawicki <kevin@github.com>
2011-11-10 10:19:25 -0800

    [repoView] Add operation to delete tags
    One or more selected tags can now be deleted by select
    the Delete Tag action in the context menu.
    
    Bug: 337046
    Change-Id: I7ac9ac819d4d1048604c936ab945b4d6fe31d6dd
    Signed-off-by: Kevin Sawicki <kevin@github.com>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/repositories/GitRepositoriesViewTagHandlingTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/repositories/GitRepositoriesViewTagHandlingTest.java
@@ -101,6 +101,52 @@ public class GitRepositoriesViewTagHandlingTest extends
 	}
 
 	@Test
+	public void testDeleteTag() throws Exception {
+		SWTBotTree tree = getOrOpenView().bot().tree();
+		int initialCount = myRepoViewUtil.getTagsItem(tree, repositoryFile)
+				.expand().rowCount();
+
+		createTag("Delete1", "The first tag");
+		refreshAndWait();
+		SWTBotTreeItem tagsItem = myRepoViewUtil.getTagsItem(tree,
+				repositoryFile).expand();
+		SWTBotTreeItem[] items = tagsItem.getItems();
+		assertEquals("Wrong number of tags", initialCount + 1, items.length);
+		tagsItem.getNode("Delete1").select().contextMenu("Delete Tag").click();
+		bot.shell(UIText.DeleteTagCommand_titleConfirm).bot()
+				.button(IDialogConstants.OK_LABEL).click();
+		TestUtil.joinJobs(JobFamilies.TAG);
+		refreshAndWait();
+		tagsItem = myRepoViewUtil.getTagsItem(tree, repositoryFile).expand();
+		items = tagsItem.getItems();
+		assertEquals("Wrong number of tags", initialCount, items.length);
+	}
+
+	@Test
+	public void testDeleteTags() throws Exception {
+		SWTBotTree tree = getOrOpenView().bot().tree();
+		int initialCount = myRepoViewUtil.getTagsItem(tree, repositoryFile)
+				.expand().rowCount();
+
+		createTag("Delete2", "The first tag");
+		createTag("Delete3", "The second tag");
+		refreshAndWait();
+		SWTBotTreeItem tagsItem = myRepoViewUtil.getTagsItem(tree,
+				repositoryFile).expand();
+		SWTBotTreeItem[] items = tagsItem.getItems();
+		assertEquals("Wrong number of tags", initialCount + 2, items.length);
+		tagsItem.select("Delete2", "Delete3");
+		ContextMenuHelper.clickContextMenu(tree, "Delete Tag");
+		bot.shell(UIText.DeleteTagCommand_titleConfirm).bot()
+				.button(IDialogConstants.OK_LABEL).click();
+		TestUtil.joinJobs(JobFamilies.TAG);
+		refreshAndWait();
+		tagsItem = myRepoViewUtil.getTagsItem(tree, repositoryFile).expand();
+		items = tagsItem.getItems();
+		assertEquals("Wrong number of tags", initialCount, items.length);
+	}
+
+	@Test
 	public void testResetToTag() throws Exception {
 		SWTBotTree tree = getOrOpenView().bot().tree();
 
