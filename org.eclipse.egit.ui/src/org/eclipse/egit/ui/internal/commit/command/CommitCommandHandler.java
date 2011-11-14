242d800ab2cce18e14edfaa50bcb1f2989d1a718
Kevin Sawicki <kevin@github.com>
2011-11-14 09:57:33 -0800

    Add cherry pick action to commit editor toolbar
    This moves the bulk of the logic of the cherry pick handler
    to a class in the commit.command package and updates the
    history handler to forward to the new commit handler.
    
    Bug: 360124
    Change-Id: Iafb4c1774ab408cb5c3a189ec12c31a94b016670
    Signed-off-by: Kevin Sawicki <kevin@github.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commit/command/CommitCommandHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commit/command/CommitCommandHandler.java
@@ -10,23 +10,16 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.commit.command;
 
-import java.util.LinkedList;
 import java.util.List;
 
-import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.egit.ui.internal.commit.RepositoryCommit;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.StructuredSelection;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.egit.ui.internal.handler.SelectionHandler;
 
 /**
  * Commit command handler
  */
-public abstract class CommitCommandHandler extends AbstractHandler {
+public abstract class CommitCommandHandler extends SelectionHandler {
 
 	/**
 	 * Get commits in current selection
@@ -35,25 +28,6 @@ public abstract class CommitCommandHandler extends AbstractHandler {
 	 * @return non-null but possibly empty list of commits
 	 */
 	protected List<RepositoryCommit> getCommits(ExecutionEvent event) {
-		List<RepositoryCommit> commits = new LinkedList<RepositoryCommit>();
-		ISelection selection = HandlerUtil.getCurrentSelection(event);
-		if (selection == null || selection.isEmpty())
-			selection = HandlerUtil.getActiveMenuSelection(event);
-		if (selection == null || selection.isEmpty()) {
-			IEditorPart editor = HandlerUtil.getActiveEditor(event);
-			if (editor != null)
-				selection = new StructuredSelection(editor);
-		}
-		if (selection instanceof IStructuredSelection && !selection.isEmpty())
-			for (Object selected : ((IStructuredSelection) selection).toArray())
-				if (selected instanceof RepositoryCommit)
-					commits.add((RepositoryCommit) selected);
-				else if (selected instanceof IAdaptable) {
-					selected = ((IAdaptable) selected)
-							.getAdapter(RepositoryCommit.class);
-					if (selected != null)
-						commits.add((RepositoryCommit) selected);
-				}
-		return commits;
+		return getSelectedItems(RepositoryCommit.class, event);
 	}
 }
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/CherryPickHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/CherryPickHandler.java
@@ -12,139 +12,35 @@
 
 package org.eclipse.egit.ui.internal.history.command;
 
-import java.text.MessageFormat;
-import java.util.Map;
-import java.util.Map.Entry;
-
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.MultiStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.egit.core.op.CherryPickOperation;
-import org.eclipse.egit.ui.Activator;
-import org.eclipse.egit.ui.JobFamilies;
-import org.eclipse.egit.ui.UIText;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jgit.api.CherryPickResult;
-import org.eclipse.jgit.api.CherryPickResult.CherryPickStatus;
+import org.eclipse.egit.ui.internal.CommonUtils;
+import org.eclipse.egit.ui.internal.commit.RepositoryCommit;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.merge.ResolveMerger.MergeFailureReason;
 import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.osgi.util.NLS;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.PlatformUI;
 
 /**
- * Executes the CherryPick
+ * Executes a cherry pick on the currently selected commit
  */
 public class CherryPickHandler extends AbstractHistoryCommandHandler {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		RevCommit commit = (RevCommit) getSelection(getPage())
 				.getFirstElement();
+		if (commit == null)
+			return null;
 		Repository repo = getRepository(event);
-		final Shell parent = getPart(event).getSite().getShell();
-		final CherryPickOperation op = new CherryPickOperation(repo, commit);
+		if (repo == null)
+			return null;
 
-		Job job = new Job(MessageFormat.format(
-				UIText.CherryPickHandler_JobName, commit.name())) {
-			@Override
-			protected IStatus run(IProgressMonitor monitor) {
-				try {
-					op.execute(monitor);
-					CherryPickResult cherryPickResult = op.getResult();
-					RevCommit newHead = cherryPickResult.getNewHead();
-					if (newHead != null
-							&& cherryPickResult.getCherryPickedRefs().isEmpty())
-						showNotPerformedDialog(parent);
-					if (newHead == null) {
-						CherryPickStatus status = cherryPickResult.getStatus();
-						switch (status) {
-						case CONFLICTING:
-							showConflictDialog(parent);
-							break;
-						case FAILED:
-							showFailure(cherryPickResult);
-							break;
-						case OK:
-							break;
-						}
-					}
-				} catch (CoreException e) {
-					Activator.logError(
-							UIText.CherryPickOperation_InternalError, e);
-				}
-				return Status.OK_STATUS;
-			}
-
-			@Override
-			public boolean belongsTo(Object family) {
-				if (JobFamilies.CHERRY_PICK.equals(family))
-					return true;
-				return super.belongsTo(family);
-			}
-		};
-		job.setUser(true);
-		job.setRule(op.getSchedulingRule());
-		job.schedule();
+		final IStructuredSelection selected = new StructuredSelection(
+				new RepositoryCommit(repo, commit));
+		CommonUtils
+				.runCommand(
+						org.eclipse.egit.ui.internal.commit.command.CherryPickHandler.ID,
+						selected);
 		return null;
-	}
-
-	private void showNotPerformedDialog(final Shell shell) {
-		PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
-
-			public void run() {
-				MessageDialog.openWarning(shell,
-						UIText.CherryPickHandler_NoCherryPickPerformedTitle,
-						UIText.CherryPickHandler_NoCherryPickPerformedMessage);
-			}
-		});
-	}
-
-	private void showConflictDialog(final Shell shell) {
-		PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
-
-			public void run() {
-				MessageDialog.openWarning(shell,
-						UIText.CherryPickHandler_CherryPickConflictsTitle,
-						UIText.CherryPickHandler_CherryPickConflictsMessage);
-			}
-		});
-	}
-
-	private void showFailure(CherryPickResult result) {
-		IStatus details = getErrorList(result.getFailingPaths());
-		Activator.showErrorStatus(
-				UIText.CherryPickHandler_CherryPickFailedMessage, details);
-	}
-
-	private IStatus getErrorList(Map<String, MergeFailureReason> failingPaths) {
-		MultiStatus result = new MultiStatus(Activator.getPluginId(),
-				IStatus.ERROR,
-				UIText.CherryPickHandler_CherryPickFailedMessage, null);
-		for (Entry<String, MergeFailureReason> entry : failingPaths.entrySet()) {
-			String path = entry.getKey();
-			String reason = getReason(entry.getValue());
-			String errorMessage = NLS.bind(
-					UIText.CherryPickHandler_ErrorMsgTemplate, path, reason);
-			result.add(Activator.createErrorStatus(errorMessage));
-		}
-		return result;
-	}
-
-	private String getReason(MergeFailureReason mergeFailureReason) {
-		switch (mergeFailureReason) {
-		case COULD_NOT_DELETE:
-			return UIText.CherryPickHandler_CouldNotDeleteFile;
-		case DIRTY_INDEX:
-			return UIText.CherryPickHandler_IndexDirty;
-		case DIRTY_WORKTREE:
-			return UIText.CherryPickHandler_WorktreeDirty;
-		}
-		return UIText.CherryPickHandler_unknown;
 	}
 }
