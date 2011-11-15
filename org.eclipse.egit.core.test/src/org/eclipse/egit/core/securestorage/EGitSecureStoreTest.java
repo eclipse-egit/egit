70de347844d96aa07dc84985f9e5c3e9ec5c911c
Matthias Sohn <matthias.sohn@sap.com>
2011-11-15 18:05:14 +0100

    Fix lookup of securestore entry with protocol's default port
    With URIs using the protocol's default port we failed to find the URI 
    in the secure store if we stored it without port number and tried to
    retrieve it with port number or vice versa.
    
    Change-Id: I43e0f3962e4e0b86e8a7fd185ea57bebdd03d0c1
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.core.test/src/org/eclipse/egit/core/securestorage/EGitSecureStoreTest.java
+++ org.eclipse.egit.core.test/src/org/eclipse/egit/core/securestorage/EGitSecureStoreTest.java
@@ -131,6 +131,61 @@ public class EGitSecureStoreTest {
 	}
 
 	@Test
+	public void testEnsureDefaultPortHttp() throws Exception {
+		URIish uri = new URIish("http://testRepo.example.com/testrepo");
+		UserPasswordCredentials credentials = new UserPasswordCredentials(
+				"agitter", "letmein");
+		store.putCredentials(uri, credentials);
+		URIish uri2 = new URIish("http://testRepo.example.com:80/testrepo");
+		assertEquals(credentials.getUser(), store.getCredentials(uri2).getUser());
+		assertEquals(credentials.getPassword(), store.getCredentials(uri2).getPassword());
+	}
+
+	@Test
+	public void testEnsureDefaultPortHttps() throws Exception {
+		URIish uri = new URIish("https://testRepo.example.com/testrepo");
+		UserPasswordCredentials credentials = new UserPasswordCredentials(
+				"agitter", "letmein");
+		store.putCredentials(uri, credentials);
+		URIish uri2 = new URIish("https://testRepo.example.com:443/testrepo");
+		assertEquals(credentials.getUser(), store.getCredentials(uri2).getUser());
+		assertEquals(credentials.getPassword(), store.getCredentials(uri2).getPassword());
+	}
+
+	@Test
+	public void testEnsureDefaultPortSftp() throws Exception {
+		URIish uri = new URIish("sftp://testRepo.example.com/testrepo");
+		UserPasswordCredentials credentials = new UserPasswordCredentials(
+				"agitter", "letmein");
+		store.putCredentials(uri, credentials);
+		URIish uri2 = new URIish("sftp://testRepo.example.com:22/testrepo");
+		assertEquals(credentials.getUser(), store.getCredentials(uri2).getUser());
+		assertEquals(credentials.getPassword(), store.getCredentials(uri2).getPassword());
+	}
+
+	@Test
+	public void testEnsureDefaultPortFtp() throws Exception {
+		URIish uri = new URIish("ftp://testRepo.example.com/testrepo");
+		UserPasswordCredentials credentials = new UserPasswordCredentials(
+				"agitter", "letmein");
+		store.putCredentials(uri, credentials);
+		URIish uri2 = new URIish("ftp://testRepo.example.com:21/testrepo");
+		assertEquals(credentials.getUser(), store.getCredentials(uri2).getUser());
+		assertEquals(credentials.getPassword(), store.getCredentials(uri2).getPassword());
+	}
+
+	@Test
+	public void testEnsureDefaultPortSsh() throws Exception {
+		URIish uri = new URIish("ssh://agitter@testRepo.example.com/testrepo");
+		UserPasswordCredentials credentials = new UserPasswordCredentials(
+				"agitter", "letmein");
+		store.putCredentials(uri, credentials);
+		URIish uri2 = new URIish("ssh://testRepo.example.com:22/testrepo");
+		assertEquals(credentials.getUser(), store.getCredentials(uri2).getUser());
+		assertEquals(credentials.getPassword(), store.getCredentials(uri2).getPassword());
+	}
+
+	@Test
 	public void testClearCredentialsTwice() throws Exception {
 		URIish uri = new URIish("http://testRepo.example.com/testrepo");
 		UserPasswordCredentials credentials = new UserPasswordCredentials(
