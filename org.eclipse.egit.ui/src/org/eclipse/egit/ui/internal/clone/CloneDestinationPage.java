432880d6cf9f90cd959a173eac2fc0165ac2e452
Kevin Sawicki <kevin@github.com>
2011-11-18 00:41:24 +0100

    Support automatic project import after clone
    Adds a checkbox to the clone destination wizard page
    that will import all projects found in the repository
    into the workspace when the clone operation completes.
    
    Change-Id: I6c24e6d1dff584d71413962acc1c465ba23e6928
    Signed-off-by: Kevin Sawicki <kevin@github.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/CloneDestinationPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/CloneDestinationPage.java
@@ -21,6 +21,7 @@ import org.eclipse.egit.ui.UIPreferences;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.internal.components.RepositorySelection;
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ComboViewer;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -43,7 +44,9 @@ import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.IWorkingSet;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.dialogs.WorkingSetGroup;
 
 /**
  * Wizard page that allows the user entering the location of a repository to be
@@ -59,11 +62,17 @@ class CloneDestinationPage extends WizardPage {
 
 	private Ref validatedHEAD;
 
+	private boolean showProjectImport;
+
 	private ComboViewer initialBranch;
 
 	private Text directoryText;
 
 	private Text remoteText;
+
+	private Button importProjectsButton;
+
+	private WorkingSetGroup workingSetGroup;
 
 	private String helpContext = null;
 
@@ -80,6 +89,9 @@ class CloneDestinationPage extends WizardPage {
 
 		createDestinationGroup(panel);
 		createConfigGroup(panel);
+		if (showProjectImport)
+			createProjectGroup(panel);
+
 		Dialog.applyDialogFont(panel);
 		setControl(panel);
 		checkPage();
@@ -188,6 +200,34 @@ class CloneDestinationPage extends WizardPage {
 		});
 	}
 
+	private void createProjectGroup(final Composite parent) {
+		final Group group = createGroup(parent,
+				UIText.CloneDestinationPage_groupProjects);
+
+		GridLayoutFactory.swtDefaults().applyTo(group);
+		importProjectsButton = new Button(group, SWT.CHECK);
+		importProjectsButton.setText(UIText.CloneDestinationPage_importButton);
+		importProjectsButton.setSelection(Activator.getDefault()
+				.getPreferenceStore()
+				.getBoolean(UIPreferences.CLONE_WIZARD_IMPORT_PROJECTS));
+		importProjectsButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				Activator
+						.getDefault()
+						.getPreferenceStore()
+						.setValue(UIPreferences.CLONE_WIZARD_IMPORT_PROJECTS,
+								importProjectsButton.getSelection());
+			}
+		});
+
+		// TODO: replace hardcoded ids once bug 245106 is fixed
+		String[] workingSetTypes = new String[] {
+				"org.eclipse.ui.resourceWorkingSetPage", //$NON-NLS-1$
+				"org.eclipse.jdt.ui.JavaWorkingSetPage" //$NON-NLS-1$
+		};
+		workingSetGroup = new WorkingSetGroup(group, null, workingSetTypes);
+	}
+
 	private static Group createGroup(final Composite parent, final String text) {
 		final Group g = new Group(parent, SWT.NONE);
 		final GridLayout layout = new GridLayout();
@@ -207,6 +247,23 @@ class CloneDestinationPage extends WizardPage {
 
 	private static GridData createFieldGridData() {
 		return new GridData(SWT.FILL, SWT.DEFAULT, true, false);
+	}
+
+	/**
+	 * @return true to import projects, false otherwise
+	 */
+	public boolean isImportProjects() {
+		return importProjectsButton != null
+				&& importProjectsButton.getSelection();
+	}
+
+	/**
+	 * @return selected working sets
+	 */
+	public IWorkingSet[] getWorkingSets() {
+		if (workingSetGroup == null)
+			return new IWorkingSet[0];
+		return workingSetGroup.getSelectedWorkingSets();
 	}
 
 	/**
@@ -338,4 +395,14 @@ class CloneDestinationPage extends WizardPage {
 		checkPage();
 	}
 
+	/**
+	 * Set whether to show project import options
+	 *
+	 * @param show
+	 * @return this wizard page
+	 */
+	public CloneDestinationPage setShowProjectImport(boolean show) {
+		showProjectImport = show;
+		return this;
+	}
 }
