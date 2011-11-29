fe0b5bc069b451aca71d2ddae595ee1f803b4ef0
Kevin Sawicki <kevin@github.com>
2011-11-29 13:14:49 -0800

    Add missing unit tests to AllLocalTests
    Adds two new suites for search and commit unit tests
    
    Change-Id: Id32f4b0667142c12fdc4477f69c4a3181c4bf4ca

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/AllLocalTests.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/AllLocalTests.java
@@ -12,6 +12,8 @@ import org.eclipse.egit.ui.httpauth.PushTest;
 import org.eclipse.egit.ui.internal.dialogs.SpellcheckableMessageAreaTest;
 import org.eclipse.egit.ui.operations.GitScopeUtilTest;
 import org.eclipse.egit.ui.prefpages.configuration.GlobalConfigurationPageTest;
+import org.eclipse.egit.ui.search.SearchTests;
+import org.eclipse.egit.ui.test.commit.CommitTests;
 import org.eclipse.egit.ui.test.history.HistoryViewTest;
 import org.eclipse.egit.ui.test.team.actions.AllTeamActionTests;
 import org.eclipse.egit.ui.test.trace.TraceConfigurationDialogTest;
@@ -38,7 +40,9 @@ import org.junit.runners.Suite.SuiteClasses;
 		SpellcheckableMessageAreaTest.class,
 		TraceConfigurationDialogTest.class,
 		SynchronizeViewWorkspaceModelTest.class,
-		SynchronizeViewGitChangeSetModelTest.class })
+		SynchronizeViewGitChangeSetModelTest.class,
+		CommitTests.class,
+		SearchTests.class })
 public class AllLocalTests {
 	// empty class, don't need anything here
 }
