6e43df8a5721cc4eef23465fedbdac20c3eb0220
Kevin Sawicki <kevin@github.com>
2011-12-01 00:18:44 +0100

    [reflogView] Guard against empty selection in CheckoutHandler
    This occurs when right-clicking an empty RefLog view and running
    the action causing a NPE to be logged since the selection is empty
    
    Change-Id: Ie4b3c9884b9529a6b099acf192c7993087153ddc
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/reflog/command/CheckoutHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/reflog/command/CheckoutHandler.java
@@ -13,12 +13,12 @@ import java.io.IOException;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.egit.ui.internal.branch.BranchOperationUI;
-import org.eclipse.jgit.errors.IncorrectObjectTypeException;
-import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.ReflogEntry;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
  * Checkout handler
@@ -26,17 +26,16 @@ import org.eclipse.jgit.storage.file.ReflogEntry;
 public class CheckoutHandler extends AbstractReflogCommandHandler {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		ReflogEntry entry = (ReflogEntry) getSelection(getView())
-				.getFirstElement();
+		ReflogEntry entry = (ReflogEntry) ((IStructuredSelection) HandlerUtil
+				.getCurrentSelectionChecked(event)).getFirstElement();
+		if (entry == null)
+			return null;
+
 		Repository repo = getRepository(event);
 		RevCommit commit = null;
 		try {
 			RevWalk w = new RevWalk(repo);
 			commit = w.parseCommit(entry.getNewId());
-		} catch (MissingObjectException e) {
-			throw new ExecutionException(e.getMessage(), e);
-		} catch (IncorrectObjectTypeException e) {
-			throw new ExecutionException(e.getMessage(), e);
 		} catch (IOException e) {
 			throw new ExecutionException(e.getMessage(), e);
 		}
