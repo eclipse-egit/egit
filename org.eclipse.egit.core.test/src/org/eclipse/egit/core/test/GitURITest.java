eclipse-egit/egit

commit 199adc61d412c5eb13088b741a673de5debd2335

Allow GitProjectSetCapability to accept SCM URIs
Bug: 356448
Change-Id: I04bcf212469f4a9422fd7eb839f874f55cf3f92c
Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

org.eclipse.egit.core/src/org/eclipse/egit/core/internal/GitURI.java
@@ -1,0 +1,121 @@
+/*******************************************************************************
+ * Copyright (c) 2011, IBM Corporation
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Tomasz Zarna (IBM) - initial implementation
+ *******************************************************************************/
+package org.eclipse.egit.core.internal;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.egit.core.Activator;
+import org.eclipse.egit.core.CoreText;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.transport.URIish;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.team.core.ProjectSetCapability;
+
+/**
+ * This URI like construct used for a Git SCM URL. See
+ * http://maven.apache.org/scm/scm-url-format.html for the format description.
+ */
+public class GitURI {
+	private static final String SCHEME_GIT = "git"; //$NON-NLS-1$
+
+	private static final String KEY_PATH = "path"; //$NON-NLS-1$
+
+	private static final String KEY_PROJECT = "project"; //$NON-NLS-1$
+
+	private static final String KEY_TAG = "tag"; //$NON-NLS-1$
+
+	private final URIish repository;
+
+	private IPath path;
+
+	private String tag;
+
+	private String projectName;
+
+	/**
+	 * Construct the {@link GitURI} for the given URI.
+	 *
+	 * @param uri
+	 *            the URI in the SCM URL format
+	 */
+	public GitURI(URI uri) {
+		try {
+			if (ProjectSetCapability.SCHEME_SCM.equals(uri.getScheme())) {
+				final String ssp = uri.getSchemeSpecificPart();
+				if (ssp.startsWith(SCHEME_GIT)) {
+					int indexOfSemicolon = ssp.indexOf(';');
+					URIish r = new URIish(ssp.substring(
+							SCHEME_GIT.length() + 1, indexOfSemicolon));
+					IPath p = null;
+					String t = Constants.MASTER; // default
+					String pn = null;
+					String[] params = ssp.substring(indexOfSemicolon)
+							.split(";"); //$NON-NLS-1$
+					for (String param : params) {
+						if (param.startsWith(KEY_PATH + '=')) {
+							p = new Path(
+									param.substring(param.indexOf('=') + 1));
+						} else if (param.startsWith(KEY_TAG + '=')) {
+							t = param.substring(param.indexOf('=') + 1);
+						} else if (param.startsWith(KEY_PROJECT + '=')) {
+							pn = param.substring(param.indexOf('=') + 1);
+						}
+					}
+					this.repository = r;
+					this.path = p;
+					this.tag = t;
+					this.projectName = pn;
+					return;
+				}
+			}
+			throw new IllegalArgumentException(NLS.bind(
+					CoreText.GitURI_InvalidSCMURL,
+					new String[] { uri.toString() }));
+		} catch (URISyntaxException e) {
+			Activator.logError(e.getMessage(), e);
+			throw new IllegalArgumentException(NLS.bind(
+					CoreText.GitURI_InvalidURI, new String[] { uri.toString(),
+							e.getMessage() }));
+		}
+	}
+
+	/**
+	 * @return path
+	 */
+	public IPath getPath() {
+		return path;
+	}
+
+	/**
+	 * @return repository
+	 */
+	public URIish getRepository() {
+		return repository;
+	}
+
+	/**
+	 * @return tag
+	 */
+	public String getTag() {
+		return tag;
+	}
+
+	/**
+	 * @return project name
+	 */
+	public String getProjectName() {
+		return projectName;
+	}
+}

org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/GitURITest.java
@@ -1,0 +1,108 @@
+/*******************************************************************************
+ * Copyright (c) 2011, IBM Corporation
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Tomasz Zarna (IBM) - initial implementation
+ *******************************************************************************/
+package org.eclipse.egit.core.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.net.URI;
+
+import org.eclipse.egit.core.GitProjectSetCapability;
+import org.eclipse.egit.core.internal.GitURI;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.transport.URIish;
+import org.eclipse.team.core.ScmUrlImportDescription;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GitURITest {
+
+	private GitProjectSetCapability capability;
+
+	@Before
+	public void setUp() {
+		capability = new GitProjectSetCapability();
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidScmUriWithQuotationMarks() throws Exception {
+		URI.create("scm:git:git://git.eclipse.org/gitroot/platform/eclipse.platform.team.git;path=\"bundles/org.eclipse.team.core\"");
+		// expected IAE, " are not allowed in a URI reference
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidScmUriForCVS() throws Exception {
+		new GitURI(URI.create("scm:cvs:pserver:dev.eclipse.org:/cvsroot/eclipse:org.eclipse.compare"));
+		// expected IAE, it's a CVS SCM URL
+	}
+
+	// ScmUrlImportDescription can handle " in Strings expected to be URI refs
+	@Test
+	public void testScmUriWithPath() throws Exception {
+		ScmUrlImportDescription description = new ScmUrlImportDescription(
+				"scm:git:git://git.eclipse.org/gitroot/platform/eclipse.platform.team.git;path=\"bundles/org.eclipse.team.core\"",
+				null);
+		URI uri = description.getUri();
+		GitURI gitUri = new GitURI(uri);
+		assertEquals("bundles/org.eclipse.team.core", gitUri.getPath()
+				.toString());
+		URIish uriish = new URIish(
+				"git://git.eclipse.org/gitroot/platform/eclipse.platform.team.git");
+		assertEquals(uriish, gitUri.getRepository());
+		assertEquals(Constants.MASTER, gitUri.getTag());
+
+		String refString = capability.asReference(uri, "org.eclipse.team.core");
+		assertEquals(
+				"1.0,git://git.eclipse.org/gitroot/platform/eclipse.platform.team.git,master,bundles/org.eclipse.team.core",
+				refString);
+	}
+
+	@Test
+	public void testScmUriWithPathAndTag() throws Exception {
+		ScmUrlImportDescription description = new ScmUrlImportDescription(
+				"scm:git:git://git.eclipse.org/gitroot/platform/eclipse.platform.ui.git;path=\"bundles/org.eclipse.jface\";tag=v20111107-2125",
+				null);
+		URI uri = description.getUri();
+		GitURI gitUri = new GitURI(uri);
+		assertEquals("bundles/org.eclipse.jface", gitUri.getPath().toString());
+		URIish uriish = new URIish(
+				"git://git.eclipse.org/gitroot/platform/eclipse.platform.ui.git");
+		assertEquals(uriish, gitUri.getRepository());
+		assertEquals("v20111107-2125", gitUri.getTag());
+
+		String refString = capability.asReference(uri, "org.eclipse.jface");
+		assertEquals(
+				"1.0,git://git.eclipse.org/gitroot/platform/eclipse.platform.ui.git,v20111107-2125,bundles/org.eclipse.jface",
+				refString);
+	}
+
+	@Test
+	public void testScmUriWithPathProjectAndTag() throws Exception {
+		ScmUrlImportDescription description = new ScmUrlImportDescription(
+				"scm:git:git://git.eclipse.org/gitroot/equinox/rt.equinox.bundles.git;path=\"bundles/org.eclipse.equinox.http.jetty6\";project=\"org.eclipse.equinox.http.jetty\";tag=v20111010-1614",
+				null);
+		URI uri = description.getUri();
+		GitURI gitUri = new GitURI(uri);
+		assertEquals("bundles/org.eclipse.equinox.http.jetty6", gitUri
+				.getPath().toString());
+		URIish uriish = new URIish(
+				"git://git.eclipse.org/gitroot/equinox/rt.equinox.bundles.git");
+		assertEquals(uriish, gitUri.getRepository());
+		assertEquals("v20111010-1614", gitUri.getTag());
+		assertEquals("org.eclipse.equinox.http.jetty", gitUri.getProjectName());
+
+		String refString = capability.asReference(uri,
+				"org.eclipse.equinox.http.jetty");
+		assertEquals(
+				"1.0,git://git.eclipse.org/gitroot/equinox/rt.equinox.bundles.git,v20111010-1614,bundles/org.eclipse.equinox.http.jetty6",
+				refString);
+	}
+}

org.eclipse.egit.core/src/org/eclipse/egit/core/CoreText.java
@@ -350,6 +350,12 @@ public class CoreText extends NLS {
 	/** */
 	public static String GitRemoteFolder_fetchingMembers;
 
+	/** */
+	public static String GitURI_InvalidSCMURL;
+
+	/** */
+	public static String GitURI_InvalidURI;
+
 	static {
 		initializeMessages(BUNDLE_NAME,	CoreText.class);
 	}

org.eclipse.egit.core/src/org/eclipse/egit/core/GitProjectSetCapability.java
@@ -9,12 +9,14 @@
  *
  * Contributors:
  *     Manuel Doninger <manuel.doninger@googlemail.com>
+ *     Tomasz Zarna <Tomasz.Zarna@pl.ibm.com>
  *******************************************************************************/
 package org.eclipse.egit.core;
 
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -24,6 +26,7 @@ import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IWorkspace;
@@ -33,6 +36,7 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.egit.core.internal.GitURI;
 import org.eclipse.egit.core.op.CloneOperation;
 import org.eclipse.egit.core.op.ConnectProviderOperation;
 import org.eclipse.egit.core.project.RepositoryMapping;
@@ -87,6 +91,10 @@ public final class GitProjectSetCapability extends ProjectSetCapability {
 		if (projectPath.equals("")) //$NON-NLS-1$
 			projectPath = "."; //$NON-NLS-1$
 
+		return asReference(url, branch, projectPath);
+	}
+
+	private String asReference(String url, String branch, String projectPath) {
 		StringBuilder sb = new StringBuilder();
 
 		sb.append(VERSION);
@@ -205,6 +213,12 @@ public final class GitProjectSetCapability extends ProjectSetCapability {
 		final IProject[] result = importedProjects
 				.toArray(new IProject[importedProjects.size()]);
 		return result;
+	}
+
+	@Override
+	public String asReference(URI uri, String projectName) {
+		GitURI gitURI = new GitURI(uri);
+		return asReference(gitURI.getRepository().toString(), gitURI.getTag(), gitURI.getPath().toString());
 	}
 
 	private TeamException throwTeamException(Throwable th) throws TeamException{

org.eclipse.egit.core/src/org/eclipse/egit/core/coretext.properties
@@ -146,3 +146,6 @@ GitResourceVariantTreeSubscriber_fetchTaskName=Fetching data from git repositori
 GitSyncObjectCache_noData=Cache doesn''t contain data for key: {0}
 
 GitRemoteFolder_fetchingMembers=Fetching members of {0}
+
+GitURI_InvalidSCMURL=Invalid SCM URL {0}
+GitURI_InvalidURI=Invalid uri {0}: {1}

commit 25ea320918635f8a42bce9f0625592004e3edee2

Fix build broken on Galileo
Commit 199adc61d412c5eb13088b741a673de5debd2335 broke the EGit build
since it uses org.eclipse.team.core.ScmUrlImportDescription
and org.eclipse.team.core.ProjectSetCapability.SCHEME_SCM
which are only available in team framework starting with 3.6. Copy them
to fix the build until we drop support for Galileo.

Change-Id: Ia07c31b8d546fb8e3156b912bbfa3f19383e30ab
Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

org.eclipse.egit.core/src/org/eclipse/egit/core/internal/GitURI.java
@@ -21,13 +21,16 @@ import org.eclipse.egit.core.CoreText;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.transport.URIish;
 import org.eclipse.osgi.util.NLS;
-import org.eclipse.team.core.ProjectSetCapability;
 
 /**
  * This URI like construct used for a Git SCM URL. See
  * http://maven.apache.org/scm/scm-url-format.html for the format description.
  */
 public class GitURI {
+	// TODO replace with org.eclipse.team.core.ProjectSetCapability.SCHEME_SCM
+	// when we drop support for Galileo
+	private static final String SCHEME_SCM = "scm"; //$NON-NLS-1$
+
 	private static final String SCHEME_GIT = "git"; //$NON-NLS-1$
 
 	private static final String KEY_PATH = "path"; //$NON-NLS-1$
@@ -52,7 +55,7 @@ public class GitURI {
 	 */
 	public GitURI(URI uri) {
 		try {
-			if (ProjectSetCapability.SCHEME_SCM.equals(uri.getScheme())) {
+			if (SCHEME_SCM.equals(uri.getScheme())) {
 				final String ssp = uri.getSchemeSpecificPart();
 				if (ssp.startsWith(SCHEME_GIT)) {
 					int indexOfSemicolon = ssp.indexOf(';');

org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/GitURITest.java
@@ -14,12 +14,12 @@ package org.eclipse.egit.core.test;
 import static org.junit.Assert.assertEquals;
 
 import java.net.URI;
+import java.util.HashMap;
 
 import org.eclipse.egit.core.GitProjectSetCapability;
 import org.eclipse.egit.core.internal.GitURI;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.transport.URIish;
-import org.eclipse.team.core.ScmUrlImportDescription;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -105,4 +105,95 @@ public class GitURITest {
 				"1.0,git://git.eclipse.org/gitroot/equinox/rt.equinox.bundles.git,v20111010-1614,bundles/org.eclipse.equinox.http.jetty6",
 				refString);
 	}
+
+	// TODO remove this copy of org.eclipse.team.core.ScmUrlImportDescription
+	// when we drop support for Galileo
+	/**
+	 * Copy of org.eclipse.team.core.ScmUrlImportDescription to support tests until
+	 * we drop support for Galileo. DON'T USE OUTSIDE OF TEST BUNDLE!
+	 *
+	 * Describes how a bundle import will be executed. A bundle importer delegate
+	 * creates bundle import descriptions when it validates bundle manifests for
+	 * importing. The result, a set of bundle import descriptions is then passed to
+	 * TeamUI, which basing on the info from the descriptions instantiate and
+	 * initialize IScmUrlImportWizardPage pages. The pages can be used to alter the
+	 * default import configuration e.g. for bundles stored in a CVS repository the
+	 * user may want to check out HEAD rather than a specific version.
+	 * <p>
+	 * <strong>EXPERIMENTAL</strong>. This class has been added as part of a work in
+	 * progress. There is no guarantee that this API will work or that it will
+	 * remain the same. Please do not use this API without consulting with the Team
+	 * team.
+	 *
+	 * @since 3.6
+	 */
+	protected static class ScmUrlImportDescription {
+		private String url;
+		private String project;
+		private HashMap properties;
+
+		public ScmUrlImportDescription(String url, String project) {
+			this.url = url;
+			this.project = project;
+		}
+
+		/**
+		 * @return project name
+		 */
+		public String getProject() {
+			return project;
+		}
+
+		/**
+		 * SCM URL
+		 *
+		 * @return a string representation of the SCM URL
+		 */
+		public String getUrl() {
+			return url;
+		}
+
+		public URI getUri() {
+			return URI.create(url.replaceAll("\"", "")); //$NON-NLS-1$//$NON-NLS-2$
+		}
+
+		public void setUrl(String url) {
+			this.url = url;
+		}
+
+		/**
+		 * Sets or removes a client property.
+		 *
+		 * @param key
+		 *            property key
+		 * @param value
+		 *            property value or <code>null</code> to remove the property
+		 */
+		public synchronized void setProperty(String key, Object value) {
+			if (properties == null) {
+				properties = new HashMap();
+			}
+			if (value == null) {
+				properties.remove(key);
+			} else {
+				properties.put(key, value);
+			}
+
+		}
+
+		/**
+		 * Returns the specified client property, or <code>null</code> if none.
+		 *
+		 * @param key
+		 *            property key
+		 * @return property value or <code>null</code>
+		 */
+		public synchronized Object getProperty(String key) {
+			if (properties == null) {
+				return null;
+			}
+			return properties.get(key);
+		}
+	}
+
 }