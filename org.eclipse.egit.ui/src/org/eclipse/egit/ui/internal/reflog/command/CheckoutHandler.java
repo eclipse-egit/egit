a0a5a859344531e9d0d890b0603f19131bd07e48
Matthias Sohn <matthias.sohn@sap.com>
2011-12-07 00:03:55 +0100

    [reflogView] Extract getSelectedCommit()
    This allows to reuse this method in other command handlers used in the
    Reflog View.
    
    Change-Id: Ic0ef23d555ac75a78b0191b0258bc863d541bf97
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/reflog/command/CheckoutHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/reflog/command/CheckoutHandler.java
@@ -8,17 +8,12 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.reflog.command;
 
-import java.io.IOException;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.egit.ui.internal.branch.BranchOperationUI;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.revwalk.RevWalk;
-import org.eclipse.jgit.storage.file.ReflogEntry;
-import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
  * Checkout handler
@@ -26,19 +21,8 @@ import org.eclipse.ui.handlers.HandlerUtil;
 public class CheckoutHandler extends AbstractReflogCommandHandler {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		ReflogEntry entry = (ReflogEntry) ((IStructuredSelection) HandlerUtil
-				.getCurrentSelectionChecked(event)).getFirstElement();
-		if (entry == null)
-			return null;
-
 		Repository repo = getRepository(event);
-		RevCommit commit = null;
-		try {
-			RevWalk w = new RevWalk(repo);
-			commit = w.parseCommit(entry.getNewId());
-		} catch (IOException e) {
-			throw new ExecutionException(e.getMessage(), e);
-		}
+		RevCommit commit = getSelectedCommit(event, repo);
 		if (commit != null) {
 			final BranchOperationUI op = BranchOperationUI.checkout(repo,
 					commit.name());
