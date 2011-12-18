97e793480c6434cb08ea4759420b074272520ca8
Robin Rosenberg <robin.rosenberg@dewire.com>
2011-12-18 22:56:49 +0100

    Improve performance of the refs tree views
    Instead of calling getRefs over and over call it only when needed, i.e
    the first time and if the refs in a repository are updated.
    
    Change-Id: I2c9e167c7282c822f0b1cc2b50dc99143c3ba855

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoriesViewContentProvider.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoriesViewContentProvider.java
@@ -18,10 +18,13 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.Map.Entry;
+import java.util.WeakHashMap;
 
 import org.eclipse.core.commands.IStateListener;
 import org.eclipse.core.commands.State;
@@ -54,6 +57,8 @@ import org.eclipse.egit.ui.internal.repository.tree.WorkingDirNode;
 import org.eclipse.egit.ui.internal.repository.tree.command.ToggleBranchHierarchyCommand;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jgit.events.RefsChangedEvent;
+import org.eclipse.jgit.events.RefsChangedListener;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.RefDatabase;
@@ -74,6 +79,8 @@ public class RepositoriesViewContentProvider implements ITreeContentProvider,
 	private final State commandState;
 
 	private boolean branchHierarchyMode = false;
+
+	private Map<Repository, Map<String, Ref>> branchRefs = new WeakHashMap<Repository, Map<String, Ref>>();
 
 	/**
 	 * Constructs this instance
@@ -175,8 +182,7 @@ public class RepositoriesViewContentProvider implements ITreeContentProvider,
 			} else {
 				List<RepositoryTreeNode<Ref>> refs = new ArrayList<RepositoryTreeNode<Ref>>();
 				try {
-					for (Entry<String, Ref> refEntry : repo.getRefDatabase()
-							.getRefs(Constants.R_HEADS).entrySet()) {
+					for (Entry<String, Ref> refEntry : getRefs(repo, Constants.R_HEADS).entrySet()) {
 						if (!refEntry.getValue().isSymbolic())
 							refs.add(new RefNode(node, repo, refEntry
 									.getValue()));
@@ -209,8 +215,7 @@ public class RepositoriesViewContentProvider implements ITreeContentProvider,
 			} else {
 				List<RepositoryTreeNode<Ref>> refs = new ArrayList<RepositoryTreeNode<Ref>>();
 				try {
-					for (Entry<String, Ref> refEntry : repo.getRefDatabase()
-							.getRefs(Constants.R_REMOTES).entrySet()) {
+					for (Entry<String, Ref> refEntry : getRefs(repo, Constants.R_REMOTES).entrySet()) {
 						if (!refEntry.getValue().isSymbolic())
 							refs.add(new RefNode(node, repo, refEntry
 									.getValue()));
@@ -258,8 +263,7 @@ public class RepositoriesViewContentProvider implements ITreeContentProvider,
 		case ADDITIONALREFS: {
 			List<RepositoryTreeNode<Ref>> refs = new ArrayList<RepositoryTreeNode<Ref>>();
 			try {
-				for (Entry<String, Ref> refEntry : repo.getRefDatabase()
-						.getRefs(RefDatabase.ALL).entrySet()) {
+				for (Entry<String, Ref> refEntry : getRefs(repo, RefDatabase.ALL).entrySet()) {
 					String name=refEntry.getKey();
 					if (!(name.startsWith(Constants.R_HEADS) || name.startsWith(Constants.R_TAGS)|| name.startsWith(Constants.R_REMOTES)))
 						refs.add(new AdditionalRefNode(node, repo, refEntry
@@ -485,4 +489,28 @@ public class RepositoriesViewContentProvider implements ITreeContentProvider,
 			Activator.handleError(e.getMessage(), e, false);
 		}
 	}
+
+	private synchronized Map<String, Ref> getRefs(final Repository repo, final String prefix) throws IOException {
+		Map<String, Ref> allRefs = branchRefs.get(repo);
+		if (allRefs == null) {
+			allRefs = repo.getRefDatabase().getRefs(RefDatabase.ALL);
+			branchRefs.put(repo, allRefs);
+			repo.getListenerList().addRefsChangedListener(new RefsChangedListener() {
+				public void onRefsChanged(RefsChangedEvent event) {
+					synchronized (RepositoriesViewContentProvider.this) {
+						branchRefs.remove(repo);
+					}
+				}
+			});
+		}
+		if (prefix.equals(RefDatabase.ALL))
+			return allRefs;
+		Map<String, Ref> filtered = new HashMap<String, Ref>();
+		for (Map.Entry<String, Ref> entry : allRefs.entrySet()) {
+			if (entry.getKey().startsWith(prefix))
+				filtered.put(entry.getKey(), entry.getValue());
+		}
+		return filtered;
+	}
+
 }
