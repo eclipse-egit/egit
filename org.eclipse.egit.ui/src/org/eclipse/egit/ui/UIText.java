78994126715d010d7af96356101881d69f561183
Stefan Lay <stefan.lay@sap.com>
2011-12-21 11:00:02 +0100

    Redesign GitImportWizard to include contributed clone sources
    The GitImportWizard now includes the extensions of the
    org.eclipse.egit.ui.cloneSourceProvider. The flow of pages is the same
    as described in the Mock-Up which is attached to Bug 361251.
    
    Bug: 361251
    Change-Id: I957ee48121b24fd78a91fe5e2855130c4614649f
    Signed-off-by: Stefan Lay <stefan.lay@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -973,6 +973,12 @@ public class UIText extends NLS {
 	public static String RepositoryCommit_UserAndDate;
 
 	/** */
+	public static String RepositoryLocationPage_info;
+
+	/** */
+	public static String RepositoryLocationPage_title;
+
+	/** */
 	public static String RepositorySearchDialog_browse;
 
 	/** */
@@ -2981,6 +2987,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String GitImportWithDirectoriesPage_SelectFolderMessage;
+
+	/** */
+	public static String GitImportWizard_errorParsingURI;
 
 	/** */
 	public static String GitImportWizard_WizardTitle;
