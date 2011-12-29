396d18a007151e23ef2f2ebf9204790706083672
Robin Rosenberg <robin.rosenberg@dewire.com>
2011-12-29 08:07:03 +0100

    Improve abysmal performance of commit info update in history view
    When the number of branches is large, the large amounts of
    RevWalk.isMerge translates a gross a amount calls to
    MergeBaseGenerator.carryOntoOne with deep stacks. For 8200 branches,
    e.g. if you fetch refs/changes/* from Gerrit, this translates
    to 50 million calls to carryOntoOne.
    
    In real that is ~3.3 seconds for a repo with the large number
    of refs and .06 for a normal clone.
    
    Take a simpler approach and scan every branch until we find the
    selected commit or one of it's parents. After this fix info update
    with the large refs set is about .5 seconds. For normal clones
    there is no difference.
    
    Furthe improvve by using the heuristic that says commit times should
    be increasing for each new commit and therefore do not examine branches
    whose tip commit is older than the commit we are lookig at. Allow for
    a clock skew of 24 hours. After this getBranches() is down to 0.35
    seconds.
    
    Change-Id: I6de60760f1644b4e968c7b0caeffcc0007b2da33
    Signed-off-by: Robin Rosenberg <robin.rosenberg@dewire.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitInfoBuilder.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitInfoBuilder.java
@@ -21,6 +21,7 @@ import java.io.UnsupportedEncodingException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -41,8 +42,10 @@ import org.eclipse.jgit.diff.DiffFormatter;
 import org.eclipse.jgit.diff.RawText;
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
 import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.lib.AnyObjectId;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectIdSubclassMap;
 import org.eclipse.jgit.lib.PersonIdent;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
@@ -302,22 +305,48 @@ public class CommitInfoBuilder {
 		List<Ref> result = new ArrayList<Ref>();
 
 		try {
+			// searches from branches can be cut off early if any parent of the
+			// search-for commit is found. This is quite likely, so optimize for this.
+			revWalk.markStart(Arrays.asList(commit.getParents()));
+			ObjectIdSubclassMap<ObjectId> cutOff = new ObjectIdSubclassMap<ObjectId>();
 			Map<String, Ref> refsMap = new HashMap<String, Ref>();
 			refsMap.putAll(db.getRefDatabase().getRefs(Constants.R_HEADS));
 			// add remote heads to search
 			refsMap.putAll(db.getRefDatabase().getRefs(Constants.R_REMOTES));
 
+			final int SKEW = 24*3600; // one day clock skew
+
 			for (Ref ref : refsMap.values()) {
 				RevCommit headCommit = revWalk.parseCommit(ref.getObjectId());
-				// the base RevCommit also must be allocated using same RevWalk
-				// instance,
-				// otherwise isMergedInto returns wrong result!
-				RevCommit base = revWalk.parseCommit(commit);
 
-				if (revWalk.isMergedInto(base, headCommit))
-					result.add(ref); // commit is reachable
-				// from this head
+				// if commit is in the ref branch, then the tip of ref should be
+				// newer than the commit we are looking for. Allow for a large
+				// clock skew.
+				if (headCommit.getCommitTime() + SKEW < commit.getCommitTime())
+					continue;
+
+				List<ObjectId> maybeCutOff = new ArrayList<ObjectId>(cutOff.size()); // guess rough size
+				revWalk.resetRetain();
+				revWalk.markStart(headCommit);
+				RevCommit current;
+				Ref found = null;
+				while ((current = revWalk.next()) != null) {
+					if (AnyObjectId.equals(current, commit)) {
+						found = ref;
+						break;
+					}
+					if (cutOff.contains(current))
+						break;
+					maybeCutOff.add(current.toObjectId());
+				}
+				if (found != null)
+					result.add(ref);
+				else
+					for (ObjectId id : maybeCutOff)
+						cutOff.addIfAbsent(id);
+
 			}
+			revWalk.dispose();
 		} catch (IOException e) {
 			// skip exception
 		}
