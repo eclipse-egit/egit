4e7bde8e17220b1508a3972d489e9ee40dc35f2b
Kevin Sawicki <kevin@github.com>
2012-01-02 16:54:08 -0800

    Use wizard for adding existing Git repositories
    The following elements have been changed in the workflow:
    
      - Wizard progress bar used instead of modal progress dialog
      - Search button moved to the right of Browse... button
      - Search results updated automatically after closing browse dialog
      - Wizard initially opens with results of pre-filled path
    
    Bug: 342590
    Change-Id: I5aabb6afafc52096fd7467112cd31220b9cd6047

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositorySearchWizard.java
@@ -0,0 +1,57 @@
+/******************************************************************************
+ *  Copyright (c) 2011 GitHub Inc.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *    Kevin Sawicki (GitHub Inc.) - initial API and implementation
+ *****************************************************************************/
+package org.eclipse.egit.ui.internal.repository;
+
+import java.util.Collection;
+import java.util.Set;
+
+import org.eclipse.egit.ui.UIText;
+import org.eclipse.jface.wizard.Wizard;
+
+/**
+ * Repository search wizard
+ */
+public class RepositorySearchWizard extends Wizard {
+
+	private final Collection<String> dirs;
+
+	private RepositorySearchDialog searchPage;
+
+	/**
+	 * Create repository search wizard
+	 *
+	 * @param existingDirs
+	 */
+	public RepositorySearchWizard(Collection<String> existingDirs) {
+		dirs = existingDirs;
+		setWindowTitle(UIText.RepositorySearchDialog_AddGitRepositories);
+		setNeedsProgressMonitor(true);
+	}
+
+	@Override
+	public void addPages() {
+		searchPage = new RepositorySearchDialog(dirs, true);
+		addPage(searchPage);
+	}
+
+	/**
+	 * Get selected directories
+	 *
+	 * @return directories
+	 */
+	public Set<String> getDirectories() {
+		return searchPage.getDirectories();
+	}
+
+	public boolean performFinish() {
+		return true;
+	}
+}
