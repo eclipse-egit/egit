df5d2aa79fd6612ddfedc5861e03a05d6aadd7d3
Kevin Sawicki <kevin@github.com>
2012-01-09 08:44:03 -0800

    [RFC] Recover from lock failed exceptions
    This begins the work to prompt the user to delete the .lock
    file left over by a crashed process that was holding it.
    
    A new LockOperation is added that wraps an IEGitOperation
    and prompts the user to delete the lock failure being reported
    in a wrapped CoreException.
    
    This commit only currently modified ResetCommand but if this
    approach  works then other commands can begin wrapping
    core operations in a LockOperation.
    
    JGit-Dependency: I580166d386126bfb54a318a65253070a6e325936
    Bug: 360141
    Change-Id: Icdd58cc2d7606b23ca1cac550ca287f94f072312

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/operations/LockOperation.java
@@ -0,0 +1,71 @@
+/******************************************************************************
+ *  Copyright (c) 2011 GitHub Inc.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *    Kevin Sawicki (GitHub Inc.) - initial API and implementation
+ *****************************************************************************/
+package org.eclipse.egit.ui.internal.operations;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.jobs.ISchedulingRule;
+import org.eclipse.egit.core.op.IEGitOperation;
+import org.eclipse.egit.ui.UIUtils;
+import org.eclipse.jgit.errors.LockFailedException;
+
+/**
+ * Operation that wraps another operation and handles lock failures
+ */
+public class LockOperation implements IEGitOperation {
+
+	private final IEGitOperation op;
+
+	/**
+	 * Create lock operation
+	 *
+	 * @param op
+	 */
+	public LockOperation(final IEGitOperation op) {
+		this.op = op;
+	}
+
+	/**
+	 * Unwind a {@link CoreException} to a possibly nested
+	 * {@link LockFailedException}
+	 *
+	 * @param e
+	 * @return lock failed exception or null if no cause found
+	 */
+	public LockFailedException unwind(CoreException e) {
+		Throwable cause = e.getCause();
+		while (!(cause instanceof LockFailedException)) {
+			Throwable parent = cause.getCause();
+			if (parent == null || parent == cause)
+				return null;
+			else
+				cause = parent;
+		}
+		if (cause instanceof LockFailedException)
+			return (LockFailedException) cause;
+		else
+			return null;
+	}
+
+	public void execute(IProgressMonitor monitor) throws CoreException {
+		try {
+			op.execute(monitor);
+		} catch (CoreException e) {
+			LockFailedException lockException = unwind(e);
+			if (lockException != null)
+				UIUtils.handleLockDelete(lockException);
+		}
+	}
+
+	public ISchedulingRule getSchedulingRule() {
+		return op.getSchedulingRule();
+	}
+}
