ce1a96bf852c51081a5976cc2630501a3994762c
Kevin Sawicki <kevin@github.com>
2012-01-10 16:36:45 -0800

    [RFC] Recover from lock failed exceptions
    This begins the work to prompt the user to delete the .lock
    file left over by a crashed process that was holding it.
    
    A new LockOperation is added that wraps an IEGitOperation
    and prompts the user to delete the lock failure being reported
    in a wrapped CoreException.
    
    This commit only currently modified ResetCommand but if this
    approach  works then other commands can begin wrapping
    core operations in a LockOperation.
    
    Bug: 360141
    Change-Id: Icdd58cc2d7606b23ca1cac550ca287f94f072312

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/ResetCommand.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/ResetCommand.java
@@ -23,6 +23,7 @@ import org.eclipse.egit.core.op.ResetOperation.ResetType;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.JobFamilies;
 import org.eclipse.egit.ui.UIText;
+import org.eclipse.egit.ui.internal.operations.LockOperation;
 import org.eclipse.egit.ui.internal.repository.SelectResetTypePage;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
 import org.eclipse.jface.dialogs.MessageDialog;
@@ -92,8 +93,10 @@ public class ResetCommand extends
 									final ResetOperation operation = new ResetOperation(
 											node.getRepository(), targetBranch,
 											resetType);
-									JobUtil.scheduleUserJob(operation, jobname,
-											JobFamilies.RESET);
+									final LockOperation lockWrapper = new LockOperation(
+											operation);
+									JobUtil.scheduleUserJob(lockWrapper,
+											jobname, JobFamilies.RESET);
 								}
 							});
 				} catch (InvocationTargetException ite) {
