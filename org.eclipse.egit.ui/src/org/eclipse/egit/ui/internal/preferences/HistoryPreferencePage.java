940ac23d75a56308289b454b8b0c1cc9fe9c6903
Daniel Megert <daniel_megert@ch.ibm.com>
2012-01-13 11:12:37 +0100

    [historyView] Add preferences for max branch and tag length
    This controls how branch and tag labels in the table are
    truncated with an ellipsis when the specified length is exceeded.
    
    This breaks the "Views" preference page into two separate pages,
    one for the History view and one for the Synchronize view.
    
    The initial default for both preferences is 15 which is
    larger than the previous internal value of 12.
    
    Bug: 341593
    Bug: 356210
    Change-Id: Id1c11283408ee37429dda794eea453e1d18344c3

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/preferences/HistoryPreferencePage.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * Copyright (C) 2011, Mathias Kinzler <mathias.kinzler@sap.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.preferences;
+
+import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.UIPreferences;
+import org.eclipse.egit.ui.UIText;
+import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.IntegerFieldEditor;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+/**
+ * Preference page for the History view
+ */
+public class HistoryPreferencePage extends FieldEditorPreferencePage implements
+		IWorkbenchPreferencePage {
+
+	/**
+	 * The default constructor
+	 */
+	public HistoryPreferencePage() {
+		super(GRID);
+	}
+
+	protected IPreferenceStore doGetPreferenceStore() {
+		return Activator.getDefault().getPreferenceStore();
+	}
+
+	public void init(final IWorkbench workbench) {
+		// Do nothing.
+	}
+
+	@Override
+	protected void createFieldEditors() {
+		addField(new BooleanFieldEditor(
+				UIPreferences.RESOURCEHISTORY_SHOW_RELATIVE_DATE,
+				UIText.ResourceHistory_toggleRelativeDate,
+				getFieldEditorParent()));
+		addField(new BooleanFieldEditor(
+				UIPreferences.RESOURCEHISTORY_SHOW_NOTES,
+				UIText.ResourceHistory_toggleShowNotes, getFieldEditorParent()));
+		addField(new BooleanFieldEditor(
+				UIPreferences.RESOURCEHISTORY_FOLLOW_RENAMES,
+				UIText.GitHistoryPage_FollowRenames, getFieldEditorParent()));
+		addField(new BooleanFieldEditor(
+				UIPreferences.RESOURCEHISTORY_SHOW_COMMENT_WRAP,
+				UIText.ResourceHistory_toggleCommentWrap,
+				getFieldEditorParent()));
+		addField(new BooleanFieldEditor(
+				UIPreferences.RESOURCEHISTORY_SHOW_REV_COMMENT,
+				UIText.ResourceHistory_toggleRevComment, getFieldEditorParent()));
+		addField(new BooleanFieldEditor(
+				UIPreferences.RESOURCEHISTORY_SHOW_REV_DETAIL,
+				UIText.ResourceHistory_toggleRevDetail, getFieldEditorParent()));
+		addField(new BooleanFieldEditor(
+				UIPreferences.HISTORY_SHOW_TAG_SEQUENCE,
+				UIText.ResourceHistory_ShowTagSequence, getFieldEditorParent()));
+		addField(new IntegerFieldEditor(UIPreferences.HISTORY_MAX_NUM_COMMITS,
+				UIText.ResourceHistory_MaxNumCommitsInList,
+				getFieldEditorParent()));
+		addField(new IntegerFieldEditor(UIPreferences.HISTORY_MAX_TAG_LENGTH,
+				UIText.HistoryPreferencePage_MaxTagLength,
+				getFieldEditorParent()));
+		addField(new IntegerFieldEditor(
+				UIPreferences.HISTORY_MAX_BRANCH_LENGTH,
+				UIText.HistoryPreferencePage_MaxBranchLength,
+				getFieldEditorParent()));
+	}
+}
---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/preferences/SynchronizePreferencePage.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (C) 2011, Mathias Kinzler <mathias.kinzler@sap.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.preferences;
+
+import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.UIPreferences;
+import org.eclipse.egit.ui.UIText;
+import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+/** Preference page for views preferences */
+public class SynchronizePreferencePage extends FieldEditorPreferencePage
+		implements IWorkbenchPreferencePage {
+
+	/**
+	 * The default constructor
+	 */
+	public SynchronizePreferencePage() {
+		super(FLAT);
+	}
+
+	protected IPreferenceStore doGetPreferenceStore() {
+		return Activator.getDefault().getPreferenceStore();
+	}
+
+	public void init(final IWorkbench workbench) {
+		// Do nothing.
+	}
+
+	@Override
+	protected void createFieldEditors() {
+		addField(new BooleanFieldEditor(
+				UIPreferences.SYNC_VIEW_FETCH_BEFORE_LAUNCH,
+				UIText.GitPreferenceRoot_fetchBeforeSynchronization,
+				getFieldEditorParent()));
+		addField(new BooleanFieldEditor(
+				UIPreferences.SYNC_VIEW_ALWAYS_SHOW_CHANGESET_MODEL,
+				UIText.GitPreferenceRoot_automaticallyEnableChangesetModel,
+				getFieldEditorParent()));
+	}
+}
