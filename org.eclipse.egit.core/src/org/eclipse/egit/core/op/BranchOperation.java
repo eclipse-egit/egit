2c3f7996f172426b91e428fbad6e162019453d50
Markus Duft <markus.duft@salomon.at>
2012-01-20 08:32:57 +0100

    Nice checkout conflict display instead of internal error
    When switching branches by double-clicking their label, a nice checkout
    conflict display appears if a conflict exists. When creating a new
    branch, an ugly error box is shown instead. By detecting the
    CheckoutConflictException explicitly and just returning, the
    BranchResultDialog will be used there too, and show a nice overview of
    all conflicts.
    
    Change-Id: Ic772f519c5e36dcc1b0326dc1045497b211aaa66

--- org.eclipse.egit.core/src/org/eclipse/egit/core/op/BranchOperation.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/op/BranchOperation.java
@@ -33,6 +33,7 @@ import org.eclipse.jgit.api.CheckoutResult.Status;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.JGitInternalException;
+import org.eclipse.jgit.errors.CheckoutConflictException;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
@@ -101,7 +102,11 @@ public class BranchOperation extends BaseOperation {
 				try {
 					co.call();
 				} catch (JGitInternalException e) {
-					throw new CoreException(Activator.error(e.getMessage(), e));
+					if(e.getCause() instanceof CheckoutConflictException) {
+						return;
+					} else {
+						throw new CoreException(Activator.error(e.getMessage(), e));
+					}
 				} catch (GitAPIException e) {
 					throw new CoreException(Activator.error(e.getMessage(), e));
 				} finally {
