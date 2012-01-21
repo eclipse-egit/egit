5922341006cdaffcf83f034cb6bbb063ae499ee6
Daniel Megert <daniel_megert@ch.ibm.com>
2012-01-21 01:26:07 +0100

    Added context menu to the Commit Editor's header text
    Added a context menu with 'Copy SHA-1' and 'Copy' to the header of the
    Commit Editor.
    
    Bug 359038
    Change-Id: I418fe61db9e6cd9b52d67cefdf14555b5190e5ae
    Signed-off-by: Daniel Megert <daniel_megert@ch.ibm.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -2,7 +2,7 @@
  * Copyright (C) 2008, Roger C. Soares <rogersoares@intelinet.com.br>
  * Copyright (C) 2008, Shawn O. Pearce <spearce@spearce.org>
  * Copyright (C) 2010, Matthias Sohn <matthias.sohn@sap.com>
- *
+ * Copyright (C) 2011, Daniel Megert <daniel_megert@ch.ibm.com>
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -3372,6 +3372,18 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CommitEditorPage_TooltipSignedOffByCommitter;
+
+	/** */
+	public static String Header_contextMenu_copy;
+
+	/** */
+	public static String Header_contextMenu_copy_SHA1;
+	/** */
+
+	public static String Header_copy_SHA1_error_title;
+
+	/** */
+	public static String Header_copy_SHA1_error_message;
 
 	/** */
 	public static String CommitFileDiffViewer_CanNotOpenCompareEditorTitle;
