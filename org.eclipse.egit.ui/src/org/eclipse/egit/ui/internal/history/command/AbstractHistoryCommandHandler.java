014cf48dc012bd3b7a99ea21cec7fa9fe1cc47bb
Kevin Sawicki <kevin@github.com>
2012-01-24 14:53:57 -0800

    [historyView] Support linking with commit editor
    Opening the commit editor with the history view in link mode
    will now set the input of the view to be the commit's repository
    and the current commit being viewed will be selected once the
    view loads
    
    Bug: 368346
    Change-Id: Icccc2d7032ba11b1ead5c993995efaca36251bf0

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/AbstractHistoryCommandHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/AbstractHistoryCommandHandler.java
@@ -23,6 +23,7 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.egit.core.AdapterUtils;
 import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.internal.CompareUtils;
@@ -97,6 +98,11 @@ abstract class AbstractHistoryCommandHandler extends AbstractHandler {
 			}
 
 		}
+
+		Repository repo = AdapterUtils.adapt(input, Repository.class);
+		if (repo != null)
+			return repo;
+
 		throw new ExecutionException(
 				UIText.AbstractHistoryCommanndHandler_CouldNotGetRepositoryMessage);
 	}
