d46569547fec49e929bf831cf6a390ac6c7a5336
Kevin Sawicki <kevin@github.com>
2012-01-24 10:54:54 -0800

    [repoView] Add support for syncing/updating submodules
    The Submodules node and all child repository nodes now have
    an Update and Sync action available from the context menu
    
    Change-Id: I2afea7dca9e40a2748c10a7f835b2deae301ba73

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -4187,6 +4187,18 @@ public class UIText extends NLS {
 	public static String StagingView_StageItemMenuLabel;
 
 	/** */
+	public static String SubmoduleSyncCommand_SyncError;
+
+	/** */
+	public static String SubmoduleSyncCommand_Title;
+
+	/** */
+	public static String SubmoduleUpdateCommand_Title;
+
+	/** */
+	public static String SubmoduleUpdateCommand_UpdateError;
+
+	/** */
 	public static String SynchronizeWithMenu_custom;
 
 	/** */
