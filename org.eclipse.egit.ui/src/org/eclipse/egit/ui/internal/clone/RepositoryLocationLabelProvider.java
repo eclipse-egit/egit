649e0159bc29a6ca76c79098531044da575bed1f
Stefan Lay <stefan.lay@sap.com>
2012-01-24 08:59:42 +0100

    Add an icon attribute to the cloneSourceProvider extension point
    Change-Id: Ibac3450de851351131fe57e6bb7c78a2fc3d4d49
    Signed-off-by: Stefan Lay <stefan.lay@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/RepositoryLocationLabelProvider.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/RepositoryLocationLabelProvider.java
@@ -10,12 +10,21 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.clone;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.egit.ui.UIIcons;
 import org.eclipse.egit.ui.internal.clone.GitCloneSourceProviderExtension.CloneSourceProvider;
 import org.eclipse.egit.ui.internal.provisional.wizards.RepositoryServerInfo;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.graphics.Image;
 
 class RepositoryLocationLabelProvider extends LabelProvider {
+
+	private Image repoImage = UIIcons.CLONEGIT.createImage();
+
+	private List<Image> images = new ArrayList<Image>();
+
 	@Override
 	public String getText(Object element) {
 		if (element instanceof CloneSourceProvider)
@@ -27,6 +36,20 @@ class RepositoryLocationLabelProvider extends LabelProvider {
 
 	@Override
 	public Image getImage(Object element) {
-		return super.getImage(element);
+		if (element instanceof CloneSourceProvider) {
+			Image image = ((CloneSourceProvider) element).getImage().createImage();
+			images.add(image);
+			return image;
+		}
+		else if (element instanceof RepositoryServerInfo)
+			return repoImage;
+		return null;
 	}
+
+	public void dispose() {
+		repoImage.dispose();
+		for (Image image  : images)
+			image.dispose();
+	}
+
 }
