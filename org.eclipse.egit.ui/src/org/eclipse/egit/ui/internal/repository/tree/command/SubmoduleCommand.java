eb34602f166a365db0651f6e55e9332e4421be46
Kevin Sawicki <kevin@github.com>
2012-01-24 10:39:42 -0800

    [repoView] Add support for updating submodules
    The Submodules node and child repository nodes now have
    an "Update" action that updates the selected submodules
    
    Change-Id: I2afea7dca9e40a2748c10a7f835b2deae301ba73

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/SubmoduleCommand.java
@@ -0,0 +1,64 @@
+/******************************************************************************
+ *  Copyright (c) 2012 GitHub Inc.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *    Kevin Sawicki (GitHub Inc.) - initial API and implementation
+ *****************************************************************************/
+package org.eclipse.egit.ui.internal.repository.tree.command;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
+import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNodeType;
+import org.eclipse.jgit.lib.Repository;
+
+/**
+ * Base submodule command with helpers for finding the selected submodule paths
+ * and parent repositories
+ *
+ * @param <V>
+ */
+public abstract class SubmoduleCommand<V> extends
+		RepositoriesViewCommandHandler<V> {
+
+	/**
+	 * Get submodule from selected nodes
+	 * <p>
+	 * Keys with null values denote repositories where all submodules should be
+	 * used for the current command being executed
+	 *
+	 * @param nodes
+	 * @return non-null but possibly empty map of parent repository's to
+	 *         submodule paths
+	 */
+	protected Map<Repository, List<String>> getSubmodules(
+			final List<RepositoryTreeNode<?>> nodes) {
+		final Map<Repository, List<String>> repoPaths = new HashMap<Repository, List<String>>();
+		for (RepositoryTreeNode<?> node : nodes) {
+			if (node.getType() == RepositoryTreeNodeType.REPO) {
+				Repository parent = node.getParent().getRepository();
+				String path = Repository.stripWorkDir(parent.getWorkTree(),
+						node.getRepository().getWorkTree());
+				List<String> paths = repoPaths.get(parent);
+				if (paths == null) {
+					paths = new ArrayList<String>();
+					repoPaths.put(parent, paths);
+				}
+				paths.add(path);
+			}
+		}
+		for (RepositoryTreeNode<?> node : nodes)
+			if (node.getType() == RepositoryTreeNodeType.SUBMODULES)
+				// Clear paths so all submodules are updated
+				repoPaths.put(node.getParent().getRepository(), null);
+		return repoPaths;
+	}
+
+}
