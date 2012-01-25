dff0f1a7317d8e29479bd430f21fde5cd2a56917
Dariusz Luksza <dariusz@luksza.org>
2012-01-25 23:51:13 +0100

    Fix GitCloneWizartTest.invalidPortFreezesDialog
    Screenshot taken by SWTBot after failing test cases doesn't show clone
    wizard page. Before we didn't check if it does appear, this change
    ensures that it is shown.
    
    Change-Id: I44a88f51b8ad1d3f7b1f2926c28039af7483966e
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/TestUtil.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/TestUtil.java
@@ -487,4 +487,19 @@ public class TestUtil {
 		});
 	}
 
+	public static void waitUntilViewWithGivenTitleShows(final String viewTitle) {
+		waitForView(new BaseMatcher<IViewReference>() {
+			public boolean matches(Object item) {
+				if (item instanceof IViewReference)
+					return viewTitle.equals(((IViewReference) item).getTitle());
+
+				return false;
+			}
+
+			public void describeTo(Description description) {
+				description.appendText("Wait for view with title " + viewTitle);
+			}
+		});
+	}
+
 }
