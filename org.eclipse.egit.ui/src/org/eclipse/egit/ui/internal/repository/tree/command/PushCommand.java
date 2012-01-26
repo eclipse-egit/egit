c6b49d601d76f7fc00b2eeeea798104745a36cad
Markus Duft <markus.duft@salomon.at>
2012-01-26 13:28:00 +0100

    Allow push of single Refs/Commits from Repo & History Views
    This adds a new SimplePushWizard which just allows selecting a target
    remote and ref name. It then pushes out the selected Ref/Commit to the
    selected target ref.
    
    This fixes three bugs (see below), and i feel it would be a really cool
    feature for 1.3!
    
    There is room for improvement, and i need help with implementing content
    assist for the target ref selection - this should be a combo much like
    the destination combo in the existing RefSpecPanel
    
    Bug: 341076
    Bug: 356314
    Bug: 337747
    Change-Id: If3be09886c23b031489116d448cae2dce29e281a

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/PushCommand.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/PushCommand.java
@@ -16,24 +16,41 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.internal.push.PushWizard;
+import org.eclipse.egit.ui.internal.push.SimplePushRefWizard;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryNode;
+import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
+import org.eclipse.jface.wizard.IWizard;
 import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.jgit.lib.Ref;
 
 /**
  * Implements "Push" from a Repository
  */
 public class PushCommand extends RepositoriesViewCommandHandler<RepositoryNode> {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		RepositoryNode node = getSelectedNodes(event).get(0);
+		RepositoryTreeNode node = getSelectedNodes(event).get(0);
+
+		IWizard pushWiz = null;
 
 		try {
-			WizardDialog dlg = new WizardDialog(getShell(event),
-					new PushWizard(node.getRepository()));
-			dlg.setHelpAvailable(true);
-			dlg.open();
+			switch (node.getType()) {
+			case REF:
+			case TAG:
+				pushWiz = new SimplePushRefWizard(node.getRepository(), (Ref)node.getObject());
+				break;
+			case REPO:
+				pushWiz = new PushWizard(node.getRepository());
+				break;
+			default:
+				throw new UnsupportedOperationException("type not supported!"); //$NON-NLS-1$
+			}
 		} catch (URISyntaxException e1) {
 			Activator.handleError(e1.getMessage(), e1, true);
 		}
+
+		WizardDialog dlg = new WizardDialog(getShell(event), pushWiz);
+		dlg.setHelpAvailable(true);
+		dlg.open();
 
 		return null;
 	}
