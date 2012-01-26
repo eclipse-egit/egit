24f678ac8eb51dd9160f75425627a5952e1879cd
Kevin Sawicki <kevin@github.com>
2012-01-26 11:35:20 -0800

    [repoView] Add support for adding new submodules
    New action on root repositories and 'Submodules' nodes
    opens a wizard to configure a new submodule that will
    be configured and cloned on completion
    
    Change-Id: I3b3963d420b4e18061e5699e4c1aa6caea9294f7

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/submodule/AddSubmoduleWizard.java
@@ -0,0 +1,77 @@
+/******************************************************************************
+ *  Copyright (c) 2012 GitHub Inc.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *    Kevin Sawicki (GitHub Inc.) - initial API and implementation
+ *****************************************************************************/
+package org.eclipse.egit.ui.internal.submodule;
+
+import org.eclipse.egit.ui.UIIcons;
+import org.eclipse.egit.ui.UIText;
+import org.eclipse.egit.ui.internal.SecureStoreUtils;
+import org.eclipse.egit.ui.internal.components.RepositorySelectionPage;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.transport.URIish;
+
+/**
+ * Wizard to configure a new submodule
+ */
+public class AddSubmoduleWizard extends Wizard {
+
+	private final Repository repo;
+
+	private SubmodulePathWizardPage pathPage;
+
+	private RepositorySelectionPage uriPage;
+
+	/**
+	 * Create wizard
+	 *
+	 * @param repo
+	 */
+	public AddSubmoduleWizard(Repository repo) {
+		this.repo = repo;
+		setWindowTitle(UIText.AddSubmoduleWizard_WindowTitle);
+		setDefaultPageImageDescriptor(UIIcons.WIZBAN_IMPORT_REPO);
+	}
+
+	public void addPages() {
+		pathPage = new SubmodulePathWizardPage(repo);
+		addPage(pathPage);
+		uriPage = new RepositorySelectionPage(true, null);
+		uriPage.setPageComplete(false);
+		addPage(uriPage);
+	}
+
+	/**
+	 * Get path of submodule
+	 *
+	 * @return path
+	 */
+	public String getPath() {
+		return pathPage.getPath();
+	}
+
+	/**
+	 * Get URI of submodule
+	 *
+	 * @return uri
+	 */
+	public URIish getUri() {
+		return uriPage.getSelection().getURI();
+	}
+
+	public boolean performFinish() {
+		if (uriPage.getStoreInSecureStore()
+				&& !SecureStoreUtils.storeCredentials(uriPage.getCredentials(),
+						uriPage.getSelection().getURI()))
+			return false;
+
+		return true;
+	}
+}
