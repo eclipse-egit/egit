69530833ca12f53c7ba54b289102689bbbe3b77e
Stefan Lay <stefan.lay@sap.com>
2012-01-27 13:56:10 +0100

    New wizard to configure a remote as Gerrit
    Bug: 357222
    Change-Id: If1b2e062501650a7742ce2c857fa78ed55c11bb3
    Signed-off-by: Stefan Lay <stefan.lay@sap.com>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/ConfigureGerritRemoteCommand.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (C) 2012, Stefan Lay <stefan.lay@sap.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.repository.tree.command;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.egit.ui.internal.gerrit.ConfigureGerritWizard;
+import org.eclipse.egit.ui.internal.repository.tree.RemoteNode;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+
+/**
+ * Starts a UI to configure a remote configuration for Gerrit Code Review
+ */
+public class ConfigureGerritRemoteCommand extends
+		RepositoriesViewCommandHandler<RemoteNode> {
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		final RemoteNode node = getSelectedNodes(event).get(0);
+		Repository repository = node.getRepository();
+		final String remoteName = node.getObject();
+
+		Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+				.getShell();
+		ConfigureGerritWizard configureGerritWizard = new ConfigureGerritWizard(
+				repository.getConfig(), remoteName);
+		WizardDialog dlg = new WizardDialog(shell, configureGerritWizard);
+		dlg.setHelpAvailable(false);
+		dlg.open();
+
+		return null;
+	}
+}
