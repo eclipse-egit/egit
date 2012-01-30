5d39d0ca7ac2c9c209a57ef5f4cbd051ceff25f8
Stefan Lay <stefan.lay@sap.com>
2012-01-30 09:24:25 +0100

    Add an extension point to provide repository search
    The new extension point can be used to contribute the ability to search
    for repositories on a remote server. It will be used in a new version
    of an import wizard. For this use case an implementor can provide a
    Wizard page implementing such a search.
    
    Bug: 361251
    Change-Id: I4e3a3ff88dbb5642dd61c55d751f4be12e93f720
    Signed-off-by: Stefan Lay <stefan.lay@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -301,6 +301,9 @@ public class UIText extends NLS {
 	public static String ExistingOrNewPage_WrongPathErrorDialogTitle;
 
 	/** */
+	public static String GitCloneSourceProviderExtension_Local;
+
+	/** */
 	public static String GitCloneWizard_abortingCloneMsg;
 
 	/** */
