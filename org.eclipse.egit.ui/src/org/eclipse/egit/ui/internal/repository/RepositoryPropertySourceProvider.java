2da8a17ece985b2b4b2c2dfe733417d81e5346e6
Mathias Kinzler <mathias.kinzler@sap.com>
2012-02-01 17:46:59 +0100

    RFC: allow to show/edit branch configuration
    It should be possible to see/change branch configuration (upstream
    configuration) when selecting a branch in the Repositories View.
    
    A Properties View can be opened which will show the upstream
    configuration for the selected branch. From there, it is possible to
    open an editor for this configuration.
    
    Alternatively, a right-click on the branch node will open the editor
    directly.
    
    Change-Id: I3976c90da855d7524cd9a64d765264e87622e36a
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoryPropertySourceProvider.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoryPropertySourceProvider.java
@@ -12,6 +12,9 @@ package org.eclipse.egit.ui.internal.repository;
 
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNodeType;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.ui.views.properties.IPropertySource;
 import org.eclipse.ui.views.properties.IPropertySourceProvider;
@@ -29,6 +32,9 @@ public class RepositoryPropertySourceProvider implements
 	private Object lastObject;
 
 	private IPropertySource lastRepositorySource;
+
+	// 0: Repository, 1: Remote, 2: Branch
+	private int lastSourceType = -1;
 
 	/**
 	 * @param page
@@ -50,11 +56,13 @@ public class RepositoryPropertySourceProvider implements
 
 		if (node.getType() == RepositoryTreeNodeType.REPO) {
 			lastObject = object;
+			checkChangeType(0);
 			lastRepositorySource = new RepositoryPropertySource(
 					(Repository) node.getObject(), myPage);
 			return lastRepositorySource;
 		} else if (node.getType() == RepositoryTreeNodeType.REMOTE) {
 			lastObject = object;
+			checkChangeType(1);
 			lastRepositorySource = new RepositoryRemotePropertySource(node
 					.getRepository().getConfig(), (String) node.getObject(),
 					myPage);
@@ -62,8 +70,39 @@ public class RepositoryPropertySourceProvider implements
 		} else if (node.getType() == RepositoryTreeNodeType.FETCH
 				|| node.getType() == RepositoryTreeNodeType.PUSH) {
 			return getPropertySource(node.getParent());
+		} else if (node.getType() == RepositoryTreeNodeType.REF) {
+			lastObject = object;
+			Ref ref = (Ref) node.getObject();
+			if (ref.getName().startsWith(Constants.R_HEADS) || ref.getName().startsWith(Constants.R_REMOTES)){
+				checkChangeType(2);
+				Repository repository = (Repository) node.getAdapter(Repository.class);
+				lastRepositorySource =  new BranchPropertySource(repository, ref.getName(), myPage);
+				return lastRepositorySource;
+			}
+			return null;
 		} else {
 			return null;
 		}
+	}
+
+	private void checkChangeType(int i) {
+		// the different pages contribute different actions, so if we
+		// change to a different page type, we need to clear them
+		if (lastSourceType != i) {
+			IToolBarManager mgr = myPage.getSite().getActionBars()
+					.getToolBarManager();
+			boolean update = false;
+			update = update
+					| mgr.remove(RepositoryPropertySource.CHANGEMODEACTIONID) != null;
+			update = update
+					| mgr.remove(RepositoryPropertySource.SINGLEVALUEACTIONID) != null;
+			update = update
+					| mgr.remove(RepositoryPropertySource.EDITACTIONID) != null;
+			update = update
+					| mgr.remove(BranchPropertySource.EDITACTIONID) != null;
+			if (update)
+				mgr.update(false);
+		}
+		lastSourceType = i;
 	}
 }
