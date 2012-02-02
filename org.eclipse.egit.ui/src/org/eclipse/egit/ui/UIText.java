5f46042c94db43e8860de8c3bf94e6df71d9f3f4
Mathias Kinzler <mathias.kinzler@sap.com>
2012-02-02 14:13:14 +0100

    [historyView] Hide additional Refs by default
    The normal user is not interested in Refs like FETCH_HEAD, ORIG_HEAD
    etc. when using the HistoryView. However, these Refs can pollute the
    view as they can not be hidden independently if "Show All Branches" is
    active.
    
    The additional Refs are now hidden by default and the user can have
    them displayed by checking the Show->Additional Refs menu item toggle
    button. The HEAD Ref is always shown independent from this switch.
    
    Change-Id: I2b5df21e88ab7015012ef670de33819bd65de037
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -514,6 +514,9 @@ public class UIText extends NLS {
 	public static String GitHistoryPage_SetAsBaselineMenuLabel;
 
 	/** */
+	public static String GitHistoryPage_ShowAdditionalRefsMenuLabel;
+
+	/** */
 	public static String GitHistoryPage_ShowAllBranchesMenuLabel;
 
 	/** */
