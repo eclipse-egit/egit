31a9c126a5ad439cba2bdf3ddb04bac725fda1c9
Steffen Pingel <steffen.pingel@tasktop.com>
2012-02-04 15:42:30 +0100

    Fix regions decorated by commit hyperlink detector
    * The offset of the hyperlinked region is now calculated correctly.
    * Gerrit change ids are not detected as commit ids.
    * Performance is improved through using a regular expression rather
      than tokenizing the text.
    
    Bug: 355868
    Change-Id: I35e36dc057a4ce1279836cf1aa65cbea6f83dbdb

--- org.eclipse.egit.mylyn.ui.test/src/org/eclipse/egit/internal/mylyn/CommitHyperlinkDetectorTest.java
+++ org.eclipse.egit.mylyn.ui.test/src/org/eclipse/egit/internal/mylyn/CommitHyperlinkDetectorTest.java
@@ -118,6 +118,30 @@ public class CommitHyperlinkDetectorTest {
 		assertNull(hyperlinks);
 	}
 
+	@Test
+	public void testMultiLine() {
+		setText("Test multi-line text\n" + EXAMPLE_ID);
+		IHyperlink[] hyperlinks = detectHyperlinks(0,textViewer.getDocument().getLength());
+		assertEquals(1, hyperlinks.length);
+		assertEquals(EXAMPLE_ID, hyperlinks[0].getHyperlinkText());
+		assertEquals(new Region(21, EXAMPLE_ID.length()), hyperlinks[0].getHyperlinkRegion());
+	}
+
+	@Test
+	public void testGerritId() {
+		setText("I" + EXAMPLE_ID);
+		IHyperlink[] hyperlinks = detectHyperlinks(0,textViewer.getDocument().getLength());
+		assertNull(hyperlinks);
+	}
+
+	@Test
+	public void testGerritIdWithinText() {
+		setText("abc I" + EXAMPLE_ID);
+		IHyperlink[] hyperlinks = detectHyperlinks(5,textViewer.getDocument().getLength());
+		assertNull(hyperlinks);
+	}
+
+
 	private IHyperlink[] detectHyperlinks() {
 		return detectHyperlinks(0, textViewer.getDocument().getLength());
 	}
