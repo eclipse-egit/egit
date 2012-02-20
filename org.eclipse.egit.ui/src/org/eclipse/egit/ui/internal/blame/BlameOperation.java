7c3416f7eed2b5975beba4716477c3cf4294fcbe
Kevin Sawicki <kevin@github.com>
2012-02-21 00:55:07 +0100

    Show commit in history view when annotation is selected
    BlameOperation now listens for revision selection events
    and shows/selects the commit in the history view when a
    revision is selected.
    
    This changes the behavior to initially load the history view
    while the editor ruler is being shown so it is ready to show
    selections once the editor and gutter area are explored.
    
    Bug: 356313
    Change-Id: Ia9cf2c6ec44206ee3567367487df7f4a6f11cd42
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/blame/BlameOperation.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/blame/BlameOperation.java
@@ -10,18 +10,30 @@
  *****************************************************************************/
 package org.eclipse.egit.ui.internal.blame;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IStorage;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
+import org.eclipse.egit.core.AdapterUtils;
 import org.eclipse.egit.core.op.IEGitOperation;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.UIPreferences;
+import org.eclipse.egit.ui.internal.history.HistoryPageInput;
+import org.eclipse.jface.text.revisions.IRevisionRulerColumn;
+import org.eclipse.jface.text.revisions.IRevisionRulerColumnExtension;
 import org.eclipse.jface.text.revisions.RevisionInformation;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jgit.api.BlameCommand;
 import org.eclipse.jgit.blame.BlameResult;
 import org.eclipse.jgit.diff.RawTextComparator;
@@ -29,15 +41,92 @@ import org.eclipse.jgit.lib.AnyObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.team.ui.history.IHistoryView;
 import org.eclipse.team.ui.history.RevisionAnnotationController;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.texteditor.AbstractDecoratedTextEditor;
 
 /**
  * Blame operation
  */
 public class BlameOperation implements IEGitOperation {
+
+	private static class BlameHistoryPageInput extends HistoryPageInput
+			implements IAdaptable {
+
+		private final RevCommit commit;
+
+		private BlameHistoryPageInput(Repository repository, RevCommit commit,
+				File file) {
+			super(repository, new File[] { file });
+			this.commit = commit;
+		}
+
+		private BlameHistoryPageInput(Repository repository, RevCommit commit,
+				IResource file) {
+			super(repository, new IResource[] { file });
+			this.commit = commit;
+		}
+
+		private BlameHistoryPageInput(Repository repository, RevCommit commit) {
+			super(repository);
+			this.commit = commit;
+		}
+
+		public Object getAdapter(Class adapter) {
+			if (RevCommit.class == adapter)
+				return commit;
+			return Platform.getAdapterManager().getAdapter(this, adapter);
+		}
+	}
+
+	private static class RevisionSelectionHandler implements
+			ISelectionChangedListener {
+
+		private IFile resourceFile;
+
+		private File nonResourceFile;
+
+		private RevisionSelectionHandler(Repository repository, String path,
+				IStorage storage) {
+			if (storage instanceof IFile)
+				resourceFile = (IFile) storage;
+			else if (!repository.isBare())
+				nonResourceFile = new File(repository.getWorkTree(), path);
+		}
+
+		public void selectionChanged(SelectionChangedEvent event) {
+			ISelection selection = event.getSelection();
+			if (selection.isEmpty()
+					|| !(selection instanceof IStructuredSelection))
+				return;
+			Object first = ((IStructuredSelection) selection).getFirstElement();
+			if (!(first instanceof BlameRevision))
+				return;
+
+			IHistoryView part = (IHistoryView) PlatformUI.getWorkbench()
+					.getActiveWorkbenchWindow().getActivePage()
+					.findView(IHistoryView.VIEW_ID);
+			if (part == null)
+				return;
+
+			BlameRevision revision = (BlameRevision) first;
+			BlameHistoryPageInput input;
+			if (resourceFile != null)
+				input = new BlameHistoryPageInput(revision.getRepository(),
+						revision.getCommit(), resourceFile);
+			else if (nonResourceFile != null)
+				input = new BlameHistoryPageInput(revision.getRepository(),
+						revision.getCommit(), nonResourceFile);
+			else
+				input = new BlameHistoryPageInput(revision.getRepository(),
+						revision.getCommit());
+			part.showHistoryFor(input);
+		}
+
+	}
 
 	private Repository repository;
 
@@ -127,24 +216,58 @@ public class BlameOperation implements IEGitOperation {
 
 		shell.getDisplay().asyncExec(new Runnable() {
 			public void run() {
-				try {
-					AbstractDecoratedTextEditor editor;
-					if (storage instanceof IFile)
-						editor = RevisionAnnotationController.openEditor(page,
-								(IFile) storage);
-					else
-						editor = RevisionAnnotationController.openEditor(page,
-								storage, storage);
-					if (editor != null)
-						editor.showRevisionInformation(info,
-								"org.eclipse.egit.ui.internal.decorators.GitQuickDiffProvider"); //$NON-NLS-1$
-				} catch (PartInitException e) {
-					Activator.handleError(
-							"Error displaying blame annotations", e, //$NON-NLS-1$
-							false);
-				}
+				openEditor(info);
 			}
 		});
+	}
+
+	private void openEditor(final RevisionInformation info) {
+		AbstractDecoratedTextEditor editor;
+		try {
+			if (storage instanceof IFile)
+				editor = RevisionAnnotationController.openEditor(page,
+						(IFile) storage);
+			else
+				editor = RevisionAnnotationController.openEditor(page, storage,
+						storage);
+		} catch (PartInitException e) {
+			Activator.handleError("Error displaying blame annotations", e, //$NON-NLS-1$
+					false);
+			return;
+		}
+		if (editor == null)
+			return;
+
+		// Show history view for path
+		try {
+			IHistoryView part = (IHistoryView) page.showView(
+					IHistoryView.VIEW_ID, null, IWorkbenchPage.VIEW_VISIBLE);
+			HistoryPageInput input;
+			if (storage instanceof IFile)
+				input = new HistoryPageInput(repository,
+						new IResource[] { (IResource) storage });
+			else if (!repository.isBare())
+				input = new HistoryPageInput(repository, new File[] { new File(
+						repository.getWorkTree(), path) });
+			else
+				input = new HistoryPageInput(repository);
+			part.showHistoryFor(input);
+		} catch (PartInitException e) {
+			Activator.handleError("Error displaying blame annotations", e, //$NON-NLS-1$
+					false);
+		}
+
+		editor.showRevisionInformation(info,
+				"org.eclipse.egit.ui.internal.decorators.GitQuickDiffProvider"); //$NON-NLS-1$
+
+		IRevisionRulerColumn revisionRuler = AdapterUtils.adapt(editor,
+				IRevisionRulerColumn.class);
+		if (revisionRuler instanceof IRevisionRulerColumnExtension)
+			((IRevisionRulerColumnExtension) revisionRuler)
+					.getRevisionSelectionProvider()
+					.addSelectionChangedListener(
+							new RevisionSelectionHandler(repository, path,
+									storage));
 	}
 
 	public ISchedulingRule getSchedulingRule() {
