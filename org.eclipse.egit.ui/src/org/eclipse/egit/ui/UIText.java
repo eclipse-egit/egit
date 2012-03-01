4ffe19bb3eec203fd7bc52afd76684284548dbb3
Markus Duft <markus.duft@salomon.at>
2012-03-01 14:03:58 +0100

    Fix rebase > continue when nothing left to commit.
    If after resolving all conflicts nothing is left to commit, behave like
    cgit, and display a nice message informing the user.
    
    Currently, EGit displays a "HEAD advanced fast forward" message, which
    is absolutely not helpful at all.
    
    This depends on a change with the same ID in jgit!
    
    Bug-Id: 336812
    Change-Id: If2665005cf54a5b51c0fe80bad019fa42b0205af

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -682,6 +682,9 @@ public class UIText extends NLS {
 	public static String RebaseResultDialog_NextStepsDoNothing;
 
 	/** */
+	public static String RebaseResultDialog_NothingToCommit;
+
+	/** */
 	public static String RebaseResultDialog_notInWorkspace;
 
 	/** */
