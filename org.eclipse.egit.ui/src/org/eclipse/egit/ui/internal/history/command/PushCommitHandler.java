5a2276ac8b1dec07b73d6896a71be92ef5bf0d3e
Markus Duft <markus.duft@salomon.at>
2012-03-14 23:03:25 +0100

    Allow push of single Refs/Commits from Repo & History Views
    This adds a new SimplePushWizard which just allows selecting a target
    remote and ref name. It then pushes out the selected Ref/Commit to the
    selected target ref.
    
    While implementing this I split out the Ref content assist functionality
    from the existing RefSpecDialog to be reusable by this code.
    
    Bug: 341076
    Bug: 356314
    Bug: 337747
    CQ: 6347
    Change-Id: If3be09886c23b031489116d448cae2dce29e281a
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/PushCommitHandler.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (C) 2012, Markus Duft <markus.duft@salomon.at>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.history.command;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.internal.history.GitHistoryPage;
+import org.eclipse.egit.ui.internal.push.SimplePushRefWizard;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revplot.PlotCommit;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+/**
+ * Command handler to enable pushing commits from the Git History View
+ */
+public class PushCommitHandler extends AbstractHistoryCommandHandler {
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		PlotCommit commit = (PlotCommit) getSelection(getPage()).getFirstElement();
+		final Repository repo = getRepository(event);
+
+		try {
+			WizardDialog dlg = new WizardDialog(HandlerUtil
+					.getActiveShellChecked(event), new SimplePushRefWizard(repo, commit.getId()));
+			dlg.setHelpAvailable(true);
+			dlg.open();
+		} catch (Exception e) {
+			Activator.handleError(e.getMessage(), e, true);
+		}
+
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		GitHistoryPage page = getPage();
+		if (page == null)
+			return false;
+		IStructuredSelection sel = getSelection(page);
+		return sel.size() == 1 && sel.getFirstElement() instanceof RevCommit;
+	}
+}
---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/push/SimplePushRefWizard.java
@@ -0,0 +1,154 @@
+/*******************************************************************************
+ * Copyright (C) 2011,  Markus Duft <markus.duft@salomon.at>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.egit.ui.internal.push;
+
+import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.egit.core.op.PushOperation;
+import org.eclipse.egit.core.op.PushOperationSpecification;
+import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.UIPreferences;
+import org.eclipse.egit.ui.internal.components.RefContentAssistProvider;
+import org.eclipse.egit.ui.internal.components.RepositorySelection;
+import org.eclipse.egit.ui.internal.components.RepositorySelectionPage;
+import org.eclipse.egit.ui.internal.components.SimplePushSpecPage;
+import org.eclipse.egit.ui.internal.push.PushWizard.PushJob;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.jgit.lib.AbbreviatedObjectId;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.transport.RemoteConfig;
+import org.eclipse.jgit.transport.RemoteRefUpdate;
+
+/**
+ * A simple push wizard, which only pushes out the selected ref/commit to the
+ * selected repo.
+ */
+public class SimplePushRefWizard extends Wizard {
+
+	/**
+	 * The commit that will be pushed out.
+	 */
+	private ObjectId pushObj;
+
+	private RepositorySelectionPage repoPage;
+
+	private String nicePushName;
+
+	private Repository repo;
+
+	private SimplePushSpecPage targetPage;
+
+	/**
+	 * Creates a new simple push wizard for a ref
+	 *
+	 * @param repo
+	 *            the repository the ref belongs to
+	 * @param refToPush
+	 *            the ref to push
+	 * @throws URISyntaxException
+	 */
+	public SimplePushRefWizard(Repository repo, Ref refToPush)
+			throws URISyntaxException {
+		this(repo, refToPush.getObjectId(), refToPush.getName());
+	}
+
+	/**
+	 * Creates a new simple push wizard which can be used to push out a certain
+	 * object.
+	 *
+	 * @param repo
+	 *            the repository the object belongs to
+	 * @param objectId
+	 *            the object that should be pushed.
+	 * @throws URISyntaxException
+	 */
+	public SimplePushRefWizard(Repository repo, ObjectId objectId)
+			throws URISyntaxException {
+		this(repo, objectId, AbbreviatedObjectId.fromObjectId(objectId).name());
+	}
+
+	private SimplePushRefWizard(Repository repo, ObjectId objectId, String name)
+			throws URISyntaxException {
+		final List<RemoteConfig> remotes = RemoteConfig
+				.getAllRemoteConfigs(repo.getConfig());
+
+		this.nicePushName = name;
+		this.pushObj = objectId;
+		this.repo = repo;
+
+		repoPage = new RepositorySelectionPage(false, remotes, null);
+		targetPage = new SimplePushSpecPage(nicePushName, repo) {
+			@Override
+			public void setVisible(boolean visible) {
+				super.setVisible(visible);
+
+				if (visible)
+					try {
+						if (assist != null
+								&& assist.getRepository().equals(
+										SimplePushRefWizard.this.repo)
+								&& assist.getRemoteURI().equals(
+										repoPage.getSelection().getURI(true)))
+							return;
+
+						assist = new RefContentAssistProvider(
+								SimplePushRefWizard.this.repo, repoPage
+										.getSelection().getURI(true), getShell());
+
+					} finally {
+						updateDestinationField();
+					}
+			}
+		};
+	}
+
+	@Override
+	public void addPages() {
+		addPage(repoPage);
+		addPage(targetPage);
+	}
+
+	@Override
+	public boolean performFinish() {
+		try {
+			int timeout = Activator.getDefault().getPreferenceStore()
+					.getInt(UIPreferences.REMOTE_CONNECTION_TIMEOUT);
+
+			PushOperationSpecification specification = new PushOperationSpecification();
+			RepositorySelection remote = repoPage.getSelection();
+
+			RemoteRefUpdate update = new RemoteRefUpdate(repo, null, pushObj,
+					targetPage.getTargetRef(), targetPage.isForceUpdate(),
+					null, null);
+
+			specification.addURIRefUpdates(remote.getURI(true),
+					Collections.singleton(update));
+
+			PushOperation pop = new PushOperation(repo, specification, false,
+					timeout);
+
+			PushJob job = new PushWizard.PushJob(repo, pop, null,
+					PushWizard.getDestinationString(remote));
+			job.setUser(true);
+			job.schedule();
+
+			repoPage.saveUriInPrefs();
+		} catch (Exception e) {
+			Activator.handleError(e.getMessage(), e, true);
+		}
+
+		return true;
+	}
+
+}
