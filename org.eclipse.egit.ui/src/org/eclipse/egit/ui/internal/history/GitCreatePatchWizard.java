1343e86990f989722e39f86a7aef81c34fa5d7cc
Tomasz Zarna <Tomasz.Zarna@pl.ibm.com>
2012-03-16 01:47:46 +0100

    IAE: Empty path not permitted. when creating a patch
    The exception is thrown when trying to create a patch for a project
    which is at the same level as repository.
    
    Change-Id: I43c369f6301c4b436f835c73fe41aeee1c0046ed
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitCreatePatchWizard.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitCreatePatchWizard.java
@@ -223,7 +223,11 @@ public class GitCreatePatchWizard extends Wizard {
 			RepositoryMapping rm = RepositoryMapping.getMapping(r);
 			String repoRelativePath = rm.getRepoRelativePath(r);
 			if (repoRelativePath != null)
-				filters.add(PathFilter.create(repoRelativePath));
+				if (repoRelativePath.equals("")) //$NON-NLS-1$
+					// repository selected
+					return TreeFilter.ALL;
+				else
+					filters.add(PathFilter.create(repoRelativePath));
 		}
 		if (filters.size() == 0)
 			return null;
