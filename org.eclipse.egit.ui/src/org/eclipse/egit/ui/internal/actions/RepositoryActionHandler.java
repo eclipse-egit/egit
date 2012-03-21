f733392a875e79c922adad667c23ff186b0c33b6
Mathias Kinzler <mathias.kinzler@sap.com>
2012-03-21 12:46:35 -0700

    Correct wrong enablement of commands
    Bug: 374953
    Change-Id: Ibbac745d562d4ba485653ca4205974c71041434c
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>
    Signed-off-by: Kevin Sawicki <kevin@github.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/RepositoryActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/RepositoryActionHandler.java
@@ -291,7 +291,7 @@ abstract class RepositoryActionHandler extends AbstractHandler {
 	 */
 	protected Repository[] getRepositories(ExecutionEvent event)
 			throws ExecutionException {
-		IProject[] selectedProjects = getSelectedProjects(event);
+		IProject[] selectedProjects = getProjectsForSelectedResources(event);
 		if (selectedProjects.length > 0)
 			return getRepositoriesFor(selectedProjects);
 		IStructuredSelection selection = getSelection(event);
@@ -318,7 +318,7 @@ abstract class RepositoryActionHandler extends AbstractHandler {
 	 * @return repositories for selection, or an empty array
 	 */
 	protected Repository[] getRepositories() {
-		IProject[] selectedProjects = getSelectedProjects(getSelection());
+		IProject[] selectedProjects = getProjectsForSelectedResources();
 		if (selectedProjects.length > 0)
 			return getRepositoriesFor(selectedProjects);
 		IStructuredSelection selection = getSelection();
@@ -461,25 +461,6 @@ abstract class RepositoryActionHandler extends AbstractHandler {
 				return adapter;
 		}
 		return null;
-	}
-
-	private IProject[] getSelectedProjects(ExecutionEvent event)
-			throws ExecutionException {
-		IStructuredSelection selection = getSelection(event);
-		return getSelectedProjects(selection);
-	}
-
-	private IProject[] getSelectedProjects(IStructuredSelection selection) {
-		IResource[] selectedResources = getSelectedResources(selection);
-		if (selectedResources.length == 0)
-			return new IProject[0];
-		ArrayList<IProject> projects = new ArrayList<IProject>();
-		for (int i = 0; i < selectedResources.length; i++) {
-			IResource resource = selectedResources[i];
-			if (resource.getType() == IResource.PROJECT)
-				projects.add((IProject) resource);
-		}
-		return projects.toArray(new IProject[projects.size()]);
 	}
 
 	/**
