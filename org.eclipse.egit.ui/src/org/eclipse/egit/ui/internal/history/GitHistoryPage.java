4af32d16219f3c3f106ae58aa30842a8ac56ed0d
Mathias Kinzler <mathias.kinzler@sap.com>
2012-03-21 19:40:07 +0100

    [historyView] Allow to suppress displaying of additional Refs
    The normal user is not interested in Refs like FETCH_HEAD, ORIG_HEAD
    etc. when using the HistoryView. However, these Refs can pollute the
    view as they can not be hidden independently if "Show All Branches" is
    active.
    
    With this change, the additional Refs can be hidden by the user.
    The HEAD Ref is always shown independent from this switch.
    
    The toggle was also added to the History Preference page (along with
    the "Show all Branches and Tags" toggle).
    
    When the user does an import from Gerrit and selects either "Update
    FETCH_HEAD only" or "Checkout FETCH_HEAD" and the toggle is off,
    a checkbox will be shown informing the user about this fact and adding
    the possibility to change this toggle during the course of the fetch.
    
    Bug: 359111
    Change-Id: I2b5df21e88ab7015012ef670de33819bd65de037
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitHistoryPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitHistoryPage.java
@@ -230,6 +230,8 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 
 		IWorkbenchAction showAllBranchesAction;
 
+		IWorkbenchAction showAdditionalRefsAction;
+
 		BooleanPrefAction followRenamesAction;
 
 		IWorkbenchAction reuseCompareEditorAction;
@@ -257,6 +259,7 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 			createCompareModeAction();
 			createReuseCompareEditorAction();
 			createShowAllBranchesAction();
+			createShowAdditionalRefsAction();
 			createShowCommentAction();
 			createShowFilesAction();
 			createShowRelativeDateAction();
@@ -365,6 +368,19 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 			showAllBranchesAction
 					.setToolTipText(UIText.GitHistoryPage_showAllBranches);
 			actionsToDispose.add(showAllBranchesAction);
+		}
+
+		private void createShowAdditionalRefsAction() {
+			showAdditionalRefsAction = new BooleanPrefAction(
+					UIPreferences.RESOURCEHISTORY_SHOW_ADDITIONAL_REFS,
+					UIText.GitHistoryPage_ShowAdditionalRefsMenuLabel) {
+
+				@Override
+				void apply(boolean value) {
+					historyPage.refresh();
+				}
+			};
+			actionsToDispose.add(showAdditionalRefsAction);
 		}
 
 		private void createFollowRenamesAction() {
@@ -588,6 +604,8 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 	private Repository currentRepo;
 
 	private boolean currentShowAllBranches;
+
+	private boolean currentShowAdditionalRefs;
 
 	private boolean currentShowNotes;
 
@@ -897,6 +915,7 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 				UIText.GitHistoryPage_ShowSubMenuLabel);
 		viewMenuMgr.add(showSubMenuMgr);
 		showSubMenuMgr.add(actions.showAllBranchesAction);
+		showSubMenuMgr.add(actions.showAdditionalRefsAction);
 		showSubMenuMgr.add(actions.showNotesAction);
 		showSubMenuMgr.add(actions.followRenamesAction);
 		showSubMenuMgr.add(new Separator());
@@ -1508,6 +1527,11 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 		currentShowAllBranches = store
 			.getBoolean(UIPreferences.RESOURCEHISTORY_SHOW_ALL_BRANCHES);
 
+		boolean additionalRefsChange = currentShowAdditionalRefs != store
+				.getBoolean(UIPreferences.RESOURCEHISTORY_SHOW_ADDITIONAL_REFS);
+		currentShowAdditionalRefs = store
+				.getBoolean(UIPreferences.RESOURCEHISTORY_SHOW_ADDITIONAL_REFS);
+
 		boolean showNotesChanged = currentShowNotes != store
 				.getBoolean(UIPreferences.RESOURCEHISTORY_SHOW_NOTES);
 		currentShowNotes = store
@@ -1522,7 +1546,7 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 
 		return pathChanged
 			|| currentWalk == null || headChanged || repoChanged || allBranchesChanged
-			|| showNotesChanged || followRenamesChanged;
+			|| additionalRefsChange || showNotesChanged || followRenamesChanged;
 	}
 
 	/**
@@ -1636,9 +1660,12 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 			currentWalk.release();
 		currentWalk = new SWTWalk(db);
 		try {
-			currentWalk.addAdditionalRefs(db.getRefDatabase().getAdditionalRefs());
-			currentWalk.addAdditionalRefs(db.getRefDatabase().
-					getRefs(Constants.R_NOTES).values());
+			if (store
+					.getBoolean(UIPreferences.RESOURCEHISTORY_SHOW_ADDITIONAL_REFS))
+				currentWalk.addAdditionalRefs(db.getRefDatabase()
+						.getAdditionalRefs());
+			currentWalk.addAdditionalRefs(db.getRefDatabase()
+					.getRefs(Constants.R_NOTES).values());
 		} catch (IOException e) {
 			throw new IllegalStateException(NLS.bind(
 					UIText.GitHistoryPage_errorReadingAdditionalRefs, Activator
@@ -1657,6 +1684,9 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 				markStartAllRefs(Constants.R_HEADS);
 				markStartAllRefs(Constants.R_REMOTES);
 				markStartAllRefs(Constants.R_TAGS);
+			}
+			if (store
+					.getBoolean(UIPreferences.RESOURCEHISTORY_SHOW_ADDITIONAL_REFS)) {
 				markStartAdditionalRefs();
 			}
 			if (store
