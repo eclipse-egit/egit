9e1787c69407b59a0649a1c42b0b393769549db9
Mathias Kinzler <mathias.kinzler@sap.com>
2012-03-23 15:34:23 +0100

    Add a "Previous Version" option to the Replace With menu
    If the current commit is a merge commit, this will bring a dialog asking
    the user to select one of the parents.
    
    This dialog is also added to the "Compare with Previous" action handler.
    
    Change-Id: I4b140e254c76477ba644b15eb77286d65887515c

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CompareWithPreviousActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CompareWithPreviousActionHandler.java
@@ -12,6 +12,8 @@ package org.eclipse.egit.ui.internal.actions;
 
 import java.io.IOException;
 import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 import org.eclipse.compare.CompareEditorInput;
@@ -23,6 +25,7 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
 import org.eclipse.egit.core.internal.job.JobUtil;
 import org.eclipse.egit.core.op.IEGitOperation;
@@ -31,9 +34,11 @@ import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.internal.CompareUtils;
 import org.eclipse.egit.ui.internal.GitCompareFileRevisionEditorInput;
+import org.eclipse.egit.ui.internal.dialogs.CommitSelectDialog;
 import org.eclipse.egit.ui.internal.dialogs.CompareTreeView;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jface.window.Window;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.FollowFilter;
@@ -52,18 +57,6 @@ import org.eclipse.ui.handlers.HandlerUtil;
 public class CompareWithPreviousActionHandler extends RepositoryActionHandler {
 
 	private static class CompareWithPreviousOperation implements IEGitOperation {
-
-		private static class PreviousCommit {
-
-			final RevCommit commit;
-
-			final String path;
-
-			PreviousCommit(final RevCommit commit, final String path) {
-				this.commit = commit;
-				this.path = path;
-			}
-		}
 
 		private ExecutionEvent event;
 
@@ -84,21 +77,37 @@ public class CompareWithPreviousActionHandler extends RepositoryActionHandler {
 		}
 
 		public void execute(IProgressMonitor monitor) throws CoreException {
-			PreviousCommit previous = findPreviousCommit();
-			if (previous != null)
-				if (resource instanceof IFile) {
-					final ITypedElement base = SaveableCompareEditorInput
-							.createFileElement((IFile) resource);
-					ITypedElement next = CompareUtils
-							.getFileRevisionTypedElement(previous.path,
-									previous.commit, repository);
-					CompareEditorInput input = new GitCompareFileRevisionEditorInput(
-							base, next, null);
-					CompareUI.openCompareEditor(input);
-				} else
-					openCompareTreeView(previous.commit);
-			else
+			final List<RevCommit> previousList = findPreviousCommits();
+			final AtomicReference<RevCommit> previous = new AtomicReference<RevCommit>();
+			if (previousList.size() == 0) {
 				showNotFoundDialog();
+				return;
+			} else if (previousList.size() > 1)
+				HandlerUtil.getActiveShell(event).getDisplay()
+						.syncExec(new Runnable() {
+							public void run() {
+								CommitSelectDialog dlg = new CommitSelectDialog(
+										HandlerUtil.getActiveShell(event),
+										previousList);
+								if (dlg.open() == Window.OK)
+									previous.set(dlg.getSelectedCommit());
+								else
+									throw new OperationCanceledException();
+							}
+						});
+			else
+				previous.set(previousList.get(0));
+
+			if (resource instanceof IFile) {
+				final ITypedElement base = SaveableCompareEditorInput
+						.createFileElement((IFile) resource);
+				ITypedElement next = CompareUtils.getFileRevisionTypedElement(
+						getRepositoryPath(), previous.get(), repository);
+				CompareEditorInput input = new GitCompareFileRevisionEditorInput(
+						base, next, null);
+				CompareUI.openCompareEditor(input);
+			} else
+				openCompareTreeView(previous.get());
 		}
 
 		private void openCompareTreeView(final RevCommit previous) {
@@ -119,8 +128,9 @@ public class CompareWithPreviousActionHandler extends RepositoryActionHandler {
 			});
 		}
 
-		private PreviousCommit findPreviousCommit() {
+		private List<RevCommit> findPreviousCommits() {
 			final AtomicReference<String> previousPath = new AtomicReference<String>();
+			List<RevCommit> result = new ArrayList<RevCommit>();
 			RevWalk rw = new RevWalk(repository);
 			try {
 				String path = getRepositoryPath();
@@ -140,22 +150,21 @@ public class CompareWithPreviousActionHandler extends RepositoryActionHandler {
 						Constants.HEAD).getObjectId());
 				rw.markStart(headCommit);
 				headCommit = rw.next();
-
-				if (headCommit == null)
-					return null;
-				RevCommit previousCommit = rw.next();
-				if (previousCommit == null)
-					return null;
-
-				if (previousPath.get() == null)
-					previousPath.set(getRepositoryPath());
-				return new PreviousCommit(previousCommit, previousPath.get());
+				if (headCommit != null) {
+					RevCommit[] headParents = headCommit.getParents();
+					for (int i = 0; i < 2; i++) {
+						RevCommit possibleParent = rw.next();
+						for (RevCommit parent : headParents)
+							if (parent.equals(possibleParent))
+								result.add(possibleParent);
+					}
+				}
 			} catch (IOException e) {
 				Activator.handleError(e.getMessage(), e, true);
 			} finally {
 				rw.dispose();
 			}
-			return null;
+			return result;
 		}
 
 		private void showNotFoundDialog() {
@@ -196,5 +205,10 @@ public class CompareWithPreviousActionHandler extends RepositoryActionHandler {
 					UIText.CompareWithPreviousActionHandler_TaskGeneratingInput,
 					null);
 		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return super.isEnabled() && getSelectedResources().length == 1;
 	}
 }
