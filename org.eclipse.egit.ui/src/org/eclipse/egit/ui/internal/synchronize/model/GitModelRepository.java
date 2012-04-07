eclipse-egit/egit

commit f52917721af7c7dfce44cc655fb3586a811793fb

[sync] Dispose resources used by 'Git Commit' model
Dispose all nested objects in collection 'Git Commit' model. This should
make things easier for garbage collector since all circular
dependencies should be cleaned.

Also prevent creating new instance of
GitResourceVariantTreeSubscriber when
GitRepositoryProviderType.getSubscriber() method is called.

Change-Id: Ie77ea8d9faa2d69c443cd07cf49e0f3be6b3a199
Signed-off-by: Dariusz Luksza <dariusz@luksza.org>
Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/model/GitModelRepository.java
@@ -25,6 +25,7 @@ import org.eclipse.egit.core.synchronize.GitCommitsModelCache.Commit;
 import org.eclipse.egit.core.synchronize.StagedChangeCache;
 import org.eclipse.egit.core.synchronize.WorkingTreeChangeCache;
 import org.eclipse.egit.core.synchronize.dto.GitSynchronizeData;
+import org.eclipse.egit.ui.Activator;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
@@ -40,11 +41,7 @@ public class GitModelRepository extends GitModelObjectContainer implements HasPr
 
 	private final GitSynchronizeData gsd;
 
-	private final List<Commit> commitCache;
-
-	private final Map<String, Change> stagedChanges;
-
-	private final Map<String, Change> workingChanges;
+	private GitModelObject[] children;
 
 	/**
 	 * @param gsd
@@ -54,30 +51,36 @@ public class GitModelRepository extends GitModelObjectContainer implements HasPr
 	public GitModelRepository(GitSynchronizeData gsd) throws IOException {
 		super(null);
 		this.gsd = gsd;
-
-		Repository repo = gsd.getRepository();
-		stagedChanges = StagedChangeCache.build(repo);
-		workingChanges = WorkingTreeChangeCache.build(repo);
-
-		RevCommit srcRevCommit = gsd.getSrcRevCommit();
-		RevCommit dstRevCommit = gsd.getDstRevCommit();
-		TreeFilter pathFilter = gsd.getPathFilter();
-		if (srcRevCommit != null && dstRevCommit != null)
-			commitCache = GitCommitsModelCache.build(repo, srcRevCommit,
-					dstRevCommit, pathFilter);
-		else
-			commitCache = null;
 	}
 
 	@Override
 	public GitModelObject[] getChildren() {
-		List<GitModelObjectContainer> result = new ArrayList<GitModelObjectContainer>();
-		if (commitCache != null && !commitCache.isEmpty())
-			result.addAll(getListOfCommit());
+		if (children == null) {
+			List<GitModelObjectContainer> result = new ArrayList<GitModelObjectContainer>();
+			Repository repo = gsd.getRepository();
+			RevCommit srcRevCommit = gsd.getSrcRevCommit();
+			RevCommit dstRevCommit = gsd.getDstRevCommit();
+			TreeFilter pathFilter = gsd.getPathFilter();
+			List<Commit> commitCache;
+			if (srcRevCommit != null && dstRevCommit != null)
+				try {
+					commitCache = GitCommitsModelCache.build(repo, srcRevCommit,
+							dstRevCommit, pathFilter);
+				} catch (IOException e) {
+					Activator.logError(e.getMessage(), e);
+					commitCache = null;
+				}
+			else
+				commitCache = null;
+			if (commitCache != null && !commitCache.isEmpty())
+				result.addAll(getListOfCommit(commitCache));
 
-		result.addAll(getWorkingChanges());
+			result.addAll(getWorkingChanges());
 
-		return result.toArray(new GitModelObjectContainer[result.size()]);
+			children = result.toArray(new GitModelObjectContainer[result.size()]);
+		}
+
+		return children;
 	}
 
 	@Override
@@ -120,6 +123,12 @@ public class GitModelRepository extends GitModelObjectContainer implements HasPr
 	}
 
 	@Override
+	public void dispose() {
+		for (GitModelObject objects : children)
+			objects.dispose();
+	}
+
+	@Override
 	public boolean equals(Object obj) {
 		if (obj == this)
 			return true;
@@ -143,7 +152,7 @@ public class GitModelRepository extends GitModelObjectContainer implements HasPr
 		return "ModelRepository[" + gsd.getRepository().getWorkTree() + "]"; //$NON-NLS-1$ //$NON-NLS-2$
 	}
 
-	private List<GitModelObjectContainer> getListOfCommit() {
+	private List<GitModelObjectContainer> getListOfCommit(List<Commit> commitCache) {
 		Repository repo = gsd.getRepository();
 		Set<IProject> projectsSet = gsd.getProjects();
 		IProject[] projects = projectsSet.toArray(new IProject[projectsSet.size()]);
@@ -159,10 +168,12 @@ public class GitModelRepository extends GitModelObjectContainer implements HasPr
 		List<GitModelObjectContainer> result = new ArrayList<GitModelObjectContainer>();
 		if (gsd.shouldIncludeLocal()) {
 			Repository repo = gsd.getRepository();
+			Map<String, Change> stagedChanges = StagedChangeCache.build(repo);
 			GitModelCache gitCache = new GitModelCache(this, repo,
 					stagedChanges);
 			int gitCacheLen = gitCache.getChildren().length;
 
+			Map<String, Change> workingChanges = WorkingTreeChangeCache.build(repo);
 			GitModelWorkingTree gitWorkingTree = new GitModelWorkingTree(this,
 					repo, workingChanges);
 			int gitWorkingTreeLen = gitWorkingTree.getChildren().length;

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/model/GitModelRoot.java
@@ -23,6 +23,8 @@ public class GitModelRoot {
 
 	private final GitSynchronizeDataSet gsds;
 
+	private GitModelObject[] children;
+
 	/**
 	 * @param gsds
 	 */
@@ -41,6 +43,23 @@ public class GitModelRoot {
 	 * @return children
 	 */
 	public GitModelObject[] getChildren() {
+		if (children == null)
+			children = getChildrenImpl();
+
+		return children;
+	}
+
+	/**
+	 *  Disposes all nested resources
+	 */
+	public void dispose() {
+		for (GitModelObject child : children)
+			child.dispose();
+
+		gsds.dispose();
+	}
+
+	private GitModelObject[] getChildrenImpl() {
 		List<GitModelObject> restult = new ArrayList<GitModelObject>();
 		try {
 			if (gsds.size() == 1) {

org.eclipse.egit.core/src/org/eclipse/egit/core/internal/GitRepositoryProviderType.java
@@ -30,7 +30,12 @@ import org.eclipse.team.core.subscribers.Subscriber;
  */
 public class GitRepositoryProviderType extends RepositoryProviderType {
 
-	public Subscriber getSubscriber() {
+	private final Subscriber subscriber;
+
+	/**
+	 * Creates {@link GitRepositoryProviderType}
+	 */
+	public GitRepositoryProviderType() {
 		GitSynchronizeDataSet set = new GitSynchronizeDataSet();
 		try {
 			Repository[] repositories = Activator.getDefault()
@@ -44,9 +49,13 @@ public class GitRepositoryProviderType extends RepositoryProviderType {
 			// do nothing
 		}
 
-		GitResourceVariantTreeSubscriber subscriber = new GitResourceVariantTreeSubscriber(set);
-		subscriber.init(new NullProgressMonitor());
+		GitResourceVariantTreeSubscriber gitSubscriber = new GitResourceVariantTreeSubscriber(set);
+		gitSubscriber.init(new NullProgressMonitor());
 
+		subscriber = gitSubscriber;
+	}
+
+	public Subscriber getSubscriber() {
 		return subscriber;
 	}
 

org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/GitCommitsModelCache.java
@@ -142,6 +142,13 @@ public class GitCommitsModelCache {
 			return children;
 		}
 
+		/**
+		 * Disposes nested resources
+		 */
+		public void dispose() {
+			children.clear();
+		}
+
 	}
 
 	/**

org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/GitResourceVariantTree.java
@@ -15,10 +15,10 @@ import static org.eclipse.egit.core.internal.util.ResourceUtil.isNonWorkspace;
 import static org.eclipse.jgit.lib.ObjectId.zeroId;
 import static org.eclipse.jgit.lib.Repository.stripWorkDir;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.WeakHashMap;
 
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IResource;
@@ -42,7 +42,7 @@ abstract class GitResourceVariantTree extends ResourceVariantTree {
 
 	private final GitSynchronizeDataSet gsds;
 
-	private final Map<IResource, IResourceVariant> cache = new HashMap<IResource, IResourceVariant>();
+	private final Map<IResource, IResourceVariant> cache = new WeakHashMap<IResource, IResourceVariant>();
 
 
 	GitResourceVariantTree(ResourceVariantByteStore store,
@@ -62,6 +62,16 @@ abstract class GitResourceVariantTree extends ResourceVariantTree {
 					roots.add(container.getProject());
 
 		return roots.toArray(new IResource[roots.size()]);
+	}
+
+	/**
+	 * Disposes all nested resources
+	 */
+	public void dispose() {
+		if (gsds != null)
+			gsds.dispose();
+
+		cache.clear();
 	}
 
 	@Override

org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/GitResourceVariantTreeSubscriber.java
@@ -49,12 +49,12 @@ public class GitResourceVariantTreeSubscriber extends
 	/**
 	 * A resource variant tree of the remote branch(es).
 	 */
-	private IResourceVariantTree remoteTree;
+	private GitRemoteResourceVariantTree remoteTree;
 
 	/**
 	 * A resource variant tree against HEAD.
 	 */
-	private IResourceVariantTree baseTree;
+	private GitBaseResourceVariantTree baseTree;
 
 	private GitSynchronizeDataSet gsds;
 
@@ -205,6 +205,17 @@ public class GitResourceVariantTreeSubscriber extends
 		roots = null;
 		baseTree = null;
 		remoteTree = null;
+	}
+
+	/**
+	 * Disposes nested resources
+	 */
+	public void dispose() {
+		if (baseTree != null)
+			baseTree.dispose();
+		if (remoteTree != null)
+			remoteTree.dispose();
+		gsds.dispose();
 	}
 
 	@Override

org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/GitSubscriberMergeContext.java
@@ -48,6 +48,8 @@ public class GitSubscriberMergeContext extends SubscriberMergeContext {
 
 	private final IResourceChangeListener resourceChangeListener;
 
+	private final GitResourceVariantTreeSubscriber subscriber;
+
 	/**
 	 * @param subscriber
 	 * @param manager
@@ -56,13 +58,14 @@ public class GitSubscriberMergeContext extends SubscriberMergeContext {
 	public GitSubscriberMergeContext(final GitResourceVariantTreeSubscriber subscriber,
 			ISynchronizationScopeManager manager, GitSynchronizeDataSet gsds) {
 		super(subscriber, manager);
+		this.subscriber = subscriber;
 		this.gsds = gsds;
 
 
 		indexChangeListener = new IndexDiffChangedListener() {
 			public void indexDiffChanged(Repository repository,
 					IndexDiffData indexDiffData) {
-				handleRepositoryChange(subscriber, repository);
+				handleRepositoryChange(repository);
 			}
 		};
 		resourceChangeListener = new IResourceChangeListener() {
@@ -72,7 +75,7 @@ public class GitSubscriberMergeContext extends SubscriberMergeContext {
 				if (delta == null)
 					return;
 
-				handleResourceChange(subscriber, delta);
+				handleResourceChange(delta);
 			}
 		};
 		IndexDiffCache indexDiffCache = Activator.getDefault().getIndexDiffCache();
@@ -123,11 +126,11 @@ public class GitSubscriberMergeContext extends SubscriberMergeContext {
 			indexDiffCache.removeIndexDiffChangedListener(indexChangeListener);
 
 		ResourcesPlugin.getWorkspace().removeResourceChangeListener(resourceChangeListener);
+		subscriber.dispose();
 		super.dispose();
 	}
 
-	private void handleRepositoryChange(
-			GitResourceVariantTreeSubscriber subscriber, Repository which) {
+	private void handleRepositoryChange(Repository which) {
 		for (GitSynchronizeData gsd : gsds)
 			if (which.equals(gsd.getRepository()))
 				updateRevs(gsd);
@@ -143,8 +146,7 @@ public class GitSubscriberMergeContext extends SubscriberMergeContext {
 		}
 	}
 
-	private void handleResourceChange(
-			GitResourceVariantTreeSubscriber subscriber, IResourceDelta delta) {
+	private void handleResourceChange(IResourceDelta delta) {
 		IResourceDelta[] children = delta.getAffectedChildren();
 		for (IResourceDelta resourceDelta : children) {
 			IResource resource = resourceDelta.getResource();
@@ -152,13 +154,12 @@ public class GitSubscriberMergeContext extends SubscriberMergeContext {
 			if (mapping == null)
 				continue;
 
-			scanDeltaAndRefresh(subscriber, mapping, resourceDelta);
+			scanDeltaAndRefresh(mapping, resourceDelta);
 		}
 	}
 
-	private void scanDeltaAndRefresh(
-			GitResourceVariantTreeSubscriber subscriber,
-			RepositoryMapping mapping, IResourceDelta delta) {
+	private void scanDeltaAndRefresh(RepositoryMapping mapping,
+			IResourceDelta delta) {
 		Repository repo = mapping.getRepository();
 		GitResourceDeltaVisitor visitor = new GitResourceDeltaVisitor(repo);
 		try {
@@ -169,15 +170,14 @@ public class GitSubscriberMergeContext extends SubscriberMergeContext {
 
 			for (GitSynchronizeData gsd : gsds) {
 				if (repo.equals(gsd.getRepository()))
-					refreshResources(subscriber, files);
+					refreshResources(files);
 			}
 		} catch (CoreException e) {
 			Activator.logError(e.getMessage(), e);
 		}
 	}
 
-	private void refreshResources(GitResourceVariantTreeSubscriber subscriber,
-			Collection<IFile> resources) {
+	private void refreshResources(Collection<IFile> resources) {
 		IResource[] files = resources.toArray(new IResource[resources
 				.size()]);
 		try {

org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/dto/GitSynchronizeData.java
@@ -260,6 +260,16 @@ public class GitSynchronizeData {
 	}
 
 	/**
+	 * Disposes all nested resources
+	 */
+	public void dispose() {
+		if (projects != null)
+			projects.clear();
+		if (includedPaths != null)
+			includedPaths.clear();
+	}
+
+	/**
 	 * @return instance of {@link TreeFilter} when synchronization was launched
 	 *         from nested node (like folder) or {@code null} otherwise
 	 */

commit c5f67dfaab5e5a085dff9dbbed77fc4416a68a85

[sync] Fix refreshing Git Commits model after repository change
Commit f52917721af7c7dfce44cc655fb3586a811793fb broke refresh Git
Commits model after repository change event capability by caching result
of first children computation inside repository and root model object.

This patch preserve resource disposition feature plus fixes mentioned
refresh of Git Commits model.

Bug: 345286
Change-Id: I7be92924973f67e21ce811aa17ee2d9afcc33118
Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/model/GitModelRepository.java
@@ -55,30 +55,28 @@ public class GitModelRepository extends GitModelObjectContainer implements HasPr
 
 	@Override
 	public GitModelObject[] getChildren() {
-		if (children == null) {
-			List<GitModelObjectContainer> result = new ArrayList<GitModelObjectContainer>();
-			Repository repo = gsd.getRepository();
-			RevCommit srcRevCommit = gsd.getSrcRevCommit();
-			RevCommit dstRevCommit = gsd.getDstRevCommit();
-			TreeFilter pathFilter = gsd.getPathFilter();
-			List<Commit> commitCache;
-			if (srcRevCommit != null && dstRevCommit != null)
-				try {
-					commitCache = GitCommitsModelCache.build(repo, srcRevCommit,
-							dstRevCommit, pathFilter);
-				} catch (IOException e) {
-					Activator.logError(e.getMessage(), e);
-					commitCache = null;
-				}
-			else
+		List<GitModelObjectContainer> result = new ArrayList<GitModelObjectContainer>();
+		Repository repo = gsd.getRepository();
+		RevCommit srcRevCommit = gsd.getSrcRevCommit();
+		RevCommit dstRevCommit = gsd.getDstRevCommit();
+		TreeFilter pathFilter = gsd.getPathFilter();
+		List<Commit> commitCache;
+		if (srcRevCommit != null && dstRevCommit != null)
+			try {
+				commitCache = GitCommitsModelCache.build(repo, srcRevCommit,
+						dstRevCommit, pathFilter);
+			} catch (IOException e) {
+				Activator.logError(e.getMessage(), e);
 				commitCache = null;
-			if (commitCache != null && !commitCache.isEmpty())
-				result.addAll(getListOfCommit(commitCache));
+			}
+		else
+			commitCache = null;
+		if (commitCache != null && !commitCache.isEmpty())
+			result.addAll(getListOfCommit(commitCache));
 
-			result.addAll(getWorkingChanges());
-
-			children = result.toArray(new GitModelObjectContainer[result.size()]);
-		}
+		result.addAll(getWorkingChanges());
+		disposeOldChildren();
+		children = result.toArray(new GitModelObjectContainer[result.size()]);
 
 		return children;
 	}
@@ -124,8 +122,7 @@ public class GitModelRepository extends GitModelObjectContainer implements HasPr
 
 	@Override
 	public void dispose() {
-		for (GitModelObject objects : children)
-			objects.dispose();
+		disposeOldChildren();
 	}
 
 	@Override
@@ -192,6 +189,14 @@ public class GitModelRepository extends GitModelObjectContainer implements HasPr
 	 */
 	public Repository getRepository() {
 		return gsd.getRepository();
+	}
+
+	private void disposeOldChildren() {
+		if (children == null)
+			return;
+		for (GitModelObject child : children)
+			child.dispose();
+
 	}
 
 }

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/model/GitModelRoot.java
@@ -43,19 +43,14 @@ public class GitModelRoot {
 	 * @return children
 	 */
 	public GitModelObject[] getChildren() {
-		if (children == null)
-			children = getChildrenImpl();
-
-		return children;
+		return getChildrenImpl();
 	}
 
 	/**
 	 *  Disposes all nested resources
 	 */
 	public void dispose() {
-		for (GitModelObject child : children)
-			child.dispose();
-
+		disposeOldChildren();
 		gsds.dispose();
 	}
 
@@ -76,8 +71,17 @@ public class GitModelRoot {
 		} catch (IOException e) {
 				Activator.logError(e.getMessage(), e);
 		}
+		disposeOldChildren();
+		children = restult.toArray(new GitModelObject[restult.size()]);
 
-		return restult.toArray(new GitModelObject[restult.size()]);
+		return children;
+	}
+
+	private void disposeOldChildren() {
+		if (children == null)
+			return;
+		for (GitModelObject child : children)
+			child.dispose();
 	}
 
 }