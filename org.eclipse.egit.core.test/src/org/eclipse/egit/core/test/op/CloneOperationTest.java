7788ea654ff6b3f3da46c0b231a381a10b40bdc1
Robin Rosenberg <robin.rosenberg@dewire.com>
2012-04-14 12:44:36 +0200

    Restore ability to not checkout any branch in clone operation
    Change-Id: Iba9fcfbe4cb1511cc5f44900365c3a484c159339
    Signed-off-by: Robin Rosenberg <robin.rosenberg@dewire.com>

--- org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/op/CloneOperationTest.java
+++ org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/op/CloneOperationTest.java
@@ -10,6 +10,7 @@
 package org.eclipse.egit.core.test.op;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
@@ -84,6 +85,36 @@ public class CloneOperationTest extends DualRepositoryTestCase {
 
 		File file = new File(workdir2, "file1.txt");
 		assertTrue(file.exists());
+	}
+
+	@Test
+	public void testCloneNoCheckout() throws Exception {
+		URIish uri = new URIish("file:///"
+				+ repository1.getRepository().getDirectory().toString());
+		CloneOperation clop = new CloneOperation(uri, true, null, workdir2,
+				null, "origin", 0);
+		clop.run(null);
+
+		Repository clonedRepo = new FileRepository(new File(workdir2,
+				Constants.DOT_GIT));
+		assertEquals(
+				"",
+				uri.toString(),
+				clonedRepo.getConfig().getString(
+						ConfigConstants.CONFIG_REMOTE_SECTION, "origin", "url"));
+		assertEquals(
+				"",
+				"+refs/heads/*:refs/remotes/origin/*",
+				clonedRepo.getConfig().getString(
+						ConfigConstants.CONFIG_REMOTE_SECTION, "origin",
+						"fetch"));
+
+		File file = new File(workdir2, "file1.txt");
+		assertFalse(file.exists());
+		String[] filesInWorkDir = workdir2.list();
+		assertEquals(1, filesInWorkDir.length);
+		assertEquals(".git", filesInWorkDir[0]);
+		assertFalse(new File(workdir2, ".git/index").exists());
 	}
 
 	@Test
