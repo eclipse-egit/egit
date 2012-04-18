ea1cdc4299562afb6a2b785373a666059b6dd064
Mathias Kinzler <mathias.kinzler@sap.com>
2012-04-18 23:54:12 +0200

    Reuse BranchRenameDialog in Repositories View
    Change-Id: Idf3170326c86a8d35a38a9ce90b9f46fd6047e7a
    Signed-off-by: Mathias Kinzler <mathias.kinzler@sap.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/RenameBranchCommand.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/RenameBranchCommand.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 EclipseSource.
+ * Copyright (c) 2010, 2012 EclipseSource.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *    Benjamin Muskalla (EclipseSource) - initial implementation
+ *    Mathias Kinzler <mathias.kinzler@sap.com> - replace InputDialog with RenameBranchDialog
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.repository.tree.command;
 
@@ -14,18 +15,8 @@ import java.util.List;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.egit.core.op.RenameBranchOperation;
-import org.eclipse.egit.ui.Activator;
-import org.eclipse.egit.ui.UIText;
-import org.eclipse.egit.ui.internal.ValidationUtils;
+import org.eclipse.egit.ui.internal.dialogs.BranchRenameDialog;
 import org.eclipse.egit.ui.internal.repository.tree.RefNode;
-import org.eclipse.jface.dialogs.IInputValidator;
-import org.eclipse.jface.dialogs.InputDialog;
-import org.eclipse.jface.window.Window;
-import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.widgets.Shell;
 
 /**
@@ -38,33 +29,7 @@ public class RenameBranchCommand extends
 		RefNode refNode = nodes.get(0);
 
 		Shell shell = getShell(event);
-		String oldName = refNode.getObject().getName();
-		String prefix;
-		if (oldName.startsWith(Constants.R_HEADS))
-			prefix = Constants.R_HEADS;
-		else if (oldName.startsWith(Constants.R_REMOTES))
-			prefix = Constants.R_REMOTES;
-		else
-			throw new ExecutionException(NLS.bind(
-					UIText.RenameBranchCommand_WrongNameMessage, oldName));
-		Repository db = refNode.getRepository();
-		IInputValidator inputValidator = ValidationUtils
-				.getRefNameInputValidator(db, prefix, true);
-		String defaultValue = Repository.shortenRefName(oldName);
-		InputDialog newNameDialog = new InputDialog(shell,
-				UIText.RepositoriesView_RenameBranchTitle, NLS.bind(
-						UIText.RepositoriesView_RenameBranchMessage,
-						defaultValue), defaultValue, inputValidator);
-		if (newNameDialog.open() == Window.OK) {
-			try {
-				String newName = newNameDialog.getValue();
-				new RenameBranchOperation(db, refNode.getObject(), newName)
-						.execute(null);
-			} catch (CoreException e) {
-				Activator.handleError(
-						UIText.RepositoriesView_RenameBranchFailure, e, true);
-			}
-		}
+		new BranchRenameDialog(shell, refNode.getRepository(), refNode.getObject()).open();
 		return null;
 	}
 }
