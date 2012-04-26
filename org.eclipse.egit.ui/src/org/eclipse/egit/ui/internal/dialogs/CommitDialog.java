5055b89378f59237b89019dd5311321a68c33de3
Robin Stocker <robin@nibor.org>
2012-04-27 00:53:24 +0200

    Extract class for saving/loading previous commit messages
    This enables reuse for implementing content assist in StagingView.
    
    Bug: 368081
    Change-Id: Id80411e36842262b9078e9eb6ec1010061b4679f
    Signed-off-by: Robin Stocker <robin@nibor.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitDialog.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitDialog.java
@@ -15,8 +15,6 @@ package org.eclipse.egit.ui.internal.dialogs;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
 import java.net.URI;
 import java.text.MessageFormat;
 import java.util.ArrayList;
@@ -24,7 +22,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.Set;
 
 import org.eclipse.core.resources.IFile;
@@ -38,6 +35,7 @@ import org.eclipse.egit.ui.UIPreferences;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.UIUtils;
 import org.eclipse.egit.ui.internal.CompareUtils;
+import org.eclipse.egit.ui.internal.commit.CommitMessageHistory;
 import org.eclipse.egit.ui.internal.commit.CommitProposalProcessor;
 import org.eclipse.egit.ui.internal.dialogs.CommitItem.Status;
 import org.eclipse.jface.dialogs.IDialogConstants;
@@ -107,11 +105,8 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
 import org.eclipse.team.core.RepositoryProvider;
-import org.eclipse.ui.IMemento;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.WorkbenchException;
-import org.eclipse.ui.XMLMemento;
 import org.eclipse.ui.dialogs.PreferencesUtil;
 import org.eclipse.ui.forms.IFormColors;
 import org.eclipse.ui.forms.widgets.ExpandableComposite;
@@ -124,75 +119,8 @@ import org.eclipse.ui.forms.widgets.Section;
  */
 public class CommitDialog extends TitleAreaDialog {
 
-	private static final String KEY_MESSAGE = "message"; //$NON-NLS-1$
-
-	private static final String KEY_MESSAGES = "messages"; //$NON-NLS-1$
-
 	private static IPreferenceStore getPreferenceStore() {
 		return org.eclipse.egit.ui.Activator.getDefault().getPreferenceStore();
-	}
-
-	private static Set<String> getCommitHistory() {
-		String all = getPreferenceStore().getString(
-				UIPreferences.COMMIT_DIALOG_HISTORY_MESSAGES);
-		if (all.length() == 0)
-			return Collections.emptySet();
-		int max = getCommitHistorySize();
-		if (max < 1)
-			return Collections.emptySet();
-		XMLMemento memento;
-		try {
-			memento = XMLMemento.createReadRoot(new StringReader(all));
-		} catch (WorkbenchException e) {
-			org.eclipse.egit.ui.Activator.logError(
-					"Error reading commit message history", e); //$NON-NLS-1$
-			return Collections.emptySet();
-		}
-		Set<String> messages = new LinkedHashSet<String>();
-		for (IMemento child : memento.getChildren(KEY_MESSAGE)) {
-			messages.add(child.getTextData());
-			if (messages.size() == max)
-				break;
-		}
-		return messages;
-	}
-
-	private static void saveCommitHistory(String message) {
-		if (message == null || message.length() == 0)
-			return;
-		int size = getCommitHistorySize();
-		if (size < 1)
-			return;
-
-		XMLMemento memento = XMLMemento.createWriteRoot(KEY_MESSAGES);
-		memento.createChild(KEY_MESSAGE).putTextData(message);
-
-		int count = 1;
-		if (count < size) {
-			Set<String> history = getCommitHistory();
-			history.remove(message);
-			for (String previous : history) {
-				memento.createChild(KEY_MESSAGE).putTextData(previous);
-				count++;
-				if (count == size)
-					break;
-			}
-		}
-		StringWriter writer = new StringWriter();
-		try {
-			memento.save(writer);
-			getPreferenceStore().setValue(
-					UIPreferences.COMMIT_DIALOG_HISTORY_MESSAGES,
-					writer.toString());
-		} catch (IOException e) {
-			org.eclipse.egit.ui.Activator.logError(
-					"Error writing commit message history", e); //$NON-NLS-1$
-		}
-	}
-
-	private static int getCommitHistorySize() {
-		return getPreferenceStore().getInt(
-				UIPreferences.COMMIT_DIALOG_HISTORY_SIZE);
 	}
 
 	static class CommitStatusLabelProvider extends ColumnLabelProvider {
@@ -696,7 +624,7 @@ public class CommitDialog extends TitleAreaDialog {
 				ContentAssistant assistant = new ContentAssistant();
 				assistant.enableAutoInsert(true);
 				Collection<String> paths = getFileList();
-				Collection<String> messages = getCommitHistory();
+				Collection<String> messages = CommitMessageHistory.getCommitHistory();
 				final CommitProposalProcessor processor = new CommitProposalProcessor(
 						messages.toArray(new String[messages.size()]),
 						paths.toArray(new String[paths.size()]));
@@ -1153,7 +1081,7 @@ public class CommitDialog extends TitleAreaDialog {
 		IDialogSettings settings = org.eclipse.egit.ui.Activator
 			.getDefault().getDialogSettings();
 		settings.put(SHOW_UNTRACKED_PREF, showUntracked);
-		saveCommitHistory(getCommitMessage());
+		CommitMessageHistory.saveCommitHistory(getCommitMessage());
 		super.okPressed();
 	}
 
