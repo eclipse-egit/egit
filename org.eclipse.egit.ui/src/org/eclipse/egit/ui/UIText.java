269a4c549695da4d07e0c756c4f3befa94f8aeea
Kevin Sawicki <kevin@github.com>
2012-05-03 09:59:10 -0700

    Load branches, tags, and diffs on background thread
    Previously the CommitEditorPage loads all data displayed
    on the UI thread which would cause temporary freezes for
    branches with many tags/branches or commits that changed
    many files.
    
    Now all loading is done in a background job with only the
    author, committer, parent, and message displaying initially.
    
    Change-Id: I92b882f9318d33cd7b3e892eba57efd0fefe5d2b

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -3500,6 +3500,12 @@ public class UIText extends NLS {
 	public static String CommitEditorInput_ToolTip;
 
 	/** */
+	public static String CommitEditorPage_JobName;
+
+	/** */
+	public static String CommitEditorPage_SectionBranchesEmpty;
+
+	/** */
 	public static String CommitEditorPage_LabelAuthor;
 
 	/** */
@@ -3516,6 +3522,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CommitEditorPage_SectionFiles;
+
+	/** */
+	public static String CommitEditorPage_SectionFilesEmpty;
 
 	/** */
 	public static String CommitEditorPage_SectionMessage;
