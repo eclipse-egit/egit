01302d6e24a9e091d9aef13d6b70fdaddc09a952
Robin Stocker <robin@nibor.org>
2012-05-29 19:16:23 +0200

    Extract methods for showing and calculating hover
    Small refactoring in preparation for fixing bug 380815.
    
    Change-Id: I030923ed337b7d0e8fd8a93e80830f68aa27cb8e
    Signed-off-by: Robin Stocker <robin@nibor.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitGraphTable.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitGraphTable.java
@@ -227,30 +227,35 @@ class CommitGraphTable {
 
 					int relativeX = e.x - item.getBounds().x;
 					for (int i = 0; i < commit.getRefCount(); i++) {
-						Point textSpan = renderer.getRefHSpan(commit.getRef(i));
+						Ref ref = commit.getRef(i);
+						Point textSpan = renderer.getRefHSpan(ref);
 						if ((textSpan != null)
-								&& (relativeX >= textSpan.x && relativeX <= textSpan.y)) {
-							hoverShell = new Shell(getTableView().getTable()
-									.getShell(), SWT.ON_TOP | SWT.NO_FOCUS
-									| SWT.TOOL);
-							hoverShell.setLayout(new FillLayout());
-							Point tableLocation = getTableView().getTable()
-									.toControl(0, 0);
-							hoverShell.setLocation(
-									-tableLocation.x + e.x,
-									-tableLocation.y + e.y
-											- renderer.getTextHeight());
-							Label label = new Label(hoverShell, SWT.NONE);
-							label.setText(getHooverText(commit.getRef(i)));
-							label.setBackground(infoBackgroundColor);
-							hoverShell.pack();
-							hoverShell.setVisible(true);
-						}
+								&& (relativeX >= textSpan.x && relativeX <= textSpan.y))
+							showHover(ref, e);
 					}
 				}
 			}
 
-			private String getHooverText(Ref r) {
+			private void showHover(Ref ref, MouseEvent e) {
+				hoverShell = new Shell(getTableView().getTable()
+						.getShell(), SWT.ON_TOP | SWT.NO_FOCUS
+						| SWT.TOOL);
+				hoverShell.setLayout(new FillLayout());
+				hoverShell.setLocation(getHoverLocation(e));
+				Label label = new Label(hoverShell, SWT.NONE);
+				label.setText(getHoverText(ref));
+				label.setBackground(infoBackgroundColor);
+				hoverShell.pack();
+				hoverShell.setVisible(true);
+			}
+
+			private Point getHoverLocation(MouseEvent e) {
+				Point tableLocation = getTableView().getTable().toControl(0, 0);
+				return new Point(-tableLocation.x + e.x,
+						-tableLocation.y + e.y - renderer.getTextHeight());
+			}
+
+			private String getHoverText(Ref r) {
 				String name = r.getName();
 				if (r.isSymbolic())
 					name += ": " + r.getLeaf().getName(); //$NON-NLS-1$
