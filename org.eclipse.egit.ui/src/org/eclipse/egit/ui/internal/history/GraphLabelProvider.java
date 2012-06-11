910ecee58cb814632e6e100ceba5e2bdf4f11208
Mathias Kinzler <mathias.kinzler@sap.com>
2012-06-11 16:47:48 +0200

    Add a "Previous Version" option to the Replace With menu
    If the current commit is a merge commit, this will open a dialog asking
    the user to select one of the parents.
    
    This dialog is also added to the "Compare with Previous" action handler.
    
    Change-Id: I8a5076ba29ebae7da28f837ed1217b89afe1474d
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GraphLabelProvider.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GraphLabelProvider.java
@@ -3,6 +3,7 @@
  * Copyright (C) 2008, Shawn O. Pearce <spearce@spearce.org>
  * Copyright (C) 2011, Matthias Sohn <matthias.sohn@sap.com>
  * Copyright (C) 2011, IBM Corporation
+ * Copyright (C) 2012, Mathias Kinzler <mathias.kinzler@sap.com>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -14,32 +15,16 @@ package org.eclipse.egit.ui.internal.history;
 import java.io.IOException;
 
 import org.eclipse.egit.ui.Activator;
-import org.eclipse.jface.viewers.BaseLabelProvider;
+import org.eclipse.egit.ui.internal.dialogs.CommitLabelProvider;
 import org.eclipse.jface.viewers.ITableLabelProvider;
-import org.eclipse.jgit.lib.PersonIdent;
-import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.util.GitDateFormatter;
-import org.eclipse.jgit.util.GitDateFormatter.Format;
-import org.eclipse.swt.graphics.Image;
 
-class GraphLabelProvider extends BaseLabelProvider implements
+/**
+ * A Label Provider for Commits
+ */
+class GraphLabelProvider extends CommitLabelProvider implements
 		ITableLabelProvider {
-	private GitDateFormatter dateFormatter;
-
-	private boolean showEmail;
-
-	private RevCommit lastCommit;
-
-	private PersonIdent lastAuthor;
-
-	private PersonIdent lastCommitter;
-
-	private Format format = Format.LOCALE;
-
-	GraphLabelProvider() {
-	}
-
-	public String getColumnText(final Object element, final int columnIndex) {
+	@Override
+	public String getColumnText(Object element, int columnIndex) {
 		final SWTCommit c = (SWTCommit) element;
 		try {
 			c.parseBody();
@@ -47,85 +32,6 @@ class GraphLabelProvider extends BaseLabelProvider implements
 			Activator.error("Error parsing body", e); //$NON-NLS-1$
 			return ""; //$NON-NLS-1$
 		}
-		if (columnIndex == 0)
-			return c.getId().abbreviate(7).name();
-		if (columnIndex == 1)
-			return c.getShortMessage();
-		if (columnIndex == 2 || columnIndex == 3) {
-			final PersonIdent author = authorOf(c);
-			if (author != null)
-				switch (columnIndex) {
-				case 2:
-					if (showEmail)
-						return author.getName()
-								+ " <" + author.getEmailAddress() + '>'; //$NON-NLS-1$
-					else
-						return author.getName();
-				case 3:
-					return getDateFormatter().formatDate(author);
-				}
-		}
-		if (columnIndex == 4 || columnIndex == 5) {
-			final PersonIdent committer = committerOf(c);
-			if (committer != null)
-				switch (columnIndex) {
-				case 4:
-					if (showEmail)
-						return committer.getName()
-								+ " <" + committer.getEmailAddress() + '>'; //$NON-NLS-1$
-					else
-						return committer.getName();
-				case 5:
-					return getDateFormatter().formatDate(committer);
-				}
-		}
-
-		return ""; //$NON-NLS-1$
-	}
-
-	private GitDateFormatter getDateFormatter() {
-		if (dateFormatter == null)
-			dateFormatter = new GitDateFormatter(format);
-		return dateFormatter;
-	}
-
-	private PersonIdent authorOf(final RevCommit c) {
-		if (lastCommit != c) {
-			lastCommit = c;
-			lastAuthor = c.getAuthorIdent();
-			lastCommitter = c.getCommitterIdent();
-		}
-		return lastAuthor;
-	}
-
-	private PersonIdent committerOf(final RevCommit c) {
-		if (lastCommit != c) {
-			lastCommit = c;
-			lastAuthor = c.getAuthorIdent();
-			lastCommitter = c.getCommitterIdent();
-		}
-		return lastCommitter;
-	}
-
-	public Image getColumnImage(final Object element, final int columnIndex) {
-		return null;
-	}
-
-	/**
-	 * @param relative {@code true} if the date column should show relative dates
-	 */
-	public void setRelativeDate(boolean relative) {
-		dateFormatter = null;
-		if (relative)
-			format = Format.RELATIVE;
-		else
-			format = Format.LOCALE;
-	}
-
-	/**
-	 * @param showEmail true to show e-mail addresses, false otherwise
-	 */
-	public void setShowEmailAddresses(boolean showEmail) {
-		this.showEmail = showEmail;
+		return super.getColumnText(c, columnIndex);
 	}
 }
