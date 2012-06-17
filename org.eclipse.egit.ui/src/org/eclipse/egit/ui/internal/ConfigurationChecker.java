e33c12d62d587b963c8758a380ab1396a0467b05
Robin Rosenberg <robin.rosenberg@dewire.com>
2012-06-18 01:05:46 +0200

    Handles missing HOME on Unix-like systems
    Admittedly this is am exotic case, but help testing the code when not on
    windows.
    
    Change-Id: I44029a9e35cb2dad62b1ef365e17ee5a562ca1c3

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/ConfigurationChecker.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/ConfigurationChecker.java
@@ -81,12 +81,17 @@ public class ConfigurationChecker {
 	}
 
 	private static String calcHomeDir() {
-		String homeDrive = System.getenv("HOMEDRIVE"); //$NON-NLS-1$
-		if (homeDrive != null) {
-			String homePath = SystemReader.getInstance().getenv("HOMEPATH"); //$NON-NLS-1$
-			return new File(homeDrive, homePath).getAbsolutePath();
+		if (runsOnWindows()) {
+			String homeDrive = System.getenv("HOMEDRIVE"); //$NON-NLS-1$
+			if (homeDrive != null) {
+				String homePath = SystemReader.getInstance().getenv("HOMEPATH"); //$NON-NLS-1$
+				return new File(homeDrive, homePath).getAbsolutePath();
+			}
+			return System.getenv("HOMESHARE"); //$NON-NLS-1$
+		} else {
+			// The user.home property is not compatible with Git for Windows
+			return System.getProperty("user.home"); //$NON-NLS-1$
 		}
-		return System.getenv("HOMESHARE"); //$NON-NLS-1$
 	}
 
 	private static boolean runsOnWindows() {
