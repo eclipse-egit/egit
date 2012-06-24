c7ef19812979d807f9e12ea6ec9703f5dc87c009
Robin Stocker <robin@nibor.org>
2012-06-24 21:18:25 +0200

    [historyView] Use resource encoding for compare editor nodes
    Otherwise the default encoding is used to decode the contents, which may
    be different from the resource encoding. That then would result in the
    file being displayed incorrectly.
    
    See bug 345160 for similar fixes.
    
    Change-Id: I3a4e16cce44b87b9276b4e28a75609a1c4eca233
    Signed-off-by: Robin Stocker <robin@nibor.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/merge/GitCompareEditorInput.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/merge/GitCompareEditorInput.java
@@ -29,6 +29,7 @@ import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.egit.core.AdaptableFileTreeIterator;
+import org.eclipse.egit.core.internal.CompareCoreUtils;
 import org.eclipse.egit.core.internal.storage.GitFileRevision;
 import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.egit.ui.Activator;
@@ -276,58 +277,65 @@ public class GitCompareEditorInput extends CompareEditorInput {
 								.isEntryIgnored())
 					continue;
 
+
 				if (compareVersionIterator != null
 						&& baseVersionIterator != null) {
-					// content exists on both sides
 					boolean equalContent = compareVersionIterator
 							.getEntryObjectId().equals(
 									baseVersionIterator.getEntryObjectId());
 					if (equalContent)
 						continue;
-					monitor.setTaskName(baseVersionIterator
-							.getEntryPathString());
-					GitFileRevision baseRev = GitFileRevision.inCommit(
-							repository, baseCommit, baseVersionIterator
-									.getEntryPathString(), tw
-									.getObjectId(baseTreeIndex));
-					GitFileRevision compareRev;
+				}
+
+				String encoding = null;
+
+				GitFileRevision compareRev = null;
+				if (compareVersionIterator != null) {
+					String entryPath = compareVersionIterator.getEntryPathString();
+					encoding = CompareCoreUtils.getResourceEncoding(repository, entryPath);
 					if (!useIndex)
 						compareRev = GitFileRevision.inCommit(repository,
-								compareCommit, compareVersionIterator
-										.getEntryPathString(), tw
-										.getObjectId(compareTreeIndex));
+								compareCommit, entryPath,
+								tw.getObjectId(compareTreeIndex));
 					else
 						compareRev = GitFileRevision.inIndex(repository,
-								compareVersionIterator.getEntryPathString());
+								entryPath);
+				}
 
+				GitFileRevision baseRev = null;
+				if (baseVersionIterator != null) {
+					String entryPath = baseVersionIterator.getEntryPathString();
+					if (encoding == null) {
+						encoding = CompareCoreUtils.getResourceEncoding(repository, entryPath);
+					}
+					baseRev = GitFileRevision.inCommit(repository, baseCommit,
+							entryPath, tw.getObjectId(baseTreeIndex));
+				}
+
+				if (compareVersionIterator != null
+						&& baseVersionIterator != null) {
+					monitor.setTaskName(baseVersionIterator
+							.getEntryPathString());
+					// content exists on both sides
 					add(result, baseVersionIterator.getEntryPathString(),
-							new DiffNode(new FileRevisionTypedElement(compareRev),
-									new FileRevisionTypedElement(baseRev)));
-
+							new DiffNode(new FileRevisionTypedElement(compareRev, encoding),
+									new FileRevisionTypedElement(baseRev, encoding)));
 				} else if (baseVersionIterator != null
 						&& compareVersionIterator == null) {
 					monitor.setTaskName(baseVersionIterator
 							.getEntryPathString());
 					// only on base side
-					GitFileRevision baseRev = GitFileRevision.inCommit(
-							repository, baseCommit, baseVersionIterator
-									.getEntryPathString(), tw
-									.getObjectId(baseTreeIndex));
 					add(result, baseVersionIterator.getEntryPathString(),
 							new DiffNode(Differencer.DELETION | Differencer.RIGHT, null, null,
-									new FileRevisionTypedElement(baseRev)));
+									new FileRevisionTypedElement(baseRev, encoding)));
 				} else if (compareVersionIterator != null
 						&& baseVersionIterator == null) {
 					monitor.setTaskName(compareVersionIterator
 							.getEntryPathString());
 					// only on compare side
-					GitFileRevision compareRev = GitFileRevision.inCommit(
-							repository, compareCommit, compareVersionIterator
-									.getEntryPathString(), tw
-									.getObjectId(compareTreeIndex));
 					add(result, compareVersionIterator.getEntryPathString(),
 							new DiffNode(Differencer.ADDITION | Differencer.RIGHT, null,
-									new FileRevisionTypedElement(compareRev), null));
+									new FileRevisionTypedElement(compareRev, encoding), null));
 				}
 
 				if (monitor.isCanceled())
