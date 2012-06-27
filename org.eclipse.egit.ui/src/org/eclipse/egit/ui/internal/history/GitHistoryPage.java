57804a756a417936c82a2efa018d330f4ea40c63
Mathias Kinzler <mathias.kinzler@sap.com>
2012-06-27 13:40:26 +0200

    Cleanup and repair of History View Show menu
    Add "Tag Sequence" action and adjust commit viewer to react on this
    Move "Wrap Comments", "Fill Paragraph", and "Tag Sequence" to a sub-menu
    "In Revision Comment"
    Revive the context menu in "Revision Comment" area (was broken but is
    still described in the user guide)
    Remove some redundant "Show"s from label texts
    Adjust the Git History Preference page to show the preferences in a
    similar sequence as the menu
    Use Groups in the preference page to better visualize the scope of the
    preferences
    
    Change-Id: Ib107293658cb74469a51ccc9bae8ae12ed40b8a0

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitHistoryPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitHistoryPage.java
@@ -219,6 +219,8 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 
 		BooleanPrefAction showNotesAction;
 
+		BooleanPrefAction showTagSequenceAction;
+
 		BooleanPrefAction wrapCommentAction;
 
 		BooleanPrefAction fillCommentAction;
@@ -270,6 +272,7 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 			createShowRelativeDateAction();
 			createShowEmailAddressesAction();
 			createShowNotesAction();
+			createShowTagSequenceAction();
 			createWrapCommentAction();
 			createFillCommentAction();
 			createFollowRenamesAction();
@@ -460,6 +463,18 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 			};
 			showNotesAction.apply(showNotesAction.isChecked());
 			actionsToDispose.add(showNotesAction);
+		}
+
+		private void createShowTagSequenceAction() {
+			showTagSequenceAction = new BooleanPrefAction(
+					UIPreferences.HISTORY_SHOW_TAG_SEQUENCE,
+					UIText.ResourceHistory_ShowTagSequence) {
+				void apply(boolean value) {
+					// nothing, just set the Preference
+				}
+			};
+			showTagSequenceAction.apply(showTagSequenceAction.isChecked());
+			actionsToDispose.add(showTagSequenceAction);
 		}
 
 		private void createWrapCommentAction() {
@@ -936,11 +951,18 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 		showSubMenuMgr.add(actions.followRenamesAction);
 		showSubMenuMgr.add(new Separator());
 		showSubMenuMgr.add(actions.findAction);
-		showSubMenuMgr.add(actions.showFilesAction);
 		showSubMenuMgr.add(actions.showCommentAction);
+		showSubMenuMgr.add(actions.showFilesAction);
 		showSubMenuMgr.add(new Separator());
 		showSubMenuMgr.add(actions.showRelativeDateAction);
 		showSubMenuMgr.add(actions.showEmailAddressesAction);
+
+		IMenuManager showInMessageManager = new MenuManager(
+				UIText.GitHistoryPage_InRevisionCommentSubMenuLabel);
+		showSubMenuMgr.add(showInMessageManager);
+		showInMessageManager.add(actions.showTagSequenceAction);
+		showInMessageManager.add(actions.wrapCommentAction);
+		showInMessageManager.add(actions.fillCommentAction);
 
 		IMenuManager filterSubMenuMgr = new MenuManager(
 				UIText.GitHistoryPage_FilterSubMenuLabel);
@@ -953,10 +975,6 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 		viewMenuMgr.add(new Separator());
 		viewMenuMgr.add(actions.compareModeAction);
 		viewMenuMgr.add(actions.reuseCompareEditorAction);
-
-		viewMenuMgr.add(new Separator());
-		viewMenuMgr.add(actions.wrapCommentAction);
-		viewMenuMgr.add(actions.fillCommentAction);
 	}
 
 	public void dispose() {
