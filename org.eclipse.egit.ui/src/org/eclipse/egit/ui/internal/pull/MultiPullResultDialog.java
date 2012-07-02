5d876b36228cd91a97a4a86c1de3c7ce29dcf81e
Markus Keller <markus_keller@ch.ibm.com>
2012-07-02 19:45:33 +0200

    Open multiple detail dialogs from MultiPullResultDialog at once
    This change enables multi-select in the MultiPullResultDialog and
    changes the Details button to work with multi-selections. Together with
    <https://git.eclipse.org/r/#/c/6291/>, this offers a better workflow for
    users who want to review incoming changes from multiple repositories.
    
    Change-Id: If69fc53801db0742bec4bd50302276567fd6b51a
    Signed-off-by: Markus Keller <markus_keller@ch.ibm.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/pull/MultiPullResultDialog.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/pull/MultiPullResultDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 SAP AG.
+ * Copyright (c) 2011, 2012 SAP AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,10 +7,12 @@
  *
  * Contributors:
  *    Mathias Kinzler (SAP AG) - initial implementation
+ *    Markus Keller <markus_keller@ch.ibm.com> - Open multiple detail dialogs from MultiPullResultDialog at once
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.pull;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -37,6 +39,8 @@ import org.eclipse.jgit.lib.Repository;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
@@ -168,19 +172,17 @@ public class MultiPullResultDialog extends Dialog {
 		Composite main = new Composite(parent, SWT.NONE);
 		GridLayoutFactory.fillDefaults().applyTo(main);
 		GridDataFactory.fillDefaults().grab(true, true).applyTo(main);
-		tv = new TableViewer(main, SWT.SINGLE | SWT.FULL_SELECTION | SWT.BORDER);
+		tv = new TableViewer(main, SWT.MULTI | SWT.FULL_SELECTION | SWT.BORDER);
 		tv.setContentProvider(ArrayContentProvider.getInstance());
 
 		tv.addSelectionChangedListener(new ISelectionChangedListener() {
-			@SuppressWarnings("unchecked")
 			public void selectionChanged(SelectionChangedEvent event) {
 				IStructuredSelection sel = (IStructuredSelection) event
 						.getSelection();
-				boolean enabled = !sel.isEmpty();
-				if (enabled) {
-					Entry<Repository, Object> entry = (Entry<Repository, Object>) sel
-							.getFirstElement();
-					enabled = entry.getValue() instanceof PullResult;
+				boolean enabled = false;
+				for (Entry<Repository, Object> entry : (List<Entry<Repository, Object>>) sel
+						.toList()) {
+					enabled |= entry.getValue() instanceof PullResult;
 				}
 				getButton(DETAIL_BUTTON).setEnabled(enabled);
 			}
@@ -225,17 +227,27 @@ public class MultiPullResultDialog extends Dialog {
 				true);
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	protected void buttonPressed(int buttonId) {
 		if (buttonId == DETAIL_BUTTON) {
 			IStructuredSelection sel = (IStructuredSelection) tv.getSelection();
-			Entry<Repository, Object> item = (Entry<Repository, Object>) sel
-					.getFirstElement();
-			if (item.getValue() instanceof PullResult) {
-				PullResultDialog dialog = new PullResultDialog(getShell(), item
-						.getKey(), (PullResult) item.getValue());
-				dialog.open();
+			Point location = null;
+			Rectangle clientArea = getShell().getMonitor().getClientArea();
+			int yMin = clientArea.y + clientArea.height;
+			for (Entry<Repository, Object> item : (List<Entry<Repository, Object>>) sel
+					.toList()) {
+				if (item.getValue() instanceof PullResult) {
+					PullResultDialog dialog = new PullResultDialog(getShell(),
+							item.getKey(), (PullResult) item.getValue());
+					dialog.open();
+					Shell shell = dialog.getShell();
+					if (location != null) {
+						int delta = shell.getFont().getFontData()[0].getHeight() * 3;
+						int y = Math.min(location.y + delta, yMin - shell.getClientArea().height);
+						shell.setLocation(location.x, y);
+					}
+					location = shell.getLocation();
+				}
 			}
 		}
 		super.buttonPressed(buttonId);
