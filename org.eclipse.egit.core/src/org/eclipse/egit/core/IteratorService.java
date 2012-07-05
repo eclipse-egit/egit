7a8ccd1368c45c78e0cbae9182704cd265821d2d
Markus Duft <markus.duft@salomon.at>
2012-07-05 09:38:25 +0200

    Use fast ProjectUtil.findContainer:w instead of slow Eclipse call
    The IWorkspaceRoot.findContainersForLocationURI call is dead slow, so
    rather use code that calculates containers based on patch comparison,
    which is way faster.
    
    Change-Id: I8cd4dbb2240fa83d752e390604ffa235431c53e1

--- org.eclipse.egit.core/src/org/eclipse/egit/core/IteratorService.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/IteratorService.java
@@ -13,6 +13,7 @@ import java.io.File;
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.egit.core.internal.util.ProjectUtil;
 import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.treewalk.WorkingTreeIterator;
@@ -44,9 +45,29 @@ public class IteratorService {
 			return null;
 		}
 		IContainer container = findContainer(root, repository.getWorkTree());
+		// IContainer container = findContainer(repository.getWorkTree());
 		if (container != null)
 			return new ContainerTreeIterator(repository, container);
-		return new AdaptableFileTreeIterator(repository, root);
+		return new AdaptableFileTreeIterator(repository, ResourcesPlugin.getWorkspace().getRoot());
+	}
+
+	/**
+	 * The method searches a container resource related to the given file. The
+	 * container must reside in a project that is shared with Git
+	 *
+	 * @param file
+	 * @return a container that matches the description above or null if such a
+	 *         container does not exist
+	 * @since 2.3
+	 */
+	public static IContainer findContainer(File file) {
+		if (!file.isDirectory())
+			throw new IllegalArgumentException(
+					"file " + file.getAbsolutePath() + " is no directory"); //$NON-NLS-1$//$NON-NLS-2$
+		final IContainer container = ProjectUtil.findContainer(file);
+		if (container.isAccessible() && isProjectSharedWithGit(container))
+			return container;
+		return null;
 	}
 
 	/**
@@ -58,6 +79,7 @@ public class IteratorService {
 	 * @param file
 	 * @return a container that matches the description above or null if such a
 	 *         container does not exist
+	 * @deprecated Use {@link #findContainer(File)} instead
 	 */
 	public static IContainer findContainer(IWorkspaceRoot root, File file) {
 		if (!file.isDirectory())
