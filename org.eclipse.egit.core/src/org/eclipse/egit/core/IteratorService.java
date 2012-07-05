566ff6fc869bc016de15b0dfc306eebee4cd4c0d
Markus Duft <markus.duft@salomon.at>
2012-07-05 09:38:25 +0200

    Use ProjectUtil.findContainerFast instead of slow Eclipse call
    The IWorkspaceRoot.findContainersForLocationURI call is dead slow, so
    rather use code that calculates containers based on patch comparison,
    which is way faster.
    
    Change-Id: I8cd4dbb2240fa83d752e390604ffa235431c53e1

--- org.eclipse.egit.core/src/org/eclipse/egit/core/IteratorService.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/IteratorService.java
@@ -11,8 +11,8 @@ package org.eclipse.egit.core;
 import java.io.File;
 
 import org.eclipse.core.resources.IContainer;
-import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.egit.core.internal.util.ProjectUtil;
 import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.treewalk.WorkingTreeIterator;
@@ -35,32 +35,27 @@ public class IteratorService {
 	 */
 	public static WorkingTreeIterator createInitialIterator(
 			Repository repository) {
-		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		IContainer container = findContainer(root, repository.getWorkTree());
+		IContainer container = findContainer(repository.getWorkTree());
 		if (container != null)
 			return new ContainerTreeIterator(repository, container);
-		return new AdaptableFileTreeIterator(repository, root);
+		return new AdaptableFileTreeIterator(repository, ResourcesPlugin.getWorkspace().getRoot());
 	}
 
 	/**
 	 * The method searches a container resource related to the given file. The
 	 * container must reside in a project that is shared with Git
 	 *
-	 * @param root
-	 *            the workspace root
 	 * @param file
 	 * @return a container that matches the description above or null if such a
 	 *         container does not exist
 	 */
-	public static IContainer findContainer(IWorkspaceRoot root, File file) {
+	public static IContainer findContainer(File file) {
 		if (!file.isDirectory())
 			throw new IllegalArgumentException(
 					"file " + file.getAbsolutePath() + " is no directory"); //$NON-NLS-1$//$NON-NLS-2$
-		final IContainer[] containers = root.findContainersForLocationURI(file
-				.toURI());
-		for (IContainer container : containers)
-			if (container.isAccessible() && isProjectSharedWithGit(container))
-				return container;
+		final IContainer container = ProjectUtil.findContainerFast(file);
+		if (container.isAccessible() && isProjectSharedWithGit(container))
+			return container;
 		return null;
 	}
 
