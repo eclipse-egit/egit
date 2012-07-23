5edf354e5de9413705d2b084564c420679684ab4
Robin Stocker <robin@nibor.org>
2012-07-23 17:34:23 +0200

    Commit should only prompt if unsaved files are from repo
    Before, it would always prompt, even if the unsaved files belonged to
    another repository.
    
    Bug: 376700
    Change-Id: I8a29e5121524f9610a3c05d613780d864f0aa0a6
    Signed-off-by: Robin Stocker <robin@nibor.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commit/CommitUI.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commit/CommitUI.java
@@ -57,6 +57,7 @@ import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
 
 /**
  * UI component for performing a commit
@@ -113,16 +114,17 @@ public class CommitUI  {
 	 * Performs a commit
 	 */
 	public void commit() {
+		final IProject[] projects = getProjectsOfRepositories();
+
 		// let's see if there is any dirty editor around and
 		// ask the user if they want to save or abort
-		if (!PlatformUI.getWorkbench().saveAllEditors(true)) {
+		if (!IDE.saveAllEditors(projects, true)) {
 			return;
 		}
 
 		BasicConfigurationDialog.show(new Repository[]{repo});
 
 		resetState();
-		final IProject[] projects = getProjectsOfRepositories();
 		try {
 			PlatformUI.getWorkbench().getProgressService().busyCursorWhile(new IRunnableWithProgress() {
 
