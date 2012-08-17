a26c3266f463d8cdbf6d96b69a8fa1ab6754b674
Robin Stocker <robin@nibor.org>
2012-08-17 21:44:05 +0200

    [historyView] Rebase: Pass a better Ref as upstream
    JGit change I1333a8dd170bb0077f491962013485efb6f2a926 is about improving
    the ours/theirs conflict markers during rebase. The way the rebase
    action worked before, it would always pass the commit ID to rebase,
    which is not very helpful during a rebase conflict.
    
    This change makes it use a Ref if there is one. If there are multiple
    refs pointing to the selected commit:
    
    - and if the branch being rebased tracks another one, this tracked
      branch should be the one to pick if it's in the list
    - otherwise pick a remote tracking branch if it's in the list
    
    Also adds a "does it work at all?" test for rebase in History view.
    
    Bug: 336819
    Change-Id: Ib78c3e8965e2f57e2c844a065629511ab1158160
    Signed-off-by: Robin Stocker <robin@nibor.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/RebaseCurrentHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/RebaseCurrentHandler.java
@@ -13,14 +13,19 @@
 
 package org.eclipse.egit.ui.internal.history.command;
 
+import java.io.IOException;
+
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.internal.rebase.RebaseHelper;
+import org.eclipse.jgit.lib.BranchConfig;
+import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectIdRef;
+import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.Ref.Storage;
-import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revplot.PlotCommit;
 import org.eclipse.osgi.util.NLS;
 
 /**
@@ -30,17 +35,52 @@ public class RebaseCurrentHandler extends AbstractHistoryCommandHandler {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 
-		RevCommit commit = (RevCommit) getSelection(getPage()).getFirstElement();
+		PlotCommit commit = (PlotCommit) getSelection(getPage()).getFirstElement();
 		final Repository repository = getRepository(event);
 		if (repository == null)
 			return null;
 
+		String currentBranch = getCurrentBranch(repository);
+		Ref ref = getRef(commit, repository, currentBranch);
+
 		String jobname = NLS.bind(
-				UIText.RebaseCurrentRefCommand_RebasingCurrentJobName, commit);
-		RebaseHelper.runRebaseJob(repository, jobname,
-				new ObjectIdRef.Unpeeled(Storage.LOOSE, commit.getName(),
-						commit));
+				UIText.RebaseCurrentRefCommand_RebasingCurrentJobName, currentBranch);
+		RebaseHelper.runRebaseJob(repository, jobname, ref);
 		return null;
 	}
 
+	private Ref getRef(PlotCommit commit, Repository repository, String currentBranch) {
+		int count = commit.getRefCount();
+		if (count == 0)
+			return new ObjectIdRef.Unpeeled(Storage.LOOSE, commit.getName(), commit);
+		else if (count == 1)
+			return commit.getRef(0);
+		else {
+			BranchConfig branchConfig = new BranchConfig(repository.getConfig(), currentBranch);
+			String trackingBranch = branchConfig.getTrackingBranch();
+			Ref remoteRef = null;
+
+			for (int i = 0; i < count; i++) {
+				Ref ref = commit.getRef(i);
+				if (trackingBranch != null && trackingBranch.equals(ref.getName()))
+					return ref;
+				if (ref.getName().startsWith(Constants.R_REMOTES))
+					remoteRef = ref;
+			}
+
+			if (remoteRef != null)
+				return remoteRef;
+			else
+				// We tried to pick a nice ref, just pick the first then
+				return commit.getRef(0);
+		}
+	}
+
+	private String getCurrentBranch(Repository repository) throws ExecutionException {
+		try {
+			return repository.getBranch();
+		} catch (IOException e) {
+			throw new ExecutionException(UIText.RebaseCurrentRefCommand_ErrorGettingCurrentBranchMessage, e);
+		}
+	}
 }
