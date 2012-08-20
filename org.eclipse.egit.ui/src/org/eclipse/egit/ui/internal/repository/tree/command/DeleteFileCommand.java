9e29c1cf7e9f85a52945d946da6796d0dd0838b8
Robin Stocker <robin@nibor.org>
2012-08-20 15:33:39 +0200

    [repoView] Add Delete command for files and folders
    Works on workspace and non-workspace resources.
    
    For sharing the code with the Staging View, extracted the UI logic into
    a new DeletePathsOperationUI class. This dispatches either to the
    standard action from org.eclipse.ui or to DeletePathsOperation.
    
    DeleteResourcesOperation was renamed to DeletePathsOperation to make it
    clear that it operates on paths, not on potentially non-existing
    resources.
    
    Undo is also supported if all selected elements are resources.
    
    Bug: 385001
    Change-Id: I0d113cfa0fdc2c58ec33fa1c7ffd34b74e6c11a9
    Signed-off-by: Robin Stocker <robin@nibor.org>
    Also-by: Stefan Lay <stefan.lay@sap.com>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/DeleteFileCommand.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (C) 2012, Robin Stocker <robin@nibor.org>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.repository.tree.command;
+
+import java.util.Collection;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.egit.ui.internal.operations.DeletePathsOperationUI;
+import org.eclipse.egit.ui.internal.repository.RepositoriesView;
+import org.eclipse.egit.ui.internal.repository.tree.FileNode;
+
+/**
+ * Delete a working dir file or folder from the repositories view.
+ */
+public class DeleteFileCommand extends RepositoriesViewCommandHandler<FileNode> {
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		Collection<IPath> paths = getSelectedFileAndFolderPaths(event);
+
+		RepositoriesView view = getView(event);
+		DeletePathsOperationUI operation = new DeletePathsOperationUI(paths, view.getSite());
+		operation.run();
+
+		view.refresh();
+		return null;
+	}
+
+	@Override
+	public void setEnabled(Object evaluationContext) {
+		enableWorkingDirCommand(evaluationContext);
+	}
+}
