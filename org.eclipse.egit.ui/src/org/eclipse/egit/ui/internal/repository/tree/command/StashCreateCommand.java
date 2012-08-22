ed119527f1d30405bdf8d891206a49c0f4933f35
Stefan Lay <stefan.lay@sap.com>
2012-08-22 22:44:39 +0200

    Continue checkout after removal of uncommitted changes
    When a checkout conflict occurs a dialog is presented to the user. From
    here different actions can be triggered to remove the uncommitted
    changes: commit, stash and reset.
    
    With this commit the checkout is triggered again after one of the
    operations above have been performed successfully.
    
    Bug: 343072
    Change-Id: Ic64052246e39b9560b3d77272a6b21b71ed73b3b
    Signed-off-by: Stefan Lay <stefan.lay@sap.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/StashCreateCommand.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/StashCreateCommand.java
@@ -14,21 +14,9 @@ import java.util.List;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.egit.core.op.StashCreateOperation;
-import org.eclipse.egit.ui.Activator;
-import org.eclipse.egit.ui.JobFamilies;
-import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryNode;
-import org.eclipse.jface.dialogs.InputDialog;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.window.Window;
+import org.eclipse.egit.ui.internal.stash.StashCreateUI;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.handlers.HandlerUtil;
 
@@ -52,55 +40,10 @@ public class StashCreateCommand extends
 			return null;
 
 		final Shell shell = HandlerUtil.getActiveShell(event);
-		InputDialog commitMessageDialog = new InputDialog(shell,
-				UIText.StashCreateCommand_titleEnterCommitMessage,
-				UIText.StashCreateCommand_messageEnterCommitMessage,
-				null, null);
-		if (commitMessageDialog.open() != Window.OK)
-			return null;
-		String message = commitMessageDialog.getValue();
-		if (message.length() == 0)
-			message = null;
+		StashCreateUI stashCreateUI = new StashCreateUI(shell, repo);
+		stashCreateUI.createStash();
 
-		final StashCreateOperation op = new StashCreateOperation(repo, message);
-		Job job = new Job(UIText.StashCreateCommand_jobTitle) {
-			@Override
-			protected IStatus run(IProgressMonitor monitor) {
-				monitor.beginTask("", 1); //$NON-NLS-1$
-				try {
-					op.execute(monitor);
-					RevCommit commit = op.getCommit();
-					if (commit == null)
-						showNoChangesToStash(shell);
-
-				} catch (CoreException e) {
-					Activator
-							.logError(UIText.StashCreateCommand_stashFailed, e);
-				}
-				return Status.OK_STATUS;
-			}
-
-			@Override
-			public boolean belongsTo(Object family) {
-				if (JobFamilies.STASH.equals(family))
-					return true;
-				return super.belongsTo(family);
-			}
-		};
-		job.setUser(true);
-		job.setRule(op.getSchedulingRule());
-		job.schedule();
 		return null;
 	}
 
-	private void showNoChangesToStash(final Shell shell) {
-		shell.getDisplay().asyncExec(new Runnable() {
-
-			public void run() {
-				MessageDialog.openInformation(shell,
-						UIText.StashCreateCommand_titleNoChanges,
-						UIText.StashCreateCommand_messageNoChanges);
-			}
-		});
-	}
 }
