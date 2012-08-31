2bd0da8a1dea0d0d9e85faf78fd57af057335477
Christian Halstrick <christian.halstrick@sap.com>
2012-08-31 17:47:34 +0200

    Add a statistics page to repository properties
    When a user opens the properties of a repository (Git Repositories
    View-> right-click on a repository node -> Properties) a new page is
    added to the Component. This page "Statistics" contains info about the
    number and size of loose and packed objects. Also the number of loose
    an packed refs and the number of packfiles are listed.
    
    Change-Id: I61281be7db512ccdf5399c98a90b39000365dd71

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -3934,6 +3934,27 @@ public class UIText extends NLS {
 	public static String RenameBranchOnCommitHandler_SelectBranchDialogTitle;
 
 	/** */
+	public static String RepositoryStatistics_Description;
+
+	/** */
+	public static String RepositoryStatistics_LooseObjects;
+
+	/** */
+	public static String RepositoryStatistics_NrOfObjects;
+
+	/** */
+	public static String RepositoryStatistics_NrOfPackfiles;
+
+	/** */
+	public static String RepositoryStatistics_NrOfRefs;
+
+	/** */
+	public static String RepositoryStatistics_PackedObjects;
+
+	/** */
+	public static String RepositoryStatistics_SpaceNeededOnFilesystem;
+
+	/** */
 	public static String RevertFailureDialog_Message;
 
 	/** */
