41bedf43db6cfcd792079053bee66bdc2ad83a34
Christian Halstrick <christian.halstrick@sap.com>
2012-09-15 13:44:01 +0200

    Handle newly introduced ParseExceptions thrown by GC
    When the JGits garbage collector can't parse some configuration
    parameters he will throw a ParseException. These exceptions have to
    be catched and wrapped. This change needs JGit change
    Ib3c1aa64b65ed0e0270791a365f2fa72ab78a3f4.
    
    Change-Id: I6e8945debf665db68f86cff2910a5e627382182a

--- org.eclipse.egit.core/src/org/eclipse/egit/core/op/GarbageCollectOperation.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/op/GarbageCollectOperation.java
@@ -9,6 +9,7 @@
 package org.eclipse.egit.core.op;
 
 import java.io.IOException;
+import java.text.ParseException;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -46,6 +47,9 @@ public class GarbageCollectOperation implements IEGitOperation {
 		try {
 			gc.gc();
 		} catch (IOException e) {
+			throw new CoreException(new Status(IStatus.ERROR,
+					Activator.getPluginId(), e.getMessage(), e));
+		} catch (ParseException e) {
 			throw new CoreException(new Status(IStatus.ERROR,
 					Activator.getPluginId(), e.getMessage(), e));
 		}
