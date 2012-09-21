56e73cc3cc8b3c540bcf2352644b27bf86b4e74a
Matthias Sohn <matthias.sohn@sap.com>
2012-09-21 08:06:49 +0200

    Auto-ignore derived resources
    Automatically ignore derived resources located immediately under
    projects being shared using GitProvider.
    
    Bug: 297674
    Change-Id: I02d0e12e9bee94b1121a1fa57eedfc268dd8e5ef
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/op/ConnectProviderOperationTest.java
+++ org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/op/ConnectProviderOperationTest.java
@@ -20,12 +20,19 @@ import java.io.IOException;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.egit.core.op.ConnectProviderOperation;
+import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.egit.core.test.GitTestCase;
 import org.eclipse.egit.core.test.TestRepository;
 import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.FileMode;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepository;
+import org.eclipse.jgit.treewalk.FileTreeIterator;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.eclipse.jgit.treewalk.WorkingTreeIterator;
+import org.eclipse.jgit.treewalk.filter.PathFilter;
 import org.eclipse.team.core.RepositoryProvider;
 import org.junit.Test;
 
@@ -55,6 +62,41 @@ public class ConnectProviderOperationTest extends GitTestCase {
 		assertTrue(RepositoryProvider.isShared(project.getProject()));
 
 		assertTrue(gitDir.exists());
+	}
+
+	@Test
+	public void testAutoIgnoresDerivedFolder() throws CoreException,
+			IOException {
+		Repository repository = new FileRepository(gitDir);
+		repository.create();
+		repository.close();
+		project.setBinFolderDerived();
+		ConnectProviderOperation operation = new ConnectProviderOperation(
+				project.getProject(), gitDir);
+		operation.execute(null);
+
+		assertTrue(RepositoryProvider.isShared(project.getProject()));
+		IPath binPath = project.getProject().getLocation().append("bin");
+		assertTrue(isIgnored(binPath));
+		assertTrue(gitDir.exists());
+	}
+
+	private boolean isIgnored(IPath path) throws IOException {
+		RepositoryMapping mapping = RepositoryMapping.getMapping(path);
+		Repository repository = mapping.getRepository();
+		String repoRelativePath = mapping.getRepoRelativePath(path);
+		TreeWalk walk = new TreeWalk(repository);
+		walk.addTree(new FileTreeIterator(repository));
+		walk.setFilter(PathFilter.create(repoRelativePath));
+		while (walk.next()) {
+			WorkingTreeIterator workingTreeIterator = walk.getTree(0,
+					WorkingTreeIterator.class);
+			if (walk.getPathString().equals(repoRelativePath))
+				return workingTreeIterator.isEntryIgnored();
+			if (workingTreeIterator.getEntryFileMode().equals(FileMode.TREE))
+				walk.enterSubtree();
+		}
+		return false;
 	}
 
 	@Test
