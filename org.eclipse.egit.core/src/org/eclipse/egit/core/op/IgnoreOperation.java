478d7b263a8d4a1e26093ce66b40e26c8000f39d
Matthias Sohn <matthias.sohn@sap.com>
2012-09-21 08:06:49 +0200

    [DRAFT] Auto-ignore derived resources
    Automatically ignore derived resources. Register a resource change
    listener to detect resources marked as derived asynchronously.
    
    TODO: add preference to enable switching off auto-ignore
    
    Bug: 297674
    Change-Id: I02d0e12e9bee94b1121a1fa57eedfc268dd8e5ef
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/op/IgnoreOperation.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/op/IgnoreOperation.java
@@ -35,15 +35,11 @@ import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
 import org.eclipse.egit.core.Activator;
 import org.eclipse.egit.core.CoreText;
+import org.eclipse.egit.core.RepositoryUtil;
 import org.eclipse.egit.core.internal.job.RuleUtil;
 import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.FileMode;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.treewalk.FileTreeIterator;
-import org.eclipse.jgit.treewalk.TreeWalk;
-import org.eclipse.jgit.treewalk.WorkingTreeIterator;
-import org.eclipse.jgit.treewalk.filter.PathFilter;
 import org.eclipse.osgi.util.NLS;
 
 /**
@@ -96,7 +92,7 @@ public class IgnoreOperation implements IEGitOperation {
 				// NB This does the same thing in
 				// DecoratableResourceAdapter, but neither currently
 				// consult .gitignore
-				if (!isIgnored(path))
+				if (!RepositoryUtil.isIgnored(path))
 					addIgnore(monitor, path);
 				monitor.worked(1);
 			}
