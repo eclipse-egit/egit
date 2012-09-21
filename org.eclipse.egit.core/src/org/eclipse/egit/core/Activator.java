58a5ccff339fc9aa19488ce303e0879762df98ff
Matthias Sohn <matthias.sohn@sap.com>
2012-09-21 08:06:49 +0200

    [DRAFT] Auto-ignore derived resources
    Automatically ignore derived resources located immediately under
    projects being shared using GitProvider. Also register a resource change
    listener to detect derived resources generated at a later point in time.
    
    TODO: detection via resource change listener doesn't yet work
    
    Bug: 297674
    Change-Id: I02d0e12e9bee94b1121a1fa57eedfc268dd8e5ef
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/Activator.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/Activator.java
@@ -23,6 +23,7 @@ import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.IResourceDeltaVisitor;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Plugin;
@@ -57,6 +58,7 @@ public class Activator extends Plugin implements DebugOptionsListener {
 	private RepositoryUtil repositoryUtil;
 	private EGitSecureStore secureStore;
 	private AutoShareProjects shareGitProjectsJob;
+	private IgnoreDerivedResources ignoreDerivedResourcesListener;
 
 	/**
 	 * @return the singleton {@link Activator}
@@ -135,6 +137,7 @@ public class Activator extends Plugin implements DebugOptionsListener {
 		secureStore = new EGitSecureStore(SecurePreferencesFactory.getDefault());
 
 		registerAutoShareProjects();
+		registerAutoIgnoreDerivedResources();
 	}
 
 	public void optionsChanged(DebugOptions options) {
@@ -260,5 +263,55 @@ public class Activator extends Plugin implements DebugOptionsListener {
 				return;
 			}
 		}
+	}
+
+	private void registerAutoIgnoreDerivedResources() {
+		ignoreDerivedResourcesListener = new IgnoreDerivedResources();
+		ResourcesPlugin.getWorkspace().addResourceChangeListener(
+				ignoreDerivedResourcesListener,
+				IResourceChangeEvent.POST_CHANGE
+						| IResourceChangeEvent.POST_BUILD);
+	}
+
+	private static class IgnoreDerivedResources implements
+			IResourceChangeListener {
+		private static int INTERESTING_CHANGES = IResourceDelta.ADDED
+				| IResourceDelta.DERIVED_CHANGED;
+
+		protected boolean doAutoIgnoreDerived() {
+			return true;
+		}
+
+		public void resourceChanged(IResourceChangeEvent event) {
+			try {
+				IResourceDelta d = event.getDelta();
+				if (d == null)
+					return;
+				d.accept(new IResourceDeltaVisitor() {
+
+					public boolean visit(IResourceDelta delta)
+							throws CoreException {
+						if (!doAutoIgnoreDerived())
+							return false;
+						if ((delta.getFlags() & INTERESTING_CHANGES) == 0)
+							return true;
+						final IResource r = delta.getResource();
+						IPath p = r.getLocation();
+						if (p.toString().indexOf("bin") >= 0) //$NON-NLS-1$
+							System.out.println(p);
+						if (r.isDerived()) {
+							System.out.println(p);
+						}
+						return false;
+					}
+
+				});
+			} catch (CoreException e) {
+				Activator.logError(e.getMessage(), e);
+				return;
+			}
+
+		}
+
 	}
 }
