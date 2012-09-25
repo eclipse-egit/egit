998ce045f57752688c3c378714f11ed49ec8a06a
Robin Stocker <robin@nibor.org>
2012-09-25 23:22:28 +0200

    [historyView] Also fix patch contents created by dragging
    Same problem and solution as I2afa11033b26069795f57d2381e66973865cd7a9.
    
    Change-Id: I136edf1103142c7b9e523c9b8319642adccff8cd

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitGraphTable.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitGraphTable.java
@@ -67,6 +67,7 @@ import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revplot.PlotCommit;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevFlag;
+import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.util.FileUtils;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.SWT;
@@ -676,7 +677,15 @@ class CommitGraphTable {
 			IStructuredSelection selection = (IStructuredSelection) table
 					.getSelection();
 			RevCommit commit = (RevCommit) selection.getFirstElement();
-			return commit;
+			RevWalk walk = new org.eclipse.jgit.revwalk.RevWalk(input.getRepository());
+			try {
+				return walk.parseCommit(commit.getId());
+			} catch (IOException e) {
+				throw new RuntimeException(
+						"Could not parse commit " + commit.getId(), e); //$NON-NLS-1$
+			} finally {
+				walk.release();
+			}
 		}
 
 		private void writeToFile(final String fileName, String content)
