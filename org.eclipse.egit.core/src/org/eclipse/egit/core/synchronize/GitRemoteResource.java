14885af70e4315f275663226e61bda12e51f4e44
Robin Stocker <robin@nibor.org>
2012-10-30 16:25:30 +0100

    [sync] Show author of remote files in workspace model
    Bug: 359071
    Change-Id: Id39b5b26e72d31d49999892e0b61591c5b6757a1

--- org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/GitRemoteResource.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/GitRemoteResource.java
@@ -11,6 +11,7 @@ package org.eclipse.egit.core.synchronize;
 import static org.eclipse.jgit.lib.ObjectId.zeroId;
 
 import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.PersonIdent;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.team.core.variants.CachedResourceVariant;
 
@@ -22,10 +23,13 @@ abstract class GitRemoteResource extends CachedResourceVariant {
 
 	private final ObjectId objectId;
 
+	private final PersonIdent authorIdent;
+
 	GitRemoteResource(RevCommit commitId, ObjectId objectId, String path) {
 		this.path = path;
 		this.objectId = objectId;
 		this.commitId = commitId;
+		this.authorIdent = commitId.getAuthorIdent();
 	}
 
 	public String getName() {
@@ -34,7 +38,15 @@ abstract class GitRemoteResource extends CachedResourceVariant {
 	}
 
 	public String getContentIdentifier() {
-		return commitId.abbreviate(7).name() + "..."; //$NON-NLS-1$
+		StringBuilder s = new StringBuilder();
+		s.append(commitId.abbreviate(7).name());
+		s.append("..."); //$NON-NLS-1$
+		if (authorIdent != null) {
+			s.append(" ("); //$NON-NLS-1$
+			s.append(authorIdent.getName());
+			s.append(")"); //$NON-NLS-1$
+		}
+		return s.toString();
 	}
 
 	public byte[] asBytes() {
