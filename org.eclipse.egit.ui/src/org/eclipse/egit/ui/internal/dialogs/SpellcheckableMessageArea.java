db01bb996ce1c38b262402323dc3fd286daf49d4
Dani Megert <Daniel_Megert@ch.ibm.com>
2012-11-01 17:35:54 +0100

    Added missing copyright entry
    Change-Id: I5b08372af787229f90ffd99a3386ff7392452bea
    Signed-off-by: Dani Megert <Daniel_Megert@ch.ibm.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/SpellcheckableMessageArea.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/SpellcheckableMessageArea.java
@@ -2,6 +2,7 @@
  * Copyright (C) 2010, Benjamin Muskalla <bmuskalla@eclipsesource.com>
  * Copyright (C) 2011, Matthias Sohn <matthias.sohn@sap.com>
  * Copyright (C) 2011-2012, IBM Corporation
+ * Copyright (C) 2012, Robin Stocker <robin@nibor.org>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
