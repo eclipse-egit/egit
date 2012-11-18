acb7fee2faa7b181ded810c6e5dc5371b428c473
Tomasz Zarna <tzarna@gmail.com>
2012-11-18 22:40:04 +0100

    Support git --no-ff as an EGit preference
    Bug: 336933
    Change-Id: I22471ee4c8be0ad094976137aa01c263e17f841d

--- org.eclipse.egit.core/src/org/eclipse/egit/core/op/MergeOperation.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/op/MergeOperation.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * Copyright (c) 2010 SAP AG.
- * Copyright (C) 2012, Tomasz Zarna <Tomasz.Zarna@pl.ibm.com>
+ * Copyright (C) 2012, 2013 Tomasz Zarna <tzarna@gmail.com>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -31,11 +31,13 @@ import org.eclipse.egit.core.CoreText;
 import org.eclipse.egit.core.internal.util.ProjectUtil;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.MergeCommand;
+import org.eclipse.jgit.api.MergeCommand.FastForwardMode;
 import org.eclipse.jgit.api.MergeResult;
 import org.eclipse.jgit.api.errors.CheckoutConflictException;
 import org.eclipse.jgit.api.errors.ConcurrentRefUpdateException;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.NoHeadException;
+import org.eclipse.jgit.lib.ConfigConstants;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
@@ -107,11 +109,12 @@ public class MergeOperation implements IEGitOperation {
 				mymonitor.worked(1);
 				MergeCommand merge;
 				try {
+					FastForwardMode ffmode = getFFmode();
 					Ref ref = repository.getRef(refName);
 					if (ref != null)
-						merge = git.merge().include(ref);
+						merge = git.merge().include(ref).setFastForward(ffmode);
 					else
-						merge = git.merge().include(ObjectId.fromString(refName));
+						merge = git.merge().include(ObjectId.fromString(refName)).setFastForward(ffmode);
 				} catch (IOException e) {
 					throw new TeamException(CoreText.MergeOperation_InternalError, e);
 				}
@@ -151,6 +154,18 @@ public class MergeOperation implements IEGitOperation {
 		};
 		// lock workspace to protect working tree changes
 		ResourcesPlugin.getWorkspace().run(action, monitor);
+	}
+
+	private FastForwardMode getFFmode() throws IOException {
+		FastForwardMode ffmode = FastForwardMode.valueOf(repository.getConfig()
+				.getEnum(ConfigConstants.CONFIG_KEY_MERGE, null,
+						ConfigConstants.CONFIG_KEY_FF,
+						FastForwardMode.Merge.TRUE));
+		ffmode = FastForwardMode.valueOf(repository.getConfig().getEnum(
+				ConfigConstants.CONFIG_BRANCH_SECTION, repository.getBranch(),
+				ConfigConstants.CONFIG_KEY_MERGEOPTIONS,
+				FastForwardMode.MergeOptions.valueOf(ffmode)));
+		return ffmode;
 	}
 
 	/**
