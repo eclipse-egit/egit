dad43a5457e0c683d59b5c3a97b364cc893c003f
François Rey <eclipse.org@francois.rey.name>
2012-11-24 18:55:48 +0100

    Gracefully ignore linked resources
    A project may contain resources that are not located within the
    project's directory in the local file system. These resources are
    referred to as linked resources.
    This patch adds a few checks to gracefully ignore them in a few
    functional cases: iterator, handlers, actions, etc.
    
    Bug: 333338
    CQ: 6965
    Change-Id: I5005ed46cdfab47fc6913dbd46fc1291b2ac79ab
    Signed-off-by: François Rey <eclipse.org@francois.rey.name>
    Signed-off-by: Chris Aniszczyk <zx@twitter.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CompareWithRefActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CompareWithRefActionHandler.java
@@ -104,6 +104,6 @@ public class CompareWithRefActionHandler extends RepositoryActionHandler {
 
 	@Override
 	public boolean isEnabled() {
-		return getRepository() != null;
+		return selectionMapsToSingleRepository();
 	}
 }
