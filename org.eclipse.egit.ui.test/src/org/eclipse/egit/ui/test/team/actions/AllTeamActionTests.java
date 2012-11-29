036bd93f47a277b98b7b5c5e379c714b93e49178
Robin Stocker <robin@nibor.org>
2012-11-29 16:48:31 +0100

    Add UI test for "Merge Tool" action with "use HEAD" option
    Bug: 395177
    Change-Id: I5dc32d7c08230f6d59ced3b6158512ac4d61327c

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/team/actions/AllTeamActionTests.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/team/actions/AllTeamActionTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 SAP AG.
+ * Copyright (c) 2010, 2012 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -24,7 +24,8 @@ import org.junit.runners.Suite.SuiteClasses;
 		ShowInTest.class, //
 		CompareActionsTest.class, //
 		ReplaceActionsTest.class, //
-		CreatePatchActionTest.class })
+		CreatePatchActionTest.class, //
+		MergeToolTest.class})
 public class AllTeamActionTests {
 	// nothing
 }
