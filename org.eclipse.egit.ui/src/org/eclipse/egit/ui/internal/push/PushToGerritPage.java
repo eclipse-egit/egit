fb34ce4ecac23914640834e6ca852a8ad56d95dd
Kamil Sobon <iso.poczta@gmail.com>
2012-12-06 15:46:25 +0100

    Add "reviewer" text area in push to gerrit dialog
    UI prototype. Needs a lot of work (probably some ideas may be take from
    Mylyn gerrit's connector).
    
    Change-Id: I254a7b4385172a1af791446860b43f561281431a

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/push/PushToGerritPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/push/PushToGerritPage.java
@@ -84,6 +84,8 @@ class PushToGerritPage extends WizardPage {
 
 	private Text branchText;
 
+	private Text reviewerText;
+
 	/**
 	 * @param repository
 	 */
@@ -166,6 +168,14 @@ class PushToGerritPage extends WizardPage {
 		selectLastUsedUri();
 		setLastUsedBranch();
 		branchText.setFocus();
+
+		Label reviewerLabel = new Label(main, SWT.NONE);
+		reviewerLabel.setText("Reviewer:"); //$NON-NLS-1$
+
+		reviewerText = new Text(main, SWT.BORDER);
+		GridDataFactory.fillDefaults().grab(true, false).span(2, 1)
+		.applyTo(reviewerText);
+
 		Dialog.applyDialogFont(main);
 		setControl(main);
 	}
@@ -228,6 +238,9 @@ class PushToGerritPage extends WizardPage {
 			PushOperationUI op = new PushOperationUI(repository, spec, timeout,
 					false);
 			op.setCredentialsProvider(new EGitCredentialsProvider());
+
+			// It's just a prototype to present that it really works
+			op.setReceivePack(String.format("git receive-pack --reviewer=%s", reviewerText.getText())); //$NON-NLS-1$
 			PushOperationResult result = op.execute(monitor);
 			PushResultDialog dlg = new PushResultDialog(getShell(), repository,
 					result, op.getDestinationString());
