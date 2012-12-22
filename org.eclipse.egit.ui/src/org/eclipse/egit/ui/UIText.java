14f5bb3efff1e985f14ee8b30c86467f27a31c21
Robin Stocker <robin@nibor.org>
2012-12-22 15:33:44 +0100

    Prevent IO_FAILURE by checking for conflicts in ref name validation
    E.g. "a/1" is a conflict if "a" already exists, "b" is a conflict if
    "b/1" already exists.
    
    With the JGit change that this depends on, we can even show the user
    which existing refs cause the conflict.
    
    Bug: 393914
    JGit-Dependency: I11e0ba2f300adb8b3612943c304ba68bbe73db8a
    Change-Id: Ibea9984121ae88c488858b8a8e73b593195b15e0
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -4315,6 +4315,9 @@ public class UIText extends NLS {
 	public static String ValidationUtils_RefAlreadyExistsMessage;
 
 	/** */
+	public static String ValidationUtils_RefNameConflictsWithExistingMessage;
+
+	/** */
 	public static String ValidationUtils_PleaseEnterNameMessage;
 
 	/** */
