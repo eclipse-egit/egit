8af459e5bf7dca34b7d86359803715663fe3561f
Robin Rosenberg <robin.rosenberg@dewire.com>
2012-12-28 17:23:21 +0100

    Only show current HEAD in hard reset dialog when target is same
    This is the dialog invoked from the repository view.
    
    Change-Id: Ie6d33ac41e08a80a2ef37101efce32620801a3b9

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/SelectResetTypePage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/SelectResetTypePage.java
@@ -141,29 +141,31 @@ public class SelectResetTypePage extends WizardPage {
 			}
 		}
 
-		RevCommit targetCommit = getLatestCommit(target);
+		if (!current.equals(target)) {
+			RevCommit targetCommit = getLatestCommit(target);
 
-		Label targetLabel = new Label(displayArea, SWT.NONE);
-		targetLabel.setText(UIText.SelectResetTypePage_labelResettingTo);
-		targetLabel
-				.setToolTipText(UIText.SelectResetTypePage_tooltipResettingTo);
+			Label targetLabel = new Label(displayArea, SWT.NONE);
+			targetLabel.setText(UIText.SelectResetTypePage_labelResettingTo);
+			targetLabel
+					.setToolTipText(UIText.SelectResetTypePage_tooltipResettingTo);
 
-		CLabel targetValue = new CLabel(displayArea, SWT.NONE);
-		Image targetIcon = getIcon(target);
-		UIUtils.hookDisposal(targetValue, targetIcon);
-		targetValue.setImage(targetIcon);
-		targetValue.setText(Repository.shortenRefName(target));
+			CLabel targetValue = new CLabel(displayArea, SWT.NONE);
+			Image targetIcon = getIcon(target);
+			UIUtils.hookDisposal(targetValue, targetIcon);
+			targetValue.setImage(targetIcon);
+			targetValue.setText(Repository.shortenRefName(target));
 
-		if (targetCommit != null) {
-			if (isCommit(target))
-				targetValue.setText(formatCommit(targetCommit));
-			else {
-				new Label(displayArea, SWT.NONE);
-				CLabel commitLabel = new CLabel(displayArea, SWT.NONE);
-				Image commitIcon = UIIcons.CHANGESET.createImage();
-				UIUtils.hookDisposal(commitLabel, commitIcon);
-				commitLabel.setImage(commitIcon);
-				commitLabel.setText(formatCommit(targetCommit));
+			if (targetCommit != null) {
+				if (isCommit(target))
+					targetValue.setText(formatCommit(targetCommit));
+				else {
+					new Label(displayArea, SWT.NONE);
+					CLabel commitLabel = new CLabel(displayArea, SWT.NONE);
+					Image commitIcon = UIIcons.CHANGESET.createImage();
+					UIUtils.hookDisposal(commitLabel, commitIcon);
+					commitLabel.setImage(commitIcon);
+					commitLabel.setText(formatCommit(targetCommit));
+				}
 			}
 		}
 
