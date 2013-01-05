93e07f0408c36f82c6ec93f351be804096d494de
Robin Rosenberg <robin.rosenberg@dewire.com>
2013-01-05 17:59:42 +0100

    Fix NPE when fetching from Gerrit
    Bug: 397399
    Change-Id: Ie0b6f0097e452c7c5d2dee5cf792a39271ca3988

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/fetch/FetchGerritChangePage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/fetch/FetchGerritChangePage.java
@@ -144,15 +144,17 @@ public class FetchGerritChangePage extends WizardPage {
 		Clipboard clipboard = new Clipboard(parent.getDisplay());
 		String clipText = (String) clipboard.getContents(TextTransfer
 				.getInstance());
-		final String pattern = "git fetch (\\w+:\\S+) (refs/changes/\\d+/\\d+/\\d+) && git (\\w+) FETCH_HEAD"; //$NON-NLS-1$
-		Matcher matcher = Pattern.compile(pattern).matcher(clipText);
 		String defaultUri = null;
 		String defaultCommand = null;
 		String defaultChange = null;
-		if (matcher.matches()) {
-			defaultUri = matcher.group(1);
-			defaultChange = matcher.group(2);
-			defaultCommand = matcher.group(3);
+		if (clipText != null) {
+			final String pattern = "git fetch (\\w+:\\S+) (refs/changes/\\d+/\\d+/\\d+) && git (\\w+) FETCH_HEAD"; //$NON-NLS-1$
+			Matcher matcher = Pattern.compile(pattern).matcher(clipText);
+			if (matcher.matches()) {
+				defaultUri = matcher.group(1);
+				defaultChange = matcher.group(2);
+				defaultCommand = matcher.group(3);
+			}
 		}
 		Composite main = new Composite(parent, SWT.NONE);
 		main.setLayout(new GridLayout(2, false));
