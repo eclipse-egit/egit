b09d30794332c7448abefe222e6153c1a33d6c93
Matthias Sohn <matthias.sohn@sap.com>
2013-01-16 10:08:14 +0100

    Show detached HEAD warning again
    This warning dialog got lost with commit
    e3e898a6f332776305714deaefb47119b33b9b09
    
    Bug: 398127
    Change-Id: I9159cbce19cf73ebb539d943e71624004a6cea3d
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIText.java
@@ -2763,6 +2763,9 @@ public class UIText extends NLS {
 	public static String BranchResultDialog_CheckoutConflictsTitle;
 
 	/** */
+	public static String BranchResultDialog_dontShowAgain;
+
+	/** */
 	public static String CheckoutDialog_ErrorCouldNotCreateNewRef;
 
 	/** */
