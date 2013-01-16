a0bebee2b6999f639b5e9bbadd43b4b250501716
Matthias Sohn <matthias.sohn@sap.com>
2013-01-16 10:08:14 +0100

    Show detached HEAD warning again
    This warning dialog got lost with commit
    e3e898a6f332776305714deaefb47119b33b9b09
    
    Bug: 398127
    Change-Id: I9159cbce19cf73ebb539d943e71624004a6cea3d
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/branch/BranchResultDialog.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/branch/BranchResultDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2012 SAP AG and others.
+ * Copyright (c) 2010-2013 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,12 +11,15 @@
 package org.eclipse.egit.ui.internal.branch;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import org.eclipse.core.resources.IResource;
 import org.eclipse.egit.core.internal.job.JobUtil;
 import org.eclipse.egit.core.op.ResetOperation;
+import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.JobFamilies;
+import org.eclipse.egit.ui.UIPreferences;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.internal.commit.CommitUI;
 import org.eclipse.egit.ui.internal.dialogs.NonDeletedFilesDialog;
@@ -24,10 +27,13 @@ import org.eclipse.egit.ui.internal.dialogs.NonDeletedFilesTree;
 import org.eclipse.egit.ui.internal.stash.StashCreateUI;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.dialogs.MessageDialogWithToggle;
 import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jgit.api.CheckoutResult;
 import org.eclipse.jgit.api.ResetCommand.ResetType;
 import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.SWT;
@@ -90,6 +96,13 @@ public class BranchResultDialog extends MessageDialog {
 							.getUndeletedList()).open();
 				}
 			});
+		} else if (result.getStatus() == CheckoutResult.Status.OK) {
+			try {
+				if (ObjectId.isId(repository.getFullBranch()))
+					showDetachedHeadWarning();
+			} catch (IOException e) {
+				// TODO
+			}
 		}
 	}
 
@@ -164,5 +177,25 @@ public class BranchResultDialog extends MessageDialog {
 				UIText.BranchResultDialog_buttonReset, false);
 		createButton(parent, IDialogConstants.CANCEL_ID,
 				IDialogConstants.CANCEL_LABEL, true);
+	}
+
+	private static void showDetachedHeadWarning() {
+		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+			public void run() {
+				IPreferenceStore store = Activator.getDefault()
+						.getPreferenceStore();
+
+				if (store.getBoolean(UIPreferences.SHOW_DETACHED_HEAD_WARNING)) {
+					String toggleMessage = UIText.BranchResultDialog_dontShowAgain;
+					MessageDialogWithToggle.openInformation(PlatformUI
+							.getWorkbench().getActiveWorkbenchWindow()
+							.getShell(),
+							UIText.BranchOperationUI_DetachedHeadTitle,
+							UIText.BranchOperationUI_DetachedHeadMessage,
+							toggleMessage, false, store,
+							UIPreferences.SHOW_DETACHED_HEAD_WARNING);
+				}
+			}
+		});
 	}
 }
