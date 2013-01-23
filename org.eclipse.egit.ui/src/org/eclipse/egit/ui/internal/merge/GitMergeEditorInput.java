02d004ce5221e2707b2311e95376550dd78a6785
Robin Stocker <robin@nibor.org>
2013-01-23 23:09:12 +0100

    Make Merge Tool also usable for conflicts of unstashing
    Use index entries (2 and 3) for merge editor sides. This way, it is also
    usable for conflicts of "stash apply". There should be no difference for
    merge/rebase conflicts.
    
    Bug: 376135
    Change-Id: Ib9ac9ecd1a9f96217965cd288bc90fa377aad63b

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/merge/GitMergeEditorInput.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/merge/GitMergeEditorInput.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2010, 2012 Mathias Kinzler <mathias.kinzler@sap.com> and others.
+ * Copyright (C) 2010, 2013 Mathias Kinzler <mathias.kinzler@sap.com> and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -28,12 +28,14 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.egit.core.internal.CompareCoreUtils;
 import org.eclipse.egit.core.internal.storage.GitFileRevision;
 import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.internal.CompareUtils;
 import org.eclipse.egit.ui.internal.FileEditableRevision;
+import org.eclipse.egit.ui.internal.FileRevisionTypedElement;
 import org.eclipse.egit.ui.internal.LocalFileRevision;
 import org.eclipse.egit.ui.internal.GitCompareFileRevisionEditorInput.EmptyTypedElement;
 import org.eclipse.jface.operation.IRunnableContext;
@@ -310,10 +312,13 @@ public class GitMergeEditorInput extends CompareEditorInput {
 					continue;
 
 				ITypedElement right;
-				if (conflicting)
-					right = CompareUtils.getFileRevisionTypedElement(gitPath,
-							rightCommit, repository);
-				else
+				if (conflicting) {
+					GitFileRevision revision = GitFileRevision.inIndex(
+							repository, gitPath, DirCacheEntry.STAGE_3);
+					String encoding = CompareCoreUtils.getResourceEncoding(
+							repository, gitPath);
+					right = new FileRevisionTypedElement(revision, encoding);
+				} else
 					right = CompareUtils.getFileRevisionTypedElement(gitPath,
 							headCommit, repository);
 
@@ -335,8 +340,8 @@ public class GitMergeEditorInput extends CompareEditorInput {
 				if (!conflicting || useWorkspace)
 					rev = new LocalFileRevision(file);
 				else
-					rev = GitFileRevision.inCommit(repository, headCommit,
-							gitPath, null);
+					rev = GitFileRevision.inIndex(repository, gitPath,
+							DirCacheEntry.STAGE_2);
 
 				IRunnableContext runnableContext = getContainer();
 				if (runnableContext == null)
