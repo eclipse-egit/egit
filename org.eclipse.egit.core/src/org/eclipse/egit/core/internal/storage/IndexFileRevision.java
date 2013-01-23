4c9e4c96cdb86cc54459cca6bd72006cdee208f2
Robin Stocker <robin@nibor.org>
2013-01-23 22:27:05 +0100

    Add support for specific stage to IndexFileRevision
    This is needed by several changes. The current behavior of using the
    first available stage is preserved for now.
    
    Change-Id: I26dbc133e2ab11682778ca564448f98bc5217c7e

--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/storage/IndexFileRevision.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/storage/IndexFileRevision.java
@@ -3,6 +3,7 @@
  * Copyright (C) 2006, Robin Rosenberg <robin.rosenberg@dewire.com>
  * Copyright (C) 2008, Shawn O. Pearce <spearce@spearce.org>
  * Copyright (C) 2011, Dariusz Luksza <dariusz@luksza.org>
+ * Copyright (C) 2013, Robin Stocker <robin@nibor.org>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -18,6 +19,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.egit.core.Activator;
 import org.eclipse.egit.core.CoreText;
+import org.eclipse.jgit.dircache.DirCache;
 import org.eclipse.jgit.dircache.DirCacheEntry;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
@@ -26,16 +28,27 @@ import org.eclipse.team.core.history.IFileRevision;
 
 /** An {@link IFileRevision} for the version in the Git index. */
 class IndexFileRevision extends GitFileRevision {
+
+	// This is to maintain compatibility with the old behavior
+	private static final int FIRST_AVAILABLE = -1;
+
 	private final Repository db;
 
 	private final String path;
 
+	private final int stage;
+
 	private ObjectId blobId;
 
 	IndexFileRevision(final Repository repo, final String fileName) {
+		this(repo, fileName, FIRST_AVAILABLE);
+	}
+
+	IndexFileRevision(final Repository repo, final String fileName, int stage) {
 		super(fileName);
-		db = repo;
-		path = fileName;
+		this.db = repo;
+		this.path = fileName;
+		this.stage = stage;
 	}
 
 	public IStorage getStorage(IProgressMonitor monitor) throws CoreException {
@@ -66,11 +79,24 @@ class IndexFileRevision extends GitFileRevision {
 
 	private ObjectId locateBlobObjectId() throws CoreException {
 		try {
-			DirCacheEntry entry = db.readDirCache().getEntry(path);
-			if (entry == null)
+			DirCache dc = db.readDirCache();
+			int firstIndex = dc.findEntry(path);
+			if (firstIndex < 0)
 				return null;
 
-			return entry.getObjectId();
+			// Try to avoid call to nextEntry if first entry already matches
+			DirCacheEntry firstEntry = dc.getEntry(firstIndex);
+			if (stage == FIRST_AVAILABLE || firstEntry.getStage() == stage)
+				return firstEntry.getObjectId();
+
+			// Ok, we have to search
+			int nextIndex = dc.nextEntry(firstIndex);
+			for (int i = firstIndex; i < nextIndex; i++) {
+				DirCacheEntry entry = dc.getEntry(i);
+				if (entry.getStage() == stage)
+					return entry.getObjectId();
+			}
+			return null;
 		} catch (IOException e) {
 			throw new CoreException(Activator.error(NLS.bind(
 					CoreText.IndexFileRevision_errorLookingUpPath, path), e));
