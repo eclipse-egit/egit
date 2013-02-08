62af296b080f2762b7b2000a7f9b859762eaa71d
Robin Stocker <robin@nibor.org>
2013-02-08 16:53:55 +0100

    Remember "Search for nested projects" checkbox state in import wizards
    Bug: 400202
    Change-Id: Ia7b578b8e2fe79056159c5eef7af5045b173065f
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/GitCreateProjectViaWizardWizard.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/GitCreateProjectViaWizardWizard.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 SAP AG.
+ * Copyright (c) 2010, 2013 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -65,6 +65,7 @@ public class GitCreateProjectViaWizardWizard extends Wizard {
 		setWindowTitle(NLS.bind(
 				UIText.GitCreateProjectViaWizardWizard_WizardTitle,
 				myRepository.getDirectory().getPath()));
+		setDialogSettings(GitImportWizard.getImportWizardDialogSettings());
 	}
 
 	@Override
@@ -157,6 +158,7 @@ public class GitCreateProjectViaWizardWizard extends Wizard {
 					IWorkingSet[] workingSetArray = myProjectsImportPage
 							.getSelectedWorkingSets();
 					workingSets.addAll(Arrays.asList(workingSetArray));
+					myProjectsImportPage.saveWidgetValues();
 				}
 			});
 			ProjectUtils.createProjects(projectsToCreate, myRepository,
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/GitImportWizard.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/GitImportWizard.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010-2012 SAP AG.
+ * Copyright (c) 2010-2013 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -38,6 +38,7 @@ import org.eclipse.egit.ui.internal.clone.GitCloneSourceProviderExtension.CloneS
 import org.eclipse.egit.ui.internal.provisional.wizards.GitRepositoryInfo;
 import org.eclipse.egit.ui.internal.provisional.wizards.IRepositorySearchResult;
 import org.eclipse.egit.ui.internal.provisional.wizards.NoRepositoryInfoException;
+import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.IWizardPage;
@@ -53,6 +54,7 @@ import org.eclipse.ui.actions.NewProjectAction;
  * A wizard which allows to optionally clone a repository and to import projects from a repository.
  */
 public class GitImportWizard extends AbstractGitCloneWizard implements IImportWizard {
+	private static final String GIT_IMPORT_SECTION = "GitImportWizard"; //$NON-NLS-1$
 
 	private GitSelectRepositoryPage selectRepoPage = new GitSelectRepositoryPage();
 
@@ -105,6 +107,7 @@ public class GitImportWizard extends AbstractGitCloneWizard implements IImportWi
 		super(searchResult);
 		setWindowTitle(UIText.GitImportWizard_WizardTitle);
 		setDefaultPageImageDescriptor(UIIcons.WIZBAN_IMPORT_REPO);
+		setDialogSettings(getImportWizardDialogSettings());
 	}
 
 	@Override
@@ -232,6 +235,7 @@ public class GitImportWizard extends AbstractGitCloneWizard implements IImportWi
 							.getSelectedWorkingSets();
 					workingSets.addAll(Arrays.asList(workingSetArray));
 					repository[0] = getTargetRepository();
+					projectsImportPage.saveWidgetValues();
 				}
 			});
 			ProjectUtils.createProjects(projectsToCreate, repository[0],
@@ -320,5 +324,15 @@ public class GitImportWizard extends AbstractGitCloneWizard implements IImportWi
 		}
 	}
 
+	static IDialogSettings getImportWizardDialogSettings() {
+		IDialogSettings settings = Activator.getDefault().getDialogSettings();
 
+		IDialogSettings wizardSettings = settings
+				.getSection(GitImportWizard.GIT_IMPORT_SECTION);
+		if (wizardSettings == null) {
+			wizardSettings = settings
+					.addNewSection(GitImportWizard.GIT_IMPORT_SECTION);
+		}
+		return wizardSettings;
+	}
 }
