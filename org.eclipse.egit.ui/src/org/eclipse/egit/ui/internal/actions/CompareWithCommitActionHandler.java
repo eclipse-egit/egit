6211583f9760800e0fb1b72dd1e1e8ca47634058
Laurent Goubet <laurent.goubet@obeo.fr>
2013-02-15 17:17:03 +0100

    Consider the ancestor when comparing with remote revisions
    This changes all of the actions where comparing with a common ancestor
    makes sense :
    - Compare With > Commit
    - Compare With > Branch, Tag, Reference
    - Double-click a commit in the history view
    - Right-click a commit in the history view > Compare With Workspace
    - Right-click a file in the history view > Compare With Workspace
    - Right-clik a file in the commit viewer > Compare With Workspace
    
    Bug: 400943
    Change-Id: I26bce7a1bde3b944f10f40eacbbdb7c41e486c0b

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CompareWithCommitActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CompareWithCommitActionHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2011, 2012 Mathias Kinzler <mathias.kinzler@sap.com> and others.
+ * Copyright (C) 2011, 2013 Mathias Kinzler <mathias.kinzler@sap.com> and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -11,26 +11,17 @@ package org.eclipse.egit.ui.internal.actions;
 
 import java.io.IOException;
 
-import org.eclipse.compare.CompareUI;
-import org.eclipse.compare.ITypedElement;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.UIText;
 import org.eclipse.egit.ui.internal.CompareUtils;
-import org.eclipse.egit.ui.internal.GitCompareFileRevisionEditorInput;
 import org.eclipse.egit.ui.internal.dialogs.CompareTreeView;
 import org.eclipse.egit.ui.internal.history.CommitSelectionDialog;
 import org.eclipse.jface.window.Window;
-import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.revwalk.RevWalk;
-import org.eclipse.team.ui.synchronize.SaveableCompareEditorInput;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 
@@ -55,32 +46,14 @@ public class CompareWithCommitActionHandler extends RepositoryActionHandler {
 		if (resources.length == 1 && resources[0] instanceof IFile) {
 			final IFile baseFile = (IFile) resources[0];
 
-			final ITypedElement base = SaveableCompareEditorInput
-					.createFileElement(baseFile);
-
-			final ITypedElement next;
-			final ITypedElement ancestor;
 			try {
-				RepositoryMapping mapping = RepositoryMapping
-						.getMapping(resources[0]);
-				next = getElementForCommit(mapping.getRepository(), mapping
-						.getRepoRelativePath(baseFile), dlg.getCommitId());
-
-				ancestor =  CompareUtils.getFileRevisionTypedElementForCommonAncestor(mapping
-						.getRepoRelativePath(baseFile), repo.resolve(Constants.HEAD),
-						dlg.getCommitId(), repo);
+				CompareUtils.compareWorkspaceWithRef(repo, baseFile, dlg
+						.getCommitId().getName(), null);
 			} catch (IOException e) {
 				Activator.handleError(
-						UIText.CompareWithIndexAction_errorOnAddToIndex, e,
-						true);
-				return null;
+								UIText.CompareWithRefAction_errorOnSynchronize,
+								e, true);
 			}
-
-			final GitCompareFileRevisionEditorInput in = new GitCompareFileRevisionEditorInput(
-					base, next, ancestor, null);
-			in.getCompareConfiguration()
-					.setRightLabel(dlg.getCommitId().name());
-			CompareUI.openCompareEditor(in);
 		} else {
 			CompareTreeView view;
 			try {
@@ -93,15 +66,6 @@ public class CompareWithCommitActionHandler extends RepositoryActionHandler {
 			}
 		}
 		return null;
-	}
-
-	private ITypedElement getElementForCommit(final Repository repository,
-			final String gitPath, final ObjectId commitId) throws IOException {
-		RevWalk rw = new RevWalk(repository);
-		RevCommit commit = rw.parseCommit(commitId);
-		rw.release();
-
-		return CompareUtils.getFileRevisionTypedElement(gitPath, commit, repository);
 	}
 
 	@Override
