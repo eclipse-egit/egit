27faa71e8e9a21f2a3253b6abdc32a2a6d540f87
Robin Stocker <robin@nibor.org>
2013-03-08 21:15:41 +0100

    [stagingView] Improve usability of "Replace with" on missing
    Only provide one of the equivalent options for a file that has been
    deleted. For this to work, we now distinguish between missing MISSING
    and MISSING_AND_CHANGED (which has changes in the index). For the latter
    we still have to provide both options.
    
    See also I65601162f7ef506ea270933218601322fde1e929 where the same change
    was done for modified files.
    
    Change-Id: Id4ff449791e7a911809281bab63f9d3e3d17b5ed

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingViewContentProvider.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingViewContentProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2011, Bernard Leach <leachbj@bouncycastle.org>
+ * Copyright (C) 2011, 2013 Bernard Leach <leachbj@bouncycastle.org> and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -12,6 +12,7 @@ import static org.eclipse.egit.ui.internal.staging.StagingEntry.State.ADDED;
 import static org.eclipse.egit.ui.internal.staging.StagingEntry.State.CHANGED;
 import static org.eclipse.egit.ui.internal.staging.StagingEntry.State.CONFLICTING;
 import static org.eclipse.egit.ui.internal.staging.StagingEntry.State.MISSING;
+import static org.eclipse.egit.ui.internal.staging.StagingEntry.State.MISSING_AND_CHANGED;
 import static org.eclipse.egit.ui.internal.staging.StagingEntry.State.MODIFIED;
 import static org.eclipse.egit.ui.internal.staging.StagingEntry.State.PARTIALLY_MODIFIED;
 import static org.eclipse.egit.ui.internal.staging.StagingEntry.State.REMOVED;
@@ -84,7 +85,11 @@ public class StagingViewContentProvider implements
 		final Repository repository = update.repository;
 		if (isWorkspace) {
 			for (String file : indexDiff.getMissing())
-				nodes.add(new StagingEntry(repository, MISSING, file));
+				if (indexDiff.getChanged().contains(file))
+					nodes.add(new StagingEntry(repository, MISSING_AND_CHANGED,
+							file));
+				else
+					nodes.add(new StagingEntry(repository, MISSING, file));
 			for (String file : indexDiff.getModified())
 				if (indexDiff.getChanged().contains(file))
 					nodes.add(new StagingEntry(repository, PARTIALLY_MODIFIED,
