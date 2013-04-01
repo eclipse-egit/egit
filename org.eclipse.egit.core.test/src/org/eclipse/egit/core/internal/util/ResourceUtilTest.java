c16aa64f54120b62350d1c2adf77c60cca34ed5e
Robin Rosenberg <robin.rosenberg@dewire.com>
2013-04-02 00:13:47 +0200

    Resources should be opened in the most specific project
    Some types of resources, e.g. Java source files, must be opened as
    a member of the right project. Knowing which project to reference is
    somewhat fuzzy, but we assume the most deeply nested projhect is the
    right one.
    
    Bug: 404629
    Change-Id: I6a713f39894f02a1c5e07e62665c579bab10b2c0

--- org.eclipse.egit.core.test/src/org/eclipse/egit/core/internal/util/ResourceUtilTest.java
+++ org.eclipse.egit.core.test/src/org/eclipse/egit/core/internal/util/ResourceUtilTest.java
@@ -19,6 +19,7 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.egit.core.test.GitTestCase;
 import org.eclipse.egit.core.test.TestProject;
 import org.junit.Test;
@@ -47,13 +48,27 @@ public class ResourceUtilTest extends GitTestCase {
 	}
 
 	@Test
-	public void getFileForLocationShouldReturnExistingFileInCaseOfNestedProjectWithClosedRoot() throws Exception {
-		TestProject nested = new TestProject(true, "Project-1/Project-2");
+	public void getFileForLocationShouldReturnExistingFileInCaseOfNestedProject()
+			throws Exception {
+		TestProject nested = new TestProject(true, "Project-1/Project-0");
 		IFile file = nested.createFile("a.txt", new byte[] {});
 		IPath location = file.getLocation();
-		// Close root project
-		project.getProject().close(null);
 
+		IFile result = ResourceUtil.getFileForLocation(location);
+		assertThat(result, notNullValue());
+		assertTrue("Returned IFile should exist", result.exists());
+		assertThat(result.getProject(), is(nested.getProject()));
+	}
+
+	@Test
+	public void getFileForLocationShouldReturnExistingFileInCaseOfNestedNotClosedProject()
+			throws Exception {
+		TestProject nested = new TestProject(true, "Project-1/Project-0");
+		TestProject nested2 = new TestProject(true,
+				"Project-1/Project-0/Project");
+		IFile file = nested2.createFile("a.txt", new byte[] {});
+		IPath location = file.getLocation();
+		nested2.project.close(new NullProgressMonitor());
 		IFile result = ResourceUtil.getFileForLocation(location);
 		assertThat(result, notNullValue());
 		assertTrue("Returned IFile should exist", result.exists());
--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/util/ResourceUtil.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/util/ResourceUtil.java
@@ -44,8 +44,11 @@ public class ResourceUtil {
 
 	/**
 	 * Return the corresponding resource if it exists.
+	 * <p>
+	 * The returned file will be relative to the most nested non-closed project.
 	 *
-	 * @param location the path to check
+	 * @param location
+	 *            the path to check
 	 * @return the resources, or null
 	 */
 	public static IResource getResourceForLocation(IPath location) {
@@ -55,14 +58,24 @@ public class ResourceUtil {
 		if (file != null)
 			return file;
 		IContainer[] containers = root.findContainersForLocationURI(uri);
-		for (IContainer container : containers)
-			if (container.exists())
-				return container;
-		return null;
+		int shortestContainerPath = Integer.MAX_VALUE;
+		IContainer containeeWithShortestPath = null;
+		for (IContainer container : containers) {
+			if (!container.exists())
+				continue;
+			IPath fullPath = container.getFullPath();
+			if (fullPath.segmentCount() < shortestContainerPath) {
+				shortestContainerPath = fullPath.segmentCount();
+				containeeWithShortestPath = container;
+			}
+		}
+		return containeeWithShortestPath;
 	}
 
 	/**
 	 * Return the corresponding file if it exists.
+	 * <p>
+	 * The returned file will be relative to the most nested non-closed project.
 	 *
 	 * @param location
 	 * @return the file, or null
@@ -75,6 +88,8 @@ public class ResourceUtil {
 
 	/**
 	 * Get the {@link IFile} corresponding to the arguments if it exists.
+	 * <p>
+	 * The returned file will be relative to the most nested non-closed project.
 	 *
 	 * @param repository
 	 *            the repository of the file
@@ -152,10 +167,18 @@ public class ResourceUtil {
 
 	private static IFile getFileForLocationURI(IWorkspaceRoot root, URI uri) {
 		IFile[] files = root.findFilesForLocationURI(uri);
-		for (IFile file : files)
-			if (file.exists())
-				return file;
-		return null;
+		int shortestPathSegmentCount = Integer.MAX_VALUE;
+		IFile shortestPath = null;
+		for (IFile file : files) {
+			IPath fullPath = file.getFullPath();
+			if (!file.exists())
+				continue;
+			if (fullPath.segmentCount() < shortestPathSegmentCount) {
+				shortestPath = file;
+				shortestPathSegmentCount = fullPath.segmentCount();
+			}
+		}
+		return shortestPath;
 	}
 
 	private static void addPathToMap(RepositoryMapping repositoryMapping,
