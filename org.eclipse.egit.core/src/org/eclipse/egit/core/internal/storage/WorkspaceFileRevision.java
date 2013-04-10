eda8c7410e699007d95237b326684f5b63d11290
Laurent Goubet <laurent.goubet@obeo.fr>
2013-04-10 09:05:36 +0200

    Ambiguous parameter naming
    What GirFileRevision really expects is a path, not a file name. Most
    usages of the constructors already passed in a file path... but the
    parameter naming led to a number of clients to use a file name, which
    creates FileRevisions with no resolvable path. This fixes all
    ambiguous parameter names and usages.
    
    Change-Id: Id597d427555f71a6b4f422acb06a1faf26c796bd

--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/storage/WorkspaceFileRevision.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/storage/WorkspaceFileRevision.java
@@ -24,7 +24,7 @@ public class WorkspaceFileRevision extends GitFileRevision {
 	 * @param resource
 	 */
 	public WorkspaceFileRevision(final IResource resource) {
-		super(resource.getName());
+		super(resource.getFullPath().toString());
 		rsrc = resource;
 	}
 
