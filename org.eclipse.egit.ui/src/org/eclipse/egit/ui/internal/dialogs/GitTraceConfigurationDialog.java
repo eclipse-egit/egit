5fac3dc56e33ca38d5cd1983bf3471a63d713f2a
Robin Rosenberg <robin.rosenberg@dewire.com>
2013-04-15 21:41:09 +0200

    Fix potential NPE if .options doesn't exist
    If .options does not exist, getResources will return null rather
    than causing an IOException in openStream.
    
    Change-Id: I082d102d5b219be710ec2299e2e552ae0fe8478a
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/GitTraceConfigurationDialog.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/GitTraceConfigurationDialog.java
@@ -10,6 +10,7 @@ package org.eclipse.egit.ui.internal.dialogs;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -457,9 +458,16 @@ public class GitTraceConfigurationDialog extends TitleAreaDialog {
 		for (PluginNode plugin : PLUGIN_LIST) {
 			Properties props = new Properties();
 			try {
-				InputStream is = Platform.getBundle(plugin.getPlugin())
-						.getResource(".options").openStream(); //$NON-NLS-1$
-				props.load(is);
+				URL resource = Platform.getBundle(plugin.getPlugin())
+						.getResource(".options"); //$NON-NLS-1$
+				if (resource != null) {
+					InputStream is = resource.openStream();
+					try {
+						props.load(is);
+					} finally {
+						is.close();
+					}
+				}
 			} catch (IOException e) {
 				Activator.handleError(e.getMessage(), e, true);
 			}
@@ -497,9 +505,16 @@ public class GitTraceConfigurationDialog extends TitleAreaDialog {
 		for (PluginNode plugin : PLUGIN_LIST) {
 			Properties props = new Properties();
 			try {
-				InputStream is = Platform.getBundle(plugin.getPlugin())
-						.getResource(".options").openStream(); //$NON-NLS-1$
-				props.load(is);
+				URL resource = Platform.getBundle(plugin.getPlugin())
+						.getResource(".options"); //$NON-NLS-1$
+				if (resource != null) {
+					InputStream is = resource.openStream();
+					try {
+						props.load(is);
+					} finally {
+						is.close();
+					}
+				}
 			} catch (IOException e) {
 				Activator.handleError(e.getMessage(), e, true);
 			}
