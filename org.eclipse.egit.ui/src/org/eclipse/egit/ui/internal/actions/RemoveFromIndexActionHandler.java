0e689bd3f768c147d4e5e42e1e333bc9e6c92f25
Robin Stocker <robin@nibor.org>
2013-04-19 13:33:19 +0200

    Include related model elements in "Remove from Index" action
    Bug: 395904
    Change-Id: I7db0d157bf2aa7d57020b57dd74d87a6a98f9345

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/RemoveFromIndexActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/RemoveFromIndexActionHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2011, 2012 Dariusz Luksza <dariusz@luksza.org> and others.
+ * Copyright (C) 2011, 2013 Dariusz Luksza <dariusz@luksza.org> and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -9,9 +9,13 @@
 package org.eclipse.egit.ui.internal.actions;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashSet;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -23,6 +27,8 @@ import org.eclipse.egit.core.op.RemoveFromIndexOperation;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.JobFamilies;
 import org.eclipse.egit.ui.internal.UIText;
+import org.eclipse.egit.ui.internal.operations.GitScopeUtil;
+import org.eclipse.ui.IWorkbenchPart;
 
 /**
  * Action for removing resource from the git index
@@ -32,11 +38,11 @@ import org.eclipse.egit.ui.internal.UIText;
 public class RemoveFromIndexActionHandler extends RepositoryActionHandler {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		final IPath[] sel = getSelectedLocations(event);
-		if (sel.length == 0)
+		Collection<IPath> locations = getSelectedAndRelatedLocations(event);
+		if (locations.isEmpty())
 			return null;
 
-		final RemoveFromIndexOperation removeOperation = new RemoveFromIndexOperation(Arrays.asList(sel));
+		final RemoveFromIndexOperation removeOperation = new RemoveFromIndexOperation(locations);
 		Job job = new Job(UIText.RemoveFromIndexAction_removingFiles) {
 			@Override
 			protected IStatus run(IProgressMonitor monitor) {
@@ -65,6 +71,35 @@ public class RemoveFromIndexActionHandler extends RepositoryActionHandler {
 		job.schedule();
 
 		return null;
+	}
+
+	private Collection<IPath> getSelectedAndRelatedLocations(ExecutionEvent event)
+			throws ExecutionException {
+		final IPath[] selectedLocations = getSelectedLocations(event);
+		if (selectedLocations.length == 0)
+			return Collections.emptyList();
+
+		final LinkedHashSet<IPath> locations = new LinkedHashSet<IPath>(
+				Arrays.asList(selectedLocations));
+
+		final IResource[] selectedResources = getSelectedResources(event);
+		if (selectedResources.length != 0) {
+			final IWorkbenchPart part = getPart(event);
+			try {
+				final IResource[] resourcesInScope = GitScopeUtil
+						.getRelatedChanges(part, selectedResources);
+				for (IResource resource : resourcesInScope) {
+					IPath l = resource.getLocation();
+					if (l != null)
+						locations.add(l);
+				}
+			} catch (InterruptedException e) {
+				// ignore, we will not execute the operation in case the user
+				// cancels the scope operation
+				return Collections.emptyList();
+			}
+		}
+		return locations;
 	}
 
 	@Override
