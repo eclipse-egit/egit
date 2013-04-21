25e2ea64756e56bd338856ee47c0c2c941c0bf7f
Tomasz Zarna <tomasz.zarna@tasktop.com>
2013-04-21 23:44:49 +0200

    Branch Creation: default for pull strategy is inconsistent
    Bug: 384605
    Change-Id: I273f8f520ea4daa8d1682de125cbb442c126cf87

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/CreateBranchOnCommitHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/CreateBranchOnCommitHandler.java
@@ -1,5 +1,6 @@
 /*******************************************************************************
  * Copyright (C) 2010, Mathias Kinzler <mathias.kinzler@sap.com>
+ * Copyright (C) 2013, Tomasz Zarna <tomasz.zarna@tasktop.com>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,12 +9,18 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.history.command;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.egit.ui.internal.history.GitHistoryPage;
 import org.eclipse.egit.ui.internal.repository.CreateBranchWizard;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.IWizard;
 import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revplot.PlotCommit;
 import org.eclipse.jgit.revwalk.RevCommit;
@@ -24,15 +31,36 @@ import org.eclipse.ui.handlers.HandlerUtil;
  */
 public class CreateBranchOnCommitHandler extends AbstractHistoryCommandHandler {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		PlotCommit commit = (PlotCommit) getSelection(getPage()).getFirstElement();
+		GitHistoryPage page = getPage();
 		Repository repo = getRepository(event);
 
+		IWizard wiz = null;
+		List<Ref> branches = getBranchesExcludingCurrent(page, repo);
+
+		if (branches.isEmpty()) {
+			PlotCommit commit = (PlotCommit) getSelection(page)
+					.getFirstElement();
+			wiz = new CreateBranchWizard(repo, commit.name());
+		} else {
+			Ref branch = branches.get(0);
+			wiz = new CreateBranchWizard(repo, branch.getName());
+		}
+
 		WizardDialog dlg = new WizardDialog(
-				HandlerUtil.getActiveShellChecked(event),
-				new CreateBranchWizard(repo, commit.name()));
+				HandlerUtil.getActiveShellChecked(event), wiz);
 		dlg.setHelpAvailable(false);
 		dlg.open();
 		return null;
+	}
+
+	private List<Ref> getBranchesExcludingCurrent(GitHistoryPage page,
+			Repository repo) {
+		try {
+			return getBranchesOfCommit(page, repo, true);
+		} catch (IOException e) {
+			// ignore, use commit name
+			return Collections.<Ref> emptyList();
+		}
 	}
 
 	@Override
