a6d705e365db8108d58b13af652aa9a870f026ca
Robin Stocker <robin@nibor.org>
2013-05-01 18:42:35 +0200

    Perform CRLF conversion when saving index in partial staging
    Before, it would just copy over the bytes as they were in the editor
    (with working directory EOLs). So they ended up in the index and in the
    repository.
    
    Now we perform input CRLF conversion on save if configured.
    
    Also gets rid of some unnecessary array copies in EditableRevision.
    
    Bug: 406798
    Change-Id: Ia28595a76454e9c8af63674c481404718642cf27

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/EditableRevision.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/EditableRevision.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2009, Yann Simon <yann.simon.fr@gmail.com>
+ * Copyright (C) 2009, 2013 Yann Simon <yann.simon.fr@gmail.com> and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -27,8 +27,8 @@ import org.eclipse.team.core.history.IFileRevision;
 import org.eclipse.team.internal.ui.synchronize.EditableSharedDocumentAdapter;
 
 /**
- * @author simon
- *
+ * Editable revision which supports listening to content changes by adding
+ * {@link IContentChangeListener}.
  */
 public class EditableRevision extends FileRevisionTypedElement implements
 		IEditableContent, IContentChangeNotifier {
@@ -133,22 +133,16 @@ public class EditableRevision extends FileRevisionTypedElement implements
 	}
 
 	public void setContent(byte[] newContent) {
-		if (newContent != null) {
-			modifiedContent = new byte[newContent.length];
-			System.arraycopy(newContent, 0, modifiedContent, 0,
-					newContent.length);
-		} else
-			modifiedContent = null;
+		modifiedContent = newContent;
 		fireContentChanged();
 	}
 
 	/**
-	 * @return the modified content
+	 * @return The modified content for reading. The data is owned by this
+	 *         class, do not modify it.
 	 */
 	public byte[] getModifiedContent() {
-		byte[] result = new byte[modifiedContent.length];
-		System.arraycopy(modifiedContent, 0, result, 0, modifiedContent.length);
-		return result;
+		return modifiedContent;
 	}
 
 	public Object getAdapter(Class adapter) {
