e7195c8edbab9324b002ced3d3c6faef08988b83
Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
2013-05-09 17:42:59 -0400

    Bug 407690 - Commit dialog should remember its last dialog size
    Change-Id: I72d5bbcbd5d4a7b6c102d2f74151cf8ffa71c214
    Signed-off-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitDialog.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitDialog.java
@@ -34,9 +34,9 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.Path;
-import org.eclipse.egit.core.Activator;
 import org.eclipse.egit.core.AdaptableFileTreeIterator;
 import org.eclipse.egit.core.internal.util.ResourceUtil;
+import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.UIPreferences;
 import org.eclipse.egit.ui.UIUtils;
 import org.eclipse.egit.ui.internal.CachedCheckboxTreeViewer;
@@ -327,6 +327,9 @@ public class CommitDialog extends TitleAreaDialog {
 
 	private static final String SHOW_UNTRACKED_PREF = "CommitDialog.showUntracked"; //$NON-NLS-1$
 
+	private static final String DIALOG_SETTINGS_SECTION_NAME = Activator
+			.getPluginId() + ".COMMIT_DIALOG_SECTION"; //$NON-NLS-1$
+
 	/**
 	 * A constant used for the 'commit and push button' button
 	 */
@@ -583,6 +586,17 @@ public class CommitDialog extends TitleAreaDialog {
 		Control help = super.createHelpControl(parent);
 		toolkit.adapt(help, false, false);
 		return help;
+	}
+
+	@Override
+	protected IDialogSettings getDialogBoundsSettings() {
+		IDialogSettings settings = Activator.getDefault().getDialogSettings();
+		IDialogSettings section = settings
+				.getSection(DIALOG_SETTINGS_SECTION_NAME);
+		if (section == null) {
+			section = settings.addNewSection(DIALOG_SETTINGS_SECTION_NAME);
+		}
+		return section;
 	}
 
 	/**
