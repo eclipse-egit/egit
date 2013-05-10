5ad8818600b9d96d3457cfae3e12035c4d7e010d
Stephen Elsemore <selsemore@collab.net>
2013-05-10 09:31:08 -0700

    Move commit-related buttons to "Commit Message" section of Staging View.
    Bug: 407760
    Change-Id: Ic62087e7220eb6b490d5e31ef9f563fd77cbc4e9
    Signed-off-by: Stephen Elsemore <selsemore@collab.net>
    
    Add presentation options to Staging View.  Follow up - Fix problem with
    Tree presentation when no Eclipse project exists.
    
    Bug: 407607
    Change-Id: I6509f2ee0f63fccf0391418cc3d41223e71c41de
    Signed-off-by: Stephen Elsemore <selsemore@collab.net>
    
    Add presentation options to Staging View.
    Follow up - Fix problems with "Tree" presentation when there is no
    Eclipse project.
    
    Bug: 407607
    Change-Id: I6509f2ee0f63fccf0391418cc3d41223e71c41de
    Signed-off-by: Stephen Elsemore <selsemore@collab.net>
    
    Add presentation options to Staging View.
    
    Fix tree structure if changes are done in different 2nd level folders
    under the same top level folder.
    
    Preserve tree expansion state across refresh.
    
    Bug: 407607
    Change-Id: I6509f2ee0f63fccf0391418cc3d41223e71c41de
    Signed-off-by: Stephen Elsemore <selsemore@collab.net>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/PluginPreferenceInitializer.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/PluginPreferenceInitializer.java
@@ -16,6 +16,7 @@ import java.io.File;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.egit.ui.internal.decorators.GitLightweightDecorator;
+import org.eclipse.egit.ui.internal.staging.StagingView;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jgit.util.FS;
 
@@ -96,6 +97,10 @@ public class PluginPreferenceInitializer extends AbstractPreferenceInitializer {
 		store.setDefault(UIPreferences.BLAME_IGNORE_WHITESPACE, false);
 		store.setDefault(UIPreferences.REMOTE_CONNECTION_TIMEOUT, 30 /* seconds */);
 		store.setDefault(UIPreferences.STAGING_VIEW_FILENAME_MODE, true);
+		store.setDefault(UIPreferences.STAGING_VIEW_PRESENTATION_UNSTAGED,
+				StagingView.PRESENTATION_FLAT);
+		store.setDefault(UIPreferences.STAGING_VIEW_PRESENTATION_STAGED,
+				StagingView.PRESENTATION_FLAT);
 		store.setDefault(UIPreferences.CLONE_WIZARD_STORE_SECURESTORE, false);
 		store.setDefault(UIPreferences.COMMIT_DIALOG_HISTORY_SIZE, 10);
 		store.setDefault(UIPreferences.CHECKOUT_PROJECT_RESTORE, true);
---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingFolderEntry.java
@@ -0,0 +1,115 @@
+/*******************************************************************************
+ * Copyright (C) 2013, Stephen Elsemore <selsemore@collab.net>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.staging;
+
+import java.io.File;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.egit.ui.internal.decorators.IProblemDecoratable;
+
+/**
+ * A staged/unstaged folder entry in the tree
+ */
+public class StagingFolderEntry implements IAdaptable, IProblemDecoratable {
+	private File file;
+	private IPath path;
+	private StagingFolderEntry parent;
+
+	/**
+	 * @param file
+	 */
+	public StagingFolderEntry(File file) {
+		super();
+		this.file = file;
+		path = new Path(file.getAbsolutePath());
+	}
+
+	/**
+	 * @return the container corresponding to the entry, if it exists in the
+	 *         workspace, null otherwise.
+	 */
+	public IContainer getContainer() {
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IContainer resource = root.getContainerForLocation(path);
+		return resource;
+	}
+
+	public int getProblemSeverity() {
+		IContainer container = getContainer();
+		if (container == null)
+			return SEVERITY_NONE;
+
+		try {
+			return container.findMaxProblemSeverity(IMarker.PROBLEM, true, IResource.DEPTH_ONE);
+		} catch (CoreException e) {
+			return SEVERITY_NONE;
+		}
+	}
+
+	public Object getAdapter(Class adapter) {
+		if (adapter == IResource.class || adapter == IContainer.class) {
+			return getContainer();
+		}
+		else if (adapter == IPath.class) {
+			return path;
+		}
+		return null;
+	}
+
+	/**
+	 * @return the path corresponding to the folder entry
+	 */
+	public IPath getPath() {
+		return path;
+	}
+
+	/**
+	 * @return the file corresponding to the folder entry
+	 */
+	public File getFile() {
+		return file;
+	}
+
+	/**
+	 * @return the parent folder entry
+	 */
+	public StagingFolderEntry getParent() {
+		return parent;
+	}
+
+	/**
+	 * @param parent
+	 */
+	public void setParent(StagingFolderEntry parent) {
+		this.parent = parent;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof StagingFolderEntry) {
+			return ((StagingFolderEntry) obj).getFile().getAbsolutePath()
+					.equals(file.getAbsolutePath());
+		}
+		return super.equals(obj);
+	}
+
+	@Override
+	public int hashCode() {
+		return file.getAbsolutePath().hashCode();
+	}
+
+}
