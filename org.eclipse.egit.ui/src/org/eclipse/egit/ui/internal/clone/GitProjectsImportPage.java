672170766ec1d8e176905af0976b92eb6bd041a6
Robin Stocker <robin@nibor.org>
2013-05-26 22:11:47 +0200

    Improve usability of project filter in import projects page
    * Don't match the part of the text that is the same between all elements
      (the path that was used to find the projects)
    * Set filter to include leading wildcard
    
    Bug: 363371
    Change-Id: I98254e32071d018a7866174e5ec32d2263d46cca

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/GitProjectsImportPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/clone/GitProjectsImportPage.java
@@ -180,9 +180,24 @@ public class GitProjectsImportPage extends WizardPage {
 				if (getCheckedProjects().contains(element))
 					return true;
 
-				return super.isElementVisible(viewer, element);
+				if (element instanceof ProjectRecord) {
+					ProjectRecord p = (ProjectRecord) element;
+					if (wordMatches(p.getProjectName()))
+						return true;
+					String projectPath = p.getProjectSystemFile().getParent();
+					if (projectPath.startsWith(lastPath)) {
+						String distinctPath = projectPath.substring(lastPath
+								.length());
+						return wordMatches(distinctPath);
+					} else {
+						return wordMatches(projectPath);
+					}
+				}
+
+				return false;
 			}
 		};
+		filter.setIncludeLeadingWildcard(true);
 
 		FilteredCheckboxTree filteredTree = new FilteredCheckboxTree(
 				listComposite, null, SWT.NONE, filter);
