ac09c1f55353ffbd66cb50ce27731e288f56a744
Laurent Goubet <laurent.goubet@obeo.fr>
2013-05-30 15:46:52 +0200

    Factorize comparison and synchronization code
    Use the same code for all comparison actions, making sure that
    a comparison launches a synchronization instead when there is more
    than a single resource to compare (either when the user selected
    more than one resource or when the selected resource is part of a
    larger logical model).
    
    This created dead code within CompareUtil and GitSynchronizeModel,
    which has been removed or made private.
    
    Change-Id: I4ae8abfc4dbe7ed772749afa80743278247585ee
    Signed-off-by: Laurent Goubet <laurent.goubet@obeo.fr>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/CompareWithWorkingTreeHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/CompareWithWorkingTreeHandler.java
@@ -16,12 +16,15 @@ import java.io.IOException;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.internal.CompareUtils;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.history.GitHistoryPage;
-import org.eclipse.egit.ui.internal.synchronize.GitModelSynchronize;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.ui.IWorkbenchPage;
@@ -43,19 +46,18 @@ public class CompareWithWorkingTreeHandler extends
 		Repository repository = getRepository(event);
 
 		try {
+			String dstRevCommit = commit.getId().getName();
 			IWorkbenchPage workBenchPage = HandlerUtil
 					.getActiveWorkbenchWindowChecked(event).getActivePage();
 			if (input instanceof IFile) {
-				IFile file = (IFile) input;
-				if (CompareUtils.canDirectlyOpenInCompare(file))
-					CompareUtils.compareWorkspaceWithRef(repository, file,
-							commit.getId().getName(), workBenchPage);
-				else
-					GitModelSynchronize.synchronizeModelWithWorkspace(file,
-							repository, commit.getName());
-			} else
-				CompareUtils.compareLocalWithRef(repository, (File) input,
-						commit.getId().getName(), workBenchPage);
+				IResource[] resources = new IResource[] { (IFile) input, };
+				CompareUtils.compare(resources, repository, Constants.HEAD,
+						dstRevCommit, true, workBenchPage);
+			} else {
+				IPath location = new Path(((File) input).getAbsolutePath());
+				CompareUtils.compare(location, repository, Constants.HEAD,
+						dstRevCommit, true, workBenchPage);
+			}
 		} catch (IOException e) {
 			Activator.handleError(
 					UIText.CompareWithRefAction_errorOnSynchronize, e, true);
