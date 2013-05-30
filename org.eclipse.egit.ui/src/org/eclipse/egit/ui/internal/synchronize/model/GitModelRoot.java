144495d8f8c6a416e8fe51507701e5f3970e615e
Laurent Goubet <laurent.goubet@obeo.fr>
2013-05-30 15:46:52 +0200

    Factorize comparison and synchronization code
    Use the same code for all comparison actions, making sure that
    a comparison launches a synchronization instead when there is more
    than a single resource to compare (either when the user selected
    more than one resource or when the selected resource is part of a
    larger logical model).
    
    This created dead code within CompareUtil and GitSynchronizeModel,
    which has been removed or made private.
    
    CQ: 7586
    Bug: 416808
    Change-Id: I4ae8abfc4dbe7ed772749afa80743278247585ee
    Signed-off-by: Laurent Goubet <laurent.goubet@obeo.fr>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/model/GitModelRoot.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/model/GitModelRoot.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2010, Dariusz Luksza <dariusz@luksza.org>
+ * Copyright (C) 2010, 2013 Dariusz Luksza <dariusz@luksza.org> and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -51,7 +51,6 @@ public class GitModelRoot {
 	 */
 	public void dispose() {
 		disposeOldChildren();
-		gsds.dispose();
 	}
 
 	private GitModelObject[] getChildrenImpl() {
