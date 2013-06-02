7bc77936c093fc1000e81274a11ca57cf49e2839
Christian Georgi <christian.georgi@sap.com>
2013-06-02 13:10:59 +0200

    Consider URLs without user name when importing project sets
    Project set files usually do not contain user names, but remote URLs
    in repo configs often do.  The opposite case might also happen, though
    more rarely: repo remote URL is anonymous, but project is not.
    Still consider these URLs (and thus the repos) the same.  
    
    Change-Id: I52690c6e804181d848b9dff3f8851fb84636fd85

--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/ProjectReferenceImporter.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/ProjectReferenceImporter.java
@@ -242,6 +242,13 @@ public class ProjectReferenceImporter {
 			URIish existingUrl = new URIish(remoteUrl);
 			if (existingUrl.equals(url))
 				return true;
+
+			// try URLs without user name, since often project sets contain
+			// anonymous URLs, and remote URL might be anonymous as well
+			URIish anonExistingUrl = existingUrl.setUser(null);
+			URIish anonUrl = url.setUser(null);
+			if (anonExistingUrl.equals(anonUrl))
+				return true;
 		}
 		return false;
 	}
