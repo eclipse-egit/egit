76a4e845508f6741a66c3675b49aafe6b38c7e5b
Robin Stocker <robin@nibor.org>
2013-07-10 23:14:51 +0200

    Remove no longer needed copy of ScmUrlImportDescription
    It was only needed for supporting Eclipse older than 3.6.
    
    Change-Id: I147e558652178dee4b00ce8898f50a02851a0544
    Signed-off-by: Robin Stocker <robin@nibor.org>

--- org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/GitURITest.java
+++ org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/GitURITest.java
@@ -14,12 +14,12 @@ package org.eclipse.egit.core.test;
 import static org.junit.Assert.assertEquals;
 
 import java.net.URI;
-import java.util.HashMap;
 
 import org.eclipse.egit.core.GitProjectSetCapability;
 import org.eclipse.egit.core.internal.GitURI;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.transport.URIish;
+import org.eclipse.team.core.ScmUrlImportDescription;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -105,95 +105,4 @@ public class GitURITest {
 				"1.0,git://git.eclipse.org/gitroot/equinox/rt.equinox.bundles.git,v20111010-1614,bundles/org.eclipse.equinox.http.jetty6",
 				refString);
 	}
-
-	// TODO remove this copy of org.eclipse.team.core.ScmUrlImportDescription
-	// when we drop support for Galileo
-	/**
-	 * Copy of org.eclipse.team.core.ScmUrlImportDescription to support tests until
-	 * we drop support for Galileo. DON'T USE OUTSIDE OF TEST BUNDLE!
-	 *
-	 * Describes how a bundle import will be executed. A bundle importer delegate
-	 * creates bundle import descriptions when it validates bundle manifests for
-	 * importing. The result, a set of bundle import descriptions is then passed to
-	 * TeamUI, which basing on the info from the descriptions instantiate and
-	 * initialize IScmUrlImportWizardPage pages. The pages can be used to alter the
-	 * default import configuration e.g. for bundles stored in a CVS repository the
-	 * user may want to check out HEAD rather than a specific version.
-	 * <p>
-	 * <strong>EXPERIMENTAL</strong>. This class has been added as part of a work in
-	 * progress. There is no guarantee that this API will work or that it will
-	 * remain the same. Please do not use this API without consulting with the Team
-	 * team.
-	 *
-	 * @since 3.6
-	 */
-	protected static class ScmUrlImportDescription {
-		private String url;
-		private String project;
-		private HashMap properties;
-
-		public ScmUrlImportDescription(String url, String project) {
-			this.url = url;
-			this.project = project;
-		}
-
-		/**
-		 * @return project name
-		 */
-		public String getProject() {
-			return project;
-		}
-
-		/**
-		 * SCM URL
-		 *
-		 * @return a string representation of the SCM URL
-		 */
-		public String getUrl() {
-			return url;
-		}
-
-		public URI getUri() {
-			return URI.create(url.replaceAll("\"", "")); //$NON-NLS-1$//$NON-NLS-2$
-		}
-
-		public void setUrl(String url) {
-			this.url = url;
-		}
-
-		/**
-		 * Sets or removes a client property.
-		 *
-		 * @param key
-		 *            property key
-		 * @param value
-		 *            property value or <code>null</code> to remove the property
-		 */
-		public synchronized void setProperty(String key, Object value) {
-			if (properties == null) {
-				properties = new HashMap();
-			}
-			if (value == null) {
-				properties.remove(key);
-			} else {
-				properties.put(key, value);
-			}
-
-		}
-
-		/**
-		 * Returns the specified client property, or <code>null</code> if none.
-		 *
-		 * @param key
-		 *            property key
-		 * @return property value or <code>null</code>
-		 */
-		public synchronized Object getProperty(String key) {
-			if (properties == null) {
-				return null;
-			}
-			return properties.get(key);
-		}
-	}
-
 }
