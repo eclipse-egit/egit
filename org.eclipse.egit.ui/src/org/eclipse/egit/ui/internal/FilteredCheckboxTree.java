cd80898e472c522f89c096449e9eeb8f21746494
Robin Stocker <robin@nibor.org>
2013-07-12 17:29:39 +0200

    Fix TODO for when we no longer support Eclipse 3.4
    Change-Id: I4b4b5637baf2b7074bcb29ecc0737d718c15660a
    Signed-off-by: Robin Stocker <robin@nibor.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/FilteredCheckboxTree.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/FilteredCheckboxTree.java
@@ -137,13 +137,7 @@ public class FilteredCheckboxTree extends FilteredTree {
 		return checkboxViewer;
 	}
 
-	/**
-	 * Overrides org.eclipse.ui.dialogs.FilteredTree#getRefreshJobDelay() in
-	 * Eclipse 3.5 and newer. No effect in eclipse 3.4
-	 *
-	 * @return refresh delay
-	 */
-	//	TODO: remove this javadoc when support for 3.4 is dropped
+	@Override
 	protected long getRefreshJobDelay() {
 		return FILTER_DELAY;
 	}
