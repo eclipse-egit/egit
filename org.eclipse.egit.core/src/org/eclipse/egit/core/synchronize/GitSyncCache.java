9cf111212cff830f021289d173b381dfcad58265
Mikaël Barbero <mikael.barbero@obeo.fr>
2013-07-30 11:55:23 +0200

    GitSyncObjectCache erroneously changes the sync state of resources
    When using the mapping context to determine the synchronization state
    of individual resources, the GitSyncObjectCache changes all other
    resources' state to "in-sync"... and never looks back. This causes
    very weird issues when using Team APIs, and a totally unspecified
    behavior such as a resource going from "MODIFIED" to "IN-SYNC" for no
    obvious reason.
    
    Bug: 415430
    Change-Id: Ib9d3686e87a5cb490a55b3a379b81b0fc0b1c3d9
    Also-by: Laurent Goubet <laurent.goubet@obeo.fr>
    Signed-off-by: Mikael Barbero <mikael.barbero@obeo.fr>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/GitSyncCache.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/synchronize/GitSyncCache.java
@@ -60,7 +60,7 @@ class GitSyncCache {
 
 		for (Entry<GitSynchronizeData, Collection<String>> entry : updateRequests
 				.entrySet()) {
-			cache.merge(getAllData(entry.getKey(), entry.getValue()));
+			cache.merge(getAllData(entry.getKey(), entry.getValue()), null);
 			m.worked(1);
 		}
 
@@ -176,13 +176,17 @@ class GitSyncCache {
 		return cache.get(repo.getDirectory());
 	}
 
-	public void merge(GitSyncCache newCache) {
+	public void merge(GitSyncCache newCache, Collection<String> updateRequests) {
 		for (Entry<File, GitSyncObjectCache> entry : newCache.cache.entrySet()) {
 			File key = entry.getKey();
-			if (cache.containsKey(key))
-				cache.get(key).merge(entry.getValue());
-			else
-				cache.put(key, entry.getValue());
+			// Bug #415430
+			if (updateRequests == null
+					|| (updateRequests.contains(key.getPath()))) {
+				if (cache.containsKey(key))
+					cache.get(key).merge(entry.getValue());
+				else
+					cache.put(key, entry.getValue());
+			}
 		}
 	}
 
