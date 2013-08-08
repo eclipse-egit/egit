5d1530c345cdc84a77c4548de7b1fa819e1e1e9b
Robin Stocker <robin@nibor.org>
2013-08-08 16:43:41 +0200

    [repoView] Ask whether to remove/delete repo when pressing Delete
    Before, it would always show the "Delete Repository..." dialog. Now it's
    more like other Eclipse behavior in that the Delete key also offers to
    just remove the selection without deleting anything.
    
    Bug: 395351
    Change-Id: I93e1efa4308a6a2939f82b258c63215fda5a63b4
    Signed-off-by: Robin Stocker <robin@nibor.org>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -4039,6 +4039,18 @@ public class UIText extends NLS {
 	public static String RemoveCommand_RemoveRepositoriesJob;
 
 	/** */
+	public static String RemoveOrDeleteRepositoryCommand_DeleteRepositoryButton;
+
+	/** */
+	public static String RemoveOrDeleteRepositoryCommand_DialogMessage;
+
+	/** */
+	public static String RemoveOrDeleteRepositoryCommand_DialogTitle;
+
+	/** */
+	public static String RemoveOrDeleteRepositoryCommand_RemoveFromViewButton;
+
+	/** */
 	public static String RenameBranchDialog_DialogMessage;
 
 	/** */
---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/RemoveOrDeleteRepositoryCommand.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Robin Stocker <robin@nibor.org> and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.repository.tree.command;
+
+import java.text.MessageFormat;
+import java.util.List;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.internal.UIText;
+import org.eclipse.egit.ui.internal.repository.tree.RepositoryNode;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jgit.lib.Repository;
+
+/**
+ * Removes or deletes a repository, depending on user input from dialog.
+ */
+public class RemoveOrDeleteRepositoryCommand extends RemoveCommand {
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		List<RepositoryNode> selectedNodes = getSelectedNodes(event);
+		if (selectedNodes.size() != 1)
+			return null;
+
+		Repository repository = selectedNodes.get(0).getObject();
+		String repositoryName = Activator.getDefault().getRepositoryUtil()
+				.getRepositoryName(repository);
+
+		String message = MessageFormat.format(
+				UIText.RemoveOrDeleteRepositoryCommand_DialogMessage, repositoryName);
+		String[] buttonLabels = {
+				UIText.RemoveOrDeleteRepositoryCommand_RemoveFromViewButton,
+				UIText.RemoveOrDeleteRepositoryCommand_DeleteRepositoryButton,
+				IDialogConstants.CANCEL_LABEL };
+		MessageDialog dialog = new MessageDialog(
+				getShell(event),
+				UIText.RemoveOrDeleteRepositoryCommand_DialogTitle,
+				null,
+				message,
+				MessageDialog.QUESTION, buttonLabels, 0);
+
+		int result = dialog.open();
+		if (result == 0) // Remove from View
+			super.removeRepository(event, false);
+		else if (result == 1) // Delete Repository...
+			super.removeRepository(event, true);
+		return null;
+	}
+}
