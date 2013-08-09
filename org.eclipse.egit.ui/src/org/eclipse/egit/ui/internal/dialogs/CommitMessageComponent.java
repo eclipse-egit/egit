65b5c754d3ed0ce3989ae2f839b12cdfa7011837
Robin Stocker <robin@nibor.org>
2013-08-09 22:23:46 +0200

    Fix NPE in CommitMessageComponent#setDefaults
    Repository does not yet have to be set to a non-null value, so check for
    null in setDefaults.
    
    Bug: 380782
    Change-Id: I0f788cf022dd970acd06b56e1e58d90ddd9200c9
    Signed-off-by: Robin Stocker <robin@nibor.org>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitMessageComponent.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitMessageComponent.java
@@ -7,7 +7,7 @@
  * Copyright (C) 2011, Mathias Kinzler <mathias.kinzler@sap.com>
  * Copyright (C) 2011, Jens Baumgart <jens.baumgart@sap.com>
  * Copyright (C) 2012, IBM Corporation (Markus Keller <markus_keller@ch.ibm.com>)
- * Copyright (C) 2012, Robin Stocker <robin@nibor.org>
+ * Copyright (C) 2012, 2013 Robin Stocker <robin@nibor.org>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -524,12 +524,13 @@ public class CommitMessageComponent {
 	 * Sets the defaults for change id and signed off
 	 */
 	public void setDefaults() {
-		createChangeId = repository.getConfig().getBoolean(
-				ConfigConstants.CONFIG_GERRIT_SECTION,
-				ConfigConstants.CONFIG_KEY_CREATECHANGEID, false);
+		if (repository != null)
+			createChangeId = repository.getConfig().getBoolean(
+					ConfigConstants.CONFIG_GERRIT_SECTION,
+					ConfigConstants.CONFIG_KEY_CREATECHANGEID, false);
 		signedOff = org.eclipse.egit.ui.Activator.getDefault()
-		.getPreferenceStore()
-		.getBoolean(UIPreferences.COMMIT_DIALOG_SIGNED_OFF_BY);
+				.getPreferenceStore()
+				.getBoolean(UIPreferences.COMMIT_DIALOG_SIGNED_OFF_BY);
 	}
 
 	/**
