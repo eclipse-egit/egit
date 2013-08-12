9d5439642e316b1ab95da454795f68613848013f
Robin Stocker <robin@nibor.org>
2013-08-12 21:26:43 +0200

    Replace use of internal GC class by command in GarbageCollectOperation
    Change-Id: Id6772d603237b378c97c21862605ee97d90aeec7
    Signed-off-by: Robin Stocker <robin@nibor.org>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/op/GarbageCollectOperation.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/op/GarbageCollectOperation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2012, Matthias Sohn <matthias.sohn@sap.com>
+ * Copyright (C) 2012, 2013 Matthias Sohn <matthias.sohn@sap.com> and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,9 +7,6 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
 package org.eclipse.egit.core.op;
-
-import java.io.IOException;
-import java.text.ParseException;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -19,9 +16,9 @@ import org.eclipse.core.runtime.jobs.ISchedulingRule;
 import org.eclipse.egit.core.Activator;
 import org.eclipse.egit.core.EclipseGitProgressTransformer;
 import org.eclipse.egit.core.internal.job.RuleUtil;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.internal.storage.file.FileRepository;
-import org.eclipse.jgit.internal.storage.file.GC;
 
 /**
  * Operation to garbage collect a git repository
@@ -41,15 +38,12 @@ public class GarbageCollectOperation implements IEGitOperation {
 	 * Execute garbage collection
 	 */
 	public void execute(IProgressMonitor monitor) throws CoreException {
-		GC gc = new GC((FileRepository) repository);
-		gc.setProgressMonitor(new EclipseGitProgressTransformer(
-				monitor));
+		Git git = new Git(repository);
+		EclipseGitProgressTransformer pm = new EclipseGitProgressTransformer(
+				monitor);
 		try {
-			gc.gc();
-		} catch (IOException e) {
-			throw new CoreException(new Status(IStatus.ERROR,
-					Activator.getPluginId(), e.getMessage(), e));
-		} catch (ParseException e) {
+			git.gc().setProgressMonitor(pm).call();
+		} catch (GitAPIException e) {
 			throw new CoreException(new Status(IStatus.ERROR,
 					Activator.getPluginId(), e.getMessage(), e));
 		}
