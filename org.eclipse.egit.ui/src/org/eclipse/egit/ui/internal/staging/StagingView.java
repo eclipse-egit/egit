9dc4e89feada0e446ec0ab1d25a13198f9f6e095
Robin Stocker <robin@nibor.org>
2013-08-14 14:25:10 +0200

    [stagingView] If "Expand All" was selected, keep expanding new nodes
    If a user always wants to see all the files, they can now click "Expand
    All" and new nodes will also be expanded (as opposed to being collapsed
    initially). As soon as the user collapses anything, this auto-expand
    mode is disabled again.
    
    Change-Id: I39b2eb19baf21a0df5bb6818dfa257c24cc6251f
    Signed-off-by: Robin Stocker <robin@nibor.org>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingView.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingView.java
@@ -84,6 +84,7 @@ import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.jface.preference.IPersistentPreferenceStore;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.util.LocalSelectionTransfer;
+import org.eclipse.jface.viewers.AbstractTreeViewer;
 import org.eclipse.jface.viewers.ContentViewer;
 import org.eclipse.jface.viewers.DecoratingLabelProvider;
 import org.eclipse.jface.viewers.IBaseLabelProvider;
@@ -93,8 +94,10 @@ import org.eclipse.jface.viewers.IOpenListener;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.ITreeViewerListener;
 import org.eclipse.jface.viewers.OpenEvent;
 import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.TreeExpansionEvent;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
@@ -498,6 +501,7 @@ public class StagingView extends ViewPart implements IShowInSource {
 				compareWith(event);
 			}
 		});
+		addListenerToDisableAutoExpandOnCollapse(unstagedViewer);
 
 		commitMessageSection = toolkit.createSection(
 				horizontalSashForm, ExpandableComposite.TITLE_BAR);
@@ -709,6 +713,7 @@ public class StagingView extends ViewPart implements IShowInSource {
 				compareWith(event);
 			}
 		});
+		addListenerToDisableAutoExpandOnCollapse(stagedViewer);
 
 		selectionChangedListener = new ISelectionListener() {
 			public void selectionChanged(IWorkbenchPart part,
@@ -830,6 +835,7 @@ public class StagingView extends ViewPart implements IShowInSource {
 				IAction.AS_PUSH_BUTTON) {
 			public void run() {
 				unstagedViewer.expandAll();
+				enableAutoExpand(unstagedViewer);
 			}
 		};
 		unstagedExpandAllAction.setImageDescriptor(UIIcons.EXPAND_ALL);
@@ -838,6 +844,7 @@ public class StagingView extends ViewPart implements IShowInSource {
 				IAction.AS_PUSH_BUTTON) {
 			public void run() {
 				unstagedViewer.collapseAll();
+				disableAutoExpand(unstagedViewer);
 			}
 		};
 		unstagedCollapseAllAction.setImageDescriptor(UIIcons.COLLAPSEALL);
@@ -861,6 +868,7 @@ public class StagingView extends ViewPart implements IShowInSource {
 				IAction.AS_PUSH_BUTTON) {
 			public void run() {
 				stagedViewer.expandAll();
+				enableAutoExpand(stagedViewer);
 			}
 		};
 		stagedExpandAllAction.setImageDescriptor(UIIcons.EXPAND_ALL);
@@ -869,6 +877,7 @@ public class StagingView extends ViewPart implements IShowInSource {
 				IAction.AS_PUSH_BUTTON) {
 			public void run() {
 				stagedViewer.collapseAll();
+				disableAutoExpand(stagedViewer);
 			}
 		};
 		stagedCollapseAllAction.setImageDescriptor(UIIcons.COLLAPSEALL);
@@ -890,6 +899,27 @@ public class StagingView extends ViewPart implements IShowInSource {
 		layout.marginLeft = 0;
 		layout.marginRight = 0;
 		return layout;
+	}
+
+	private static void addListenerToDisableAutoExpandOnCollapse(
+			TreeViewer treeViewer) {
+		treeViewer.addTreeListener(new ITreeViewerListener() {
+			public void treeCollapsed(TreeExpansionEvent event) {
+				disableAutoExpand(event.getTreeViewer());
+			}
+
+			public void treeExpanded(TreeExpansionEvent event) {
+				// Nothing to do
+			}
+		});
+	}
+
+	private static void enableAutoExpand(AbstractTreeViewer treeViewer) {
+		treeViewer.setAutoExpandLevel(AbstractTreeViewer.ALL_LEVELS);
+	}
+
+	private static void disableAutoExpand(AbstractTreeViewer treeViewer) {
+		treeViewer.setAutoExpandLevel(0);
 	}
 
 	/**
@@ -1909,6 +1939,10 @@ public class StagingView extends ViewPart implements IShowInSource {
 
 	private void expandPreviousExpandedAndPaths(Object[] previous,
 			TreeViewer viewer, Set<IPath> additionalPaths) {
+		// Auto-expand is on, so don't change expanded items
+		if (viewer.getAutoExpandLevel() == AbstractTreeViewer.ALL_LEVELS)
+			return;
+
 		Set<IPath> paths = new HashSet<IPath>(additionalPaths);
 		// Instead of just expanding the previous elements directly, also expand
 		// all parent paths. This makes it work in case of "re-folding" of
