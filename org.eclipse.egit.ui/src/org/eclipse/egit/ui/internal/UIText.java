9245d9b7ceab07369229fab47de8a3a61d0f00ba
Matthias Sohn <matthias.sohn@sap.com>
2013-09-06 10:20:45 +0200

    [stagingView] Show rebase buttons if rebase is in progress
    In order to enable showing a conflict warning when there are unresolved
    conflicts we need to enhance RepositoryState in JGit to provide
    additional states representing rebase with conflicts. This will be done
    in another change.
    
    Bug: 414930
    Change-Id: I54ebec9cbfb89b994e857cd8a20b2be6a55c65fb
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>
    Also-by: Mathias Kinzler <mathias.kinzler@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -4471,6 +4471,18 @@ public class UIText extends NLS {
 	public static String StagingView_ColumnLayout;
 
 	/** */
+	public static String StagingView_RebaseAbort;
+
+	/** */
+	public static String StagingView_RebaseContinue;
+
+	/** */
+	public static String StagingView_RebaseLabel;
+
+	/** */
+	public static String StagingView_RebaseSkip;
+
+	/** */
 	public static String StagingView_Refresh;
 
 	/** */
