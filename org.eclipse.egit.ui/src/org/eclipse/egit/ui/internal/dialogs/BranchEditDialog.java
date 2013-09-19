fd708307bde29511c9bb7221376c4cfd44866a59
Tomasz Zarna <tomasz.zarna@tasktop.com>
2013-09-19 12:57:52 +0200

    Provide a reusable composite for selecting (and editing) a branch
    * Allow to edit branches while fetching from Gerrit
    * Adopt the new composite in CheckoutDialog
    
    Change-Id: I282c748ffbc21fd4fc3fe2fa955c8dee4ffe3524
    Signed-off-by: Tomasz Zarna <tomasz.zarna@tasktop.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/BranchEditDialog.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/BranchEditDialog.java
@@ -1,47 +1,41 @@
 /*******************************************************************************
- * Copyright (c) 2011 SAP AG.
+ * Copyright (c) 2013 Tasktop Technologies
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Mathias Kinzler (SAP AG) - initial implementation
+ *    Tomasz Zarna (Tasktop Technologies) - initial implementation
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.dialogs;
 
 import org.eclipse.egit.ui.internal.UIText;
-import org.eclipse.jface.window.Window;
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Shell;
 
 /**
- * Dialog for checking out a branch, tag, or Reference.
- *
+ * Dialog for renaming or deleting a local branch.
  */
-public class CheckoutDialog extends BranchSelectionAndEditDialog {
-
+public class BranchEditDialog extends LocalBranchSelectionDialog {
 	/**
-	 * @param parentShell
-	 * @param repo
+	 * @param shell
+	 * @param repository
+	 * @param branchToMark
 	 */
-	public CheckoutDialog(Shell parentShell, Repository repo) {
-		super(parentShell, repo, SHOW_LOCAL_BRANCHES | SHOW_REMOTE_BRANCHES
-				| SHOW_TAGS | SHOW_REFERENCES | EXPAND_LOCAL_BRANCHES_NODE
-				| ALLOW_MULTISELECTION);
-	}
-
-	@Override
-	protected String getTitle() {
-		return UIText.CheckoutDialog_Title;
+	public BranchEditDialog(Shell shell, Repository repository,
+			String branchToMark) {
+		super(shell, repository, branchToMark);
 	}
 
 	@Override
 	protected void createButtonsForButtonBar(Composite parent) {
-		super.createButtonsForButtonBar(parent);
+		createRenameButton(parent);
+		createDeleteButton(parent);
 
-		getButton(Window.OK).setText(UIText.CheckoutDialog_OkCheckout);
+		createButton(parent, IDialogConstants.CANCEL_ID,
+				UIText.BranchSelectionAndEditDialog_OkClose, true);
 	}
-
 }
