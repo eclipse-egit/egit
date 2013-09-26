ee9319bbee8b273192e2e8888891e6cc8a892296
Tobias Pfeifer <to.pfeifer@web.de>
2013-09-26 19:51:47 +0200

    [RFC] Add a view for interactive rebase
    TODOs:
    - Preferences page
    - Use the toolbar
    - Tests
    - Link Selection to History View?
    
    Change-Id: I6df48dcc31a803d362860b2a2287f72036ea607c
    Signed-off-by: Tobias Pfeifer <to.pfeifer@web.de>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>
    Signed-off-by: Stefan Lay <stefan.lay@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -681,6 +681,60 @@ public class UIText extends NLS {
 	public static String RebaseCurrentRefCommand_ErrorGettingCurrentBranchMessage;
 
 	/** */
+	public static String RebaseInteractiveStepActionToolBarProvider_DeleteText;
+
+	/** */
+	public static String RebaseInteractiveStepActionToolBarProvider_EditText;
+
+	/** */
+	public static String RebaseInteractiveStepActionToolBarProvider_FixupText;
+
+	/** */
+	public static String RebaseInteractiveStepActionToolBarProvider_MoveDownText;
+
+	/** */
+	public static String RebaseInteractiveStepActionToolBarProvider_MoveUpText;
+
+	/** */
+	public static String RebaseInteractiveStepActionToolBarProvider_PickText;
+
+	/** */
+	public static String RebaseInteractiveStepActionToolBarProvider_RedoText;
+
+	/** */
+	public static String RebaseInteractiveStepActionToolBarProvider_RewordText;
+
+	/** */
+	public static String RebaseInteractiveStepActionToolBarProvider_SquashText;
+
+	/** */
+	public static String RebaseInteractiveStepActionToolBarProvider_UndoText;
+
+	/** */
+	public static String RebaseInteractiveView_HeadingAction;
+
+	/** */
+	public static String RebaseInteractiveView_HeadingCommitId;
+
+	/** */
+	public static String RebaseInteractiveView_HeadingMessage;
+
+	/** */
+	public static String RebaseInteractiveView_HeadingStatus;
+
+	/** */
+	public static String RebaseInteractiveView_NoSelection;
+
+	/** */
+	public static String RebaseInteractiveView_StatusCurrent;
+
+	/** */
+	public static String RebaseInteractiveView_StatusDone;
+
+	/** */
+	public static String RebaseInteractiveView_StatusTodo;
+
+	/** */
 	public static String RebaseResultDialog_Aborted;
 
 	/** */
@@ -3911,6 +3965,24 @@ public class UIText extends NLS {
 
 	/** */
 	public static String ImportProjectsSelectionInRepositoryRequired;
+
+	/** */
+	public static String InteractiveRebaseView_abortItem_text;
+
+	/** */
+	public static String InteractiveRebaseView_continueItem_text;
+
+	/** */
+	public static String InteractiveRebaseView_refreshItem_text;
+
+	/** */
+	public static String InteractiveRebaseView_skipItem_text;
+
+	/** */
+	public static String InteractiveRebaseView_startItem_text;
+
+	/** */
+	public static String InteractiveRebaseView_this_partName;
 
 	/** */
 	public static String LocalFileRevision_CurrentVersion;
