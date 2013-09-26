7256c3c954ffc552b29d9dd6ebbaae7a05d773bf
Tobias Pfeifer <to.pfeifer@sap.com>
2013-09-26 16:57:33 +0200

    [RFC] Make the rebase command stop after initialization
    Preparing the rebase todo lines in a callback method may be a long
    lasting user interaction. Doing this with the callback method
    prepareSteps(...) in the RebaseInteractiveHandler implies to make the
    thread that is calling back sleep until the user has finished
    preparation. When the rebase command is returning after initialization
    there is no need to make a thread sleep. The rebase todo file can be
    edited via writeRebaseTodoFile(...) of the Repository class. The rebase
    (interactive) has then to be invoked with PROCESS_STEPS in order to
    start the processing.  Note that the Method prepareSteps(...) of the
    RebaseInteractiveHandler will be called anyway just before starting to
    process the rebase todo file.
    
    Depends on JGit Change-Id: Change-Id:
    I2d6f0de82010ea6523fbce6fb4501e847bdcdddc
    
    Change-Id: I52a1fdae82308f3c7e4e4950b007e83807d0ae41
    Signed-off-by: Tobias Pfeifer <to.pfeifer@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/rebase/RebaseResultDialog.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/rebase/RebaseResultDialog.java
@@ -21,8 +21,6 @@ import java.util.Set;
 
 import org.eclipse.compare.CompareEditorInput;
 import org.eclipse.compare.CompareUI;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -30,16 +28,18 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.jobs.IJobChangeEvent;
+import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.egit.core.internal.FileChecker;
 import org.eclipse.egit.core.internal.FileChecker.CheckResult;
 import org.eclipse.egit.core.internal.FileChecker.CheckResultEntry;
+import org.eclipse.egit.core.internal.job.JobUtil;
+import org.eclipse.egit.core.op.RebaseOperation;
 import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.JobFamilies;
 import org.eclipse.egit.ui.UIPreferences;
 import org.eclipse.egit.ui.internal.UIText;
-import org.eclipse.egit.ui.internal.commands.shared.AbortRebaseCommand;
-import org.eclipse.egit.ui.internal.commands.shared.AbstractRebaseCommandHandler;
-import org.eclipse.egit.ui.internal.commands.shared.SkipRebaseCommand;
 import org.eclipse.egit.ui.internal.dialogs.CheckoutConflictDialog;
 import org.eclipse.egit.ui.internal.merge.GitMergeEditorInput;
 import org.eclipse.egit.ui.internal.merge.MergeModeDialog;
@@ -51,6 +51,7 @@ import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.window.Window;
+import org.eclipse.jgit.api.RebaseCommand.Operation;
 import org.eclipse.jgit.api.RebaseResult;
 import org.eclipse.jgit.api.RebaseResult.Status;
 import org.eclipse.jgit.dircache.DirCache;
@@ -158,6 +159,8 @@ public class RebaseResultDialog extends MessageDialog {
 			return UIText.RebaseResultDialog_FastForward;
 		case NOTHING_TO_COMMIT:
 			return UIText.RebaseResultDialog_NothingToCommit;
+		case INTERACTIVE_PREPARED:
+			return UIText.RebaseResultDialog_InteractivePrepared;
 		default:
 			throw new IllegalStateException(status.name());
 		}
@@ -554,33 +557,33 @@ public class RebaseResultDialog extends MessageDialog {
 				return;
 			} else if (skipCommitButton.getSelection()) {
 				// skip the rebase
-				SkipRebaseCommand skipCommand = new SkipRebaseCommand() {
-					protected Repository getRepository(ExecutionEvent event) {
-						return repo;
-					}
-				};
-				execute(skipCommand);
+				final RebaseOperation op = new RebaseOperation(repo,
+						Operation.SKIP);
+				JobUtil.scheduleUserJob(op,
+						UIText.RebaseResultDialog_JobNameSkipCommit,
+						JobFamilies.REBASE, new JobChangeAdapter() {
+							@Override
+							public void done(IJobChangeEvent event) {
+								show(op.getResult(), repo);
+							}
+						});
 			} else if (abortRebaseButton.getSelection()) {
 				// abort the rebase
-				AbortRebaseCommand abortCommand = new AbortRebaseCommand() {
-					protected Repository getRepository(ExecutionEvent event) {
-						return repo;
-					}
-				};
-				execute(abortCommand);
+				final RebaseOperation op = new RebaseOperation(repo,
+						Operation.ABORT);
+				JobUtil.scheduleUserJob(op,
+						UIText.RebaseResultDialog_JobNameAbortRebase,
+						JobFamilies.REBASE, new JobChangeAdapter() {
+							@Override
+							public void done(IJobChangeEvent event) {
+								show(op.getResult(), repo);
+							}
+						});
 			} else if (doNothingButton.getSelection()) {
 				// nothing
 			}
 		}
 		super.buttonPressed(buttonId);
-	}
-
-	private void execute(AbstractRebaseCommandHandler command) {
-		try {
-			command.execute(null);
-		} catch (ExecutionException e) {
-			Activator.showError(e.getMessage(), e);
-		}
 	}
 
 	private void createToggleButton(Composite parent) {
