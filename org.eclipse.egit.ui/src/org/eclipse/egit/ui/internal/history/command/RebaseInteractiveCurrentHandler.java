8f893f10c7cbb0fef387a9c1abe6259c7f657aa3
Tobias Pfeifer <to.pfeifer@web.de>
2013-09-26 19:38:57 +0200

    Enable to start RebaseOperation interactively
    - Refactor rebase command handlers
    - Add rebase interactive handler
    
    Change-Id: I9e717d01047b7c97be8f4bd2059f2e165560180d
    Signed-off-by: Tobias Pfeifer <to.pfeifer@web.de>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/RebaseInteractiveCurrentHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/RebaseInteractiveCurrentHandler.java
@@ -9,18 +9,20 @@
 package org.eclipse.egit.ui.internal.history.command;
 
 import org.eclipse.egit.core.op.RebaseOperation;
+import org.eclipse.egit.ui.internal.rebase.RebaseInteractiveHandler;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 
+
 /**
- * Executes the Rebase
+ * Executes the Rebase (interactively)
  */
-public class RebaseCurrentHandler extends AbstractRebaseHistoryCommandHandler {
+public class RebaseInteractiveCurrentHandler extends AbstractRebaseHistoryCommandHandler {
 
 	@Override
 	protected RebaseOperation createRebaseOperation(Repository repository,
 			Ref ref) {
-		return new RebaseOperation(repository, ref);
+		return new RebaseOperation(repository, ref,
+				RebaseInteractiveHandler.INSTANCE);
 	}
-
 }
