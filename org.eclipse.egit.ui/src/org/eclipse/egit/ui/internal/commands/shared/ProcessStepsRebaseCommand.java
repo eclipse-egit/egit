6d71587d444719d12235cc4c6e88a9d079d3a08c
Tobias Pfeifer <to.pfeifer@sap.com>
2013-09-26 17:12:31 +0200

    [RFC] Add ProcessStepsRebaseCommand for rebase interactive
    When a rebase interactive has been prepared it has to be invoked with
    the Operation PROCESS_STEPS.
    
    Change-Id: I3c399995266c4b3c6e2989e5ffd52002b3d0ebcb
    Signed-off-by: Tobias Pfeifer <to.pfeifer@sap.com>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commands/shared/ProcessStepsRebaseCommand.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2010 SAP AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Mathias Kinzler (SAP AG) - initial implementation
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.commands.shared;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.egit.core.op.RebaseOperation;
+import org.eclipse.egit.ui.internal.UIText;
+import org.eclipse.egit.ui.internal.rebase.RebaseInteracitveHandler;
+import org.eclipse.jgit.api.RebaseCommand.Operation;
+import org.eclipse.jgit.lib.Repository;
+
+/**
+ * Implements "Abort Rebase"
+ */
+public class ProcessStepsRebaseCommand extends AbstractRebaseCommandHandler {
+	/**
+	 * Default constructor
+	 */
+	public ProcessStepsRebaseCommand() {
+		super(UIText.ProcessStepsRebaseCommand_JobName,
+				UIText.ProcessStepsRebaseCommand_CancelDialogMessage);
+	}
+
+	@Override
+	protected RebaseOperation createRebaseOperation(Repository repository)
+			throws ExecutionException {
+		return new RebaseOperation(repository, Operation.PROCESS_STEPS,
+				RebaseInteracitveHandler.INSTANCE);
+	}
+}
