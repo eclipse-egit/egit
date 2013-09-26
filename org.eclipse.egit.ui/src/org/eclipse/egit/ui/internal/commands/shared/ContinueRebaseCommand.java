fe6bc77d6bd6809fba2de44dbf3635c44ccc053b
Tobias Pfeifer <to.pfeifer@sap.com>
2013-09-26 19:38:57 +0200

    [RFC] Add rebase interactive handler
    Change-Id: I9e717d01047b7c97be8f4bd2059f2e165560180d
    Signed-off-by: Tobias Pfeifer <to.pfeifer@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commands/shared/ContinueRebaseCommand.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commands/shared/ContinueRebaseCommand.java
@@ -13,6 +13,7 @@ package org.eclipse.egit.ui.internal.commands.shared;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.egit.core.op.RebaseOperation;
 import org.eclipse.egit.ui.internal.UIText;
+import org.eclipse.egit.ui.internal.rebase.RebaseInteracitveHandler;
 import org.eclipse.jgit.api.RebaseCommand.Operation;
 import org.eclipse.jgit.lib.Repository;
 
@@ -31,6 +32,7 @@ public class ContinueRebaseCommand extends AbstractRebaseCommandHandler {
 	@Override
 	protected RebaseOperation createRebaseOperation(Repository repository)
 			throws ExecutionException {
-		return new RebaseOperation(repository, Operation.CONTINUE);
+		return new RebaseOperation(repository, Operation.CONTINUE,
+				RebaseInteracitveHandler.INSTANCE);
 	}
 }
