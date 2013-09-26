7a8774973e070541ed14921a6bc0e73548e08993
Tobias Pfeifer <to.pfeifer@web.de>
2013-09-26 19:51:47 +0200

    Add a view for interactive rebase
    The new interactive rebase view allows to edit and execute the rebase
    plan. During plan execution it visualizes the current status.
    
    CQ: 7690
    Bug: 340967
    Change-Id: I6df48dcc31a803d362860b2a2287f72036ea607c
    Signed-off-by: Tobias Pfeifer <to.pfeifer@web.de>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>
    Signed-off-by: Stefan Lay <stefan.lay@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/RebaseInteractiveCurrentHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/RebaseInteractiveCurrentHandler.java
@@ -8,10 +8,21 @@
 
 package org.eclipse.egit.ui.internal.history.command;
 
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.egit.core.op.RebaseOperation;
+import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.internal.UIText;
+import org.eclipse.egit.ui.internal.commands.shared.AbstractRebaseCommandHandler;
 import org.eclipse.egit.ui.internal.rebase.RebaseInteractiveHandler;
+import org.eclipse.egit.ui.internal.rebase.RebaseInteractiveView;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.RepositoryState;
+import org.eclipse.jgit.revplot.PlotCommit;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 
 /**
@@ -19,7 +30,47 @@ import org.eclipse.jgit.lib.Repository;
  */
 public class RebaseInteractiveCurrentHandler extends AbstractRebaseHistoryCommandHandler {
 
-	@Override
+	public boolean isEnabled() {
+		final Repository repository = getRepository(getPage());
+		if (repository == null)
+			return false;
+		return repository.getRepositoryState().equals(RepositoryState.SAFE);
+	}
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+
+		PlotCommit commit = (PlotCommit) getSelection(getPage()).getFirstElement();
+		final Repository repository = getRepository(event);
+		if (repository == null)
+			return null;
+
+		String currentBranch = getCurrentBranch(repository);
+		final Ref ref = getRef(commit, repository, currentBranch);
+
+		String jobname = NLS.bind(
+				UIText.RebaseCurrentRefCommand_RebasingCurrentJobName,
+				currentBranch, ref.getName());
+		AbstractRebaseCommandHandler rebaseCurrentRef = new AbstractRebaseCommandHandler(
+				jobname, UIText.RebaseCurrentRefCommand_RebaseCanceledMessage) {
+			@Override
+			protected RebaseOperation createRebaseOperation(
+					Repository repository2) throws ExecutionException {
+				return new RebaseOperation(repository2, ref,
+						RebaseInteractiveHandler.INSTANCE);
+			}
+		};
+		rebaseCurrentRef.execute(repository);
+		try {
+			RebaseInteractiveView rebaseInteractiveView = (RebaseInteractiveView) HandlerUtil
+					.getActiveWorkbenchWindowChecked(event).getActivePage()
+					.showView(RebaseInteractiveView.VIEW_ID);
+			rebaseInteractiveView.setInput(repository);
+		} catch (PartInitException e) {
+			Activator.showError(e.getMessage(), e);
+		}
+		return null;
+	}
+
 	protected RebaseOperation createRebaseOperation(Repository repository,
 			Ref ref) {
 		return new RebaseOperation(repository, ref,
