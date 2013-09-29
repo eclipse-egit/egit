a333156e6d226d4e428a9ed773fcc2fc9cd9237e
Robin Stocker <robin@nibor.org>
2013-09-29 17:54:31 +0200

    Fix showing deleted file in history from commit viewer
    Also implement showing history for more than one file via
    HistoryPageInput while we're at it.
    
    Bug: 348601
    Change-Id: Ia68b472e488bc00d7e414d8b29d1abe45fde256a
    Signed-off-by: Robin Stocker <robin@nibor.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitFileDiffViewer.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitFileDiffViewer.java
@@ -399,16 +399,24 @@ public class CommitFileDiffViewer extends TableViewer {
 		IPath workTreePath = new Path(db.getWorkTree().getAbsolutePath());
 		IStructuredSelection selection = (IStructuredSelection) getSelection();
 		List<Object> elements = new ArrayList<Object>();
+		List<File> files = new ArrayList<File>();
 		for (Object selectedElement : selection.toList()) {
 			FileDiff fileDiff = (FileDiff) selectedElement;
-			IPath path = workTreePath.append(fileDiff.getNewPath());
+			IPath path = workTreePath.append(fileDiff.getPath());
 			IFile file = ResourceUtil.getFileForLocation(path);
 			if (file != null)
 				elements.add(file);
 			else
 				elements.add(path);
+			files.add(path.toFile());
 		}
-		return new ShowInContext(null, new StructuredSelection(elements));
+		HistoryPageInput historyPageInput = null;
+		if (!files.isEmpty()) {
+			historyPageInput = new HistoryPageInput(db,
+					files.toArray(new File[files.size()]));
+		}
+		return new ShowInContext(historyPageInput, new StructuredSelection(
+				elements));
 	}
 
 	private void openFileInEditor(String filePath) {
