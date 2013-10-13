c23ef9138f925a88582f251e2cc9be84cc0fc3fe
Robin Stocker <robin@nibor.org>
2013-10-13 15:42:54 +0200

    Warn when switching branches and a launch is running for the repo
    On certain cumbersome platforms (looking at you, Windows), a branch
    switch may fail when something is running that uses the files. In case
    of launch configurations inside Eclipse, we can detect that and warn the
    user about it.
    
    We could even offer to terminate it, but as termination may be async, we
    just offer continue/cancel. The user can also choose to not show any
    warnings again.
    
    The code for detecting this searches the ISourceContainer of the
    launches to see if one is for a project of the repository. This works
    e.g. for "Java Application" run configurations and also for servers
    (WST). I first tried ILaunchConfiguration#getMappedResources, but it
    does not return anything for servers. Maybe there is an easier way, but
    I couldn't find it.
    
    See bug for how to test this with servers.
    
    Bug: 419296
    Change-Id: I2fd7459b0cf342c13dda087745bd68dc1b6da4c3
    Signed-off-by: Robin Stocker <robin@nibor.org>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -2930,10 +2930,22 @@ public class UIText extends NLS {
 	public static String BranchOperationUI_CheckoutRemoteTrackingTitle;
 
 	/** */
+	public static String BranchOperationUI_Continue;
+
+	/** */
 	public static String BranchOperationUI_DetachedHeadTitle;
 
 	/** */
 	public static String BranchOperationUI_DetachedHeadMessage;
+
+	/** */
+	public static String BranchOperationUI_RunningLaunchDontShowAgain;
+
+	/** */
+	public static String BranchOperationUI_RunningLaunchMessage;
+
+	/** */
+	public static String BranchOperationUI_RunningLaunchTitle;
 
 	/** */
 	public static String BranchRenameDialog_Message;
@@ -3336,6 +3348,9 @@ public class UIText extends NLS {
 
 	/** */
 	public static String DialogsPreferencePage_RebaseCheckbox;
+
+	/** */
+	public static String DialogsPreferencePage_RunningLaunchOnCheckout;
 
 	/** */
 	public static String DialogsPreferencePage_ShowInitialConfigCheckbox;
