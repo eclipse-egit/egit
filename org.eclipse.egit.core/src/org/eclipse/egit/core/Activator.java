973cb9779a759fadd4409a1e76e33cb173a9814b
Kaloyan Raev <kaloyan.r@zend.com>
2013-10-17 17:12:11 +0300

    Project deletion fails if project is closed and contains .git
    There is pre-delete project listener that reconfigures the windows
    cache. This listener takes effect only if the project is open -
    RepositoryProvider.getProvider() returns null if the project is closed.
    
    This patch makes the listener to fallback to using java.io API if the
    project is closed.
    
    Bug: 419706
    Change-Id: Icddbad35abeb1cf4c2d3cf3c43cf5da282eaf1db
    Signed-off-by: Kaloyan Raev <kaloyan.r@zend.com>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/Activator.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/Activator.java
@@ -161,10 +161,21 @@ public class Activator extends Plugin implements DebugOptionsListener {
 					IResource resource = event.getResource();
 					if (resource instanceof IProject) {
 						IProject project = (IProject) resource;
-						if (RepositoryProvider.getProvider(project) instanceof GitProvider) {
-							IResource dotGit = project.findMember(Constants.DOT_GIT);
-							if (dotGit != null && dotGit.getType() == IResource.FOLDER)
+						if (project.isAccessible()) {
+							if (RepositoryProvider.getProvider(project) instanceof GitProvider) {
+								IResource dotGit = project
+										.findMember(Constants.DOT_GIT);
+								if (dotGit != null
+										&& dotGit.getType() == IResource.FOLDER)
+									GitProjectData.reconfigureWindowCache();
+							}
+						} else {
+							// bug 419706: project is closed - use java.io API
+							File location = project.getLocation().toFile();
+							File dotGit = new File(location, Constants.DOT_GIT);
+							if (dotGit.exists() && dotGit.isDirectory()) {
 								GitProjectData.reconfigureWindowCache();
+							}
 						}
 					}
 				}
