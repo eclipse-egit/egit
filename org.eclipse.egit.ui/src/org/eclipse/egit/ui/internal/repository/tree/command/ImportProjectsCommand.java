81199b93188e0b533a60263beebf8d703706282a
Johan Wannheden <johan.wannheden@paranor.ch>
2013-10-18 20:32:46 +0200

    Add dialog settings to project import dialog
    Improves user experience of the import projects wizard dialog by
    providing dialog settings for remembering bounds on subsequent
    invocation.
    
    Bug: 419631
    Change-Id: Ic3564d54d5af18da90dcd0e7fc2459695b0fc7e3
    Signed-off-by: Johan Wannheden <johan.wannheden@paranor.ch>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/ImportProjectsCommand.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/ImportProjectsCommand.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 SAP AG.
+ * Copyright (c) 2010, 2013 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,10 +14,12 @@ import java.util.List;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.clone.GitCreateProjectViaWizardWizard;
 import org.eclipse.egit.ui.internal.repository.tree.FolderNode;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
+import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Display;
@@ -63,7 +65,13 @@ public class ImportProjectsCommand extends
 
 		WizardDialog dlg = new WizardDialog(
 				getShell(event),
-				new GitCreateProjectViaWizardWizard(node.getRepository(), path));
+				new GitCreateProjectViaWizardWizard(node.getRepository(), path)) {
+			@Override
+			protected IDialogSettings getDialogBoundsSettings() {
+				// preserve dialog bounds
+				return Activator.getDefault().getDialogSettings();
+			}
+		};
 		dlg.setHelpAvailable(false);
 		dlg.open();
 
