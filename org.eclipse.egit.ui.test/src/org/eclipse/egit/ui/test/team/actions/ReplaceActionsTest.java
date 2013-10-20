48320498a2385e9b87344a3e6a7966e12e4628f3
Robin Stocker <robin@nibor.org>
2013-10-21 00:43:52 +0200

    Fix ReplaceActionsTest instability
    It always failed when run locally. Also changed it a bit to be easier to
    understand what is going on.
    
    Change-Id: I6ab52afd60bf9c35c5cee1e90ba485d6b42e2401
    Signed-off-by: Robin Stocker <robin@nibor.org>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/team/actions/ReplaceActionsTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/team/actions/ReplaceActionsTest.java
@@ -10,32 +10,23 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.test.team.actions;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.util.Date;
-import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
 
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.egit.core.op.TagOperation;
 import org.eclipse.egit.ui.JobFamilies;
 import org.eclipse.egit.ui.common.LocalRepositoryTestCase;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.test.ContextMenuHelper;
+import org.eclipse.egit.ui.test.JobJoiner;
 import org.eclipse.egit.ui.test.TestUtil;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.PersonIdent;
-import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.lib.TagBuilder;
-import org.eclipse.jgit.util.RawParseUtils;
+import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
@@ -50,60 +41,44 @@ import org.junit.runner.RunWith;
 public class ReplaceActionsTest extends LocalRepositoryTestCase {
 	private File repositoryFile;
 
-	private static ObjectId commitOfTag;
-
 	@Before
 	public void setup() throws Exception {
 		repositoryFile = createProjectAndCommitToRepository();
-		Repository repo = lookupRepository(repositoryFile);
-
-		TagBuilder tag = new TagBuilder();
-		tag.setTag("SomeTag");
-		tag.setTagger(RawParseUtils.parsePersonIdent(TestUtil.TESTAUTHOR));
-		tag.setMessage("I'm just a little tag");
-		tag.setObjectId(repo.resolve(repo.getFullBranch()),
-				Constants.OBJ_COMMIT);
-		commitOfTag = tag.getObjectId();
-		TagOperation top = new TagOperation(repo, tag, false);
-		top.execute(null);
-		touchAndSubmit(null);
 	}
 
 	@Test
 	public void testReplaceWithPrevious() throws Exception {
-		String newContent = getTestFileContent();
+		touchAndSubmit(null);
+		String initialContent = getTestFileContent();
 		String menuLabel = util
 				.getPluginLocalizedValue("replaceWithPreviousVersionAction.label");
 		clickReplaceWith(menuLabel);
-		bot.shell(UIText.DiscardChangesAction_confirmActionTitle).bot()
-				.button(IDialogConstants.OK_LABEL).click();
-		TestUtil.joinJobs(JobFamilies.DISCARD_CHANGES);
-		ResourcesPlugin.getWorkspace().getRoot()
-				.refreshLocal(IResource.DEPTH_INFINITE, null);
-		waitInUI();
-		String oldContent = getTestFileContent();
-		assertFalse(newContent.equals(oldContent));
+		SWTBotShell confirm = bot
+				.shell(UIText.DiscardChangesAction_confirmActionTitle);
+		executeReplace(confirm);
+		String replacedContent = getTestFileContent();
+		assertThat(replacedContent, not(initialContent));
 	}
 
 	@Test
 	public void testReplaceWithPreviousWithMerge() throws Exception {
 		Repository repo = lookupRepository(repositoryFile);
 		Git git = new Git(repo);
-		ObjectId masterId = repo.resolve("refs/heads/master");
-		Ref newBranch = git.checkout().setCreateBranch(true)
-				.setStartPoint(commitOfTag.name()).setName("toMerge").call();
-		ByteArrayInputStream bis = new ByteArrayInputStream(
-				"Modified".getBytes());
-		ResourcesPlugin.getWorkspace().getRoot().getProject(PROJ1)
-				.getFolder(FOLDER).getFile(FILE2)
-				.setContents(bis, false, false, null);
-		bis.close();
-		PersonIdent committer = new PersonIdent("COMMITTER", "a.c@d",
-				new Date(), TimeZone.getTimeZone("GMT-03:30"));
-		git.commit().setAll(true).setCommitter(committer)
-				.setMessage("To be merged").call();
-		git.merge().include(masterId).call();
-		String newContent = getTestFileContent();
+
+		git.checkout().setCreateBranch(true).setName("side").call();
+		touch(PROJ1, "folder/test.txt", "side");
+		RevCommit sideCommit = git.commit().setAll(true)
+				.setMessage("Side commit").call();
+
+		git.checkout().setName("master").call();
+		touch(PROJ1, "folder/test2.txt", "master");
+		git.commit().setAll(true).setMessage("Master commit").call();
+
+		git.merge().include(sideCommit).call();
+
+		String contentAfterMerge = getTestFileContent();
+		assertEquals("side", contentAfterMerge);
+
 		String menuLabel = util
 				.getPluginLocalizedValue("replaceWithPreviousVersionAction.label");
 		clickReplaceWith(menuLabel);
@@ -114,22 +89,19 @@ public class ReplaceActionsTest extends LocalRepositoryTestCase {
 		assertEquals(2, selectDialog.bot().table().rowCount());
 		selectDialog.close();
 		// we have closed, so nothing should have changed
-		String oldContent = getTestFileContent();
-		assertTrue(newContent.equals(oldContent));
+		String contentAfterClose = getTestFileContent();
+		assertEquals(contentAfterMerge, contentAfterClose);
 
 		clickReplaceWith(menuLabel);
 		bot.shell(UIText.DiscardChangesAction_confirmActionTitle).bot()
 				.button(IDialogConstants.OK_LABEL).click();
 		selectDialog = bot.shell(UIText.CommitSelectDialog_WindowTitle);
-		selectDialog.bot().table().select(1);
-		selectDialog.bot().button(IDialogConstants.OK_LABEL).click();
-		TestUtil.joinJobs(org.eclipse.egit.ui.JobFamilies.DISCARD_CHANGES);
-		oldContent = getTestFileContent();
-		assertFalse(newContent.equals(oldContent));
-		// cleanup: checkout again master and delete merged branch
-		git.checkout().setName("refs/heads/master").call();
-		git.branchDelete().setBranchNames(newBranch.getName()).setForce(true)
-				.call();
+		// Select first parent, which should be the master commit
+		selectDialog.bot().table().select(0);
+		executeReplace(selectDialog);
+
+		String replacedContent = getTestFileContent();
+		assertThat(replacedContent, not(contentAfterMerge));
 	}
 
 	private void clickReplaceWith(String menuLabel) {
@@ -137,5 +109,12 @@ public class ReplaceActionsTest extends LocalRepositoryTestCase {
 		getProjectItem(projectExplorerTree, PROJ1).select();
 		ContextMenuHelper.clickContextMenu(projectExplorerTree, "Replace With",
 				menuLabel);
+	}
+
+	private void executeReplace(SWTBotShell dialog) {
+		JobJoiner jobJoiner = JobJoiner.startListening(
+				JobFamilies.DISCARD_CHANGES, 30, TimeUnit.SECONDS);
+		dialog.bot().button(IDialogConstants.OK_LABEL).click();
+		jobJoiner.join();
 	}
 }
