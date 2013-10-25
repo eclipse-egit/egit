ff0f69bb8f87ba3998f1c4dc6ecb2ed3fbab8349
Stefan Lay <stefan.lay@sap.com>
2013-10-25 16:04:03 +0200

    Show dialog to edit the commit message in interactive rebase
    Change-Id: I542cbadf20ddde82bd7908a7a9c8b16f771d2701
    Signed-off-by: Stefan Lay <stefan.lay@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/rebase/RebaseInteractiveHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/rebase/RebaseInteractiveHandler.java
@@ -12,8 +12,12 @@ package org.eclipse.egit.ui.internal.rebase;
 
 import java.util.List;
 
+import org.eclipse.egit.ui.internal.UIText;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.window.Window;
 import org.eclipse.jgit.api.RebaseCommand.InteractiveHandler;
 import org.eclipse.jgit.lib.RebaseTodoLine;
+import org.eclipse.ui.PlatformUI;
 
 /**
  * Singleton {@link InteractiveHandler}
@@ -24,9 +28,23 @@ public enum RebaseInteractiveHandler implements InteractiveHandler {
 	 */
 	INSTANCE;
 
-	public String modifyCommitMessage(String commit) {
-		// TODO show a CommitMessageDialog to change the commit message
-		return commit;
+	public String modifyCommitMessage(final String commitMessage) {
+		final String[] result = new String[1];
+		result[0] = commitMessage;
+		PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
+
+			public void run() {
+				InputDialog dialog = new InputDialog(PlatformUI.getWorkbench()
+						.getDisplay().getActiveShell(),
+						UIText.RebaseInteractiveHandler_EditMessageDialogTitle,
+						UIText.RebaseInteractiveHandler_EditMessageDialogText,
+						commitMessage, null);
+				if (dialog.open() == Window.OK) {
+					result[0] = dialog.getValue();
+				}
+			}
+		});
+		return result[0];
 	}
 
 	public void prepareSteps(List<RebaseTodoLine> steps) {
