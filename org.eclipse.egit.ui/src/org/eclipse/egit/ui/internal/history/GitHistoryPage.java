249ace39b153476344157c4661a61bd6f157ad0f
Robin Stocker <robin@nibor.org>
2013-10-27 22:35:46 +0100

    [historyView] Add Show In > History to file diff pane
    This is useful for showing the history of a deleted file. Note that this
    depends on the fix for bug 418275 in Platform UI, which will be
    available in Eclipse 4.4. For older versions, the commit can be opened
    in the commit viewer, where Show In > History is available.
    
    Bug: 348601
    Change-Id: Iadc6505000b55a91617bc22de0ff54397d87b322
    Signed-off-by: Robin Stocker <robin@nibor.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitHistoryPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitHistoryPage.java
@@ -115,6 +115,7 @@ import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Link;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.team.ui.history.HistoryPage;
+import org.eclipse.team.ui.history.IHistoryView;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchPartSite;
@@ -124,12 +125,13 @@ import org.eclipse.ui.dialogs.PreferencesUtil;
 import org.eclipse.ui.editors.text.EditorsUI;
 import org.eclipse.ui.editors.text.TextSourceViewerConfiguration;
 import org.eclipse.ui.part.IShowInSource;
+import org.eclipse.ui.part.IShowInTargetList;
 import org.eclipse.ui.part.ShowInContext;
 import org.eclipse.ui.progress.IWorkbenchSiteProgressService;
 
 /** Graphical commit history viewer. */
 public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
-		ISchedulingRule, TableLoader, IShowInSource {
+		ISchedulingRule, TableLoader, IShowInSource, IShowInTargetList {
 
 	private static final int INITIAL_ITEM = -1;
 
@@ -2022,5 +2024,9 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 			return fileViewer.getShowInContext();
 		else
 			return null;
+	}
+
+	public String[] getShowInTargetIds() {
+		return new String[] { IHistoryView.VIEW_ID };
 	}
 }
