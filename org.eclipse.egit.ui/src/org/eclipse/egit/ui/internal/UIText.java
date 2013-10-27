fa43e84098fb8e3a73c2b7ee9f94a05704acacfa
Robin Stocker <robin@nibor.org>
2013-10-27 15:14:12 +0100

    Show tooltips for refs (history view, branch selection dialogs)
    Adds tooltips for HEAD, ORIG_HEAD, FETCH_HEAD and refs/stash with
    descriptions that explain what the refs are about.
    
    This change also fixes bug 420452.
    
    Bug: 420454
    Change-Id: Ia644bb62adfed327d4988ee07dc4652d5d6be58b
    Signed-off-by: Robin Stocker <robin@nibor.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -4549,6 +4549,21 @@ public class UIText extends NLS {
 	public static String GitActionContributor_Pull;
 
 	/** */
+	public static String GitLabelProvider_RefDescriptionFetchHead;
+
+	/** */
+	public static String GitLabelProvider_RefDescriptionHead;
+
+	/** */
+	public static String GitLabelProvider_RefDescriptionHeadSymbolic;
+
+	/** */
+	public static String GitLabelProvider_RefDescriptionOrigHead;
+
+	/** */
+	public static String GitLabelProvider_RefDescriptionStash;
+
+	/** */
 	public static String GitLabelProvider_UnableToRetrieveLabel;
 
 	/** */
---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoriesViewStyledCellLabelProvider.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Robin Stocker <robin@nibor.org> and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.repository;
+
+import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider;
+import org.eclipse.jface.viewers.ILabelProvider;
+
+/**
+ * Wraps {@link RepositoriesViewLabelProvider} in a
+ * {@link DelegatingStyledCellLabelProvider} to provide styled text support for
+ * use in tree or table viewers.
+ * <p>
+ * Also provides support for tool tips (see bug 236006 in platform which would
+ * make this unnecessary).
+ * <p>
+ * Also implements ILabelProvider for use with PatternFilter (see bug 258029 in
+ * platform which would make this unnecessary).
+ */
+public class RepositoriesViewStyledCellLabelProvider extends
+		DelegatingStyledCellLabelProvider implements ILabelProvider {
+
+	/** */
+	public RepositoriesViewStyledCellLabelProvider() {
+		super(new RepositoriesViewLabelProvider());
+	}
+
+	public String getText(Object element) {
+		return getStyledStringProvider().getStyledText(element).getString();
+	}
+
+	@Override
+	public String getToolTipText(Object element) {
+		return ((RepositoriesViewLabelProvider) getStyledStringProvider())
+				.getToolTipText(element);
+	}
+
+}
