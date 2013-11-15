412c2d45eade3ad7d0c0d0bcec1e1f26c2d189e5
Robin Stocker <robin@nibor.org>
2013-11-15 21:48:36 +0100

    Extract method to get DiffEntry corresponding to a changed path
    The next change wants to reuse this outside of RepositoryActionHandler.
    
    Change-Id: I1982dd4513972e6e6cc07f0a73853640f37e2626
    Signed-off-by: Robin Stocker <robin@nibor.org>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/CompareCoreUtils.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/CompareCoreUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, SAP AG
+ * Copyright (c) 2010, 2013 SAP AG and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -12,13 +12,23 @@
  *******************************************************************************/
 package org.eclipse.egit.core.internal;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.eclipse.core.resources.IEncodedStorage;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.egit.core.internal.util.ResourceUtil;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.diff.RenameDetector;
+import org.eclipse.jgit.diff.DiffEntry.ChangeType;
 import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.NullProgressMonitor;
+import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.treewalk.TreeWalk;
 
 /**
  * Utility class for compare-related functionality
@@ -70,4 +80,51 @@ public class CompareCoreUtils {
 		return charset;
 	}
 
+	/**
+	 * Get the {@link DiffEntry} corresponding to a change in file path. If the
+	 * file was renamed, the resulting {@link DiffEntry} will contain the old
+	 * path and blob ID. If the file was only added, null will be returned.
+	 *
+	 * @param repository
+	 * @param newPath
+	 *            path of the file in new commit
+	 * @param newCommit
+	 *            new commit
+	 * @param oldCommit
+	 *            old commit, e.g. parent commit of newCommit
+	 * @param objectReader
+	 *            reader for the repository
+	 * @return the diff entry corresponding to the change for path, or null if
+	 *         none could be found
+	 * @throws IOException
+	 */
+	public static DiffEntry getChangeDiffEntry(Repository repository, String newPath,
+			RevCommit newCommit, RevCommit oldCommit, ObjectReader objectReader)
+			throws IOException {
+		TreeWalk walk = new TreeWalk(objectReader);
+		walk.setRecursive(true);
+		walk.addTree(oldCommit.getTree());
+		walk.addTree(newCommit.getTree());
+
+		List<DiffEntry> entries = DiffEntry.scan(walk);
+
+		for (DiffEntry diff : entries)
+			if (diff.getChangeType() == ChangeType.MODIFY
+					&& newPath.equals(diff.getNewPath()))
+				return diff;
+
+		if (entries.size() < 2)
+			return null;
+
+		RenameDetector detector = new RenameDetector(repository);
+		detector.addAll(entries);
+		List<DiffEntry> renames = detector.compute(walk.getObjectReader(),
+				NullProgressMonitor.INSTANCE);
+		for (DiffEntry diff : renames)
+			if (diff.getChangeType() == ChangeType.RENAME
+					&& newPath.equals(diff.getNewPath()))
+				return diff;
+
+		return null;
+	}
 }
