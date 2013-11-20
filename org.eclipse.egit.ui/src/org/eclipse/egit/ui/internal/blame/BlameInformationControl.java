85d4ee32906e834230c88b76f1ed4da36e23e6b1
Robin Stocker <robin@nibor.org>
2013-11-20 22:06:04 +0100

    Blame annotations: Show start commit when opening editor
    Before, the commit that was shown was the blamed commit from the cursor
    position, which could be confusing to the user.
    
    To solve this, two things needed to be changed:
    
    The first time a selection changed event happens for the revision
    selection provider, we ignore it as it is for the "initial" selection.
    The confusing thing about this is that the user has not yet consciously
    selected a revision. To fix this, we now always ignore the very first
    selection.
    
    When opening an editor with annotation for a start commit (e.g. for the
    show annotations link in a hover), we now show this commit in the
    history view.
    
    Bug: 422100
    Change-Id: I5e1e529684f298b51fea5ccf99f18d901b874c5a
    Signed-off-by: Robin Stocker <robin@nibor.org>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/blame/BlameInformationControl.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/blame/BlameInformationControl.java
@@ -388,8 +388,8 @@ public class BlameInformationControl extends AbstractInformationControl
 			IWorkbenchPage page = PlatformUI.getWorkbench()
 					.getActiveWorkbenchWindow().getActivePage();
 			BlameOperation operation = new BlameOperation(
-					revision.getRepository(), storage, path,
-					parent.toObjectId(), getShell(), page, line);
+					revision.getRepository(), storage, path, parent,
+					getShell(), page, line);
 			JobUtil.scheduleUserJob(operation, UIText.ShowBlameHandler_JobName,
 					JobFamilies.BLAME);
 		} catch (IOException e) {
