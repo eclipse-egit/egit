275dbda766c9818a9efa25c6629fa0fc4122a920
Matthias Sohn <matthias.sohn@sap.com>
2013-11-21 14:42:11 +0100

    Use different icons for start/continue interactive rebase
    Change-Id: Ibe8f50570dcc0702c7ad1f7f0648bed6eff21011
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIIcons.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIIcons.java
@@ -345,7 +345,7 @@ public class UIIcons {
 		REBASE_CONTINUE = map("elcl16/continue.gif"); //$NON-NLS-1$
 		REBASE_SKIP = map("elcl16/skip.gif"); //$NON-NLS-1$
 		REBASE_ABORT = map("elcl16/progress_stop.gif"); //$NON-NLS-1$
-		REBASE_PROCESS_STEPS = map("elcl16/continue.gif"); //$NON-NLS-1$
+		REBASE_PROCESS_STEPS = map("elcl16/start.gif"); //$NON-NLS-1$
 		OVR_ERROR = map("ovr/error.png"); //$NON-NLS-1$
 		MERGE = map("obj16/merge.gif"); //$NON-NLS-1$
 		TAG_ANNOTATED = map("obj16/annotated-tag.gif"); //$NON-NLS-1$
