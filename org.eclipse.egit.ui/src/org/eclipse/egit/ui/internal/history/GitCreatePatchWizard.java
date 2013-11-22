9abb6ddf4fd14dba3bc54f406b51203ea1472c34
Matthias Sohn <matthias.sohn@sap.com>
2013-11-23 00:18:51 +0100

    Remove unused import
    Change-Id: I1bbd37e259f7b64999c941c40783147b4fd15b37
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitCreatePatchWizard.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitCreatePatchWizard.java
@@ -18,7 +18,6 @@ package org.eclipse.egit.ui.internal.history;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
