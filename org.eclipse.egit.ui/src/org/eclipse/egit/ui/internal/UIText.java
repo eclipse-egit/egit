485d59671012d96dedc80eaae30d3c6ba9a9af9e
Ben Hammen <hammenb@gmail.com>
2013-12-01 02:05:27 -0600

    [repoView] Add dropping multiple stashed commits
    Makes menu option to delete stashed commits available
    when more than one stash node is selected.
    
    Bug: 372883
    Change-Id: If198857271f1d1441f37054fc35e635c1acf563b
    Signed-off-by: Ben Hammen <hammenb@gmail.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -8,6 +8,7 @@
  * Copyright (C) 2012, 2013 Robin Stocker <robin@nibor.org>
  * Copyright (C) 2012, Laurent Goubet <laurent.goubet@obeo.fr>
  * Copyright (C) 2012, Gunnar Wagenknecht <gunnar@wagenknecht.org>
+ * Copyright (C) 2013, Ben Hammen <hammenb@gmail.com>
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -4867,7 +4868,10 @@ public class UIText extends NLS {
 	public static String StashCreateCommand_titleNoChanges;
 
 	/** */
-	public static String StashDropCommand_confirmMessage;
+	public static String StashDropCommand_confirmSingle;
+
+	/** */
+	public static String StashDropCommand_confirmMultiple;
 
 	/** */
 	public static String StashDropCommand_confirmTitle;
