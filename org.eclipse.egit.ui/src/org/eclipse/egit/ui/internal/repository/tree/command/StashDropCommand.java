136d3f7e1d079badf3b25a686c656e98b29e0040
Ben Hammen <hammenb@gmail.com>
2013-12-01 02:05:27 -0600

    [repoView] Add dropping multiple stashed commits
    Makes menu option to delete stashed commits available
    when more than one stash node is selected.
    
    Bug: 372883
    Change-Id: If198857271f1d1441f37054fc35e635c1acf563b
    Signed-off-by: Ben Hammen <hammenb@gmail.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/StashDropCommand.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/StashDropCommand.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- *  Copyright (c) 2012 GitHub Inc.
+ *  Copyright (C) 2012, 2013 GitHub Inc. and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -11,6 +11,8 @@
 package org.eclipse.egit.ui.internal.repository.tree.command;
 
 import java.text.MessageFormat;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -38,60 +40,89 @@ public class StashDropCommand extends
 		RepositoriesViewCommandHandler<StashedCommitNode> {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		List<StashedCommitNode> nodes = getSelectedNodes(event);
+		final List<StashedCommitNode> nodes = getSelectedNodes(event);
 		if (nodes.isEmpty())
 			return null;
 
-		StashedCommitNode node = nodes.get(0);
-		final Repository repo = node.getRepository();
+		final Repository repo = nodes.get(0).getRepository();
 		if (repo == null)
 			return null;
-		final int index = node.getIndex();
-		if (index < 0)
-			return null;
-		final RevCommit commit = node.getObject();
-		if (commit == null)
-			return null;
 
-		// Confirm deletion of selected tags
+		// Confirm deletion of selected nodes
 		final AtomicBoolean confirmed = new AtomicBoolean();
 		final Shell shell = getActiveShell(event);
 		shell.getDisplay().syncExec(new Runnable() {
 
 			public void run() {
+				String message;
+				if (nodes.size() > 1)
+					message = MessageFormat.format(
+							UIText.StashDropCommand_confirmMultiple,
+							Integer.toString(nodes.size()));
+				else
+					message = MessageFormat.format(
+							UIText.StashDropCommand_confirmSingle,
+							Integer.toString(nodes.get(0).getIndex()));
+
 				confirmed.set(MessageDialog.openConfirm(shell,
-						UIText.StashDropCommand_confirmTitle, MessageFormat
-								.format(UIText.StashDropCommand_confirmMessage,
-										Integer.toString(index))));
+						UIText.StashDropCommand_confirmTitle, message));
 			}
 		});
 		if (!confirmed.get())
 			return null;
 
-		final StashDropOperation op = new StashDropOperation(repo, index);
-		Job job = new Job(MessageFormat.format(
-				UIText.StashDropCommand_jobTitle, commit.name())) {
+		Job job = new Job(UIText.StashDropCommand_jobTitle) {
+
 			@Override
 			protected IStatus run(IProgressMonitor monitor) {
-				try {
-					op.execute(monitor);
-				} catch (CoreException e) {
-					Activator.logError(MessageFormat.format(
-							UIText.StashDropCommand_dropFailed, commit.name()),
-							e);
+				monitor.beginTask(UIText.StashDropCommand_jobTitle,
+						nodes.size());
+
+				// Sort by highest to lowest stash commit index.
+				// This avoids shifting problems that cause the indices of the
+				// selected nodes not match the indices in the repository
+				Collections.sort(nodes, new Comparator<StashedCommitNode>() {
+
+					public int compare(StashedCommitNode n1,
+							StashedCommitNode n2) {
+						return n1.getIndex() < n2.getIndex() ? 1 : -1;
+					}
+				});
+
+				for (StashedCommitNode node : nodes) {
+					final int index = node.getIndex();
+					if (index < 0)
+						return null;
+					final RevCommit commit = node.getObject();
+					if (commit == null)
+						return null;
+					final String stashName = node.getObject().getName();
+					final StashDropOperation op = new StashDropOperation(repo,
+							node.getIndex());
+					monitor.subTask(stashName);
+					try {
+						op.execute(monitor);
+					} catch (CoreException e) {
+						Activator.logError(MessageFormat.format(
+								UIText.StashDropCommand_dropFailed,
+								node.getObject().name()), e);
+					}
+					monitor.worked(1);
 				}
+				monitor.done();
 				return Status.OK_STATUS;
 			}
 
 			@Override
 			public boolean belongsTo(Object family) {
-				if (JobFamilies.STASH.equals(family))
+				if (family.equals(JobFamilies.STASH))
 					return true;
 				return super.belongsTo(family);
 			}
 		};
 		job.setUser(true);
-		job.setRule(op.getSchedulingRule());
+		job.setRule((new StashDropOperation(repo, nodes.get(0).getIndex()))
+				.getSchedulingRule());
 		job.schedule();
 		return null;
 	}
