a53b01598e5aca9e12d7a39d6e03b0e9caf1f8e7
Robin Stocker <robin@nibor.org>
2013-12-12 13:55:21 +0100

    Revert "Improve order of menu entries in "Compare With" menu"
    This reverts commit 286de3ec27a8bd10087e2002624b38be0762f303.
    
    Using the org.eclipse.ui.menus extension point instead of
    org.eclipse.ui.popupMenus triggers bug 422909 in Platform UI. The effect
    is that the editor context menu does not have the menu EGit entries.
    
    Bug: 422907
    Change-Id: Icb0c11ac6b487658cbb3cf1a979cfef519498fde
    Signed-off-by: Robin Stocker <robin@nibor.org>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CompareWithCommitAction.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (C) 2011, Mathias Kinzler <mathias.kinzler@sap.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.egit.ui.internal.actions;
+
+/**
+ * The "compare with ref" action. This action opens a Ref selection dialog to
+ * select a branch or tag and then a compare editor comparing the version.
+ */
+public class CompareWithCommitAction extends RepositoryAction {
+	/**
+	 *
+	 */
+	public CompareWithCommitAction() {
+		super(ActionCommands.COMPARE_WITH_COMMIT_ACTION,
+				new CompareWithCommitActionHandler());
+	}
+}
---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CompareWithIndexAction.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (C) 2009, Yann Simon <yann.simon.fr@gmail.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.egit.ui.internal.actions;
+
+/**
+ * The "compare with index" action. This action opens a diff editor comparing
+ * the file as found in the working directory and the version found in the index
+ * of the repository.
+ */
+public class CompareWithIndexAction extends RepositoryAction {
+	/**
+	 *
+	 */
+	public CompareWithIndexAction() {
+		super(ActionCommands.COMPARE_WITH_INDEX_ACTION, new CompareWithIndexActionHandler());
+	}
+}
