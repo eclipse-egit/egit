8e2b6e42c5cf59d476c33e91aa0ebf0f41f00b1c
Gerd Zanker <gerd.zanker@web.de>
2014-01-01 19:39:03 +0100

    Automated branching on task activation
    This change enables the automatic checkout of branches on task
    activation. On task deactivation, a project set file is stored in the
    task context containing all referenced repositories and their respective
    branches. On task activation the branch state is restored from the
    context.
    
    Rebased code to compile with master branch of egit
    
    Bug: 309578
    Change-Id: I03b927ab8701caa13ec62e536ebdb351d11439e6
    AlsoBy: Manuel Doninger <manuel.doninger@googlemail.com>
    Signed-off-by: Gerd Zanker <gerd.zanker@web.de>

--- org.eclipse.egit.mylyn.ui/src/org/eclipse/egit/internal/mylyn/ui/commit/TaskWorkflowProvider.java
+++ org.eclipse.egit.mylyn.ui/src/org/eclipse/egit/internal/mylyn/ui/commit/TaskWorkflowProvider.java
@@ -10,23 +10,23 @@
  *     Manuel Doninger <manuel@doninger.net>
  *     Benjamin Muskalla <benjamin.muskalla@tasktop.com>
  *     Thorsten Kamann <thorsten@kamann.info>
+ *     Steffen Pingel <steffen.pingel@tasktop.com>
  *******************************************************************************/
 package org.eclipse.egit.internal.mylyn.ui.commit;
 
 import org.eclipse.core.resources.IResource;
-import org.eclipse.egit.ui.ICommitMessageProvider;
-import org.eclipse.mylyn.context.core.ContextCore;
-import org.eclipse.mylyn.context.core.IInteractionContext;
+import org.eclipse.egit.ui.IBranchNameProvider;
+import org.eclipse.mylyn.internal.tasks.ui.util.TasksUiInternal;
 import org.eclipse.mylyn.tasks.core.ITask;
 import org.eclipse.mylyn.tasks.ui.TasksUi;
 import org.eclipse.mylyn.team.ui.TeamUiUtil;
 
-
 /**
- * Gets the active task and combines the description and title with
- * the commit message template defined in the preferences
+ * Gets the active task and combines the description and title with the commit
+ * message template defined in the preferences
  */
-public class MylynCommitMessageProvider implements ICommitMessageProvider {
+@SuppressWarnings("restriction")
+public class TaskWorkflowProvider implements IBranchNameProvider {
 
 	/**
 	 * @return the mylyn commit message template defined in the preferences
@@ -39,22 +39,43 @@ public class MylynCommitMessageProvider implements ICommitMessageProvider {
 		if (task == null)
 			return message;
 		boolean checkTaskRepository = true;
-		message = TeamUiUtil.getComment(checkTaskRepository, task, resources);
+		message = TeamUiUtil.getComment(checkTaskRepository, task,
+				resources);
+
 		return message;
 	}
 
 	/**
-	* @return the currently activated task or <code>null</code> if no task is
-	*         activated
-	*/
+	 * @return the currently activated task or <code>null</code> if no task is
+	 *         activated
+	 */
 	protected ITask getCurrentTask() {
 		return TasksUi.getTaskActivityManager().getActiveTask();
 	}
 
-	/**
-	* @return the activecontext or <code>null</code> if no activecontext exists
-	*/
-	protected IInteractionContext getActiveContext() {
-		return ContextCore.getContextManager().getActiveContext();
+	public String getBranchNameSuggestion() {
+		ITask task = getCurrentTask();
+		if (task == null)
+			return null;
+
+		String taskKey = task.getTaskKey();
+		if (taskKey == null)
+			taskKey = task.getTaskId();
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(TasksUiInternal.getTaskPrefix(task.getConnectorKind()));
+		sb.append(task.getTaskKey());
+		sb.append("-"); //$NON-NLS-1$
+		sb.append(task.getSummary());
+		return normalizeBranchName(sb.toString());
 	}
+
+	private String normalizeBranchName(String name) {
+		String normalized = name.replaceAll("\\s+", "_").replaceAll("[^\\w-]", ""); //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		if (normalized.length() > 30)
+			normalized = normalized.substring(0, 30);
+		return normalized;
+	}
+
 }
+
