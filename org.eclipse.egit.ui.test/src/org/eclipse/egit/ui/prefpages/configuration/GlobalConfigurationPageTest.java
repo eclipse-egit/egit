ad57d00fe75be5a7288ad0656dfedd1ee7e20be5
Matthias Sohn <matthias.sohn@sap.com>
2014-01-26 01:14:17 +0100

    Allow to create configuration entries containing dots
    This allows to e.g. persist a configuration entry
    [branch "2.0.0/master"]
    	foo = bar
    by specifying key "branch.2.0.0/master.foo" and value "bar".
    
    Bug: 382931
    Change-Id: I773980334771b3abef3c9beafbb9813627a79a65
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>
    Signed-off-by: Robin Stocker <robin@nibor.org>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/prefpages/configuration/GlobalConfigurationPageTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/prefpages/configuration/GlobalConfigurationPageTest.java
@@ -16,6 +16,7 @@ import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 import org.eclipse.egit.ui.common.EGitTestCase;
 import org.eclipse.egit.ui.internal.UIText;
@@ -237,13 +238,37 @@ public class GlobalConfigurationPageTest {
 		// ok: two dots
 		assertTrue(addDialog.bot().button(IDialogConstants.OK_LABEL)
 				.isEnabled());
+	}
+
+	@Test
+	public void testSubsectionWithDot() throws Exception {
+		preferencePage.bot()
+				.button(UIText.ConfigurationEditorComponent_AddButton).click();
+		SWTBotShell addDialog = bot
+				.shell(UIText.AddConfigEntryDialog_AddConfigTitle);
+		addDialog.activate();
+
+		// subsection containing a dot
+		String subsection = TESTSUBSECTION + "." + TESTNAME;
 		addDialog.bot().textWithLabel(UIText.AddConfigEntryDialog_KeyLabel)
-				.setText(
-						TESTSECTION + "." + TESTSUBSECTION + "." + TESTNAME
-								+ "." + TESTNAME);
-		// too many dots
-		assertTrue(!addDialog.bot().button(IDialogConstants.OK_LABEL)
+				.setText(TESTSECTION + "." + subsection + "." + TESTNAME);
+		addDialog.bot().textWithLabel(UIText.AddConfigEntryDialog_ValueLabel)
+				.setText("true");
+		assertTrue(addDialog.bot().button(IDialogConstants.OK_LABEL)
 				.isEnabled());
+
+		// close the dialog
+		addDialog.bot().button(IDialogConstants.OK_LABEL).click();
+		// close the editor
+		preferencePage.bot().button(IDialogConstants.OK_LABEL).click();
+
+		config.load();
+		assertTrue("Missing section", config.getSections()
+				.contains(TESTSECTION));
+		Set<String> subsections = config.getSubsections(TESTSECTION);
+		assertTrue("Missing subsection", subsections.contains(subsection));
+		assertEquals("Wrong value", "true",
+				config.getString(TESTSECTION, subsection, TESTNAME));
 		addDialog.close();
 	}
 
