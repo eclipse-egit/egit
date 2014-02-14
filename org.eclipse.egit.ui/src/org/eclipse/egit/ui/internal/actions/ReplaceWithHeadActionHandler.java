e7cfb1919b75d3af947e6c544c92da84321a11e8
Tomasz Zarna <tomasz.zarna@tasktop.com>
2014-02-14 11:25:54 +0100

    [WIP] 'Replace With' > 'HEAD Revision' shouldn't be available on untracked files
    Bug: 384602
    Change-Id: Ifb9800bdb8bc1196a390cc157607cba6ef38e092
    Signed-off-by: Tomasz Zarna <tomasz.zarna@tasktop.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/ReplaceWithHeadActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/ReplaceWithHeadActionHandler.java
@@ -11,8 +11,16 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.actions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
 import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.egit.core.internal.indexdiff.IndexDiffData;
 import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.Repository;
 
 /**
  * Replace with HEAD revision action handler
@@ -22,6 +30,46 @@ public class ReplaceWithHeadActionHandler extends DiscardChangesActionHandler {
 	@Override
 	protected String gatherRevision(ExecutionEvent event) {
 		return Constants.HEAD;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return super.isEnabled() && isAtLeastOneTracked();
+	}
+
+	private boolean isAtLeastOneTracked() {
+		Repository[] repositories = getRepositories();
+		IResource[] selectedResources = getSelectedResources();
+		List<IResource> notUntracked = new ArrayList<IResource>(
+				Arrays.asList(selectedResources));
+		for (Repository repository : repositories) {
+			IndexDiffData indexDiff = org.eclipse.egit.core.Activator
+					.getDefault().getIndexDiffCache()
+					.getIndexDiffCacheEntry(repository).getIndexDiff();
+			if (indexDiff != null) {
+				for (IResource selectedResource : selectedResources) {
+					String repoRelativePath = makeRepoRelative(repository,
+							selectedResource);
+					if (containsPrefix(indexDiff.getUntracked(),
+							repoRelativePath)) {
+						notUntracked.remove(selectedResource);
+					}
+				}
+			}
+		}
+		return !notUntracked.isEmpty();
+	}
+
+	private static String makeRepoRelative(Repository repo, IResource res) {
+		return Repository.stripWorkDir(repo.getWorkTree(), res.getLocation()
+				.toFile());
+	}
+
+	private boolean containsPrefix(Set<String> collection, String prefix) {
+		for (String path : collection)
+			if (path.startsWith(prefix))
+				return true;
+		return false;
 	}
 
 }
