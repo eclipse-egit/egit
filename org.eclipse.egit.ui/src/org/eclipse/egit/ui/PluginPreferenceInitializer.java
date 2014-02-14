266c2790091598e93324b88e1d530c6a0c127d00
Robin Stocker <robin@nibor.org>
2014-02-14 15:58:21 +0100

    [historyView] Ref shortening with "..." can be longer than original
    Also count the "..." for the max ref length. To compensate for
    shortening more than before, increase the default max length to 18.
    
    Before: "origin/stable-3..."
    After:  "origin/stable-3.3"
    
    Bug: 428201
    Change-Id: I5a79b98902268f445355aa60f7747f0d448bd259
    Signed-off-by: Robin Stocker <robin@nibor.org>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/PluginPreferenceInitializer.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/PluginPreferenceInitializer.java
@@ -105,8 +105,8 @@ public class PluginPreferenceInitializer extends AbstractPreferenceInitializer {
 		store.setDefault(UIPreferences.CLONE_WIZARD_STORE_SECURESTORE, false);
 		store.setDefault(UIPreferences.COMMIT_DIALOG_HISTORY_SIZE, 10);
 		store.setDefault(UIPreferences.CHECKOUT_PROJECT_RESTORE, true);
-		store.setDefault(UIPreferences.HISTORY_MAX_TAG_LENGTH, 15);
-		store.setDefault(UIPreferences.HISTORY_MAX_BRANCH_LENGTH, 15);
+		store.setDefault(UIPreferences.HISTORY_MAX_TAG_LENGTH, 18);
+		store.setDefault(UIPreferences.HISTORY_MAX_BRANCH_LENGTH, 18);
 		store.setDefault(UIPreferences.CLONE_WIZARD_SHOW_DETAILED_FAILURE_DIALOG, true);
 	}
 
