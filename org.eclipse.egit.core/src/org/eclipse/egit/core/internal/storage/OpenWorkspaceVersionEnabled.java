df90f993af83abcfe818e595daf61dba08fdb4c6
Konrad Kügler <swamblumat-eclipsebugs@yahoo.de>
2014-02-15 19:33:09 +0100

    Open workspace version from context menu of the compare editor
    Bug: 426614
    Change-Id: I93393f73d0ba246c45e2d9918b06bcefd9334e1f
    Signed-off-by: Konrad Kügler <swamblumat-eclipsebugs@yahoo.de>

---
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/storage/OpenWorkspaceVersionEnabled.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (C) 2014, Konrad Kügler <swamblumat-eclipsebugs@yahoo.de>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.core.internal.storage;
+
+import org.eclipse.jgit.lib.Repository;
+
+/**
+ * Provides the workspace version of a file. <br>
+ *
+ * This interface serves as a hint to GitCompareFileRevisionEditorInput to
+ * provide a "Open Workspace Version" action to the user.
+ */
+public interface OpenWorkspaceVersionEnabled {
+
+	/**
+	 * @return the repository containing this file
+	 */
+	public Repository getRepository();
+
+	/**
+	 * @return the file path relative to the repository's working directory
+	 */
+	public String getGitPath();
+
+}
