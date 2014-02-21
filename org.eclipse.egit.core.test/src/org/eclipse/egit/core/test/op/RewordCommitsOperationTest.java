434e4fab9ce347e554fede38067d76de2a0a2b02
Maik Schreiber <blizzy@blizzy.de>
2014-02-21 11:48:03 +0100

    Add ability to reword a commit directly from the history view.
    This adds a new menu item Modify > Reword to the context menu of
    the history view. It can be used to reword a commit, prompting for
    a commit message.
    
    An error will be displayed if the selected commit cannot be reached
    from the current HEAD commit. This is done so as to avoid the need
    to checkout another commit automatically before executing the reword
    procedure.
    
    CQ: 8055
    Bug: 429365
    Change-Id: I51acd62f5c088a539921fd0f297c5e6bded7aef5
    Signed-off-by: Maik Schreiber <blizzy@blizzy.de>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

---
+++ org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/op/RewordCommitsOperationTest.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ *  Copyright (c) 2014 Maik Schreiber
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *    Maik Schreiber - initial implementation
+ *******************************************************************************/
+package org.eclipse.egit.core.test.op;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.egit.core.op.RewordCommitOperation;
+import org.eclipse.egit.core.test.GitTestCase;
+import org.eclipse.egit.core.test.TestRepository;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.LogCommand;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RewordCommitsOperationTest extends GitTestCase {
+	private TestRepository testRepository;
+
+	private RevCommit commit;
+
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+		gitDir = new File(project.getProject().getLocationURI().getPath(),
+				Constants.DOT_GIT);
+		testRepository = new TestRepository(gitDir);
+		testRepository.connect(project.getProject());
+		testRepository.createInitialCommit("initial");
+
+		File file = testRepository.createFile(project.getProject(), "file");
+		commit = testRepository.addAndCommit(project.getProject(), file,
+				"a commit");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		testRepository.dispose();
+		super.tearDown();
+	}
+
+	@Test
+	public void reword() throws Exception {
+		RewordCommitOperation op = new RewordCommitOperation(
+				testRepository.getRepository(), commit, "new message");
+		op.execute(new NullProgressMonitor());
+
+		LogCommand log = new Git(testRepository.getRepository()).log();
+		RevCommit newCommit = log.call().iterator().next();
+		assertEquals("new message", newCommit.getFullMessage());
+	}
+}
