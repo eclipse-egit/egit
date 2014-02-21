292e17b0a3f759299d8c4e394c331703a23a2aae
Maik Schreiber <blizzy@blizzy.de>
2014-02-21 11:48:03 +0100

    Add ability to reword a commit directly from the history view.
    This adds a new menu item Modify > Reword to the context menu of
    the history view. It can be used to reword a commit, prompting for
    a commit message.
    
    An error will be displayed if the selected commit cannot be reached
    from the current HEAD commit. This is done so as to avoid the need
    to checkout another commit automatically before executing the reword
    procedure.
    
    Change-Id: I51acd62f5c088a539921fd0f297c5e6bded7aef5
    Signed-off-by: Maik Schreiber <blizzy@blizzy.de>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/CoreText.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/CoreText.java
@@ -301,6 +301,9 @@ public class CoreText extends NLS {
 	public static String RevertCommitOperation_reverting;
 
 	/** */
+	public static String RewordCommitOperation_rewording;
+
+	/** */
 	public static String BranchOperation_closingMissingProject;
 
 	/** */
