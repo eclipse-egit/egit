dec46bb6013b9e80cb832bd2efb0049d41aae09d
Philip Langer <planger@eclipsesource.com>
2014-02-28 11:48:19 +0100

    [wip] Let the ModelProviders compare their own file formats.
    Team allows ModelProviders to provide their own means of
    comparing their supervised files. This changes EGit's merge
    tool so that it uses this information instead of always
    computing its own "textual" differences between files.
    
    Change-Id: Ie38c9e6d8de0d9b6a8194c48ff0aa9e33489b243
    Signed-off-by: Laurent Goubet <laurent.goubet@obeo.fr>
    Signed-off-by: Axel Richard <axel.richard@obeo.fr>
    Signed-off-by: Philip Langer <planger@eclipsesource.com>
    Also-By: Axel Richard <axel.richard@obeo.fr>
    Also-By: Stefan Dirix <sdirix@eclipsesource.com>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/merge/LogicalModels.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/merge/LogicalModels.java
@@ -146,6 +146,8 @@ public final class LogicalModels {
 	@SuppressWarnings("unchecked")
 	public static <T> T findAdapter(Set<IResource> model, Class<T> adapterClass)
 			throws CoreException {
+		if (model.isEmpty())
+			return null;
 		final IResource[] modelArray = model
 				.toArray(new IResource[model.size()]);
 		final IModelProviderDescriptor[] descriptors = ModelProvider
@@ -194,12 +196,10 @@ public final class LogicalModels {
 				if (matchingResources.length > 0) {
 					final ModelProvider modelProvider = descriptor
 							.getModelProvider();
-					for (IResource resource : model) {
-						final ResourceMapping[] modelMappings = modelProvider
-								.getMappings(resource, mappingContext,
-										new NullProgressMonitor());
-						allMappings.addAll(Arrays.asList(modelMappings));
-					}
+					final ResourceMapping[] modelMappings = modelProvider
+							.getMappings(modelArray, mappingContext,
+									new NullProgressMonitor());
+					allMappings.addAll(Arrays.asList(modelMappings));
 				}
 			} catch (CoreException e) {
 				Activator.logError(e.getMessage(), e);
