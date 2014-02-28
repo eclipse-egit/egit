0f53fbfc487fd0d5e5003b5521b7ce4a1fefe9b2
Laurent Goubet <laurent.goubet@obeo.fr>
2014-02-28 11:31:46 +0100

    Use a workspace-aware merging strategy when working from EGit
    This introduces an Eclipse-specific sub-class of the
    RecursiveMerger that will make use of the mergers provided
    by ModelProviders instead of ignoring them to always use
    EGit's custom merging algorithms.
    
    Any file which does not have a specific merger (or which
    merger cannot be found, such as for files outside of the
    workspace) is still handled as it would by the
    RecursiveMerger.
    
    Signed-off-by: Laurent Goubet <laurent.goubet@obeo.fr>
    Change-Id: I1b27061ab215ebe600baef56f4e19a038c4b31a8

--- org.eclipse.egit.core/src/org/eclipse/egit/core/op/CherryPickOperation.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/op/CherryPickOperation.java
@@ -1,5 +1,7 @@
 /******************************************************************************
  *  Copyright (c) 2011 GitHub Inc.
+ *  Copyright (c) 2014, Obeo.
+ *  
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -22,6 +24,7 @@ import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
 import org.eclipse.egit.core.internal.CoreText;
 import org.eclipse.egit.core.internal.job.RuleUtil;
+import org.eclipse.egit.core.internal.merge.StrategyRecursiveModel;
 import org.eclipse.egit.core.internal.util.ProjectUtil;
 import org.eclipse.jgit.api.CherryPickCommand;
 import org.eclipse.jgit.api.CherryPickResult;
@@ -70,8 +73,9 @@ public class CherryPickOperation implements IEGitOperation {
 				pm.subTask(MessageFormat.format(
 						CoreText.CherryPickOperation_cherryPicking,
 						commit.name()));
-				CherryPickCommand command = new Git(repo).cherryPick().include(
-						commit.getId());
+				CherryPickCommand command = new Git(repo).cherryPick()
+						.include(commit.getId())
+						.setStrategy(new StrategyRecursiveModel());
 				try {
 					result = command.call();
 				} catch (GitAPIException e) {
