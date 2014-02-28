a886beb245d5191ec3a5f4d8c3ece07edbde26fe
Laurent Goubet <laurent.goubet@obeo.fr>
2014-02-28 11:31:46 +0100

    Use a workspace-aware merging strategy when working from EGit
    This introduces an Eclipse-specific sub-class of the
    RecursiveMerger that will make use of the mergers provided
    by ModelProviders instead of ignoring them to always use
    EGit's custom merging algorithms.
    
    Any file which does not have a specific merger (or which
    merger cannot be found, such as for files outside of the
    workspace) is still handled as it would by the
    RecursiveMerger.
    
    Signed-off-by: Laurent Goubet <laurent.goubet@obeo.fr>
    Change-Id: I1b27061ab215ebe600baef56f4e19a038c4b31a8

--- org.eclipse.egit.core/src/org/eclipse/egit/core/op/MergeOperation.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/op/MergeOperation.java
@@ -1,6 +1,7 @@
 /*******************************************************************************
  * Copyright (c) 2010, 2014 SAP AG and others.
  * Copyright (C) 2012, 2013 Tomasz Zarna <tzarna@gmail.com>
+ * Copyright (c) 2014, Obeo.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -29,6 +30,7 @@ import org.eclipse.core.runtime.jobs.ISchedulingRule;
 import org.eclipse.egit.core.Activator;
 import org.eclipse.egit.core.internal.CoreText;
 import org.eclipse.egit.core.internal.job.RuleUtil;
+import org.eclipse.egit.core.internal.merge.StrategyRecursiveModel;
 import org.eclipse.egit.core.internal.util.ProjectUtil;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.MergeCommand;
@@ -144,9 +146,10 @@ public class MergeOperation implements IEGitOperation {
 					merge.setCommit(commit.booleanValue());
 				if (squash != null)
 					merge.setSquash(squash.booleanValue());
-				if (mergeStrategy != null) {
+				if (mergeStrategy != null)
 					merge.setStrategy(mergeStrategy);
-				}
+				else
+					merge.setStrategy(new StrategyRecursiveModel());
 				try {
 					mergeResult = merge.call();
 					mymonitor.worked(1);
