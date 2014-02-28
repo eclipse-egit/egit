67edaaa0e576275d260c0ebb716461109cca9075
Axel Richard <axel.richard@obeo.fr>
2014-02-28 11:48:19 +0100

    [wip] Let the ModelProviders compare their own file formats.
    Team allows ModelProviders to provide their own means of
    comparing their supervised files. This changes EGit's merge
    tool so that it uses this information instead of always
    computing its own "textual" differences between files.
    
    Change-Id: Ie38c9e6d8de0d9b6a8194c48ff0aa9e33489b243
    Signed-off-by: Laurent Goubet <laurent.goubet@obeo.fr>
    Signed-off-by: Axel Richard <axel.richard@obeo.fr>
    Signed-off-by: Philip Langer <planger@eclipsesource.com>
    Also-By: Stefan Dirix <sdirix@eclipsesource.com>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/merge/DirCacheResourceVariantTreeProvider.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/merge/DirCacheResourceVariantTreeProvider.java
@@ -8,17 +8,18 @@
  *******************************************************************************/
 package org.eclipse.egit.core.internal.merge;
 
+// CHECKSTYLE:OFF
 import java.io.IOException;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
+import org.eclipse.egit.core.internal.storage.GitLocalResourceVariant;
 import org.eclipse.egit.core.internal.storage.IndexResourceVariant;
 import org.eclipse.egit.core.internal.util.ResourceUtil;
 import org.eclipse.jgit.dircache.DirCache;
 import org.eclipse.jgit.dircache.DirCacheEntry;
+import org.eclipse.jgit.lib.FileMode;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.team.core.variants.IResourceVariantTree;
 
@@ -29,6 +30,9 @@ import org.eclipse.team.core.variants.IResourceVariantTree;
  * Files that are not located within the workspace will be ignored and thus will
  * not be accessible through the trees created by this provider.
  * </p>
+ *
+ * @author <a href="mailto:laurent.goubet@obeo.fr">Laurent Goubet</a>
+ * @author <a href="mailto:laurent.delaigue@obeo.fr">Laurent Delaigue</a>
  */
 public class DirCacheResourceVariantTreeProvider implements
 		GitResourceVariantTreeProvider {
@@ -49,10 +53,14 @@ public class DirCacheResourceVariantTreeProvider implements
 	 * @param repository
 	 *            The repository which DirCache info we need to cache as
 	 *            IResourceVariantTrees.
+	 * @param useWorkspace
+	 *            Whether we should use local data instead of what's in the
+	 *            index for our side.
 	 * @throws IOException
 	 *             if we somehow cannot read the DirCache.
 	 */
-	public DirCacheResourceVariantTreeProvider(Repository repository)
+	public DirCacheResourceVariantTreeProvider(Repository repository,
+			boolean useWorkspace)
 			throws IOException {
 		final DirCache cache = repository.readDirCache();
 		final GitResourceVariantCache baseCache = new GitResourceVariantCache();
@@ -61,9 +69,10 @@ public class DirCacheResourceVariantTreeProvider implements
 
 		for (int i = 0; i < cache.getEntryCount(); i++) {
 			final DirCacheEntry entry = cache.getEntry(i);
-			final IPath path = new Path(entry.getPathString());
 			final IResource resource = ResourceUtil
-					.getResourceHandleForLocation(path);
+					.getResourceHandleForLocation(
+					repository, entry.getPathString(),
+					FileMode.fromBits(entry.getRawMode()) == FileMode.TREE);
 			// Resource variants only make sense for IResources. Do not consider
 			// files outside of the workspace or otherwise non accessible.
 			if (resource == null || resource.getProject() == null
@@ -72,15 +81,30 @@ public class DirCacheResourceVariantTreeProvider implements
 			}
 			switch (entry.getStage()) {
 			case DirCacheEntry.STAGE_0:
-				// Skipped on purpose (no conflict)
+				if (useWorkspace) {
+					sourceCache.setVariant(resource,
+							new GitLocalResourceVariant(resource));
+				} else {
+					sourceCache.setVariant(resource,
+							IndexResourceVariant.create(repository, entry));
+				}
+				baseCache.setVariant(resource,
+						IndexResourceVariant.create(repository, entry));
+				remoteCache.setVariant(resource,
+						IndexResourceVariant.create(repository, entry));
 				break;
 			case DirCacheEntry.STAGE_1:
 				baseCache.setVariant(resource,
 						IndexResourceVariant.create(repository, entry));
 				break;
 			case DirCacheEntry.STAGE_2:
-				sourceCache.setVariant(resource,
-						IndexResourceVariant.create(repository, entry));
+				if (useWorkspace) {
+					sourceCache.setVariant(resource,
+							new GitLocalResourceVariant(resource));
+				} else {
+					sourceCache.setVariant(resource,
+							IndexResourceVariant.create(repository, entry));
+				}
 				break;
 			case DirCacheEntry.STAGE_3:
 				remoteCache.setVariant(resource,
@@ -127,3 +151,4 @@ public class DirCacheResourceVariantTreeProvider implements
 		return roots;
 	}
 }
+// CHECKSTYLE:ON
