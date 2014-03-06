fabd3c1238884ff1454b8fb51292524eac2f9c4a
Laurent Goubet <laurent.goubet@obeo.fr>
2014-03-06 15:13:51 +0100

    Do not pass on an uninitialized scope to callers from GitScopeUtil
    This scope manager will be passed on as-is to Team, and
    some implementations of the Team APIs will rely on its
    proper initialization lest they fail in NPEs.
    
    Signed-off-by: Laurent Goubet <laurent.goubet@obeo.fr>
    Change-Id: I0025ac1a6c53eeba48ee4d35f230964ddd3072a0

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -5128,6 +5128,9 @@ public class UIText extends NLS {
 	public static String GitModelSynchronize_fetchGitDataJobName;
 
 	/** */
+	public static String GitModelSynchronize_fetchInterrupted;
+
+	/** */
 	public static String FetchChangeFromGerritCommand_noRepositorySelectedTitle;
 
 	/** */
