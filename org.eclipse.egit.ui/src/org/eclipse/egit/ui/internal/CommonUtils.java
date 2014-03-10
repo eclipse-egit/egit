357d64dcc5db6138c69333c07f1fb200b81d11a8
Konrad Kügler <swamblumat-eclipsebugs@yahoo.de>
2014-03-10 23:16:45 +0100

    Fix CommonUtils.STRING_ASCENDING_COMPARATOR for equal strings
    Equal strings resulted in -1, but should return 0. The same applies when
    comparing two empty strings.
    
    This became apparent since the Switch-To menu uses this comparator now
    (https://git.eclipse.org/r/#/c/22911/). This results in duplicated menu
    entries, e.g. if a branch appears in the reflog too.
    
    Change-Id: I9ba316e9eb078b4f97c8c9e9d9749aadea0b5068
    Signed-off-by: Konrad Kügler <swamblumat-eclipsebugs@yahoo.de>
    Signed-off-by: Robin Stocker <robin@nibor.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/CommonUtils.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/CommonUtils.java
@@ -43,8 +43,12 @@ public class CommonUtils {
 	 */
 	public static final Comparator<String> STRING_ASCENDING_COMPARATOR = new Comparator<String>() {
 		public int compare(String o1, String o2) {
-			if (o1.length() == 0)
-				return -1;
+			if (o1.length() == 0) {
+				if (o2.length() == 0)
+					return 0;
+				else
+					return -1;
+			}
 			if (o2.length() == 0)
 				return 1;
 
@@ -75,7 +79,10 @@ public class CommonUtils {
 					return result;
 			}
 
-			return -1;
+			if (o2PartsIterator.hasNext())
+				return -1;
+			else
+				return 0;
 		}
 	};
 
