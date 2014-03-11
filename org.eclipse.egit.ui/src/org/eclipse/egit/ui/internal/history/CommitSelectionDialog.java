b5228f4e4ce385578d385b0f10e4080e50afbaf6
Tomasz Zarna <tomasz.zarna@tasktop.com>
2014-03-11 09:42:43 +0100

    "Select a commit" dialog does not use relative dates
    Bug: 421317
    Change-Id: If62b13daf5e4c563d0c19c5b9dd1c0bcae614fb9
    Signed-off-by: Tomasz Zarna <tomasz.zarna@tasktop.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitSelectionDialog.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitSelectionDialog.java
@@ -108,6 +108,7 @@ public class CommitSelectionDialog extends TitleAreaDialog {
 				JFaceResources.getResources());
 		UIUtils.hookDisposal(main, resources);
 		table = new CommitGraphTable(main, null, resources);
+		table.setRelativeDate(GitHistoryPage.isShowingRelativeDates());
 		table.getTableView().addSelectionChangedListener(
 				new ISelectionChangedListener() {
 					public void selectionChanged(SelectionChangedEvent event) {
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitHistoryPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitHistoryPage.java
@@ -2013,7 +2013,7 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 		}
 	}
 
-	private boolean isShowingRelativeDates() {
+	static boolean isShowingRelativeDates() {
 		return Activator.getDefault().getPreferenceStore().getBoolean(UIPreferences.RESOURCEHISTORY_SHOW_RELATIVE_DATE);
 	}
 
