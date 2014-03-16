51702a1bfc17441373d80a69fce4d04107419e09
Michael Keppler <michael.keppler@gmx.de>
2014-03-16 08:32:24 +0100

    Remove delay in local repositories filter
    When trying to filter the directory with the Android open source
    repositories (around 1400 repos) updating the tree viewer takes 4.3
    seconds after typing something into the filter box. During that time the
    UI is frozen.
    
    Using only the leaf nodes for updating the filter, this drops down to
    0.2 seconds and the UI feels responsive.
    
    Change-Id: Iab65452ae62551c3f8e7a2ee594a5a238b802144
    Signed-off-by: Michael Keppler <michael.keppler@gmx.de>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositorySearchDialog.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositorySearchDialog.java
@@ -430,7 +430,7 @@ public class RepositorySearchDialog extends WizardPage {
 
 	private HashSet<String> getCheckedItems() {
 		HashSet<String> ret = new HashSet<String>();
-		for (Object item : fTreeViewer.getCheckedElements())
+		for (Object item : fTreeViewer.getCheckedLeafElements())
 			ret.add((String) item);
 		return ret;
 	}
