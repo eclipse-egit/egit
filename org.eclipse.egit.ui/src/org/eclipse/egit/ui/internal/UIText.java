aa9815e51e8294c3f91860ec351233dc30c87204
Robin Stocker <robin@nibor.org>
2014-03-29 16:09:28 +0100

    Reload GitDocument for Quick Diff in job instead of UI thread
    When doing a pull, commit or other operations that change refs, existing
    GitDocuments are reloaded. GitDocument is used for Quick Diff, which is
    active when the user uses Quick Diff, or when "Show Annotations" was
    called.
    
    Doing this possibly expensive operation (it can involve rename
    detection) on the UI thread blocks the UI, so do it in a background job
    instead. Note that RemoteRevisionQuickDiffProvider (for CVS) of
    eclipse.platform.team does it the same way.
    
    Bug: 413497
    Change-Id: Iccac6ebce9c63c2ccc95bda28134519d2f379e1b
    Signed-off-by: Robin Stocker <robin@nibor.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -384,6 +384,10 @@ public class UIText extends NLS {
 	/** */
 	public static String GitDocument_errorResolveQuickdiff;
 
+	public static String GitDocument_ReloadJobError;
+
+	public static String GitDocument_ReloadJobName;
+
 	/** */
 	public static String GitHistoryPage_AllChangesInRepoHint;
 
