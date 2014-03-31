5ec5a434dd88a48f55ebefbc82178d1c17907b72
Konrad Kügler <swamblumat-eclipsebugs@yahoo.de>
2014-03-31 23:54:48 +0200

    Unregister RebaseInteractiveView from plan on view disposal
    The view registers as a RebaseInteractivePlanChangeListener with the
    plan, but it never unregistered. After closing the view on a running
    interactive rebase and reopening it, it would notify the already
    disposed old view, too. This resulted in a "Widget disposed" exception,
    e.g. when using the buttons to move commits up or down in the rebase
    view.
    
    Also unregisters when another plan is shown.
    
    Change-Id: I67b582e1add240bb4490e26c538f6e30c6c5e6e6
    Signed-off-by: Konrad Kügler <swamblumat-eclipsebugs@yahoo.de>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/rebase/RebaseInteractiveView.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/rebase/RebaseInteractiveView.java
@@ -186,6 +186,8 @@ public class RebaseInteractiveView extends ViewPart implements
 				ISelectionService.class);
 		srv.removePostSelectionListener(RepositoriesView.VIEW_ID,
 				selectionChangedListener);
+		if (currentPlan != null)
+			currentPlan.removeRebaseInteractivePlanChangeListener(this);
 	}
 
 	@Override
@@ -642,6 +644,8 @@ public class RebaseInteractiveView extends ViewPart implements
 			return;
 		}
 
+		if (currentPlan != null)
+			currentPlan.removeRebaseInteractivePlanChangeListener(this);
 		currentPlan = RebaseInteractivePlan.getPlan(repository);
 		currentPlan.addRebaseInteractivePlanChangeListener(this);
 		form.setText(getRepositoryName(repository));
