ec605fec156463de9ab017ca7dbd737d6ef1583d
Matthias Sohn <matthias.sohn@sap.com>
2014-04-06 01:16:18 +0200

    Prevent race conditions when reloading StagingView
    Clearing and resetting paths asynchronously may lead to NPEs caused by
    race conditions.
    
    Bug: 432090
    Change-Id: Iddb640e4f7900da178956a4dc3de2b00dc766828
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingView.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingView.java
@@ -2046,7 +2046,7 @@ public class StagingView extends ViewPart implements IShowInSource {
 		if (form.isDisposed())
 			return;
 		if (repository == null) {
-			asyncExec(new Runnable() {
+			syncExec(new Runnable() {
 				public void run() {
 					clearRepository();
 				}
@@ -2059,7 +2059,7 @@ public class StagingView extends ViewPart implements IShowInSource {
 
 		final boolean repositoryChanged = currentRepository != repository;
 
-		asyncExec(new Runnable() {
+		syncExec(new Runnable() {
 
 			public void run() {
 				if (form.isDisposed())
@@ -2413,8 +2413,8 @@ public class StagingView extends ViewPart implements IShowInSource {
 			refsChangedListener.remove();
 	}
 
-	private void asyncExec(Runnable runnable) {
-		PlatformUI.getWorkbench().getDisplay().asyncExec(runnable);
+	private void syncExec(Runnable runnable) {
+		PlatformUI.getWorkbench().getDisplay().syncExec(runnable);
 	}
 
 }
