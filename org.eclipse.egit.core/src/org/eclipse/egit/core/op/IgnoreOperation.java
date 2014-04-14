b1ae09f7ad9443e1c6f80b58a3284582ecdaff6b
Matthias Sohn <matthias.sohn@sap.com>
2014-04-14 22:07:35 +0200

    Use correct syntax for auto ignoring directories
    Ignore patterns for ignoring directories should end with a trailing
    slash.
    
    Bug: 410872
    Change-Id: Ie9cef9d0ee875540af15130f2ca72e5b5163bd2c
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/op/IgnoreOperation.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/op/IgnoreOperation.java
@@ -123,10 +123,19 @@ public class IgnoreOperation implements IEGitOperation {
 		IPath parent = path.removeLastSegments(1);
 		IResource resource = ResourceUtil.getResourceForLocation(path);
 		IContainer container = null;
-		if (resource != null)
+		boolean isDirectory = false;
+		if (resource != null) {
+			isDirectory = resource instanceof IContainer;
 			container = resource.getParent();
+		} else
+			isDirectory = path.toFile().isDirectory();
 
-		String entry = "/" + path.lastSegment() + "\n"; //$NON-NLS-1$  //$NON-NLS-2$
+		StringBuilder b = new StringBuilder('/');
+		b.append(path.lastSegment());
+		if (isDirectory)
+			b.append('/');
+		b.append('\n');
+		String entry = b.toString();
 
 		if (container == null || container instanceof IWorkspaceRoot) {
 			Repository repository = RepositoryMapping.getMapping(
