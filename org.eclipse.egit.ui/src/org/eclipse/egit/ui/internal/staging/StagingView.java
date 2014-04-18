5994bcf715139750f42a49d5ae6b87c370884c2c
Konrad Kügler <swamblumat-eclipsebugs@yahoo.de>
2014-04-18 18:16:25 +0200

    [stagingView] Don't clear restored commit message state in the UI
    When viewing a repository in the Staging View for the first time after
    starting Eclipse, the IndexDiff data for this repository is usually not
    available yet. During StagingView.reload(), first the stored Staging
    View state is restored. Then enableCommitWidgets(false) is called,
    which disables everything, and worse, also clears the just restored
    inputs, including the commit message.
    
    When the IndexDiff for the repository becomes available, reload() will
    be called again.
    This change postpones restoring the view state to that time. This way
    the restored commit (and amend etc.) state remains intact. With the
    IndexDiff available, the inputs will not be cleared after restoring them
    (unless there are conflicts to be resolved).
    
    This change prepares for commit message restore after restarting
    Eclipse.
    
    Change-Id: I4fb24196bf07d4af9fa18b2a8390793e2057f6eb
    Signed-off-by: Konrad Kügler <swamblumat-eclipsebugs@yahoo.de>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingView.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingView.java
@@ -2114,7 +2114,8 @@ public class StagingView extends ViewPart implements IShowInSource {
 				rebaseContinueButton.setEnabled(rebaseContinueEnabled);
 
 				form.setText(StagingView.getRepositoryName(repository));
-				updateCommitMessageComponent(repositoryChanged, indexDiffAvailable);
+				updateCommitMessageComponent(repositoryChanged,
+						indexDiffAvailable);
 				enableCommitWidgets(indexDiffAvailable
 						&& indexDiff.getConflicting().isEmpty());
 				updateSectionText();
@@ -2185,13 +2186,19 @@ public class StagingView extends ViewPart implements IShowInSource {
 	}
 
 	void updateCommitMessageComponent(boolean repositoryChanged, boolean indexDiffAvailable) {
-		CommitHelper helper = new CommitHelper(currentRepository);
-		CommitMessageComponentState oldState = null;
-		if (repositoryChanged) {
+		if (repositoryChanged)
 			if (userEnteredCommmitMessage())
 				saveCommitMessageComponentState();
 			else
 				deleteCommitMessageComponentState();
+		if (!indexDiffAvailable)
+			return; // only try to restore the stored repo commit message if
+					// indexDiff is ready
+
+		CommitHelper helper = new CommitHelper(currentRepository);
+		CommitMessageComponentState oldState = null;
+		if (repositoryChanged
+				|| commitMessageComponent.getRepository() != currentRepository) {
 			oldState = loadCommitMessageComponentState();
 			commitMessageComponent.setRepository(currentRepository);
 			if (oldState == null)
@@ -2208,7 +2215,7 @@ public class StagingView extends ViewPart implements IShowInSource {
 				loadInitialState(helper);
 		amendPreviousCommitAction.setChecked(commitMessageComponent
 				.isAmending());
-		amendPreviousCommitAction.setEnabled(indexDiffAvailable && helper.amendAllowed());
+		amendPreviousCommitAction.setEnabled(helper.amendAllowed());
 		updateMessage();
 	}
 
