dde4d22f77c251d03ddbb8f63c0793c9f3f3d888
Tomasz Zarna <tomasz.zarna@tasktop.com>
2014-06-09 14:14:06 +0200

    fix compiler warnings in RepositoryStatisticsPage
    restricted usage and empty, undocumented block
    
    Change-Id: Id2347b78649765987c05176dfceceaf3d4faa215
    Signed-off-by: Tomasz Zarna <tomasz.zarna@tasktop.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoryStatisticsPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoryStatisticsPage.java
@@ -10,14 +10,15 @@
  *****************************************************************************/
 package org.eclipse.egit.ui.internal.repository;
 
-import java.io.IOException;
 import java.text.NumberFormat;
+import java.util.Properties;
 
+import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.internal.UIText;
+import org.eclipse.jgit.api.GarbageCollectCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.internal.storage.file.FileRepository;
-import org.eclipse.jgit.internal.storage.file.GC;
-import org.eclipse.jgit.internal.storage.file.GC.RepoStatistics;
 import org.eclipse.jgit.util.SystemReader;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
@@ -48,6 +49,7 @@ public class RepositoryStatisticsPage extends PropertyPage {
 				.getLocale());
 	}
 
+	@SuppressWarnings("boxing")
 	protected Control createContents(Composite parent) {
 		Table table = new Table(parent, SWT.MULTI | SWT.BORDER
 				| SWT.FULL_SELECTION);
@@ -63,46 +65,47 @@ public class RepositoryStatisticsPage extends PropertyPage {
 				.getAdapter(Repository.class);
 		if (repo == null)
 			return table;
-		if (repo instanceof FileRepository) {
-			GC gc = new GC((FileRepository) repo);
-			try {
-				RepoStatistics stats = gc.getStatistics();
+		Git git = new Git(repo);
+		GarbageCollectCommand gc = git.gc();
+		try {
+			Properties stats = gc.getStatistics();
 
-				table.setLinesVisible(true);
-				table.setHeaderVisible(true);
-				GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
-				data.heightHint = 200;
-				table.setLayoutData(data);
+			table.setLinesVisible(true);
+			table.setHeaderVisible(true);
+			GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
+			data.heightHint = 200;
+			table.setLayoutData(data);
 
-				TableItem item = new TableItem(table, SWT.NONE);
-				item.setText(0, UIText.RepositoryStatistics_NrOfObjects);
-				item.setText(1, bigIntFmt.format(stats.numberOfLooseObjects));
-				item.setText(2, bigIntFmt.format(stats.numberOfPackedObjects));
+			TableItem item = new TableItem(table, SWT.NONE);
+			item.setText(0, UIText.RepositoryStatistics_NrOfObjects);
+			item.setText(1, bigIntFmt.format(stats.get("numberOfLooseObjects"))); //$NON-NLS-1$
+			item.setText(2,
+					bigIntFmt.format(stats.get("numberOfPackedObjects"))); //$NON-NLS-1$
 
-				item = new TableItem(table, SWT.NONE);
-				item.setText(0, UIText.RepositoryStatistics_NrOfPackfiles);
-				item.setText(2, bigIntFmt.format(stats.numberOfPackFiles)
-						.toString());
+			item = new TableItem(table, SWT.NONE);
+			item.setText(0, UIText.RepositoryStatistics_NrOfPackfiles);
+			item.setText(2,
+					bigIntFmt.format(stats.get("numberOfPackFiles"))); //$NON-NLS-1$
 
-				item = new TableItem(table, SWT.NONE);
-				item.setText(0, UIText.RepositoryStatistics_NrOfRefs);
-				item.setText(1, bigIntFmt.format(stats.numberOfLooseRefs)
-						.toString());
-				item.setText(2, bigIntFmt.format(stats.numberOfPackedRefs)
-						.toString());
+			item = new TableItem(table, SWT.NONE);
+			item.setText(0, UIText.RepositoryStatistics_NrOfRefs);
+			item.setText(1,
+					bigIntFmt.format(stats.get("numberOfLooseRefs"))); //$NON-NLS-1$
+			item.setText(2, bigIntFmt.format(stats.get("numberOfPackedRefs")));//$NON-NLS-1$
 
-				item = new TableItem(table, SWT.NONE);
-				item.setText(0,
-						UIText.RepositoryStatistics_SpaceNeededOnFilesystem);
-				item.setText(1, describeSize(stats.sizeOfLooseObjects));
-				item.setText(2, describeSize(stats.sizeOfPackedObjects));
+			item = new TableItem(table, SWT.NONE);
+			item.setText(0, UIText.RepositoryStatistics_SpaceNeededOnFilesystem);
+			item.setText(1,
+					describeSize((Long) stats.get("sizeOfLooseObjects"))); //$NON-NLS-1$
+			item.setText(2,
+					describeSize((Long) stats.get("sizeOfPackedObjects"))); //$NON-NLS-1$
 
-				for (int i = 0; i < titles.length; i++) {
-					table.getColumn(i).pack();
-				}
-				parent.pack();
-			} catch (IOException e) {
+			for (int i = 0; i < titles.length; i++) {
+				table.getColumn(i).pack();
 			}
+			parent.pack();
+		} catch (GitAPIException e) {
+			Activator.handleError(e.getMessage(), e, false);
 		}
 		return table;
 	}
