2ba72a984f3f92754840139096eff03290317469
Andrey Loskutov <loskutov@gmx.de>
2014-07-30 11:15:09 +0200

    Fix IteratorService performance on big repositories/workspaces
    IWorkspaceRoot.findContainersForLocationURI(URI) method performs very
    poor on big repositories and workspaces, and gets even worse with lot of
    links in the workspace too.
    
    The fix tries first to get the direct workspace resources map hit by
    using IWorkspaceRoot.getContainerForLocation(IPath) and only if this
    fails, fail over to the slow findContainersForLocationURI method.
    
    This works very fast for almost all cases (constant execution time) and
    adds only a small overhead if the matching project/folder is contained
    in a *closed* project which overlaps location of another *opened*
    project.
    
    Bug: 440722
    Change-Id: Ia6eda95f082111355cf96209db588664c59476b6
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/IteratorService.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/IteratorService.java
@@ -14,6 +14,7 @@ import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.treewalk.WorkingTreeIterator;
@@ -65,14 +66,41 @@ public class IteratorService {
 		if (!file.isDirectory())
 			throw new IllegalArgumentException(
 					"file " + file.getAbsolutePath() + " is no directory"); //$NON-NLS-1$//$NON-NLS-2$
+
+		// fast path to get desired container
+		IContainer resource = root.getContainerForLocation(new Path(file
+				.getAbsolutePath()));
+
+		// Eclipse has no mappings for this path
+		if (resource == null)
+			return null;
+
+		if (isValid(resource))
+			return resource;
+
+		// the case where the project is either closed or resource is linked:
+		// try to find *all* possible candidates (hello overlapping projects!)
+		// The code below performs exceptionally slow because it tries to find
+		// all linked resources too
 		final IContainer[] containers = root.findContainersForLocationURI(file
 				.toURI());
 		for (IContainer container : containers)
-			if (container.isAccessible()
-					&& !container.isLinked(IResource.CHECK_ANCESTORS)
-					&& isProjectSharedWithGit(container))
+			if (isValid(container))
 				return container;
 		return null;
+	}
+
+	/**
+	 * sort out closed, linked or not shared directories
+	 *
+	 * @param container
+	 * @return true if the container is shared with git, not a link and
+	 *         accessible in Eclipse
+	 */
+	private static boolean isValid(IContainer container) {
+		return container.isAccessible()
+				&& !container.isLinked(IResource.CHECK_ANCESTORS)
+				&& isProjectSharedWithGit(container);
 	}
 
 	private static boolean isProjectSharedWithGit(IContainer container) {
