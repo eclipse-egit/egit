36109b06bcdf6f5ac4898c901b9952a8494d34d8
Andrey Loskutov <loskutov@gmx.de>
2014-08-01 14:59:42 +0200

    [historyView] better highlighting for "interesting" files
    Shows "interesting files" with bold font. Don't show intersting files in
    bold font if all files in the viewer are "interesting".
    
    Bug: 440979
    Change-Id: I3f45fc6d509e2b3ad5658892a9d5b56cbee70b48
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/FileDiffLabelProvider.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/FileDiffLabelProvider.java
@@ -10,13 +10,19 @@ package org.eclipse.egit.ui.internal.history;
 
 import java.text.MessageFormat;
 
+import org.eclipse.egit.ui.UIPreferences;
+import org.eclipse.egit.ui.UIUtils;
 import org.eclipse.egit.ui.internal.UIText;
+import org.eclipse.jface.resource.FontDescriptor;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.resource.LocalResourceManager;
 import org.eclipse.jface.resource.ResourceManager;
 import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.jgit.diff.DiffEntry.ChangeType;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.RGB;
 
@@ -28,6 +34,10 @@ public class FileDiffLabelProvider extends ColumnLabelProvider {
 	private final ResourceManager resourceManager = new LocalResourceManager(
 			JFaceResources.getResources());
 	private final Color dimmedForegroundColor;
+
+	private Font boldFont;
+
+	private boolean allAreInteresting;
 
 	/**
 	 * @param dimmedForegroundRgb the color used for as foreground color for "unhighlighted" entries
@@ -47,13 +57,15 @@ public class FileDiffLabelProvider extends ColumnLabelProvider {
 
 	@Override
 	public void dispose() {
+		boldFont = null;
 		this.resourceManager.dispose();
 		super.dispose();
 	}
 
 	public Color getForeground(Object element) {
 		final FileDiff c = (FileDiff) element;
-		if (!c.isMarked(FileDiffContentProvider.INTERESTING_MARK_TREE_FILTER_INDEX))
+		if (!allAreInteresting
+				&& !c.isMarked(FileDiffContentProvider.INTERESTING_MARK_TREE_FILTER_INDEX))
 			return dimmedForegroundColor;
 		else
 			return null;
@@ -69,4 +81,29 @@ public class FileDiffLabelProvider extends ColumnLabelProvider {
 		}
 		return null;
 	}
+
+	@Override
+	public Font getFont(Object element) {
+		final FileDiff c = (FileDiff) element;
+		if (!allAreInteresting
+				&& c.isMarked(FileDiffContentProvider.INTERESTING_MARK_TREE_FILTER_INDEX)) {
+			if (boldFont == null) {
+				Font font = UIUtils
+						.getFont(UIPreferences.THEME_CommitGraphHighlightFont);
+				FontData fontData[] = font.getFontData();
+				for (int i = 0; i < fontData.length; i++) {
+					fontData[i].setStyle(fontData[i].getStyle() | SWT.BOLD);
+				}
+				boldFont = resourceManager.createFont(FontDescriptor
+						.createFrom(fontData));
+			}
+			return boldFont;
+		}
+		return super.getFont(element);
+	}
+
+	void setAllInteresting(boolean all) {
+		this.allAreInteresting = all;
+	}
+
 }
