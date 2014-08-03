89660ab94032daeb850ff8dc3b18a67893a6b7d8
Robin Stocker <robin@nibor.org>
2014-08-03 20:01:08 +1000

    Fix getSelectedResources not using all ResourceTraversals
    It only used the resources of the first traversal before. E.g. a working
    set has multiple traversals. I don't know if this bug was affecting
    anything else.
    
    Change-Id: I19df313931d1d94fe291cbc0bc9dee73f50381db
    Signed-off-by: Robin Stocker <robin@nibor.org>

---
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/internal/selection/SelectionUtilsTest.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (C) 2014 Robin Stocker <robin@nibor.org> and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.selection;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.mapping.ResourceMapping;
+import org.eclipse.core.resources.mapping.ResourceTraversal;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.junit.Test;
+
+public class SelectionUtilsTest {
+
+	@Test
+	public void getSelectedResourcesShouldUseAllTraversals() throws Exception {
+		IResource resource1 = mock(IResource.class);
+		IResource resource2 = mock(IResource.class);
+
+		ResourceTraversal traversal1 = new ResourceTraversal(
+				new IResource[] { resource1 }, 1, 0);
+		ResourceTraversal traversal2 = new ResourceTraversal(
+				new IResource[] { resource2 }, 1, 0);
+
+		ResourceMapping mapping = mock(ResourceMapping.class);
+		when(mapping.getTraversals(null, null)).thenReturn(
+				new ResourceTraversal[] { traversal1, traversal2 });
+		StructuredSelection selection = new StructuredSelection(mapping);
+
+		IResource[] result = SelectionUtils.getSelectedResources(selection);
+
+		assertEquals(2, result.length);
+		assertEquals(resource1, result[0]);
+		assertEquals(resource2, result[1]);
+	}
+}
