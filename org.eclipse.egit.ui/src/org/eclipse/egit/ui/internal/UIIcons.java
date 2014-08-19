43c491319618b82e4622c40fbfe9cc4051e51bd1
Vadim Dmitriev <dmgloss@mail.ru>
2014-08-19 23:47:29 +0400

    Update interactive rebase plan row status icons
    "Todo" icon is removed and "checkmark" icon is updated.
    
    Bug: 434974
    Change-Id: Ifea5d49ef8229cc18f1c0942076cd371e818313a
    Signed-off-by: Vadim Dmitriev <dmgloss@mail.ru>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIIcons.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIIcons.java
@@ -267,9 +267,6 @@ public class UIIcons {
 	/** Reword for current rebase step */
 	public final static ImageDescriptor CURRENT_STEP;
 
-	/** Reword for todo rebase step */
-	public final static ImageDescriptor TODO_STEP;
-
 	/** base URL */
 	public final static URL base;
 
@@ -360,9 +357,8 @@ public class UIIcons {
 		SQUASH = map("obj16/squash.gif"); //$NON-NLS-1$
 		FIXUP = map("obj16/fixup.gif"); //$NON-NLS-1$
 		REWORD = map("obj16/reword.gif"); //$NON-NLS-1$
-		DONE_STEP = map("obj16/done_step.gif"); //$NON-NLS-1$
+		DONE_STEP = map("obj16/done_step.png"); //$NON-NLS-1$
 		CURRENT_STEP = map("obj16/current_step.gif"); //$NON-NLS-1$
-		TODO_STEP = map("obj16/todo_step.gif"); //$NON-NLS-1$
 	}
 
 	private static ImageDescriptor map(final String icon) {
