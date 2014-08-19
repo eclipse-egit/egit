535843b31bfc901ebccf2b035fc9367135b78e9e
Andrey Loskutov <loskutov@gmx.de>
2014-08-19 16:26:04 +0200

    Don't use alternating row colors for staging areas
    To be consistent with other similar file viewers in Eclipse we should
    avoid using alternate colors for simple file lists - they only add
    visual noise. See also https://git.eclipse.org/r/#/c/31804/ removing
    alternating row colors for commit file diff viewer.
    
    Change-Id: Ida5aaef0d3338433a1c1690bed005275a75b382f
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingView.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingView.java
@@ -499,7 +499,6 @@ public class StagingView extends ViewPart implements IShowInSource {
 				.applyTo(unstagedViewer.getControl());
 		unstagedViewer.getTree().setData(FormToolkit.KEY_DRAW_BORDER,
 				FormToolkit.TREE_BORDER);
-		unstagedViewer.getTree().setLinesVisible(true);
 		unstagedViewer.setLabelProvider(createLabelProvider(unstagedViewer));
 		unstagedViewer.setContentProvider(createStagingContentProvider(true));
 		unstagedViewer.addDragSupport(DND.DROP_MOVE | DND.DROP_COPY
@@ -763,7 +762,6 @@ public class StagingView extends ViewPart implements IShowInSource {
 				.applyTo(stagedViewer.getControl());
 		stagedViewer.getTree().setData(FormToolkit.KEY_DRAW_BORDER,
 				FormToolkit.TREE_BORDER);
-		stagedViewer.getTree().setLinesVisible(true);
 		stagedViewer.setLabelProvider(createLabelProvider(stagedViewer));
 		stagedViewer.setContentProvider(createStagingContentProvider(false));
 		stagedViewer.addDragSupport(
@@ -1350,7 +1348,6 @@ public class StagingView extends ViewPart implements IShowInSource {
 	private TreeViewer createTree(Composite composite) {
 		Tree tree = toolkit.createTree(composite, SWT.FULL_SELECTION
 				| SWT.MULTI);
-		tree.setLinesVisible(true);
 		TreeViewer treeViewer = new TreeViewer(tree);
 		return treeViewer;
 	}
