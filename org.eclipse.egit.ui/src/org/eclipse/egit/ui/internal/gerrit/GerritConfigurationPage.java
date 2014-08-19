cde606c778a205a9f2ac96353a6353242e19beb0
Robin Stocker <robin@nibor.org>
2014-08-19 21:52:39 +1000

    Fix IAE in Gerrit configuration page on null scheme
    URIish#getScheme can return null, handle it.
    
    Change-Id: If2cc15049b449f490f67332b914a271075134847
    Signed-off-by: Robin Stocker <robin@nibor.org>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/gerrit/GerritConfigurationPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/gerrit/GerritConfigurationPage.java
@@ -281,7 +281,9 @@ class GerritConfigurationPage extends WizardPage {
 	private void checkPage() {
 		try {
 			pushURI = new URIish(uriText.getText());
-			scheme.select(scheme.indexOf(pushURI.getScheme()));
+			String uriScheme = pushURI.getScheme();
+			if (uriScheme != null)
+				scheme.select(scheme.indexOf(uriScheme));
 		} catch (URISyntaxException e) {
 			setErrorMessage(e.getLocalizedMessage());
 			setPageComplete(false);
