ea6cd22e9865f3d2052bec6ab59d03ea021f7313
Tobias Melcher <tobias.melcher@sap.com>
2014-09-10 16:20:55 +0200

    Introduce context menu "import changed projects"
    The context menu action "import changed projects" analyzes the files of
    the commit and is importing the matching projects. This is a handy
    feature for very large repositories with lots of projects where it is
    impossible to import all projects. Import only the relevant projects
    needed for the selected change/commit.
    
    Change-Id: I55aac6258659d61088e9abdafe95f5a4a6a6fe63
    Signed-off-by: Tobias Melcher <tobias.melcher@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitGraphTable.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitGraphTable.java
@@ -840,6 +840,7 @@ class CommitGraphTable {
 				popupMgr.add(getCommandContributionItem(
 						HistoryViewCommands.REBASE_INTERACTIVE_CURRENT,
 						UIText.GitHistoryPage_rebaseInteractiveMenuItem));
+
 				popupMgr.add(new Separator());
 
 				MenuManager resetManager = ResetMenu.createMenu(site);
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/HistoryViewCommands.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/command/HistoryViewCommands.java
@@ -100,4 +100,5 @@ public class HistoryViewCommands {
 
 	/** Show Blame Annotations */
 	public static final String SHOW_BLAME = "org.eclipse.egit.ui.history.ShowBlame"; //$NON-NLS-1$
+
 }
