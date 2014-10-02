6b45bfe2c62b47ab6104c2cfd08c84b50e08f6f5
Arthur Daussy <arthur.daussy@obeo.fr>
2014-10-02 16:11:15 +0200

    Storages should override equals
    The use of storages in sets does not work since the equals has not been
    overriden.
    
    Change-Id: I25fa8a318bcd28dd8484f1386d7a33e57cffd3fd
    Signed-off-by: Arthur Daussy <arthur.daussy@obeo.fr>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/storage/CommitBlobStorage.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/storage/CommitBlobStorage.java
@@ -55,4 +55,39 @@ public class CommitBlobStorage extends BlobStorage {
 		return repoPath.append(Path.fromPortableString(pathString));
 	}
 
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		return prime * super.hashCode()
+				+ ((commit == null) ? 0 : commit.hashCode());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CommitBlobStorage other = (CommitBlobStorage) obj;
+		if (commit == null) {
+			if (other.commit != null)
+				return false;
+		} else if (!commit.equals(other.commit))
+			return false;
+		return true;
+	}
+
+
 }
