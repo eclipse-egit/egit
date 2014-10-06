d3be651ddd66c87b2c2bba63e97f38cf2ad5e2a6
Christian Halstrick <christian.halstrick@sap.com>
2014-10-06 14:04:11 +0200

    Fix ContainerTreeIterator to return on-disk size of a folder
    A ContainerTreeIterator always returned 0 as length of a directory. This
    was inconsistent with JGits FileTreeIterator which asked the filesystem
    object about the length. At least on unix filesystems the file-system
    size of the directory was returned which was different from 0. On
    Windows 0 was returned by java.io.File.
    Because of this inconsistency folders containing submodules often have
    been marked as dirty by mistake. The index was filled with data coming
    from a FileTreeIterator (length!=0) while the ContainerTreeIterator
    returned length==0.
    
    This has been fixed by forcing ContainerTreeIterator to ask the
    filesystem for the length of a folder.
    
    Bug: 391280
    Change-Id: I4b75ee1e875ead1d0d2f772056aca918d37d3ffd
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

---
+++ org.eclipse.egit.core.test/src/org/eclipse/egit/core/test/SubmoduleAndContainerTreeIteratorTest.java
@@ -0,0 +1,117 @@
+/*******************************************************************************
+ * Copyright (C) 2014, Christian Halstrick <christian.halstrick@sap.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.core.test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.egit.core.Activator;
+import org.eclipse.egit.core.AdaptableFileTreeIterator;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.Status;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.errors.NoWorkTreeException;
+import org.eclipse.jgit.junit.RepositoryTestCase;
+import org.eclipse.jgit.lib.Constants;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SubmoduleAndContainerTreeIteratorTest {
+	private TestUtils testUtils;
+
+	private IProject childProject;
+
+	private File childProjectDir;
+
+	private TestRepository childRepository;
+
+	private IProject parentProject;
+
+	private File parentProjectDir;
+
+	private TestRepository parentRepository;
+
+	private IFile parentFile;
+
+	private AdaptableFileTreeIterator treeIt;
+
+	@Before
+	public void setUp() throws Exception {
+		testUtils = new TestUtils();
+
+		// Create child repo and project
+		childProject = testUtils.createProjectInLocalFileSystem("child");
+		childProjectDir = childProject.getRawLocation().toFile();
+		childRepository = new TestRepository(new File(childProjectDir,
+				Constants.DOT_GIT));
+		testUtils.addFileToProject(childProject, "child.txt", "Hello world");
+		childRepository.connect(childProject);
+		// We have to wait after until the filesystem timer has advanced to
+		// avoid smudged states
+		RepositoryTestCase.fsTick(null);
+		childRepository.trackAllFiles(childProject);
+		childRepository.commit("Initial commit");
+		// Create parent repo and project
+		parentProject = testUtils.createProjectInLocalFileSystem("parent");
+		parentProjectDir = parentProject.getRawLocation().toFile();
+		parentRepository = new TestRepository(new File(parentProjectDir,
+				Constants.DOT_GIT));
+		parentFile = testUtils.addFileToProject(parentProject, "parent.txt",
+				"Hello world");
+		Git.wrap(parentRepository.getRepository()).submoduleAdd()
+				.setPath("children/child")
+				.setURI(childProjectDir.toURI().toString()).call();
+		parentRepository.connect(parentProject);
+		RepositoryTestCase.fsTick(null);
+		parentRepository.trackAllFiles(parentProject);
+		parentRepository.commit("Initial commit");
+
+		treeIt = new AdaptableFileTreeIterator(
+				parentRepository.getRepository(), parentFile.getWorkspace()
+						.getRoot());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		childRepository.dispose();
+		parentRepository.dispose();
+		childProject.delete(true, true, null);
+		parentProject.delete(true, true, null);
+		Activator.getDefault().getRepositoryCache().clear();
+		testUtils.deleteTempDirs();
+	}
+
+	@Test
+	public void testCleanStateAfterInit() throws NoWorkTreeException,
+			GitAPIException {
+		Git parentGit = Git.wrap(parentRepository.getRepository());
+
+		Status status = parentGit.status().setWorkingTreeIt(treeIt).call();
+		assertTrue(status.isClean());
+	}
+
+	@Test
+	public void testCleanStateFirstCommit() throws NoWorkTreeException,
+			GitAPIException, IOException, CoreException, InterruptedException {
+		testUtils.changeContentOfFile(parentProject, parentFile, "new content");
+		RepositoryTestCase.fsTick(null);
+		parentRepository.trackAllFiles(parentProject);
+		parentRepository.commit("modified parent.txt");
+		Git parentGit = Git.wrap(parentRepository.getRepository());
+
+		Status status = parentGit.status().setWorkingTreeIt(treeIt).call();
+		assertTrue(status.isClean());
+	}
+}
