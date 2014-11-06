c37f68a41c5c97e76e153ce1e1e8fa444c14fe99
Laurent Goubet <laurent.goubet@obeo.fr>
2014-11-06 11:18:33 +0100

    Provide access to the absolute path of the git storages
    We've implemented IStorage#getFullPath() in such a way that the
    underlying resource is unresolveable for any client not aware of git
    innards. This opens a new API to allow such clients a workaround so
    that they may access the path if the needs arises.
    
    Change-Id: I70724413c79f4756cdaffaa4229f4d9a1d8082e5
    Signed-off-by: Laurent Goubet <laurent.goubet@obeo.fr>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/storage/IndexBlobStorage.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/storage/IndexBlobStorage.java
@@ -1,5 +1,6 @@
 /*******************************************************************************
  * Copyright (C) 2010, Jens Baumgart <jens.baumgart@sap.com>
+ * Copyright (C) 2014, Obeo
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -10,6 +11,9 @@ package org.eclipse.egit.core.internal.storage;
 
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.egit.core.Activator;
+import org.eclipse.egit.core.RepositoryUtil;
+import org.eclipse.egit.core.storage.GitBlobStorage;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 
@@ -20,7 +24,7 @@ import org.eclipse.jgit.lib.Repository;
  * @see CommitBlobStorage
  *
  */
-public class IndexBlobStorage extends BlobStorage {
+public class IndexBlobStorage extends GitBlobStorage {
 
 	IndexBlobStorage(final Repository repository, final String fileName,
 			final ObjectId blob) {
@@ -29,6 +33,8 @@ public class IndexBlobStorage extends BlobStorage {
 
 	@Override
 	public IPath getFullPath() {
+		final RepositoryUtil repositoryUtil = Activator.getDefault()
+				.getRepositoryUtil();
 		IPath repoPath = new Path(repositoryUtil.getRepositoryName(db));
 		String pathString = super.getFullPath().toPortableString() + " index"; //$NON-NLS-1$
 		return repoPath.append(Path.fromPortableString(pathString));
