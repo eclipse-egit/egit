148cc8c12cd3913ddaf1fc91ed54f29d7016e641
Matthias Sohn <matthias.sohn@sap.com>
2014-12-03 15:22:33 +0100

    Revert "Add support for cherry-picking multiple commits at once"
    This reverts commit e7a0ec115347e2d147b8794d78cc5a8883f1cc3e.
    
    Bug: 447115
    Change-Id: Ife9a30e68660d879e8d394d2c3ecb87d568fe34d
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -1138,6 +1138,30 @@ public class UIText extends NLS {
 	public static String CheckoutHandler_SelectBranchTitle;
 
 	/** */
+	public static String CherryPickHandler_NoCherryPickPerformedMessage;
+
+	/** */
+	public static String CherryPickHandler_NoCherryPickPerformedTitle;
+
+	/** */
+	public static String CherryPickHandler_CherryPickConflictsMessage;
+
+	/** */
+	public static String CherryPickHandler_CherryPickConflictsTitle;
+
+	/** */
+	public static String CherryPickHandler_CherryPickFailedMessage;
+
+	/** */
+	public static String CherryPickHandler_CouldNotDeleteFile;
+
+	/** */
+	public static String CherryPickHandler_ErrorMsgTemplate;
+
+	/** */
+	public static String CherryPickHandler_IndexDirty;
+
+	/** */
 	public static String CherryPickHandler_JobName;
 
 	/** */
@@ -1145,6 +1169,12 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CherryPickHandler_ConfirmTitle;
+
+	/** */
+	public static String CherryPickHandler_unknown;
+
+	/** */
+	public static String CherryPickHandler_WorktreeDirty;
 
 	/** */
 	public static String CherryPickOperation_InternalError;
