bb9ff65b73f9df0e1a38cb5a77a9ad4803078ad6
Andrey Loskutov <loskutov@gmx.de>
2014-12-19 23:53:00 +0100

    Avoid iterating on deleted repositories in IteratorService
    Bug: 455815
    Change-Id: I01c593c8a53f3a1e8a44de1e21698f334da9c713
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/IteratorService.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/IteratorService.java
@@ -45,7 +45,10 @@ public class IteratorService {
 			// workspace is closed
 			return null;
 		}
-		IContainer container = findContainer(root, repository.getWorkTree());
+		File workTree = repository.getWorkTree();
+		if (!workTree.exists())
+			return null;
+		IContainer container = findContainer(root, workTree);
 		if (container != null)
 			return new ContainerTreeIterator(repository, container);
 		return new AdaptableFileTreeIterator(repository, root);
