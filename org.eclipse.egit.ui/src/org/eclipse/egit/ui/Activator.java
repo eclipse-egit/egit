bd13d0c45896000110c78a35002610b0aa958c0f
Matthias Sohn <matthias.sohn@sap.com>
2014-12-23 00:50:43 +0100

    UI Activator: allow tracing if workbench UI is active
    Change-Id: Ie7304bc3de53471633bfe4b316ec3efdc8fd8242
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/Activator.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/Activator.java
@@ -249,7 +249,21 @@ public class Activator extends AbstractUIPlugin implements DebugOptionsListener 
 			private void updateUiState() {
 				Display.getCurrent().asyncExec(new Runnable() {
 					public void run() {
+						boolean wasActive = uiIsActive;
 						uiIsActive = Display.getCurrent().getActiveShell() != null;
+						if (uiIsActive != wasActive
+								&& GitTraceLocation.REPOSITORYCHANGESCANNER
+										.isActive())
+							traceUiIsActive();
+					}
+
+					private void traceUiIsActive() {
+						StringBuilder message = new StringBuilder(
+								"workbench is "); //$NON-NLS-1$
+						message.append(uiIsActive ? "active" : "inactive"); //$NON-NLS-1$//$NON-NLS-2$
+						GitTraceLocation.getTrace().trace(
+								GitTraceLocation.REPOSITORYCHANGESCANNER
+										.getLocation(), message.toString());
 					}
 				});
 			}
