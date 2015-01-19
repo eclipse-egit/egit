e8d99d445567b38bdee05a609beca8e63d9ee70e
Andrey Loskutov <loskutov@gmx.de>
2015-01-19 16:14:45 +0100

    Scalability: don't run incremental update if there are too many changes
    Always check if the number of changed files exceeds the magic limit for
    incremental index updates. In case of huge repositories with lot of
    ignored files (> 65000) the incremental merge of ignored lists can be
    highly inefficient if the number of changed files is higher then 2000.
    
    Added tests for job scheduling to make sure reload and update are used
    as expected.
    
    Bug 452237
    Change-Id: I6cb733ae24a1eec33becb3aff0f96da151365a41
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

---
+++ org.eclipse.egit.core.test/src/org/eclipse/egit/core/internal/indexdiff/IndexDiffDataTest.java
@@ -0,0 +1,80 @@
+package org.eclipse.egit.core.internal.indexdiff;
+
+import static java.util.Arrays.asList;
+import static org.eclipse.egit.core.internal.indexdiff.IndexDiffData.isAnyPrefixOf;
+import static org.eclipse.egit.core.internal.indexdiff.IndexDiffData.mergeIgnored;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.egit.core.test.GitTestCase;
+import org.junit.Test;
+
+public class IndexDiffDataTest extends GitTestCase {
+
+	@Test
+	public void testIsAnyPrefixOf() {
+		Collection<String> possiblePrefixes = asList("", "/");
+		assertTrue(isAnyPrefixOf("", possiblePrefixes));
+
+		possiblePrefixes = asList("", "/");
+		assertTrue(isAnyPrefixOf("/", possiblePrefixes));
+
+		possiblePrefixes = asList("a");
+		assertTrue(isAnyPrefixOf("a", possiblePrefixes));
+
+		possiblePrefixes = asList("a/");
+		assertTrue(isAnyPrefixOf("a", possiblePrefixes));
+
+		possiblePrefixes = asList("b");
+		assertFalse(isAnyPrefixOf("a", possiblePrefixes));
+
+		possiblePrefixes = asList("b", "ab", "b/", "aa");
+		assertFalse(isAnyPrefixOf("a", possiblePrefixes));
+	}
+
+	@Test
+	public void testMergeIgnored() {
+		Set<String> result;
+		Set<String> expected = new HashSet<String>();
+		Set<String> oldIgnoredPaths = new HashSet<String>();
+		Collection<String> changedPaths = new HashSet<String>();
+		Set<String> newIgnoredPaths = new HashSet<String>();
+
+		result = mergeIgnored(oldIgnoredPaths, changedPaths, newIgnoredPaths);
+		assertEquals(expected, result);
+
+		newIgnoredPaths.add("a");
+		changedPaths.add("a");
+		expected.add("a");
+		result = mergeIgnored(oldIgnoredPaths, changedPaths, newIgnoredPaths);
+		assertEquals(expected, result);
+
+		newIgnoredPaths.add("b");
+		expected.add("b");
+		result = mergeIgnored(oldIgnoredPaths, changedPaths, newIgnoredPaths);
+		assertEquals(expected, result);
+
+		changedPaths.add("b");
+		result = mergeIgnored(oldIgnoredPaths, changedPaths, newIgnoredPaths);
+		assertEquals(expected, result);
+
+		oldIgnoredPaths.add("b");
+		result = mergeIgnored(oldIgnoredPaths, changedPaths, newIgnoredPaths);
+		assertEquals(expected, result);
+
+		oldIgnoredPaths.add("c");
+		expected.add("c");
+		result = mergeIgnored(oldIgnoredPaths, changedPaths, newIgnoredPaths);
+		assertEquals(expected, result);
+
+		newIgnoredPaths.add("b");
+		expected.add("b");
+		result = mergeIgnored(oldIgnoredPaths, changedPaths, newIgnoredPaths);
+		assertEquals(expected, result);
+	}
+}
