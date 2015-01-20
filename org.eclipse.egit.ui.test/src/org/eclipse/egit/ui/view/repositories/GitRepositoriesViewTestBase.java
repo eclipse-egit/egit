edd6ded6ce7631f2efb64e6ba04aedeb99fdbdb4
Peter Karena <peter.karena@arcor.de>
2015-01-20 18:51:54 +0100

    Show that there are changes in "Git Repositories" view
    Equal to the modification of a file, where the sign '>' is added to the
    filename, the same character is added, when a file in a repository gets
    changed. It's very usefull when you work with many repositories.
    
    Change-Id: I5fe30cb738c0ec5334430fc710d2d826deaebff1
    Signed-off-by: Peter Karena <peter.karena@arcor.de>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/repositories/GitRepositoriesViewTestBase.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/repositories/GitRepositoriesViewTestBase.java
@@ -99,8 +99,7 @@ public abstract class GitRepositoriesViewTestBase extends
 		final SWTBotTreeItem[] items = tree.getAllItems();
 		boolean found = false;
 		for (SWTBotTreeItem item : items) {
-			if (item.getText().startsWith(
-					repositoryDir.getParentFile().getName())) {
+			if (item.getText().contains(repositoryDir.getParentFile().getName())) {
 				found = true;
 				break;
 			}
