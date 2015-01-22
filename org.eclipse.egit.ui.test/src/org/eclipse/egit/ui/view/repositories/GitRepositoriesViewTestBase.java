4c5d0f158279cd7ab4ab259d2ecfe998386e6be1
Matthias Sohn <matthias.sohn@sap.com>
2015-01-22 09:08:56 +0100

    Fix "Unnecessary cast to ICommandService cast" warning
    Change-Id: I51c521210bcaf1718a7dff84c23b4dc141f90d6a
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/repositories/GitRepositoriesViewTestBase.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/view/repositories/GitRepositoriesViewTestBase.java
@@ -25,6 +25,7 @@ import org.eclipse.egit.core.Activator;
 import org.eclipse.egit.core.RepositoryUtil;
 import org.eclipse.egit.ui.JobFamilies;
 import org.eclipse.egit.ui.common.LocalRepositoryTestCase;
+import org.eclipse.egit.ui.internal.CommonUtils;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.repository.RepositoriesView;
 import org.eclipse.egit.ui.internal.repository.tree.command.ToggleBranchCommitCommand;
@@ -79,8 +80,8 @@ public abstract class GitRepositoriesViewTestBase extends
 	}
 
 	protected static void setVerboseBranchMode(boolean state) {
-		ICommandService srv = (ICommandService) PlatformUI.getWorkbench()
-				.getService(ICommandService.class);
+		ICommandService srv = CommonUtils.getService(PlatformUI.getWorkbench(),
+				ICommandService.class);
 		State verboseBranchModeState = srv.getCommand(
 				ToggleBranchCommitCommand.ID).getState(
 				ToggleBranchCommitCommand.TOGGLE_STATE);
