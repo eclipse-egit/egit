8a6d3ef8d201ae3462fd5081ff6cf0712b57e82a
Kyle J. Harms <kyle.harms@gmail.com>
2015-02-08 13:07:59 -0600

    Java templates variables for Git config variables
    Java templates variables do not currently grant access to the git config
    variables. This change allows git config variables to be accessed
    within Java templates.
    
    Bug: 458505
    Change-Id: I9becfb264df1ba21736523bd758947cf977ac4f5
    Signed-off-by: Kyle J. Harms <kyle.harms@gmail.com>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/variables/GitTemplateVariableResolver.java
@@ -0,0 +1,148 @@
+/*******************************************************************************
+ * Copyright (c) 2012, 2015 Kyle J. Harms <harmsk@seas.wustl.edu>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.variables;
+
+import java.util.List;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.egit.core.project.RepositoryMapping;
+import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.internal.UIText;
+import org.eclipse.jdt.internal.corext.template.java.CodeTemplateContext;
+import org.eclipse.jface.text.templates.TemplateContext;
+import org.eclipse.jface.text.templates.TemplateVariable;
+import org.eclipse.jface.text.templates.TemplateVariableResolver;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.StoredConfig;
+
+/**
+ * Resolves Git variables within templates
+ *
+ * @see <a
+ *      href="http://help.eclipse.org/topic/org.eclipse.jdt.doc.user/concepts/concept-template-variables.htm">http://help.eclipse.org/topic/org.eclipse.jdt.doc.user/concepts/concept-template-variables.htm</a>
+ */
+public class GitTemplateVariableResolver extends TemplateVariableResolver {
+
+	/**
+	 * Creates an instance of <code>GitTemplateVariableResolver</code>.
+	 *
+	 * @param type
+	 *            the name of the type
+	 * @param description
+	 *            the description for the type
+	 */
+	protected GitTemplateVariableResolver(String type, String description) {
+		super(type, description);
+	}
+
+	/**
+	 * Creates an instance of <code>GitTemplateVariableResolver</code>.
+	 */
+	public GitTemplateVariableResolver() {
+		super("git_config", //$NON-NLS-1$
+				UIText.GitTemplateVariableResolver_GitConfigDescription);
+	}
+
+	@Override
+	public void resolve(TemplateVariable variable, TemplateContext context) {
+		resolveVariable(variable, context);
+	}
+
+	/**
+	 * Resolves the git_config variable for a project
+	 *
+	 * @param variable
+	 *            the current template variable.
+	 * @param project
+	 *            the current project.
+	 */
+	protected static void resolveVariable(TemplateVariable variable,
+			IProject project) {
+		final List<String> params = variable.getVariableType().getParams();
+		if (params.isEmpty()) {
+			return;
+		}
+
+		final String gitKey = params.get(0);
+		if (gitKey == null || gitKey.length() == 0) {
+			return;
+		}
+
+		// Get git's config
+		RepositoryMapping mapping = RepositoryMapping.getMapping(project);
+		Repository repository = null;
+
+		if (mapping != null) {
+			repository = mapping.getRepository();
+		}
+		if (repository == null) {
+			return;
+		}
+
+		StoredConfig config = repository.getConfig();
+		if (config == null) {
+			return;
+		}
+
+		// Get the value of the key
+		final String[] splits = gitKey.split("\\."); //$NON-NLS-1$
+		String section = null;
+		String subSection = null;
+		String name = null;
+
+		if (splits.length == 3) {
+			section = splits[0];
+			subSection = splits[1];
+			name = splits[2];
+		} else if (splits.length == 2) {
+			section = splits[0];
+			name = splits[1];
+		} else {
+			return;
+		}
+
+		String gitValue = config.getString(section, subSection, name);
+		if (gitValue != null) {
+			variable.setValue(gitValue);
+		}
+	}
+
+	/**
+	 * Resolves the git_config variable
+	 *
+	 * @param variable
+	 *            the current template variable.
+	 * @param context
+	 *            the current template context.
+	 */
+	protected static void resolveVariable(TemplateVariable variable,
+			TemplateContext context) {
+		IProject project = getProject(context);
+		if (project != null) {
+			resolveVariable(variable, project);
+		}
+	}
+
+	/**
+	 * Retrieves the current project from a template context.
+	 *
+	 * @param context
+	 *            the current template context.
+	 * @return the current project
+	 */
+	protected static IProject getProject(TemplateContext context) {
+		IProject project = null;
+		if (Activator.hasJavaPlugin()
+				&& (context instanceof CodeTemplateContext)) {
+			project = ((CodeTemplateContext) context).getJavaProject()
+					.getProject();
+		}
+		return project;
+	}
+}
