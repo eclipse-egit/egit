c403d7501cfa0173721afa56572970cbcfbea97e
Matthias Sohn <matthias.sohn@sap.com>
2015-02-24 15:18:46 +0100

    Auto-configure Gerrit configuration if repository is cloned from Gerrit
    If a repository is cloned over http or https protocol use Gerrit's
    REST API endpoint "Get Version" [1] to find out if the repository is
    hosted by a Gerrit server.
    
    Implement a shortcut for https and ssh endpoint of the Eclipse Gerrit
    server.
    
    If we detect the repository was cloned from a Gerrit server auto
    configure the repository for Gerrit. Configure the push refspec to
    HEAD:refs/for/master which should be ok in most cases.
    
    [1] https://gerrit-documentation.storage.googleapis.com/Documentation/2.11/rest-api-config.html#get-version
    
    Bug: 460423
    Change-Id: I30b916e279e0bc84f86abbffae241f8aab3fa8df
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

---
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/gerrit/GerritUtil.java
@@ -0,0 +1,130 @@
+/*******************************************************************************
+ * Copyright (c) 2013, 2015 Robin Stocker <robin@nibor.org> and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.core.internal.gerrit;
+
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jgit.lib.Config;
+import org.eclipse.jgit.lib.ConfigConstants;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.transport.RefSpec;
+import org.eclipse.jgit.transport.RemoteConfig;
+import org.eclipse.jgit.transport.URIish;
+
+/**
+ * Gerrit utilities
+ */
+public class GerritUtil {
+
+	/**
+	 * The Gerrit push prefix {@value} .
+	 */
+	public static final String REFS_FOR = "refs/for/"; //$NON-NLS-1$
+
+	/**
+	 * @param config
+	 * @return true if there if "create change ID" is configured
+	 */
+	public static boolean getCreateChangeId(Config config) {
+		return config.getBoolean(ConfigConstants.CONFIG_GERRIT_SECTION,
+				ConfigConstants.CONFIG_KEY_CREATECHANGEID, false);
+	}
+
+	/**
+	 * Set the "create change ID" configuration
+	 *
+	 * @param config
+	 */
+	public static void setCreateChangeId(Config config) {
+		config.setBoolean(ConfigConstants.CONFIG_GERRIT_SECTION, null,
+				ConfigConstants.CONFIG_KEY_CREATECHANGEID, true);
+	}
+
+	/**
+	 * Find the remote config for the given remote name
+	 *
+	 * @param config
+	 *            the configuration containing the remote config
+	 * @param remoteName
+	 *            the name of the remote to find
+	 * @return the found remoteConfig or {@code null}
+	 * @throws URISyntaxException
+	 *             if the configuration contains an illegal URI
+	 */
+	public static RemoteConfig findRemoteConfig(Config config, String remoteName)
+			throws URISyntaxException {
+		RemoteConfig remoteConfig = null;
+		List<RemoteConfig> allRemoteConfigs = RemoteConfig
+				.getAllRemoteConfigs(config);
+		for (RemoteConfig rc : allRemoteConfigs) {
+			if (rc.getName().equals(remoteName)) {
+				remoteConfig = rc;
+				break;
+			}
+		}
+		return remoteConfig;
+	}
+
+	/**
+	 * Configure the pushURI for Gerrit
+	 *
+	 * @param remoteConfig
+	 *            the remote configuration to add this to
+	 * @param pushURI
+	 *            the pushURI to configure
+	 */
+	public static void configurePushURI(RemoteConfig remoteConfig,
+			URIish pushURI) {
+		List<URIish> pushURIs = new ArrayList<URIish>(
+				remoteConfig.getPushURIs());
+		for (URIish urIish : pushURIs) {
+			remoteConfig.removePushURI(urIish);
+		}
+		remoteConfig.addPushURI(pushURI);
+	}
+
+	/**
+	 * Configure the gerrit push refspec HEAD:refs/for/<gerritBranch>
+	 *
+	 * @param remoteConfig
+	 *            the remote configuration to configure this in
+	 * @param gerritBranch
+	 *            the branch to push to review for
+	 */
+	public static void configurePushRefSpec(RemoteConfig remoteConfig,
+			String gerritBranch) {
+		List<RefSpec> pushRefSpecs = new ArrayList<RefSpec>(
+				remoteConfig.getPushRefSpecs());
+		for (RefSpec refSpec : pushRefSpecs) {
+			remoteConfig.removePushRefSpec(refSpec);
+		}
+		remoteConfig.addPushRefSpec(new RefSpec(
+				"HEAD:" + GerritUtil.REFS_FOR + gerritBranch)); //$NON-NLS-1$
+	}
+
+	/**
+	 * Configure fetching review summary notes
+	 *
+	 * @param remoteConfig
+	 *            the remote configuration to configure this in
+	 */
+	public static void configureFetchNotes(RemoteConfig remoteConfig) {
+		String notesRef = Constants.R_NOTES + "*"; //$NON-NLS-1$
+		List<RefSpec> fetchRefSpecs = remoteConfig.getFetchRefSpecs();
+		for (RefSpec refSpec : fetchRefSpecs) {
+			if (refSpec.matchSource(notesRef)) {
+				return;
+			}
+		}
+		remoteConfig.addFetchRefSpec(new RefSpec(notesRef + ":" + notesRef)); //$NON-NLS-1$
+	}
+
+
+}
---
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/op/ConfigureGerritAfterCloneTask.java
@@ -0,0 +1,198 @@
+/*******************************************************************************
+ * Copyright (C) 2015, Matthias Sohn <matthias.sohn@sap.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.core.op;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.egit.core.Activator;
+import org.eclipse.egit.core.internal.gerrit.GerritUtil;
+import org.eclipse.egit.core.op.CloneOperation.PostCloneTask;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.StoredConfig;
+import org.eclipse.jgit.transport.RemoteConfig;
+import org.eclipse.jgit.transport.URIish;
+
+/**
+ * Configure Gerrit if repository was cloned from a Gerrit server
+ */
+public class ConfigureGerritAfterCloneTask implements PostCloneTask {
+
+	private static final String GIT_ECLIPSE_ORG = "git.eclipse.org"; //$NON-NLS-1$
+
+	private static final String GERRIT_CONTEXT_ROOT = "/r"; //$NON-NLS-1$
+
+	private static final String HTTP = "http"; //$NON-NLS-1$
+
+	private static final String HTTPS = "https"; //$NON-NLS-1$
+
+	private static final String SSH = "ssh"; //$NON-NLS-1$
+
+	private static final String GERRIT_CONFIG_SERVER_VERSION_API = "/config/server/version"; //$NON-NLS-1$
+
+	/**
+	 * To prevent against Cross Site Script Inclusion (XSSI) attacks, the Gerrit
+	 * JSON response body starts with a magic prefix line we can use as a second
+	 * marker beyond the get version endpoint [1] to detect a Gerrit server
+	 * <p/>
+	 * [1] <a href=
+	 * "https://gerrit-documentation.storage.googleapis.com/Documentation/2.11
+	 * /rest-api-config.html#get-version">Gerrit 2.11 Get Version REST
+	 * endpoint</a>
+	 */
+	private static final String GERRIT_XSSI_MAGIC_STRING = ")]}\'\n"; //$NON-NLS-1$
+
+	private String uri;
+
+	private String remoteName;
+
+	/**
+	 * @param uri
+	 * @param remoteName
+	 */
+	public ConfigureGerritAfterCloneTask(String uri, String remoteName) {
+		this.uri = uri;
+		this.remoteName = remoteName;
+	}
+
+	public void execute(Repository repository, IProgressMonitor monitor)
+			throws CoreException {
+		try {
+			if (isGerrit()) {
+				Activator.logInfo(uri
+						+ " was detected to be hosted by a Gerrit server"); //$NON-NLS-1$
+				configureGerrit(repository);
+			}
+		} catch (Exception e) {
+			throw new CoreException(Activator.error(e.getMessage(), e));
+		}
+	}
+
+	/**
+	 * Try to use Gerrit's "Get Version" REST API endpoint [1] to detect if the
+	 * repository is hosted on a Gerrit server.
+	 * <p/>
+	 * [1] <a href=
+	 * "https://gerrit-documentation.storage.googleapis.com/Documentation/2.11
+	 * /rest-api-config.html#get-version">Gerrit 2.11 Get Version REST
+	 * endpoint</a>
+	 *
+	 * @return {@code true} if the repository is hosted on a Gerrit server
+	 * @throws IOException
+	 * @throws MalformedURLException
+	 * @throws URISyntaxException
+	 */
+	private boolean isGerrit() throws MalformedURLException, IOException,
+			URISyntaxException {
+		URIish u = new URIish(uri);
+		String s = u.getScheme();
+		String host = u.getHost();
+		// shortcut for Eclipse Gerrit server
+		if (host != null && host.equals(GIT_ECLIPSE_ORG)) {
+			if (s != null && s.equals(HTTPS)
+					&& (u.getPort() == 443 || u.getPort() == -1)
+					&& u.getPath() != null
+					&& u.getPath().startsWith(GERRIT_CONTEXT_ROOT)) {
+				return true;
+			} else if (s.equals(SSH) && (u.getPort() == 29418)) {
+				return true;
+			}
+		}
+
+		if (s != null && (s.equals(HTTP) || s.equals(HTTPS))) {
+			String baseURL = uri.substring(0, uri.lastIndexOf(u.getPath()));
+			String path = ""; //$NON-NLS-1$
+			HttpURLConnection httpConnection = null;
+			try {
+				while (true) {
+					int slash = 1;
+					InputStream in = null;
+					try {
+						httpConnection = (HttpURLConnection) new URL(baseURL
+								+ path + GERRIT_CONFIG_SERVER_VERSION_API)
+								.openConnection();
+						httpConnection.setRequestMethod("GET"); //$NON-NLS-1$
+						int responseCode = httpConnection.getResponseCode();
+						switch (responseCode) {
+						case HttpURLConnection.HTTP_OK:
+							in = httpConnection.getInputStream();
+							String response = readFully(in, "UTF-8"); //$NON-NLS-1$
+							if (response.startsWith(GERRIT_XSSI_MAGIC_STRING)) {
+								return true;
+							} else {
+								return false;
+							}
+						case HttpURLConnection.HTTP_NOT_FOUND:
+							String p = u.getPath();
+							slash = p.indexOf('/', slash);
+							if (slash == p.length()) {
+								return false;
+							} else if (slash == -1) {
+								slash = p.length() - 1;
+							}
+							path = p.substring(0, slash);
+							break;
+						default:
+							return false;
+						}
+					} finally {
+						if (in != null) {
+							in.close();
+						}
+					}
+				}
+			} finally {
+				if (httpConnection != null) {
+					httpConnection.disconnect();
+				}
+			}
+		}
+		return false;
+	}
+
+	private String readFully(InputStream inputStream, String encoding)
+			throws IOException {
+		return new String(readFully(inputStream), encoding);
+	}
+
+	private byte[] readFully(InputStream inputStream) throws IOException {
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		byte[] buffer = new byte[1024];
+		int length = 0;
+		while ((length = inputStream.read(buffer)) != -1) {
+			os.write(buffer, 0, length);
+		}
+		return os.toByteArray();
+	}
+
+	private void configureGerrit(Repository repository)
+			throws URISyntaxException, IOException {
+		StoredConfig config = repository.getConfig();
+		RemoteConfig remoteConfig;
+		remoteConfig = GerritUtil.findRemoteConfig(config, remoteName);
+		if (remoteConfig == null) {
+			return;
+		}
+		GerritUtil.configurePushURI(remoteConfig, new URIish(uri));
+		GerritUtil.configurePushRefSpec(remoteConfig, Constants.MASTER);
+		GerritUtil.configureFetchNotes(remoteConfig);
+		GerritUtil.setCreateChangeId(config);
+		remoteConfig.update(config);
+		config.save();
+	}
+
+}
