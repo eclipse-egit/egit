a20ab1a95955a9c5a865e84d412615bb3dcfc32a
Matthias Sohn <matthias.sohn@sap.com>
2015-03-02 00:45:42 +0100

    Replace deprecated ObjectReader.release()
    Where possible use try-with-resources statements instead.
    
    Change-Id: Ica07a4957d48e9e452882795d9cfd828e2457854
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/fetch/FetchResultTable.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/fetch/FetchResultTable.java
@@ -330,7 +330,7 @@ class FetchResultTable {
 		treePanel.addDisposeListener(new DisposeListener() {
 			public void widgetDisposed(DisposeEvent e) {
 				if (reader != null)
-					reader.release();
+					reader.close();
 			}
 		});
 
