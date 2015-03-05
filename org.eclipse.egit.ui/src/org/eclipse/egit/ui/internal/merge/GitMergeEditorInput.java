4e38d5e3d8cd991981daa18b9e25bb96bac5d9b3
Matthias Sohn <matthias.sohn@sap.com>
2015-03-05 09:46:15 +0100

    Fix SWTException: invalid thread access
    Bug: 461468
    Change-Id: I49d8e856393d35ecf10fe63280541f2cd109fffa
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/merge/GitMergeEditorInput.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/merge/GitMergeEditorInput.java
@@ -67,6 +67,7 @@ import org.eclipse.jgit.util.IO;
 import org.eclipse.jgit.util.RawParseUtils;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.team.core.history.IFileRevision;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
@@ -106,7 +107,8 @@ public class GitMergeEditorInput extends CompareEditorInput {
 
 	@Override
 	public Object getAdapter(Class adapter) {
-		if (adapter == IFile.class || adapter == IResource.class) {
+		if ((adapter == IFile.class || adapter == IResource.class)
+				&& isUIThread()) {
 			Object selectedEdition = getSelectedEdition();
 			if (selectedEdition instanceof DiffNode) {
 				DiffNode diffNode = (DiffNode) selectedEdition;
@@ -118,6 +120,10 @@ public class GitMergeEditorInput extends CompareEditorInput {
 			}
 		}
 		return super.getAdapter(adapter);
+	}
+
+	private static boolean isUIThread() {
+		return Display.getCurrent() != null;
 	}
 
 	@Override
