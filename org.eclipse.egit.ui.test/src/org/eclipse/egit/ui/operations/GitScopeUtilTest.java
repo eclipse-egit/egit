0f95e0f39540971f8a5ea4da1d0b0f3d36c6d62e
Andrey Loskutov <loskutov@gmx.de>
2015-04-18 11:43:12 +0200

    Fix unnecessary cast errors from generified IAdaptable#getAdapter(..)
    Bug: 460685
    
    Change-Id: Iec050627f71b31f0b10b19771b67e5eb86f1aa86
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/operations/GitScopeUtilTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/operations/GitScopeUtilTest.java
@@ -32,6 +32,7 @@ import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.egit.core.Activator;
 import org.eclipse.egit.core.JobFamilies;
+import org.eclipse.egit.core.internal.Utils;
 import org.eclipse.egit.ui.common.LocalRepositoryTestCase;
 import org.eclipse.egit.ui.internal.operations.GitScopeOperation;
 import org.eclipse.egit.ui.internal.operations.GitScopeOperationFactory;
@@ -230,7 +231,7 @@ public class GitScopeUtilTest extends LocalRepositoryTestCase {
 		}
 
 		private ResourceMapping getMappingAdapter(IResource resource) {
-			return (ResourceMapping) resource.getAdapter(ResourceMapping.class);
+			return Utils.getAdapter(resource, ResourceMapping.class);
 		}
 	}
 }
