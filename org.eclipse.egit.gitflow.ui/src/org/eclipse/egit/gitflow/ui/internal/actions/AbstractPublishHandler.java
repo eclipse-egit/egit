595d29d0e3d0710ea081cbb55945911661b81211
Max Hohenegger <eclipse@hohenegger.eu>
2015-04-30 11:48:23 +0200

    Extend EGit to allow use of git-flow
    Initial implementation:
    - Operations for all commonly used git flow commands without optional
      parameters
    - Menu contributions in the repository and history view, as well as the
      team menu on projects
    - Added new feature as incubation project to update site
    - Non-UI tests (see launch config)
    
    Bug: 348610
    Change-Id: I215ed28e6434a789fa7260d7072532b402a1e714
    Signed-off-by: Max Hohenegger <eclipse@hohenegger.eu>

---
+++ org.eclipse.egit.gitflow.ui/src/org/eclipse/egit/gitflow/ui/internal/actions/AbstractPublishHandler.java
@@ -0,0 +1,70 @@
+/*******************************************************************************
+ * Copyright (C) 2015, Max Hohenegger <eclipse@hohenegger.eu>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.gitflow.ui.internal.actions;
+
+import static org.eclipse.egit.gitflow.ui.Activator.error;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.PlatformObject;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.egit.gitflow.GitFlowRepository;
+import org.eclipse.egit.gitflow.op.CurrentBranchPublishOperation;
+import org.eclipse.egit.gitflow.ui.Activator;
+import org.eclipse.egit.ui.UIPreferences;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+/**
+ * git flow {feature,release,hotfix} finish
+ */
+public abstract class AbstractPublishHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IStructuredSelection selection = (IStructuredSelection) HandlerUtil
+				.getCurrentSelection(event);
+		PlatformObject firstElement = (PlatformObject) selection
+				.getFirstElement();
+		Repository repository = (Repository) firstElement
+				.getAdapter(Repository.class);
+		final GitFlowRepository gfRepo = new GitFlowRepository(repository);
+
+		Job job = new Job(getProgressText()) {
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				try {
+					int timeout = Activator.getDefault().getPreferenceStore()
+							.getInt(UIPreferences.REMOTE_CONNECTION_TIMEOUT);
+					CurrentBranchPublishOperation featurePublishOperation = new CurrentBranchPublishOperation(
+							gfRepo, timeout);
+					featurePublishOperation.execute(monitor);
+				} catch (CoreException e) {
+					return error(e.getMessage(), e);
+				}
+				return Status.OK_STATUS;
+			}
+		};
+		job.setUser(true);
+		job.schedule();
+
+		return null;
+	}
+
+	/**
+	 * @return Text to be shown when the operation is in progress
+	 */
+	abstract protected String getProgressText();
+}
