2954c8813403bfaa24ce5916c6cc5ed84a2094ff
Stefan Dirix <sdirix@eclipsesource.com>
2015-05-20 18:04:57 +0200

    Compare/ReplaceWithPrevious support for multiple resources (in mappings)
    The CompareWithPrevious and ReplaceWithPrevious actions now also support
    scenarios where the current selection consists of a single object but
    maps to multiple files (commonly via ResourceMappings).
    
    The previous commit is the next-to-last commit which touched any of the
    resources.
    
    Bug: 496916
    Change-Id: I1a208bd9949711e602309cac702a5a158b0eab4b
    Signed-off-by: Stefan Dirix <sdirix@eclipsesource.com>
    Signed-off-by: Philip Langer <planger@eclipsesource.com>
    Also-by: Laurent Delaigue <laurent.delaigue@obeo.fr>
    Also-by: Mathieu Cartaud <mathieu.cartaud@obeo.fr>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CompareWithPreviousActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/CompareWithPreviousActionHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2011, 2013 GitHub Inc. and others.
+ *  Copyright (c) 2011, 2016 GitHub Inc. and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -9,12 +9,15 @@
  *    Kevin Sawicki (GitHub Inc.) - initial API and implementation
  *    François Rey - gracefully ignore linked resources
  *    Laurent Goubet <laurent.goubet@obeo.fr>
+ *    Stefan Dirix <sdirix@eclipsesource.com>
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.actions;
 
 import java.io.IOException;
 import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -26,10 +29,12 @@ import org.eclipse.egit.ui.internal.CompareUtils;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.dialogs.CommitSelectDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.window.Window;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.util.StringUtils;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
@@ -48,23 +53,24 @@ public class CompareWithPreviousActionHandler extends RepositoryActionHandler {
 		if (repository == null) {
 			return null;
 		}
-
 		final IResource[] resources = getSelectedResources(event);
-		if (resources.length != 1) {
-			return null;
-		}
-		try {
-			IWorkbenchPage workBenchPage = HandlerUtil
-					.getActiveWorkbenchWindowChecked(event).getActivePage();
-			final PreviousCommit previous = getPreviousRevision(event,
-					resources[0]);
-			if (previous != null) {
-				CompareUtils.compare(resources, repository, Constants.HEAD,
-						previous.commit.getName(), true, workBenchPage);
+		if (resources != null && resources.length > 0) {
+			try {
+				IWorkbenchPage workBenchPage = HandlerUtil
+						.getActiveWorkbenchWindowChecked(event).getActivePage();
+				final RevCommit previous = getPreviousRevision(event,
+						resources);
+				if (previous != null) {
+					CompareUtils.compare(resources, repository, Constants.HEAD,
+							previous.getName(), true, workBenchPage);
+				} else {
+					showNotFoundDialog(event, resources);
+				}
+			} catch (Exception e) {
+				Activator.handleError(
+						UIText.CompareWithRefAction_errorOnSynchronize, e,
+						true);
 			}
-		} catch (Exception e) {
-			Activator.handleError(
-					UIText.CompareWithRefAction_errorOnSynchronize, e, true);
 		}
 
 		return null;
@@ -72,25 +78,24 @@ public class CompareWithPreviousActionHandler extends RepositoryActionHandler {
 
 	@Override
 	public boolean isEnabled() {
-		IResource[] selectedResources = getSelectedResources();
-		return super.isEnabled() && selectedResources.length == 1 &&
-				selectionMapsToSingleRepository();
+		IStructuredSelection selection = getSelection();
+		return super.isEnabled() && selection.size() == 1
+				&& selectionMapsToSingleRepository();
 	}
 
-	private PreviousCommit getPreviousRevision(final ExecutionEvent event,
-			final IResource resource) throws IOException {
+	private RevCommit getPreviousRevision(final ExecutionEvent event,
+			final IResource[] resources) throws IOException {
+		final List<RevCommit> previousList = findPreviousCommits(
+				Arrays.asList(resources));
 
-		final List<PreviousCommit> previousList = findPreviousCommits();
+		final AtomicReference<RevCommit> previous = new AtomicReference<>();
 
-		final AtomicReference<PreviousCommit> previous = new AtomicReference<>();
-		if (previousList.size() == 0)
-			showNotFoundDialog(event, resource);
-		else if (previousList.size() == 1)
+		if (previousList.size() == 1)
 			previous.set(previousList.get(0));
 		else {
 			final List<RevCommit> commits = new ArrayList<>();
-			for (PreviousCommit pc : previousList)
-				commits.add(pc.commit);
+			for (RevCommit pc : previousList)
+				commits.add(pc);
 			HandlerUtil.getActiveShell(event).getDisplay()
 					.syncExec(new Runnable() {
 						@Override
@@ -98,9 +103,8 @@ public class CompareWithPreviousActionHandler extends RepositoryActionHandler {
 							CommitSelectDialog dlg = new CommitSelectDialog(
 									HandlerUtil.getActiveShell(event), commits);
 							if (dlg.open() == Window.OK)
-								for (PreviousCommit pc : previousList)
-									if (pc.commit.equals(dlg
-											.getSelectedCommit())) {
+								for (RevCommit pc : previousList)
+									if (pc.equals(dlg.getSelectedCommit())) {
 										previous.set(pc);
 										break;
 									}
@@ -112,10 +116,20 @@ public class CompareWithPreviousActionHandler extends RepositoryActionHandler {
 	}
 
 	private void showNotFoundDialog(final ExecutionEvent event,
-			IResource resource) {
+			IResource[] resources) {
+
+		final List<String> names = new LinkedList<>();
+		for (IResource resource : resources) {
+			if (resource.getName() != null) {
+				names.add(resource.getName());
+			}
+		}
+
+		final String resourceNames = StringUtils.join(names, ", ", " & "); //$NON-NLS-1$ //$NON-NLS-2$
+
 		final String message = MessageFormat
 				.format(UIText.CompareWithPreviousActionHandler_MessageRevisionNotFound,
-						resource.getName());
+						resourceNames);
 		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
 
 			@Override
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/ReplaceWithPreviousActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/ReplaceWithPreviousActionHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2012, Mathias Kinzler <mathias.kinzler@sap.com>
+ * Copyright (C) 2012, 2016 Mathias Kinzler <mathias.kinzler@sap.com>
  * and other copyright owners as documented in the project's IP log.
  *
  * All rights reserved. This program and the accompanying materials
@@ -11,7 +11,7 @@ package org.eclipse.egit.ui.internal.actions;
 
 import java.io.IOException;
 import java.text.MessageFormat;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.eclipse.core.commands.ExecutionEvent;
@@ -21,6 +21,7 @@ import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.dialogs.CommitSelectDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.window.Window;
 import org.eclipse.jgit.revwalk.RevCommit;
 
@@ -34,15 +35,10 @@ public class ReplaceWithPreviousActionHandler extends
 	protected String gatherRevision(ExecutionEvent event)
 			throws ExecutionException {
 		IResource[] resources = getSelectedResources(event);
-		if (resources.length != 1)
-			throw new ExecutionException(
-					"Unexpected number of selected Resources"); //$NON-NLS-1$
 		try {
-			List<PreviousCommit> pcs = findPreviousCommits();
-			List<RevCommit> previousCommits = new ArrayList<>();
-			for (PreviousCommit pc: pcs)
-				previousCommits.add(pc.commit);
-			int parentCount = previousCommits.size();
+			List<RevCommit> pcs = findPreviousCommits(Arrays.asList(resources));
+
+			int parentCount = pcs.size();
 			if (parentCount == 0) {
 				MessageDialog
 						.openError(
@@ -54,13 +50,13 @@ public class ReplaceWithPreviousActionHandler extends
 				throw new OperationCanceledException();
 			} else if (parentCount > 1) {
 				CommitSelectDialog dlg = new CommitSelectDialog(
-						getShell(event), previousCommits);
+						getShell(event), pcs);
 				if (dlg.open() == Window.OK)
 					return dlg.getSelectedCommit().getName();
 				else
 					throw new OperationCanceledException();
 			} else
-				return previousCommits.get(0).getName();
+				return pcs.get(0).getName();
 		} catch (IOException e) {
 			throw new ExecutionException(e.getMessage(), e);
 		}
@@ -68,8 +64,8 @@ public class ReplaceWithPreviousActionHandler extends
 
 	@Override
 	public boolean isEnabled() {
-		IResource[] selectedResources = getSelectedResources();
-		return super.isEnabled() && selectedResources.length == 1 &&
-				selectionMapsToSingleRepository();
+		IStructuredSelection selection = getSelection();
+		return super.isEnabled() && selection.size() == 1
+				&& selectionMapsToSingleRepository();
 	}
 }
