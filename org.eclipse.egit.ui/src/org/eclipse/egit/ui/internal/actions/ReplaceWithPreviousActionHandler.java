568d5e5a4ca5fcaf4b31d123a64d3d9f1dc17023
Stefan Dirix <sdirix@eclipsesource.com>
2015-05-20 18:04:57 +0200

    Compare/ReplaceWithPrevious support for multiple resources (in mappings)
    The CompareWithPrevious and ReplaceWithPrevious actions now also support
    scenarios where the current selection consists of a single object but
    maps to multiple files (commonly via ResourceMappings).
    
    The previous commit is the next-to-last commit which touched any of the
    resources.
    
    Bug: 496916
    Change-Id: I1a208bd9949711e602309cac702a5a158b0eab4b
    Signed-off-by: Stefan Dirix <sdirix@eclipsesource.com>
    Signed-off-by: Philip Langer <planger@eclipsesource.com>
    Also-by: Laurent Delaigue <laurent.delaigue@obeo.fr>
    Also-by: Mathieu Cartaud <mathieu.cartaud@obeo.fr>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/ReplaceWithPreviousActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/ReplaceWithPreviousActionHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2012, Mathias Kinzler <mathias.kinzler@sap.com>
+ * Copyright (C) 2012, 2016 Mathias Kinzler <mathias.kinzler@sap.com>
  * and other copyright owners as documented in the project's IP log.
  *
  * All rights reserved. This program and the accompanying materials
@@ -11,16 +11,18 @@ package org.eclipse.egit.ui.internal.actions;
 
 import java.io.IOException;
 import java.text.MessageFormat;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.egit.ui.internal.CommonUtils;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.dialogs.CommitSelectDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.window.Window;
 import org.eclipse.jgit.revwalk.RevCommit;
 
@@ -34,33 +36,31 @@ public class ReplaceWithPreviousActionHandler extends
 	protected String gatherRevision(ExecutionEvent event)
 			throws ExecutionException {
 		IResource[] resources = getSelectedResources(event);
-		if (resources.length != 1)
-			throw new ExecutionException(
-					"Unexpected number of selected Resources"); //$NON-NLS-1$
 		try {
-			List<PreviousCommit> pcs = findPreviousCommits();
-			List<RevCommit> previousCommits = new ArrayList<>();
-			for (PreviousCommit pc: pcs)
-				previousCommits.add(pc.commit);
-			int parentCount = previousCommits.size();
+			List<RevCommit> pcs = findPreviousCommits(Arrays.asList(resources));
+
+			int parentCount = pcs.size();
 			if (parentCount == 0) {
+				final String resourceNames = CommonUtils
+						.getResourceNames(Arrays.asList(resources));
+
 				MessageDialog
 						.openError(
 								getShell(event),
 								UIText.ReplaceWithPreviousActionHandler_NoParentCommitDialogTitle,
 								MessageFormat
 										.format(UIText.ReplaceWithPreviousActionHandler_NoParentCommitDialogMessage,
-												resources[0].getName()));
+												resourceNames));
 				throw new OperationCanceledException();
 			} else if (parentCount > 1) {
 				CommitSelectDialog dlg = new CommitSelectDialog(
-						getShell(event), previousCommits);
+						getShell(event), pcs);
 				if (dlg.open() == Window.OK)
 					return dlg.getSelectedCommit().getName();
 				else
 					throw new OperationCanceledException();
 			} else
-				return previousCommits.get(0).getName();
+				return pcs.get(0).getName();
 		} catch (IOException e) {
 			throw new ExecutionException(e.getMessage(), e);
 		}
@@ -68,8 +68,8 @@ public class ReplaceWithPreviousActionHandler extends
 
 	@Override
 	public boolean isEnabled() {
-		IResource[] selectedResources = getSelectedResources();
-		return super.isEnabled() && selectedResources.length == 1 &&
-				selectionMapsToSingleRepository();
+		IStructuredSelection selection = getSelection();
+		return super.isEnabled() && selection.size() == 1
+				&& selectionMapsToSingleRepository();
 	}
 }
