150b37937c5e2e887c659c9e97e3bd17300c5bd2
Andre Bossert <anb0s@anbos.de>
2015-05-26 21:29:24 +0200

    added support for reading "diff and merge tool" from gitconfig
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=356832
    
    Change-Id: Id61b754844a245e0c1dbc71e236d0f7d1c3601b1
    Signed-off-by: Andre Bossert <anb0s@anbos.de>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIPreferences.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/UIPreferences.java
@@ -6,6 +6,7 @@
  * Copyright (C) 2012, Daniel Megert <daniel_megert@ch.ibm.com>
  * Copyright (C) 2013, Tobias Pfeifer <to.pfeifer@sap.com>
  * Copyright (C) 2014, Marc Khouzam <marc.khouzam@ericsson.com>
+ * Copyright (C) 2015, Andre Bossert <anb0s@anbos.de>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -165,6 +166,16 @@ public class UIPreferences {
 
 	/** */
 	public static final String MERGE_MODE = "merge_mode"; //$NON-NLS-1$
+
+	/** */
+	public static final String MERGE_TOOL = "merge_tool"; //$NON-NLS-1$
+	/** */
+	public static final String MERGE_TOOL_CUSTOM = "merge_tool_custom"; //$NON-NLS-1$
+	/** */
+	public static final String DIFF_TOOL = "diff_tool"; //$NON-NLS-1$
+	/** */
+	public static final String DIFF_TOOL_CUSTOM = "diff_tool_custom"; //$NON-NLS-1$
+
 	/** */
 	public static final String SHOW_REBASE_CONFIRM = "show_rebase_confirm"; //$NON-NLS-1$
 	/** */
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/preferences/GitPreferenceRoot.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/preferences/GitPreferenceRoot.java
@@ -3,6 +3,7 @@
  * Copyright (C) 2010, Robin Rosenberg <robin.rosenberg@dewire.com>
  * Copyright (C) 2010, Mathias Kinzler <mathias.kinzler@sap.com>
  * Copyright (C) 2013, Dariusz Luksza <dariusz.luksza@gmail.com>
+ * Copyright (C) 2015, Andre Bossert <anb0s@anbos.de>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -12,6 +13,10 @@
 package org.eclipse.egit.ui.internal.preferences;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.variables.IStringVariableManager;
@@ -38,6 +43,13 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.ConfigConstants;
+//import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.StoredConfig;
+//import org.eclipse.jgit.lib.UserConfig;
+import org.eclipse.jgit.util.FS;
+import org.eclipse.jgit.util.SystemReader;
 
 /** Root preference page for the all of our workspace preferences. */
 public class GitPreferenceRoot extends FieldEditorPreferencePage implements
@@ -45,6 +57,10 @@ public class GitPreferenceRoot extends FieldEditorPreferencePage implements
 	private final static int GROUP_SPAN = 3;
 
 	private final static String[][] MERGE_MODE_NAMES_AND_VALUES = new String[3][2];
+
+	private final static String[][] MERGE_TOOL_NAMES_AND_VALUES = new String[3][2];
+
+	private final static String[][] DIFF_TOOL_NAMES_AND_VALUES = new String[3][2];
 
 	private final static boolean HAS_DEBUG_UI = hasDebugUiBundle();
 
@@ -55,6 +71,24 @@ public class GitPreferenceRoot extends FieldEditorPreferencePage implements
 		MERGE_MODE_NAMES_AND_VALUES[1][1] = "1";//$NON-NLS-1$
 		MERGE_MODE_NAMES_AND_VALUES[2][0] = UIText.GitPreferenceRoot_MergeMode_2_Label;
 		MERGE_MODE_NAMES_AND_VALUES[2][1] = "2"; //$NON-NLS-1$
+	}
+
+	static {
+		MERGE_TOOL_NAMES_AND_VALUES[0][0] = UIText.GitPreferenceRoot_MergeTool_0_Label;
+		MERGE_TOOL_NAMES_AND_VALUES[0][1] = "0";//$NON-NLS-1$
+		MERGE_TOOL_NAMES_AND_VALUES[1][0] = UIText.GitPreferenceRoot_MergeTool_1_Label;
+		MERGE_TOOL_NAMES_AND_VALUES[1][1] = "1";//$NON-NLS-1$
+		MERGE_TOOL_NAMES_AND_VALUES[2][0] = UIText.GitPreferenceRoot_MergeTool_2_Label;
+		MERGE_TOOL_NAMES_AND_VALUES[2][1] = "2"; //$NON-NLS-1$
+	}
+
+	static {
+		DIFF_TOOL_NAMES_AND_VALUES[0][0] = UIText.GitPreferenceRoot_DiffTool_0_Label;
+		DIFF_TOOL_NAMES_AND_VALUES[0][1] = "0";//$NON-NLS-1$
+		DIFF_TOOL_NAMES_AND_VALUES[1][0] = UIText.GitPreferenceRoot_DiffTool_1_Label;
+		DIFF_TOOL_NAMES_AND_VALUES[1][1] = "1";//$NON-NLS-1$
+		DIFF_TOOL_NAMES_AND_VALUES[2][0] = UIText.GitPreferenceRoot_DiffTool_2_Label;
+		DIFF_TOOL_NAMES_AND_VALUES[2][1] = "2"; //$NON-NLS-1$
 	}
 
 	/**
@@ -199,7 +233,47 @@ public class GitPreferenceRoot extends FieldEditorPreferencePage implements
 		mergeMode.getLabelControl(mergeGroup).setToolTipText(
 				UIText.GitPreferenceRoot_MergeModeTooltip);
 		addField(mergeMode);
+
+		ComboFieldEditor mergeTool = new ComboFieldEditor(
+				UIPreferences.MERGE_TOOL,
+				UIText.GitPreferenceRoot_MergeToolLabel,
+				MERGE_TOOL_NAMES_AND_VALUES, mergeGroup);
+		mergeTool.getLabelControl(mergeGroup)
+				.setToolTipText(UIText.GitPreferenceRoot_MergeToolTooltip);
+		addField(mergeTool);
+
+		ComboFieldEditor mergeToolCustom = new ComboFieldEditor(
+				UIPreferences.MERGE_TOOL_CUSTOM,
+				UIText.GitPreferenceRoot_MergeToolCustomLabel,
+				getCustomMergeTools(),
+				mergeGroup);
+		mergeToolCustom.getLabelControl(mergeGroup).setToolTipText(
+				UIText.GitPreferenceRoot_MergeToolCustomTooltip);
+		addField(mergeToolCustom);
+
 		updateMargins(mergeGroup);
+
+		Group diffGroup = new Group(main, SWT.SHADOW_ETCHED_IN);
+		GridDataFactory.fillDefaults().grab(true, false).span(GROUP_SPAN, 1)
+				.applyTo(diffGroup);
+		diffGroup.setText(UIText.GitPreferenceRoot_DiffGroupHeader);
+
+		ComboFieldEditor diffTool = new ComboFieldEditor(
+				UIPreferences.DIFF_TOOL, UIText.GitPreferenceRoot_DiffToolLabel,
+				DIFF_TOOL_NAMES_AND_VALUES, diffGroup);
+		diffTool.getLabelControl(diffGroup)
+				.setToolTipText(UIText.GitPreferenceRoot_DiffToolTooltip);
+		addField(diffTool);
+
+		ComboFieldEditor diffToolCustom = new ComboFieldEditor(
+				UIPreferences.DIFF_TOOL_CUSTOM,
+				UIText.GitPreferenceRoot_DiffToolCustomLabel,
+				getCustomDiffTools(), diffGroup);
+		diffToolCustom.getLabelControl(diffGroup)
+				.setToolTipText(UIText.GitPreferenceRoot_DiffToolCustomTooltip);
+		addField(diffToolCustom);
+
+		updateMargins(diffGroup);
 
 		Group blameGroup = new Group(main, SWT.SHADOW_ETCHED_IN);
 		GridDataFactory.fillDefaults().grab(true, false).span(GROUP_SPAN, 1)
@@ -216,6 +290,67 @@ public class GitPreferenceRoot extends FieldEditorPreferencePage implements
 		addField(new BooleanFieldEditor(UIPreferences.CLONE_WIZARD_STORE_SECURESTORE,
 				UIText.GitPreferenceRoot_SecureStoreUseByDefault, secureGroup));
 		updateMargins(secureGroup);
+	}
+
+	private static StoredConfig loadUserScopedConfig() {
+		StoredConfig c = SystemReader.getInstance().openUserConfig(null,
+				FS.DETECTED);
+		try {
+			c.load();
+		} catch (IOException e) {
+			Activator.handleError(e.getMessage(), e, true);
+		} catch (ConfigInvalidException e) {
+			Activator.handleError(e.getMessage(), e, true);
+		}
+		return c;
+	}
+
+	private String[][] getCustomDiffOrMergeTools(String sectionName4AllTools,
+			String sectionName4DefaultTool) {
+		List<String> toolsList = new ArrayList<String>();
+		StoredConfig userScopedConfig = loadUserScopedConfig();
+		if (userScopedConfig != null) {
+			// get default diff / merge tool
+			String defaultDiffMergeTool = userScopedConfig
+					.getString(sectionName4DefaultTool, null, "tool"); //$NON-NLS-1$
+			// get all diff / merge tools
+			Set<String> diffMergeTools = userScopedConfig
+					.getSubsections(sectionName4AllTools);
+			// if no diff / merge tools found check the default one or none
+			if (diffMergeTools.size() == 0) {
+				if (defaultDiffMergeTool != null) {
+					toolsList.add(defaultDiffMergeTool);
+				} else {
+					toolsList.add("none"); //$NON-NLS-1$
+				}
+			} else {
+				// add default diff / merge tool if not in the list already
+				if (defaultDiffMergeTool != null
+						&& !diffMergeTools.contains(defaultDiffMergeTool)) {
+					toolsList.add(defaultDiffMergeTool);
+				}
+				for (String mergeTool : diffMergeTools) {
+					toolsList.add(mergeTool);
+				}
+			}
+		}
+		// convert to right type
+		String[][] toolsArray = new String[toolsList.size()][2];
+		for (int index = 0; index < toolsList.size(); index++) {
+			toolsArray[index][0] = toolsList.get(index);
+			toolsArray[index][1] = toolsList.get(index);
+		}
+		return toolsArray;
+	}
+
+	private String[][] getCustomMergeTools() {
+		return getCustomDiffOrMergeTools("mergetool", //$NON-NLS-1$
+				ConfigConstants.CONFIG_KEY_MERGE);
+	}
+
+	private String[][] getCustomDiffTools() {
+		return getCustomDiffOrMergeTools("difftool", //$NON-NLS-1$
+				ConfigConstants.CONFIG_DIFF_SECTION);
 	}
 
 	private void updateMargins(Group group) {
