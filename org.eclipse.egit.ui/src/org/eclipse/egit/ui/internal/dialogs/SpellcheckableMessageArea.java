b2fe00aed1f1ee611450986e325adad8f2d00fea
Dani Megert <Daniel_Megert@ch.ibm.com>
2015-06-08 15:33:34 +0200

    Fix broken spell checking in commit message and Staging view
    Bug: 428320
    Change-Id: I76514be1be4b8bcd1cf7e457416cdd7ada252be5
    Signed-off-by: Dani Megert <Daniel_Megert@ch.ibm.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/SpellcheckableMessageArea.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/SpellcheckableMessageArea.java
@@ -745,14 +745,33 @@ public class SpellcheckableMessageArea extends Composite {
 
 				@Override
 				public void textChanged(TextEvent event) {
-					textWidget.setStyleRanges(
-							new StyleRange[0]);
+					removeHyperlinkStyleRanges();
 					UIUtils.applyHyperlinkDetectorStyleRanges(sourceViewer,
 							configuration.getHyperlinkDetectors(sourceViewer));
 					if (undoAction != null)
 						undoAction.update();
 					if (redoAction != null)
 						redoAction.update();
+				}
+
+				private void removeHyperlinkStyleRanges() {
+					StyleRange[] hyperlinkStyleRanges = textWidget.getStyleRanges(true);
+					Color blue = Display.getDefault()
+							.getSystemColor(SWT.COLOR_BLUE);
+					Color white = Display.getDefault()
+							.getSystemColor(SWT.COLOR_WHITE);
+					for (int i = 0; i < hyperlinkStyleRanges.length; i++) {
+						StyleRange styleRange = hyperlinkStyleRanges[i];
+						if (styleRange.underline == true
+								&& styleRange.foreground == blue
+								&& styleRange.background == white) {
+							styleRange = (StyleRange) styleRange.clone();
+							styleRange.background = null;
+							styleRange.foreground = null;
+							styleRange.underline = false;
+							textWidget.setStyleRange(styleRange);
+						}
+					}
 				}
 			});
 
