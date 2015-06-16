1997c15f8a53f49789efd289da42c093a10d4925
Andre Bossert <anb0s@anbos.de>
2015-06-16 12:44:28 +0200

    Fixed confirmation dialog in case of trustExitCode.
    Show message about failed tool execution to user. Preparation for adding
    to index after merge success.
    
    Change-Id: Ia9ce4f505df0a320b67820e1639b30d9bcb455fb
    Signed-off-by: Andre Bossert <anb0s@anbos.de>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/MergeToolActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/MergeToolActionHandler.java
@@ -12,6 +12,7 @@
 package org.eclipse.egit.ui.internal.actions;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Map;
@@ -66,20 +67,20 @@ public class MergeToolActionHandler extends RepositoryActionHandler {
 			input = new GitMergeEditorInput(useWorkspace, locations);
 		}
 		if (GitPreferenceRoot.useExternalMergeTool()) {
-			openInCompareExternal(input);
+			openMergeToolExternal(input);
 		} else {
-			openInCompareInternal(input);
+			openMergeToolInternal(input);
 		}
 		return null;
 	}
 
-	private static void openInCompareInternal(CompareEditorInput input) {
+	private static void openMergeToolInternal(CompareEditorInput input) {
 		CompareUI.openCompareEditor(input);
 	}
 
-	private static void openInCompareExternal(CompareEditorInput input) {
+	private static void openMergeToolExternal(CompareEditorInput input) {
 		System.out.println(
-				"---------------- openInCompare with external tool ------------------"); //$NON-NLS-1$
+				"---------------- openMergeToolExternal ----------------"); //$NON-NLS-1$
 
 		final GitMergeEditorInput gitMergeInput = (GitMergeEditorInput) input;
 
@@ -121,11 +122,12 @@ public class MergeToolActionHandler extends RepositoryActionHandler {
 					FileRevisionTypedElement baseRevision = (FileRevisionTypedElement) node
 							.getAncestor();
 					// get file names
-					String mergedCompareFilePath = null;
-					String mergedCompareFileName = null;
-					String localCompareFilePath = null;
-					String remoteCompareFilePath = null;
-					String baseCompareFilePath = null;
+					String mergedAbsoluteFilePath = null;
+					String mergedRelativeFilePath = null;
+					String mergedFileName = null;
+					String localAbsoluteFilePath = null;
+					String remoteAbsoluteFilePath = null;
+					String baseAbsoluteFilePath = null;
 					String mergeCmd = null;
 					boolean prompt = false;
 					boolean trustExitCode = true;
@@ -135,16 +137,18 @@ public class MergeToolActionHandler extends RepositoryActionHandler {
 					File baseDir = null;
 					File tempDir = null;
 					if (leftResource != null) {
-						mergedCompareFilePath = leftResource.getRawLocation()
+						mergedAbsoluteFilePath = leftResource.getRawLocation()
 								.toOSString();
-						mergedCompareFileName = leftResource.getName();
+						mergedFileName = leftResource.getName();
 						baseDir = leftResource.getRawLocation().toFile()
 								.getParentFile();
-						System.out.println("mergedCompareFilePath: " //$NON-NLS-1$
-								+ mergedCompareFilePath);
+						System.out.println("file: " //$NON-NLS-1$
+								+ mergedAbsoluteFilePath);
 					}
-					if (mergedCompareFilePath != null && leftRevision != null
+					if (mergedAbsoluteFilePath != null && leftRevision != null
 							&& rightRevision != null) {
+						// get the relative project path from left revision here
+						mergedRelativeFilePath = leftRevision.getPath();
 						// get the tool
 						ITool tool = GitPreferenceRoot.getExternalMergeTool();
 						if (tool != null) {
@@ -171,7 +175,7 @@ public class MergeToolActionHandler extends RepositoryActionHandler {
 								int response = ToolsUtils.askUserAboutToolExecution(
 										"mergetool", //$NON-NLS-1$
 										"Merging file: " //$NON-NLS-1$
-												+ mergedCompareFilePath
+														+ mergedRelativeFilePath
 												+ "\n\nLaunch '" //$NON-NLS-1$
 												+ tool.getName()
 												+ "' ?"); //$NON-NLS-1$
@@ -187,56 +191,76 @@ public class MergeToolActionHandler extends RepositoryActionHandler {
 										.createDirectoryForTempFiles();
 								baseDir = tempDir;
 							}
-							localCompareFilePath = ToolsUtils.loadToTempFile(baseDir,
-									mergedCompareFileName, "LOCAL", //$NON-NLS-1$
+							localAbsoluteFilePath = ToolsUtils.loadToTempFile(baseDir,
+									mergedFileName, "LOCAL", //$NON-NLS-1$
 									leftRevision, writeToTemp);
-							remoteCompareFilePath = ToolsUtils.loadToTempFile(baseDir,
-									mergedCompareFileName, "REMOTE", //$NON-NLS-1$
+							remoteAbsoluteFilePath = ToolsUtils.loadToTempFile(baseDir,
+									mergedFileName, "REMOTE", //$NON-NLS-1$
 									rightRevision, writeToTemp);
-							baseCompareFilePath = ToolsUtils.loadToTempFile(baseDir,
-									mergedCompareFileName, "BASE", //$NON-NLS-1$
+							baseAbsoluteFilePath = ToolsUtils.loadToTempFile(baseDir,
+									mergedFileName, "BASE", //$NON-NLS-1$
 									baseRevision, writeToTemp);
 						}
 					}
 					// execute
-					int exitCode = ToolsUtils.executeTool(mergedCompareFilePath,
-							localCompareFilePath,
-							remoteCompareFilePath, baseCompareFilePath,
-							mergeCmd, tempDir);
-					// delete temp
-					if (tempDir != null && !keepTemporaries) {
-						ToolsUtils.deleteDirectoryForTempFiles(tempDir);
+					int exitCode = -1;
+					try {
+						exitCode = ToolsUtils.executeTool(mergedAbsoluteFilePath,
+								localAbsoluteFilePath, remoteAbsoluteFilePath,
+								baseAbsoluteFilePath, mergeCmd, tempDir);
+					} catch (IOException | InterruptedException e) {
+						e.printStackTrace();
+						ToolsUtils.informUserAboutError("mergetool - error", //$NON-NLS-1$
+								e.getMessage() + "\n\nMerge aborted!"); //$NON-NLS-1$
+						return; // abort the merge process
+					} finally {
+						System.out.println("exitCode: " //$NON-NLS-1$
+								+ Integer.toString(exitCode));
+						// delete temp
+						if (tempDir != null && !keepTemporaries) {
+							ToolsUtils.deleteDirectoryForTempFiles(tempDir);
+						}
 					}
 					// add file to stage if successfully merged
-					if (exitCode == 0) {
-						if (!trustExitCode) {
-							int response = ToolsUtils.askUserAboutToolExecution(
-									"mergetool", //$NON-NLS-1$
-									"Merging file: " //$NON-NLS-1$
-											+ mergedCompareFilePath
-											+ "\n\nWas the merge successful?"); //$NON-NLS-1$
-							if (response == SWT.YES) {
-								/*
-								 * TODO: implement add
-								AddCommand addCommand = new Git(repo).add();
-								boolean fileAdded = false;
-								for (String path : notTracked)
-									if (commitFileList.contains(path)) {
-										addCommand.addFilepattern(path);
-										fileAdded = true;
-									}
-								if (fileAdded)
-									try {
-										addCommand.call();
-									} catch (Exception e) {
-										throw new CoreException(Activator
-												.error(e.getMessage(), e));
-									}
-								*/
-							} else if (response == SWT.CANCEL) {
-								return;
-							}
+					boolean addFile = false;
+					if (trustExitCode) {
+						System.out.println("trustExitCode: true"); //$NON-NLS-1$
+						if (exitCode == 0) {
+							addFile = true;
 						}
+					} else {
+						System.out.println("trustExitCode: false"); //$NON-NLS-1$
+						int response = ToolsUtils.askUserAboutToolExecution(
+								"mergetool - trustExitCode: false", //$NON-NLS-1$
+								"Merging file: " //$NON-NLS-1$
+										+ mergedRelativeFilePath
+										+ "\n\nWas the merge successful?"); //$NON-NLS-1$
+						if (response == SWT.YES) {
+							addFile = true;
+						} else if (response == SWT.CANCEL) {
+							return; // abort the merge process
+						}
+					}
+					if (addFile) {
+						System.out.println("addFile: " //$NON-NLS-1$
+								+ mergedFileName);
+						/*
+						 * TODO: implement add
+						AddCommand addCommand = new Git(repo).add();
+						boolean fileAdded = false;
+						for (String path : notTracked)
+							if (commitFileList.contains(path)) {
+								addCommand.addFilepattern(path);
+								fileAdded = true;
+							}
+						if (fileAdded)
+							try {
+								addCommand.call();
+							} catch (Exception e) {
+								throw new CoreException(Activator
+										.error(e.getMessage(), e));
+							}
+						*/
 					}
 					break;
 				}
