d7689385555e4aba6fc58025387224ebf87bd03c
Max Hohenegger <eclipse@hohenegger.eu>
2015-06-23 10:52:31 +0200

    [gitflow] add UI tests
    - git flow init
    - git flow feature start/stop
    - git flow feature rebase
    
    Bug: 470710
    Change-Id: I4dd936abcb2250ba2b358d01b61319948114fc99
    Signed-off-by: Max Hohenegger <eclipse@hohenegger.eu>

---
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/gitflow/FeatureRebaseHandlerTest.java
@@ -0,0 +1,103 @@
+/*******************************************************************************
+ * Copyright (C) 2015, Max Hohenegger <eclipse@hohenegger.eu>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.gitflow;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.egit.core.op.BranchOperation;
+import org.eclipse.egit.gitflow.GitFlowRepository;
+import org.eclipse.egit.gitflow.op.FeatureCheckoutOperation;
+import org.eclipse.egit.gitflow.op.FeatureStartOperation;
+import org.eclipse.egit.gitflow.op.InitOperation;
+import org.eclipse.egit.gitflow.ui.Activator;
+import org.eclipse.egit.gitflow.ui.internal.JobFamilies;
+import org.eclipse.egit.gitflow.ui.internal.UIText;
+import org.eclipse.egit.ui.test.ContextMenuHelper;
+import org.eclipse.egit.ui.test.JobJoiner;
+import org.eclipse.egit.ui.test.TestUtil;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.Status;
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.waits.Conditions;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
+import org.eclipse.ui.PlatformUI;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Tests for the Team->Gitflow->Rebase Feature actions
+ */
+@RunWith(SWTBotJunit4ClassRunner.class)
+public class FeatureRebaseHandlerTest extends AbstractGitflowHandlerTest {
+
+	@Test
+	public void testRebase() throws Exception {
+		Git git = Git.wrap(repository);
+		String featureName = "myFeature";
+		String develop = "develop";
+
+		init();
+
+		createFeature(featureName);
+		setContentAddAndCommit("foo");
+
+		checkoutBranch(develop);
+		setContentAddAndCommit("bar");
+
+		checkoutFeature(featureName);
+
+		rebaseFeature();
+
+		Status call = git.status().call();
+		Object[] conflicting = call.getConflicting().toArray();
+		assertEquals(1, conflicting.length);
+		assertEquals(FILE1_PATH, conflicting[0]);
+
+		assertEquals("org.eclipse.egit.ui.InteractiveRebaseView", bot.activeView().getReference().getId());
+	}
+
+	private void init() throws CoreException {
+		new InitOperation(repository).execute(null);
+	}
+
+	private void createFeature(String featureName) throws CoreException {
+		new FeatureStartOperation(new GitFlowRepository(repository),
+				featureName).execute(null);
+	}
+
+	private void checkoutFeature(String featureName) throws CoreException {
+		new FeatureCheckoutOperation(new GitFlowRepository(repository), featureName).execute(null);
+	}
+
+	private void checkoutBranch(String branchToCheckout) throws CoreException {
+		new BranchOperation(repository, branchToCheckout).execute(null);
+	}
+
+	private void rebaseFeature() {
+		final SWTBotTree projectExplorerTree = TestUtil.getExplorerTree();
+		getProjectItem(projectExplorerTree, PROJ1).select();
+		final String[] menuPath = new String[] {
+				util.getPluginLocalizedValue("TeamMenu.label"),
+				util.getPluginLocalizedValue("TeamGitFlowMenu.name", false, Activator.getDefault().getBundle()),
+				util.getPluginLocalizedValue("TeamGitFlowFeatureRebase.name", false, Activator.getDefault().getBundle()) };
+		JobJoiner jobJoiner = JobJoiner.startListening(JobFamilies.GITFLOW_FAMILY, 60, TimeUnit.SECONDS);
+		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+			@Override
+			public void run() {
+				ContextMenuHelper.clickContextMenuSync(projectExplorerTree, menuPath);
+			}
+		});
+		bot.waitUntil(Conditions.shellIsActive(UIText.FeatureRebaseHandler_conflicts));
+		bot.button().click();
+		jobJoiner.join();
+	}
+}
--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/TestUtil.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/TestUtil.java
@@ -27,6 +27,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -71,6 +72,7 @@ import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.osgi.util.tracker.ServiceTracker;
@@ -90,7 +92,7 @@ public class TestUtil {
 
 	private final static char AMPERSAND = '&';
 
-	private ResourceBundle myBundle;
+	private Map<Bundle, ResourceBundle> bundle2ResourceBundle = new HashMap<>();
 
 	/**
 	 * Allows access to the localized values of the EGit UI Plug-in
@@ -126,10 +128,28 @@ public class TestUtil {
 	 */
 	public synchronized String getPluginLocalizedValue(String key,
 			boolean keepAmpersands) throws MissingResourceException {
-		if (myBundle == null) {
+		return getPluginLocalizedValue(key, keepAmpersands, Activator.getDefault().getBundle());
+	}
 
-			BundleContext context = Activator.getDefault().getBundle()
-					.getBundleContext();
+	/**
+	 * Allows access to the localized values of the given Bundle
+	 * <p>
+	 *
+	 * @param key
+	 *            see {@link #getPluginLocalizedValue(String)}
+	 * @param keepAmpersands
+	 *            if <code>true</code>, ampersands will be kept
+	 * @param bundle
+	 *            the Bundle that contains the localization
+	 * @return see {@link #getPluginLocalizedValue(String)}
+	 * @throws MissingResourceException
+	 *             see {@link #getPluginLocalizedValue(String)}
+	 */
+	public synchronized String getPluginLocalizedValue(String key,
+			boolean keepAmpersands, Bundle bundle) throws MissingResourceException {
+		ResourceBundle myBundle = bundle2ResourceBundle.get(bundle);
+		if (myBundle == null) {
+			BundleContext context = bundle.getBundleContext();
 
 			ServiceTracker<BundleLocalization, BundleLocalization> localizationTracker =
 					new ServiceTracker<BundleLocalization, BundleLocalization>(
@@ -137,9 +157,11 @@ public class TestUtil {
 			localizationTracker.open();
 
 			BundleLocalization location = localizationTracker.getService();
-			if (location != null)
-				myBundle = location.getLocalization(Activator.getDefault()
-						.getBundle(), Locale.getDefault().toString());
+			if (location != null) {
+				myBundle = location.getLocalization(bundle, Locale.getDefault().toString());
+				bundle2ResourceBundle.put(bundle, myBundle);
+			}
+
 		}
 		if (myBundle != null) {
 			String raw = myBundle.getString(key);
