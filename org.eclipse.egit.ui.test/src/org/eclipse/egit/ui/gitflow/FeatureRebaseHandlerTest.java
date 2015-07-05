b6d20d9e43e9c824f4f49696d6fe42d9377740f8
Max Hohenegger <eclipse@hohenegger.eu>
2015-07-05 23:48:14 +0200

    [gitflow] Feature finish squash should allow changing the commit message
    - open standard commit message edit dialog to change squashed message
    - added UI test for feature finish squash
    - pulled up some redundant test code
    
    Bug: 473550 
    Change-Id: I028a05f21c7ca3f3a501dba160a3ed4ff035c87b
    Signed-off-by: Max Hohenegger <eclipse@hohenegger.eu>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/gitflow/FeatureRebaseHandlerTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/gitflow/FeatureRebaseHandlerTest.java
@@ -14,7 +14,6 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.egit.core.op.BranchOperation;
 import org.eclipse.egit.gitflow.GitFlowRepository;
 import org.eclipse.egit.gitflow.op.FeatureCheckoutOperation;
-import org.eclipse.egit.gitflow.op.FeatureStartOperation;
 import org.eclipse.egit.gitflow.op.InitOperation;
 import org.eclipse.egit.gitflow.ui.Activator;
 import org.eclipse.egit.gitflow.ui.internal.JobFamilies;
@@ -104,12 +103,7 @@ public class FeatureRebaseHandlerTest extends AbstractGitflowHandlerTest {
 		new InitOperation(repository).execute(null);
 	}
 
-	private void createFeature(String featureName) throws CoreException {
-		new FeatureStartOperation(new GitFlowRepository(repository),
-				featureName).execute(null);
-	}
-
-	private void checkoutFeature(String featureName) throws CoreException {
+	protected void checkoutFeature(String featureName) throws CoreException {
 		new FeatureCheckoutOperation(new GitFlowRepository(repository), featureName).execute(null);
 	}
 
