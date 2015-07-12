c63183c625ec906d1d8cd2f542164dc2820ea748
Peter Karena <peter.karena@arcor.de>
2015-07-12 15:51:32 +0200

    Multi push support
    In comparison to the multi pull dialog, the multi push is supported. 
    
    Change-Id: Ic2a6954a2e1d8d5884f546cfd28b3089c5cae5b6
    Signed-off-by: Peter Karena <peter.karena@arcor.de>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/PushUpstreamOrBranchActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/PushUpstreamOrBranchActionHandler.java
@@ -8,19 +8,11 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.actions;
 
-import java.io.IOException;
-
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.egit.ui.internal.push.PushBranchWizard;
-import org.eclipse.egit.ui.internal.push.PushOperationUI;
-import org.eclipse.egit.ui.internal.push.SimpleConfigurePushDialog;
-import org.eclipse.jface.wizard.WizardDialog;
-import org.eclipse.jgit.lib.Constants;
+import org.eclipse.egit.ui.internal.push.MultiPushOperationUI;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.transport.RemoteConfig;
-import org.eclipse.swt.widgets.Shell;
 
 /**
  * Action for "Push to Upstream" or "Push Branch..." if not configured
@@ -28,64 +20,31 @@ import org.eclipse.swt.widgets.Shell;
 public class PushUpstreamOrBranchActionHandler extends RepositoryActionHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		final Repository repository = getRepository(true, event);
-		if (repository == null)
+		final Repository[] repositories = getRepositories(event);
+
+		if (repositories == null) {
 			return null;
-		Shell shell = getShell(event);
-		RemoteConfig config = SimpleConfigurePushDialog
-				.getConfiguredRemote(repository);
-
-		pushOrConfigure(repository, config, shell);
-		return null;
-	}
-
-	/**
-	 * @param repository
-	 * @param config
-	 * @param shell
-	 */
-	public static void pushOrConfigure(final Repository repository,
-			RemoteConfig config, Shell shell) {
-		if (config != null) {
-			PushOperationUI op = new PushOperationUI(repository,
-					config.getName(), false);
-			op.start();
-		} else {
-			Ref head = getHeadIfSymbolic(repository);
-			if (head != null) {
-				PushBranchWizard pushBranchWizard = new PushBranchWizard(
-						repository, head);
-
-				WizardDialog dlg = new WizardDialog(shell,
-						pushBranchWizard);
-				dlg.open();
-			}
 		}
+
+		MultiPushOperationUI multiPushOperationUI = new MultiPushOperationUI();
+		multiPushOperationUI.setStatusAndPush(repositories);
+
+		return null;
 	}
 
 	@Override
 	public boolean isEnabled() {
-		final Repository repository = getRepository();
-		if (repository == null)
-			return false;
+		final Repository[] repositories = getRepositories();
+		for (final Repository repository : repositories) {
+			if (repository == null) {
+				return false;
+			}
 
-		Ref head = getHeadIfSymbolic(repository);
-		if (head == null)
-			return false;
-
+			final Ref head = MultiPushOperationUI.getHeadIfSymbolic(repository);
+			if (head == null) {
+				return false;
+			}
+		}
 		return true;
 	}
-
-	private static Ref getHeadIfSymbolic(Repository repository) {
-		try {
-			Ref head = repository.getRef(Constants.HEAD);
-			if (head != null && head.isSymbolic())
-				return head;
-			else
-				return null;
-		} catch (IOException e) {
-			return null;
-		}
-	}
-
 }
