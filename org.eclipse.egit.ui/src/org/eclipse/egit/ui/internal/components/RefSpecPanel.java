9b93e12d17de289d01e01ac455537f8946bd5bde
Karsten Panier <karsten.panier@aysada.de>
2015-07-24 20:47:44 +0200

    Fix unexpected tab order in push dialog's "Push refspec" page
    "Add refspec" was at the end of the tab order. Instead it should come
    after the destination ref field in the tab order.
    
    Bug: 441357
    Change-Id: Ic0941aacdb4629de052e3a24865421366054cdda
    Signed-off-by: Karsten Panier <karsten.panier@aysada.de>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RefSpecPanel.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RefSpecPanel.java
@@ -1,10 +1,13 @@
 /*******************************************************************************
- * Copyright (C) 2008, 2012 Marek Zawirski <marek.zawirski@gmail.com> and others.
+ * Copyright (C) 2008, 2015 Marek Zawirski <marek.zawirski@gmail.com> and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *           Karsten Panier <karsten.panier@aysada.org> - Bug 441357
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.components;
 
@@ -753,6 +756,9 @@ public class RefSpecPanel {
 		};
 		creationSrcCombo.addModifyListener(validator);
 		creationDstCombo.addModifyListener(validator);
+		Control[] tabList = new Control[] { creationSrcCombo, creationDstCombo,
+				creationButton };
+		creationPanel.setTabList(tabList);
 	}
 
 	private void createDeleteCreationPanel() {
