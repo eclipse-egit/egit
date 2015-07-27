647c9c4eaef02c826f818cf841ef04487d5950b7
Max Hohenegger <eclipse@hohenegger.eu>
2015-07-27 18:11:23 +0200

    [gitflow] Fixed 'Merges do not appear to be using --no-ff flag'
    Bug 470626 
    Signed-off-by: Max Hohenegger <eclipse@hohenegger.eu>
    Change-Id: I7a7c3a57a78930f95c0775f99eed2e99bf856544

--- org.eclipse.egit.gitflow/src/org/eclipse/egit/gitflow/op/GitFlowOperation.java
+++ org.eclipse.egit.gitflow/src/org/eclipse/egit/gitflow/op/GitFlowOperation.java
@@ -29,6 +29,7 @@ import org.eclipse.egit.gitflow.GitFlowRepository;
 import org.eclipse.egit.gitflow.internal.CoreText;
 import org.eclipse.jgit.api.CheckoutResult;
 import org.eclipse.jgit.api.CheckoutResult.Status;
+import static org.eclipse.jgit.api.MergeCommand.FastForwardMode.NO_FF;
 import org.eclipse.jgit.api.MergeResult;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Ref;
@@ -173,6 +174,7 @@ abstract public class GitFlowOperation implements IEGitOperation {
 			if (squash) {
 				mergeOperation.setCommit(true);
 			}
+			mergeOperation.setFastForwardMode(NO_FF);
 			mergeOperation.execute(monitor);
 
 			return mergeOperation.getResult();
