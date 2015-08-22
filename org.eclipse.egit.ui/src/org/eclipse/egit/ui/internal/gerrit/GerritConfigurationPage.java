92de59d06e5eb67856257a3eafac2e83febbb916
Thomas Wolf <thomas.wolf@paranor.ch>
2015-08-22 23:05:28 +0200

    Respect push URL's port configuration in Gerrit configuration
    If the push URI already is ssh, it most likely already has a port,
    and if the user cloned the repo from Gerrit, it will even be correct.
    Replacing that by Gerrit's default ssh port of 29418 is most confusing
    if that Gerrit runs its sshd on a non-standard port.
    
    Bug: 475740
    Change-Id: I6043be2c101e5efd68de66bfff851b03f0ee79b2
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/gerrit/GerritConfigurationPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/gerrit/GerritConfigurationPage.java
@@ -272,7 +272,7 @@ class GerritConfigurationPage extends WizardPage {
 
 	private void setDefaults(URIish uri, String targetBranch) {
 		URIish newPushURI = uri;
-		if (Protocol.SSH.handles(uri)) {
+		if (Protocol.SSH.handles(uri) && uri.getPort() < 0) {
 			newPushURI = newPushURI.setPort(GERRIT_DEFAULT_SSH_PORT);
 		} else if (Protocol.GIT.handles(uri)) {
 			newPushURI = newPushURI.setScheme(Protocol.SSH.getDefaultScheme());
