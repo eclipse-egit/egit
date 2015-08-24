b7e29e7aac150f74b093a081926fb94f73640cb8
Max Hohenegger <eclipse@hohenegger.eu>
2015-08-24 10:09:58 +0200

    Fix NPE on FeatureCheckout
    - added @Nullable annotation
    - check for null on each path
    - prevent handler from being enabled on non-initialized repositories
    
    Bug: 475701
    Change-Id: Ibf46f80530496f256fb668b857ff435defc2fd6c
    Signed-off-by: Max Hohenegger <eclipse@hohenegger.eu>

--- org.eclipse.egit.gitflow.ui/src/org/eclipse/egit/gitflow/ui/internal/actions/ReleaseFinishHandler.java
+++ org.eclipse.egit.gitflow.ui/src/org/eclipse/egit/gitflow/ui/internal/actions/ReleaseFinishHandler.java
@@ -39,6 +39,9 @@ public class ReleaseFinishHandler extends AbstractGitFlowHandler {
 	@Override
 	public Object execute(final ExecutionEvent event) throws ExecutionException {
 		final GitFlowRepository gfRepo = GitFlowHandlerUtil.getRepository(event);
+		if (gfRepo == null) {
+			return error(UIText.Handlers_noGitflowRepositoryFound);
+		}
 
 		final ReleaseFinishOperation releaseFinishOperation;
 		try {
