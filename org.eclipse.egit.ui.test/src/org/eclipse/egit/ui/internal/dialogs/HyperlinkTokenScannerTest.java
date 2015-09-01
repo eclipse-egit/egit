c7ef959ae1b507eccf9563d8848a4a4d5ea31092
Thomas Wolf <thomas.wolf@paranor.ch>
2015-09-01 22:22:03 +0200

    Fix hyperlink display in SpellcheckableMessageArea
    This is first and foremost a syntax coloring problem, and should be
    handled as such. This change implements that:
    
    * Adds a token scanner that recognizes hyperlinks using the configured
      hyperlink detectors.
    * Adds a presentation reconciler with a damager/repairer that uses this
      token scanner.
    * Gets rid of the overridden hideHyperlinks() method -- if we do proper
      syntax coloring, this can wreck havoc when the text is edited (the
      super implementation does some StyleRange cleanup; if that is
      omitted, a stale and possibly out-of-bounds range remains).
    * Uses SWT.UNDERLINE_LINK to style hyperlink partitions.
    * Adds a few unit tests for the tokenizing.
    * Adds org.objenesis to required bundles in org.eclipse.egit.ui.test
      to enable mocking.
    
    In my manual UI tests, this syntax-colors hyperlinks nicely in the
    staging view, spell-checking works, auto-wrap works, whitespace display
    works, the "hand" pointer appears only over hyperlinks, and bug 471355
    is solved. It also has no adverse effects on the commit viewer or the
    git history view.
    
    Bug: 471355
    Change-Id: I623ead62d9600028973353cf63183638a1e06a3e
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

---
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/internal/dialogs/HyperlinkTokenScannerTest.java
@@ -0,0 +1,122 @@
+/*******************************************************************************
+ * Copyright (C) 2015, Thomas Wolf <thomas.wolf@paranor.ch>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.dialogs;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+
+import org.eclipse.jface.text.Document;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.hyperlink.IHyperlinkDetector;
+import org.eclipse.jface.text.hyperlink.URLHyperlinkDetector;
+import org.eclipse.jface.text.rules.IToken;
+import org.eclipse.jface.text.source.ISourceViewer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class HyperlinkTokenScannerTest {
+
+	@Mock
+	private ISourceViewer viewer;
+
+	private IHyperlinkDetector[] detectors = new IHyperlinkDetector[] {
+			new URLHyperlinkDetector() };
+
+	@Test
+	public void tokenizeEmpty() {
+		String testString = "";
+		String expected = "";
+		assertTokens(testString, 0, testString.length(), expected);
+	}
+
+	@Test
+	public void tokenizeNoHyperlinks() {
+		String testString = "hello world";
+		String expected = "DDDDDDDDDDD";
+		assertTokens(testString, 0, testString.length(), expected);
+	}
+
+	@Test
+	public void tokenizeLeadingHyperlink() {
+		String testString = "http://foo bar";
+		String expected = "HHHHHHHHHHDDDD";
+		assertTokens(testString, 0, testString.length(), expected);
+	}
+
+	@Test
+	public void tokenizeEmbeddedHyperlink() {
+		String testString = "Link: http://foo bar";
+		String expected = "DDDDDDHHHHHHHHHHDDDD";
+		assertTokens(testString, 0, testString.length(), expected);
+	}
+
+	@Test
+	public void tokenizeMultipleHyperlinksSimple() {
+		String testString = "Link: http://foo http://www.example.com bar";
+		String expected = "DDDDDDHHHHHHHHHHDHHHHHHHHHHHHHHHHHHHHHHDDDD";
+		assertTokens(testString, 0, testString.length(), expected);
+	}
+
+	@Test
+	public void tokenizeMultipleHyperlinksMultiline() {
+		String testString = "Link: http://foo\n\n* http://foo\n* ftp://somewhere\n\nTwo links above.";
+		String expected = "DDDDDDHHHHHHHHHHDDDDHHHHHHHHHHDDDHHHHHHHHHHHHHHHDDDDDDDDDDDDDDDDDD";
+		assertTokens(testString, 0, testString.length(), expected);
+	}
+
+	@Test
+	public void tokenizeHyperlinksOutsideRegion() {
+		String testString = "Link: http://foo\n\n* http://foo\n* ftp://somewhere\n\nTwo links above.";
+		String expected = "DDDDD                                                             ";
+		assertTokens(testString, 0, 5, expected);
+		expected = "                                                  DDDDDDDDDDDD    ";
+		assertTokens(testString, 50, 12, expected);
+	}
+
+	@Test
+	public void tokenizeHyperlinksCutByRegion() {
+		String testString = "Link: http://foo\n\n* http://foo\n* ftp://somewhere\n\nTwo links above.";
+		String expected = "DDDDDDHHHHHHHH                                                    ";
+		assertTokens(testString, 0, 14, expected);
+		expected = "               HDDDDHHHHHHHHH                                     ";
+		assertTokens(testString, 15, 14, expected);
+	}
+
+	private void assertTokens(String text, int offset, int length,
+			String expected) {
+		assertEquals("Test definition problem: 'expected' length mismatch",
+				text.length(), expected.length());
+		IDocument testDocument = new Document(text);
+		when(viewer.getDocument()).thenReturn(testDocument);
+		HyperlinkTokenScanner scanner = new HyperlinkTokenScanner(detectors,
+				viewer);
+		scanner.setRange(testDocument, offset, length);
+		IToken token = null;
+		char[] found = new char[text.length()];
+		Arrays.fill(found, ' ');
+		while (!(token = scanner.nextToken()).isEOF()) {
+			int tokenOffset = scanner.getTokenOffset();
+			int tokenLength = scanner.getTokenLength();
+			char ch = 'x';
+			if (token == HyperlinkTokenScanner.HYPERLINK) {
+				ch = 'H';
+			} else if (token == HyperlinkTokenScanner.DEFAULT) {
+				ch = 'D';
+			}
+			Arrays.fill(found, tokenOffset, tokenOffset + tokenLength, ch);
+		}
+		assertEquals("Unexpected tokens", expected, new String(found));
+	}
+
+}
