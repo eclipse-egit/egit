bf51cd93b4959b7157ccd80002f12956682169ec
Axel Richard <axel.richard@obeo.fr>
2015-09-03 17:51:24 +0200

    [wip] Use a workspace-aware merging strategy when working from EGit
    This introduces an Eclipse-specific sub-class of the
    RecursiveMerger that will make use of the mergers provided
    by ModelProviders instead of ignoring them to always use
    EGit's custom merging algorithms.
    
    Any file which does not have a specific merger (or which
    merger cannot be found, such as for files outside of the
    workspace) is still handled as it would by the
    RecursiveMerger.
    
    Change-Id: I1b27061ab215ebe600baef56f4e19a038c4b31a8
    Also-by: Laurent Goubet <laurent.goubet@obeo.fr>
    Signed-off-by: Axel Richard <axel.richard@obeo.fr>
    Signed-off-by: Laurent Delaigue <laurent.delaigue@obeo.fr>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

---
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/storage/GitLocalResourceVariant.java
@@ -0,0 +1,80 @@
+/******************************************************************************
+ * Copyright (c) 2015 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Laurent Goubet <laurent.goubet@obeo.fr> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.egit.core.internal.storage;
+
+//CHECKSTYLE:OFF
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IStorage;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.team.core.TeamException;
+import org.eclipse.team.core.variants.IResourceVariant;
+
+/**
+ * Used by the GitSourceVariantTree when it needs to include local changes.
+ * <p>
+ * Mimics Team's LocalResourceVariant.
+ * </p>
+ * 
+ * @author <a href="mailto:laurent.goubet@obeo.fr">Laurent Goubet</a>
+ * @author <a href="mailto:laurent.delaigue@obeo.fr">Laurent Delaigue</a>
+ */
+public class GitLocalResourceVariant implements IResourceVariant {
+	private final IResource resource;
+
+	/**
+	 * Default constructor.
+	 *
+	 * @param resource
+	 *            The resource we need a local variant for.
+	 */
+	public GitLocalResourceVariant(IResource resource) {
+		this.resource = resource;
+	}
+
+	public byte[] asBytes() {
+		try {
+			return getContentIdentifier().getBytes("UTF-8"); //$NON-NLS-1$
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public String getContentIdentifier() {
+		return new Date(resource.getLocalTimeStamp()).toString();
+	}
+
+	public IStorage getStorage(IProgressMonitor monitor) throws TeamException {
+		if (resource.getType() == IResource.FILE) {
+			return (IFile)resource;
+		}
+		return null;
+	}
+
+	public boolean isContainer() {
+		return resource.getType() != IResource.FILE;
+	}
+
+	public String getName() {
+		return resource.getName();
+	}
+
+	/**
+	 * @return the underlying resource of this variant.
+	 */
+	public IResource getResource() {
+		return resource;
+	}
+}
+// CHECKSTYLE:ON
