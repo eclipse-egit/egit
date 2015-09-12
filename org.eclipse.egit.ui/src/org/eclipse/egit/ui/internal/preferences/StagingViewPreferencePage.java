be241894b4b50e45385740df8cbdda96514c94b1
Andrey Loskutov <loskutov@gmx.de>
2015-09-13 00:03:11 +0200

    Introduce new preference page for Staging view
    Bug: 474064
    Change-Id: Ief5d80004c03a76f046339818ee37b56fd70b5ae
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/preferences/StagingViewPreferencePage.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (C) 2015 Andrey Loskutov <loskutov@gmx.de>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.preferences;
+
+import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.UIPreferences;
+import org.eclipse.egit.ui.internal.UIText;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.IntegerFieldEditor;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+/** Preferences for committing with commit dialog/staging view. */
+public class StagingViewPreferencePage extends FieldEditorPreferencePage
+		implements IWorkbenchPreferencePage {
+
+	/** */
+	public StagingViewPreferencePage() {
+		super(GRID);
+		setTitle(UIText.StagingViewPreferencePage_title);
+	}
+
+	@Override
+	public void init(IWorkbench workbench) {
+		// Nothing to do
+	}
+
+	@Override
+	protected IPreferenceStore doGetPreferenceStore() {
+		return Activator.getDefault().getPreferenceStore();
+	}
+
+	@Override
+	protected void createFieldEditors() {
+		Composite main = getFieldEditorParent();
+
+		IntegerFieldEditor historySize = new IntegerFieldEditor(
+				UIPreferences.STAGING_VIEW_MAX_LIMIT_LIST_MODE,
+				UIText.StagingViewPreferencePage_maxLimitListMode, main);
+		addField(historySize);
+	}
+
+
+}
