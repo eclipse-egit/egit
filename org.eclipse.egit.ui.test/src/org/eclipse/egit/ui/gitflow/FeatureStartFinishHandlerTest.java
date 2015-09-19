9088b6b19b49de5a070607a14e9afb91c4568ed4
Max Hohenegger <eclipse@hohenegger.eu>
2015-09-19 18:19:11 +0200

    Refactored feature finish UI tests to reduce redundancy
    Change-Id: I4f70f569736d171eddfc11088bb210939eede659
    Signed-off-by: Max Hohenegger <eclipse@hohenegger.eu>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/gitflow/FeatureStartFinishHandlerTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/gitflow/FeatureStartFinishHandlerTest.java
@@ -8,24 +8,12 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.gitflow;
 
-import static org.eclipse.swtbot.swt.finder.waits.Conditions.shellIsActive;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.egit.core.op.BranchOperation;
 import org.eclipse.egit.gitflow.GitFlowRepository;
-import org.eclipse.egit.gitflow.op.InitOperation;
-import org.eclipse.egit.gitflow.ui.Activator;
-import org.eclipse.egit.gitflow.ui.internal.JobFamilies;
-import org.eclipse.egit.gitflow.ui.internal.UIText;
-import org.eclipse.egit.ui.test.ContextMenuHelper;
-import org.eclipse.egit.ui.test.TestUtil;
 import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.swtbot.eclipse.finder.waits.Conditions;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
-import org.eclipse.ui.PlatformUI;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -33,7 +21,7 @@ import org.junit.runner.RunWith;
  * Tests for the Team->Gitflow->Feature Start/Finish actions
  */
 @RunWith(SWTBotJunit4ClassRunner.class)
-public class FeatureStartFinishHandlerTest extends AbstractGitflowHandlerTest {
+public class FeatureStartFinishHandlerTest extends AbstractFeatureFinishHandlerTest {
 
 	@Test
 	public void testFeatureStart() throws Exception {
@@ -57,72 +45,8 @@ public class FeatureStartFinishHandlerTest extends AbstractGitflowHandlerTest {
 		assertNull(findBranch(gfRepo.getConfig().getFeatureBranchName(FEATURE_NAME)));
 	}
 
-	private void finishFeature() {
-		final SWTBotTree projectExplorerTree = TestUtil.getExplorerTree();
-		getProjectItem(projectExplorerTree, PROJ1).select();
-		final String[] menuPath = new String[] {
-				util.getPluginLocalizedValue("TeamMenu.label"),
-				util.getPluginLocalizedValue("TeamGitFlowMenu.name", false, Activator.getDefault().getBundle()),
-				util.getPluginLocalizedValue("TeamGitFlowFeatureFinish.name", false, Activator.getDefault().getBundle()) };
-		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
-			@Override
-			public void run() {
-				ContextMenuHelper.clickContextMenuSync(projectExplorerTree, menuPath);
-			}
-		});
-		bot.button("OK").click();
-		bot.waitUntil(Conditions.waitForJobs(JobFamilies.GITFLOW_FAMILY, "Git flow jobs"));
-	}
-
-	private void init() throws CoreException {
-		new InitOperation(repository).execute(null);
-	}
-
 	@Override
-	protected void createFeature(String featureName) {
-		final SWTBotTree projectExplorerTree = TestUtil.getExplorerTree();
-		getProjectItem(projectExplorerTree, PROJ1).select();
-		final String[] menuPath = new String[] {
-				util.getPluginLocalizedValue("TeamMenu.label"),
-				util.getPluginLocalizedValue("TeamGitFlowMenu.name", false, Activator.getDefault().getBundle()),
-				util.getPluginLocalizedValue("TeamGitFlowFeatureStart.name", false, Activator.getDefault().getBundle()) };
-
-		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
-			@Override
-			public void run() {
-				ContextMenuHelper.clickContextMenuSync(projectExplorerTree, menuPath);
-			}
-		});
-
-		bot.waitUntil(shellIsActive(UIText.FeatureStartHandler_provideFeatureName));
-		bot.text().typeText(featureName);
-		bot.button("OK").click();
-		bot.waitUntil(Conditions.waitForJobs(JobFamilies.GITFLOW_FAMILY, "Git flow jobs"));
-	}
-
-	@Override
-	public void checkoutFeature(String featureName) {
-		final SWTBotTree projectExplorerTree = TestUtil.getExplorerTree();
-		getProjectItem(projectExplorerTree, PROJ1).select();
-		final String[] menuPath = new String[] {
-				util.getPluginLocalizedValue("TeamMenu.label"),
-				util.getPluginLocalizedValue("TeamGitFlowMenu.name", false, Activator.getDefault().getBundle()),
-				util.getPluginLocalizedValue("TeamGitFlowFeatureCheckout.name", false, Activator.getDefault().getBundle()) };
-
-		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
-			@Override
-			public void run() {
-				ContextMenuHelper.clickContextMenuSync(projectExplorerTree, menuPath);
-			}
-		});
-
-		bot.waitUntil(shellIsActive(UIText.FeatureCheckoutHandler_selectFeature));
-		bot.table().select(featureName);
-		bot.button("OK").click();
-		bot.waitUntil(Conditions.waitForJobs(JobFamilies.GITFLOW_FAMILY, "Git flow jobs"));
-	}
-
-	private void checkoutBranch(String branchToCheckout) throws CoreException {
-		new BranchOperation(repository, branchToCheckout).execute(null);
+	protected void selectOptions() {
+		// do nothing
 	}
 }
