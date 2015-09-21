50bfa33e1ee729c693eaa4630957337d8d8495ac
Max Hohenegger <eclipse@hohenegger.eu>
2015-09-21 11:00:19 +0200

    Allow saving feature finish options as default
    - added checkbox to feature finish dialog
    - save defaults to preference store
    - re-store options from preference store
    - extended UI tests
    
    Bug: 478056
    Change-Id: I232c338dab209dd0829f3fdb683b64441e22b7b0
    Signed-off-by: Max Hohenegger <eclipse@hohenegger.eu>

---
+++ org.eclipse.egit.gitflow.ui/src/org/eclipse/egit/gitflow/ui/internal/UIPreferences.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (C) 2015, Max Hohenegger <eclipse@hohenegger.eu>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.gitflow.ui.internal;
+
+/**
+ * Preferences used by the Gitflow UI plug-in.
+ * <p>
+ * All plug-in preferences shall be referenced by a constant in this class.
+ */
+public class UIPreferences {
+	/** */
+	public final static String FEATURE_FINISH_SQUASH = "feature_finish_squash"; //$NON-NLS-1$
+
+	/** */
+	public final static String FEATURE_FINISH_KEEP_BRANCH = "feature_finish_keep_branch"; //$NON-NLS-1$
+}
--- org.eclipse.egit.gitflow.ui/src/org/eclipse/egit/gitflow/ui/internal/dialogs/FinishFeatureDialog.java
+++ org.eclipse.egit.gitflow.ui/src/org/eclipse/egit/gitflow/ui/internal/dialogs/FinishFeatureDialog.java
@@ -8,9 +8,17 @@
  *******************************************************************************/
 package org.eclipse.egit.gitflow.ui.internal.dialogs;
 
+import static org.eclipse.egit.gitflow.ui.internal.UIPreferences.FEATURE_FINISH_KEEP_BRANCH;
+import static org.eclipse.egit.gitflow.ui.internal.UIPreferences.FEATURE_FINISH_SQUASH;
+
+import org.eclipse.egit.gitflow.ui.Activator;
 import org.eclipse.egit.gitflow.ui.internal.UIText;
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.IMessageProvider;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
@@ -35,6 +43,8 @@ public class FinishFeatureDialog extends TitleAreaDialog {
 	private Button keepBranchButton;
 
 	private String featureBranch;
+
+	private Button rememberOptionsButton;
 
 	/**
 	 * @param parentShell
@@ -68,13 +78,49 @@ public class FinishFeatureDialog extends TitleAreaDialog {
 		keepBranchButton = new Button(container, SWT.CHECK);
 		keepBranchButton.setText(UIText.FinishFeatureDialog_keepBranch);
 
+		restoreInput();
+
 		return area;
+	}
+
+	private void restoreInput() {
+		IPreferenceStore prefStore = Activator
+				.getDefault()
+				.getPreferenceStore();
+		squashButton.setSelection(prefStore.getBoolean(FEATURE_FINISH_SQUASH));
+		keepBranchButton.setSelection(prefStore.getBoolean(FEATURE_FINISH_KEEP_BRANCH));
 	}
 
 	@Override
 	protected Control createButtonBar(Composite parent) {
-		// TODO: we should have options to persist the selected configuration
-		return super.createButtonBar(parent);
+		final Composite customButtonBar = new Composite(parent, SWT.NONE);
+
+		int horizontalSpacing = convertHorizontalDLUsToPixels(IDialogConstants.HORIZONTAL_SPACING);
+		GridLayoutFactory.swtDefaults().numColumns(2).equalWidth(false)
+				.spacing(horizontalSpacing, 0).applyTo(customButtonBar);
+
+		GridDataFactory.swtDefaults().grab(true, false)
+				.align(SWT.FILL, SWT.BOTTOM).applyTo(customButtonBar);
+
+		customButtonBar.setFont(parent.getFont());
+
+		rememberOptionsButton = new Button(customButtonBar, SWT.CHECK);
+		rememberOptionsButton.setText(UIText.FinishFeatureDialog_saveAsDefault);
+
+		// TODO: Checkbox "Don't ask again"
+
+		int horizontlIndent = convertHorizontalDLUsToPixels(IDialogConstants.HORIZONTAL_MARGIN);
+		GridDataFactory.swtDefaults().grab(true, true)
+				.align(SWT.FILL, SWT.CENTER).indent(horizontlIndent, 0)
+				.applyTo(rememberOptionsButton);
+
+		// add the dialog's button bar to the right
+		final Control buttonControl = super.createButtonBar(customButtonBar);
+		GridDataFactory.swtDefaults().grab(true, false)
+				.align(SWT.RIGHT, SWT.CENTER).applyTo(buttonControl);
+
+
+		return customButtonBar;
 	}
 
 	@Override
@@ -89,7 +135,16 @@ public class FinishFeatureDialog extends TitleAreaDialog {
 
 	private void saveInput() {
 		this.squash = squashButton.getSelection();
+
 		this.keepBranch = keepBranchButton.getSelection();
+		if (rememberOptionsButton.getSelection()) {
+			IPreferenceStore preferenceStore = Activator
+					.getDefault()
+					.getPreferenceStore();
+			preferenceStore.setValue(FEATURE_FINISH_SQUASH, squashButton.getSelection());
+			preferenceStore.setValue(FEATURE_FINISH_KEEP_BRANCH, keepBranchButton.getSelection());
+		}
+
 	}
 
 	@Override
