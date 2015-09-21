27f1c235d80ced2f5df8a4b1041cc5f0d33cab6c
Stephan Hackstedt <stephan.hackstedt@googlemail.com>
2015-09-21 16:40:07 +0200

    Bug 477695
    Change-Id: I1652625c5eb4d3e911f0128464fd1e6318fc6f63
    Signed-off-by: Stephan Hackstedt<stephan.hackstedt@googlemail.com>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/op/DisconnectProviderOperation.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/op/DisconnectProviderOperation.java
@@ -1,10 +1,14 @@
 /*******************************************************************************
  * Copyright (C) 2007, Shawn O. Pearce <spearce@spearce.org>
+ * Copyright (C) 2015, Stephan Hackstedt <stephan.hackstedt@googlemail.com>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Stephan Hackstedt - Bug 477695
  *******************************************************************************/
 package org.eclipse.egit.core.op;
 
@@ -15,8 +19,7 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.SubProgressMonitor;
+import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
 import org.eclipse.core.runtime.jobs.MultiRule;
 import org.eclipse.egit.core.internal.CoreText;
@@ -49,14 +52,11 @@ public class DisconnectProviderOperation implements IEGitOperation {
 	 */
 	@Override
 	public void execute(IProgressMonitor m) throws CoreException {
-		IProgressMonitor monitor;
-		if (m == null)
-			monitor = new NullProgressMonitor();
-		else
-			monitor = m;
 
-		monitor.beginTask(CoreText.DisconnectProviderOperation_disconnecting,
+		SubMonitor progress = SubMonitor.convert(m,
+				CoreText.DisconnectProviderOperation_disconnecting,
 				projectList.size() * 200);
+
 		try {
 			for (IProject p : projectList) {
 				// TODO is this the right location?
@@ -66,13 +66,15 @@ public class DisconnectProviderOperation implements IEGitOperation {
 							"disconnect " + p.getName()); //$NON-NLS-1$
 				unmarkTeamPrivate(p);
 				RepositoryProvider.unmap(p);
-				monitor.worked(100);
+				progress.worked(100);
 
 				p.refreshLocal(IResource.DEPTH_INFINITE,
-						new SubProgressMonitor(monitor, 100));
+						progress.newChild(100));
 			}
 		} finally {
-			monitor.done();
+			// TODO really necassary @Stephan!? Wird done wenn 100% ticks
+			// erreicht sind.
+			progress.done();
 		}
 	}
 
