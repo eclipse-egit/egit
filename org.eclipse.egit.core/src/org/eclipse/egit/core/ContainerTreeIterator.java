217524341b2eb740f9036f7219a86894152e2484
Matthias Sohn <matthias.sohn@sap.com>
2015-09-23 11:40:41 +0200

    Fix ContainerTreeIterator to not skip team private members
    ContainerTreeIterator.entries() didn't list members which are considered
    team private by the platform. This leads to the effect that e.g. CVS
    folder is marked missing in IndexDiff despite the fact that its git
    status is clean.
    
    Bug: 478148
    Change-Id: Ic123849ec6dd66e5bec6453152f428a0566149f4
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/ContainerTreeIterator.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/ContainerTreeIterator.java
@@ -175,7 +175,8 @@ public class ContainerTreeIterator extends WorkingTreeIterator {
 	private Entry[] entries(final boolean hasInheritedResourceFilters) {
 		final IResource[] resources;
 		try {
-			resources = node.members(IContainer.INCLUDE_HIDDEN);
+			resources = node.members(IContainer.INCLUDE_HIDDEN
+					| IContainer.INCLUDE_TEAM_PRIVATE_MEMBERS);
 		} catch (CoreException err) {
 			return EOF;
 		}
