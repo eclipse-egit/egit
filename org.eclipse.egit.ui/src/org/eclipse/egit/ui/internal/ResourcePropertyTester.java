c4fe5a756141d7f1ec763527fcaa9285ca70b195
Andrey Loskutov <loskutov@gmx.de>
2015-10-26 14:02:39 +0100

    Allow "canAbortRebase" property be "true" on REBASING_MERGE state
    Bug: 470013
    Change-Id: I8ceb7fb545641f094ff9bc511ab9cd1e513d840b
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/ResourcePropertyTester.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/ResourcePropertyTester.java
@@ -115,6 +115,8 @@ public class ResourcePropertyTester extends PropertyTester {
 					return true;
 				case REBASING_REBASING:
 					return true;
+				case REBASING_MERGE:
+					return true;
 				default:
 					return false;
 				}
