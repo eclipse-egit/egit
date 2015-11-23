26a0a8fd0461b437285f381f96f3b2e100e45b08
Thomas Wolf <thomas.wolf@paranor.ch>
2015-11-23 07:34:57 +0100

    Extract non-decoration things into new IResourceState
    Separates UI-related things like branch names (actually labels) from
    the state of the file in the repository and enables use of this state
    elsewhere without incurring overhead for name calculations.
    
    Bug: 355809
    Change-Id: If283be82612b15d14c773eea628777eb79f263ae
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/decorators/IDecoratableResource.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/decorators/IDecoratableResource.java
@@ -5,32 +5,20 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Thomas Wolf <thomas.wolf@paranor.ch> - Factored out IResourceState
  *******************************************************************************/
 
 package org.eclipse.egit.ui.internal.decorators;
 
 import org.eclipse.core.resources.IResource;
+import org.eclipse.egit.ui.internal.resources.IResourceState;
 
 /**
  * Represents the state of a resource that can be used as a basis for decoration
  */
-public interface IDecoratableResource {
-
-	/**
-	 * Set of possible staging states for a resource
-	 */
-	public enum Staged {
-		/** Represents a resource that is not staged */
-		NOT_STAGED,
-		/** Represents a resource that has been modified */
-		MODIFIED,
-		/** Represents a resource that is added to Git */
-		ADDED,
-		/** Represents a resource that is removed from Git */
-		REMOVED,
-		/** Represents a resource that has been renamed */
-		RENAMED
-	}
+public interface IDecoratableResource extends IResourceState {
 
 	/**
 	 * Gets the type of the resource as defined by {@link IResource}
@@ -68,48 +56,4 @@ public interface IDecoratableResource {
 	 */
 	String getBranchStatus();
 
-	/**
-	 * Returns whether or not the resource is tracked by Git
-	 *
-	 * @return whether or not the resource is tracked by Git
-	 */
-	boolean isTracked();
-
-	/**
-	 * Returns whether or not the resource is ignored, either by a global team
-	 * ignore in Eclipse, or by .git/info/exclude et al.
-	 *
-	 * @return whether or not the resource is ignored
-	 */
-	boolean isIgnored();
-
-	/**
-	 * Returns whether or not the resource has changes that are not staged
-	 *
-	 * @return whether or not the resource is dirty
-	 */
-	boolean isDirty();
-
-	/**
-	 * Returns the staged state of the resource
-	 *
-	 * The set of allowed values are defined by the <code>Staged</code> enum
-	 *
-	 * @return the staged state of the resource
-	 */
-	Staged staged();
-
-	/**
-	 * Returns whether or not the resource has merge conflicts
-	 *
-	 * @return whether or not the resource has merge conflicts
-	 */
-	boolean hasConflicts();
-
-	/**
-	 * Returns whether or not the resource is assumed valid
-	 *
-	 * @return whether or not the resource is assumed valid
-	 */
-	boolean isAssumeValid();
 }
