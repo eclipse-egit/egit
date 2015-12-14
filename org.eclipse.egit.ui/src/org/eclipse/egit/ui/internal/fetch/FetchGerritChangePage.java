fe581db869ea04e4944eab784ad5d0ebe366f5cf
Alexander Kurtakov <akurtako@redhat.com>
2015-12-14 15:16:38 +0200

    Make "Fetch from gerrit" trim input text
    When coping a ref from gerrit web ui it often happens that it's copied
    with a leading and/or trailing space. Adding verify listener on the text
    field that trims the string handles these cases automatically and saves
    time to not delete the space manually.
    
    Bug: 484335
    Change-Id: I796c3351539bcc0711a4a0e9761395555877d543
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/fetch/FetchGerritChangePage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/fetch/FetchGerritChangePage.java
@@ -80,6 +80,8 @@ import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.VerifyEvent;
+import org.eclipse.swt.events.VerifyListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -214,6 +216,12 @@ public class FetchGerritChangePage extends WizardPage {
 			refText.setText(defaultChange);
 		GridDataFactory.fillDefaults().grab(true, false).applyTo(refText);
 		addRefContentProposalToText(refText);
+		refText.addVerifyListener(new VerifyListener() {
+			@Override
+			public void verifyText(VerifyEvent event) {
+				event.text = event.text.trim();
+			}
+		});
 
 		final Group checkoutGroup = new Group(main, SWT.SHADOW_ETCHED_IN);
 		checkoutGroup.setLayout(new GridLayout(3, false));
