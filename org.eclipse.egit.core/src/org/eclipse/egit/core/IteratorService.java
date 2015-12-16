7a6175f1a6145bdbf8631922de32401f5dee7c05
Andrey Loskutov <loskutov@gmx.de>
2015-12-16 16:47:38 +0100

    [wip] Get rid of ContainerTreeIterator
    Don't hook it at all. Just curious what will break. After all, if there
    are no issues without the ContainerTreeIterator,
    AdaptableFileTreeIterator can be simply replaced with FileTreeIterator.
    
    Bug: 484498
    Change-Id: I026abee1706e6fbcd470cc7262b9fc1e59a7fe8b
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/IteratorService.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/IteratorService.java
@@ -48,9 +48,9 @@ public class IteratorService {
 		File workTree = repository.getWorkTree();
 		if (!workTree.exists())
 			return null;
-		IContainer container = findContainer(root, workTree);
-		if (container != null)
-			return new ContainerTreeIterator(repository, container);
+		// IContainer container = findContainer(root, workTree);
+		// if (container != null)
+		// return new ContainerTreeIterator(repository, container);
 		return new AdaptableFileTreeIterator(repository, root);
 	}
 
