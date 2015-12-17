7f61cbf8fa1c57d5bd6220c4ca44da6ab0ea3632
Andrey Loskutov <loskutov@gmx.de>
2015-12-17 11:37:53 +0100

    Staging view: show progress while refresh and diff are running
    Show busy cursor in the staging view as long as the *both* refresh and
    index diff jobs are running. Previously only the refresh of resources
    was shown, but not the diff which takes much more time.
    
    Change-Id: Ia9eb40ade9e85c076142828714afeade0b9ad817
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/indexdiff/IndexDiffCacheEntry.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/indexdiff/IndexDiffCacheEntry.java
@@ -34,6 +34,7 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.ISchedulingRule;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.egit.core.Activator;
 import org.eclipse.egit.core.EclipseGitProgressTransformer;
@@ -156,7 +157,7 @@ public class IndexDiffCacheEntry {
 	 * @return new job ready to be scheduled, never null
 	 */
 	public Job createRefreshResourcesAndIndexDiffJob() {
-		String repositoryName = Activator.getDefault().getRepositoryUtil()
+		final String repositoryName = Activator.getDefault().getRepositoryUtil()
 				.getRepositoryName(repository);
 		String jobName = MessageFormat
 				.format(CoreText.IndexDiffCacheEntry_refreshingProjects,
@@ -165,19 +166,49 @@ public class IndexDiffCacheEntry {
 
 			@Override
 			public IStatus runInWorkspace(IProgressMonitor monitor) {
+				final long start = System.currentTimeMillis();
+				ISchedulingRule rule = RuleUtil.getRule(repository);
 				try {
-					IProject[] validOpenProjects = ProjectUtil
-							.getValidOpenProjects(repository);
-					ProjectUtil.refreshResources(validOpenProjects, monitor);
-				} catch (CoreException e) {
-					return Activator.error(e.getMessage(), e);
+					Job.getJobManager().beginRule(rule, monitor);
+					try {
+						IProject[] validOpenProjects = ProjectUtil
+								.getValidOpenProjects(repository);
+						ProjectUtil.refreshResources(validOpenProjects,
+								monitor);
+					} catch (CoreException e) {
+						return Activator.error(e.getMessage(), e);
+					}
+					if (Activator.getDefault().isDebugging()) {
+						final long refresh = System.currentTimeMillis();
+						Activator.logInfo("Resources refresh took " //$NON-NLS-1$
+								+ (refresh - start) + " ms for " //$NON-NLS-1$
+								+ repositoryName);
+
+					}
+				} catch (OperationCanceledException e) {
+					return Status.CANCEL_STATUS;
+				} finally {
+					Job.getJobManager().endRule(rule);
 				}
 				refresh();
+				Job next = reloadJob;
+				if (next != null) {
+					try {
+						next.join();
+					} catch (InterruptedException e) {
+						return Status.CANCEL_STATUS;
+					}
+				}
+				if (Activator.getDefault().isDebugging()) {
+					final long refresh = System.currentTimeMillis();
+					Activator.logInfo("Diff took " + (refresh - start) //$NON-NLS-1$
+							+ " ms for " + repositoryName); //$NON-NLS-1$
+
+				}
 				return Status.OK_STATUS;
 			}
 
 		};
-		job.setRule(RuleUtil.getRule(repository));
 		return job;
 	}
 
