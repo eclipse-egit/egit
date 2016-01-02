6f9760c06623c2794117b14bf85b87fbbb74b9ab
Thomas Wolf <thomas.wolf@paranor.ch>
2016-01-02 12:53:11 +0100

    Avoid expensive property tester evaluations
    The GitFlow property tester tests the presence of certain values in the
    git config. Accessing the git config incurs three file accesses to get
    the last-modified times of the system, user, and repo config files to
    determine whether they have changed, and if so, to reload them. These
    are potentially slow operations involving file I/O. Even just getting
    the last-modified time apparently can be expensive, for instance if the
    user config is on a non-local file system.
    
    The GitFlow property testers were based on the current selection and
    thus were re-evaluated by the Eclipse framework whenever the current
    selection changed, including text selections. The text selection may
    change on each keypress. This could lead to very noticeable delays if
    access to the last-modified time of a config file was slow.
    
    I don't see an easy way to improve the handling of config files. The
    other approach to minimize the overhead of property testers is to
    minimize the number of times they're re-evaluated. Since the GitFlow
    property testers are not really interested in the current selection
    but in a derived property of that (the current repository), this
    change provides this current repository as a variable in the
    IEvaluationContext and makes the property tester use that new variable.
    As a result, the tests are only re-evaluated when the current selection
    changes such that the current repository changes. Keypresses and text
    selection changes in an editor no longer trigger re-evaluations of the
    GitFlow property tests.
    
    The current repository is still determined on each selection change.
    However,
    
    * this is normally a relatively fast operation as the repository is
      already cached most of the time, 
    * the repository is determined only once in the source provider instead
      of again and again in each property test.
    
    Bug: 484785
    Change-Id: Id305fb54a6666f8e9715dcf0adb070880e1c098a
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.gitflow.ui/src/org/eclipse/egit/gitflow/ui/internal/properties/RepositoryPropertyTester.java
+++ org.eclipse.egit.gitflow.ui/src/org/eclipse/egit/gitflow/ui/internal/properties/RepositoryPropertyTester.java
@@ -1,32 +1,29 @@
 /*******************************************************************************
- * Copyright (C) 2015, Max Hohenegger <eclipse@hohenegger.eu>
+ * Copyright (C) 2015, 2016 Max Hohenegger <eclipse@hohenegger.eu> and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Thomas Wolf <thomas.wolf@paranor.ch> Bug 484785
  *******************************************************************************/
-package org.eclipse.egit.gitflow.ui.internal.selection;
+package org.eclipse.egit.gitflow.ui.internal.properties;
 
 import static org.eclipse.egit.gitflow.ui.Activator.error;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
 
 import org.eclipse.core.expressions.PropertyTester;
 import org.eclipse.egit.gitflow.Activator;
 import org.eclipse.egit.gitflow.GitFlowRepository;
-import org.eclipse.egit.ui.internal.selection.SelectionUtils;
-import org.eclipse.jface.text.ITextSelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jgit.lib.Repository;
 
 /**
  * Testing Git Flow states.
  */
-public class SelectionPropertyTester extends PropertyTester {
+public class RepositoryPropertyTester extends PropertyTester {
 	private static final String IS_MASTER = "isMaster"; //$NON-NLS-1$
 
 	private static final String IS_DEVELOP = "isDevelop"; //$NON-NLS-1$
@@ -44,11 +41,11 @@ public class SelectionPropertyTester extends PropertyTester {
 	@Override
 	public boolean test(Object receiver, String property, Object[] args,
 			Object expectedValue) {
-		if (receiver == null) {
+		if (!(receiver instanceof Repository)) {
 			return false;
 		}
-		Repository repository = getRepository((Collection<?>) receiver);
-		if (repository == null || repository.isBare()) {
+		Repository repository = (Repository) receiver;
+		if (repository.isBare()) {
 			return false;
 		}
 		GitFlowRepository gitFlowRepository = new GitFlowRepository(repository);
@@ -74,19 +71,4 @@ public class SelectionPropertyTester extends PropertyTester {
 		return false;
 	}
 
-	private Repository getRepository(Collection<?> collection) {
-		if (collection.isEmpty()) {
-			return null;
-		}
-		IStructuredSelection selection = null;
-		Object first = collection.iterator().next();
-		if (collection.size() == 1 && first instanceof ITextSelection) {
-			selection = SelectionUtils
-					.getStructuredSelection((ITextSelection) first);
-		} else {
-			selection = new StructuredSelection(new ArrayList<>(collection));
-		}
-		return SelectionUtils.getRepository(selection);
-
-	}
 }
