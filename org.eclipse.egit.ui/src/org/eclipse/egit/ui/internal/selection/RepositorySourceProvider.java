97283d9ae8237249af006bcc6119f5beb1bf8f7c
Matthias Sohn <matthias.sohn@sap.com>
2016-01-04 22:35:56 +0100

    RepositorySourceProvider: update selection in background job
    Move selection update to a background job to prevent UI freezes.
    
    See https://git.eclipse.org/r/#/c/63410/2/
    
    Change-Id: I60aac4695fbc34fc99d193f76eb577c6a1890af5
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/selection/RepositorySourceProvider.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/selection/RepositorySourceProvider.java
@@ -11,7 +11,13 @@ package org.eclipse.egit.ui.internal.selection;
 import java.util.Collections;
 import java.util.Map;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.ui.AbstractSourceProvider;
 import org.eclipse.ui.ISelectionListener;
@@ -30,6 +36,58 @@ import org.eclipse.ui.services.IServiceLocator;
 public class RepositorySourceProvider extends AbstractSourceProvider
 		implements ISelectionListener, IWindowListener {
 
+	private final class UpdateSelectionJob extends Job {
+		private ISelection sel;
+
+		private UpdateSelectionJob() {
+			super(UIText.RepositorySourceProvider_updateRepoSelection);
+			setSystem(true);
+			setUser(false);
+		}
+
+		@Override
+		protected IStatus run(IProgressMonitor monitor) {
+			ISelection s;
+			synchronized (this) {
+				// make sure we don't leak reference to the selection
+				s = sel;
+				sel = null;
+			}
+			if (monitor.isCanceled()) {
+				return Status.CANCEL_STATUS;
+			}
+			Repository newRepository;
+			if (s == null || s.isEmpty()) {
+				newRepository = null;
+			} else {
+				IStructuredSelection ss = SelectionUtils
+						.getStructuredSelection(s);
+				if (ss.isEmpty()) {
+					newRepository = null;
+				} else {
+					newRepository = SelectionUtils.getRepository(ss);
+				}
+			}
+			if (monitor.isCanceled()) {
+				return Status.CANCEL_STATUS;
+			}
+			if (repository != newRepository) {
+				repository = newRepository;
+				fireSourceChanged(ISources.ACTIVE_WORKBENCH_WINDOW,
+						REPOSITORY_PROPERTY, repository);
+			}
+			return Status.OK_STATUS;
+		}
+
+		public void schedule(ISelection selection) {
+			cancel();
+			synchronized (this) {
+				sel = selection;
+			}
+			schedule();
+		}
+	}
+
 	/**
 	 * Key for the new variable in the
 	 * {@link org.eclipse.core.expressions.IEvaluationContext}; may be used in a
@@ -37,11 +95,14 @@ public class RepositorySourceProvider extends AbstractSourceProvider
 	 */
 	public static final String REPOSITORY_PROPERTY = "org.eclipse.egit.ui.currentRepository"; //$NON-NLS-1$
 
-	private Repository repository;
+	private volatile Repository repository;
+
+	private UpdateSelectionJob updateSelectionJob;
 
 	@Override
 	public void initialize(IServiceLocator locator) {
 		super.initialize(locator);
+		updateSelectionJob = new UpdateSelectionJob();
 		PlatformUI.getWorkbench().addWindowListener(this);
 	}
 
@@ -49,6 +110,11 @@ public class RepositorySourceProvider extends AbstractSourceProvider
 	public void dispose() {
 		PlatformUI.getWorkbench().removeWindowListener(this);
 		repository = null;
+		if (updateSelectionJob != null) {
+			updateSelectionJob.cancel();
+			updateSelectionJob = null;
+		}
+
 	}
 
 	@Override
@@ -63,19 +129,11 @@ public class RepositorySourceProvider extends AbstractSourceProvider
 
 	@Override
 	public void selectionChanged(IWorkbenchPart part, ISelection selection) {
-		Repository newRepository;
-		if (selection == null) {
-			newRepository = null;
-		} else {
-			newRepository = SelectionUtils.getRepository(
-					SelectionUtils.getStructuredSelection(selection));
+		UpdateSelectionJob job = updateSelectionJob;
+		if (job == null) {
+			return;
 		}
-		if (repository != newRepository) {
-			repository = newRepository;
-			fireSourceChanged(ISources.ACTIVE_WORKBENCH_WINDOW,
-					REPOSITORY_PROPERTY,
-					repository);
-		}
+		job.schedule(selection);
 	}
 
 	@Override
@@ -86,6 +144,7 @@ public class RepositorySourceProvider extends AbstractSourceProvider
 	@Override
 	public void windowDeactivated(IWorkbenchWindow window) {
 		window.getSelectionService().removeSelectionListener(this);
+		updateSelectionJob.cancel();
 	}
 
 	@Override
