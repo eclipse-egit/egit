4136ec50b4e344d65a11d21180b0b650ae9e8f31
Matthias Sohn <matthias.sohn@sap.com>
2016-01-04 22:35:56 +0100

    RepositorySourceProvider: update selection in background job
    Move selection update to a background job to prevent UI freezes.
    
    See https://git.eclipse.org/r/#/c/63410/2/
    
    Change-Id: I60aac4695fbc34fc99d193f76eb577c6a1890af5
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/selection/RepositorySourceProvider.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/selection/RepositorySourceProvider.java
@@ -8,11 +8,25 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.selection;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Collections;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.egit.ui.internal.UIText;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jgit.annotations.Nullable;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.AbstractSourceProvider;
 import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.ISources;
@@ -20,6 +34,7 @@ import org.eclipse.ui.IWindowListener;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.progress.IProgressService;
 import org.eclipse.ui.services.IServiceLocator;
 
 /**
@@ -37,11 +52,16 @@ public class RepositorySourceProvider extends AbstractSourceProvider
 	 */
 	public static final String REPOSITORY_PROPERTY = "org.eclipse.egit.ui.currentRepository"; //$NON-NLS-1$
 
-	private Repository repository;
+	private volatile Repository repository;
+
+	private volatile boolean updateInProgress;
+
+	private UpdateSelectionJob updateSelectionJob;
 
 	@Override
-	public void initialize(IServiceLocator locator) {
+	public void initialize(final IServiceLocator locator) {
 		super.initialize(locator);
+		updateSelectionJob = new UpdateSelectionJob();
 		PlatformUI.getWorkbench().addWindowListener(this);
 	}
 
@@ -49,11 +69,20 @@ public class RepositorySourceProvider extends AbstractSourceProvider
 	public void dispose() {
 		PlatformUI.getWorkbench().removeWindowListener(this);
 		repository = null;
+		if (updateSelectionJob != null) {
+			updateSelectionJob.cancel();
+			updateSelectionJob = null;
+		}
 	}
 
 	@Override
 	public Map getCurrentState() {
-		return Collections.singletonMap(REPOSITORY_PROPERTY, repository);
+		if (updateInProgress) {
+			// IEvaluationContext.UNDEFINED_VARIABLE seem to evaluate to false
+			return Collections.singletonMap(REPOSITORY_PROPERTY, new Object());
+		} else {
+			return Collections.singletonMap(REPOSITORY_PROPERTY, repository);
+		}
 	}
 
 	@Override
@@ -63,29 +92,28 @@ public class RepositorySourceProvider extends AbstractSourceProvider
 
 	@Override
 	public void selectionChanged(IWorkbenchPart part, ISelection selection) {
-		Repository newRepository;
-		if (selection == null) {
-			newRepository = null;
-		} else {
-			newRepository = SelectionUtils.getRepository(
-					SelectionUtils.getStructuredSelection(selection));
+		UpdateSelectionJob job = updateSelectionJob;
+		if (job == null) {
+			return;
 		}
-		if (repository != newRepository) {
-			repository = newRepository;
-			fireSourceChanged(ISources.ACTIVE_WORKBENCH_WINDOW,
-					REPOSITORY_PROPERTY,
-					repository);
+		if (!(selection instanceof IStructuredSelection)) {
+			// we should not try to compute anything while user selects text:
+			// this may show disturbing dialogs in case user just continuously
+			// selects in the editor
+			job.schedule(StructuredSelection.EMPTY);
+		} else {
+			job.schedule((IStructuredSelection) selection);
 		}
 	}
 
 	@Override
 	public void windowActivated(IWorkbenchWindow window) {
-		window.getSelectionService().addSelectionListener(this);
+		// Nothing to do
 	}
 
 	@Override
 	public void windowDeactivated(IWorkbenchWindow window) {
-		window.getSelectionService().removeSelectionListener(this);
+		// Nothing to do: this happens also if *any* dialog is opened
 	}
 
 	@Override
@@ -95,7 +123,134 @@ public class RepositorySourceProvider extends AbstractSourceProvider
 
 	@Override
 	public void windowOpened(IWorkbenchWindow window) {
-		// Nothing to do
+		window.getSelectionService().addSelectionListener(this);
 	}
 
+	/**
+	 * This method will block current thread and wait until the pending
+	 * computation result is available. If no computation is running, it will
+	 * immediately return last computed value or {@code null} if no value is
+	 * known.
+	 *
+	 * @return latest repository state, can be {@code null}
+	 */
+	@Nullable
+	public Repository waitFor() {
+		UpdateSelectionJob job = updateSelectionJob;
+		if (job == null || !updateInProgress) {
+			return repository;
+		}
+
+		// NON UI caller goes this way.
+		if (Display.getCurrent() == null) {
+			while (updateInProgress) {
+				try {
+					Job.getJobManager().join(RepositorySourceProvider.class,
+							new NullProgressMonitor());
+				} catch (OperationCanceledException | InterruptedException e) {
+					break;
+				}
+			}
+			return repository;
+		}
+
+		// UI caller will get a nice progress dialog if it takes longer
+		IProgressService service = PlatformUI.getWorkbench()
+				.getService(IProgressService.class);
+		final AtomicBoolean cancelled = new AtomicBoolean(false);
+		while (updateInProgress && !cancelled.get()) {
+			try {
+				service.run(false, true, new IRunnableWithProgress() {
+					@Override
+					public void run(IProgressMonitor monitor)
+							throws InvocationTargetException,
+							InterruptedException {
+						if (monitor.isCanceled()) {
+							cancelled.set(true);
+							return;
+						}
+						Job.getJobManager().join(RepositorySourceProvider.class,
+								monitor);
+					}
+				});
+			} catch (InvocationTargetException | InterruptedException e) {
+				break;
+			}
+		}
+		return repository;
+	}
+
+	private final class UpdateSelectionJob extends Job {
+		private IStructuredSelection sel;
+
+		private UpdateSelectionJob() {
+			super(UIText.RepositorySourceProvider_updateRepoSelection);
+			setSystem(true);
+			setUser(false);
+		}
+
+		@Override
+		public boolean belongsTo(Object family) {
+			return RepositorySourceProvider.class == family;
+		}
+
+		public void schedule(IStructuredSelection selection) {
+			if (selection.isEmpty()) {
+				// no need to do heavy weight job scheduling if nothing has to
+				// be done
+				boolean needUpdate;
+				synchronized (this) {
+					needUpdate = repository != null;
+					repository = null;
+				}
+				if (needUpdate) {
+					fireSourceChanged(ISources.ACTIVE_WORKBENCH_WINDOW,
+							REPOSITORY_PROPERTY, null);
+				}
+				return;
+			}
+
+			// Whatever is being computed is obsolete now: cancel job...
+			cancel();
+
+			// Remember selection, mark us as busy and schedule
+			synchronized (this) {
+				updateInProgress = true;
+				sel = selection;
+			}
+			schedule();
+		}
+
+		@Override
+		protected IStatus run(IProgressMonitor monitor) {
+			IStructuredSelection s;
+			synchronized (this) {
+				// make sure we don't leak reference to the selection
+				s = sel;
+				sel = null;
+			}
+			if (s == null || monitor.isCanceled()) {
+				return cancelled();
+			}
+			Repository newRepository = SelectionUtils.getRepository(s);
+			boolean needUpdate;
+			synchronized (this) {
+				needUpdate = repository != newRepository;
+				repository = newRepository;
+				updateInProgress = false;
+			}
+			if (needUpdate) {
+				fireSourceChanged(ISources.ACTIVE_WORKBENCH_WINDOW,
+						REPOSITORY_PROPERTY, newRepository);
+			}
+			return Status.OK_STATUS;
+		}
+
+		IStatus cancelled() {
+			synchronized (this) {
+				updateInProgress = false;
+			}
+			return Status.CANCEL_STATUS;
+		}
+	}
 }
