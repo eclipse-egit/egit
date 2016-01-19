eclipse-egit/egit

commit dd1543befe748afdf4fa7e7763e69fb80fc202e0

Disable Gitflow menu in History view already on first access
Previously the sub menu was enabled, and only disabled when trying to
open it while a non-develop branch was checked out.

Change-Id: I924a7d4cce95bdc544a9b6498f554fb151d94020
Signed-off-by: Max Hohenegger <eclipse@hohenegger.eu>
Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

org.eclipse.egit.gitflow.ui/src/org/eclipse/egit/gitflow/ui/internal/menu/DynamicHistoryMenu.java
@@ -35,14 +35,9 @@ public class DynamicHistoryMenu extends ContributionItem {
 	@Override
 	public void fill(Menu menu, int index) {
 		GitFlowRepository gfRepo = getRepository();
-		try {
-			if (gfRepo == null || !gfRepo.isDevelop()) {
+		if (gfRepo == null) {
 				return;
 			}
-		} catch (IOException e) {
-			Activator.getDefault().getLog().log(Activator.error(e.getMessage()));
-			return;
-		}
 
 		RevCommit selectedCommit = getSelectedCommit();
 		if (selectedCommit == null) {
@@ -58,6 +53,14 @@ public class DynamicHistoryMenu extends ContributionItem {
 				UIText.DynamicHistoryMenu_startGitflowReleaseFrom,
 				abbreviate(selectedCommit)));
 		menuItem.addSelectionListener(listener);
+
+		try {
+			menuItem.setEnabled(!gfRepo.isDevelop());
+		} catch (IOException e) {
+			Activator.getDefault().getLog()
+					.log(Activator.error(e.getMessage()));
+			return;
+		}
 	}
 
 	private String abbreviate(RevCommit selectedCommit) {

commit 728050d00d68c411e9a0e215c6af80998d24594e

Fixed: Gitflow start release is enabled on non-Gitflow repositories
This fixes issues introduced in dd1543befe748afdf4fa7e7763e69fb80fc202e0

Change-Id: I8bc769f635f8d11ff8db355b98149a4ccd3ab419
Signed-off-by: Max Hohenegger <eclipse@hohenegger.eu>

org.eclipse.egit.gitflow.ui/src/org/eclipse/egit/gitflow/ui/internal/menu/DynamicHistoryMenu.java
@@ -36,8 +36,8 @@ public class DynamicHistoryMenu extends ContributionItem {
 	public void fill(Menu menu, int index) {
 		GitFlowRepository gfRepo = getRepository();
 		if (gfRepo == null) {
-				return;
-			}
+			return;
+		}
 
 		RevCommit selectedCommit = getSelectedCommit();
 		if (selectedCommit == null) {
@@ -55,7 +55,7 @@ public class DynamicHistoryMenu extends ContributionItem {
 		menuItem.addSelectionListener(listener);
 
 		try {
-			menuItem.setEnabled(!gfRepo.isDevelop());
+			menuItem.setEnabled(gfRepo.getConfig().isInitialized());
 		} catch (IOException e) {
 			Activator.getDefault().getLog()
 					.log(Activator.error(e.getMessage()));