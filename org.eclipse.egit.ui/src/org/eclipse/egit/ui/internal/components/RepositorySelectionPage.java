eclipse-egit/egit

commit 39f3c68d70a6812ecf0f95a2c8a1129026bfdea4

Improve auto-filling the repository URL from clipboard in clone wizard
So far in the clone wizard we auto-fill the repository URL if the
clipboard contains a valid URL. Enhance this to also accept a valid git
clone command.

Also auto-fill the repository URL for http and https protocols.

Bug: 484412
Change-Id: I6d56e17380e401badafd9b564e8c095d7894bac4
Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RepositorySelectionPage.java
@@ -325,25 +325,27 @@ public class RepositorySelectionPage extends WizardPage implements IRepositorySe
 
 		String preset = presetUri;
 		if (presetUri == null) {
-			Clipboard clippy = new Clipboard(Display.getCurrent());
-			String text = (String) clippy.getContents(TextTransfer
-					.getInstance());
+			Clipboard clipboard = new Clipboard(Display.getCurrent());
+			String text = (String) clipboard
+					.getContents(TextTransfer.getInstance());
 			try {
 				if (text != null) {
-					text = text.trim();
+					text = stripGitCloneCommand(text);
 					int index = text.indexOf(' ');
 					if (index > 0)
 						text = text.substring(0, index);
 					URIish u = new URIish(text);
 					if (canHandleProtocol(u))
 						if (Protocol.GIT.handles(u) || Protocol.SSH.handles(u)
+								|| Protocol.HTTP.handles(u)
+								|| Protocol.HTTPS.handles(u)
 								|| text.endsWith(Constants.DOT_GIT_EXT))
 							preset = text;
 				}
 			} catch (URISyntaxException e) {
 				// ignore, preset is null
 			}
-			clippy.dispose();
+			clipboard.dispose();
 		}
 		this.presetUri = preset;
 

commit 655cf491d9010d26065cc7cb10154d09f067b188

Do not autofill clone wizard for arbitrary clipboard input
The clone wizard would auto-fill also if the clipboard contained
arbitrary, non-git related HTTP(S) URLs. This is a regression that was
introduced in commit 39f3c68.

Remove the tests for HTTP protocols. They are allowed anyway by the
general test that the clipboard content ends in .git.

Also consider only clipboard content up to the first whitespace (after
having removed a potential "git clone" prefix). Formerly, we stopped at
the first blank, not general whitespace, and would thus autofill even
with multi-line clipboard content like

  foobar
  baz
  .git

(No blank in there, just line endings.)

Finally, add missing braces and remove a spurious trim() call.

Change-Id: I04e5912b3a28dc911a54dea8255dd64eca58896e
Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RepositorySelectionPage.java
@@ -331,16 +331,14 @@ public class RepositorySelectionPage extends WizardPage implements IRepositorySe
 			try {
 				if (text != null) {
 					text = stripGitCloneCommand(text);
-					int index = text.indexOf(' ');
-					if (index > 0)
-						text = text.substring(0, index);
+					text = text.split("\\s|\\h", 2)[0]; //$NON-NLS-1$
 					URIish u = new URIish(text);
-					if (canHandleProtocol(u))
+					if (canHandleProtocol(u)) {
 						if (Protocol.GIT.handles(u) || Protocol.SSH.handles(u)
-								|| Protocol.HTTP.handles(u)
-								|| Protocol.HTTPS.handles(u)
-								|| text.endsWith(Constants.DOT_GIT_EXT))
+								|| text.endsWith(Constants.DOT_GIT_EXT)) {
 							preset = text;
+						}
+					}
 				}
 			} catch (URISyntaxException e) {
 				// ignore, preset is null
@@ -878,7 +876,7 @@ public class RepositorySelectionPage extends WizardPage implements IRepositorySe
 		if (input.startsWith(GIT_CLONE_COMMAND_PREFIX)) {
 			return input.substring(GIT_CLONE_COMMAND_PREFIX.length()).trim();
 		}
-		return input.trim();
+		return input;
 	}
 
 	private boolean setSafePassword(String p) {