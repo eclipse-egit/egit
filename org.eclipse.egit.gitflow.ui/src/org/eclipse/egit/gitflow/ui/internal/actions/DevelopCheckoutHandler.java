209755c017f68e67bd6ccc24b5c84e8a7db0158d
Max Hohenegger <eclipse@hohenegger.eu>
2016-02-08 23:31:45 +0100

    Added Gitflow convenience command for checking out develop branch
    Switching back and forth between feature branches, and the develop
    branch is a frequently used operation for Gitflow users. Hence, there
    should be a quick way of doing so.
    
    Change-Id: I761a2bf4db9ddd3bd32f99541eb512d5f53eefcb
    Signed-off-by: Max Hohenegger <eclipse@hohenegger.eu>

---
+++ org.eclipse.egit.gitflow.ui/src/org/eclipse/egit/gitflow/ui/internal/actions/DevelopCheckoutHandler.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (C) 2016, Max Hohenegger <eclipse@hohenegger.eu>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.gitflow.ui.internal.actions;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.egit.gitflow.GitFlowRepository;
+import org.eclipse.egit.ui.internal.branch.BranchOperationUI;
+
+/**
+ * Checkout develop branch
+ */
+public class DevelopCheckoutHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		final GitFlowRepository gfRepo = GitFlowHandlerUtil.getRepository(event);
+
+		BranchOperationUI.checkout(gfRepo.getRepository(),
+				gfRepo.getConfig().getDevelopFull()).start();
+
+		return null;
+	}
+}
