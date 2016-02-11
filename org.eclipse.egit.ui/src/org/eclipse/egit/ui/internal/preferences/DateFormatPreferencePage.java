292099f2d5bfbd6acff3ef4de981220ca285323c
Markus Keller <markus_keller@ch.ibm.com>
2016-02-11 16:46:53 +0100

    Fix "Git > Date Format" preference page message bugs
    Fix bad labels.
    Fix "Date preview" for the "User-defined format".
    Fix missing preview when opening page with non user-defined format.
    
    Bug: 487678
    Change-Id: I8adc77e5fe5ca3d90ce913e3b8b08dcf53fd6923
    Signed-off-by: Markus Keller <markus_keller@ch.ibm.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/preferences/DateFormatPreferencePage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/preferences/DateFormatPreferencePage.java
@@ -154,12 +154,7 @@ public class DateFormatPreferencePage extends FieldEditorPreferencePage
 				.getString(UIPreferences.DATE_FORMAT);
 		String initialValue = getPreferenceStore()
 				.getString(UIPreferences.DATE_FORMAT_CHOICE);
-		GitDateFormatter.Format initialFormat = fromString(initialValue);
-		FormatInfo info = DATA.get(initialFormat);
-		dateFormat.setEnabled(initialFormat == null, pane);
-		dateFormat.setStringValue(
-				initialFormat == null ? lastCustomValue : info.format);
-		formatExplanation.setText(info.explanation);
+		updateFields(initialValue);
 	}
 
 	@Override
@@ -216,7 +211,6 @@ public class DateFormatPreferencePage extends FieldEditorPreferencePage
 	}
 
 	private void updatePreview(SimpleDateFormat format) {
-		format.setTimeZone(SAMPLE.getTimeZone());
 		dateFormatPreview.setText(format.format(SAMPLE.getWhen()));
 	}
 
