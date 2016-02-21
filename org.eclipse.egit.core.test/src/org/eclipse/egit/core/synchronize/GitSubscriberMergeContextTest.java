85a3acdd4ffbc7c4fbb20d525e1a59963fbc8731
Thomas Wolf <thomas.wolf@paranor.ch>
2016-02-21 10:58:25 +0100

    [WIP] Test stability: GitSubscriberMergeContextTest
    I can't get that test to fail locally; neither on native OS X not on a
    Linux (Ubuntu 15.04) VM. Yet it fails frequently on Hudson. I don't see
    anythign wrong with the test either. Let's include a lot of debug output
    in an attempt to figure out what's going on.
    
    Change-Id: Icdd054c5b7f878771046706f615746e3f47e9b4f
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.core.test/src/org/eclipse/egit/core/synchronize/GitSubscriberMergeContextTest.java
+++ org.eclipse.egit.core.test/src/org/eclipse/egit/core/synchronize/GitSubscriberMergeContextTest.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.util.LinkedHashSet;
@@ -381,6 +382,23 @@ public class GitSubscriberMergeContextTest extends ModelTestCase {
 		iProject.refreshLocal(IResource.DEPTH_INFINITE,
 				new NullProgressMonitor());
 		TestUtils.waitForJobs(500, 5000, null);
+		if (!iFile2.exists()) {
+			// Debug output to track down sporadically failing test
+			System.out.println(iFile2 + " is synchronized? " + Boolean
+					.toString(iFile2.isSynchronized(IResource.DEPTH_ZERO)));
+			System.out.println(TestUtils.dumpThreads());
+			System.out.println("***** WARNING: IFile reported as not existing");
+			System.out.println(iProject + " is open? "
+					+ Boolean.toString(iProject.isOpen()));
+			System.out.println(file2.getPath() + " exists? "
+					+ Boolean.toString(file2.exists()));
+			System.out.println(iFile2 + " exists now? "
+					+ Boolean.toString(iFile2.exists()));
+			iFile2 = iProject.getFile(iFile2.getName());
+			System.out.println(iFile2 + " exists now? "
+					+ Boolean.toString(iFile2.exists()));
+			fail(iFile2 + " reported not to exist");
+		}
 		assertTrue(iFile2.exists());
 
 		final String masterChanges = "some changes\n";
