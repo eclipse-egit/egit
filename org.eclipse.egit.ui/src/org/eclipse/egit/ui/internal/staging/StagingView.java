5121953b8bdd3adb554dd0bd67d55386e28053c6
Andrey Loskutov <loskutov@gmx.de>
2016-02-26 16:27:12 +0100

    [staging view] Fixed sort order: added check for "names first" case
    Bug: 487004
    Change-Id: I59b0c32c8c077111ebc2e61026961332933e8b54

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingView.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingView.java
@@ -703,8 +703,9 @@ public class StagingView extends ViewPart implements IShowInSource {
 				compareWith(event);
 			}
 		});
-		unstagedViewer
-				.setComparator(new UnstagedComparator(getSortCheckState()));
+		unstagedViewer.setComparator(
+				new UnstagedComparator(getSortCheckState(), getPreferenceStore()
+						.getBoolean(UIPreferences.STAGING_VIEW_FILENAME_MODE)));
 		enableAutoExpand(unstagedViewer);
 		addListenerToDisableAutoExpandOnCollapse(unstagedViewer);
 
@@ -1679,9 +1680,12 @@ public class StagingView extends ViewPart implements IShowInSource {
 				getLabelProvider(unstagedViewer).setFileNameMode(enable);
 				getContentProvider(stagedViewer).setFileNameMode(enable);
 				getContentProvider(unstagedViewer).setFileNameMode(enable);
-				refreshViewersPreservingExpandedElements();
+				UnstagedComparator comparator = (UnstagedComparator) unstagedViewer
+						.getComparator();
+				comparator.setFileNamesFirst(enable);
 				getPreferenceStore().setValue(
 						UIPreferences.STAGING_VIEW_FILENAME_MODE, enable);
+				refreshViewersPreservingExpandedElements();
 			}
 		};
 		fileNameModeAction.setChecked(getPreferenceStore().getBoolean(
@@ -3322,9 +3326,21 @@ public class StagingView extends ViewPart implements IShowInSource {
 
 		private Comparator<String> comparator;
 
-		private UnstagedComparator(boolean alphabeticSort) {
+		private boolean fileNamesFirst;
+
+		private UnstagedComparator(boolean alphabeticSort,
+				boolean fileNamesFirst) {
 			this.alphabeticSort = alphabeticSort;
+			this.setFileNamesFirst(fileNamesFirst);
 			comparator = CommonUtils.STRING_ASCENDING_COMPARATOR;
+		}
+
+		public boolean isFileNamesFirst() {
+			return fileNamesFirst;
+		}
+
+		public void setFileNamesFirst(boolean fileNamesFirst) {
+			this.fileNamesFirst = fileNamesFirst;
 		}
 
 		private void setAlphabeticSort(boolean sort) {
@@ -3355,19 +3371,23 @@ public class StagingView extends ViewPart implements IShowInSource {
 				return cat1 - cat2;
 			}
 
-			String name1 = getStagingEntryName(e1);
-			String name2 = getStagingEntryName(e2);
+			String name1 = getStagingEntryText(e1);
+			String name2 = getStagingEntryText(e2);
 
 			return comparator.compare(name1, name2);
 		}
 
-		private String getStagingEntryName(Object element) {
-			String name = ""; //$NON-NLS-1$
+		private String getStagingEntryText(Object element) {
+			String text = ""; //$NON-NLS-1$
 			StagingEntry stagingEntry = getStagingEntry(element);
 			if (stagingEntry != null) {
-				name = stagingEntry.getName();
+				if (isFileNamesFirst()) {
+					text = stagingEntry.getName();
+				} else {
+					text = stagingEntry.getPath();
+				}
 			}
-			return name;
+			return text;
 		}
 
 		@Nullable
@@ -3397,5 +3417,6 @@ public class StagingView extends ViewPart implements IShowInSource {
 				return super.category(entry);
 			}
 		}
+
 	}
 }
