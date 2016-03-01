131fbaff7f08f4c7539f969818cc8f64efb40428
Matthias Sohn <matthias.sohn@sap.com>
2016-03-01 01:10:28 +0100

    Show short commit message also in project text decorator
    Use the new decoration variable introduced for submodule decoration also
    to show short commit message on Eclipse projects.
    
    Change-Id: Ib32b2a3c56ce3529717900569217b23d893cd7db
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/decorators/GitLightweightDecorator.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/decorators/GitLightweightDecorator.java
@@ -292,7 +292,7 @@ public class GitLightweightDecorator extends LabelProvider implements
 		public static final String FOLDER_FORMAT_DEFAULT = "{dirty:>} {name}"; //$NON-NLS-1$
 
 		/** */
-		public static final String PROJECT_FORMAT_DEFAULT = "{dirty:>} {name} [{repository }{branch}{ branch_status}]"; //$NON-NLS-1$
+		public static final String PROJECT_FORMAT_DEFAULT = "{dirty:>} {name} [{repository }{branch}{ branch_status}]{ short_message}"; //$NON-NLS-1$
 
 		/** */
 		public static final String SUBMODULE_FORMAT_DEFAULT = "{dirty:>} {name} [{branch}{ branch_status}]{ short_message}"; //$NON-NLS-1$
