ec497b0950af3ffad53f619fd47ed9b77220303e
Andre Bossert <anb0s@anbos.de>
2016-03-16 17:56:14 +0100

    Fixed repository search dialog to detect .git files.
    The recursive search did not detected working tree with .git file in
    root folder. It always looked in sub-folders and in case of .git folders
    it worked fine. Now it first checks the selected root folder and then
    looks in sub-folders if selected. The Git private folders like .git
    folder itself are not traversed anymore.
    
    This prepares EGit to still properly detect git repositories when
    JGit change "https://git.eclipse.org/r/#/c/67873/" adds the evaluation
    of ".git" file with "gitdir" link to worktree and evaluates commondir.
    
    Bug: 477475
    Change-Id: I9282c3431ca9946159f389168e4ad322ab42838c
    Signed-off-by: Andre Bossert <anb0s@anbos.de>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositorySearchDialog.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositorySearchDialog.java
@@ -42,6 +42,7 @@ import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.RepositoryCache.FileKey;
 import org.eclipse.jgit.util.FS;
 import org.eclipse.jgit.util.FileUtils;
@@ -407,28 +408,34 @@ public class RepositorySearchDialog extends WizardPage {
 		if (!root.exists() || !root.isDirectory()) {
 			return;
 		}
-		File[] children = root.listFiles();
-		// simply ignore null
-		if (children == null)
-			return;
 
-		for (File child : children) {
-			if (monitor.isCanceled())
+		// check the root first
+		File resolved = FileKey.resolve(root, FS.DETECTED);
+		if (resolved != null) {
+			strings.add(resolved.getAbsoluteFile());
+			monitor.setTaskName(NLS.bind(
+					UIText.RepositorySearchDialog_RepositoriesFound_message,
+					Integer.valueOf(strings.size())));
+		}
+
+		// check nested only if enabled and we are not in private git folder
+		// ".git" itself
+		if (lookForNestedRepositories && !root.equals(resolved)) {
+			File[] children = root.listFiles();
+			// simply ignore null
+			if (children == null)
 				return;
-			if (!child.isDirectory())
-				continue;
-
-			File resolved = FileKey.resolve(child, FS.DETECTED);
-			if (resolved != null) {
-				strings.add(resolved.getAbsoluteFile());
-				monitor.setTaskName(NLS.bind(
-						UIText.RepositorySearchDialog_RepositoriesFound_message,
-						Integer.valueOf(strings.size())));
-			}
-			else if (lookForNestedRepositories) {
-				monitor.subTask(child.getPath());
-				findGitDirsRecursive(child, strings, monitor,
-						lookForNestedRepositories);
+			for (File child : children) {
+				if (monitor.isCanceled())
+					return;
+				// skip files and .git subfolders of root, because already added
+				// above, see 'check the root first'
+				if (child.isDirectory()
+						&& !child.equals(new File(root, Constants.DOT_GIT))) {
+					monitor.subTask(child.getPath());
+					findGitDirsRecursive(child, strings, monitor,
+							lookForNestedRepositories);
+				}
 			}
 		}
 	}
