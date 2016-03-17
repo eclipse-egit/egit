5d7cb62df85169e6389ec37fab9219dbe03705e6
Thomas Wolf <thomas.wolf@paranor.ch>
2016-03-17 07:58:23 +0100

    Fix potential NPE in IndexDiffCacheEntry
    It's possible that an IndexDiffCacheEntry is disposed while an
    IndexDiffUpdateJob is running. While we do cancel such a job, we do not
    join on it. We do, however, null out the previous IndexDiffData. A still
    running job therefore needs to be careful about using this property.
    
    I've seen this occasionally in our Hudson logs, the last time in [1].
    
    [1] https://hudson.eclipse.org/egit/job/egit.gerrit/8298/console
    
    Change-Id: I88165cc20d4886444009c6654293ff461a777b02
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/indexdiff/IndexDiffCacheEntry.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/indexdiff/IndexDiffCacheEntry.java
@@ -569,7 +569,13 @@ public class IndexDiffCacheEntry {
 		diffForChangedResources.setFilter(PathFilterGroup
 				.createFromStrings(treeFilterPaths));
 		diffForChangedResources.diff(jgitMonitor, 0, 0, jobName);
-		return new IndexDiffData(indexDiffData, filesToUpdate,
+		IndexDiffData previous = indexDiffData;
+		if (monitor.isCanceled() || previous == null) {
+			// Can happen when the index diff cache entry is already disposed,
+			// but the updateJob is still running (and about to cancel).
+			return null;
+		}
+		return new IndexDiffData(previous, filesToUpdate,
 				resourcesToUpdate, diffForChangedResources);
 	}
 
