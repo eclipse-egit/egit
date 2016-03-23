9e0485d5ab125a1ab4fec41f44023dfb70e71b4a
Matthias Sohn <matthias.sohn@sap.com>
2016-03-23 01:13:46 +0100

    Move EclipseSshSessionFactory to org.eclipse.egit.core
    This allows using this factory in headless environment. Preparation to
    expose an extension point to allow custom ssh session configuration.
    
    Bug: 489870
    Change-Id: I7d4c7aff49b4f4c2f72739c51b0a07d1fc9e3a4a
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/Activator.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/Activator.java
@@ -62,8 +62,6 @@ import org.eclipse.jgit.events.ListenerHandle;
 import org.eclipse.jgit.events.RepositoryEvent;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.transport.CredentialsProvider;
-import org.eclipse.jgit.transport.SshSessionFactory;
-import org.eclipse.jsch.core.IJSchService;
 import org.eclipse.osgi.service.debug.DebugOptions;
 import org.eclipse.osgi.service.debug.DebugOptionsListener;
 import org.eclipse.swt.graphics.Font;
@@ -311,7 +309,6 @@ public class Activator extends AbstractUIPlugin implements DebugOptionsListener 
 		context.registerService(DebugOptionsListener.class.getName(), this,
 				props);
 
-		setupSSH(context);
 		setupProxy(context);
 		setupRepoChangeScanner();
 		setupRepoIndexRefresh();
@@ -681,17 +678,6 @@ public class Activator extends AbstractUIPlugin implements DebugOptionsListener 
 		rcs = new RepositoryChangeScanner();
 		rcs.setSystem(true);
 		rcs.schedule(RepositoryChangeScanner.REPO_SCAN_INTERVAL);
-	}
-
-	@SuppressWarnings("unchecked")
-	private void setupSSH(final BundleContext context) {
-		final ServiceReference ssh;
-
-		ssh = context.getServiceReference(IJSchService.class.getName());
-		if (ssh != null) {
-			SshSessionFactory.setInstance(new EclipseSshSessionFactory(
-					(IJSchService) context.getService(ssh)));
-		}
 	}
 
 	private void setupProxy(final BundleContext context) {
