b4d9412fa65ecf11ce5482603f82709d1df63c4b
Thomas Wolf <thomas.wolf@paranor.ch>
2016-04-02 23:55:37 +0200

    Respect openssh config in EGit
    Ensure the JSch instance used knows about ~/.ssh/config. This
    enables JSch to honor more user configurations (see class
    com.jcraft.jsch.Session.applyConfig()), in particular also the
    UserKnownHostsFile configuration.
    
    Note that JSch limits the value of UserKnownHostsFile to a single
    value that moreover must not be quoted or contain blanks.
    
    Bug: 490939
    Change-Id: Icd777d4fb0a1dc33485233beb3751608c4e6500f
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/EclipseSshSessionFactory.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/EclipseSshSessionFactory.java
@@ -10,6 +10,10 @@
  *******************************************************************************/
 package org.eclipse.egit.core;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.transport.CredentialsProvider;
 import org.eclipse.jgit.transport.CredentialsProviderUserInfo;
 import org.eclipse.jgit.transport.JschConfigSessionFactory;
@@ -19,6 +23,7 @@ import org.eclipse.jsch.core.IJSchService;
 
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.OpenSSHConfig;
 import com.jcraft.jsch.Session;
 import com.jcraft.jsch.UserInfo;
 
@@ -36,16 +41,31 @@ class EclipseSshSessionFactory extends JschConfigSessionFactory {
 		// our parent class in case non-default JSch instances need to be made.
 		//
 		provider.createSession("127.0.0.1", 0, "eclipse"); //$NON-NLS-1$ //$NON-NLS-2$
-		return provider.getJSch();
+		JSch jsch = provider.getJSch();
+		File home = fs.userHome();
+		if (home == null) {
+			home = new File(".").getAbsoluteFile(); //$NON-NLS-1$
+		}
+		File config = new File(new File(home, ".ssh"), //$NON-NLS-1$
+				Constants.CONFIG);
+		if (config.canRead()) {
+			try {
+				jsch.setConfigRepository(
+						OpenSSHConfig.parseFile(config.getPath()));
+			} catch (IOException e) {
+				throw new JSchException(e.getLocalizedMessage(), e);
+			}
+		}
+		return jsch;
 	}
 
 	@Override
 	protected Session createSession(final OpenSshConfig.Host hc,
 			final String user, final String host, final int port, FS fs)
 			throws JSchException {
-		final JSch jsch = getJSch(hc, FS.DETECTED);
+		final JSch jsch = getJSch(hc, fs);
 		if (jsch == provider.getJSch()) {
-			// If its the default JSch desired, let the provider
+			// If it's the default JSch desired, let the provider
 			// manage the session creation for us.
 			//
 			return provider.createSession(host, port, user);
@@ -53,6 +73,7 @@ class EclipseSshSessionFactory extends JschConfigSessionFactory {
 			// This host configuration is using a different IdentityFile,
 			// one that is not available through the default JSch.
 			//
+			jsch.setConfigRepository(provider.getJSch().getConfigRepository());
 			return jsch.getSession(user, host, port);
 		}
 	}
