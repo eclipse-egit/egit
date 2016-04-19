8b44b4390af2a029000515aa3d8bdcbffb41a14d
Max Hohenegger <eclipse@hohenegger.eu>
2016-04-19 22:57:50 +0200

    Mark current branch with check symbol in Gitflow feature checkout dialog
    This should make the dialog more consistent with other EGit checkout
    dialogs.
    
    Change-Id: Iafcf345e3589af903c7a003c8f13df6166b80f3e
    Signed-off-by: Max Hohenegger <eclipse@hohenegger.eu>

---
+++ org.eclipse.egit.gitflow.ui/src/org/eclipse/egit/gitflow/ui/internal/dialogs/DecoratedBranchLabelProvider.java
@@ -0,0 +1,61 @@
+package org.eclipse.egit.gitflow.ui.internal.dialogs;
+
+import java.io.IOException;
+
+import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.internal.UIIcons;
+import org.eclipse.jface.resource.ResourceManager;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.swt.graphics.Image;
+
+class DecoratedBranchLabelProvider extends ColumnLabelProvider {
+	private ResourceManager resourceManager = Activator.getDefault().getResourceManager();
+
+	private Repository repository;
+
+	private String prefix;
+
+	public DecoratedBranchLabelProvider(Repository repository, String prefix) {
+		this.repository = repository;
+		this.prefix = prefix;
+	}
+
+	@Override
+	public String getText(Object element) {
+		if (element instanceof Ref) {
+			String name = ((Ref) element).getName();
+			return name.substring(prefix.length());
+		}
+		return super.getText(element);
+	}
+
+	@Override
+	public Image getImage(Object element) {
+		if (element instanceof Ref) {
+			return decorateImage((Ref) element);
+		}
+		return super.getImage(element);
+	}
+
+	private Image decorateImage(Ref node) {
+		String refName = node.getName();
+
+		String branchName;
+		String compareString;
+
+		try {
+			branchName = repository.getFullBranch();
+			compareString = refName;
+		} catch (IOException e) {
+			return UIIcons.getImage(resourceManager, UIIcons.BRANCH);
+		}
+
+		if (compareString.equals(branchName)) {
+			return UIIcons.getImage(resourceManager, UIIcons.CHECKED_OUT_BRANCH);
+		} else {
+			return UIIcons.getImage(resourceManager, UIIcons.BRANCH);
+		}
+	}
+}
