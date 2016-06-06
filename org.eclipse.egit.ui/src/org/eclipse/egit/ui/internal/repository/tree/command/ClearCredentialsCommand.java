b0e3081184e65d5c81743c7ee4828dbd8c905d40
Matthias Sohn <matthias.sohn@sap.com>
2016-06-06 16:18:22 +0200

    Improve error handling when using SecureStore
    - if reading credentials fails remove the failing credentials from
    secure store, otherwise the same error will occur again next time when
    trying again to get the same credentials from a corrupted entry stored
    in secure store
    - improve error messages logged when a secure store operation fails
    - add SecureStoreUtils.clearCredentials() to simplify error handling
    
    Bug: 495539
    Change-Id: Ie6e8712b4a8eb936bfe874c2f01ce081305ba156
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/ClearCredentialsCommand.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/ClearCredentialsCommand.java
@@ -8,13 +8,12 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.repository.tree.command;
 
-import java.io.IOException;
 import java.net.URISyntaxException;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.egit.ui.Activator;
-import org.eclipse.egit.ui.internal.UIText;
+import org.eclipse.egit.ui.internal.SecureStoreUtils;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
 import org.eclipse.jgit.transport.URIish;
 
@@ -37,11 +36,7 @@ public class ClearCredentialsCommand extends
 			Activator.handleError(e.getMessage(), e, true);
 			return null;
 		}
-		try {
-			org.eclipse.egit.core.Activator.getDefault().getSecureStore().clearCredentials(uri);
-		} catch (IOException e) {
-			Activator.handleError(UIText.ClearCredentialsCommand_clearingCredentialsFailed, e, true);
-		}
+		SecureStoreUtils.clearCredentials(uri);
 		return null;
 	}
 }
