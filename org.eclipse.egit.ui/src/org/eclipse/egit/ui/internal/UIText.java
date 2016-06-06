a69e7617c2551004f3bcf57ef7ba8ea610446c5a
Matthias Sohn <matthias.sohn@sap.com>
2016-06-06 16:18:22 +0200

    Improve error handling when using SecureStore
    - if reading credentials fails remove the failing credentials from
    secure store, otherwise the same error will occur again next time when
    trying again to get the same credentials from a corrupted entry stored
    in secure store
    - improve error messages logged when a secure store operation fails
    - add SecureStoreUtils.clearCredentials() to simplify error handling
    
    Bug: 495539
    Change-Id: Ie6e8712b4a8eb936bfe874c2f01ce081305ba156
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -192,6 +192,12 @@ public class UIText extends NLS {
 	public static String WizardProjectsImportPage_CreateProjectsTask;
 
 	/** */
+	public static String SecureStoreUtils_errorClearingCredentials;
+
+	/** */
+	public static String SecureStoreUtils_errorReadingCredentials;
+
+	/** */
 	public static String SecureStoreUtils_writingCredentialsFailed;
 
 	/** */
@@ -265,12 +271,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String GerritConfigurationPage_UserLabel;
-
-	/** */
-	public static String EGitCredentialsProvider_errorReadingCredentials;
-
-	/** */
-	public static String EGitCredentialsProvider_FailedToClearCredentials;
 
 	/** */
 	public static String EgitUiUtils_CouldNotOpenEditorMessage;
