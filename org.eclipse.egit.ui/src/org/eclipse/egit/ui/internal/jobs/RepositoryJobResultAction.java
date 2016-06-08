cfde29256566f8620688db0586ece6e9b2e3a9d1
Thomas Wolf <thomas.wolf@paranor.ch>
2016-06-08 22:10:35 +0200

    Avoid opening dialogs from push/fetch background jobs
    Doing so quite frequently led to UI deadlocks when modal dialogs were
    active when the job's result dialog popped up. Instead use the progress
    framework's mechanisms to give the user a possibility to open a job's
    result dialog at his leisure. Thus there is no danger of conflicting
    with any modal shells.
    
    Push and fetch UI tests adapted to the new behavior.
    
    Bug: 495512
    Change-Id: I10f8df000fdfc4c91f3d5a9b75354caf2a37d876
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/jobs/RepositoryJobResultAction.java
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Thomas Wolf <thomas.wolf@paranor.ch>
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.jobs;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.MessageFormat;
+
+import org.eclipse.egit.core.RepositoryCache;
+import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.internal.UIText;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jgit.annotations.NonNull;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.RepositoryCache.FileKey;
+import org.eclipse.jgit.util.FS;
+
+/**
+ * An {@link Action} to report some background job result.
+ */
+public abstract class RepositoryJobResultAction extends Action {
+
+	/**
+	 * Because this action may be associated with a job and may be invoked long
+	 * after the repository operation had been performed we do not keep a
+	 * reference to the {@link Repository} here. The repository might have been
+	 * deleted in the meantime.
+	 */
+	private final File repositoryDir;
+
+	private boolean repositoryGone;
+
+	/**
+	 * Creates a new {@link RepositoryJobResultAction}.
+	 *
+	 * @param repository
+	 *            the result belongs to
+	 * @param title
+	 *            of the action
+	 */
+	public RepositoryJobResultAction(@NonNull Repository repository,
+			String title) {
+		super(title);
+		this.repositoryDir = repository.getDirectory();
+	}
+
+	@Override
+	public final void run() {
+		Repository repo = null;
+		if (!repositoryGone) {
+			RepositoryCache repoCache = org.eclipse.egit.core.Activator
+					.getDefault().getRepositoryCache();
+			repo = repoCache.getRepository(repositoryDir);
+			if (repo == null
+					&& FileKey.isGitRepository(repositoryDir, FS.DETECTED)) {
+				// No longer in the Egit cache but still on disk
+				try {
+					repo = repoCache.lookupRepository(repositoryDir);
+				} catch (IOException e) {
+					// Ignore, repo remains null
+				}
+			}
+			repositoryGone = repo == null;
+		}
+		if (repositoryGone || repo == null) {
+			Activator.showError(MessageFormat.format(
+					UIText.RepositoryJobResultAction_RepositoryGone,
+					repositoryDir), null);
+			return;
+		}
+		showResult(repo);
+	}
+
+	/**
+	 * Shows the job result to the user.
+	 *
+	 * @param repository
+	 *            the result belongs to
+	 */
+	abstract protected void showResult(@NonNull Repository repository);
+}
