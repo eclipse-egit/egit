7119b313588057727e73545027e7fed0a353b311
Thomas Wolf <thomas.wolf@paranor.ch>
2016-06-08 22:10:35 +0200

    Avoid opening dialogs from push/fetch background jobs
    Doing so quite frequently led to UI deadlocks when modal dialogs were
    active when the job's result dialog popped up. Instead use the progress
    framework's mechanisms to give the user a possibility to open a job's
    result dialog at his leisure. Thus there is no danger of conflicting
    with any modal shells.
    
    Push and fetch UI tests adapted to the new behavior.
    
    Bug: 495512
    Change-Id: I10f8df000fdfc4c91f3d5a9b75354caf2a37d876
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/fetch/FetchOperationUI.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/fetch/FetchOperationUI.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 SAP AG.
+ * Copyright (c) 2011, 2016 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,25 +7,27 @@
  *
  * Contributors:
  *    Mathias Kinzler (SAP AG) - initial implementation
+ *    Thomas Wolf <thomas.wolf@paranor.ch> - Bug 495512
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.fetch;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
-import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.egit.core.op.FetchOperation;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.JobFamilies;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.credentials.EGitCredentialsProvider;
+import org.eclipse.egit.ui.internal.jobs.RepositoryJob;
+import org.eclipse.egit.ui.internal.jobs.RepositoryJobResultAction;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jgit.annotations.NonNull;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.transport.CredentialsProvider;
 import org.eclipse.jgit.transport.FetchResult;
@@ -34,6 +36,8 @@ import org.eclipse.jgit.transport.RemoteConfig;
 import org.eclipse.jgit.transport.TagOpt;
 import org.eclipse.jgit.transport.URIish;
 import org.eclipse.osgi.util.NLS;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
 
 /**
  * UI Wrapper for {@link FetchOperation}
@@ -113,14 +117,20 @@ public class FetchOperationUI {
 	 * completion
 	 */
 	public void start() {
-		Job job = new WorkspaceJob(NLS.bind(
+		final Repository repo = repository;
+		if (repo == null) {
+			return;
+		}
+		Job job = new RepositoryJob(NLS.bind(
 				UIText.FetchOperationUI_FetchJobName,
 				sourceString)) {
 
+			private FetchResult result;
+
 			@Override
-			public IStatus runInWorkspace(IProgressMonitor monitor) {
+			public IStatus performJob(IProgressMonitor monitor) {
 				try {
-					execute(monitor);
+					result = execute(monitor);
 				} catch (CoreException e) {
 					return Activator.createErrorStatus(e.getStatus()
 							.getMessage(), e);
@@ -129,25 +139,21 @@ public class FetchOperationUI {
 			}
 
 			@Override
+			protected IAction getAction() {
+				return new ShowResultAction(repo, result, sourceString);
+			}
+
+			@Override
 			public boolean belongsTo(Object family) {
-				if (JobFamilies.FETCH.equals(family))
+				if (JobFamilies.FETCH.equals(family)) {
 					return true;
+				}
 				return super.belongsTo(family);
 			}
+
 		};
 		job.setUser(true);
 		job.schedule();
-		job.addJobChangeListener(new JobChangeAdapter() {
-			@Override
-			public void done(IJobChangeEvent event) {
-				if (event.getResult().isOK())
-					FetchResultDialog.show(repository, op.getOperationResult(),
-							sourceString);
-				else
-					Activator.handleError(event.getResult().getMessage(), event
-							.getResult().getException(), true);
-			}
-		});
 	}
 
 	/**
@@ -156,4 +162,28 @@ public class FetchOperationUI {
 	public String getSourceString(){
 		return sourceString;
 	}
+
+	private static class ShowResultAction extends RepositoryJobResultAction {
+
+		private final FetchResult result;
+
+		private final String source;
+
+		public ShowResultAction(@NonNull Repository repository,
+				FetchResult result, String source) {
+			super(repository, UIText.FetchOperationUI_ShowFetchResult);
+			this.result = result;
+			this.source = source;
+		}
+
+		@Override
+		protected void showResult(@NonNull Repository repository) {
+			Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+					.getShell();
+			FetchResultDialog dialog = new FetchResultDialog(shell, repository,
+					result, source);
+			dialog.open();
+		}
+	}
+
 }
