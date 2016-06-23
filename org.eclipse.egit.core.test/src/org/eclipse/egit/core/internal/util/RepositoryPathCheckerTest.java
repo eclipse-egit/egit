f19f61427fd4e6aad44b8851e7661fc814074c50
Matthias Sohn <matthias.sohn@sap.com>
2016-06-24 00:12:51 +0200

    Validate target repository path entered by the user
    Validate the repository path entered in NewRepositoryWizard or
    GitImportWizard to prevent we hit an InvalidPathException in JGit.
    
    Since it seems many users try to paste a git clone command or a URL
    instead of a local path raise a dedicated error messages for these
    cases.
    
    Bug: 496688
    Change-Id: I7c40f2d47f5b37ce50d873dbd02e6365097d7110
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

---
+++ org.eclipse.egit.core.test/src/org/eclipse/egit/core/internal/util/RepositoryPathCheckerTest.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (C) 2016, Matthias Sohn <matthias.sohn@sap.com> and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.core.internal.util;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.eclipse.jgit.util.SystemReader;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RepositoryPathCheckerTest {
+
+	private RepositoryPathChecker checker;
+
+	@Before
+	public void setup() {
+		checker = new RepositoryPathChecker();
+	}
+
+	@Test
+	public void testLocalAbsoluteFileUnix() {
+		assertTrue(checker.check("/a/b"));
+	}
+
+	@Test
+	public void testLocalAbsoluteFileWindows() {
+		Assume.assumeTrue(SystemReader.getInstance().isWindows());
+		assertTrue(checker.check("C:/a/b"));
+	}
+
+	@Test
+	public void testLocalRelativePath() {
+		assertFalse(checker.check("a/b"));
+	}
+
+	@Test
+	public void testSSH() {
+		assertFalse(checker.check("ssh://a.test:/a/b"));
+		assertFalse(checker.check("joe@a.test:/a/b"));
+	}
+
+	@Test
+	public void testHTTP() {
+		assertFalse(checker.check("https://a.test/a/b"));
+	}
+
+	@Test
+	public void testFile() {
+		assertFalse(checker.check("file://a.test/a/b"));
+	}
+
+	@Test
+	public void testFooBar() {
+		assertFalse(checker.check("foobar://a.test/a/b"));
+	}
+}
---
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/util/RepositoryPathChecker.java
@@ -0,0 +1,103 @@
+/*******************************************************************************
+ * Copyright (c) 2016, Matthias Sohn and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.core.internal.util;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.egit.core.internal.CoreText;
+import org.eclipse.jgit.annotations.Nullable;
+import org.eclipse.jgit.transport.URIish;
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * Checker for path suitable for storing a git repository
+ */
+public class RepositoryPathChecker {
+
+	private boolean hasContent;
+
+	private String errorMessage;
+
+	/**
+	 * Check if the given directory path is a valid local absolute repository
+	 * path
+	 *
+	 * @param dir
+	 *            the directory path to check
+	 * @return {@code true} if the repository path is valid
+	 */
+	public boolean check(String dir) {
+		hasContent = false;
+		errorMessage = null;
+
+		if (dir.length() == 0) {
+			errorMessage = CoreText.RepositoryPathChecker_errAbsoluteRepoPath;
+			return false;
+		}
+
+		if (dir.startsWith("git clone")) { //$NON-NLS-1$
+			errorMessage = CoreText.RepositoryPathChecker_errNoCloneCommand;
+			return false;
+		}
+		if (isURL(dir)) {
+			errorMessage = CoreText.RepositoryPathChecker_errNoURL;
+			return false;
+		}
+		File testFile = new File(dir);
+		IPath path = Path.fromOSString(dir);
+		for (String segment : path.segments()) {
+			IStatus status = ResourcesPlugin.getWorkspace()
+					.validateName(segment, IResource.FOLDER);
+			if (!status.isOK()) {
+				errorMessage = status.getMessage();
+				return false;
+			}
+		}
+		if (!path.isAbsolute()) {
+			errorMessage = NLS.bind(CoreText.RepositoryPathChecker_errNotAbsoluteRepoPath,
+					dir);
+			return false;
+		}
+		if (testFile.exists() && !testFile.isDirectory()) {
+			errorMessage = NLS.bind(CoreText.RepositoryPathChecker_errNoDirectory, dir);
+			return false;
+		}
+		hasContent = testFile.exists() && testFile.list().length > 0;
+		return true;
+	}
+
+	private boolean isURL(String dir) {
+		try {
+			URIish u = new URIish(dir);
+			return u.isRemote() || u.getScheme() != null;
+		} catch (URISyntaxException e) {
+			// skip
+		}
+		return false;
+	}
+
+	/**
+	 * @return {@code true} if the directory exists and already has content
+	 */
+	public boolean hasContent() {
+		return hasContent;
+	}
+
+	/**
+	 * @return the error message if the path is invalid
+	 */
+	public @Nullable String getErrorMessage() {
+		return errorMessage;
+	}
+}
