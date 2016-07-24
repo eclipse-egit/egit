f1c070d0317ac11086927dfe7acda986013ad216
Thomas Wolf <thomas.wolf@paranor.ch>
2016-07-24 22:18:29 +0200

    Immediate user feedback on failed push background jobs
    With commit 09e0d369 we've eliminated asynchronous dialogs from
    background jobs for pushing and fetching. Instead, we associate an
    action to open the result dialog with the job; the user can invoke that
    action from the progress view.
    
    This works nicely, but makes it easy to overlook a failed push.
    
    Try to improve that by making the job return an ERROR status when a
    push failure has occurred. This makes the job/status framework notify
    the user; the notification dialog will show the failure message and
    additionally a button giving access to the job's action, which will
    open the full push result dialog.
    
    Bug: 498176
    Change-Id: Ia12c8586a0bf3827fe8916d1fa5853cbfb1e7604
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/push/PushJob.java
@@ -0,0 +1,131 @@
+/*******************************************************************************
+ * Copyright (C) 2016, Thomas Wolf <thomas.wolf@paranor.ch>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.push;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.egit.core.op.PushOperation;
+import org.eclipse.egit.core.op.PushOperationResult;
+import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.JobFamilies;
+import org.eclipse.egit.ui.internal.UIText;
+import org.eclipse.egit.ui.internal.jobs.RepositoryJob;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.transport.PushResult;
+import org.eclipse.jgit.transport.RemoteRefUpdate;
+import org.eclipse.jgit.transport.URIish;
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * Background job for pushing to repositories.
+ */
+public class PushJob extends RepositoryJob {
+
+	private final PushOperation operation;
+
+	private final PushOperationResult resultToCompare;
+
+	private final String destinationString;
+
+	private final Repository localDb;
+
+	private final boolean showConfigureButton;
+
+	private PushOperationResult operationResult;
+
+	/**
+	 * Creates a new {@link PushJob} that performs the given
+	 * {@link PushOperation} on the given {@link Repository}.
+	 *
+	 * @param name
+	 *            of the job
+	 * @param repository
+	 *            to push to
+	 * @param operation
+	 *            to perform
+	 * @param expectedResult
+	 *            of the operation
+	 * @param destinationString
+	 *            describing where to push to
+	 * @param showConfigureButton
+	 *            whether the result dialog should have a configuration button
+	 */
+	public PushJob(String name, Repository repository, PushOperation operation,
+			PushOperationResult expectedResult, String destinationString,
+			boolean showConfigureButton) {
+		super(name);
+		this.operation = operation;
+		this.resultToCompare = expectedResult;
+		this.destinationString = destinationString;
+		this.localDb = repository;
+		this.showConfigureButton = showConfigureButton;
+	}
+
+	@Override
+	protected IStatus performJob(final IProgressMonitor monitor) {
+		try {
+			operation.run(monitor);
+		} catch (final InvocationTargetException e) {
+			return new Status(IStatus.ERROR, Activator.getPluginId(),
+					UIText.PushJob_unexpectedError, e.getCause());
+		}
+
+		operationResult = operation.getOperationResult();
+		if (!operationResult.isSuccessfulConnectionForAnyURI()) {
+			return new Status(IStatus.ERROR, Activator.getPluginId(),
+					NLS.bind(UIText.PushJob_cantConnectToAny,
+							operationResult.getErrorStringForAllURis()));
+		}
+
+		return Status.OK_STATUS;
+	}
+
+	@Override
+	protected IAction getAction() {
+		Repository repo = localDb;
+		if (repo != null && (resultToCompare == null
+				|| !resultToCompare.equals(operationResult))) {
+			return new ShowPushResultAction(repo, operationResult,
+					destinationString, showConfigureButton);
+		}
+		return null;
+	}
+
+	@Override
+	protected IStatus getDeferredStatus() {
+		for (URIish uri : operationResult.getURIs()) {
+			PushResult outcome = operationResult.getPushResult(uri);
+			for (RemoteRefUpdate update : outcome.getRemoteUpdates()) {
+				switch (update.getStatus()) {
+				case NOT_ATTEMPTED:
+				case UP_TO_DATE:
+				case OK:
+					continue;
+				default:
+					return new Status(IStatus.ERROR, Activator.getPluginId(),
+							IStatus.ERROR, update.getMessage(), null);
+				}
+			}
+		}
+		return super.getDeferredStatus();
+	}
+
+	@Override
+	public boolean belongsTo(Object family) {
+		if (JobFamilies.PUSH.equals(family)) {
+			return true;
+		}
+		return super.belongsTo(family);
+	}
+
+}
