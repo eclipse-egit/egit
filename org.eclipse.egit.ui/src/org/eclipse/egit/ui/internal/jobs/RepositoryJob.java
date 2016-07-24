e5aed89ce30a1182cfa500b633aa0131763a44f9
Thomas Wolf <thomas.wolf@paranor.ch>
2016-07-24 22:43:56 +0200

    RepositoryJob: jobs must call monitor.done()
    Jobs must tell the progress framework when they're done by calling
    monitor.done(). Without this, the status framework may use the last
    subtask's message for the action link in the progress view.
    
    Change-Id: Ic32f2f4fa1840ab1fd835ff82ac9804b03e96bc8

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/jobs/RepositoryJob.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/jobs/RepositoryJob.java
@@ -43,28 +43,30 @@ public abstract class RepositoryJob extends Job {
 
 	@Override
 	protected final IStatus run(IProgressMonitor monitor) {
-		IStatus status = performJob(monitor);
-		if (status == null) {
-			return Activator
-					.createErrorStatus(
-							MessageFormat.format(
-									UIText.RepositoryJob_NullStatus, getName()),
-							new NullPointerException());
-		} else if (!status.isOK()) {
-			return status;
-		}
-		IAction action = getAction();
-		if (action != null) {
-			if (isModal()) {
-				showResult(action);
-			} else {
-				setProperty(IProgressConstants.KEEP_PROPERTY, Boolean.TRUE);
-				setProperty(IProgressConstants.ACTION_PROPERTY, action);
-				return new Status(IStatus.OK, Activator.getPluginId(),
-						IStatus.OK, action.getText(), null);
+		try {
+			IStatus status = performJob(monitor);
+			if (status == null) {
+				return Activator.createErrorStatus(MessageFormat
+						.format(UIText.RepositoryJob_NullStatus, getName()),
+						new NullPointerException());
+			} else if (!status.isOK()) {
+				return status;
 			}
+			IAction action = getAction();
+			if (action != null) {
+				if (isModal()) {
+					showResult(action);
+				} else {
+					setProperty(IProgressConstants.KEEP_PROPERTY, Boolean.TRUE);
+					setProperty(IProgressConstants.ACTION_PROPERTY, action);
+					return new Status(IStatus.OK, Activator.getPluginId(),
+							IStatus.OK, action.getText(), null);
+				}
+			}
+			return status;
+		} finally {
+			monitor.done();
 		}
-		return status;
 	}
 
 	/**
