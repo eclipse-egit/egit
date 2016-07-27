0fab91c433ac429a7484a5c73d2a41789b445915
Thomas Wolf <thomas.wolf@paranor.ch>
2016-07-28 01:17:57 +0200

    Save preference stores in preference pages
    On preference pages that use more than one preference store, these
    extra stores must be saved explicitly in performOk(). The framework
    (PreferenceDialog) only saves the store set on the page itself, but
    not necessarily other stores set on individual field editors.
    
    Bug: 498548
    Change-Id: I5e352e0af701c9e49c9de45a2101534afe12d944
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/preferences/ProjectsPreferencePage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/preferences/ProjectsPreferencePage.java
@@ -1,13 +1,20 @@
 /*******************************************************************************
- * Copyright (C) 2011, Matthias Sohn <matthias.sohn@sap.com>
+ * Copyright (C) 2011, 2016 Matthias Sohn <matthias.sohn@sap.com> and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Thomas Wolf <thomas.wolf@paranor.ch> - Bug 498548
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.preferences;
 
+import java.io.IOException;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.egit.core.Activator;
 import org.eclipse.egit.core.GitCorePreferences;
@@ -15,7 +22,10 @@ import org.eclipse.egit.ui.UIPreferences;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.IPersistentPreferenceStore;
 import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.util.Policy;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.preferences.ScopedPreferenceStore;
@@ -37,6 +47,30 @@ public class ProjectsPreferencePage extends FieldEditorPreferencePage implements
 	@Override
 	public void init(final IWorkbench workbench) {
 		// Do nothing.
+	}
+
+	@Override
+	public boolean performOk() {
+		boolean isOk = super.performOk();
+		if (isOk) {
+			IPreferenceStore uiPreferences = org.eclipse.egit.ui.Activator
+					.getDefault().getPreferenceStore();
+			if (uiPreferences.needsSaving()
+					&& (uiPreferences instanceof IPersistentPreferenceStore)) {
+				try {
+					((IPersistentPreferenceStore) uiPreferences).save();
+				} catch (IOException e) {
+					String message = JFaceResources.format(
+							"PreferenceDialog.saveErrorMessage", getTitle(), //$NON-NLS-1$
+							e.getMessage());
+					Policy.getStatusHandler().show(
+							new Status(IStatus.ERROR, Policy.JFACE, message, e),
+							JFaceResources.getString(
+									"PreferenceDialog.saveErrorTitle")); //$NON-NLS-1$
+				}
+			}
+		}
+		return isOk;
 	}
 
 	@Override
