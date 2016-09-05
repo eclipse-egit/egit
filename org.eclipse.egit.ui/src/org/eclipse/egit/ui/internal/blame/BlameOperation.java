e7e7eb421a0be881b3865bfec8533fea959bec98
Thomas Wolf <thomas.wolf@paranor.ch>
2016-09-05 08:19:40 +0200

    [findbugs] Missing override of equals()
    Commit bc712de0 had introduced an equals() method on HistoryPageInput.
    The BlameHistoryPageInput must override this to account for the extra
    field.
    
    Change-Id: I545c5033e67a01338edf4dc6696c36531fd56de6
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/blame/BlameOperation.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/blame/BlameOperation.java
@@ -85,6 +85,25 @@ public class BlameOperation implements IEGitOperation {
 				return commit;
 			return Platform.getAdapterManager().getAdapter(this, adapter);
 		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (obj == this) {
+				return true;
+			}
+			if (!(obj instanceof BlameHistoryPageInput)) {
+				return false;
+			}
+			BlameHistoryPageInput other = (BlameHistoryPageInput) obj;
+			return super.equals(obj)
+					&& (commit == other.commit
+							|| commit != null && commit.equals(other.commit));
+		}
+
+		@Override
+		public int hashCode() {
+			return super.hashCode() ^ (commit == null ? 0 : commit.hashCode());
+		}
 	}
 
 	private static class RevisionSelectionHandler implements
