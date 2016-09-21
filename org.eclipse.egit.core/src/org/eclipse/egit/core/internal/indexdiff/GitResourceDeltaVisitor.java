58b493bbe6e46be05c01fb9ef8428cf02c9d2b0f
Andrey Rodionov <rodionovamp@mail.ru>
2016-09-21 14:39:30 +0300

    Click on search result causes EGit to stat the repository directory
    Bug: 501799
    Change-Id: I45efcbbd54bbfbb1e9acf2c19371cba2c9c0d1e4
    Signed-off-by: Andrey Rodionov <rodionovamp@mail.ru>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/indexdiff/GitResourceDeltaVisitor.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/indexdiff/GitResourceDeltaVisitor.java
@@ -174,11 +174,7 @@ public class GitResourceDeltaVisitor implements IResourceDeltaVisitor {
 			return false;
 		}
 
-		// If the file has changed but not in a way that we
-		// care about (e.g. marker changes to files) then
-		// ignore
-		if (delta.getKind() == IResourceDelta.CHANGED
-				&& (delta.getFlags() & INTERESTING_CHANGES) == 0) {
+		if (!isInteresting(delta)) {
 			return false;
 		}
 
@@ -203,6 +199,21 @@ public class GitResourceDeltaVisitor implements IResourceDeltaVisitor {
 
 		filesToUpdate.add(path);
 		resourcesToUpdate.add(resource);
+		return true;
+	}
+
+	/**
+	 * If the file has changed but not in a way that we care about (e.g. marker
+	 * changes to files) then ignore
+	 *
+	 * @param delta
+	 * @return true - if change is interesting
+	 */
+	static boolean isInteresting(IResourceDelta delta) {
+		if (delta.getKind() == IResourceDelta.CHANGED
+				&& (delta.getFlags() & INTERESTING_CHANGES) == 0) {
+			return false;
+		}
 		return true;
 	}
 
