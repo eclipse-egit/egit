0914736f278bcc755b00b05e47e3ec8d8fbafa38
Thomas Wolf <thomas.wolf@paranor.ch>
2016-10-09 21:29:21 +0200

    Fix the enablement of "Compare with->Index with HEAD"
    This menu command makes sense only for staged files, but it was enabled
    wrongly also on unstaged files. Apparently that was done on purpose in
    commit 6d975be6 to avoid a costly index diff calculation to find out
    whether the file was staged.
    
    But if we are able to display "staged" decorations speedily, we
    certainly are also able to determine quickly whether a file is staged.
    Make isEnabled() properly check again, using the same mechanisms we use
    for decorations, which rely on the IndexDiffCache. Remove the extra
    check when the command is run, including the dialog.
    
    There is one quirk here: these "Compare With..." actions are contributed
    using the old (and deprecated) org.eclipse.ui.popupMenus extension point
    with <objectContribution>. They get invoked via RepositoryAction, which
    has some extra logic to avoid re-computing enablement when the selection
    didn't change as faras resources are concerned. This of course goes
    wrong if the enablement state of a handler depends on other state that
    may have changed, such as the resources git state (staged/modified/...).
    
    Add some logic to enforce always updating enablement for such handlers.
    This works and is even uncritical when typing in an editor because:
    * determining staging state via the IndexDiffCache is fast, and
    * the enablement is only computed when the context menu is opened.
    
    Change-Id: Ic6ec1e407f2ba1403ae52bc09b4f2546f4e221c8
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/RepositoryAction.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/RepositoryAction.java
@@ -112,18 +112,28 @@ public abstract class RepositoryAction extends AbstractHandler implements
 	@Override
 	public final void selectionChanged(IAction action, ISelection selection) {
 		mySelection = selection;
-		// Compare selection of handler, as it converts it to a suitable
-		// selection. E.g. an ITextSelection is converted to a selection of the
-		// file. We are only interested in the selection change if a different
-		// file was selected, not if the offset of the text selection changed.
-		IStructuredSelection selectionBefore = handler.getSelection();
-		handler.setSelection(mySelection);
-		if (action != null) {
-			IStructuredSelection selectionAfter = handler.getSelection();
-			boolean equalSelection = (selectionBefore == null) ? selectionAfter == null
-					: selectionBefore.equals(selectionAfter);
-			if (!equalSelection)
+		if (handler.alwaysCheckEnabled()) {
+			handler.setSelection(mySelection);
+			if (action != null) {
 				action.setEnabled(isEnabled());
+			}
+		} else {
+			// Compare selection of handler, as it converts it to a suitable
+			// selection. E.g. an ITextSelection is converted to a selection of
+			// the file. We are only interested in the selection change if a
+			// different file was selected, not if the offset of the text
+			// selection changed.
+			IStructuredSelection selectionBefore = handler.getSelection();
+			handler.setSelection(mySelection);
+			if (action != null) {
+				IStructuredSelection selectionAfter = handler.getSelection();
+				boolean equalSelection = (selectionBefore == null)
+						? selectionAfter == null
+						: selectionBefore.equals(selectionAfter);
+				if (!equalSelection) {
+					action.setEnabled(isEnabled());
+				}
+			}
 		}
 	}
 
