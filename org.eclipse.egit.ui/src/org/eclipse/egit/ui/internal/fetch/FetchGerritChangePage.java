5b227fc3b7af63f3915b5569d081588f545bfd89
Matthias Sohn <matthias.sohn@sap.com>
2017-01-10 01:56:06 +0100

    Use generic comparator to sort change numbers in FetchGerritChangePage
    Change-Id: I5515e74198002e645d9881c0571fd424a668ee6f
    Also-By: Jaxsun McCarthy Huggan <jaxsun.mccarthy@tasktop.com>
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/fetch/FetchGerritChangePage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/fetch/FetchGerritChangePage.java
@@ -18,7 +18,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.SortedSet;
 import java.util.TreeSet;
@@ -669,21 +668,7 @@ public class FetchGerritChangePage extends WizardPage {
 									changeRefs.add(change);
 							}
 							Collections.sort(changeRefs,
-									new Comparator<Change>() {
-										@Override
-										public int compare(Change o1, Change o2) {
-											// change number descending
-											int changeDiff = o2.changeNumber
-													.compareTo(o1.changeNumber);
-											if (changeDiff == 0)
-												// patch set number descending
-												changeDiff = o2
-														.getPatchSetNumber()
-														.compareTo(
-																o1.getPatchSetNumber());
-											return changeDiff;
-										}
-									});
+									Collections.reverseOrder());
 						}
 					});
 		}
