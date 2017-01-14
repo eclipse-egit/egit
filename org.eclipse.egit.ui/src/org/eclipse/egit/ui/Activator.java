69f1bded3f0d453d0116b005c737ffe14e8238df
Andrey Loskutov <loskutov@gmx.de>
2017-01-14 23:59:52 +0100

    Provide a way to configure RepositoryChangeScanner interval
    RepositoryChangeScanner was triggered every 10 seconds if the UI was
    active. This is an overkill and causes unneeded CPU load by idling
    Eclipse. This change increases the interval to 5 minutes, adds new
    "Refresh interval" preference to the root Git preference page and
    changes the job in the way that it listens to the preference change. If
    the preference is set to 0, the job stops automatic repositories change
    check.
    
    Change-Id: I02751334d4cb6dea97f575d24a5fa24c6832eee4
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/Activator.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/Activator.java
@@ -386,8 +386,10 @@ public class Activator extends AbstractUIPlugin implements DebugOptionsListener 
 			@Override
 			public void windowActivated(IWorkbenchWindow window) {
 				updateUiState();
-				if (rcs.doReschedule)
-					rcs.schedule();
+				if (rcs.doReschedule) {
+					// 500: give the UI task a chance to update the active state
+					rcs.schedule(500);
+				}
 				refreshJob.triggerRefresh();
 			}
 		};
@@ -589,16 +591,22 @@ public class Activator extends AbstractUIPlugin implements DebugOptionsListener 
 	 * A Job that looks at the repository meta data and triggers a refresh of
 	 * the resources in the affected projects.
 	 */
-	static class RepositoryChangeScanner extends Job {
+	static class RepositoryChangeScanner extends Job
+			implements IPropertyChangeListener {
+
+		// volatile in order to ensure thread synchronization
+		private volatile boolean doReschedule;
+
+		private int interval;
+
 		RepositoryChangeScanner() {
 			super(UIText.Activator_repoScanJobName);
 			setRule(new RepositoryCacheRule());
+			setSystem(true);
+			setUser(false);
+			updateRefreshInterval();
 		}
 
-		// FIXME, need to be more intelligent about this to avoid too much work
-		private static final long REPO_SCAN_INTERVAL = 10000L;
-		// volatile in order to ensure thread synchronization
-		private volatile boolean doReschedule = true;
 
 		void setReschedule(boolean reschedule){
 			doReschedule = reschedule;
@@ -606,20 +614,22 @@ public class Activator extends AbstractUIPlugin implements DebugOptionsListener 
 
 		@Override
 		protected IStatus run(IProgressMonitor monitor) {
-			if (!doReschedule)
+			if (!doReschedule) {
 				return Status.OK_STATUS;
+			}
 
 			// The core plugin might have been stopped before we could cancel
 			// this job.
 			RepositoryCache repositoryCache = org.eclipse.egit.core.Activator
 					.getDefault().getRepositoryCache();
-			if (repositoryCache == null)
+			if (repositoryCache == null) {
 				return Status.OK_STATUS;
+			}
 
 			// When people use Git from the command line a lot of changes
 			// may happen. Don't scan when inactive depending on the user's
 			// choice.
-			if (Activator.getDefault().getPreferenceStore()
+			if (getDefault().getPreferenceStore()
 					.getBoolean(UIPreferences.REFESH_ONLY_WHEN_ACTIVE)) {
 				if (!isActive()) {
 					monitor.done();
@@ -628,48 +638,82 @@ public class Activator extends AbstractUIPlugin implements DebugOptionsListener 
 			}
 
 			Repository[] repos = repositoryCache.getAllRepositories();
-			if (repos.length == 0)
+			if (repos.length == 0) {
 				return Status.OK_STATUS;
+			}
 
 			monitor.beginTask(UIText.Activator_scanningRepositories,
 					repos.length);
 			try {
 				for (Repository repo : repos) {
-					if (monitor.isCanceled())
+					if (monitor.isCanceled()) {
 						break;
-					if (GitTraceLocation.REPOSITORYCHANGESCANNER.isActive())
+					}
+					if (GitTraceLocation.REPOSITORYCHANGESCANNER.isActive()) {
 						GitTraceLocation.getTrace().trace(
 								GitTraceLocation.REPOSITORYCHANGESCANNER
 										.getLocation(),
 								"Scanning " + repo + " for changes"); //$NON-NLS-1$ //$NON-NLS-2$
+					}
 
 					repo.scanForRepoChanges();
 					monitor.worked(1);
 				}
 			} catch (IOException e) {
-				if (GitTraceLocation.REPOSITORYCHANGESCANNER.isActive())
+				if (GitTraceLocation.REPOSITORYCHANGESCANNER.isActive()) {
 					GitTraceLocation.getTrace().trace(
 							GitTraceLocation.REPOSITORYCHANGESCANNER
 									.getLocation(),
 							"Stopped rescheduling " + getName() + "job"); //$NON-NLS-1$ //$NON-NLS-2$
+				}
 				return createErrorStatus(UIText.Activator_scanError, e);
 			} finally {
 				monitor.done();
 			}
-			if (GitTraceLocation.REPOSITORYCHANGESCANNER.isActive())
+			if (GitTraceLocation.REPOSITORYCHANGESCANNER.isActive()) {
 				GitTraceLocation.getTrace().trace(
 						GitTraceLocation.REPOSITORYCHANGESCANNER.getLocation(),
 						"Rescheduling " + getName() + " job"); //$NON-NLS-1$ //$NON-NLS-2$
-			if (doReschedule)
-				schedule(REPO_SCAN_INTERVAL);
+			}
+			if (doReschedule) {
+				schedule(interval);
+			}
 			return Status.OK_STATUS;
+		}
+
+		@Override
+		public void propertyChange(PropertyChangeEvent event) {
+			if (!UIPreferences.REFESH_INDEX_INTERVAL
+					.equals(event.getProperty())) {
+				return;
+			}
+			updateRefreshInterval();
+		}
+
+		void updateRefreshInterval() {
+			interval = getRefreshIndexInterval();
+			if (interval > 0) {
+				setReschedule(true);
+				schedule(interval);
+			} else {
+				setReschedule(false);
+				cancel();
+			}
+		}
+
+		/**
+		 * @return interval in milliseconds for automatic index check, 0 is if
+		 *         check should be disabled
+		 */
+		static int getRefreshIndexInterval() {
+			return 1000 * getDefault().getPreferenceStore()
+					.getInt(UIPreferences.REFESH_INDEX_INTERVAL);
 		}
 	}
 
 	private void setupRepoChangeScanner() {
 		rcs = new RepositoryChangeScanner();
-		rcs.setSystem(true);
-		rcs.schedule(RepositoryChangeScanner.REPO_SCAN_INTERVAL);
+		getPreferenceStore().addPropertyChangeListener(rcs);
 	}
 
 	@Override
