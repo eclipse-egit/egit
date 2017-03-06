155642091e63459b1e8c23da540cf4941ddd5e92
David Pursehouse <david.pursehouse@gmail.com>
2017-03-06 16:33:04 +0900

    WiP: RefSpecPage: Add options for submodule recursion on fetch
    Change-Id: Id35e9fd8fc4840ebdec8efa0f43153cb1f6fa503
    Signed-off-by: David Pursehouse <david.pursehouse@gmail.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RefSpecPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/RefSpecPage.java
@@ -26,7 +26,9 @@ import org.eclipse.egit.ui.internal.credentials.EGitCredentialsProvider;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.jgit.lib.ConfigConstants;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.SubmoduleConfig.FetchRecurseSubmodulesMode;
 import org.eclipse.jgit.transport.RefSpec;
 import org.eclipse.jgit.transport.TagOpt;
 import org.eclipse.jgit.transport.URIish;
@@ -65,6 +67,12 @@ public class RefSpecPage extends WizardPage {
 	private Button tagsFetchTagsButton;
 
 	private Button tagsNoTagsButton;
+
+	private Button recurseSubmodulesYesButton;
+
+	private Button recurseSubmodulesNoButton;
+
+	private Button recurseSubmodulesOnDemandButton;
 
 	private String transportError;
 
@@ -145,6 +153,22 @@ public class RefSpecPage extends WizardPage {
 					.setText(UIText.RefSpecPage_annotatedTagsFetchTags);
 			tagsNoTagsButton = new Button(tagsGroup, SWT.RADIO);
 			tagsNoTagsButton.setText(UIText.RefSpecPage_annotatedTagsNoTags);
+
+			final Group recurseGroup = new Group(panel, SWT.NULL);
+			recurseGroup.setLayoutData(
+					new GridData(SWT.FILL, SWT.FILL, true, false));
+			recurseGroup.setText(UIText.RefSpecPage_recurseSubmodulesGroup);
+			recurseGroup.setLayout(new GridLayout());
+			recurseSubmodulesYesButton = new Button(recurseGroup, SWT.RADIO);
+			recurseSubmodulesYesButton
+					.setText(UIText.RefSpecPage_recurseSubmodulesYes);
+			recurseSubmodulesNoButton = new Button(recurseGroup, SWT.RADIO);
+			recurseSubmodulesNoButton
+					.setText(UIText.RefSpecPage_recurseSubmodulesNo);
+			recurseSubmodulesOnDemandButton = new Button(recurseGroup,
+					SWT.RADIO);
+			recurseSubmodulesOnDemandButton
+					.setText(UIText.RefSpecPage_recurseSubmodulesOnDemand);
 		}
 
 		saveButton = new Button(panel, SWT.CHECK);
@@ -184,6 +208,21 @@ public class RefSpecPage extends WizardPage {
 		if (tagsFetchTagsButton.getSelection())
 			return TagOpt.FETCH_TAGS;
 		return TagOpt.NO_TAGS;
+	}
+
+	/**
+	 * @return selected submodule recurse mode. This result is relevant only for
+	 *         fetch page.
+	 */
+	public FetchRecurseSubmodulesMode getFetchRecurseSubmodulesMode() {
+		if (recurseSubmodulesYesButton.getSelection()) {
+			return FetchRecurseSubmodulesMode.YES;
+		}
+		if (recurseSubmodulesNoButton.getSelection()) {
+			return FetchRecurseSubmodulesMode.NO;
+		}
+
+		return FetchRecurseSubmodulesMode.ON_DEMAND;
 	}
 
 	/**
@@ -299,9 +338,31 @@ public class RefSpecPage extends WizardPage {
 					tagsNoTagsButton.setSelection(true);
 					break;
 				}
+
+				recurseSubmodulesYesButton.setSelection(false);
+				recurseSubmodulesNoButton.setSelection(false);
+				recurseSubmodulesOnDemandButton.setSelection(false);
+
+				final FetchRecurseSubmodulesMode recurse = local.getConfig()
+						.getEnum(ConfigConstants.CONFIG_FETCH_SECTION, null,
+								ConfigConstants.CONFIG_KEY_RECURSE_SUBMODULES,
+								FetchRecurseSubmodulesMode.ON_DEMAND);
+				switch (recurse) {
+				case YES:
+					recurseSubmodulesYesButton.setSelection(true);
+					break;
+				case NO:
+					recurseSubmodulesNoButton.setSelection(true);
+					break;
+				case ON_DEMAND:
+					recurseSubmodulesOnDemandButton.setSelection(true);
+					break;
+				}
 			}
-		} else if (!pushPage)
+		} else if (!pushPage) {
 			tagsAutoFollowButton.setSelection(true);
+			recurseSubmodulesOnDemandButton.setSelection(true);
+		}
 
 		checkPage();
 	}
