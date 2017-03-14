029ac157325637aa2e353f4359973d385c558d72
Matthias Sohn <matthias.sohn@sap.com>
2017-03-14 21:18:30 +0100

    Fix NPE in FetchGerritChangePage
    Bug: 513673
    Change-Id: I526a0e45d7901c700a344e1bd1426a4412647b4c
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/fetch/FetchGerritChangePage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/fetch/FetchGerritChangePage.java
@@ -611,7 +611,7 @@ public class FetchGerritChangePage extends WizardPage {
 		}
 	}
 
-	private Ref getLocalRef(String ref) {
+	private @Nullable Ref getLocalRef(String ref) {
 		Change change = Change.fromRef(ref);
 		if (change != null) {
 			try {
@@ -757,7 +757,9 @@ public class FetchGerritChangePage extends WizardPage {
 
 		if (doChangeBranch) {
 			Ref localRef = getLocalRef(spec.getSource());
-			checkout(localRef.getName(), monitor);
+			if (localRef != null) {
+				checkout(localRef.getName(), monitor);
+			}
 		} else {
 			try {
 				RevCommit commit = fetchChange(uri, spec, monitor);
