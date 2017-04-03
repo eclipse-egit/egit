80fc3f3e46c804e9904ec75bce81a2a0bdddb741
David Weiser <david.weiser@vogella.com>
2017-04-03 20:54:14 +0200

    Bug 514320 - Use verbs in PullResultDialog instead of OK
    Change-Id: I5b01b2b442922fee53b48e550846c66996ae8dfe
    Signed-off-by: David Weiser <david.weiser@vogella.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -2111,6 +2111,9 @@ public class UIText extends NLS {
 	public static String PullResultDialog_NothingToFetchFromLocal;
 
 	/** */
+	public static String PullResultDialog_closeButtonLabel;
+
+	/** */
 	public static String PushAction_wrongURIDescription;
 
 	/** */
