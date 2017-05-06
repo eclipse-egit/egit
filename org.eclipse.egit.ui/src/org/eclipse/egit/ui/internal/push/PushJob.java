5b3412af1900ed64e775fdaf2b6415b6584cbdb3
Thomas Wolf <thomas.wolf@paranor.ch>
2017-05-06 14:27:00 +0200

    Test change.
    Change-Id: I50599d124d6bd88718dd7a1ae1a3e744c46213f5

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/push/PushJob.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/push/PushJob.java
@@ -5,6 +5,8 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Foo.
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.push;
 
