76c21d90161bffb5fabc896efc13a1c9b8fc066e
Stefan Rademacher <stefan.rademacher@tk.de>
2017-05-10 10:11:10 +0200

    Support multiple ICommitMessageProvider implementations
    In case that multiple implementations of ICommitMessageProvider
    are available in Eclipse, the commit messages of all providers
    are concatenated and provided in the CommitMessageComponent.
    
    Bug: 376388
    Change-Id: I38f37116956b65fe9433ef77d1728caa4e69c6af
    Signed-off-by: Peter Bäckman <peter.backman@tieto.com>
    Signed-off-by: Stefan Rademacher <stefan.rademacher@tk.de>
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitMessageComponent.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/CommitMessageComponent.java
@@ -21,10 +21,12 @@ package org.eclipse.egit.ui.internal.dialogs;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.eclipse.core.resources.IFile;
@@ -33,6 +35,7 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.InvalidRegistryObjectException;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.egit.core.RevUtils;
 import org.eclipse.egit.core.internal.gerrit.GerritUtil;
@@ -664,7 +667,7 @@ public class CommitMessageComponent {
 	 * @param paths
 	 * @return the calculated commit message
 	 */
-	private String calculateCommitMessage(Collection<String> paths) {
+	String calculateCommitMessage(Collection<String> paths) {
 		if (commitMessage != null) {
 			// special case for merge
 			return commitMessage;
@@ -672,7 +675,7 @@ public class CommitMessageComponent {
 
 		if (amending)
 			return previousCommitMessage;
-		String calculatedCommitMessage = null;
+		StringBuilder calculatedCommitMessage = new StringBuilder();
 
 		Set<IResource> resources = new HashSet<>();
 		for (String path : paths) {
@@ -684,41 +687,58 @@ public class CommitMessageComponent {
 			resources
 					.addAll(Arrays.asList(ProjectUtil.getProjects(repository)));
 		}
-		try {
-			ICommitMessageProvider messageProvider = getCommitMessageProvider();
-			if (messageProvider != null) {
-				IResource[] resourcesArray = resources
-						.toArray(new IResource[0]);
-				calculatedCommitMessage = messageProvider
-						.getMessage(resourcesArray);
+		List<ICommitMessageProvider> messageProviders = getCommitMessageProviders();
+		IResource[] resourcesArray = resources.toArray(new IResource[0]);
+		String providedMessageSeparator = "\n\n"; //$NON-NLS-1$
+
+		for (ICommitMessageProvider messageProvider : messageProviders) {
+			String message = null;
+			try {
+				message = messageProvider.getMessage(resourcesArray);
+			} catch (RuntimeException e) {
+				Activator.logError(e.getMessage(), e);
 			}
-		} catch (CoreException coreException) {
-			Activator.logError(coreException.getLocalizedMessage(),
-					coreException);
+
+			if (message != null && !message.trim().isEmpty()) {
+				if (calculatedCommitMessage.length() > 0) {
+					calculatedCommitMessage.append(providedMessageSeparator);
+				}
+				calculatedCommitMessage.append((message.trim()));
+			}
 		}
-		if (calculatedCommitMessage != null)
-			return calculatedCommitMessage;
-		else
-			return EMPTY_STRING;
+
+		return calculatedCommitMessage.toString();
 	}
 
-	private ICommitMessageProvider getCommitMessageProvider()
-			throws CoreException {
+	List<ICommitMessageProvider> getCommitMessageProviders() {
+		List<ICommitMessageProvider> providers = new ArrayList<>();
+
 		IExtensionRegistry registry = Platform.getExtensionRegistry();
-		IConfigurationElement[] config = registry
+		IConfigurationElement[] configs = registry
 				.getConfigurationElementsFor(COMMIT_MESSAGE_PROVIDER_ID);
-		if (config.length > 0) {
+		for (IConfigurationElement config : configs) {
 			Object provider;
-			provider = config[0].createExecutableExtension("class");//$NON-NLS-1$
-			if (provider instanceof ICommitMessageProvider) {
-				return (ICommitMessageProvider) provider;
-			} else {
+			String contributorName = "<unknown>"; //$NON-NLS-1$
+			try {
+				contributorName = config.getDeclaringExtension()
+						.getContributor().getName();
+				provider = config.createExecutableExtension("class");//$NON-NLS-1$
+				if (provider instanceof ICommitMessageProvider) {
+					providers.add((ICommitMessageProvider) provider);
+				} else {
+					Activator.logError(
+							UIText.CommitDialog_WrongTypeOfCommitMessageProvider,
+							null);
+				}
+			} catch (CoreException | InvalidRegistryObjectException e) {
 				Activator.logError(
-						UIText.CommitDialog_WrongTypeOfCommitMessageProvider,
-						null);
+						MessageFormat.format(
+								UIText.CommitDialog_ErrorCreatingCommitMessageProvider,
+								contributorName),
+						e);
 			}
 		}
-		return null;
+		return providers;
 	}
 
 	private void saveOriginalChangeId() {
