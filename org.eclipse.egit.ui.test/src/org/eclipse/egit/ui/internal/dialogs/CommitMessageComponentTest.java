b9d55a1e30fcc2d9d931d30c559e723a67e3a20c
Stefan Rademacher <stefan.rademacher@tk.de>
2017-05-10 10:11:10 +0200

    Support multiple ICommitMessageProvider implementations
    In case, that multiple implementations of ICommitMessageProvider
    are available in Eclipse, the commit messages of all providers
    are concatenated and provided in the CommitMessageComponent.
    
    Bug: 376388
    Change-Id: I38f37116956b65fe9433ef77d1728caa4e69c6af
    Signed-off-by: Peter Bäckman <peter.backman@tieto.com>
    Signed-off-by: Stefan Rademacher <stefan.rademacher@tk.de>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/internal/dialogs/CommitMessageComponentTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/internal/dialogs/CommitMessageComponentTest.java
@@ -10,6 +10,13 @@ package org.eclipse.egit.ui.internal.dialogs;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.egit.ui.ICommitMessageProvider;
 import org.eclipse.egit.ui.internal.UIText;
 import org.junit.Test;
 
@@ -60,6 +67,75 @@ public class CommitMessageComponentTest {
 				.formatIssuesInCommitMessage(commitMessage);
 		assertEquals(UIText.CommitMessageComponent_MessageSecondLineNotEmpty,
 				formattedMessage);
+	}
+
+	@Test
+	public void commitMessageProvider_noProvider() throws Exception {
+		CommitMessageComponent commitMessageComponent = newCommitMessageComponent(
+				createProviderList());
+
+		String calculatedCommitMessage = commitMessageComponent
+				.calculateCommitMessage(Collections.emptyList());
+
+		assertEquals("", calculatedCommitMessage);
+	}
+
+	@Test
+	public void commitMessageProvider_oneProvider() throws Exception {
+		String message = "example single-line commit message";
+
+		CommitMessageComponent commitMessageComponent = newCommitMessageComponent(
+				createProviderList(message));
+
+		String calculatedCommitMessage = commitMessageComponent
+				.calculateCommitMessage(Collections.emptyList());
+
+		assertEquals(message, calculatedCommitMessage);
+	}
+
+	@Test
+	public void commitMessageProvider_twoProviders() throws Exception {
+		String message1 = "example single-line commit message";
+		String message2 = "example multi-line\n\ncommit message";
+
+		CommitMessageComponent commitMessageComponent = newCommitMessageComponent(
+				createProviderList(message1, message2));
+
+		String calculatedCommitMessage = commitMessageComponent
+				.calculateCommitMessage(Collections.emptyList());
+
+		assertEquals(message1 + "\n\n" + message2, calculatedCommitMessage);
+	}
+
+	private CommitMessageComponent newCommitMessageComponent(
+			List<ICommitMessageProvider> providers) {
+		// Create anonymous subclass, as mocking does not currently work.
+		// See https://bugs.eclipse.org/bugs/show_bug.cgi?id=349164
+		return new CommitMessageComponent(null) {
+
+			@Override
+			List<ICommitMessageProvider> getCommitMessageProviders()
+					throws CoreException {
+				return providers;
+			}
+		};
+	}
+
+	private List<ICommitMessageProvider> createProviderList(
+			String... messages) {
+		List<ICommitMessageProvider> providerList = new ArrayList<>();
+
+		for (String message : messages) {
+			providerList.add(new ICommitMessageProvider() {
+
+				@Override
+				public String getMessage(IResource[] resources) {
+					return message;
+				}
+			});
+		}
+
+		return providerList;
 	}
 
 }
