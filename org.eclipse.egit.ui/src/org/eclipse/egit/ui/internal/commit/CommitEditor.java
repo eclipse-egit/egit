39629adceecf811f4bd8fca979cea166ad4af4c2
Thomas Wolf <thomas.wolf@paranor.ch>
2017-05-15 20:40:37 +0200

    Remove defaultHandlers for CommitEditor commands
    Provide dedicated handlers with an appropriate activation expression
    instead. The commands work for one or several RepositoryCommits. The
    expression must not refer to the active part since these commands can
    be invoked from either the CommitEditor or the GitHistoryPage.
    
    In the CommitEditor, use intermediary actions for the toolbar
    contributions to avoid enablement problems (see bug 516679).
    
    Bug: 495064
    Change-Id: I0297a4ef6cf37b1662181570fad9a1fdecaed33d
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commit/CommitEditor.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commit/CommitEditor.java
@@ -62,6 +62,7 @@ import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.IPartListener;
 import org.eclipse.ui.IPartService;
+import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
@@ -75,8 +76,6 @@ import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.ImageHyperlink;
 import org.eclipse.ui.forms.widgets.ScrolledForm;
 import org.eclipse.ui.ide.IDE;
-import org.eclipse.ui.menus.CommandContributionItem;
-import org.eclipse.ui.menus.CommandContributionItemParameter;
 import org.eclipse.ui.menus.IMenuService;
 import org.eclipse.ui.part.IShowInSource;
 import org.eclipse.ui.part.IShowInTargetList;
@@ -257,14 +256,6 @@ public class CommitEditor extends SharedHeaderFormEditor implements
 				.addRefsChangedListener(this);
 	}
 
-	private CommandContributionItem createCommandContributionItem(
-			String commandId) {
-		CommandContributionItemParameter parameter = new CommandContributionItemParameter(
-				getSite(), commandId, commandId,
-				CommandContributionItem.STYLE_PUSH);
-		return new CommandContributionItem(parameter);
-	}
-
 	private IContributionItem createActionContributionItem(String commandId,
 			String title, ImageDescriptor icon) {
 		IAction action = new Action(title, icon) {
@@ -354,16 +345,28 @@ public class CommitEditor extends SharedHeaderFormEditor implements
 		CommonUtils.getService(getSite(), IPartService.class)
 				.addPartListener(activationListener);
 		if (commit.isStash()) {
-			toolbar.add(createCommandContributionItem(StashApplyHandler.ID));
-			toolbar.add(createCommandContributionItem(StashDropHandler.ID));
+			toolbar.add(createActionContributionItem(StashApplyHandler.ID,
+					UIText.CommitEditor_toolbarApplyStash,
+					UIIcons.STASH_APPLY));
+			toolbar.add(createActionContributionItem(StashDropHandler.ID,
+					UIText.CommitEditor_toolbarDeleteStash,
+					PlatformUI.getWorkbench().getSharedImages()
+							.getImageDescriptor(
+									ISharedImages.IMG_TOOL_DELETE)));
 		} else {
-			toolbar.add(createCommandContributionItem(CreateTagHandler.ID));
-			toolbar.add(createCommandContributionItem(CreateBranchHandler.ID));
+			toolbar.add(createActionContributionItem(CreateTagHandler.ID,
+					UIText.CommitEditor_toolbarCreateTag, UIIcons.TAG));
+			toolbar.add(createActionContributionItem(CreateBranchHandler.ID,
+					UIText.CommitEditor_toolbarCreateBranch, UIIcons.BRANCH));
 			toolbar.add(createActionContributionItem(CheckoutHandler.ID,
 					UIText.CommitEditor_toolbarCheckOut, UIIcons.CHECKOUT));
-			toolbar.add(createCommandContributionItem(CherryPickHandler.ID));
-			toolbar.add(createCommandContributionItem(RevertHandler.ID));
-			toolbar.add(createCommandContributionItem(ShowInHistoryHandler.ID));
+			toolbar.add(createActionContributionItem(CherryPickHandler.ID,
+					UIText.CommitEditor_toolbarCherryPick,
+					UIIcons.CHERRY_PICK));
+			toolbar.add(createActionContributionItem(RevertHandler.ID,
+					UIText.CommitEditor_toolbarRevert, UIIcons.REVERT));
+			toolbar.add(createActionContributionItem(ShowInHistoryHandler.ID,
+					UIText.CommitEditor_toolbarShowInHistory, UIIcons.HISTORY));
 		}
 		addContributions(toolbar);
 		toolbar.update(true);
