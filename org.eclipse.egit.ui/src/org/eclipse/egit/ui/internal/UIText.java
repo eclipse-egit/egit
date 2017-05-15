6c1191dbf2da75f92b0d75d7bdb7124956729144
Thomas Wolf <thomas.wolf@paranor.ch>
2017-05-15 20:40:37 +0200

    Remove defaultHandlers for CommitEditor commands
    Provide dedicated handlers with an appropriate activation expression
    instead. The commands work for one or several RepositoryCommits. The
    expression must not refer to the active part since these commands can
    be invoked from either the CommitEditor or the GitHistoryPage.
    
    In the CommitEditor, use intermediary actions for the toolbar
    contributions to avoid enablement problems (see bug 516679).
    
    Bug: 495064
    Change-Id: I0297a4ef6cf37b1662181570fad9a1fdecaed33d
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -4278,7 +4278,28 @@ public class UIText extends NLS {
 	public static String CommitEditor_showGitRepo;
 
 	/** */
+	public static String CommitEditor_toolbarApplyStash;
+
+	/** */
+	public static String CommitEditor_toolbarDeleteStash;
+
+	/** */
+	public static String CommitEditor_toolbarCreateTag;
+
+	/** */
+	public static String CommitEditor_toolbarCreateBranch;
+
+	/** */
 	public static String CommitEditor_toolbarCheckOut;
+
+	/** */
+	public static String CommitEditor_toolbarCherryPick;
+
+	/** */
+	public static String CommitEditor_toolbarRevert;
+
+	/** */
+	public static String CommitEditor_toolbarShowInHistory;
 
 	/** */
 	public static String CommitEditor_TitleHeaderCommit;
