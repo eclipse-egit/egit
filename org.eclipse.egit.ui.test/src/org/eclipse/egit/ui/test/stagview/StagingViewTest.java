52a5d297975a407a46fd7b639c67599629052f7e
Stefan Rademacher <stefan.rademacher@tk.de>
2017-05-22 08:52:05 +0200

    Add ICommitMessageProvider2 for caret positioning in commit messages
    The caret in the CommitMessageComponent is placed according to the
    value of getDesiredCaretPosition() of the first implementation of
    ICommitMessageProvider2 that provides caret position. In case there
    are multiple implementations available that provide a defined caret
    position the second and subsequent positions are ignored.
    
    Commit messages are not trimmed anymore to ensure consistency between
    a provided message and the provided caret position.
    
    Includes unit and UI tests.
    
    Bug: 516867
    Change-Id: I1b0ffd33ef37196d53077d09774672c2b033835f
    Signed-off-by: Stefan Rademacher <stefan.rademacher@tk.de>
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/stagview/StagingViewTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/stagview/StagingViewTest.java
@@ -14,54 +14,16 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.io.File;
-
-import org.eclipse.egit.core.JobFamilies;
-import org.eclipse.egit.ui.Activator;
-import org.eclipse.egit.ui.common.LocalRepositoryTestCase;
 import org.eclipse.egit.ui.common.StagingViewTester;
-import org.eclipse.egit.ui.internal.repository.RepositoriesView;
-import org.eclipse.egit.ui.internal.staging.StagingView;
 import org.eclipse.egit.ui.test.CommitMessageUtil;
 import org.eclipse.egit.ui.test.TestUtil;
-import org.eclipse.egit.ui.view.repositories.GitRepositoriesViewTestUtils;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.RepositoryState;
 import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-public class StagingViewTest extends LocalRepositoryTestCase {
-
-	private static final GitRepositoriesViewTestUtils repoViewUtil = new GitRepositoriesViewTestUtils();
-
-	private File repositoryFile;
-
-	private Repository repository;
-
-	@Before
-	public void before() throws Exception {
-		repositoryFile = createProjectAndCommitToRepository();
-		repository = lookupRepository(repositoryFile);
-		TestUtil.configureTestCommitterAsUser(repository);
-		Activator.getDefault().getRepositoryUtil()
-				.addConfiguredRepository(repositoryFile);
-
-		selectRepositoryNode();
-	}
-
-	@After
-	public void after() {
-		TestUtil.hideView(RepositoriesView.VIEW_ID);
-		TestUtil.hideView(StagingView.VIEW_ID);
-		Activator.getDefault().getRepositoryUtil().removeDir(repositoryFile);
-	}
+public class StagingViewTest extends AbstractStagingViewTestCase {
 
 	@Test
 	public void testCommitSingleFile() throws Exception {
@@ -153,20 +115,5 @@ public class StagingViewTest extends LocalRepositoryTestCase {
 		assertTrue(commitMessage.indexOf("Change-Id") > 0);
 		assertTrue(commitMessage.indexOf("Signed-off-by") > 0);
 		stagingViewTester.commit();
-	}
-
-	private void setContent(String content) throws Exception {
-		setTestFileContent(content);
-		TestUtil.joinJobs(JobFamilies.INDEX_DIFF_CACHE_UPDATE);
-	}
-
-	private void selectRepositoryNode() throws Exception {
-		SWTBotView repositoriesView = TestUtil
-				.showView(RepositoriesView.VIEW_ID);
-		SWTBotTree tree = repositoriesView.bot().tree();
-
-		SWTBotTreeItem repoNode = repoViewUtil
-				.getRootItem(tree, repositoryFile);
-		repoNode.select();
 	}
 }
