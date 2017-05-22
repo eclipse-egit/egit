8a27ed69ce0571dc41e68d2896be1d31f502aa11
Stefan Rademacher <stefan.rademacher@tk.de>
2017-05-22 08:52:05 +0200

    Add ICommitMessageProvider2 for caret positioning in commit messages
    The caret in the CommitMessageComponent is placed according to the
    value of getDesiredCaretPosition() of the first implementation of
    ICommitMessageProvider2 that provides caret position. In case there
    are multiple implementations available that provide a defined caret
    position the second and subsequent positions are ignored.
    
    Commit messages are not trimmed anymore to ensure consistency between
    a provided message and the provided caret position.
    
    Includes unit and UI tests.
    
    Bug: 516867
    Change-Id: I1b0ffd33ef37196d53077d09774672c2b033835f
    Signed-off-by: Stefan Rademacher <stefan.rademacher@tk.de>
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/CommitMessageWithCaretPosition.java
@@ -0,0 +1,69 @@
+package org.eclipse.egit.ui;
+
+import java.util.Objects;
+
+/**
+ * This class represents a commit message with a caret position.
+ */
+public class CommitMessageWithCaretPosition {
+
+	/**
+	 * This constant defines the value for an undefined caret position
+	 */
+	public static final int NO_POSITION = -1;
+
+	private final String message;
+
+	private final int caretPosition;
+
+	/**
+	 * Constructor for creating an immutable value object, that represents a
+	 * commit message and a caret position within that message.
+	 *
+	 * @param message
+	 *            the commit message
+	 * @param caretPosition
+	 *            the caret position within the commit message
+	 */
+	public CommitMessageWithCaretPosition(String message,
+			int caretPosition) {
+		this.message = message;
+		this.caretPosition = caretPosition;
+	}
+
+	/**
+	 * @return the commit message
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @return the desired caret position within the commit message
+	 */
+	public int getDesiredCaretPosition() {
+		return caretPosition;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(message, Integer.valueOf(caretPosition));
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		CommitMessageWithCaretPosition other = (CommitMessageWithCaretPosition) obj;
+		return caretPosition == other.caretPosition
+				&& Objects.equals(message, other.message);
+	}
+
+}
