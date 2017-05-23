bf2d77c236a92cdd8840aa5c9e30cc61d563a9f7
Thomas Wolf <thomas.wolf@paranor.ch>
2017-05-23 11:28:34 +0200

    Contextual actions in tooltips in staging view
    If there are selected elements in the staged/unstaged viewers,
    provide quick access to the "Add to index"/"Remove from index"
    actions via a toolbar shown in a tooltip when the mouse pointer
    hovers over a selected element.
    
    Also add those actions to the toolbars of the viewers, properly
    enabled/disabled depending on whether there is a selection in
    the viewer.
    
    Change the unstage icon: "stage" is a green plus sign, but "unstage"
    was an inverted "reset" icon. A red minus sign seems to be more
    appropriate and less confusing.
    
    Finally, add tooltips to the section headers of the staged and
    unstaged viewers with explanations that should make it easier
    for people new to git to get what this whole view is about.
    (The notion of being able to compose a commit in the git way is
    a novelty for many people new to git.)
    
    Bug: 516969
    Change-Id: I53f7d460aee079db25b78afd5a818bc7075547ce
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingView.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingView.java
@@ -181,6 +181,7 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.VerifyEvent;
 import org.eclipse.swt.graphics.Cursor;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.layout.RowLayout;
@@ -294,21 +295,25 @@ public class StagingView extends ViewPart
 
 	private ToolBarManager stagedToolBarManager;
 
-	private Action listPresentationAction;
+	private IAction listPresentationAction;
 
-	private Action treePresentationAction;
+	private IAction treePresentationAction;
 
-	private Action compactTreePresentationAction;
+	private IAction compactTreePresentationAction;
 
-	private Action unstagedExpandAllAction;
+	private IAction unstagedExpandAllAction;
 
-	private Action unstagedCollapseAllAction;
+	private IAction unstagedCollapseAllAction;
 
-	private Action stagedExpandAllAction;
+	private IAction stagedExpandAllAction;
 
-	private Action stagedCollapseAllAction;
+	private IAction stagedCollapseAllAction;
 
-	private Action compareModeAction;
+	private IAction unstageAction;
+
+	private IAction stageAction;
+
+	private IAction compareModeAction;
 
 	@Nullable
 	private Repository currentRepository;
@@ -799,6 +804,26 @@ public class StagingView extends ViewPart
 		GridDataFactory.fillDefaults().grab(true, true)
 				.applyTo(stagingSashForm);
 
+		unstageAction = new Action(UIText.StagingView_UnstageItemMenuLabel,
+				UIIcons.UNSTAGE) {
+			@Override
+			public void run() {
+				unstage((IStructuredSelection) stagedViewer.getSelection());
+			}
+		};
+		unstageAction.setToolTipText(UIText.StagingView_UnstageItemTooltip);
+		stageAction = new Action(UIText.StagingView_StageItemMenuLabel,
+				UIIcons.ELCL16_ADD) {
+			@Override
+			public void run() {
+				stage((IStructuredSelection) unstagedViewer.getSelection());
+			}
+		};
+		stageAction.setToolTipText(UIText.StagingView_StageItemTooltip);
+
+		unstageAction.setEnabled(false);
+		stageAction.setEnabled(false);
+
 		unstagedSection = toolkit.createSection(stagingSashForm,
 				ExpandableComposite.TITLE_BAR);
 
@@ -814,7 +839,14 @@ public class StagingView extends ViewPart
 				.applyTo(unstagedComposite);
 
 		unstagedViewer = createViewer(unstagedComposite, true,
-				selection -> unstage(selection));
+				selection -> unstage(selection), stageAction);
+		unstagedViewer.addSelectionChangedListener(event -> {
+			boolean hasSelection = !event.getSelection().isEmpty();
+			if (hasSelection != stageAction.isEnabled()) {
+				stageAction.setEnabled(hasSelection);
+				unstagedToolBarManager.update(true);
+			}
+		});
 		Composite rebaseAndCommitComposite = toolkit.createComposite(mainSashForm);
 		rebaseAndCommitComposite.setLayout(GridLayoutFactory.fillDefaults().create());
 
@@ -1079,10 +1111,17 @@ public class StagingView extends ViewPart
 				.applyTo(stagedComposite);
 
 		stagedViewer = createViewer(stagedComposite, false,
-				selection -> stage(selection));
+				selection -> stage(selection), unstageAction);
 		stagedViewer.getLabelProvider().addListener(event -> {
 			updateMessage();
 			updateCommitButtons();
+		});
+		stagedViewer.addSelectionChangedListener(event -> {
+			boolean hasSelection = !event.getSelection().isEmpty();
+			if (hasSelection != unstageAction.isEnabled()) {
+				unstageAction.setEnabled(hasSelection);
+				stagedToolBarManager.update(true);
+			}
 		});
 
 		selectionChangedListener = new ISelectionListener() {
@@ -1115,6 +1154,9 @@ public class StagingView extends ViewPart
 				.addPreferenceChangeListener(prefListener);
 
 		updateSectionText();
+		stagedSection.setToolTipText(UIText.StagingView_StagedChangesTooltip);
+		unstagedSection
+				.setToolTipText(UIText.StagingView_UnstagedChangesTooltip);
 		updateToolbar();
 		enableCommitWidgets(false);
 		refreshAction.setEnabled(false);
@@ -1467,6 +1509,7 @@ public class StagingView extends ViewPart
 
 		unstagedToolBarManager = new ToolBarManager(SWT.FLAT | SWT.HORIZONTAL);
 
+		unstagedToolBarManager.add(stageAction);
 		unstagedToolBarManager.add(sortAction);
 		unstagedToolBarManager.add(unstagedExpandAllAction);
 		unstagedToolBarManager.add(unstagedCollapseAllAction);
@@ -1505,6 +1548,7 @@ public class StagingView extends ViewPart
 
 		stagedToolBarManager = new ToolBarManager(SWT.FLAT | SWT.HORIZONTAL);
 
+		stagedToolBarManager.add(unstageAction);
 		stagedToolBarManager.add(stagedExpandAllAction);
 		stagedToolBarManager.add(stagedCollapseAllAction);
 		stagedToolBarManager.update(true);
@@ -1956,7 +2000,8 @@ public class StagingView extends ViewPart
 	}
 
 	private TreeViewer createViewer(Composite parent, boolean unstaged,
-			final Consumer<IStructuredSelection> dropAction) {
+			final Consumer<IStructuredSelection> dropAction,
+			IAction... tooltipActions) {
 		final TreeViewer viewer = createTree(parent);
 		GridDataFactory.fillDefaults().grab(true, true)
 				.applyTo(viewer.getControl());
@@ -1966,6 +2011,11 @@ public class StagingView extends ViewPart
 		StagingViewContentProvider contentProvider = createStagingContentProvider(
 				unstaged);
 		viewer.setContentProvider(contentProvider);
+		if (tooltipActions != null && tooltipActions.length > 0) {
+			StagingViewTooltips tooltips = new StagingViewTooltips(viewer,
+					tooltipActions);
+			tooltips.setShift(new Point(1, 1));
+		}
 		viewer.addDragSupport(DND.DROP_MOVE | DND.DROP_COPY | DND.DROP_LINK,
 				new Transfer[] { LocalSelectionTransfer.getTransfer(),
 						FileTransfer.getInstance() },
@@ -2627,14 +2677,18 @@ public class StagingView extends ViewPart
 						.contains(StagingEntry.Action.REPLACE_WITH_OURS_THEIRS_MENU);
 
 				if (addStage)
-					menuMgr.add(new Action(UIText.StagingView_StageItemMenuLabel) {
+					menuMgr.add(
+							new Action(UIText.StagingView_StageItemMenuLabel,
+									UIIcons.ELCL16_ADD) {
 						@Override
 						public void run() {
 							stage(selection);
 						}
 					});
 				if (addUnstage)
-					menuMgr.add(new Action(UIText.StagingView_UnstageItemMenuLabel) {
+					menuMgr.add(
+							new Action(UIText.StagingView_UnstageItemMenuLabel,
+									UIIcons.UNSTAGE) {
 						@Override
 						public void run() {
 							unstage(selection);
@@ -2889,7 +2943,8 @@ public class StagingView extends ViewPart
 		private final IStructuredSelection selection;
 
 		DeleteAction(IStructuredSelection selection) {
-			super(UIText.StagingView_DeleteItemMenuLabel);
+			super(UIText.StagingView_DeleteItemMenuLabel,
+					UIIcons.ELCL16_DELETE);
 			this.selection = selection;
 		}
 
