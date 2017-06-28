c28e824542384063c2b609ca447a766b36804cf0
Thomas Wolf <thomas.wolf@paranor.ch>
2017-06-28 13:52:17 +0200

    Work-around: don't auto-refresh ignored top-level folders in projects
    The auto-refresh done by EGit on index changes until now refreshed
    everything in all projects in a certain repository. As discussed in
    bug 500106, this may be a very costly operation and may lock the
    workspace for extended periods. Unfortunately, there doesn't seem
    to be a reasonably simple way to get precise information about which
    files in the workspace were touched by the last git command as was
    suggested in bug 416199.
    
    As a work-around, change the project refresh logic to exclude
    git-ignored top-level folders in projects. That at least will
    resolve the problem for the occurrence reported in the bug
    (Node.js nodemodules folder).
    
    Unchanged in this commit but IMO also questionable: refreshing the
    projects runs with the workspace root as scheduling rule. This might
    not be necessary since the actual refresh acquires per-project
    scheduling rules anyway. Moreover, all projects are refreshed
    sequentially. I wonder if this could not be parallelized and be
    run without a workspace lock.
    
    Bug: 500106
    Change-Id: Ie301964fe2cbd9ec39f5d2146b84e04cfe8e0b82
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/Activator.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/Activator.java
@@ -15,12 +15,15 @@ package org.eclipse.egit.ui;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Dictionary;
+import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.core.resources.IProject;
@@ -35,6 +38,7 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.egit.core.IteratorService;
 import org.eclipse.egit.core.RepositoryCache;
 import org.eclipse.egit.core.RepositoryUtil;
 import org.eclipse.egit.core.project.RepositoryMapping;
@@ -59,6 +63,12 @@ import org.eclipse.jgit.events.ListenerHandle;
 import org.eclipse.jgit.events.RepositoryEvent;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.transport.CredentialsProvider;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.eclipse.jgit.treewalk.WorkingTreeIterator;
+import org.eclipse.jgit.treewalk.filter.AndTreeFilter;
+import org.eclipse.jgit.treewalk.filter.NotIgnoredFilter;
+import org.eclipse.jgit.treewalk.filter.PathFilterGroup;
+import org.eclipse.jgit.treewalk.filter.TreeFilter;
 import org.eclipse.osgi.service.debug.DebugOptions;
 import org.eclipse.osgi.service.debug.DebugOptionsListener;
 import org.eclipse.swt.graphics.Font;
@@ -526,11 +536,7 @@ public class Activator extends AbstractUIPlugin implements DebugOptionsListener 
 							ISchedulingRule rule = p.getWorkspace().getRuleFactory().refreshRule(p);
 							try {
 								getJobManager().beginRule(rule, subMonitor);
-								// handle missing projects after branch switch
-								if (p.isAccessible()) {
-									p.refreshLocal(IResource.DEPTH_INFINITE,
-											subMonitor.newChild(1));
-								}
+								safeRefresh(p, subMonitor.newChild(1));
 							} catch (CoreException e) {
 								handleError(UIText.Activator_refreshFailed, e, false);
 							} finally {
@@ -555,6 +561,117 @@ public class Activator extends AbstractUIPlugin implements DebugOptionsListener 
 			}
 			monitor.done();
 			return Status.OK_STATUS;
+		}
+
+		/**
+		 * Refreshes the given project, skipping any top-level folders that are
+		 * ignored by git.
+		 *
+		 * @param project
+		 *            to refresh
+		 * @param monitor
+		 *            for progress reporting and cancellation
+		 * @throws CoreException
+		 *             propagated from
+		 *             {@link IResource#refreshLocal(int, IProgressMonitor)
+		 *             IResource.refreshLocal()}
+		 */
+		private void safeRefresh(IProject project, IProgressMonitor monitor)
+				throws CoreException {
+			if (!project.isAccessible() || monitor.isCanceled()) {
+				return;
+			}
+			SubMonitor progress = SubMonitor.convert(monitor, 10);
+			project.refreshLocal(IResource.DEPTH_ONE, progress.newChild(1));
+			Map<String, IResource> toRefresh = new HashMap<>();
+			for (IResource r : project.members()) {
+				if (r.getType() == IResource.FOLDER) {
+					toRefresh.put(r.getName(), r);
+				}
+			}
+			if (toRefresh.isEmpty()) {
+				return;
+			}
+			RepositoryMapping mapping = RepositoryMapping.getMapping(project);
+			Repository repo = null;
+			if (mapping != null) {
+				repo = mapping.getRepository();
+			}
+			if (mapping == null || repo == null) {
+				// Should not occur?!
+				project.refreshLocal(IResource.DEPTH_INFINITE,
+						progress.newChild(9));
+			} else {
+				WorkingTreeIterator treeIterator = IteratorService
+						.createInitialIterator(repo);
+				if (treeIterator == null) {
+					project.refreshLocal(IResource.DEPTH_INFINITE,
+							progress.newChild(9));
+					return;
+				}
+				String repoRelativePath = mapping.getRepoRelativePath(project);
+				if (repoRelativePath == null) {
+					project.refreshLocal(IResource.DEPTH_INFINITE,
+							progress.newChild(9));
+					return;
+				}
+				progress.setWorkRemaining(toRefresh.size());
+				try (TreeWalk walk = new TreeWalk(repo)) {
+					walk.addTree(treeIterator);
+					TreeFilter filter = new NotIgnoredFilter(0);
+					if (!repoRelativePath.isEmpty()) {
+						filter = AndTreeFilter.create(PathFilterGroup
+								.createFromStrings(repoRelativePath),
+								filter);
+					}
+					walk.setFilter(filter);
+					// If the project is at the root of the git repo, we're
+					// already in the project. Otherwise, we'll first descend
+					// the parent directories.
+					boolean inProject = repoRelativePath.isEmpty();
+					while (walk.next()) {
+						if (progress.isCanceled()) {
+							return;
+						}
+						if (walk.getPathString().equals(repoRelativePath)) {
+							inProject = true;
+							if (walk.isSubtree()) {
+								walk.enterSubtree();
+							}
+							continue;
+						}
+						if (walk.isSubtree()) {
+							if (inProject) {
+								IResource r = toRefresh
+										.remove(walk.getNameString());
+								if (r != null) {
+									r.refreshLocal(IResource.DEPTH_INFINITE,
+											progress.newChild(1));
+								}
+								if (toRefresh.isEmpty()) {
+									return;
+								}
+							} else {
+								// Must be a parent directory of the project
+								walk.enterSubtree();
+							}
+						}
+					}
+				} catch (IOException e) {
+					if (!toRefresh.isEmpty()) {
+						logWarning(
+								MessageFormat.format(
+										UIText.Activator_refreshProblem, Integer
+												.toString(toRefresh.size())),
+								e);
+						progress.setWorkRemaining(toRefresh.size());
+						for (IResource r : toRefresh.values()) {
+							r.refreshLocal(IResource.DEPTH_INFINITE,
+									progress.newChild(1));
+						}
+					}
+				}
+			}
 		}
 
 		@Override
--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -71,6 +71,9 @@ public class UIText extends NLS {
 	public static String Activator_refreshFailed;
 
 	/** */
+	public static String Activator_refreshProblem;
+
+	/** */
 	public static String Activator_refreshingProjects;
 
 	/** */
