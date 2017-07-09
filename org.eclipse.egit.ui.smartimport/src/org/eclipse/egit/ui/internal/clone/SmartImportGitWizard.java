33795d57d9edd5a0e8892034b6b43885983b07ec
Michael Keppler <Michael.Keppler@gmx.de>
2017-07-09 19:59:55 +0200

    Set window title in smart import wizard
    This adds the window title to the smart import wizard. All other wizards
    have a window title already.
    
    Change-Id: I477771c8c8d999c899151488f1d136f9287c7f50
    Signed-off-by: Michael Keppler <Michael.Keppler@gmx.de>

--- org.eclipse.egit.ui.smartimport/src/org/eclipse/egit/ui/internal/clone/SmartImportGitWizard.java
+++ org.eclipse.egit.ui.smartimport/src/org/eclipse/egit/ui/internal/clone/SmartImportGitWizard.java
@@ -55,6 +55,7 @@ public class SmartImportGitWizard extends AbstractGitCloneWizard
 		}
 		setDefaultPageImageDescriptor(UIIcons.WIZBAN_IMPORT_REPO);
 		this.easymportWizard = new SmartImportWizard();
+		setWindowTitle(UIText.GitImportWizard_WizardTitle);
 	}
 
 	@Override
