018e58210dc79759f6767e084cc0abc85ef5deb7
Michael Keppler <Michael.Keppler@gmx.de>
2017-07-11 19:29:57 +0200

    Disable delete config section button for no selection
    In the configuration preference page it was possible to use the "Remove"
    button without actually selecting any section or sub section before.
    This change avoids showing the error message in lines 408 ff.
    
    Change-Id: Ia2c950d4df5affbde06c869efb732d264481d242
    Signed-off-by: Michael Keppler <Michael.Keppler@gmx.de>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/preferences/ConfigurationEditorComponent.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/preferences/ConfigurationEditorComponent.java
@@ -503,7 +503,7 @@ public class ConfigurationEditorComponent {
 	}
 
 	private void updateEnablement() {
-		remove.setEnabled(editable);
+		remove.setEnabled(editable && !tv.getStructuredSelection().isEmpty());
 		newValue.setEnabled(editable);
 	}
 
