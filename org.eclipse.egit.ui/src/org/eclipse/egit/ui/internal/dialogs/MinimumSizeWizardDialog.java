ac91078bdc0666b4d729f8e7eb987aa7bb10d68f
Thomas Wolf <thomas.wolf@paranor.ch>
2017-07-14 11:01:20 +0200

    Restrict resizing of the "Fetch from Gerrit" Dialog
    WizardDialogs in general can be resized to any size; even made
    ridiculously small.
    
    Set the shell's minimum size to the size needed to display the
    starting page. This allows making the dialog larger, but not smaller
    than the starting page.
    
    If this is deemed useful, we could use the new MinimumSizeWizardDialog
    as a drop-in replacement for all other WizardDialogs in EGit. It's
    also something that platform.ui might want to adopt.
    
    Bug: 519335
    Change-Id: I12d967e2e7054304824577cdd0d4a3bd31963d5f
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/MinimumSizeWizardDialog.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * Copyright (C) 2017, Thomas Wolf <thomas.wolf@paranor.ch>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.dialogs;
+
+import org.eclipse.jface.wizard.IWizard;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.events.ShellAdapter;
+import org.eclipse.swt.events.ShellEvent;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * A {@link WizardDialog} that can prevent making the dialog smaller than the
+ * size needed to show the starting page.
+ */
+public class MinimumSizeWizardDialog extends WizardDialog {
+
+	private final boolean restrictResize;
+
+	/**
+	 * Creates a new {@link MinimumSizeWizardDialog} that prevents making the
+	 * dialog smaller than the size needed to show the starting page.
+	 *
+	 * @param parentShell
+	 *            for the dialog
+	 * @param newWizard
+	 *            to show in the dialog
+	 */
+	public MinimumSizeWizardDialog(Shell parentShell, IWizard newWizard) {
+		this(parentShell, newWizard, true);
+	}
+
+	/**
+	 * Creates a new {@link MinimumSizeWizardDialog}.
+	 *
+	 * @param parentShell
+	 *            for the dialog
+	 * @param newWizard
+	 *            to show in the dialog
+	 * @param restrictResize
+	 *            {@code true} if the dialog should prevent being resized
+	 *            smaller than necessary to show the starting page;
+	 *            {@code false} if resizing to any size should be allowed
+	 */
+	public MinimumSizeWizardDialog(Shell parentShell, IWizard newWizard,
+			boolean restrictResize) {
+		super(parentShell, newWizard);
+		this.restrictResize = restrictResize;
+	}
+
+	@Override
+	protected void configureShell(Shell newShell) {
+		super.configureShell(newShell);
+		if (restrictResize) {
+			newShell.addShellListener(new ShellAdapter() {
+
+				@Override
+				public void shellActivated(ShellEvent e) {
+					// Prevent making the dialog smaller than the starting page
+					newShell.removeShellListener(this); // Only the first time
+					newShell.setMinimumSize(newShell.getSize());
+				}
+			});
+		}
+	}
+}
