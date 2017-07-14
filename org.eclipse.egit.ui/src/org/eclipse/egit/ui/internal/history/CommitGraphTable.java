a0e6c966acaa71713e0557a7a172c75b2197c9f6
Thomas Wolf <thomas.wolf@paranor.ch>
2017-07-14 16:21:19 +0200

    Move history page actions into plugin.xml and remove default handlers
    The history view's main context menu was built programmatically,
    apparently because of bug 322751, which undid an earlier attempt
    to do this (bug 321516).
    
    Move the context menu back to plugin.xml. Remove the menu items from
    CommitGraphTable and add a new section in the menu ("history.group")
    so that we can add our built-in commands in our own group, not just
    under "additions".
    
    Basically all the history view commands had defaultHandlers. To
    remove them, we need quite a lot of new dedicated handlers.
    
    Verified that the error described in bug 322751 did not re-appear.
    
    There should be only minor visual changes in the context menu of the
    history view. We only show applicable operations, and I have slightly
    re-arranged a few separators.
    
    There is quite some duplication between handlers. With the new
    IRepositoryCommit interface, it should be possible to unify many
    commands, but this is postponed to future commits.
    
    Bug: 495064
    Change-Id: I73951763904f61ec1b116dc1781cbcc6b0fdb65f
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitGraphTable.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitGraphTable.java
@@ -24,14 +24,12 @@ import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map;
 
 import org.eclipse.core.commands.Command;
 import org.eclipse.core.commands.IParameter;
 import org.eclipse.core.commands.Parameterization;
 import org.eclipse.core.commands.ParameterizedCommand;
 import org.eclipse.core.commands.common.NotDefinedException;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.egit.core.op.CreatePatchOperation;
 import org.eclipse.egit.core.op.CreatePatchOperation.DiffHeaderFormat;
@@ -42,9 +40,8 @@ import org.eclipse.egit.ui.internal.ActionUtils;
 import org.eclipse.egit.ui.internal.CommonUtils;
 import org.eclipse.egit.ui.internal.UIIcons;
 import org.eclipse.egit.ui.internal.UIText;
-import org.eclipse.egit.ui.internal.actions.ResetMenu;
 import org.eclipse.egit.ui.internal.history.SWTCommitList.SWTLane;
-import org.eclipse.egit.ui.internal.history.command.HistoryViewCommands;
+import org.eclipse.egit.ui.internal.history.command.ShowVersionsHandler;
 import org.eclipse.egit.ui.internal.trace.GitTraceLocation;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.action.MenuManager;
@@ -55,7 +52,6 @@ import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.jface.viewers.IOpenListener;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
-import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProviderChangedEvent;
@@ -64,9 +60,6 @@ import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.TableLayout;
 import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revplot.PlotCommit;
 import org.eclipse.jgit.revwalk.RevCommit;
@@ -86,8 +79,6 @@ import org.eclipse.swt.dnd.TextTransfer;
 import org.eclipse.swt.dnd.Transfer;
 import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
-import org.eclipse.swt.events.MenuDetectEvent;
-import org.eclipse.swt.events.MenuDetectListener;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.graphics.GC;
@@ -104,8 +95,6 @@ import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.actions.ActionFactory;
 import org.eclipse.ui.commands.ICommandService;
 import org.eclipse.ui.handlers.IHandlerService;
-import org.eclipse.ui.menus.CommandContributionItem;
-import org.eclipse.ui.menus.CommandContributionItemParameter;
 import org.eclipse.ui.part.IPageSite;
 
 class CommitGraphTable {
@@ -150,8 +139,6 @@ class CommitGraphTable {
 	private HistoryPageInput input;
 
 	IAction copy;
-
-	MenuListener menuListener;
 
 	private RevCommit commitToShow;
 
@@ -289,13 +276,14 @@ class CommitGraphTable {
 
 				ICommandService srv = CommonUtils.getService(site, ICommandService.class);
 				IHandlerService hsrv = CommonUtils.getService(site, IHandlerService.class);
-				Command cmd = srv.getCommand(HistoryViewCommands.SHOWVERSIONS);
+				Command cmd = srv.getCommand(ShowVersionsHandler.COMMAND_ID);
 				Parameterization[] parms;
 				if (Activator.getDefault().getPreferenceStore().getBoolean(
 						UIPreferences.RESOURCEHISTORY_COMPARE_MODE))
 					try {
 						IParameter parm = cmd
-								.getParameter(HistoryViewCommands.COMPARE_MODE_PARAM);
+								.getParameter(
+										ShowVersionsHandler.COMPARE_MODE_PARAM);
 						parms = new Parameterization[] { new Parameterization(
 								parm, Boolean.TRUE.toString()) };
 					} catch (NotDefinedException e) {
@@ -314,11 +302,13 @@ class CommitGraphTable {
 			}
 		});
 
-		Control c = getControl();
+		menuMgr.add(new Separator(IWorkbenchActionConstants.HISTORY_GROUP));
 		menuMgr.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
+		menuMgr.add(new Separator());
+		menuMgr.add(copy);
+		Control c = getControl();
+		menuMgr.addMenuListener(manager -> c.setFocus());
 		c.setMenu(menuMgr.createContextMenu(c));
-		c.addMenuDetectListener(menuListener = new MenuListener(menuMgr,
-				getTableView(), site, copy));
 	}
 
 	Control getControl() {
@@ -408,8 +398,6 @@ class CommitGraphTable {
 
 	void setHistoryPageInput(HistoryPageInput input) {
 		this.input = input;
-		if (menuListener != null)
-			menuListener.setInput(input);
 	}
 
 	private void initCommitsMap() {
@@ -631,239 +619,4 @@ class CommitGraphTable {
 		}
 	}
 
-	private final static class MenuListener implements MenuDetectListener {
-
-		private final MenuManager popupMgr;
-
-		private final ISelectionProvider selectionProvider;
-
-		private final IPageSite site;
-
-		private final IAction copyAction;
-
-		private HistoryPageInput input;
-
-		MenuListener(MenuManager menuManager,
-				ISelectionProvider selectionProvider, IPageSite site,
-				IAction copyAction) {
-			this.popupMgr = menuManager;
-			this.selectionProvider = selectionProvider;
-			this.site = site;
-			this.copyAction = copyAction;
-		}
-
-		public void setInput(HistoryPageInput input) {
-			this.input = input;
-		}
-
-		@Override
-		public void menuDetected(MenuDetectEvent e) {
-			popupMgr.removeAll();
-
-			final HistoryPageInput lastInput = this.input;
-			if (lastInput == null)
-				return;
-
-			int selectionSize = ((IStructuredSelection) selectionProvider
-					.getSelection()).size();
-
-			if (lastInput.isSingleFile()) {
-				if (selectionSize == 1)
-					if (lastInput.getSingleFile() instanceof IResource)
-						popupMgr
-								.add(getCommandContributionItem(
-										HistoryViewCommands.COMPARE_WITH_TREE,
-										UIText.GitHistoryPage_CompareWithWorkingTreeMenuMenuLabel));
-					else
-						popupMgr
-								.add(getCommandContributionItem(
-										HistoryViewCommands.COMPARE_WITH_TREE,
-										UIText.GitHistoryPage_CompareWithCurrentHeadMenu));
-				if (selectionSize > 0) {
-					popupMgr.add(getCommandContributionItem(
-							HistoryViewCommands.OPEN,
-							UIText.GitHistoryPage_OpenMenuLabel));
-					popupMgr.add(getCommandContributionItem(
-							HistoryViewCommands.OPEN_IN_TEXT_EDITOR,
-							UIText.GitHistoryPage_OpenInTextEditorLabel));
-				}
-				if (selectionSize == 1)
-					popupMgr.add(getCommandContributionItem(
-							HistoryViewCommands.SHOW_BLAME,
-							UIText.CommitFileDiffViewer_ShowAnnotationsMenuLabel));
-			}
-
-			if (selectionSize > 0) {
-				popupMgr.add(getCommandContributionItem(
-						HistoryViewCommands.OPEN_IN_COMMIT_VIEWER,
-						UIText.CommitGraphTable_OpenCommitLabel));
-			}
-
-			if (selectionSize == 1) {
-				popupMgr.add(new Separator());
-				if (!lastInput.getRepository().isBare()) {
-					if (hasMultipleRefNodes(lastInput)) {
-						popupMgr.add(getCommandContributionItem(
-								HistoryViewCommands.CHECKOUT,
-								UIText.GitHistoryPage_CheckoutMenuLabel2));
-					} else {
-						popupMgr.add(getCommandContributionItem(
-								HistoryViewCommands.CHECKOUT,
-								UIText.GitHistoryPage_CheckoutMenuLabel));
-					}
-				}
-
-				popupMgr.add(getCommandContributionItem(
-						HistoryViewCommands.PUSH_COMMIT,
-						UIText.GitHistoryPage_pushCommit));
-				popupMgr.add(new Separator());
-				popupMgr.add(getCommandContributionItem(
-						HistoryViewCommands.CREATE_BRANCH,
-						UIText.GitHistoryPage_CreateBranchMenuLabel));
-				popupMgr.add(getCommandContributionItem(
-						HistoryViewCommands.DELETE_BRANCH,
-						UIText.CommitGraphTable_DeleteBranchAction));
-				popupMgr.add(getCommandContributionItem(
-						HistoryViewCommands.RENAME_BRANCH,
-						UIText.CommitGraphTable_RenameBranchMenuLabel));
-				popupMgr.add(getCommandContributionItem(
-						HistoryViewCommands.CREATE_TAG,
-						UIText.GitHistoryPage_CreateTagMenuLabel));
-				popupMgr.add(getCommandContributionItem(
-						HistoryViewCommands.DELETE_TAG,
-						UIText.CommitGraphTable_DeleteTagAction));
-				popupMgr.add(new Separator());
-				popupMgr.add(getCommandContributionItem(
-						HistoryViewCommands.CREATE_PATCH,
-						UIText.GitHistoryPage_CreatePatchMenuLabel));
-				popupMgr.add(getCommandContributionItem(
-						HistoryViewCommands.CHERRYPICK,
-						UIText.GitHistoryPage_cherryPickMenuItem));
-				popupMgr.add(getCommandContributionItem(
-						HistoryViewCommands.MERGE,
-						UIText.GitHistoryPage_mergeMenuItem));
-				popupMgr.add(getCommandContributionItem(
-						HistoryViewCommands.REBASECURRENT,
-						UIText.GitHistoryPage_rebaseMenuItem));
-				popupMgr.add(getCommandContributionItem(
-						HistoryViewCommands.REBASE_INTERACTIVE_CURRENT,
-						UIText.GitHistoryPage_rebaseInteractiveMenuItem));
-				popupMgr.add(new Separator());
-
-				MenuManager resetManager = ResetMenu.createMenu(site);
-				popupMgr.add(resetManager);
-			} else if (selectionSize == 2) {
-				popupMgr.add(getCommandContributionItem(
-						HistoryViewCommands.COMPARE_VERSIONS,
-						UIText.GitHistoryPage_CompareWithEachOtherMenuLabel));
-				if (!lastInput.isSingleFile())
-					popupMgr
-							.add(getCommandContributionItem(
-									HistoryViewCommands.COMPARE_VERSIONS_IN_TREE,
-									UIText.CommitGraphTable_CompareWithEachOtherInTreeMenuLabel));
-			}
-
-			popupMgr.add(new Separator());
-
-			popupMgr.add(getCommandContributionItem(HistoryViewCommands.REVERT,
-					UIText.GitHistoryPage_revertMenuItem));
-
-			popupMgr.add(new Separator());
-
-			MenuManager quickDiffManager = new MenuManager(
-					UIText.GitHistoryPage_QuickdiffMenuLabel, null, "Quickdiff"); //$NON-NLS-1$
-
-			popupMgr.add(quickDiffManager);
-
-			quickDiffManager.add(getCommandContributionItem(
-					HistoryViewCommands.SET_QUICKDIFF_BASELINE,
-					UIText.GitHistoryPage_SetAsBaselineMenuLabel));
-
-			Map<String, String> parameters = new HashMap<>();
-			parameters.put(HistoryViewCommands.BASELINE_TARGET, "HEAD"); //$NON-NLS-1$
-			quickDiffManager.add(getCommandContributionItem(
-					HistoryViewCommands.RESET_QUICKDIFF_BASELINE,
-					UIText.GitHistoryPage_ResetBaselineToHeadMenuLabel,
-					parameters));
-
-			parameters = new HashMap<>();
-			parameters.put(HistoryViewCommands.BASELINE_TARGET, "HEAD^1"); //$NON-NLS-1$
-			quickDiffManager.add(getCommandContributionItem(
-					HistoryViewCommands.RESET_QUICKDIFF_BASELINE,
-					UIText.GitHistoryPage_ResetBaselineToParentOfHeadMenuLabel,
-					parameters));
-
-			popupMgr.add(new Separator());
-
-			MenuManager modifyManager = new MenuManager(
-					UIText.GitHistoryPage_ModifyMenuLabel, null, "Modify"); //$NON-NLS-1$
-
-			popupMgr.add(modifyManager);
-
-			if (selectionSize == 1) {
-				modifyManager.add(getCommandContributionItem(
-						HistoryViewCommands.REWORD,
-						UIText.GitHistoryPage_rewordMenuItem));
-				modifyManager.add(getCommandContributionItem(
-						HistoryViewCommands.EDIT,
-						UIText.GitHistoryPage_editMenuItem));
-			}
-
-			if (selectionSize >= 2)
-				modifyManager.add(getCommandContributionItem(
-						HistoryViewCommands.SQUASH,
-						UIText.GitHistoryPage_squashMenuItem));
-
-			// copy and such after additions
-			popupMgr.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
-			popupMgr.add(copyAction);
-			popupMgr.add(new Separator());
-		}
-
-		private boolean hasMultipleRefNodes(HistoryPageInput lastInput) {
-			try {
-				Map<String, Ref> branches = lastInput.getRepository()
-						.getRefDatabase().getRefs(Constants.R_HEADS);
-				int count = 0;
-				IStructuredSelection selection = (IStructuredSelection) selectionProvider
-						.getSelection();
-				if (selection.isEmpty()) {
-					return false;
-				}
-				ObjectId selectedId = ((RevCommit) selection.getFirstElement())
-						.getId();
-				for (Ref branch : branches.values()) {
-					ObjectId objectId = branch.getLeaf().getObjectId();
-					if (objectId != null && objectId.equals(selectedId)) {
-						count++;
-					}
-				}
-				return (count > 1);
-
-			} catch (IOException e) {
-				// ignore here
-			}
-			return false;
-		}
-
-		private CommandContributionItem getCommandContributionItem(
-				String commandId, String menuLabel) {
-			CommandContributionItemParameter parameter = new CommandContributionItemParameter(
-					site, commandId, commandId,
-					CommandContributionItem.STYLE_PUSH);
-			parameter.label = menuLabel;
-			return new CommandContributionItem(parameter);
-		}
-
-		private CommandContributionItem getCommandContributionItem(
-				String commandId, String menuLabel,
-				Map<String, String> parameters) {
-			CommandContributionItemParameter parameter = new CommandContributionItemParameter(
-					site, commandId, commandId,
-					CommandContributionItem.STYLE_PUSH);
-			parameter.label = menuLabel;
-			parameter.parameters = parameters;
-			return new CommandContributionItem(parameter);
-		}
-	}
 }
