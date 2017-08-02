4c2032921e77206adc445a2aa97d4a04c46b3bf0
Thomas Wolf <thomas.wolf@paranor.ch>
2017-08-02 17:11:39 +0200

    Extensible context menu in the rebase interactive view
    A double click on a plan item already opened the commit in the commit
    viewer. Now add the corresponding action to the view's context menu
    so that this functionality is easier to discover.
    
    This also is a nice example of how the new IRepositoryCommit interface
    simplifies command management: we just define a new menu contribution
    for that context menu, *re-using* the existing handler from the
    history view. The new contribution uses its own label since the
    accelerator is changed.
    
    Adapt the visibleWhen expressions of the rebase sub-menu items for
    the "Team" sub-menu to avoid they appear in the rebase interactive
    view.
    
    Moving the existing handler from the "internal.history" package to a
    more general place is for now left for a future clean-up commit, as
    are more handler unifications and eventually unifying all these
    "AbstractXYZHandler" classes that all offer operations to get a
    repository and commits from a selection, but that all do it in
    slightly different and sometimes even view-specific ways.
    
    Change-Id: I9ae5e709e24bfe3cc938f575df9685d3cfc884bc
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/rebase/RebaseInteractiveView.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/rebase/RebaseInteractiveView.java
@@ -50,10 +50,9 @@ import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
 import org.eclipse.egit.ui.internal.selection.RepositorySelectionProvider;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.action.IMenuListener;
-import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.bindings.keys.SWTKeySupport;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
@@ -99,6 +98,7 @@ import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.ToolBar;
@@ -112,6 +112,7 @@ import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.ISelectionService;
 import org.eclipse.ui.IViewSite;
+import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchPartSite;
 import org.eclipse.ui.PartInitException;
@@ -406,7 +407,6 @@ public class RebaseInteractiveView extends ViewPart implements
 
 		getSite().setSelectionProvider(new RepositorySelectionProvider(
 				planTreeViewer, () -> currentRepository));
-
 	}
 
 	private void createCommandToolBar(Form theForm, FormToolkit toolkit) {
@@ -1018,25 +1018,25 @@ public class RebaseInteractiveView extends ViewPart implements
 		createContextMenuItems(planViewer);
 
 		MenuManager manager = new MenuManager();
-		manager.addMenuListener(new IMenuListener() {
-			@Override
-			public void menuAboutToShow(IMenuManager menuManager) {
-				boolean selectionNotEmpty = !planViewer.getSelection()
-						.isEmpty();
-				boolean rebaseNotStarted = !currentPlan
-						.hasRebaseBeenStartedYet();
-				boolean menuEnabled = selectionNotEmpty && rebaseNotStarted;
-				for (PlanContextMenuAction item : contextMenuItems)
-					item.setEnabled(menuEnabled);
+		Control c = planViewer.getControl();
+		manager.setRemoveAllWhenShown(true);
+		manager.addMenuListener(m -> {
+			c.setFocus();
+			boolean selectionNotEmpty = !planViewer.getSelection().isEmpty();
+			boolean rebaseNotStarted = currentPlan != null
+					&& !currentPlan.hasRebaseBeenStartedYet();
+			boolean menuEnabled = selectionNotEmpty && rebaseNotStarted;
+			if (menuEnabled) {
+				for (PlanContextMenuAction item : contextMenuItems) {
+					m.add(item);
+				}
 			}
+			m.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
 		});
-
-		for (PlanContextMenuAction item : contextMenuItems)
-			manager.add(item);
-
-		Menu menu = manager.createContextMenu(planViewer.getControl());
-		planViewer.getControl().setMenu(menu);
-		planViewer.getControl().addKeyListener(new ContextMenuKeyListener());
+		Menu menu = manager.createContextMenu(c);
+		c.setMenu(menu);
+		c.addKeyListener(new ContextMenuKeyListener());
+		getSite().registerContextMenu(manager, planTreeViewer);
 	}
 
 	private void createContextMenuItems(final TreeViewer planViewer) {
