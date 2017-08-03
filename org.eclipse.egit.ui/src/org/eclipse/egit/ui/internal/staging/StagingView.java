57bae75526c40353d4e2ccc108c3b6bbe6d4d3e5
Karsten Thoms <karsten.thoms@itemis.de>
2017-08-03 17:04:37 +0200

    Added context actions 'Assume Unchanged' and 'Untrack'
    Modified unstaged changes can be marked as 'Assume Unchanged' or
    untracked using a context menu action.
    
    AssumeUnchangedOperation and UntrackOperation are extended by a
    constructor that takes a path list. Reason is that it cannot be assumed
    that in the StagingView the StagingEntry's are workspace resources. In
    this case the selection is a collection of paths of the current selected
    repository.
    
    Bug: 381158
    
    Change-Id: I4eec5d307ae7b1a04ad288b3c3e769218a98a0b5
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingView.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/StagingView.java
@@ -22,6 +22,7 @@ import static org.eclipse.egit.ui.internal.CommonUtils.runCommand;
 import java.io.File;
 import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
@@ -61,8 +62,11 @@ import org.eclipse.egit.core.internal.gerrit.GerritUtil;
 import org.eclipse.egit.core.internal.indexdiff.IndexDiffCacheEntry;
 import org.eclipse.egit.core.internal.indexdiff.IndexDiffChangedListener;
 import org.eclipse.egit.core.internal.indexdiff.IndexDiffData;
+import org.eclipse.egit.core.internal.job.JobUtil;
 import org.eclipse.egit.core.internal.job.RuleUtil;
+import org.eclipse.egit.core.op.AssumeUnchangedOperation;
 import org.eclipse.egit.core.op.CommitOperation;
+import org.eclipse.egit.core.op.UntrackOperation;
 import org.eclipse.egit.core.project.RepositoryMapping;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.JobFamilies;
@@ -101,6 +105,7 @@ import org.eclipse.egit.ui.internal.push.PushMode;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
 import org.eclipse.egit.ui.internal.selection.MultiViewerSelectionProvider;
 import org.eclipse.egit.ui.internal.selection.RepositorySelectionProvider;
+import org.eclipse.egit.ui.internal.selection.SelectionUtils;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.ControlContribution;
 import org.eclipse.jface.action.IAction;
@@ -2737,8 +2742,12 @@ public class StagingView extends ViewPart
 				boolean addLaunchMergeTool = availableActions.contains(StagingEntry.Action.LAUNCH_MERGE_TOOL);
 				boolean addReplaceWithOursTheirsMenu = availableActions
 						.contains(StagingEntry.Action.REPLACE_WITH_OURS_THEIRS_MENU);
+				boolean addAssumeUnchanged = availableActions
+						.contains(StagingEntry.Action.ASSUME_UNCHANGED);
+				boolean addUntrack = availableActions
+						.contains(StagingEntry.Action.UNTRACK);
 
-				if (addStage)
+				if (addStage) {
 					menuMgr.add(
 							new Action(UIText.StagingView_StageItemMenuLabel,
 									UIIcons.ELCL16_ADD) {
@@ -2747,7 +2756,8 @@ public class StagingView extends ViewPart
 							stage(selection);
 						}
 					});
-				if (addUnstage)
+				}
+				if (addUnstage) {
 					menuMgr.add(
 							new Action(UIText.StagingView_UnstageItemMenuLabel,
 									UIIcons.UNSTAGE) {
@@ -2756,39 +2766,46 @@ public class StagingView extends ViewPart
 							unstage(selection);
 						}
 					});
+				}
 				boolean selectionIncludesNonWorkspaceResources = selectionIncludesNonWorkspaceResources(fileSelection);
-				if (addReplaceWithFileInGitIndex)
-					if (selectionIncludesNonWorkspaceResources)
+				if (addReplaceWithFileInGitIndex) {
+					if (selectionIncludesNonWorkspaceResources) {
 						menuMgr.add(new ReplaceAction(
 								UIText.StagingView_replaceWithFileInGitIndex,
 								fileSelection, false));
-					else
+					} else {
 						menuMgr.add(createItem(
 								UIText.StagingView_replaceWithFileInGitIndex,
 								ActionCommands.DISCARD_CHANGES_ACTION,
 								fileSelection)); // replace with index
-				if (addReplaceWithHeadRevision)
-					if (selectionIncludesNonWorkspaceResources)
+					}
+				}
+				if (addReplaceWithHeadRevision) {
+					if (selectionIncludesNonWorkspaceResources) {
 						menuMgr.add(new ReplaceAction(
 								UIText.StagingView_replaceWithHeadRevision,
 								fileSelection, true));
-					else
+					} else {
 						menuMgr.add(createItem(
 								UIText.StagingView_replaceWithHeadRevision,
 								ActionCommands.REPLACE_WITH_HEAD_ACTION,
 								fileSelection));
+					}
+				}
 				if (addIgnore) {
 					if (!stagingFolderSet.isEmpty()) {
 						menuMgr.add(new IgnoreFoldersAction(stagingFolderSet));
 					}
 					menuMgr.add(new IgnoreAction(fileSelection));
 				}
-				if (addDelete)
+				if (addDelete) {
 					menuMgr.add(new DeleteAction(fileSelection));
-				if (addLaunchMergeTool)
+				}
+				if (addLaunchMergeTool) {
 					menuMgr.add(createItem(UIText.StagingView_MergeTool,
 							ActionCommands.MERGE_TOOL_ACTION,
 							fileSelection));
+				}
 				if (addReplaceWithOursTheirsMenu) {
 					MenuManager replaceWithMenu = new MenuManager(
 							UIText.StagingView_ReplaceWith);
@@ -2796,6 +2813,25 @@ public class StagingView extends ViewPart
 					oursTheirsMenu.initialize(getSite());
 					replaceWithMenu.add(oursTheirsMenu);
 					menuMgr.add(replaceWithMenu);
+				}
+				if (addAssumeUnchanged) {
+					menuMgr.add(
+							new Action(UIText.StagingView_Assume_Unchanged,
+									UIIcons.ASSUME_UNCHANGED) {
+								@Override
+								public void run() {
+									assumeUnchanged(selection);
+								}
+							});
+				}
+				if (addUntrack) {
+					menuMgr.add(new Action(UIText.StagingView_Untrack,
+							UIIcons.UNTRACK) {
+						@Override
+						public void run() {
+							untrack(selection);
+						}
+					});
 				}
 				menuMgr.add(new Separator());
 				menuMgr.add(createShowInMenu());
@@ -3389,6 +3425,29 @@ public class StagingView extends ViewPart
 		default:
 			// unstaged
 		}
+	}
+
+	private void assumeUnchanged(@NonNull IStructuredSelection selection) {
+		IPath[] locations = SelectionUtils.getSelectedLocations(selection);
+		if (locations.length == 0) {
+			return;
+		}
+
+		JobUtil.scheduleUserJob(
+				new AssumeUnchangedOperation(currentRepository, Arrays.asList(locations), true),
+				UIText.AssumeUnchanged_assumeUnchanged,
+				JobFamilies.ASSUME_NOASSUME_UNCHANGED);
+	}
+
+	private void untrack(@NonNull IStructuredSelection selection) {
+		IPath[] locations = SelectionUtils.getSelectedLocations(selection);
+		if (locations.length == 0) {
+			return;
+		}
+
+		JobUtil.scheduleUserJob(
+				new UntrackOperation(currentRepository, Arrays.asList(locations)),
+				UIText.Untrack_untrack, JobFamilies.UNTRACK);
 	}
 
 	private void resetPathsToExpand() {
