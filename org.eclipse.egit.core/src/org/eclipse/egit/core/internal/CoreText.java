d4fc41a833e05cb52803c18931cd0220f5a3257b
Thomas Wolf <thomas.wolf@paranor.ch>
2017-08-22 09:07:25 +0200

    Log warnings about invalid RefSpecs in git config files
    Handle invalid RefSpecs by catching the IllegalArgumentException that
    JGit throws. Log the problem and otherwise ignore invalid RefSpecs.
    
    Bug: 517314
    Change-Id: Iea65dc0aeef31be9c9640f05d1cc413bcce23975
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/CoreText.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/CoreText.java
@@ -491,7 +491,13 @@ public class CoreText extends NLS {
 	public static String ReportingTypedConfigGetter_invalidConfig;
 
 	/** */
+	public static String ReportingTypedConfigGetter_invalidConfigIgnored;
+
+	/** */
 	public static String ReportingTypedConfigGetter_invalidConfigWithLocation;
+
+	/** */
+	public static String ReportingTypedConfigGetter_invalidConfigWithLocationIgnored;
 
 	static {
 		initializeMessages(BUNDLE_NAME,	CoreText.class);
