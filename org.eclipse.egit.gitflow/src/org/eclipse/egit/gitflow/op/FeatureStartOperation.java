0635e64c67773c8974e49f6f8f33f38b6b6cf7f8
Max Hohenegger <eclipse@hohenegger.eu>
2018-04-04 08:30:43 +0200

    Fixed: EGit allows starting features with diverging develop branches
    In comparison, when the upstream develop branch is ahead of the local
    local develop branch on command line Gitflow (AVH):
    $ git flow feature start "foo"
    Branches 'develop' and 'origin/develop' have diverged.
    Fatal: And branch 'develop' may be fast-forwarded.
    
    Adjusted JUnit tests to cover new code.
    
    Change-Id: I2629301539e96b3911987ad300a7ed538b1d0516

--- org.eclipse.egit.gitflow/src/org/eclipse/egit/gitflow/op/FeatureStartOperation.java
+++ org.eclipse.egit.gitflow/src/org/eclipse/egit/gitflow/op/FeatureStartOperation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2015, Max Hohenegger <eclipse@hohenegger.eu>
+ * Copyright (C) 2018, Max Hohenegger <eclipse@hohenegger.eu>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -10,12 +10,17 @@
  *******************************************************************************/
 package org.eclipse.egit.gitflow.op;
 
+import java.io.IOException;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
+import org.eclipse.egit.gitflow.Activator;
 import org.eclipse.egit.gitflow.GitFlowConfig;
 import org.eclipse.egit.gitflow.GitFlowRepository;
 import org.eclipse.egit.gitflow.internal.CoreText;
+import org.eclipse.jgit.lib.BranchTrackingStatus;
+import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.osgi.util.NLS;
 
@@ -35,12 +40,55 @@ public final class FeatureStartOperation extends AbstractFeatureOperation {
 	@Override
 	public void execute(IProgressMonitor monitor) throws CoreException {
 		GitFlowConfig config = repository.getConfig();
+		handleDivergingDevelop(config);
+
 		String branchName = config.getFeatureBranchName(featureName);
 		RevCommit head = repository.findHead(config.getDevelop());
 		if (head == null) {
 			throw new IllegalStateException(NLS.bind(CoreText.StartOperation_unableToFindCommitFor, config.getDevelop()));
 		}
 		start(monitor, branchName, head);
+	}
+
+	private void handleDivergingDevelop(GitFlowConfig config)
+			throws CoreException {
+		try {
+			BranchTrackingStatus developStatus = BranchTrackingStatus
+					.of(repository.getRepository(), config.getDevelop());
+			if (developStatus == null) {
+				return;
+			}
+
+			if (developStatus.getBehindCount() > 0) {
+				String message = NLS.bind(
+						CoreText.FeatureStartOperation_divergingDevelop,
+						config.getDevelop(), getOriginDevelopName(developStatus));
+				if (developStatus.getAheadCount() == 0) {
+					message += "\n" + NLS.bind( //$NON-NLS-1$
+							CoreText.FeatureStartOperation_andBranchMayBeFastForwarded,
+							config.getDevelop());
+				}
+
+				throw new CoreException(Activator.error(message));
+			} else if (developStatus.getAheadCount() > 0) {
+				String message = NLS.bind(
+						CoreText.FeatureStartOperation_divergingDevelop,
+						config.getDevelop(),
+						getOriginDevelopName(developStatus));
+				message += "\n" + NLS.bind( //$NON-NLS-1$
+						CoreText.FeatureStartOperation_andLocalDevelopIsAheadOfOrigin,
+						config.getDevelop(),
+						getOriginDevelopName(developStatus));
+				Activator.logInfo(message);
+			}
+		} catch (IOException e) {
+			throw new CoreException(Activator.error(e));
+		}
+	}
+
+	private String getOriginDevelopName(BranchTrackingStatus developStatus) {
+		return developStatus.getRemoteTrackingBranch()
+				.substring(Constants.R_REMOTES.length());
 	}
 
 	@Override
