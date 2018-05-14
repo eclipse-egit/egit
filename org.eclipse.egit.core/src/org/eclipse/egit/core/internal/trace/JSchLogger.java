2a6168830888138266c0e199622abd5ddf2cd236
Thomas Wolf <thomas.wolf@paranor.ch>
2018-05-14 23:32:01 +0200

    Add tracing of low-level JSch log messages
    Currently there is no way in Eclipse to get hold of the log messages
    JSch produces. But those might be useful to analyze connection
    problems. So add debug logging via the OSGi debug trace mechanism;
    a user can switch this on by enabling tracing in the preferences
    (Preferences->General->Tracing) and then setting the flags for
    org.eclipse.egit.core/debug and org.eclipse.egit.core/debug/core/jsch
    to true.
    
    Change-Id: I4c9befbca0330671050fae60efafd72d9925825e
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

---
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/trace/JSchLogger.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Thomas Wolf <thomas.wolf@paranor.ch>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.egit.core.internal.trace;
+
+import com.jcraft.jsch.Logger;
+
+/**
+ * Simple low-level logger for JSch using an OSGi debug trace; intended for
+ * analyzing Jsch connection problems.
+ */
+public class JSchLogger implements Logger {
+
+	@Override
+	public boolean isEnabled(int level) {
+		return GitTraceLocation.JSCH.isActive();
+	}
+
+	@Override
+	public void log(int level, String message) {
+		if (isEnabled(level)) {
+			GitTraceLocation.getTrace().trace(
+					GitTraceLocation.JSCH.getLocation(),
+					levelToString(level) + ": " + message); //$NON-NLS-1$
+		}
+	}
+
+	private String levelToString(int level) {
+		switch (level) {
+		case 0:
+			return "DEBUG"; //$NON-NLS-1$
+		case 1:
+			return "INFO"; //$NON-NLS-1$
+		case 2:
+			return "WARN"; //$NON-NLS-1$
+		case 3:
+			return "ERROR"; //$NON-NLS-1$
+		case 4:
+			return "FATAL"; //$NON-NLS-1$
+		default:
+			return "UNKNOWN(" + level + ')'; //$NON-NLS-1$
+		}
+	}
+}
