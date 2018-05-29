a27e759149c957af27c0ac9ee5f2968a0be8cfff
Matthias Sohn <matthias.sohn@sap.com>
2018-05-29 11:19:53 +0200

    Update license to EPL-2.0
    Bug: 530393
    Change-Id: Iea3b247253a97c28043187df0e35daecf518011e
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/internal/actions/LinkedResourcesTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/internal/actions/LinkedResourcesTest.java
@@ -2,9 +2,11 @@
  * Copyright (C) 2013, François Rey <eclipse.org_@_francois_._rey_._name>
  *
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *    François Rey - First implementation as part of handling linked resources
