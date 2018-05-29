a3c751b4f9c49b8da76cee6d2fb0d8c2f0771c2e
Matthias Sohn <matthias.sohn@sap.com>
2018-05-29 11:19:53 +0200

    Update license to EPL-2.0
    Bug: 530393
    Change-Id: Iea3b247253a97c28043187df0e35daecf518011e
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/PushConfiguredRemoteCommand.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/PushConfiguredRemoteCommand.java
@@ -3,9 +3,11 @@
  * Copyright (c) 2011, Matthias Sohn <matthias.sohn@sap.com>
  * Copyright (c) 2011, Dariusz Luksza <dariusz@luksa.org>
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *    Mathias Kinzler (SAP AG) - initial implementation
