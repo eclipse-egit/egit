096d9b869f5876379b6353241327a45f2230a195
Luís Copetti <lhcopetti@gmail.com>
2018-09-19 23:46:12 -0300

    Remove throws CoreException from ProjectUtil::getProjectsContaining
    Change-Id: Iafdeff291cfc46d596fed261030ae90c0f5e395d
    Signed-off-by: Luís Copetti <lhcopetti@gmail.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/factories/GitAdapterFactory.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/factories/GitAdapterFactory.java
@@ -154,22 +154,18 @@ public class GitAdapterFactory implements IAdapterFactory {
 		if (repository == null || repository.isBare()) {
 			return null;
 		}
-		try {
-			IPath repoRelativePath = gitPath.makeRelativeTo(
-					new Path(repository.getWorkTree().getAbsolutePath()));
-			IProject[] projects = ProjectUtil.getProjectsContaining(repository,
-					Collections.singleton(repoRelativePath.toString()));
-			if (projects.length > 0) {
-				IPath projectRelativePath = gitPath
-						.makeRelativeTo(projects[0].getLocation());
-				if (projectRelativePath.isEmpty()) {
-					return projects[0];
-				} else {
-					return projects[0].getFile(projectRelativePath);
-				}
+		IPath repoRelativePath = gitPath.makeRelativeTo(
+				new Path(repository.getWorkTree().getAbsolutePath()));
+		IProject[] projects = ProjectUtil.getProjectsContaining(repository,
+				Collections.singleton(repoRelativePath.toString()));
+		if (projects.length > 0) {
+			IPath projectRelativePath = gitPath
+					.makeRelativeTo(projects[0].getLocation());
+			if (projectRelativePath.isEmpty()) {
+				return projects[0];
+			} else {
+				return projects[0].getFile(projectRelativePath);
 			}
-		} catch (CoreException e) {
-			// Ignore and fall through
 		}
 		return root.getFile(gitPath);
 	}
