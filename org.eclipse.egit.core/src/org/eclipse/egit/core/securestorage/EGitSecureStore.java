332f511e8b4a5fade20dcf5ea6ca2cd231be3f5b
Thomas Wolf <thomas.wolf@paranor.ch>
2018-10-20 16:47:52 +0200

    Include the Apache MINA ssh client
    Add a preference with UI in the main Git preference page to select
    between JSch and the new Apache MINA sshd client.
    
    Read preferences from the org.eclipse.jsch.core preference node and
    install a listener to pick up preference changes.
    
    The session factory thus can use the ssh preferences as configured by
    the user. For the time being, we use the ssh directory, the default
    default identities, and the preferred authentication mechanisms. The
    latter two can be overridden through the ssh config file, which will
    be the one in the configured ssh directory. The other preferences from
    org.eclipse.jsch are not taken into account; sshd may have different
    algorithms available and I don't want to change preferences in a
    foreign bundle.
    
    Make the proxy service accessible in the EGit core activator, and use
    it in a ProxyDatabase in the EGitSshdSessionFactory.
    
    The EGitSshdSessionFactory has no key cache. Eclipse is a long-running
    application, and keeping ssh keys in memory for that long is most
    probably not wise. Instead re-load keys whenever they are needed, and
    use an IdentityPasswordProvider (which provides passwords for encrypted
    private key files) that use the Eclipse secure storage for key
    passphrases to avoid repeatedly asking the user for the same passphrases
    for the same keys.
    
    Bug: 520927
    JGit-Dependency: I1a5c34687d439b3ef8373c5d58e24004f93e63ae
    Change-Id: Id3cf850c4e132e864eab7eda52c20ff379e2b1d9
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/securestorage/EGitSecureStore.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/securestorage/EGitSecureStore.java
@@ -88,17 +88,20 @@ public class EGitSecureStore {
 	}
 
 	static String calcNodePath(URIish uri) {
-		URIish storedURI = uri.setUser(null).setPass(null).setPath(null);
-		if (uri.getPort() == -1) {
-			String s = uri.getScheme();
-			if ("http".equals(s)) //$NON-NLS-1$
-				storedURI = storedURI.setPort(80);
-			else if ("https".equals(s)) //$NON-NLS-1$
-				storedURI = storedURI.setPort(443);
-			else if ("ssh".equals(s) || "sftp".equals(s)) //$NON-NLS-1$ //$NON-NLS-2$
-				storedURI = storedURI.setPort(22);
-			else if ("ftp".equals(s)) //$NON-NLS-1$
-				storedURI = storedURI.setPort(21);
+		URIish storedURI = uri.setUser(null).setPass(null);
+		if (uri.getScheme() != null && !"file".equals(uri.getScheme())) { //$NON-NLS-1$
+			storedURI = storedURI.setPath(null);
+			if (uri.getPort() == -1) {
+				String s = uri.getScheme();
+				if ("http".equals(s)) //$NON-NLS-1$
+					storedURI = storedURI.setPort(80);
+				else if ("https".equals(s)) //$NON-NLS-1$
+					storedURI = storedURI.setPort(443);
+				else if ("ssh".equals(s) || "sftp".equals(s)) //$NON-NLS-1$ //$NON-NLS-2$
+					storedURI = storedURI.setPort(22);
+				else if ("ftp".equals(s)) //$NON-NLS-1$
+					storedURI = storedURI.setPort(21);
+			}
 		}
 		String pathName = GIT_PATH_PREFIX
 				+ EncodingUtils.encodeSlashes(storedURI.toString());
