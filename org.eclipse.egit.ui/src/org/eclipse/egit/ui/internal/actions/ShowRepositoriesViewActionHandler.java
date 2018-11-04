a14063fe5d15ebbfe845fc7565bdc2f5981e9ab2
Michael Keppler <Michael.Keppler@gmx.de>
2018-11-04 10:48:06 +0100

    Fix editor context menu Team > Show in Repository
    The handler needs to get the current editor from the context. The
    context information is lost by switching to the view. Therefore first
    remember the selection, then switch to the view.
    
    Bug: 540747
    Change-Id: Ic6c303dd06a9e6062514444e732cd21245369158
    Signed-off-by: Michael Keppler <Michael.Keppler@gmx.de>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/ShowRepositoriesViewActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/ShowRepositoriesViewActionHandler.java
@@ -18,6 +18,7 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.egit.ui.internal.repository.RepositoriesView;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ShowInContext;
@@ -31,10 +32,13 @@ public class ShowRepositoriesViewActionHandler extends RepositoryActionHandler {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		RepositoriesView view;
 		try {
+			// remember selection before activating a new view
+			IStructuredSelection selection = getSelection(event);
+
 			view = (RepositoriesView) PlatformUI.getWorkbench()
 					.getActiveWorkbenchWindow().getActivePage().showView(
 							RepositoriesView.VIEW_ID);
-			ShowInContext ctx = new ShowInContext(ResourcesPlugin.getWorkspace().getRoot(), getSelection(event));
+			ShowInContext ctx = new ShowInContext(ResourcesPlugin.getWorkspace().getRoot(), selection);
 			view.show(ctx);
 		} catch (PartInitException e) {
 			throw new ExecutionException(e.getMessage(), e);
