da63eb4c389e6f5f1ce682066a63c60d84a37d56
Michael Keppler <Michael.Keppler@gmx.de>
2018-11-22 07:53:07 +0100

    Fix Mockito deprecation warnings
    The JUnit runner hierarchy has changed, and the new super class shall be
    used instead of the previous runner.
    
    Change-Id: Ia6fbf8bb875c6fdc7860ecd21e2a39da5af2bd51
    Signed-off-by: Michael Keppler <Michael.Keppler@gmx.de>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/internal/dialogs/HyperlinkTokenScannerTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/internal/dialogs/HyperlinkTokenScannerTest.java
@@ -32,7 +32,7 @@ import org.eclipse.ui.texteditor.AbstractTextEditor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class HyperlinkTokenScannerTest {
