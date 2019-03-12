482c2a69863f61b1ef591ab30de831afcf033a84
Thomas Wolf <thomas.wolf@paranor.ch>
2019-03-12 22:42:08 +0100

    Unify "Create Branch" commands
    EGit had three different commands for "Create Branch", one in the
    repositories view, one in the history view, and one in the commit
    editor. The one from the repositories view was the only one that
    had a category ID and that thus showed up by default in the key
    binding preferences.
    
    Change that to use one single command with two different handlers,
    one for RepositoryTreeNodes and one for IRepositoryCommits as used
    in the history view and in the commit editor. Delete the dedicated
    handler for the history view, and generalize the other such that it
    works in either the history view or in the commit editor.
    
    The command re-uses the ID from the repositories view command
    because of bug bug 415382, though I would have liked to introduce
    a new command ID "org.eclipse.egit.ui.team.branch.create".
    
    Bug: 545031
    Change-Id: I57f2f6c15e2f7bb796ecb3bc4c790893b094d68c
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commit/command/CreateBranchHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commit/command/CreateBranchHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2011 GitHub Inc.
+ *  Copyright (c) 2011, 2019 GitHub Inc. and others
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License 2.0
  *  which accompanies this distribution, and is available at
@@ -12,38 +12,93 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.commit.command;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.egit.ui.internal.commit.RepositoryCommit;
+import org.eclipse.egit.core.internal.IRepositoryCommit;
+import org.eclipse.egit.ui.internal.CommonUtils;
 import org.eclipse.egit.ui.internal.repository.CreateBranchWizard;
 import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.revplot.PlotCommit;
+import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
- * Create branch handler
+ * Create branch handler. Used in the history view and in the commit editor.
  */
 public class CreateBranchHandler extends CommitCommandHandler {
 
-	/**
-	 * Command id
-	 */
-	public static final String ID = "org.eclipse.egit.ui.commit.CreateBranch"; //$NON-NLS-1$
-
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		List<RepositoryCommit> commits = getCommits(event);
+		List<IRepositoryCommit> commits = getSelectedItems(
+				IRepositoryCommit.class, event);
 		if (commits.size() == 1) {
-			RepositoryCommit commit = commits.get(0);
+			IRepositoryCommit commit = commits.get(0);
+
+			List<Ref> branches = getBranches(commit);
+
+			String base;
+			if (branches.isEmpty()) {
+				base = commit.getRevCommit().getName();
+			} else {
+				// prefer to create new branch based on a remote tracking branch
+				Collections.sort(branches, new Comparator<Ref>() {
+
+					@Override
+					public int compare(Ref o1, Ref o2) {
+						String refName1 = o1.getName();
+						String refName2 = o2.getName();
+						if (refName1.startsWith(Constants.R_REMOTES)) {
+							if (refName2.startsWith(Constants.R_HEADS))
+								return -1;
+							else
+								return CommonUtils.STRING_ASCENDING_COMPARATOR
+										.compare(refName1, refName2);
+						} else {
+							if (refName2.startsWith(Constants.R_REMOTES))
+								return 1;
+							else
+								return CommonUtils.STRING_ASCENDING_COMPARATOR
+										.compare(refName1, refName2);
+						}
+					}
+				});
+				Ref branch = branches.get(0).getLeaf();
+				base = branch.getName();
+
+			}
 			WizardDialog dlg = new WizardDialog(
 					HandlerUtil.getActiveShellChecked(event),
-					new CreateBranchWizard(commit.getRepository(), commit
-							.getRevCommit().name()));
+					new CreateBranchWizard(commit.getRepository(), base));
 			dlg.setHelpAvailable(false);
 			dlg.open();
 		}
 		return null;
+	}
+
+	private List<Ref> getBranches(IRepositoryCommit commit) {
+		RevCommit c = commit.getRevCommit();
+		if (!(c instanceof PlotCommit)) {
+			return Collections.emptyList();
+		}
+		PlotCommit p = (PlotCommit) c;
+
+		List<Ref> result = new ArrayList<>();
+		int refCount = p.getRefCount();
+		for (int i = 0; i < refCount; i++) {
+			Ref ref = p.getRef(i);
+			String refName = ref.getName();
+			if (refName.startsWith(Constants.R_HEADS)
+					|| refName.startsWith(Constants.R_REMOTES))
+				result.add(ref);
+		}
+		return result;
 	}
 
 }
