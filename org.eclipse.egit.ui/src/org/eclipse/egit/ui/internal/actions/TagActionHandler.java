4bf818e3eb6ee5a9ce5fe2d185045c2209c261a6
Thomas Wolf <thomas.wolf@paranor.ch>
2019-03-13 08:34:26 +0100

    Unify "Create Tag..." commands
    Handle commit selections in the general TagActionHandler, too.
    Delete the two special handlers for the history view and the commit
    editor, and remove the respective commands. Replace the default
    handler for the "org.eclipse.egit.ui.team.Tag" command and give it
    a dedicated handler with an activeWhen expression.
    
    Bug: 545031
    Bug: 495064
    Change-Id: I968fb4c7d577a705c27b0f51884ba63425aa7caf
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/TagActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/TagActionHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2010, 2013 Dariusz Luksza <dariusz@luksza.org> and others.
+ * Copyright (C) 2010, 2019 Dariusz Luksza <dariusz@luksza.org> and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -11,6 +11,7 @@
 package org.eclipse.egit.ui.internal.actions;
 
 import java.io.IOException;
+import java.text.MessageFormat;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -21,6 +22,8 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.IJobChangeEvent;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.core.runtime.jobs.JobChangeAdapter;
+import org.eclipse.egit.core.AdapterUtils;
+import org.eclipse.egit.core.internal.IRepositoryCommit;
 import org.eclipse.egit.core.op.TagOperation;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.JobFamilies;
@@ -28,8 +31,9 @@ import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.decorators.GitLightweightDecorator;
 import org.eclipse.egit.ui.internal.dialogs.CreateTagDialog;
 import org.eclipse.egit.ui.internal.push.PushTagsWizard;
-import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.window.Window;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.PersonIdent;
@@ -37,10 +41,9 @@ import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.TagBuilder;
 import org.eclipse.jgit.revwalk.RevObject;
 import org.eclipse.jgit.revwalk.RevWalk;
-import org.eclipse.osgi.util.NLS;
 
 /**
- * An action for creating tag.
+ * An action for creating a tag.
  *
  * @see TagOperation
  */
@@ -48,33 +51,41 @@ public class TagActionHandler extends RepositoryActionHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		final Repository repo = getRepository(true, event);
-		if (repo == null)
-			return null;
+		IRepositoryCommit commit = getCommit(getSelection(event));
+		CreateTagDialog dialog;
+		Repository repo;
+		if (commit == null) {
+			repo = getRepository(true, event);
+			if (repo == null) {
+				return null;
+			}
+			if (!repo.getRepositoryState().canCheckout()) {
+				MessageDialog.openError(getShell(event),
+						UIText.TagAction_cannotCheckout,
+						MessageFormat.format(UIText.TagAction_repositoryState,
+								repo.getRepositoryState().getDescription()));
+				return null;
+			}
 
-		if (!repo.getRepositoryState().canCheckout()) {
-			MessageDialog.openError(getShell(event),
-					UIText.TagAction_cannotCheckout, NLS.bind(
-							UIText.TagAction_repositoryState, repo
-									.getRepositoryState().getDescription()));
+			String currentBranchName;
+			try {
+				currentBranchName = repo.getBranch();
+			} catch (IOException e) {
+				Activator.handleError(UIText.TagAction_cannotGetBranchName, e,
+						true);
+				return null;
+			}
+
+			dialog = new CreateTagDialog(getShell(event), currentBranchName,
+					repo);
+		} else {
+			repo = commit.getRepository();
+			dialog = new CreateTagDialog(getShell(event),
+					commit.getRevCommit().getId(), repo);
+		}
+		if (dialog.open() != Window.OK) {
 			return null;
 		}
-
-		String currentBranchName;
-		try {
-			currentBranchName = repo.getBranch();
-		} catch (IOException e) {
-			Activator
-					.handleError(UIText.TagAction_cannotGetBranchName, e, true);
-			return null;
-		}
-
-		CreateTagDialog dialog = new CreateTagDialog(getShell(event),
-				currentBranchName, repo);
-
-		if (dialog.open() != IDialogConstants.OK_ID)
-			return null;
-
 		final TagBuilder tag = new TagBuilder();
 		PersonIdent personIdent = new PersonIdent(repo);
 		final String tagName = dialog.getTagName();
@@ -84,16 +95,21 @@ public class TagActionHandler extends RepositoryActionHandler {
 		tag.setMessage(dialog.getTagMessage());
 
 		RevObject tagTarget;
-		try {
-			tagTarget = getTagTarget(repo, dialog.getTagCommit());
-		} catch (IOException e1) {
-			Activator.handleError(UIText.TagAction_unableToResolveHeadObjectId,
-					e1, true);
-			return null;
+		if (commit == null) {
+			try {
+				tagTarget = getTagTarget(repo, dialog.getTagCommit());
+			} catch (IOException e1) {
+				Activator.handleError(
+						UIText.TagAction_unableToResolveHeadObjectId, e1, true);
+				return null;
+			}
+		} else {
+			tagTarget = commit.getRevCommit();
 		}
 		tag.setObjectId(tagTarget);
 
-		String tagJobName = NLS.bind(UIText.TagAction_creating, tagName);
+		String tagJobName = MessageFormat.format(UIText.TagAction_creating,
+				tagName);
 		final boolean shouldMoveTag = dialog.shouldOverWriteTag();
 
 		Job tagJob = new Job(tagJobName) {
@@ -113,14 +129,14 @@ public class TagActionHandler extends RepositoryActionHandler {
 
 			@Override
 			public boolean belongsTo(Object family) {
-				if (JobFamilies.TAG.equals(family))
-					return true;
-				return super.belongsTo(family);
+				return JobFamilies.TAG.equals(family)
+						|| super.belongsTo(family);
 			}
 		};
 
 		if (dialog.shouldStartPushWizard()) {
 			tagJob.addJobChangeListener(new JobChangeAdapter() {
+
 				@Override
 				public void done(IJobChangeEvent jobChangeEvent) {
 					if (jobChangeEvent.getResult().isOK())
@@ -134,12 +150,6 @@ public class TagActionHandler extends RepositoryActionHandler {
 		return null;
 	}
 
-	@Override
-	public boolean isEnabled() {
-		final Repository repo = getRepository();
-		return repo != null && containsHead(repo);
-	}
-
 	private RevObject getTagTarget(Repository repo, ObjectId objectId)
 			throws IOException {
 		try (RevWalk rw = new RevWalk(repo)) {
@@ -148,5 +158,19 @@ public class TagActionHandler extends RepositoryActionHandler {
 			else
 				return rw.parseAny(objectId);
 		}
+	}
+
+	private IRepositoryCommit getCommit(IStructuredSelection selection) {
+		if (selection != null && selection.size() == 1) {
+			Object obj = selection.getFirstElement();
+			return AdapterUtils.adapt(obj, IRepositoryCommit.class);
+		}
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// We manage activeWhen via plugin.xml
+		return true;
 	}
 }
