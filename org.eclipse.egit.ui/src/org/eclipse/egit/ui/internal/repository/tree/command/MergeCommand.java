0045bdb26ffff11f83e429133925deed205fb69c
Thomas Wolf <thomas.wolf@paranor.ch>
2019-03-18 23:20:21 +0100

    Remove duplicate code in merge command
    The "Merge" handler for the repositories view duplicated a lot of
    code for running the operation in a job and reporting the result.
    All this is already available as MergeActionHandler.doMerge().
    Use it.
    
    Change-Id: Idc2f3e811a1d7a9da3f2f2e5746f9f24e31526c2
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/MergeCommand.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/MergeCommand.java
@@ -20,32 +20,16 @@ import java.io.IOException;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.WorkspaceJob;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
-import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.egit.core.op.MergeOperation;
-import org.eclipse.egit.ui.Activator;
-import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.actions.MergeActionHandler;
 import org.eclipse.egit.ui.internal.branch.LaunchFinder;
 import org.eclipse.egit.ui.internal.dialogs.BasicConfigurationDialog;
 import org.eclipse.egit.ui.internal.dialogs.MergeTargetSelectionDialog;
-import org.eclipse.egit.ui.internal.merge.MergeResultDialog;
 import org.eclipse.egit.ui.internal.repository.tree.RefNode;
 import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
 import org.eclipse.egit.ui.internal.repository.tree.TagNode;
 import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.osgi.util.NLS;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.PlatformUI;
 
 /**
  * Implements "Merge"
@@ -55,10 +39,11 @@ public class MergeCommand extends
 	@Override
 	public Object execute(final ExecutionEvent event) throws ExecutionException {
 		RepositoryTreeNode node = getSelectedNodes(event).get(0);
-		final Repository repository = node.getRepository();
+		Repository repository = node.getRepository();
 
-		if (!MergeActionHandler.checkMergeIsPossible(repository,
-				getShell(event))
+		if (repository == null
+				|| !MergeActionHandler.checkMergeIsPossible(repository,
+						getShell(event))
 				|| LaunchFinder.shouldCancelBecauseOfRunningLaunches(repository,
 						null)) {
 			return null;
@@ -76,11 +61,11 @@ public class MergeCommand extends
 			} catch (IOException e) {
 				targetRef = null;
 			}
-		} else if (node instanceof TagNode)
+		} else if (node instanceof TagNode) {
 			targetRef = ((TagNode) node).getObject().getName();
-		else
+		} else {
 			targetRef = null;
-
+		}
 		final String refName;
 		final MergeOperation op;
 
@@ -90,9 +75,9 @@ public class MergeCommand extends
 		} else {
 			MergeTargetSelectionDialog mergeTargetSelectionDialog = new MergeTargetSelectionDialog(
 					getShell(event), repository);
-			if (mergeTargetSelectionDialog.open() != IDialogConstants.OK_ID)
+			if (mergeTargetSelectionDialog.open() != IDialogConstants.OK_ID) {
 				return null;
-
+			}
 			refName = mergeTargetSelectionDialog.getRefName();
 			op = new MergeOperation(repository, refName);
 			op.setSquash(mergeTargetSelectionDialog.isMergeSquash());
@@ -101,55 +86,7 @@ public class MergeCommand extends
 			op.setCommit(mergeTargetSelectionDialog.isCommit());
 		}
 
-		String jobname = NLS.bind(UIText.MergeAction_JobNameMerge, refName);
-		Job job = new WorkspaceJob(jobname) {
-
-			@Override
-			public IStatus runInWorkspace(IProgressMonitor monitor) {
-				try {
-					op.execute(monitor);
-				} catch (final CoreException e) {
-					return e.getStatus();
-				}
-				return Status.OK_STATUS;
-			}
-		};
-		job.setUser(true);
-		job.setRule(op.getSchedulingRule());
-		job.addJobChangeListener(new JobChangeAdapter() {
-			@Override
-			public void done(IJobChangeEvent jobEvent) {
-				IStatus result = jobEvent.getJob().getResult();
-				if (result.getSeverity() == IStatus.CANCEL)
-					Display.getDefault().asyncExec(new Runnable() {
-						@Override
-						public void run() {
-							// don't use getShell(event) here since
-							// the active shell has changed since the
-							// execution has been triggered.
-							Shell shell = PlatformUI.getWorkbench()
-									.getActiveWorkbenchWindow().getShell();
-							MessageDialog.openInformation(shell,
-									UIText.MergeAction_MergeCanceledTitle,
-									UIText.MergeAction_MergeCanceledMessage);
-						}
-					});
-				else if (!result.isOK())
-					Activator.handleError(result.getMessage(), result
-							.getException(), true);
-				else
-					Display.getDefault().asyncExec(new Runnable() {
-						@Override
-						public void run() {
-							Shell shell = PlatformUI.getWorkbench()
-									.getActiveWorkbenchWindow().getShell();
-							MergeResultDialog.getDialog(shell, repository, op.getResult()).open();
-						}
-					});
-			}
-		});
-		job.schedule();
-
+		MergeActionHandler.doMerge(repository, op, refName);
 		return null;
 	}
 
