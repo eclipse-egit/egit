e88023d6276499867a460069fa6ae7bfbbe1b589
Thomas Wolf <thomas.wolf@paranor.ch>
2019-05-17 23:08:41 +0200

    History: new operation to check out files from a commit
    Add a new operation in the CommitFileDiffViewer to check out file
    revisions shown into the working tree. Previously the user would
    have to navigate to the file in the package explorer (or project
    explorer) and use "Replace with...->Commit" there, which opens a
    dialog to choose the commit. Or use "Open This Version" and then
    copy-paste the text. Both are rather round-about ways to achieve
    this. Newly there's a "Check Out This Version" command in the context
    menu that enables the user to do this directly.
    
    The same operation is also available in the outline view of the
    unified diff page of the commit viewer.
    
    The code checking for potential loss of uncommitted changes and
    for running launches and then asking the user to confirm the
    operation has been factored out of DiscardChangesActionHandler
    into CommandConfirmation.
    
    Bug: 362907
    Change-Id: Ie237e24b9e78b888b0de816d39c1c43ca44c400a
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/DiscardChangesActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/DiscardChangesActionHandler.java
@@ -15,32 +15,20 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.actions;
 
-import java.text.MessageFormat;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Stream;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.OperationCanceledException;
-import org.eclipse.egit.core.Activator;
-import org.eclipse.egit.core.internal.indexdiff.IndexDiffCache;
-import org.eclipse.egit.core.internal.indexdiff.IndexDiffCacheEntry;
-import org.eclipse.egit.core.internal.indexdiff.IndexDiffData;
 import org.eclipse.egit.core.internal.job.JobUtil;
 import org.eclipse.egit.core.op.DiscardChangesOperation;
 import org.eclipse.egit.ui.JobFamilies;
 import org.eclipse.egit.ui.internal.UIText;
-import org.eclipse.egit.ui.internal.branch.LaunchFinder;
+import org.eclipse.egit.ui.internal.dialogs.CommandConfirmation;
 import org.eclipse.egit.ui.internal.operations.GitScopeUtil;
-import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.window.Window;
-import org.eclipse.jgit.annotations.NonNull;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.RepositoryState;
 import org.eclipse.ui.IWorkbenchPart;
@@ -64,23 +52,8 @@ public class DiscardChangesActionHandler extends RepositoryActionHandler {
 			}
 			Map<Repository, Collection<String>> paths = operation
 					.getPathsPerRepository();
-			if (haveChanges(paths)) {
-				String question = UIText.DiscardChangesAction_confirmActionMessage;
-				String launch = LaunchFinder
-						.getRunningLaunchConfiguration(paths.keySet(), null);
-				if (launch != null) {
-					question = MessageFormat.format(question,
-							"\n\n" + MessageFormat.format( //$NON-NLS-1$
-									UIText.LaunchFinder_RunningLaunchMessage,
-									launch));
-				} else {
-					question = MessageFormat.format(question, ""); //$NON-NLS-1$
-				}
-				if (!openConfirmationDialog(event, question)) {
-					return null;
-				}
-			} else if (LaunchFinder.shouldCancelBecauseOfRunningLaunches(
-					paths.keySet(), null)) {
+			if (!CommandConfirmation.confirmCheckout(getShell(event), paths,
+					!hasDirectories)) {
 				return null;
 			}
 			JobUtil.scheduleUserWorkspaceJob(operation,
@@ -91,94 +64,6 @@ public class DiscardChangesActionHandler extends RepositoryActionHandler {
 			// cleanup mySelection to avoid side effects later after execution
 			mySelection = null;
 		}
-	}
-
-	private boolean haveChanges(Map<Repository, Collection<String>> paths) {
-		IndexDiffCache cache = Activator.getDefault().getIndexDiffCache();
-		for (Map.Entry<Repository, Collection<String>> entry : paths
-				.entrySet()) {
-			Repository repo = entry.getKey();
-			Assert.isNotNull(repo);
-			IndexDiffCacheEntry indexDiff = cache.getIndexDiffCacheEntry(repo);
-			if (indexDiff == null) {
-				return true; // No info, assume worst case
-			}
-			IndexDiffData diff = indexDiff.getIndexDiff();
-			if (diff == null || hasChanges(diff, entry.getValue())) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private boolean hasChanges(@NonNull IndexDiffData diff,
-			Collection<String> paths) {
-		Set<String> repoPaths = new HashSet<>(paths);
-		// Untracked files are ignored and won't be removed.
-		if (repoPaths.contains("")) { //$NON-NLS-1$
-			// Working tree root included
-			return diff.hasChanges();
-		}
-		// Do the directories later to avoid having to do all the (potentially
-		// expensive) substrings if a plain file already matches.
-		if (containsAny(repoPaths, diff.getAdded())
-				|| containsAny(repoPaths, diff.getChanged())
-				|| containsAny(repoPaths, diff.getModified())
-				|| containsAny(repoPaths, diff.getRemoved())) {
-			return true;
-		}
-		if (hasDirectories) {
-			return containsAnyDirectory(repoPaths, diff.getAdded())
-					|| containsAnyDirectory(repoPaths, diff.getChanged())
-					|| containsAnyDirectory(repoPaths, diff.getModified())
-					|| containsAnyDirectory(repoPaths, diff.getRemoved());
-		}
-		return false;
-	}
-
-	private boolean containsAny(Set<String> repoPaths,
-			Collection<String> files) {
-		return files.stream().anyMatch(repoPaths::contains);
-	}
-
-	private boolean containsAnyDirectory(Set<String> repoPaths,
-			Collection<String> files) {
-		String lastDirectory = null;
-		for (String file : files) {
-			int j = file.lastIndexOf('/');
-			if (j <= 0) {
-				continue;
-			}
-			String directory = file.substring(0, j);
-			String withTerminator = directory + '/';
-			if (lastDirectory != null
-					&& lastDirectory.startsWith(withTerminator)) {
-				continue;
-			}
-			if (repoPaths.contains(directory)) {
-				return true;
-			}
-			lastDirectory = withTerminator;
-			for (int i = directory.indexOf('/'); i > 0; i = directory.indexOf(
-					'/', i + 1)) {
-				if (repoPaths.contains(directory.substring(0, i))) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-
-	private boolean openConfirmationDialog(ExecutionEvent event,
-			String question) throws ExecutionException {
-		MessageDialog dlg = new MessageDialog(getShell(event),
-				UIText.DiscardChangesAction_confirmActionTitle, null, question,
-				MessageDialog.CONFIRM,
-				new String[] {
-						UIText.DiscardChangesAction_discardChangesButtonText,
-						IDialogConstants.CANCEL_LABEL },
-				0);
-		return dlg.open() == Window.OK;
 	}
 
 	@Override
