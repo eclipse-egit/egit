04e9dd97163658ae7de568dd3e2f2314cbdd8e80
Mickael Istria <mistria@redhat.com>
2019-05-24 11:30:39 +0200

    Bug 547589 - Compare view: Add "Staging" to the staging side
    Change-Id: I9302a28d61a6d4864eb074a98a772f9441991930
    Signed-off-by: Mickael Istria <mistria@redhat.com>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/ShowGitStagingViewHandler.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (C) 2019 Red Hat Inc. and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Mickael Istria (Red Hat Inc.)
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.actions;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.internal.staging.StagingView;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class ShowGitStagingViewHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
+		IWorkbenchPage page = window.getActivePage();
+		if (page != null) {
+			try {
+				return page.showView(StagingView.VIEW_ID);
+			} catch (PartInitException e) {
+				Activator.logError(e.getMessage(), e);
+			}
+		}
+		return null;
+	}
+
+}
