e0a828e14a4ab28fd8ce75b77c137d89996aeba7
Matthias Sohn <matthias.sohn@sap.com>
2019-06-11 15:10:40 +0200

    Cleanup removal of adapter workaround
    - remove unnecessary casts
    - remove unnecessary type checks since they are already done by
    Adapters.adapt()
    
    Change-Id: I7ce38e96a9ce21977958f27b12810732fddf24c5
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/operations/GitScopeUtil.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/operations/GitScopeUtil.java
@@ -114,13 +114,11 @@ public class GitScopeUtil {
 		if (mapping != null) {
 			return mapping;
 		}
-		if (o instanceof IAdaptable) {
-			IContributorResourceAdapter adapted = Adapters.adapt(o,
-					IContributorResourceAdapter.class);
-			if (adapted instanceof IContributorResourceAdapter2) {
-				IContributorResourceAdapter2 cra = (IContributorResourceAdapter2) adapted;
-				return cra.getAdaptedResourceMapping((IAdaptable) o);
-			}
+		IContributorResourceAdapter adapted = Adapters.adapt(o,
+				IContributorResourceAdapter.class);
+		if (adapted instanceof IContributorResourceAdapter2) {
+			IContributorResourceAdapter2 cra = (IContributorResourceAdapter2) adapted;
+			return cra.getAdaptedResourceMapping((IAdaptable) o);
 		}
 		return null;
 	}
