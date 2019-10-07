4d63cb65d318df968c2d94a19b7b9fd763ad5f6e
Andrey Loskutov <loskutov@gmx.de>
2019-10-07 10:28:21 +0200

    [regression] NPE when re-showing hidden search field in git history toolbar
    Make sure the SubContributionItem that contains our contribution also
    have proper visibility flag set.
    
    Bug: 551067
    Change-Id: Ie8aa1742324c2d5cd2002f4ddf012489bd818be5
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitHistoryPage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GitHistoryPage.java
@@ -86,11 +86,13 @@ import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.ControlContribution;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.action.IContributionItem;
+import org.eclipse.jface.action.IContributionManager;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IStatusLineManager;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.action.SubToolBarManager;
 import org.eclipse.jface.bindings.keys.SWTKeySupport;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
@@ -1352,6 +1354,15 @@ public class GitHistoryPage extends HistoryPage implements RefsChangedListener,
 					beforeHide();
 				}
 				super.setVisible(visible);
+				IContributionManager parent = getParent();
+				if (parent instanceof SubToolBarManager) {
+					SubToolBarManager subManager = (SubToolBarManager) parent;
+					IContributionItem item = subManager.getParent()
+							.find(getId());
+					if (item != null) {
+						item.setVisible(visible);
+					}
+				}
 				// Update the toolbar. Will dispose our FindToolbar widget on
 				// hide, and will create a new one (through createControl())
 				// on show. It'll also reposition the toolbar, if needed.
