a450717cbed9f77dc64ddaa5d848d14a014e86a6
Michael Keppler <Michael.Keppler@gmx.de>
2019-10-24 09:41:24 +0200

    Use Title case for Interactive Rebase steps
    Bug: 552360
    Change-Id: I541dacb37194cda42966e2cdb8914f18ecdcbcef
    Signed-off-by: Michael Keppler <Michael.Keppler@gmx.de>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/CoreText.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/CoreText.java
@@ -546,6 +546,24 @@ public class CoreText extends NLS {
 	/** */
 	public static String SshPreferencesMirror_invalidKeyFile;
 
+	/** */
+	public static String RebaseInteractiveStep_Edit;
+
+	/** */
+	public static String RebaseInteractiveStep_Fixup;
+
+	/** */
+	public static String RebaseInteractiveStep_Pick;
+
+	/** */
+	public static String RebaseInteractiveStep_Reword;
+
+	/** */
+	public static String RebaseInteractiveStep_Skip;
+
+	/** */
+	public static String RebaseInteractiveStep_Squash;
+
 	static {
 		initializeMessages(BUNDLE_NAME,	CoreText.class);
 	}
