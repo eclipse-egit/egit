75ecc1f7e9e9c2103fb6806151bef931164ba8cf
Thomas Wolf <thomas.wolf@paranor.ch>
2019-10-30 00:52:15 +0100

    [repo groups] inline renaming of repository groups
    Set up editing support in the repositories view, triggered
    programmatically via the RenameRepositoryGroup handler. Fix
    the RepositoriesViewSorter to actually consider the categories,
    and ensure repository groups are at the bottom.
    
    Fix equals(), hashCode(), and compareTo() for RepositoryGroupNodes:
    these should store the RepositoryGroup, not the label as their object.
    Remove the then superfluous getGoup() method; use getObject() instead.
    equals() must compare the repo groups. compareTo() is used only for
    sorting, so it may compare by group name.
    
    Fix the label provider: the workbench adapter must return a label
    text, which is the group's name.
    
    Also eliminate the default handler for PasteCommand by tying the
    handler to the standard org.eclipse.ui.edit.paste command.
    
    Pasting proved to be a problem since the view registered a default
    handler for the paste command, which "stole" the paste and always
    handled it. Thus pasting into an open cell editor was not possible.
    
    JFace does have some support for overriding edit _actions_ when a
    cell editor is active, but I haven't found anything similar for
    commands. In the end I settled for making the PasteCommand simply
    call the view first to give it a chance to forward the paste to the
    cell editor.
    
    Change-Id: If1cda3768bb284d1a6801ec63dfaa80cb684e940
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/components/MessagePopupTextCellEditor.java
@@ -0,0 +1,143 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Thomas Wolf <thomas.wolf@paranor.ch>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.components;
+
+import org.eclipse.egit.ui.Activator;
+import org.eclipse.jface.viewers.ICellEditorListener;
+import org.eclipse.jface.viewers.TextCellEditor;
+import org.eclipse.jface.window.DefaultToolTip;
+import org.eclipse.jface.window.ToolTip;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+
+/**
+ * A {@link TextCellEditor} that automatically shows any message from an
+ * {@link org.eclipse.jface.viewers.ICellEditorValidator ICellEditorValidator}
+ * in a {@link DefaultToolTip} pop-up. The tooltip by default has a delay of
+ * 200ms and a light red background. The tooltip can be obtained via
+ * {@link #getToolTip()} and then customized, if needed.
+ * <p>
+ * Additionally, this editor <em>cancels</em> the edit if focus is lost.
+ * </p>
+ */
+public class MessagePopupTextCellEditor extends TextCellEditor {
+
+	/** Default pop-up delay in milliseconds. */
+	private static final int DEFAULT_DELAY_MILLIS = 200;
+
+	/**
+	 * Default background color. See css/egit.css for the use of this pinkish
+	 * color.
+	 */
+	private static final RGB DEFAULT_BACKGROUND = new RGB(0xFF, 0x96, 0x96);
+
+	private DefaultToolTip errorMessage;
+
+	/**
+	 * Creates a new {@link MessagePopupTextCellEditor} parented under the given
+	 * control.
+	 *
+	 * @param parent
+	 *            the parent control
+	 * @see TextCellEditor#TextCellEditor(Composite)
+	 */
+	public MessagePopupTextCellEditor(Composite parent) {
+		super(parent);
+	}
+
+	/**
+	 * Creates a new {@link MessagePopupTextCellEditor} parented under the given
+	 * control using the given style.
+	 *
+	 * @param parent
+	 *            the parent control
+	 * @param style
+	 *            the style bits
+	 * @see TextCellEditor#TextCellEditor(Composite, int)
+	 */
+	public MessagePopupTextCellEditor(Composite parent, int style) {
+		super(parent, style);
+	}
+
+	/**
+	 * This cell editor uses the built-in focus listener provided by the super
+	 * class.
+	 */
+	@Override
+	protected boolean dependsOnExternalFocusListener() {
+		return false;
+	}
+
+	/**
+	 * Invoked when the cell editor has lost the focus; cancels the editor
+	 * without applying the value.
+	 */
+	@Override
+	protected void focusLost() {
+		// The super implementation applies the value, but that's a bit
+		// risky. Edits should be done only if the user explicitly hit
+		// <return>.
+		if (isActivated()) {
+			fireCancelEditor();
+		}
+	}
+
+	@Override
+	protected Control createControl(Composite parent) {
+		Control control = super.createControl(parent);
+		errorMessage = new DefaultToolTip(control, ToolTip.NO_RECREATE, true);
+		// A delay enables us to cancel showing the tooltip if the user keeps
+		// typing and the value is valid again.
+		errorMessage.setPopupDelay(DEFAULT_DELAY_MILLIS);
+		errorMessage.setBackgroundColor(Activator.getDefault()
+				.getResourceManager().createColor(DEFAULT_BACKGROUND));
+		control.addDisposeListener(event -> errorMessage.hide());
+		addListener(new ICellEditorListener() {
+
+			@Override
+			public void editorValueChanged(boolean oldValidState,
+					boolean newValidState) {
+				if (newValidState) {
+					errorMessage.hide();
+					return;
+				}
+				Control editor = getControl();
+				Point pos = editor.getSize();
+				errorMessage.setText(getErrorMessage());
+				pos.x = 0;
+				errorMessage.show(pos);
+			}
+
+			@Override
+			public void cancelEditor() {
+				errorMessage.hide();
+			}
+
+			@Override
+			public void applyEditorValue() {
+				errorMessage.hide();
+			}
+		});
+		return control;
+	}
+
+	/**
+	 * Retrieves the {@link DefaultToolTip} use for the validation message
+	 * pop-up.
+	 *
+	 * @return the tooltip
+	 */
+	public DefaultToolTip getToolTip() {
+		return errorMessage;
+	}
+}
