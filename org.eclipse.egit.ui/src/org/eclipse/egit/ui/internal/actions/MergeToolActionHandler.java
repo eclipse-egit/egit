ad719035e96b59ba4aaf8f1400a619560925e498
Mykola Zakharchuk <zakharchuk.vn@gmail.com>
2019-11-21 10:08:03 +0100

    Allow use of external diff/merge tools defined in git attributes
    Contributes the new preference page for diff/merge external tools and
    allows using of external diff/merge tools only for particular file type.
    
    Bug: 552838
    Change-Id: I22ff5fd67c0effdbb6c255b5e02ff4a1fdf60673
    Signed-off-by: Mykola Zakharchuk <zakharchuk.vn@gmail.com>
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>
    Signed-off-by: Simeon Andreev <simeon.danailov.andreev@gmail.com>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/MergeToolActionHandler.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/actions/MergeToolActionHandler.java
@@ -43,6 +43,7 @@ import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.UIPreferences;
 import org.eclipse.egit.ui.internal.DiffContainerJob;
 import org.eclipse.egit.ui.internal.ToolsUtils;
+import org.eclipse.egit.ui.internal.diffmerge.DiffMergeSettings;
 import org.eclipse.egit.ui.internal.merge.GitMergeEditorInput;
 import org.eclipse.egit.ui.internal.merge.MergeModeDialog;
 import org.eclipse.egit.ui.internal.preferences.GitPreferenceRoot;
@@ -52,7 +53,7 @@ import org.eclipse.jface.window.Window;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.diffmergetool.FileElement;
-import org.eclipse.jgit.diffmergetool.MergeToolManager;
+import org.eclipse.jgit.diffmergetool.MergeTools;
 import org.eclipse.jgit.diffmergetool.PromptContinueHandler;
 import org.eclipse.jgit.diffmergetool.ToolException;
 import org.eclipse.jgit.lib.Repository;
@@ -78,10 +79,10 @@ public class MergeToolActionHandler extends RepositoryActionHandler {
 			boolean useWorkspace = mergeMode == 1;
 			input = new GitMergeEditorInput(useWorkspace, locations);
 		}
-		if (GitPreferenceRoot.useExternalMergeTool()) {
-			openMergeToolExternal(input);
-		} else {
+		if (DiffMergeSettings.useInternalMergeTool()) {
 			openMergeToolInternal(input);
+		} else {
+			openMergeToolExternal(input);
 		}
 		return null;
 	}
@@ -165,9 +166,9 @@ public class MergeToolActionHandler extends RepositoryActionHandler {
 		long modifiedBefore = merged.getFile().lastModified();
 		try {
 			// create the merge tool manager
-			MergeToolManager mergeToolMgr = new MergeToolManager(repository);
+			MergeTools mergeToolMgr = new MergeTools(repository);
 			// get the selected tool name
-			Optional<String> toolNameToUse = Optional.ofNullable(GitPreferenceRoot.getMergeToolName());
+			Optional<String> toolNameToUse = Optional.ofNullable(DiffMergeSettings.getMergeToolName());
 			Optional<Boolean> prompt = Optional.empty();
 
 			PromptContinueHandler promptContinueHandler = new FileNamePromptContinueHandler(
