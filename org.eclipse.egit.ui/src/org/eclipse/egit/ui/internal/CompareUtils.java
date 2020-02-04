bb0d34ca698a7af5bf779613645c6a7db6a91801
Simeon Andreev <simeon.danailov.andreev@gmail.com>
2020-02-04 15:55:46 +0100

    Add EGit preferences for external diff tools per file extension
    The preference is:
    
    org.eclipse.egit.ui/external_diff_tool_per_extension
    
    Its value is comma separated pairs of file extension and external diff
    command. The external diff command uses parameters $LOCAL, $MERGED and
    $REMOTE, similar to the external diff commands that can be defined in
    git attributes and configuration.
    
    Example product customization .ini contents are:
    
    org.eclipse.egit.ui/external_diff_tool_per_extension=txt,kdiff3 --L1
    "$MERGED (A)" --L2 "$MERGED (B)" "$LOCAL" "$REMOTE",ext1,command1
    org.eclipse.egit.ui/diff_tool_mode=1
    
    Bug: 552838
    Change-Id: Iede007f91e1622ca4a87681d05f77cddf2d5176d
    Signed-off-by: Simeon Andreev <simeon.danailov.andreev@gmail.com>
    Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/CompareUtils.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/CompareUtils.java
@@ -79,6 +79,7 @@ import org.eclipse.jgit.diffmergetool.FileElement;
 import org.eclipse.jgit.diffmergetool.FileElement.Type;
 import org.eclipse.jgit.diffmergetool.PromptContinueHandler;
 import org.eclipse.jgit.diffmergetool.ToolException;
+import org.eclipse.jgit.diffmergetool.UserDefinedDiffTool;
 import org.eclipse.jgit.dircache.DirCache;
 import org.eclipse.jgit.dircache.DirCacheCheckout.CheckoutMetadata;
 import org.eclipse.jgit.dircache.DirCacheEditor;
@@ -103,6 +104,7 @@ import org.eclipse.jgit.treewalk.filter.NotIgnoredFilter;
 import org.eclipse.jgit.treewalk.filter.PathFilterGroup;
 import org.eclipse.jgit.treewalk.filter.TreeFilter;
 import org.eclipse.jgit.util.IO;
+import org.eclipse.jgit.util.StringUtils;
 import org.eclipse.jgit.util.io.EolStreamTypeUtil;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.SWT;
@@ -369,30 +371,41 @@ public class CompareUtils {
 				if (monitor.isCanceled()) {
 					return Status.CANCEL_STATUS;
 				}
+				if (input instanceof GitCompareEditorInput) {
+					// no reason to open anything if we don't have a path - this
+					// may open diff on files that aren't supported by external
+					// diff, and even if it can open a diff on some supported
+					// file, it is not what user expected to see in most cases.
+					return Status.CANCEL_STATUS;
+				}
+				if (!(input instanceof GitCompareFileRevisionEditorInput)) {
+					PlatformUI.getWorkbench().getDisplay().asyncExec(
+							() -> openCompareToolInternal(workbenchPage,
+									input));
+					return Status.OK_STATUS;
+				}
 				GitCompareFileRevisionEditorInput gitCompareInput = (GitCompareFileRevisionEditorInput) input;
 				FileRevisionTypedElement leftRevision = gitCompareInput
 						.getLeftRevision();
-				IFile leftResource = (IFile) gitCompareInput
-						.getAdapter(IFile.class);
 				FileRevisionTypedElement rightRevision = gitCompareInput
 						.getRightRevision();
-				// get the relative project path from right revision here
-				String changedFilePath = null;
-				if (leftResource != null) {
-					changedFilePath = repository.getWorkTree().toPath()
-							.relativize(leftResource.getRawLocation().toFile().toPath())
-							.toString();
-				} else if (leftRevision != null) {
-					changedFilePath = leftRevision.getPath();
-				}
+				String changedFilePath = getChangedFilePath(repository, gitCompareInput);
 
 				try {
 					Optional<String> toolName = DiffMergeSettings.getDiffToolName(repository, changedFilePath);
 
+					String preferencesCommand = null;
+					if (changedFilePath != null) {
+						preferencesCommand = DiffMergeSettings
+								.getDiffToolCommandFromPreferences(changedFilePath);
+					}
 					// fall back if no tool specified or data is not in working
 					// tree
-					if (!toolName.isPresent()) {
-						openCompareToolInternal(workbenchPage, input);
+					if (!toolName.isPresent()
+							&& StringUtils.isEmptyOrNull(preferencesCommand)) {
+						PlatformUI.getWorkbench().getDisplay().asyncExec(
+								() -> openCompareToolInternal(workbenchPage,
+										input));
 						return Status.OK_STATUS;
 					}
 
@@ -441,12 +454,21 @@ public class CompareUtils {
 					boolean gui = false;
 					Optional<Boolean> unset = Optional.empty();
 					DiffTools diffToolMgr = new DiffTools(repository);
-					diffToolMgr.compare(local, remote, toolName, unset, gui,
-							unset, promptContinueHandler, tools -> {
-								ToolsUtils.informUser(
-										UIText.CompareUtils_NoDiffToolsDefined,
-										UIText.CompareUtils_NoDiffToolSpecified);
-							});
+					if (preferencesCommand != null) {
+						String customToolName = "custom_tool_" //$NON-NLS-1$
+								+ DiffMergeSettings
+								.getFileExtension(changedFilePath);
+						UserDefinedDiffTool tool = new UserDefinedDiffTool(
+								customToolName, "", preferencesCommand); //$NON-NLS-1$
+						diffToolMgr.compare(local, remote, tool, true);
+					} else {
+						diffToolMgr.compare(local, remote, toolName, unset, gui,
+								unset, promptContinueHandler, tools -> {
+									ToolsUtils.informUser(
+											UIText.CompareUtils_NoDiffToolsDefined,
+											UIText.CompareUtils_NoDiffToolSpecified);
+								});
+					}
 				} catch (ToolException e) {
 					ToolsUtils.informUserAboutError(
 							UIText.CompareUtils_ExternalDiffToolDied
@@ -461,9 +483,34 @@ public class CompareUtils {
 				return Status.OK_STATUS;
 			}
 
+
 		};
 		job.setUser(true);
 		job.schedule();
+	}
+
+	private static String getChangedFilePath(
+			Repository repository, GitCompareFileRevisionEditorInput gitCompareInput) {
+		FileRevisionTypedElement leftRevision = gitCompareInput
+				.getLeftRevision();
+		// get the relative project path from right revision here
+		IFile leftResource = (IFile) gitCompareInput.getAdapter(IFile.class);
+		String changedFilePath = null;
+		if (leftResource != null) {
+			changedFilePath = repository.getWorkTree().toPath()
+					.relativize(leftResource.getRawLocation().toFile().toPath())
+					.toString();
+		} else if (leftRevision != null) {
+			changedFilePath = leftRevision.getPath();
+		}
+		if (changedFilePath == null) {
+			FileRevisionTypedElement rightRevision = gitCompareInput
+					.getRightRevision();
+			if (rightRevision != null) {
+				changedFilePath = rightRevision.getPath();
+			}
+		}
+		return changedFilePath;
 	}
 
 	private static IEditorPart findReusableCompareEditor(
