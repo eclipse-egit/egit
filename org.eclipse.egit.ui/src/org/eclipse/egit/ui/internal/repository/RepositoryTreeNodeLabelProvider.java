467ab6e50409e63c34e24c9f3e60fc0d662344b4
Thomas Wolf <thomas.wolf@paranor.ch>
2020-03-24 08:26:27 +0100

    [repo view] Avoid potential NPE in label provider
    Don't set a style for the paths of repositories or working trees if no
    styles were set originally.
    
    Bug: 561377
    Change-Id: I0f02d8cbcdec17be844896c3ae2234b455a71f20
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoryTreeNodeLabelProvider.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/RepositoryTreeNodeLabelProvider.java
@@ -175,15 +175,18 @@ public class RepositoryTreeNodeLabelProvider
 			} else if (element instanceof WorkingDirNode) {
 				newText += repository.getWorkTree().getAbsolutePath();
 			}
-			StyleRange style = new StyleRange();
-			style.start = textAfter.length();
-			style.length = newText.length();
-			StyledString.QUALIFIER_STYLER.applyStyles(style);
-			StyleRange[] newRanges = new StyleRange[rangesAfter.length + 1];
-			System.arraycopy(rangesAfter, 0, newRanges, 0, rangesAfter.length);
-			newRanges[newRanges.length - 1] = style;
 			cell.setText(textAfter + newText);
-			cell.setStyleRanges(newRanges);
+			if (rangesAfter != null) {
+				StyleRange style = new StyleRange();
+				style.start = textAfter.length();
+				style.length = newText.length();
+				StyledString.QUALIFIER_STYLER.applyStyles(style);
+				StyleRange[] newRanges = new StyleRange[rangesAfter.length + 1];
+				System.arraycopy(rangesAfter, 0, newRanges, 0,
+						rangesAfter.length);
+				newRanges[newRanges.length - 1] = style;
+				cell.setStyleRanges(newRanges);
+			}
 		} else {
 			updater.accept(cell);
 		}
