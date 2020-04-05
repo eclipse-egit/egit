860aec07ea5ff4e21d01d5a6d241a6c81ae116ee
Michael Keppler <Michael.Keppler@gmx.de>
2020-04-05 11:58:19 +0200

    Refresh reflog only on change
    Do not set the input of the reflog view again, if neither repository nor
    target of the ref has changed.
    
    Bug: 561110
    Change-Id: I732984189302abec2e96f3c63eff227a88273873
    Signed-off-by: Michael Keppler <Michael.Keppler@gmx.de>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/reflog/ReflogView.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/reflog/ReflogView.java
@@ -634,7 +634,42 @@ public class ReflogView extends ViewPart implements RefsChangedListener, IShowIn
 				input = new ReflogInput(input.getRepository(), Constants.HEAD);
 				needRefUpdate = true;
 			}
-			refLogTreeViewer.setInput(input);
+			boolean needInputUpdate = repoChanged || needRefUpdate;
+			if (!needInputUpdate) {
+				// investigate whether the ref behind HEAD has changed
+				if (currentInput instanceof ReflogInput) {
+					try {
+						ObjectId currentObjectId = null;
+						Object[] currentChildren = ((ReflogInput) currentInput)
+								.getChildren(null);
+						if (currentChildren != null
+								&& currentChildren.length > 0) {
+							ReflogItem item = (ReflogItem) currentChildren[0];
+							currentObjectId = item.getObjectId();
+						}
+						ObjectId inputObjectId = null;
+						if (currentObjectId != null) {
+							Ref inputRef = input.getRepository()
+									.findRef(input.getRef());
+							if (inputRef != null) {
+								inputObjectId = inputRef.getTarget()
+										.getObjectId();
+							}
+						}
+						needInputUpdate = currentObjectId == null
+								|| inputObjectId == null
+								|| !currentObjectId.equals(inputObjectId);
+					} catch (IOException e) {
+						Activator.logError(UIText.ReflogView_ErrorOnOpenCommit,
+								e);
+					}
+				} else {
+					needInputUpdate = true;
+				}
+			}
+			if (needInputUpdate) {
+				refLogTreeViewer.setInput(input);
+			}
 			if (needRefUpdate) {
 				updateRefLink(input.getRef());
 			}
