8d347507f591c21a5623666759464212978a10c5
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2020-06-15 20:49:20 +0200

    Introduce preference for Push Wizard as part of Commit And Push...
    This allows to always force the Push Wizard when users want it
    
    Bug: 501419
    Change-Id: I0314ff4f6d4cb5d69b16702d9286a08d0c66a904

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commit/CommitJob.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commit/CommitJob.java
@@ -25,11 +25,14 @@ import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.egit.core.op.CommitOperation;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.JobFamilies;
+import org.eclipse.egit.ui.UIPreferences;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.decorators.GitLightweightDecorator;
 import org.eclipse.egit.ui.internal.dialogs.CommitMessageComponentStateManager;
 import org.eclipse.egit.ui.internal.push.PushMode;
+import org.eclipse.egit.ui.internal.push.PushOperationUI;
 import org.eclipse.egit.ui.internal.push.PushWizardDialog;
+import org.eclipse.egit.ui.internal.push.SimpleConfigurePushDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.jgit.api.errors.AbortedByHookException;
@@ -37,6 +40,7 @@ import org.eclipse.jgit.api.errors.CanceledException;
 import org.eclipse.jgit.api.errors.JGitInternalException;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.transport.RemoteConfig;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PlatformUI;
@@ -155,15 +159,32 @@ public class CommitJob extends Job {
 	}
 
 	private void pushUpstream(final RevCommit commit, final PushMode pushTo) {
-		final Display display = PlatformUI.getWorkbench().getDisplay();
-		display.asyncExec(() -> {
-			Wizard wizard = getPushWizard(commit, pushTo);
-			if (wizard != null) {
-				PushWizardDialog dialog = new PushWizardDialog(
-						display.getActiveShell(), wizard);
-				dialog.open();
-			}
-		});
+		final RemoteConfig config = SimpleConfigurePushDialog
+				.getConfiguredRemote(repository);
+		boolean alwaysShowPushWizard = Activator.getDefault()
+				.getPreferenceStore()
+				.getBoolean(UIPreferences.ALWAYS_SHOW_PUSH_WIZARD_ON_COMMIT);
+
+		if (alwaysShowPushWizard || pushTo == PushMode.GERRIT
+				|| config == null) {
+			final Display display = PlatformUI.getWorkbench().getDisplay();
+			display.asyncExec(new Runnable() {
+
+				@Override
+				public void run() {
+					Wizard pushWizard = getPushWizard(commit, pushTo);
+					if (pushWizard != null) {
+						PushWizardDialog dialog = new PushWizardDialog(
+								display.getActiveShell(), pushWizard);
+						dialog.open();
+					}
+				}
+			});
+		} else {
+			PushOperationUI op = new PushOperationUI(repository,
+					config.getName(), false);
+			op.start();
+		}
 	}
 
 	private Wizard getPushWizard(final RevCommit commit,
