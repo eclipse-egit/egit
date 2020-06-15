1f8b86b8b9732c6ce83855559e6a29754a9fcd90
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2020-06-15 20:49:20 +0200

    Introduce preference for Push Wizard as part of Commit And Push...
    This allows to always force the Push Wizard when users want it
    
    Bug: 501419
    Change-Id: I0314ff4f6d4cb5d69b16702d9286a08d0c66a904
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/preferences/CommittingPreferencePage.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/preferences/CommittingPreferencePage.java
@@ -183,6 +183,20 @@ public class CommittingPreferencePage extends DoublePreferencesPreferencePage
 		addField(signedOffBy);
 		updateMargins(footersGroup);
 
+		Group pushGroup = new Group(main, SWT.SHADOW_ETCHED_IN);
+		pushGroup.setText(UIText.CommittingPreferencePage_push);
+		GridDataFactory.fillDefaults().grab(true, false).span(3, 1)
+				.applyTo(pushGroup);
+
+		BooleanFieldEditor alwaysShowPushWizard = new BooleanFieldEditor(
+				UIPreferences.ALWAYS_SHOW_PUSH_WIZARD_ON_COMMIT,
+				UIText.CommittingPreferencePage_alwaysShowPushWizard,
+				pushGroup);
+		alwaysShowPushWizard.getDescriptionControl(pushGroup).setToolTipText(
+				UIText.CommittingPreferencePage_alwaysShowPushWizardTooltip);
+		addField(alwaysShowPushWizard);
+		updateMargins(pushGroup);
+
 		buildProblemsGroup = createGroup(main, 1);
 		buildProblemsGroup.setText(
 				UIText.CommittingPreferencePage_WarnBeforeCommittingTitle);
