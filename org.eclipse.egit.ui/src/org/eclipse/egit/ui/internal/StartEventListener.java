ab58691f58280c2ca4b73db3e30f65f366915bbe
Thomas Wolf <thomas.wolf@paranor.ch>
2021-02-05 23:05:37 +0100

    Use an OSGi declarative service to manage some EGit components
    Add a StartEventHandler to start things that require the workbench to
    have been created. If the workbench is already running when the
    component is activated, it initializes the EGit components right away.
    Otherwise, the handler is notified on the APP_STARTUP_COMPLETED event
    and initializes the EGit components then.
    
    This change uses this mechanism for the SelectionRepositoryStateCache
    only, but it might be used to manage other EGit internal components in
    the future.
    
    Maybe some parts of EGit might themselves become OSGi declarative
    services and deal with their own activation as they need. The single
    StartEventHandler is a useful bridge facilitating the move to OSGi
    declarative services while minimizing code changes, though.
    
    Change-Id: Ic12507ef24dc940a25028ff2e1a1b77f64276078
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/StartEventListener.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Thomas Wolf <thomas.wolf@paranor.ch> and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.eclipse.e4.ui.workbench.UIEvents;
+import org.eclipse.egit.ui.internal.selection.SelectionRepositoryStateCache;
+import org.eclipse.ui.PlatformUI;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventConstants;
+import org.osgi.service.event.EventHandler;
+
+/**
+ * Initializes some EGit components that rely on the workbench having been
+ * created.
+ */
+@Component(property = EventConstants.EVENT_TOPIC + '='
+		+ UIEvents.UILifeCycle.APP_STARTUP_COMPLETE)
+public class StartEventListener implements EventHandler {
+
+	private AtomicBoolean started = new AtomicBoolean();
+
+	private void startInternalComponents() {
+		if (started.compareAndSet(false, true)) {
+			SelectionRepositoryStateCache.INSTANCE.initialize();
+		}
+	}
+
+	@Override
+	public void handleEvent(Event event) {
+		// If the workbench wasn't running yet when we were activated, we'll
+		// initialize on the APP_STARTUP_COMPLETE event.
+		startInternalComponents();
+	}
+
+	@Activate
+	void startUp() {
+		if (PlatformUI.isWorkbenchRunning()) {
+			startInternalComponents();
+		}
+	}
+
+	@Deactivate
+	void shutDown() {
+		if (started.get()) {
+			SelectionRepositoryStateCache.INSTANCE.dispose();
+		}
+	}
+}
