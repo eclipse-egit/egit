959edb963da8fc42f5919fde6710119d0e6bfc90
Thomas Wolf <thomas.wolf@paranor.ch>
2021-02-06 20:06:04 +0100

    Simplify the EGit UI Activator by using OSGi declarative services
    The EGit UI Activator contained a lot of code to deal with external
    modifications to git repositories. Refactor this into more manageable
    components created dynamically by the framework.
    
    First, the FocusHandler: this maintains a flag whether the application
    is active, i.e., whether any of its Shells is the current active shell.
    This needs the workbench to have been created, and the FocusHandler has
    acquired over time some completely unrelated code that just also
    happened to need a workbench. To ensure that it initialized itself only
    once the workbench was ready, the FocusHandler scheduled a job that
    re-scheduled periodically until the workbench was up.
    
    Split this: move the unrelated code (setting icons for EGit core jobs)
    to the newly introduced StartEventListener. Move maintaining the
    "application active" state to ApplicationActiveListener, a dedicated
    OSGi component triggered on the APP_STARTUP_COMPLETE event. At that
    point, we are guaranteed that the workbench is ready, so no job is
    needed. The ApplicationActiveListener fires a custom event
    APPLICATION_ACTIVE whenever the active state changes; the data of the
    event is a boolean indicating the new state (true if active, false
    otherwise). Make sure an initial event is fired. The event is posted
    asynchronously.
    
    Move all the repository scanning and project refreshing code to a new
    component ExternalRepositoryScanner, which in turn listens on that new
    APPLICATION_ACTIVE event. The framework will create this component
    automatically when the first such event is fired; component creation
    will be off the UI thread since the event is posted asynchronously.
    Schedule a run whenever the state changes from "inactive" to "active"
    to resume scanning.
    
    Unregister the DebugOptionsListener on Activator.stop().
    
    Change-Id: I296d5801d62a6f2c0c8da4166aa36270c1b2ad1b
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/ApplicationActiveListener.java
@@ -0,0 +1,129 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Thomas Wolf <thomas.wolf@paranor.ch> and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Thomas Wolf - factored out of Activator
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal;
+
+import org.eclipse.e4.core.services.events.IEventBroker;
+import org.eclipse.e4.ui.workbench.UIEvents;
+import org.eclipse.swt.SWTException;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWindowListener;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventConstants;
+import org.osgi.service.event.EventHandler;
+
+/**
+ * Determines whether any shell of the application is active and fires an event
+ * when the value changes.
+ */
+@Component(property = {
+		EventConstants.EVENT_TOPIC + '='
+				+ UIEvents.UILifeCycle.APP_STARTUP_COMPLETE,
+		EventConstants.EVENT_TOPIC + '='
+				+ UIEvents.UILifeCycle.APP_SHUTDOWN_STARTED })
+public class ApplicationActiveListener implements EventHandler {
+
+	/**
+	 * Event topic for the events posted by this component.
+	 */
+	public static final String TOPIC_APPLICATION_ACTIVE = "org/eclipse/egit/ui/APPLICATION_ACTIVE"; //$NON-NLS-1$
+
+	private WindowTracker listener;
+
+	@Override
+	public void handleEvent(Event event) {
+		String topic = event.getTopic();
+		if (topic == null) {
+			return;
+		}
+		switch (topic) {
+		case UIEvents.UILifeCycle.APP_STARTUP_COMPLETE:
+			if (listener == null) {
+				listener = new WindowTracker();
+				listener.update();
+				PlatformUI.getWorkbench().addWindowListener(listener);
+			}
+			break;
+		case UIEvents.UILifeCycle.APP_SHUTDOWN_STARTED:
+			shutDown();
+			break;
+		default:
+			break;
+		}
+	}
+
+	@Deactivate
+	void shutDown() {
+		if (listener != null) {
+			PlatformUI.getWorkbench().removeWindowListener(listener);
+			listener = null;
+		}
+	}
+
+	private static class WindowTracker implements IWindowListener {
+
+		private final IEventBroker broker;
+
+		WindowTracker() {
+			this.broker = PlatformUI.getWorkbench()
+					.getService(IEventBroker.class);
+		}
+
+		private boolean isActive;
+
+		void update() {
+			if (PlatformUI.isWorkbenchRunning()) {
+				Display display = PlatformUI.getWorkbench().getDisplay();
+				if (display != null) {
+					try {
+						display.asyncExec(() -> {
+							boolean wasActive = isActive;
+							isActive = !display.isDisposed()
+									&& display.getActiveShell() != null;
+							if (wasActive != isActive) {
+								broker.post(TOPIC_APPLICATION_ACTIVE,
+										Boolean.valueOf(isActive));
+							}
+						});
+					} catch (SWTException e) {
+						// Silently ignore -- display was disposed already
+					}
+				}
+			}
+		}
+
+		@Override
+		public void windowActivated(IWorkbenchWindow window) {
+			update();
+		}
+
+		@Override
+		public void windowDeactivated(IWorkbenchWindow window) {
+			update();
+		}
+
+		@Override
+		public void windowClosed(IWorkbenchWindow window) {
+			update();
+		}
+
+		@Override
+		public void windowOpened(IWorkbenchWindow window) {
+			update();
+		}
+	}
+}
