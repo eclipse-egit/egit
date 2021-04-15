3327b8cc7dd5c42de541ccbbc034f12e86330ea4
Thomas Wolf <thomas.wolf@paranor.ch>
2021-04-15 11:24:39 +0200

    Remove unused class RevUtils.ConflictCommits
    It's unused since commit b51a1c86.
    
    Change-Id: Ia7c3ada673eaf955e5d6703d4f4f3c0e610ab5b0
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/RevUtils.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/RevUtils.java
@@ -184,34 +184,4 @@ public class RevUtils {
 		}
 		return false;
 	}
-
-	/**
-	 * The interesting commits from ours/theirs for a file in case of a
-	 * conflict.
-	 */
-	public static class ConflictCommits {
-		private final RevCommit ourCommit;
-		private final RevCommit theirCommit;
-
-		private ConflictCommits(RevCommit ourCommit, RevCommit theirCommit) {
-			this.ourCommit = ourCommit;
-			this.theirCommit = theirCommit;
-		}
-
-		/**
-		 * @return the commit from "ours" that last modified a file, or
-		 *         {@code null} if none found
-		 */
-		public RevCommit getOurCommit() {
-			return ourCommit;
-		}
-
-		/**
-		 * @return the commit from "theirs" that last modified a file, or
-		 *         {@code null} if none found
-		 */
-		public RevCommit getTheirCommit() {
-			return theirCommit;
-		}
-	}
 }
