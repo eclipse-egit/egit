46fbf9ff83b803d08716d185b2c269560f5909f0
Thomas Wolf <thomas.wolf@paranor.ch>
2021-04-29 15:39:15 +0200

    [merge] Hide changes between ancestor and current for cherry-pick
    By default hide these changes. The ancestor in a cherry-pick is a
    parent commit of 'theirs' (the commit that was cherry-picked); the
    differences between that parent and the current version are simply
    not interesting and even misleading.
    
    Add an action to the content merge viewer's toolbar to allow the user
    to toggle this setting for three-way comparisons.
    
    Bug: 441149
    Change-Id: I154cbecd445ef4481a1288c87c0e6e9cf498651f
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/merge/MergeDiffNode.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/merge/MergeDiffNode.java
@@ -1,28 +1,26 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2021 IBM Corporation and others.
+ * Copyright (C) 2021 Thomas Wolf <thomas.wolf@paranor.ch> and others.
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-package org.eclipse.egit.ui.internal.revision;
+package org.eclipse.egit.ui.internal.merge;
 
 import org.eclipse.compare.ITypedElement;
-import org.eclipse.compare.structuremergeviewer.DiffNode;
 import org.eclipse.compare.structuremergeviewer.IDiffContainer;
+import org.eclipse.egit.ui.internal.revision.NotifiableDiffNode;
 
 /**
- * A {@link DiffNode} with a public {@link #fireChange()} method.
+ * A {@link NotifiableDiffNode} specific to the Egit merge editor.
  */
-public class NotifiableDiffNode extends DiffNode {
+public class MergeDiffNode extends NotifiableDiffNode {
 
 	/**
-	 * Creates a new {@link NotifiableDiffNode} and initializes with the given
+	 * Creates a new {@link MergeDiffNode} and initializes with the given
 	 * values.
 	 *
 	 * @param parent
@@ -37,16 +35,11 @@ public class NotifiableDiffNode extends DiffNode {
 	 *            the left input to a compare
 	 * @param right
 	 *            the right input to a compare
-	 * @see DiffNode#DiffNode(IDiffContainer, int, ITypedElement, ITypedElement,
-	 *      ITypedElement)
+	 * @see org.eclipse.compare.structuremergeviewer.DiffNode#DiffNode(IDiffContainer,
+	 *      int, ITypedElement, ITypedElement, ITypedElement)
 	 */
-	public NotifiableDiffNode(IDiffContainer parent, int kind,
+	public MergeDiffNode(IDiffContainer parent, int kind,
 			ITypedElement ancestor, ITypedElement left, ITypedElement right) {
 		super(parent, kind, ancestor, left, right);
-	}
-
-	@Override
-	public void fireChange() {
-		super.fireChange();
 	}
 }
