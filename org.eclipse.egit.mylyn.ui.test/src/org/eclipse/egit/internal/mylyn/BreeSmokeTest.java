fafd9518aa2d72abdf6cafc1e0cc004c8b4a42e3
Thomas Wolf <thomas.wolf@paranor.ch>
2021-06-13 19:05:43 +0200

    [releng] Force --release and use BREE to run tests
    Make the EGit build work if code is compiled with a Java > 1.8.
    This prepares for moving the BREE to Java 11: when we do that, we
    will need to be able to build nightly with and against Java 11, but
    the stable branch against Java 1.8.
    
    The downside is that for a local maven build, you'll need a
    $HOME/.m2/toolchains.xml file providing JavaSE-1.8 and JavaSE-11.
    
    The CI pods do provide such toolchain definitions for maven.
    
    Once this change is submitted and merged to master the EGit CI
    pipelines will be changed to no longer fix the Java version at
    1.8.
    
    Change-Id: I5f4c3ea944861d1c96dbbf8e4c2849de50b25219
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

---
+++ org.eclipse.egit.mylyn.ui.test/src/org/eclipse/egit/internal/mylyn/BreeSmokeTest.java
@@ -0,0 +1,20 @@
+package org.eclipse.egit.internal.mylyn;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+
+import org.junit.Test;
+
+public class BreeSmokeTest {
+
+	@Test
+	public void testByteBuffer() {
+		// This test will fail if compiled with target=1.8 against a Java 11
+		// library and then run on a Java 8 JVM with Java 8 libraries
+		ByteBuffer buffer = ByteBuffer.allocate(10);
+		Buffer flipped = buffer.flip();
+		assertNotNull(flipped);
+	}
+}
