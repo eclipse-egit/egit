eclipse-egit/egit

commit b03a182aaeecc893ca8e1dc676235e836de8c856

Generify remaining getAdapter() methods
Some methods were not generified yet in Eclipse Neon, but are now.

Change-Id: I3376084fe7f7cd8d82cfdaaa4f4e406b4ed327e7
Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/merge/AbstractGitCompareEditorInput.java
@@ -137,14 +137,14 @@ public abstract class AbstractGitCompareEditorInput extends CompareEditorInput {
 	}
 
 	@Override
-	public Object getAdapter(Class adapter) {
+	public <T> T getAdapter(Class<T> adapter) {
 		if ((adapter == IFile.class || adapter == IResource.class)
 				&& !isMultiFile()) {
 			ITypedElement element = getElement();
 			IResource resource = getResource(element);
 			if (resource != null && adapter.isInstance(resource)
 					&& resource.exists()) {
-				return resource;
+				return adapter.cast(resource);
 			}
 		} else if (adapter == IShowInSource.class && isMultiFile()) {
 			DiffNode node = getNode();
@@ -152,19 +152,21 @@ public abstract class AbstractGitCompareEditorInput extends CompareEditorInput {
 				FolderNode folder = (FolderNode) node;
 				IContainer container = folder.getContainer();
 				if (container != null) {
-					return getShowInSource(new ShowInContext(this,
+					return adapter.cast(new ShowInContext(this,
 							new StructuredSelection(container)));
 				}
 				IPath path = folder.getPath();
 				if (path != null) {
 					if (path.isAbsolute()) {
-						return getShowInSource(new ShowInContext(this,
-								new StructuredSelection(path)));
+						return adapter
+								.cast(getShowInSource(new ShowInContext(this,
+										new StructuredSelection(path))));
 					} else {
 						GitInfo info = getGitInfo(path);
 						if (info != null) {
-							return getShowInSource(new ShowInContext(this,
-									new StructuredSelection(info)));
+							return adapter.cast(
+									getShowInSource(new ShowInContext(this,
+											new StructuredSelection(info))));
 						}
 					}
 				}
@@ -172,8 +174,8 @@ public abstract class AbstractGitCompareEditorInput extends CompareEditorInput {
 				ITypedElement element = node.getLeft();
 				IResource resource = getResource(element);
 				if (resource instanceof IFile && resource.exists()) {
-					return getShowInSource(new ShowInContext(this,
-							new StructuredSelection(resource)));
+					return adapter.cast(getShowInSource(new ShowInContext(this,
+							new StructuredSelection(resource))));
 				}
 				GitInfo info = Adapters.adapt(element, GitInfo.class);
 				if (info != null && info.getRepository() != null) {
@@ -182,21 +184,22 @@ public abstract class AbstractGitCompareEditorInput extends CompareEditorInput {
 						File f = new File(repository.getWorkTree(),
 								path.toOSString());
 						if (f.exists()) {
-							return getShowInSource(new ShowInContext(this,
+							return adapter.cast(
+									getShowInSource(new ShowInContext(this,
 									new StructuredSelection(Path.fromOSString(
-											f.getAbsolutePath()))));
+													f.getAbsolutePath())))));
 						}
 					}
 					// The repository is bare, or the path does not exist in the
 					// working tree. The history page can deal with these paths,
 					// so at least "Show in->History" should work.
-					return getShowInSource(new ShowInContext(this,
-							new StructuredSelection(info)));
+					return adapter.cast(getShowInSource(new ShowInContext(this,
+							new StructuredSelection(info))));
 				}
 			}
-			return getShowInSource(null);
+			return adapter.cast(getShowInSource(null));
 		} else if (adapter == Repository.class) {
-			return repository;
+			return adapter.cast(adapter);
 		}
 		return super.getAdapter(adapter);
 	}

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/revision/GitCompareFileRevisionEditorInput.java
@@ -361,14 +361,14 @@ public class GitCompareFileRevisionEditorInput extends SaveableCompareEditorInpu
 	 * @see org.eclipse.compare.CompareEditorInput#getAdapter(java.lang.Class)
 	 */
 	@Override
-	public Object getAdapter(Class adapter) {
+	public <T> T getAdapter(Class<T> adapter) {
 		if (adapter == IFile.class || adapter == IResource.class) {
 			if (left instanceof LocalNonWorkspaceTypedElement) {
 				return null;
 			}
-			return getResource();
+			return adapter.cast(getResource());
 		} else if (adapter == Repository.class && left != null) {
-			return Adapters.adapt(left, Repository.class);
+			return Adapters.adapt(left, adapter);
 		}
 		return super.getAdapter(adapter);
 	}

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/synchronize/compare/LocalNonWorkspaceTypedElement.java
@@ -388,15 +388,14 @@ public class LocalNonWorkspaceTypedElement extends LocalResourceTypedElement
 	}
 
 	@Override
-	public Object getAdapter(Class adapter) {
+	public <T> T getAdapter(Class<T> adapter) {
 		if (adapter == ISharedDocumentAdapter.class) {
 			if (isSharedDocumentsEnable()) {
-				return getSharedDocumentAdapter();
+				return adapter.cast(getSharedDocumentAdapter());
 			}
 			return null;
 		}
-		return Platform.getAdapterManager().getAdapter(this,
-				(Class<?>) adapter);
+		return Platform.getAdapterManager().getAdapter(this, adapter);
 	}
 
 	@Override

commit 0aee49637c6fa6508f7185f61d4f9759789ebd3e

Fix auto-completion gone wrong
Introduced in commit b03a182a. Looks like that broken autocompletion on
newer Eclipses is to blame (well, and me for not noticing it).

Bug: 576838
Change-Id: I0fe0b21d92e205847866c4fb975a72c129aec7c9
Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/merge/AbstractGitCompareEditorInput.java
@@ -199,7 +199,7 @@ public abstract class AbstractGitCompareEditorInput extends CompareEditorInput {
 			}
 			return adapter.cast(getShowInSource(null));
 		} else if (adapter == Repository.class) {
-			return adapter.cast(adapter);
+			return adapter.cast(repository);
 		}
 		return super.getAdapter(adapter);
 	}