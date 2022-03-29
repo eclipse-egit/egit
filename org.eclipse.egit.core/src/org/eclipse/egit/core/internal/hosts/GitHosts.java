f1d614e1e389db963a7c51e3b270da8d31304934
Thomas Wolf <thomas.wolf@paranor.ch>
2022-03-29 19:15:40 +0200

    [fetch pr] Relax remote URI pattern
    This pattern is used only to match against remote URIs from git config
    remote.<name>.fetch. It is not necessary to ensure it ends in ".git";
    servers may well allow access without that file extension.
    
    Bug: 579477
    Change-Id: Ieead00574cb0c26248aac8ea2e78a6c096dced67
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>

--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/hosts/GitHosts.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/hosts/GitHosts.java
@@ -52,7 +52,7 @@ public final class GitHosts {
 	private static Pattern remote(String hosts) {
 		return Pattern
 				.compile("(?:(?:https?|ssh)://)?(?:[^@:/]+(?::[^@:/]*)?@)?(?:" //$NON-NLS-1$
-						+ hosts + ")[:/][^/]+/.*\\.git"); //$NON-NLS-1$
+						+ hosts + ")[:/].*"); //$NON-NLS-1$
 	}
 
 	static {
