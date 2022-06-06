03c005a48cadc2ade5436bae6cdbae11c12546fc
Simeon Andreev <simeon.danailov.andreev@gmail.com>
2022-06-06 15:06:23 +0200

    Fixed reading config for diff or merge tool, externalized strings
    The repository configuration is not read when calling an external diff
    or merge tool. This change fixes this and adds tests for the case.
    
    Furthermore this change fixes using .gitattributes for overriding a
    default mergetool.
    
    And last, various exception and dialog messages are now externalized.
    
    Bug: 552838
    Change-Id: I2be87f498f475cbbd9ec867bcf23b114063abd78
    Signed-off-by: Simeon Andreev <simeon.danailov.andreev@gmail.com>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/internal/external/tools/ExternalDiffToolUiTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/internal/external/tools/ExternalDiffToolUiTest.java
@@ -10,6 +10,12 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.external.tools;
 
+import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_DIFFTOOL_SECTION;
+import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_DIFF_SECTION;
+import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_KEY_CMD;
+import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_KEY_PROMPT;
+import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_KEY_TOOL;
+import static org.eclipse.jgit.lib.ConfigConstants.CONFIG_KEY_TRUST_EXIT_CODE;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
@@ -19,6 +25,7 @@ import org.eclipse.egit.ui.UIPreferences;
 import org.eclipse.egit.ui.internal.diffmerge.DiffToolMode;
 import org.eclipse.egit.ui.test.ContextMenuHelper;
 import org.eclipse.egit.ui.test.TestUtil;
+import org.eclipse.jgit.lib.StoredConfig;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.junit.After;
@@ -30,9 +37,11 @@ import org.junit.Test;
  */
 public class ExternalDiffToolUiTest extends ExternalToolUiTestCase {
 
-	private int previousDiffToolModePreference;
-
 	private String previousPerExtensionPreference;
+
+	private String previousDiffToolCustomPreference;
+
+	private int previousDiffToolModePreference;
 
 	@Override
 	@Before
@@ -40,6 +49,8 @@ public class ExternalDiffToolUiTest extends ExternalToolUiTestCase {
 		super.setUp();
 		previousPerExtensionPreference = preferenceNode
 				.get(UIPreferences.EXTERNAL_DIFF_TOOL_FOR_EXTENSION, "");
+		previousDiffToolCustomPreference = preferenceNode
+				.get(UIPreferences.DIFF_TOOL_CUSTOM, "");
 		previousDiffToolModePreference = preferenceNode.getInt(
 				UIPreferences.DIFF_TOOL_MODE, DiffToolMode.INTERNAL.ordinal());
 	}
@@ -50,6 +61,8 @@ public class ExternalDiffToolUiTest extends ExternalToolUiTestCase {
 		try {
 			preferenceNode.put(UIPreferences.EXTERNAL_DIFF_TOOL_FOR_EXTENSION,
 					previousPerExtensionPreference);
+			preferenceNode.put(UIPreferences.DIFF_TOOL_CUSTOM,
+					previousDiffToolCustomPreference);
 			preferenceNode.putInt(UIPreferences.DIFF_TOOL_MODE,
 					previousDiffToolModePreference);
 		} finally {
@@ -58,7 +71,7 @@ public class ExternalDiffToolUiTest extends ExternalToolUiTestCase {
 	}
 
 	@Test
-	public void testExternalDiffTool() throws Exception {
+	public void testExternalDiffToolPreference() throws Exception {
 		assumePosixPlatform();
 
 		preferenceNode.putInt(UIPreferences.DIFF_TOOL_MODE,
@@ -83,6 +96,70 @@ public class ExternalDiffToolUiTest extends ExternalToolUiTestCase {
 				+ actualCommandOutput, matchingOutput);
 	}
 
+	@Test
+	public void testExternalDiffToolGitConfig() throws Exception {
+		assumePosixPlatform();
+
+		createMergeConflict();
+
+		int runs = 2;
+		for (int i = 0; i < runs; ++i) {
+			clearResultFile();
+
+			String toolName = "custom_tool" + i;
+			configureEchoTools(toolName);
+			preferenceNode.putInt(UIPreferences.DIFF_TOOL_MODE,
+					DiffToolMode.GIT_CONFIG.ordinal());
+			preferenceNode.put(UIPreferences.DIFF_TOOL_CUSTOM, toolName);
+
+			triggerCompareWithPreviousAction();
+
+			List<String> commandOutputLines = waitForToolOutput();
+
+			String actualCommandOutput = String.join(System.lineSeparator(),
+					commandOutputLines);
+			String expectedOutputPattern = toolName
+					+ " local=.*.txt remote=.*.txt";
+			boolean matchingOutput = Pattern.matches(expectedOutputPattern,
+					actualCommandOutput);
+			assertTrue("Command output doesn't match expected pattern: "
+					+ expectedOutputPattern + ", command output: "
+					+ actualCommandOutput, matchingOutput);
+		}
+	}
+
+	@Test
+	public void testExternalDiffToolGitAttributesOverride() throws Exception {
+		assumePosixPlatform();
+
+		String toolNameGitConfig = "custom_tool_git_config";
+		String toolNameGitAttributes = "custom_tool_git_attributes";
+		configureEchoTools(toolNameGitConfig, toolNameGitAttributes);
+		preferenceNode.putInt(UIPreferences.DIFF_TOOL_MODE,
+				DiffToolMode.GIT_CONFIG.ordinal());
+		preferenceNode.put(UIPreferences.DIFF_TOOL_CUSTOM, toolNameGitConfig);
+
+		String gitAttributesContents = "atttributes:" + "\n" + "*.txt" + "\t"
+				+ "difftool=" + toolNameGitAttributes;
+		writeFolderGitAttributes(gitAttributesContents);
+
+		createMergeConflict();
+		triggerCompareWithPreviousAction();
+
+		List<String> commandOutputLines = waitForToolOutput();
+
+		String actualCommandOutput = String.join(System.lineSeparator(),
+				commandOutputLines);
+		// expect the command from the tool in the git attributes
+		String expectedOutputPattern = toolNameGitAttributes
+				+ " local=.*.txt remote=.*.txt";
+		boolean matchingOutput = Pattern.matches(expectedOutputPattern,
+				actualCommandOutput);
+		assertTrue("Command output doesn't match expected pattern: "
+				+ expectedOutputPattern + ", command output: "
+				+ actualCommandOutput, matchingOutput);
+	}
+
 	private void triggerCompareWithPreviousAction() {
 		SWTBotTree packageExplorer = TestUtil.getExplorerTree();
 		SWTBotTreeItem project1 = getProjectItem(packageExplorer, PROJ1)
@@ -97,5 +174,46 @@ public class ExternalDiffToolUiTest extends ExternalToolUiTestCase {
 				util.getPluginLocalizedValue("CompareWithMenu.label"),
 				util.getPluginLocalizedValue(
 						"CompareWithPreviousAction.label"));
+	}
+
+	private void configureEchoTools(String defaultToolName,
+			String... extraToolNames) throws Exception {
+		configureTools(this::configureDefaultTool,
+				this::configureEchoToolSubsection, defaultToolName,
+				extraToolNames);
+	}
+
+	private void configureDefaultTool(String defaultToolName,
+			StoredConfig config) {
+		// the default diff tool is configured without a subsection
+		String subsection = null;
+		config.setString(CONFIG_DIFF_SECTION, subsection, CONFIG_KEY_TOOL,
+				defaultToolName);
+	}
+
+	private void configureEchoToolSubsection(String toolName,
+			StoredConfig config) {
+		String command = getEchoCommand(toolName);
+
+		config.setString(CONFIG_DIFFTOOL_SECTION, toolName, CONFIG_KEY_CMD,
+				command);
+		config.setString(CONFIG_DIFFTOOL_SECTION, toolName,
+				CONFIG_KEY_TRUST_EXIT_CODE, String.valueOf(Boolean.TRUE));
+		/*
+		 * prevent prompts as we are running in tests and there is no user to
+		 * interact with on the command line
+		 */
+		config.setString(CONFIG_DIFFTOOL_SECTION, toolName, CONFIG_KEY_PROMPT,
+				String.valueOf(false));
+	}
+
+	private String getEchoCommand(String toolName) {
+		/*
+		 * Use 'MERGED' placeholder, as both 'LOCAL' and 'REMOTE' will be
+		 * replaced with full paths to a temporary file during some of the
+		 * tests.
+		 */
+		return "(echo " + toolName + "  local=\"$LOCAL\" remote=\"$REMOTE\" > "
+				+ resultFile.toAbsolutePath().toString() + ")";
 	}
 }
