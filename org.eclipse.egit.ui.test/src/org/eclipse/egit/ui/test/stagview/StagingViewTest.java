b6140041fff1bae81ad19e3b44bdb7ff44d5697c
Thomas Wolf <twolf@apache.org>
2022-11-12 14:50:16 +0100

    Use rename detection in some comparisons
    Use rename detection for comparisons of a single file between index and
    HEAD, or between a commit and an immediate parent.
    
    Rename detection in general is an O(A * D) process, where A is the
    number of added files and D is the number of deleted files. If copy
    detection is done, it becomes O((A+M) * (D+M)), where M is the number
    of modified files. But if one is interested in only a single path,
    it can be done in O(D) or O(D+M).
    
    Bug: 581030
    Change-Id: Ie873a14a14c768a2697fd4b16517e0a9aaeb4a40
    Signed-off-by: Thomas Wolf <twolf@apache.org>

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/stagview/StagingViewTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/stagview/StagingViewTest.java
@@ -23,6 +23,7 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.egit.core.JobFamilies;
 import org.eclipse.egit.core.internal.indexdiff.IndexDiffCache;
+import org.eclipse.egit.ui.common.CompareEditorTester;
 import org.eclipse.egit.ui.common.StagingViewTester;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.actions.ReplaceConflictActionHandler;
@@ -400,6 +401,96 @@ public class StagingViewTest extends AbstractStagingViewTestCase {
 
 		assertEquals(expectedMessage,
 				TestUtil.getHeadCommit(repository).getShortMessage());
+	}
+
+	/**
+	 * Tests that a staged rename compares against the original file.
+	 *
+	 * @throws Exception
+	 *             on errors
+	 */
+	@Test
+	public void testCompareRenamedFileStaged() throws Exception {
+		IFile file = ResourcesPlugin.getWorkspace().getRoot().getProject(PROJ1)
+				.getFolder(FOLDER).getFile(FILE1);
+		String moved = "moved.txt";
+		String movedPath = PROJ1 + '/' + FOLDER + '/' + moved;
+		try (Git git = new Git(repository)) {
+			assertTrue(file.exists());
+			file.move(file.getParent().getFullPath().append(moved), true,
+					null);
+			assertFalse(file.exists());
+			git.rm().addFilepattern(FILE1_PATH).call();
+			git.add().addFilepattern(movedPath).call();
+			TestUtil.joinJobs(JobFamilies.INDEX_DIFF_CACHE_UPDATE);
+		}
+		StagingViewTester stagingView = StagingViewTester.openStagingView();
+		SWTBotView view = stagingView.getView();
+		SWTBot viewBot = view.bot();
+		SWTBotTree stagedTree = viewBot.tree(1);
+		TestUtil.waitUntilTreeHasNodeContainsText(viewBot, stagedTree,
+				movedPath, 10000);
+		SWTBotTreeItem item = TestUtil.getNode(stagedTree.getAllItems(),
+				movedPath);
+		item.select();
+		ContextMenuHelper.clickContextMenu(stagedTree,
+				UIText.StagingView_CompareWithHeadMenuLabel);
+
+		CompareEditorTester compareEditor = CompareEditorTester
+				.withTitleContaining("Compare " + moved);
+		String leftText = compareEditor.getLeftEditor().getText();
+		String rightText = compareEditor.getRightEditor().getText();
+		assertEquals(leftText, rightText);
+	}
+
+	/**
+	 * Tests that a staged copy compares against the original file.
+	 *
+	 * @throws Exception
+	 *             on errors
+	 */
+	@Test
+	public void testCompareCopiedFileStaged() throws Exception {
+		IFile file = ResourcesPlugin.getWorkspace().getRoot().getProject(PROJ1)
+				.getFolder(FOLDER).getFile(FILE1);
+		String moved = "moved.txt";
+		String movedPath = PROJ1 + '/' + FOLDER + '/' + moved;
+		try (Git git = new Git(repository)) {
+			assertTrue(file.exists());
+			file.move(file.getParent().getFullPath().append(moved), true, null);
+			assertFalse(file.exists());
+			touch("New content");
+			assertTrue(file.exists());
+			git.add().addFilepattern(movedPath).addFilepattern(FILE1_PATH)
+					.call();
+			TestUtil.joinJobs(JobFamilies.INDEX_DIFF_CACHE_UPDATE);
+		}
+		StagingViewTester stagingView = StagingViewTester.openStagingView();
+		SWTBotView view = stagingView.getView();
+		SWTBot viewBot = view.bot();
+		SWTBotTree stagedTree = viewBot.tree(1);
+		TestUtil.waitUntilTreeHasNodeContainsText(viewBot, stagedTree,
+				movedPath, 10000);
+		SWTBotTreeItem item = TestUtil.getNode(stagedTree.getAllItems(),
+				movedPath);
+		item.select();
+		ContextMenuHelper.clickContextMenu(stagedTree,
+				UIText.StagingView_CompareWithHeadMenuLabel);
+
+		CompareEditorTester compareEditor = CompareEditorTester
+				.withTitleContaining("Compare " + moved);
+		String leftText = compareEditor.getLeftEditor().getText();
+		String rightText = compareEditor.getRightEditor().getText();
+		assertEquals(leftText, rightText);
+		item = TestUtil.getNode(stagedTree.getAllItems(), FILE1_PATH);
+		item.select();
+		ContextMenuHelper.clickContextMenu(stagedTree,
+				UIText.StagingView_CompareWithHeadMenuLabel);
+
+		compareEditor = CompareEditorTester
+				.forTitleContaining("Compare " + FILE1);
+		leftText = compareEditor.getLeftEditor().getText();
+		assertEquals("New content", leftText);
 	}
 
 	private StagingViewTester commitOneFileChange(String fileContent)
