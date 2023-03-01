5bec99b7462e682eb5c4d07e4ecea5bb9ed8d40b
Matthias Sohn <matthias.sohn@sap.com>
2023-03-01 02:21:51 +0100

    Adapt to JGit API changes
    Adapt to JGit's modified ProgressMonitor API adding performance trace.
    
    Adapt to JGit 0518a6b0 following git 2.34 which replaced
    pull.rebase=preserve by pull.rebase=merges.
    
    Change-Id: Ic70d26ae18ac1a8b3605edfa11ccc1389fae2989

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/RebaseTargetSelectionDialog.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/dialogs/RebaseTargetSelectionDialog.java
@@ -141,7 +141,7 @@ public class RebaseTargetSelectionDialog extends AbstractBranchSelectionDialog {
 				ConfigConstants.CONFIG_BRANCH_SECTION, branchName,
 				ConfigConstants.CONFIG_KEY_REBASE, BranchRebaseMode.NONE);
 		switch (rebase) {
-		case PRESERVE:
+		case MERGES:
 			preserveMergesButton.setSelection(true);
 			preserveMerges = true;
 			break;
