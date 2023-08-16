ffbf85d4cc5543be2347ca016f2cf75ccecd7551
Jörg Kubitz <jkubitz-eclipse@gmx.de>
2023-08-16 16:17:25 +0200

    [staging] performance: cache RepositoryState
    When repository is in merging mode getRepositoryState() can be very
    slow.
    
    Bug: 578768
    Change-Id: I3ed90bd84a650eb8b88f6748e84dfba60769566e

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/staging/LazyCachedSupplier.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (C) 2023 SSI (Joerg Kubitz) and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.staging;
+
+import java.util.function.Supplier;
+
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.RepositoryState;
+
+class LazyRepositoryState implements Supplier<RepositoryState> {
+	private final Repository repo;
+
+	private RepositoryState state;
+
+	LazyRepositoryState(Repository repo) {
+		this.repo = repo;
+	}
+
+    @Override
+	public RepositoryState get() {
+		if (state == null) {
+			state = repo == null ? null : repo.getRepositoryState();
+        }
+		return state;
+	}
+
+	Repository getRepository() {
+		return repo;
+    }
+}
