566b5fd11ea812bc20172bb4a3a6cb2d2a8f9637
Matthias Sohn <matthias.sohn@sap.com>
2024-11-11 17:02:42 +0100

    EclipseSystemReader: extend SystemReader.Delegate
    to inherit the new SystemReader method implementations and simplify the
    implementation.
    
    Change-Id: I34089f7b0ef0acbde30f016269345f36166d3e5e

--- org.eclipse.egit.core/src/org/eclipse/egit/core/internal/EclipseSystemReader.java
+++ org.eclipse.egit.core/src/org/eclipse/egit/core/internal/EclipseSystemReader.java
@@ -10,22 +10,15 @@
  *******************************************************************************/
 package org.eclipse.egit.core.internal;
 
-import java.io.IOException;
-
 import org.eclipse.jgit.annotations.NonNull;
-import org.eclipse.jgit.errors.ConfigInvalidException;
-import org.eclipse.jgit.lib.Config;
 import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.StoredConfig;
-import org.eclipse.jgit.storage.file.FileBasedConfig;
-import org.eclipse.jgit.util.FS;
 import org.eclipse.jgit.util.SystemReader;
 
 /**
  * A system reader that hides certain global git environment variables from
  * JGit.
  */
-public class EclipseSystemReader extends SystemReader {
+public class EclipseSystemReader extends SystemReader.Delegate {
 
 	/**
 	 * Hide these variables lest JGit tries to use them for different
@@ -37,8 +30,6 @@ public class EclipseSystemReader extends SystemReader {
 			Constants.GIT_INDEX_FILE_KEY,
 			Constants.GIT_ALTERNATE_OBJECT_DIRECTORIES_KEY };
 
-	private final @NonNull SystemReader delegate;
-
 	/**
 	 * Creates a new instance based on the delegate.
 	 *
@@ -46,12 +37,12 @@ public class EclipseSystemReader extends SystemReader {
 	 *            to use
 	 */
 	public EclipseSystemReader(@NonNull SystemReader delegate) {
-		this.delegate = delegate;
+		super(delegate);
 	}
 
 	@Override
 	public String getenv(String variable) {
-		String result = delegate.getenv(variable);
+		String result = super.getenv(variable);
 		if (result == null) {
 			return result;
 		}
@@ -63,58 +54,5 @@ public class EclipseSystemReader extends SystemReader {
 			}
 		}
 		return result;
-	}
-
-	@Override
-	public String getHostname() {
-		return delegate.getHostname();
-	}
-
-	@Override
-	public String getProperty(String key) {
-		return delegate.getProperty(key);
-	}
-
-	@Override
-	public FileBasedConfig openUserConfig(Config parent, FS fs) {
-		return delegate.openUserConfig(parent, fs);
-	}
-
-	@Override
-	public FileBasedConfig openJGitConfig(Config parent, FS fs) {
-		return delegate.openJGitConfig(parent, fs);
-	}
-
-	@Override
-	public FileBasedConfig openSystemConfig(Config parent, FS fs) {
-		return delegate.openSystemConfig(parent, fs);
-	}
-
-	@Override
-	public long getCurrentTime() {
-		return delegate.getCurrentTime();
-	}
-
-	@Override
-	public int getTimezone(long when) {
-		return delegate.getTimezone(when);
-	}
-
-	@Override
-	public StoredConfig getUserConfig()
-			throws IOException, ConfigInvalidException {
-		return delegate.getUserConfig();
-	}
-
-	@Override
-	public StoredConfig getJGitConfig()
-			throws IOException, ConfigInvalidException {
-		return delegate.getJGitConfig();
-	}
-
-	@Override
-	public StoredConfig getSystemConfig()
-			throws IOException, ConfigInvalidException {
-		return delegate.getSystemConfig();
 	}
 }
