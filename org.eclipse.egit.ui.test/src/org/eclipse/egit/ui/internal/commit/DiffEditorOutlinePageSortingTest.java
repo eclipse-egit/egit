c642ab166b702aec92b2cabdaffd0ae52c6cbc6c
Thomas Wolf <twolf@apache.org>
2026-04-03 12:36:19 +0200

    Add a missing copyright header in a test class
    Change-Id: I5077d65d82e1b07751912f4cc67fc833b53c9a65

--- org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/internal/commit/DiffEditorOutlinePageSortingTest.java
+++ org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/internal/commit/DiffEditorOutlinePageSortingTest.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Thomas Wolf <twolf@apache.org> and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
 package org.eclipse.egit.ui.internal.commit;
 
 import static org.junit.Assert.assertArrayEquals;
