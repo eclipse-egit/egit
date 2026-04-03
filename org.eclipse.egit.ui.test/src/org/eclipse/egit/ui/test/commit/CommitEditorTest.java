eclipse-egit/egit

commit 0e458fca7694e065b4ae74fb2e228c45137f1f74

DiffEditorOutlinePage: use dynamic menu with command handlers
Reuse the FileDiff command handlers introduced for the
CommitFileDiffViewer also in the DiffEditorOutlinePage.

This removes the dynamic menu item label (showing the commit id), and
also the menu tooltips showing the commit subject. But it enables us
to reuse the existing code as is.

Change-Id: I1d7b89ef86a7039deb1e24fc423a32125f560915
Signed-off-by: Thomas Wolf <twolf@apache.org>

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commit/DiffEditorOutlinePage.java
@@ -10,27 +10,21 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.internal.commit;
 
-import java.io.File;
-import java.text.MessageFormat;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.stream.Collectors;
 
-import org.eclipse.core.runtime.Path;
-import org.eclipse.egit.core.internal.Utils;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.UIPreferences;
 import org.eclipse.egit.ui.UIUtils;
 import org.eclipse.egit.ui.internal.UIIcons;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.commit.DiffRegionFormatter.FileDiffRegion;
-import org.eclipse.egit.ui.internal.history.CommitFileDiffViewer.CheckoutAction;
+import org.eclipse.egit.ui.internal.history.CommitFileDiffViewer;
 import org.eclipse.egit.ui.internal.history.FileDiff;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
@@ -65,9 +59,6 @@ import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerComparator;
 import org.eclipse.jface.viewers.ViewerFilter;
-import org.eclipse.jgit.diff.DiffEntry;
-import org.eclipse.jgit.lib.RepositoryState;
-import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.util.StringUtils;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.KeyAdapter;
@@ -86,6 +77,7 @@ import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.IWorkbenchCommandConstants;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.SearchPattern;
@@ -97,6 +89,8 @@ import org.eclipse.ui.views.contentoutline.ContentOutlinePage;
  * for {@link DiffDocument}s.
  */
 public class DiffEditorOutlinePage extends ContentOutlinePage {
+
+	private static final String POPUP_ID = "org.eclipse.egit.ui.commit.OutlineContributions"; //$NON-NLS-1$
 
 	static final Comparator<String> CMP = (left, right) -> {
 		String l = left.startsWith("/") ? left.substring(1) : left; //$NON-NLS-1$
@@ -235,161 +229,13 @@ public class DiffEditorOutlinePage extends ContentOutlinePage {
 		contextMenu.setRemoveAllWhenShown(true);
 		contextMenu.addMenuListener(menuManager -> {
 			setFocus();
-			Collection<FileDiffRegion> selected = getSelectedFileDiffs();
-			if (selected.isEmpty()) {
-				return;
-			}
-			List<FileDiffRegion> haveNew = selected.stream()
-					.filter(diff -> !diff.getDiff().getChange()
-							.equals(DiffEntry.ChangeType.DELETE))
-					.collect(Collectors.toList());
-			List<FileDiffRegion> haveOld = selected.stream()
-					.filter(diff -> !diff.getDiff().getChange()
-							.equals(DiffEntry.ChangeType.ADD))
-					.collect(Collectors.toList());
-			List<FileDiffRegion> existing = haveNew.stream()
-					.filter(diff -> new Path(diff.getDiff().getRepository()
-							.getWorkTree().getAbsolutePath())
-									.append(diff.getDiff().getNewPath())
-									.toFile().exists())
-					.collect(Collectors.toList());
-			if (!existing.isEmpty()) {
-				menuManager.add(new Action(
-						UIText.CommitFileDiffViewer_OpenWorkingTreeVersionInEditorMenuLabel) {
-
-					@Override
-					public void run() {
-						for (FileDiffRegion fileDiff : existing) {
-							File file = new Path(
-									fileDiff.getDiff().getRepository()
-											.getWorkTree().getAbsolutePath())
-													.append(fileDiff.getDiff()
-															.getNewPath())
-													.toFile();
-							DiffViewer.openFileInEditor(file, -1);
-						}
-					}
-				});
-			}
-			if (!haveNew.isEmpty()) {
-				RevCommit commit = haveNew.get(0).getDiff().getCommit();
-				String title = MessageFormat.format(
-						UIText.CommitFileDiffViewer_OpenInEditorMenuWithCommitLabel,
-						Utils.getShortObjectId(commit));
-				String tooltip = MessageFormat.format(
-						UIText.CommitFileDiffViewer_OpenInEditorMenuTooltip,
-						UIUtils.menuText(commit.getShortMessage(), 80));
-				IAction action = new Action(title) {
-
-					@Override
-					public void run() {
-						for (FileDiffRegion fileDiff : haveNew) {
-							DiffViewer.openInEditor(fileDiff.getDiff(),
-									DiffEntry.Side.NEW, -1);
-						}
-					}
-				};
-				if (tooltip != null) {
-					action.setToolTipText(tooltip);
-				}
-				menuManager.add(action);
-			}
-			if (!haveOld.isEmpty()) {
-				FileDiff diff = haveOld.get(0).getDiff();
-				RevCommit commit = diff.getCommit();
-				RevCommit base = diff.getBase();
-				String msg;
-				if (base == null || base.equals(commit.getParent(0))) {
-					msg = UIText.CommitFileDiffViewer_OpenPreviousInEditorMenuWithCommitLabel;
-					if (base == null) {
-						base = commit.getParent(0);
-					}
-				} else {
-					msg = UIText.CommitFileDiffViewer_OpenBaseInEditorMenuWithCommitLabel;
-				}
-
-				String title = MessageFormat.format(msg,
-						Utils.getShortObjectId(base));
-				String tooltip = MessageFormat.format(
-						UIText.CommitFileDiffViewer_OpenInEditorMenuTooltip,
-						UIUtils.menuText(base.getShortMessage(), 80));
-				IAction action = new Action(title) {
-
-					@Override
-					public void run() {
-						for (FileDiffRegion fileDiff : haveOld) {
-							DiffViewer.openInEditor(fileDiff.getDiff(),
-									DiffEntry.Side.OLD, -1);
-						}
-					}
-				};
-				if (tooltip != null) {
-					action.setToolTipText(tooltip);
-				}
-				menuManager.add(action);
-			}
-			if (!haveNew.isEmpty()) {
-				boolean hasFiles = haveNew.stream()
-						.anyMatch(d -> !d.getDiff().isSubmodule());
-				if (hasFiles) {
-					menuManager.add(new Separator());
-					CheckoutAction action = new CheckoutAction(
-							this::getStructuredSelection);
-					menuManager.add(action);
-					action.setEnabled(haveNew.iterator().next().getDiff()
-							.getRepository().getRepositoryState()
-							.equals(RepositoryState.SAFE));
-				}
-			}
-			if (selected.size() == 1 && !haveNew.isEmpty()
-					&& !haveOld.isEmpty()) {
-				// "Compare with previous" makes only sense if there are
-				// both a new and a previous version.
-				FileDiff diff = haveNew.get(0).getDiff();
-				RevCommit base = diff.getBase();
-				String title;
-				if (base == null
-						|| base.equals(diff.getCommit().getParent(0))) {
-					title = UIText.CommitFileDiffViewer_CompareMenuLabel;
-				} else {
-					title = UIText.CommitFileDiffViewer_CompareSideBySideMenuLabel;
-				}
-				menuManager.add(new Separator());
-				menuManager.add(new Action(title) {
-
-					@Override
-					public void run() {
-						FileDiffRegion fileDiff = selected.iterator().next();
-						DiffViewer.showTwoWayFileDiff(fileDiff.getDiff());
-					}
-				});
-			}
+			contextMenu.add(new Separator(CommitFileDiffViewer.GROUP_ID));
+			contextMenu
+					.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
 		});
 		Menu menu = contextMenu.createContextMenu(viewer.getTree());
 		viewer.getTree().setMenu(menu);
-	}
-
-	private IStructuredSelection getStructuredSelection() {
-		ISelection currentSelection = getSelection();
-		if (currentSelection instanceof IStructuredSelection) {
-			return (IStructuredSelection) currentSelection;
-		}
-		return StructuredSelection.EMPTY;
-	}
-
-	private Collection<FileDiffRegion> getSelectedFileDiffs() {
-		IStructuredSelection currentSelection = getStructuredSelection();
-		List<FileDiffRegion> result = new ArrayList<>();
-		if (!currentSelection.isEmpty()) {
-			for (Object selected : ((StructuredSelection) currentSelection).toList()) {
-				if (selected instanceof FileDiffRegion
-						&& !((FileDiffRegion) selected).getDiff()
-								.isSubmodule()) {
-					result.add((FileDiffRegion) selected);
-				}
-			}
-		}
-		return result;
+		getSite().registerContextMenu(POPUP_ID, contextMenu, viewer);
 	}
 
 	@Override

org.eclipse.egit.ui/plugin.xml
@@ -3923,6 +3923,39 @@
    <extension
          point="org.eclipse.ui.menus">
       <menuContribution
+            locationURI="popup:org.eclipse.egit.ui.commit.OutlineContributions?after=fileDiff.group">
+         <command
+              commandId="org.eclipse.egit.ui.filediff.OpenWorkingTree"
+              label="%FileDiffOpenWorkingTree.label"
+              style="push" />
+         <command
+              commandId="org.eclipse.egit.ui.history.ShowVersions"
+              label="%FileDiffOpenThisVersion.label"
+              style="push" />
+         <command
+              commandId="org.eclipse.egit.ui.filediff.OpenPrevious"
+              label="%FileDiffOpenPreviousVersion.label"
+              style="push" />
+         <separator name="##Checkout" visible="true" />
+         <command
+              commandId="org.eclipse.egit.ui.filediff.CheckoutNew"
+              label="%FileDiffCheckoutNew.label"
+              style="push" />
+         <separator name="##Compare" visible="true" />
+         <command
+              commandId="org.eclipse.egit.ui.team.CompareWithPrevious"
+              label="%FileDiffCompareWithPrevious.label"
+              style="push" />
+         <command
+              commandId="org.eclipse.egit.ui.history.CompareWithWorkingTree"
+              label="%FileDiffCompareWithWorkingTree.label"
+              style="push" />
+         <command
+              commandId="org.eclipse.egit.ui.team.ShowBlame"
+              label="%FileDiffBlame.label"
+              style="push" />
+      </menuContribution>
+      <menuContribution
             locationURI="popup:org.eclipse.egit.ui.fileDiffContributions?after=fileDiff.group">
          <command
               commandId="org.eclipse.egit.ui.filediff.OpenWorkingTree"

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/UIText.java
@@ -5165,15 +5165,6 @@ public class UIText extends NLS {
 	public static String CommitFileDiffViewer_CanNotOpenCompareEditorTitle;
 
 	/** */
-	public static String CommitFileDiffViewer_CheckoutThisVersionMenuLabel;
-
-	/** */
-	public static String CommitFileDiffViewer_CompareMenuLabel;
-
-	/** */
-	public static String CommitFileDiffViewer_CompareSideBySideMenuLabel;
-
-	/** */
 	public static String CommitFileDiffViewer_CompareWorkingDirectoryMenuLabel;
 
 	/** */
@@ -5184,21 +5175,6 @@ public class UIText extends NLS {
 
 	/** */
 	public static String CommitFileDiffViewer_MergeCommitMultiAncestorMessage;
-
-	/** */
-	public static String CommitFileDiffViewer_OpenBaseInEditorMenuLabel;
-
-	/** */
-	public static String CommitFileDiffViewer_OpenBaseInEditorMenuWithCommitLabel;
-
-	/** */ // TODO: non-functional?
-	public static String CommitFileDiffViewer_OpenInEditorMenuTooltip;
-
-	/** */
-	public static String CommitFileDiffViewer_OpenInEditorMenuWithCommitLabel;
-
-	/** */
-	public static String CommitFileDiffViewer_OpenPreviousInEditorMenuWithCommitLabel;
 
 	/** */
 	public static String CommitFileDiffViewer_ShowInHistoryLabel;

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/CommitFileDiffViewer.java
@@ -23,13 +23,9 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.Adapters;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
@@ -39,9 +35,7 @@ import org.eclipse.core.runtime.jobs.IJobChangeEvent;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.core.runtime.jobs.JobChangeAdapter;
-import org.eclipse.egit.core.internal.job.JobUtil;
 import org.eclipse.egit.core.internal.util.ResourceUtil;
-import org.eclipse.egit.core.op.DiscardChangesOperation;
 import org.eclipse.egit.ui.Activator;
 import org.eclipse.egit.ui.JobFamilies;
 import org.eclipse.egit.ui.UIPreferences;
@@ -50,7 +44,6 @@ import org.eclipse.egit.ui.internal.ActionUtils;
 import org.eclipse.egit.ui.internal.UIIcons;
 import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.commit.DiffViewer;
-import org.eclipse.egit.ui.internal.dialogs.CommandConfirmation;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.action.MenuManager;
@@ -653,70 +646,5 @@ public class CommitFileDiffViewer extends TableViewer {
 			return contains(rule);
 		}
 
-	}
-
-	/**
-	 * An action to check out selected {@link FileDiff}s from the commit.
-	 */
-	public static class CheckoutAction extends Action {
-
-		private final Supplier<IStructuredSelection> selectionProvider;
-
-		/**
-		 * Creates a new {@link CheckoutAction}.
-		 *
-		 * @param selectionProvider
-		 *            to get the selection from
-		 */
-		public CheckoutAction(
-				Supplier<IStructuredSelection> selectionProvider) {
-			super(UIText.CommitFileDiffViewer_CheckoutThisVersionMenuLabel);
-			this.selectionProvider = selectionProvider;
-		}
-
-		@Override
-		public void run() {
-			DiscardChangesOperation operation = createOperation();
-			if (operation == null) {
-				return;
-			}
-			Map<Repository, Collection<String>> paths = operation
-					.getPathsPerRepository();
-			if (!CommandConfirmation.confirmCheckout(null, paths, true)) {
-				return;
-			}
-			JobUtil.scheduleUserWorkspaceJob(operation,
-					UIText.DiscardChangesAction_discardChanges,
-					JobFamilies.DISCARD_CHANGES);
-		}
-
-		private DiscardChangesOperation createOperation() {
-			Collection<FileDiff> diffs = getFileDiffs(selectionProvider.get());
-			if (diffs.isEmpty()) {
-				return null;
-			}
-			FileDiff first = diffs.iterator().next();
-			Repository repository = first.getRepository();
-			String revision = first.getCommit().getName();
-			List<String> paths = diffs.stream().map(FileDiff::getNewPath)
-					.collect(Collectors.toList());
-			DiscardChangesOperation operation = new DiscardChangesOperation(
-					repository, paths);
-			operation.setRevision(revision);
-			return operation;
-		}
-
-		private Collection<FileDiff> getFileDiffs(
-				IStructuredSelection selection) {
-			List<FileDiff> result = new ArrayList<>();
-			for (Object obj : selection.toList()) {
-				FileDiff diff = Adapters.adapt(obj, FileDiff.class);
-				if (diff != null && diff.getChange() != ChangeType.DELETE
-						&& !diff.isSubmodule()) {
-					result.add(diff);
-				}
-			}
-			return result;
-		}
 	}
 }

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/uitext.properties
@@ -1821,19 +1821,10 @@ MultiBranchOperationResultDialog_CheckoutResultOK=Checkout completed normally
 MultiBranchOperationResultDialog_CheckoutResultNotTried=The operation was not executed
 
 CommitFileDiffViewer_CanNotOpenCompareEditorTitle=Cannot Open Compare Editor
-CommitFileDiffViewer_CheckoutThisVersionMenuLabel=Chec&k Out This Version
-CommitFileDiffViewer_CompareMenuLabel=Compare with Previous &Version
-CommitFileDiffViewer_CompareSideBySideMenuLabel=Compare &Side-by-Side
 CommitFileDiffViewer_CompareWorkingDirectoryMenuLabel=Compare with &Working Tree
 CommitFileDiffViewer_CopyFilePathMenuLabel=&Copy File Path
 CommitFileDiffViewer_CopyAllFilePathsMenuLabel=Cop&y All Paths
 CommitFileDiffViewer_MergeCommitMultiAncestorMessage=This is a merge commit with more than one ancestor.
-CommitFileDiffViewer_OpenBaseInEditorMenuLabel=Open &Base Version
-CommitFileDiffViewer_OpenBaseInEditorMenuWithCommitLabel=Open &Base Version (Commit {0})
-CommitFileDiffViewer_OpenPreviousInEditorMenuWithCommitLabel=Open &Previous Version (Commit {0})
-CommitFileDiffViewer_OpenInEditorMenuWithCommitLabel=Open &This Version (Commit {0})
-CommitFileDiffViewer_OpenInEditorMenuTooltip=Commit message: {0}
-CommitFileDiffViewer_OpenPreviousInEditorMenuLabel=Open &Previous Version
 CommitFileDiffViewer_OpenWorkingTreeVersionInEditorMenuLabel=&Open Working Tree Version
 StagingView_CompareWithEachOtherLabel=Compare with Each Other
 StagingView_CompareWithHeadMenuLabel=Compare with HEAD

commit 9fc760d53f467910c780bf2ba5fd63bb477a470d

DiffEditorOutlinePage: fix quick outline not appearing
The quick outline in the DiffEditor (read-only diff page in the commit
viewer), bound to Cmd/Ctrl-O in the editor, was broken since commit
0e458fca7.

That commit changed the context menu to use command contributions
instead of hard-coded Actions and registered the context menu with the
view site. But there is no view site when the control is created via
the QuickOutlinePopup, and getSite() returns null. An NPE got logged and
the popup never appeared.

So instead of creating the menu and then later remove it again don't
create the menu at all in the first place.

Change-Id: I2c10de5953693d4c715c9cca59cee4f91da5bbc9

org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/commit/DiffEditorOutlinePage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2016, Thomas Wolf <thomas.wolf@paranor.ch>
+ * Copyright (C) 2016, 2026 Thomas Wolf <twolf@apache.org>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -225,6 +225,10 @@ public class DiffEditorOutlinePage extends ContentOutlinePage {
 	}
 
 	private void createContextMenu(TreeViewer viewer) {
+		if (getSite() == null) {
+			// We've been opened via the QuickOutlinePopup: no context menu.
+			return;
+		}
 		MenuManager contextMenu = new MenuManager();
 		contextMenu.setRemoveAllWhenShown(true);
 		contextMenu.addMenuListener(menuManager -> {
@@ -700,7 +704,6 @@ public class DiffEditorOutlinePage extends ContentOutlinePage {
 					}
 				}
 			});
-			tree.setMenu(null);
 			return delegate.getTreeViewer().getControl();
 		}
 

org.eclipse.egit.ui.test/src/org/eclipse/egit/ui/test/commit/CommitEditorTest.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- *  Copyright (c) 2011, 2013 GitHub Inc and others.
+ *  Copyright (c) 2011, 2026 GitHub Inc and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License 2.0
  *  which accompanies this distribution, and is available at
@@ -12,6 +12,7 @@
  *****************************************************************************/
 package org.eclipse.egit.ui.test.commit;
 
+import static org.eclipse.swtbot.swt.finder.matchers.WidgetMatcherFactory.widgetOfType;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
@@ -24,6 +25,7 @@ import org.eclipse.core.runtime.Adapters;
 import org.eclipse.egit.core.RepositoryCache;
 import org.eclipse.egit.ui.JobFamilies;
 import org.eclipse.egit.ui.common.LocalRepositoryTestCase;
+import org.eclipse.egit.ui.internal.UIText;
 import org.eclipse.egit.ui.internal.commit.CommitEditor;
 import org.eclipse.egit.ui.internal.commit.RepositoryCommit;
 import org.eclipse.egit.ui.test.ContextMenuHelper;
@@ -32,8 +34,11 @@ import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotEditor;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotMultiPageEditor;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotStyledText;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTable;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
@@ -95,6 +100,39 @@ public class CommitEditorTest extends LocalRepositoryTestCase {
 		assertTrue(botEditor.getPageCount() > 1);
 		for (String name : botEditor.getPagesTitles())
 			botEditor.activatePage(name);
+		botEditor.close();
+	}
+
+	@Test
+	public void quickOutlineShows() throws Exception {
+		final AtomicReference<IEditorPart> editorRef = new AtomicReference<>();
+		PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
+
+			@Override
+			public void run() {
+				RepositoryCommit repoCommit = new RepositoryCommit(repository,
+						commit);
+				editorRef.set(CommitEditor.openQuiet(repoCommit));
+			}
+		});
+		assertNotNull(editorRef.get());
+		IEditorPart editor = editorRef.get();
+		assertTrue(editor instanceof CommitEditor);
+		IEditorInput input = editor.getEditorInput();
+		assertNotNull(input);
+		SWTBotMultiPageEditor botEditor = bot
+				.multipageEditorByTitle(input.getName());
+		assertNotNull(botEditor);
+		assertTrue(botEditor.getPageCount() > 1);
+		botEditor.activatePage(UIText.DiffEditor_Title);
+		SWTBotStyledText botText = new SWTBotStyledText(
+				botEditor.bot().widget(widgetOfType(StyledText.class), 1));
+		ContextMenuHelper.clickContextMenu(botText,
+				UIText.DiffEditor_QuickOutlineAction);
+		SWTBotShell botShell = bot.activeShell();
+		assertNotNull(botShell.bot()
+				.label(UIText.DiffEditor_QuickOutlineFilterDescription));
+		botShell.close();
 		botEditor.close();
 	}
 