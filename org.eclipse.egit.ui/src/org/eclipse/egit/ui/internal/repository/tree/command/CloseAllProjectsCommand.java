9c5d2b49f49ba036d038df4fe250259f21c867a3
Lars Vogel <Lars.Vogel@vogella.com>
2026-05-15 20:13:17 +0200

    Repositories view: add Close/Open All Projects submenu
    Add "Close All Projects" and "Open All Projects" commands to the Git
    Repositories view, grouped together with the existing "Import Projects"
    entry under a new "Projects" submenu.
    
    "Close All Projects" closes all open Eclipse projects mapped to the
    selected repository. "Open All Projects" opens all closed projects
    whose location falls under the repository's working tree.
    
    Change-Id: Ic72fe45fb949d8740924d2729e346697d06e1afe

---
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/repository/tree/command/CloseAllProjectsCommand.java
@@ -0,0 +1,108 @@
+/*******************************************************************************
+ * Copyright (c) 2026 Lars Vogel and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Lars Vogel - initial implementation
+ *******************************************************************************/
+package org.eclipse.egit.ui.internal.repository.tree.command;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.WorkspaceJob;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubMonitor;
+import org.eclipse.egit.core.internal.util.ProjectUtil;
+import org.eclipse.egit.ui.Activator;
+import org.eclipse.egit.ui.internal.UIText;
+import org.eclipse.egit.ui.internal.repository.tree.RepositoryNode;
+import org.eclipse.egit.ui.internal.repository.tree.RepositoryTreeNode;
+import org.eclipse.jgit.lib.Repository;
+
+/**
+ * Closes all open projects belonging to the selected repository.
+ */
+public class CloseAllProjectsCommand
+		extends RepositoriesViewCommandHandler<RepositoryTreeNode> {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		List<RepositoryTreeNode> nodes = getSelectedNodes(event);
+		if (nodes.isEmpty()) {
+			return null;
+		}
+		Repository repository = nodes.get(0).getRepository();
+		if (repository == null || repository.isBare()) {
+			return null;
+		}
+		IProject[] projects = ProjectUtil.getProjects(repository);
+		List<IProject> openProjects = new ArrayList<>();
+		for (IProject project : projects) {
+			if (project.isOpen()) {
+				openProjects.add(project);
+			}
+		}
+		if (openProjects.isEmpty()) {
+			return null;
+		}
+		WorkspaceJob job = new WorkspaceJob(MessageFormat.format(
+				UIText.CloseAllProjectsCommand_jobTitle,
+				repository.getWorkTree().getName())) {
+			@Override
+			public IStatus runInWorkspace(IProgressMonitor monitor)
+					throws CoreException {
+				SubMonitor progress = SubMonitor.convert(monitor,
+						openProjects.size());
+				for (IProject project : openProjects) {
+					if (progress.isCanceled()) {
+						return Status.CANCEL_STATUS;
+					}
+					try {
+						project.close(progress.newChild(1));
+					} catch (CoreException e) {
+						Activator.logError(e.getMessage(), e);
+					}
+				}
+				return Status.OK_STATUS;
+			}
+		};
+		job.setUser(true);
+		job.schedule();
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		List<RepositoryTreeNode> nodes = getSelectedNodes();
+		if (nodes.size() != 1) {
+			return false;
+		}
+		RepositoryTreeNode node = nodes.get(0);
+		if (!(node instanceof RepositoryNode)) {
+			return false;
+		}
+		Repository repository = node.getRepository();
+		if (repository == null || repository.isBare()) {
+			return false;
+		}
+		for (IProject project : ProjectUtil.getProjects(repository)) {
+			if (project.isOpen()) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
