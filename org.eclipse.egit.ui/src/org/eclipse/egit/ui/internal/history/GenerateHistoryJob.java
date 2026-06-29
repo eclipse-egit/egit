92b0dcaef97b8ec4966f1e4df93a74e9a45d7128
Federico Jeanne <Federico.Jeanne@vector.com>
2026-06-29 08:52:30 +0200

    [History view] Don't set label inside GenerateHistoryJob::updateUI
    Set the warning label explicitly in the run method of the job. Do it
    only once, when all wanted elements have been loaded.
    
    Bug: egit-167
    Change-Id: I0909ffd73ebfadb275ec5269491d75a3caaf4c6a

--- org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GenerateHistoryJob.java
+++ org.eclipse.egit.ui/src/org/eclipse/egit/ui/internal/history/GenerateHistoryJob.java
@@ -162,6 +162,8 @@ class GenerateHistoryJob extends Job {
 					monitor.setTaskName(MessageFormat.format(
 							UIText.GenerateHistoryJob_taskFoundCommits,
 							Integer.valueOf(oldsz)));
+
+					updateUI(incomplete);
 				}
 			} catch (IOException e) {
 				status = new Status(IStatus.ERROR, Activator.PLUGIN_ID,
@@ -187,8 +189,17 @@ class GenerateHistoryJob extends Job {
 					updateUI(incomplete);
 				}
 			}
-			else
+			else {
 				updateUI(incomplete);
+			}
+
+			if (forcedRedrawsAfterListIsCompleted < 1 && !loadIncrementally
+					&& hasMore) {
+				page.setWarningTextInUIThread(this);
+			} else {
+				page.clearWarningTextInUIThread(this);
+			}
+
 		} finally {
 			monitor.done();
 			if (trace)
@@ -211,7 +222,7 @@ class GenerateHistoryJob extends Job {
 				forcedRedrawsAfterListIsCompleted++;
 			final SWTCommit[] asArray = new SWTCommit[loadedCommits.size()];
 			loadedCommits.toArray(asArray);
-			page.showCommitList(this, loadedCommits, asArray, commitToShow, incomplete, highlightFlag);
+			page.showCommitList(this, loadedCommits, asArray, commitToShow, highlightFlag);
 			commitToShow = null;
 			lastUpdateCnt = loadedCommits.size();
 		} finally {
